cd24846b8d6961fb4bea58e619368be2fb921e54
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2022-05-18 09:26:48 +0800

    Chore: Change order

--- tasks/Alibaba.DingTalk#Down/Task.ps1
+++ tasks/Alibaba.DingTalk#Down/Task.ps1
@@ -15,13 +15,13 @@ $Fetch = {
     # InstallerUrl
     $Result.InstallerUrl = $Object.win.package.url
 
-    # ReleaseNotes
-    $Result.ReleaseNotes = $Object.win.install.description[1..($Object.win.install.description.length - 1)] -join "`n" | Format-Text
-
     # ReleaseTime
     if ($Object.win.install.description[0] -cmatch '(\d{4}-\d{1,2}-\d{1,2})') {
         $Result.ReleaseTime = Get-Date -Date $Matches[1].Trim() -Format 'yyyy-MM-dd'
     }
+    
+    # ReleaseNotes
+    $Result.ReleaseNotes = $Object.win.install.description[1..($Object.win.install.description.length - 1)] -join "`n" | Format-Text
 
     return [PSCustomObject]$Result
 }
