bf22e4d9d7a2595379dc4a9a8abd5f74858786c0
SpecterShell <spectopo@outlook.com>
2022-07-03 23:28:50 +0800

    chore: Add Maxthon.Maxthon.6

---
+++ tasks/Maxthon.Maxthon.6#Intl#Up#x64/Task.ps1
@@ -0,0 +1,57 @@
+$Config = @{
+    Identifier = 'Maxthon.Maxthon.6'
+    Skip       = $false
+    Notes      = '国际版 升级源 64 位'
+}
+
+$Ping = {
+    $Uri = 'https://updater.maxthon.com/mx6/com/updater.json'
+    $Object = (Invoke-RestMethod -Uri $Uri).maxthon | Where-Object -Property 'channels' -Contains -Value 'stable'
+
+    $Result = [ordered]@{}
+
+    # Version
+    $Result.Version = $Object.version
+
+    # InstallerUrl
+    $Result.InstallerUrl = $Object.url
+
+    return $Result
+}
+
+$Pong = {
+    param (
+        [parameter(Mandatory)]
+        $Result
+    )
+
+    $Uri2 = 'https://www.maxthon.com/mx6/changelog/'
+    $Object2 = Invoke-WebRequest -Uri $Uri2 | ConvertFrom-Html
+
+    if ($Object2.SelectSingleNode('//*[@id="app"]/div[1]/h2[1]/text()').InnerText.Contains($Result.Version)) {
+        # ReleaseTime
+        $Result.ReleaseTime = [regex]::Match(
+            $Object2.SelectSingleNode('//*[@id="app"]/div[1]/h2[1]/time').InnerText,
+            '(\d{4}-\d{1,2}-\d{1,2})'
+        ).Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
+
+        # ReleaseNotes
+        $Result.ReleaseNotes = $Object2.SelectNodes('//*[@id="app"]/div[1]/p[1]/span').InnerText | Format-Text
+    }
+    else {
+        # ReleaseTime
+        $Result.ReleaseTime = $null
+
+        # ReleaseNotes
+        $Result.ReleaseNotes = $null
+    }
+
+    # ReleaseNotesUrl
+    $Result.ReleaseNotesUrl = $Uri2
+}
+
+return @{
+    Config = $Config
+    Ping   = $Ping
+    Pong   = $Pong
+}
