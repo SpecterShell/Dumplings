25ff5cba0260a02181efeec110149044c0ec27ea
SpecterShell <spectopo@outlook.com>
2022-07-24 11:10:55 +0800

    chore: Fix task

--- tasks/#Wondershare/Task.ps1
+++ tasks/#Wondershare/Task.ps1
@@ -1,19 +1,25 @@
 $Config = @{
-    Skip       = $false
+    Skip = $false
 }
 
 $Ping = {
     $script:WondershareUpgradeInfo = @{}
 
     $Products = Get-Content -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Assets_Products.yaml') | ConvertFrom-Yaml
-    (@('x64', 'x86') | ForEach-Object -Process {
-        Invoke-RestMethod -Uri 'https://pc-api.300624.com/v2/product/batch-check-upgrade' -Method Post -Body (
-            @{
-                platform = "win_${_}"
-                versions = $Products.Where({ $_.x86 -eq ($_ -eq 'x86') }).ForEach({ @{pid = $_.ProductId.ToString(); version = $_.Version } })
-            } | ConvertTo-Json -Compress
-        )
-    }).data.ForEach(
+    @('x64', 'x86').ForEach(
+        {
+            $Arch = $_
+            $Request = @{
+                Uri    = 'https://pc-api.300624.com/v2/product/batch-check-upgrade'
+                Method = 'Post'
+                Body   = @{
+                    platform = "win_${Arch}"
+                    versions = $Products.Where({ $_.x86 -eq ($Arch -eq 'x86') }).ForEach({ @{pid = $_.ProductId.ToString(); version = $_.Version } })
+                } | ConvertTo-Json -Compress
+            }
+            Invoke-RestMethod @Request
+        }
+    ).data.ForEach(
         {
             $script:WondershareUpgradeInfo[$_.pid.ToString()] = [ordered]@{
                 # Version
