170d0b6f3754064ac52d97aee4778deabe587122
SpecterShell <spectopo@outlook.com>
2022-09-16 20:32:18 +0800

    fix: Bilibili.Livehime change API

--- tasks/Bilibili.Livehime#x64/Task.ps1
+++ tasks/Bilibili.Livehime#x64/Task.ps1
@@ -1,22 +1,23 @@
 $Config = @{
     Identifier = 'Bilibili.Livehime'
     Skip       = $false
+    Notes      = '64 位'
 }
 
 $Ping = {
-    $Uri1 = 'https://api.live.bilibili.com/client/v1/LiveHime/download?type=3'
+    $Uri1 = 'https://api.live.bilibili.com/xlive/app-blink/v1/liveVersionInfo/getHomePageLiveVersion?system_version=2'
     $Object1 = Invoke-RestMethod -Uri $Uri1
 
     $Result = [ordered]@{}
 
     # Version
-    $Result.Version = [regex]::Match($Object1.data.version, '([\d\.]+)').Groups[1].Value
+    $Result.Version = $Object1.data.curr_version
 
     # InstallerUrl
-    $Result.InstallerUrl = $Object1.data.dl_url
+    $Result.InstallerUrl = $Object1.data.download_url
 
     # ReleaseNotes
-    $Result.ReleaseNotes = $Object1.data.ver_desc | Format-Text
+    $Result.ReleaseNotes = $Object1.data.instruction | Format-Text
 
     # ReleaseNotesUrl
     $Result.ReleaseNotesUrl = 'https://live.bilibili.com/liveHime/'
@@ -33,7 +34,7 @@ $Pong = {
     $Uri2 = 'https://api.live.bilibili.com/client/v1/LiveHime/getVerList?type=3'
     $Object2 = Invoke-RestMethod -Uri $Uri2
 
-    $ReleaseNotes = $Object2.data.list | Where-Object -FilterScript { $_.title.Contains($Result.Version) }
+    $ReleaseNotes = $Object2.data.list.Where({ $_.title.Contains($Result.Version) })
     if ($ReleaseNotes) {
         # ReleaseTime
         $Result.ReleaseTime = [regex]::Match($ReleaseNotes.title, '(\d{4}-\d{1,2}-\d{1,2})').Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
