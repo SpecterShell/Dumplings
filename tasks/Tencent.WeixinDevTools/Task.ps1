529a5d5aa203aaa4b28b58ac9d82a0e40985e4e0
SpecterShell <spectopo@outlook.com>
2022-09-19 20:05:51 +0800

    fix: Tencent.WeixinDevTools ReleaseTime

--- tasks/Tencent.WeixinDevTools/Task.ps1
+++ tasks/Tencent.WeixinDevTools/Task.ps1
@@ -8,16 +8,19 @@ $Ping = {
     $Object = Invoke-WebRequest -Uri $Uri | Read-ResponseContent | ConvertFrom-Json
 
     $Result = [ordered]@{}
-    
+
     # Version
     $Version = $Object.update_version.ToString()
     $Result.Version = $Version.SubString(0, 1) + '.' + $Version.SubString(1, 2) + '.' + $Version.SubString(3)
-    
+
     # InstallerUrl
     $Result.InstallerUrl = @(
         Get-RedirectedUrl -Uri "https://servicewechat.com/wxa-dev-logic/download_redirect?os=win&type=ia32&download_version=$($Object.update_version)&version_type=1&pack_type=0"
         Get-RedirectedUrl -Uri "https://servicewechat.com/wxa-dev-logic/download_redirect?os=win&type=x64&download_version=$($Object.update_version)&version_type=1&pack_type=0"
     )
+
+    # ReleaseTime
+    $Result.ReleaseTime = [datetime]::ParseExact($Version.SubString(3, 6), 'yyMMdd', $null).ToString('yyyy-MM-dd')
 
     # ReleaseNotesCN
     $Result.ReleaseNotesCN = $Object.changelog_desc | Format-Text
