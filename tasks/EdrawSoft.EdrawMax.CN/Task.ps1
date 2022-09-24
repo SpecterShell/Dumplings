01e9dbe442eda3c1e70d5e14697370faed626575
SpecterShell <spectopo@outlook.com>
2022-09-24 20:42:09 +0800

    chore: EdrawSoft.EdrawMax.CN add ReleaseTime

--- tasks/EdrawSoft.EdrawMax.CN/Task.ps1
+++ tasks/EdrawSoft.EdrawMax.CN/Task.ps1
@@ -10,7 +10,7 @@ $Ping = {
     $Result.InstallerUrl = "https://cc-download.edrawsoft.cn/cbs_down/edraw-max_cn_$($Result.version)_full5374.exe"
 
     # ReleaseNotesUrl
-    $Result.ReleaseNotesUrl = 'https://www.edrawsoft.cn/download/edrawmax/versioninfo/'
+    $Result.ReleaseNotesUrl = 'https://www.edrawmax.cn/helpcenter/pc/releases'
 
     return $Result
 }
@@ -20,6 +20,18 @@ $Pong = {
         [parameter(Mandatory)]
         $Result
     )
+
+    $Uri2 = 'https://pixsoapi.edrawsoft.cn/api/helpcenter/draw/tutorial/list?class_id=6&product=2&platform=2'
+    $Object2 = (Invoke-RestMethod -Uri $Uri2).data[0].content | ConvertFrom-Html
+
+    if ($Object2.SelectSingleNode("/h1[contains(text(), `"$($Result.Version)`")]").InnerText.Contains($Result.Version)) {
+        # ReleaseTime
+        $Result.ReleaseTime = $Object2.SelectSingleNode("/h1[contains(text(), `"$($Result.Version)`")]/following-sibling::p[1]/span").InnerText | Get-Date -Format 'yyyy-MM-dd'
+    }
+    else {
+        # ReleaseTime
+        $Result.ReleaseTime = $null
+    }
 
     # RealVersion
     $Result.RealVersion = Get-TempFile -Uri $Result.InstallerUrl | Read-ProductVersionFromExe
