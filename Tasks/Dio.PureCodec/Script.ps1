427676e81e8ead3c6d9957d6f9bf29f68f02d882
SpecterShell <spectopo@outlook.com>
2022-12-07 09:55:31 +0800

    Chore: Add tasks

---
+++ Tasks/Dio.PureCodec/Script.ps1
@@ -0,0 +1,47 @@
+$Object1 = Invoke-WebRequest -Uri 'https://www.wmzhe.com/soft-13163.html' | ConvertFrom-Html
+
+# Version
+$Task.CurrentState.Version = $Object1.SelectSingleNode('//*[@id="app"]/div[3]/div[1]/div[1]/div[2]/div[1]/ul[1]/li[4]/span[2]').InnerText.Trim()
+
+# Installer
+$Task.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.SelectSingleNode('//*[@id="download_group"]/li[9]/dl/dd/a').Attributes['href'].Value
+}
+
+# ReleaseTime
+$Task.CurrentState.ReleaseTime = [datetime]::ParseExact($Task.CurrentState.Version, 'yyyyMMdd', $null).ToString('yyyy-MM-dd')
+
+switch (Compare-State) {
+  ({ $_ -ge 1 }) {
+    $Object2 = Invoke-WebRequest -Uri 'http://qxys.3vfree.cn/ShowPost.asp?ThreadID=206' | ConvertFrom-Html
+
+    try {
+      $ReleaseNotesNodes = @()
+      switch ($Object2.SelectNodes("//*[@id='Post892']/td/table/tr[1]/td[2]/div[2]/div[2]/node()[string()='$($Task.CurrentState.Version)']/following-sibling::node()")) {
+        ({ $_.Name -eq 'br' -and $_.NextSibling.Name -eq 'br' }) { break }
+        Default { $ReleaseNotesNodes += $_ }
+      }
+
+      if ($ReleaseNotesNodes) {
+        # ReleaseNotes (zh-CN)
+        $Task.CurrentState.Locale += [ordered]@{
+          Locale = 'zh-CN'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
+        }
+      } else {
+        Write-Host -Object "Task $($Task.Name): No ReleaseNotes for version $($Task.CurrentState.Version)" -ForegroundColor Yellow
+      }
+    } catch {
+      Write-Host -Object "Task $($Task.Name): ${_}" -ForegroundColor Yellow
+    }
+
+    Write-State
+  }
+  ({ $_ -ge 2 }) {
+    Send-VersionMessage
+  }
+  ({ $_ -ge 3 }) {
+    New-Manifest
+  }
+}
---
+++ Tasks/Nutstore.Nutstore/Script.ps1
@@ -0,0 +1,63 @@
+$Response = Invoke-RestMethod -Uri 'https://www.jianguoyun.com/static/exe/latestVersion'
+# x86 + x64
+$Object1 = $Response | Where-Object -Property 'OS' -EQ -Value 'win-wpf-client'
+# arm64
+$Object2 = $Response | Where-Object -Property 'OS' -EQ -Value 'win-wpf-client-arm64'
+
+# Version
+$Task.CurrentState.Version = $Object1.exVer
+
+if ($Object1.exVer -ne $Object2.exVer) {
+  Write-Host -Object "Task $($Task.Name): The versions are different between the architectures"
+  $Task.Config.Notes = '各个架构的版本号不相同'
+}
+
+# Installer
+$Task.CurrentState.Installer += [ordered]@{
+  Architecture = 'x86'
+  InstallerUrl = "https://pkg-cdn.jianguoyun.com/static/exe/installer/$($Task.CurrentState.Version)/NutstoreWindowsWPF_Full_$($Task.CurrentState.Version).exe"
+}
+$Task.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = "https://pkg-cdn.jianguoyun.com/static/exe/installer/$($Task.CurrentState.Version)/NutstoreWindowsWPF_Full_$($Task.CurrentState.Version).exe"
+}
+$Task.CurrentState.Installer += [ordered]@{
+  Architecture = 'arm64'
+  InstallerUrl = "https://pkg-cdn.jianguoyun.com/static/exe/installer/$($Task.CurrentState.Version)/NutstoreWindowsWPF_Full_$($Task.CurrentState.Version)_ARM64.exe"
+}
+
+switch (Compare-State) {
+  ({ $_ -ge 1 }) {
+    $Object3 = Invoke-WebRequest -Uri 'https://help.jianguoyun.com/?p=1415' | ConvertFrom-Html
+
+    try {
+      $ReleaseNotesNode = $Object3.SelectSingleNode("//*[@id='post-1415']/div/p[contains(./strong/text(), '$($Task.CurrentState.Version)')]")
+      if ($ReleaseNotesNode) {
+        # ReleaseTime
+        $Task.CurrentState.ReleaseTime = [regex]::Match(
+          $ReleaseNotesNode.SelectSingleNode('./strong').InnerText,
+          '(\d{4}年\d{1,2}月\d{1,2}日)'
+        ).Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
+
+        # ReleaseNotes (zh-CN)
+        $Task.CurrentState.Locale += [ordered]@{
+          Locale = 'zh-CN'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesNode.SelectSingleNode('./following-sibling::ol[1]') | Get-TextContent | Format-Text
+        }
+      } else {
+        Write-Host -Object "Task $($Task.Name): No ReleaseTime and ReleaseNotes for version $($Task.CurrentState.Version)" -ForegroundColor Yellow
+      }
+    } catch {
+      Write-Host -Object "Task $($Task.Name): ${_}" -ForegroundColor Yellow
+    }
+
+    Write-State
+  }
+  ({ $_ -ge 2 }) {
+    Send-VersionMessage
+  }
+  ({ $_ -ge 3 -and $Object1.exVer -eq $Object2.exVer }) {
+    New-Manifest
+  }
+}
