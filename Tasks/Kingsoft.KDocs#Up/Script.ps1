aaf3adbdfcfdce7cd1a2694901b59b015152f828
SpecterShell <spectopo@outlook.com>
2022-12-14 11:43:58 +0800

    Chore: Add task

---
+++ Tasks/Kingsoft.KDocs#Up/Script.ps1
@@ -0,0 +1,26 @@
+$Object = (Invoke-RestMethod -Uri 'https://www.kdocs.cn/kdg/api/v1/configure?idList=kdesktopWinVersion').data.kdesktopWinVersion | ConvertFrom-Json
+# $Object = (Invoke-RestMethod -Uri 'https://www.kdocs.cn/kd/api/configure/list?idList=kdesktopWinVersion').data.kdesktopWinVersion | ConvertFrom-Json
+
+# Version
+$Task.CurrentState.Version = $Object.version
+
+# Installer
+$Task.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object.url
+}
+
+# ReleaseNotes (zh-CN)
+$Task.CurrentState.Locale += [ordered]@{
+  Locale = 'zh-CN'
+  Key    = 'ReleaseNotes'
+  Value  = $Object.changes | Format-Text
+}
+
+switch (Compare-State) {
+  ({ $_ -ge 1 }) {
+    Write-State
+  }
+  ({ $_ -ge 2 }) {
+    Send-VersionMessage
+  }
+}
