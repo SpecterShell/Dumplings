229849ffd19cba167fdc679d12fddc630120f593
SpecterShell <spectopo@outlook.com>
2022-12-26 17:46:49 +0800

    Chore: Add PerfectWorld.PerfectWorldArena

---
+++ Tasks/PerfectWorld.PerfectWorldArena/Script.ps1
@@ -0,0 +1,28 @@
+$Prefix = 'https://client.wmpvp.com/download/'
+
+$Task.CurrentState = Invoke-RestMethod -Uri "${Prefix}latest.yml?noCache=$((New-Guid).Guid.Split('-')[0])" | ConvertFrom-Yaml | ConvertFrom-ElectronUpdater -Prefix $Prefix -Locale 'zh-CN'
+
+switch (Compare-State) {
+  ({ $_ -ge 1 }) {
+    $Object = Invoke-RestMethod -Uri "https://pwaweblogin.wmpvp.com/platform/version?v=$($Task.CurrentState.Version)"
+
+    try {
+      # ReleaseNotes (zh-CN)
+      $Task.CurrentState.Locale += [ordered]@{
+        Locale = 'zh-CN'
+        Key    = 'ReleaseNotes'
+        Value  = $Object.data.content | ConvertFrom-Html | Get-TextContent | Format-Text
+      }
+    } catch {
+      Write-Host -Object "Task $($Task.Name): ${_}" -ForegroundColor Yellow
+    }
+
+    Write-State
+  }
+  ({ $_ -ge 2 }) {
+    Send-VersionMessage
+  }
+  ({ $_ -ge 3 }) {
+    New-Manifest
+  }
+}
