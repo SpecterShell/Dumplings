fd6282ef76c78d6f7841189f298e5d61f534f7f5
SpecterShell <spectopo@outlook.com>
2022-12-28 20:16:33 +0800

    Chore: Add Kiraku.Neatify

---
+++ Tasks/Kiraku.Neatify/Script.ps1
@@ -0,0 +1,43 @@
+$Object = Invoke-RestMethod -Uri 'https://api.neatifyapp.com/auth/version/latest?appId=5&platform=0'
+
+# Installer
+$InstallerUrl = $Object.data.downloadUrl | ConvertTo-UnescapedUri
+$Task.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $InstallerUrl
+}
+
+# Version
+$Task.CurrentState.Version = [regex]::Match($InstallerUrl, '(\d+\.\d+\.\d+\-\d+)').Groups[1].Value
+
+# ReleaseTime
+$Task.CurrentState.ReleaseTime = $Object.data.createTime | ConvertFrom-UnixTimeMilliseconds
+
+# ReleaseNotes (zh-CN)
+$ReleaseNotesObject = $Object.data.updateLog | ConvertFrom-Json
+$ReleaseNotesList = @()
+if ($ReleaseNotesObject.added) {
+  $ReleaseNotesList += $ReleaseNotesObject.added -creplace '^', '[+ADDED] '
+}
+if ($ReleaseNotesObject.changed) {
+  $ReleaseNotesList += $ReleaseNotesObject.changed -creplace '^', '[*CHANGED] '
+}
+if ($ReleaseNotesObject.fixed) {
+  $ReleaseNotesList += $ReleaseNotesObject.fixed -creplace '^', '[-FIXED] '
+}
+$Task.CurrentState.Locale += [ordered]@{
+  Locale = 'zh-CN'
+  Key    = 'ReleaseNotes'
+  Value  = $ReleaseNotesList | Format-Text
+}
+
+switch (Compare-State) {
+  ({ $_ -ge 1 }) {
+    Write-State
+  }
+  ({ $_ -ge 2 }) {
+    Send-VersionMessage
+  }
+  ({ $_ -ge 3 }) {
+    New-Manifest
+  }
+}
