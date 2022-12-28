49e7e56c5a2e93f3e666b75930196b89038d520d
SpecterShell <spectopo@outlook.com>
2022-12-28 12:45:39 +0800

    Chore: Add YY.YYAnchor

---
+++ Tasks/YY.YYAnchor/Script.ps1
@@ -0,0 +1,47 @@
+$UniVer = $Task.LastState.UniVer ?? '12C0F000'
+$Time = Get-Date -Format 'yyyyMMddHHmmss'
+$Hash = [System.BitConverter]::ToString(
+  [System.Security.Cryptography.MD5CryptoServiceProvider]::HashData(
+    [System.Text.Encoding]::UTF8.GetBytes("appid=yyanchor&timestamp=${Time}&k=BPX4%pISczDL5rd^2ot&1SRjpF7@0hET")
+  )
+).Replace('-', '').ToLower()
+$Object1 = Invoke-RestMethod -Uri "https://up.yy.com/api/check/yyanchor/check4update?timestamp=${Time}&sourceVersion=${UniVer}&n=${Hash}&manual=1"
+
+if ($Object1.code -ne 0) {
+  throw "Task $($Task.Name): $($Object1.message)"
+}
+
+$Object2 = Invoke-RestMethod -Uri "http://forceupdate.yy.com$($Object1.data.configPath)"
+
+# Version
+$Task.CurrentState.Version = $Object2.Product.Version.VerNo
+
+# UniVer
+$Task.CurrentState.UniVer = $Object2.Product.Version.UniVer
+
+# Installer
+$Task.CurrentState.Installer += [ordered]@{
+  InstallerUrl = "https://yydl.yy.com/$($Object2.Product.Version.Pack.FileUrl)"
+}
+
+# ReleaseNotes (zh-CN)
+$Task.CurrentState.Locale += [ordered]@{
+  Locale = 'zh-CN'
+  Key    = 'ReleaseNotes'
+  Value  = $Object1.data.discription | Format-Text
+}
+
+switch (Compare-State) {
+  ({ $_ -ge 1 }) {
+    # RealVersion
+    $Task.CurrentState.RealVersion = Get-TempFile -Uri $Task.CurrentState.Installer[0].InstallerUrl | Read-ProductVersionFromExe
+
+    Write-State
+  }
+  ({ $_ -ge 2 }) {
+    Send-VersionMessage
+  }
+  ({ $_ -ge 3 }) {
+    New-Manifest
+  }
+}
