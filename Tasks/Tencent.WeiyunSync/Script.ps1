5ec16188dd55174e8b04b752eac036b0d3421931
SpecterShell <spectopo@outlook.com>
2023-01-01 19:04:31 +0800

    Chore: Add Tencent.WeiyunSync

---
+++ Tasks/Tencent.WeiyunSync/Script.ps1
@@ -0,0 +1,24 @@
+$Object = Invoke-RestMethod -Uri 'https://jsonschema.qpic.cn/2993ffb0f5d89de287319113301f3fca/179b0d35c9b088e5e72862a680864254/config'
+
+# Version
+$Task.CurrentState.Version = $Object.windows_sync.version
+
+# RealVersion
+$Task.CurrentState.RealVersion = [regex]::Match($Task.CurrentState.Version, '(\d+\.\d+)').Groups[1].Value
+
+# Installer
+$Task.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object.windows_sync.download_url
+}
+
+# ReleaseTime
+$Task.CurrentState.ReleaseTime = $Object.windows_sync.date | Get-Date -Format 'yyyy-MM-dd'
+
+switch (Compare-State) {
+  ({ $_ -ge 1 }) {
+    Write-State
+  }
+  ({ $_ -ge 2 }) {
+    Send-VersionMessage
+  }
+}
