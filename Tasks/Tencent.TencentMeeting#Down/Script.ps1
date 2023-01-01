d1f67dfe2c044e6e55483f91596070eba9236cdf
SpecterShell <spectopo@outlook.com>
2023-01-01 17:33:58 +0800

    Chore: Add Tencent.TencentMeeting

---
+++ Tasks/Tencent.TencentMeeting#Down/Script.ps1
@@ -0,0 +1,24 @@
+$Object = Invoke-RestMethod -Uri 'https://meeting.tencent.com/web-service/query-download-info?q=[{%22package-type%22:%22app%22,%22channel%22:%220300000000%22,%22platform%22:%22windows%22}]&nonce=AAAAAAAAAAAAAAAA'
+
+# Version
+$Task.CurrentState.Version = $Object.'info-list'[0].version
+
+# Installer
+$Task.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object.'info-list'[0].url
+}
+
+# ReleaseTime
+$Task.CurrentState.ReleaseTime = $Object.'info-list'[0].'sub-date' | Get-Date -Format 'yyyy-MM-dd'
+
+switch (Compare-State) {
+  ({ $_ -ge 1 }) {
+    Write-State
+  }
+  ({ $_ -ge 2 }) {
+    Send-VersionMessage
+  }
+  ({ $_ -ge 3 }) {
+    New-Manifest
+  }
+}
---
+++ Tasks/Tencent.TencentMeeting#Up/Script.ps1
@@ -0,0 +1,28 @@
+$Object = Invoke-RestMethod -Uri 'https://meeting.tencent.com/web-service/query-app-update-info/?os=Windows&app_publish_channel=TencentInside&sdk_id=0300000000&from=2&appver=3.12.0.0'
+
+# Version
+$Task.CurrentState.Version = $Object.version
+
+# Installer
+$Task.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object.package_url
+}
+
+# ReleaseNotes (zh-CN)
+$Task.CurrentState.Locale += [ordered]@{
+  Locale = 'zh-CN'
+  Key    = 'ReleaseNotes'
+  Value  = $Object.features_description | Format-Text
+}
+
+switch (Compare-State) {
+  ({ $_ -ge 1 }) {
+    Write-State
+  }
+  ({ $_ -ge 2 }) {
+    Send-VersionMessage
+  }
+  ({ $_ -ge 3 }) {
+    New-Manifest
+  }
+}
