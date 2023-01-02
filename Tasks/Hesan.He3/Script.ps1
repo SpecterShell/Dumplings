4e202804d76e9564fa359955d7c3a56935ac3a8a
SpecterShell <spectopo@outlook.com>
2023-01-03 01:11:46 +0800

    Chore: Add Hesan.He3

---
+++ Tasks/Hesan.He3/Script.ps1
@@ -0,0 +1,40 @@
+$Prefix = 'https://he3-1309519128.cos.accelerate.myqcloud.com/'
+
+$Object = Invoke-RestMethod -Uri "${Prefix}latest/latest.yml?noCache=$((New-Guid).Guid.Split('-')[0])" | ConvertFrom-Yaml
+
+# Version
+$Task.CurrentState.Version = $Object.version
+
+# Installer
+$Task.CurrentState.Installer += [ordered]@{
+  Architecture = 'x86'
+  InstallerUrl = $Prefix + $Task.CurrentState.Version + '/' + $Object.files.Where({ $_.url.Contains('ia32') })[0].url
+}
+$Task.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = $Prefix + $Task.CurrentState.Version + '/' + $Object.files.Where({ $_.url.Contains('x64') })[0].url
+}
+
+# ReleaseTime
+$Task.CurrentState.ReleaseTime = $Object.releaseDate.ToUniversalTime()
+
+# ReleaseNotes (zh-CN)
+if ($Object.releaseNotes) {
+  $Task.CurrentState.Locale += [ordered]@{
+    Locale = 'zh-CN'
+    Key    = 'ReleaseNotes'
+    Value  = $Object.releaseNotes | Format-Text
+  }
+}
+
+switch (Compare-State) {
+  ({ $_ -ge 1 }) {
+    Write-State
+  }
+  ({ $_ -ge 2 }) {
+    Send-VersionMessage
+  }
+  ({ $_ -ge 3 }) {
+    New-Manifest
+  }
+}
