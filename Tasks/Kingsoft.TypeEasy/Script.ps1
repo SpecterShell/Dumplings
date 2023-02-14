7b24cf324b093f857de72779dfca0ce9c4df88c0
SpecterShell <spectopo@outlook.com>
2023-02-14 18:41:46 +0800

    Chore: Add Kingsoft.TypeEasy

---
+++ Tasks/Kingsoft.TypeEasy/Script.ps1
@@ -0,0 +1,24 @@
+$Object = (Invoke-RestMethod -Uri 'https://rubik.wps.cn/rubik-service/project/deploy?position=51dzt').data.project_components.Where({ $_.Name -eq 'dzt-banner' })[0].user_props
+
+# Version
+$Task.CurrentState.Version = [regex]::Match($Object.downloadVersion, '（([\d\.]+)）').Groups[1].Value
+
+# Installer
+$Task.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object.downloadLink
+}
+
+# ReleaseTime
+$Task.CurrentState.ReleaseTime = [regex]::Match($Object.downloadVersion, '(\d{4}\.\d{1,2}\.\d{1,2})').Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
+
+switch (Compare-State) {
+  ({ $_ -ge 1 }) {
+    Write-State
+  }
+  ({ $_ -ge 2 }) {
+    Send-VersionMessage
+  }
+  ({ $_ -ge 3 }) {
+    New-Manifest
+  }
+}
