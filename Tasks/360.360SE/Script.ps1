1d1c9c4f96051026f05eb2c76466cf195310ff0c
SpecterShell <spectopo@outlook.com>
2023-02-15 18:29:57 +0800

    Fix: 360.360SE Update tp 14

--- Tasks/360.360SE/Script.ps1
+++ Tasks/360.360SE/Script.ps1
@@ -2,7 +2,7 @@ $Object1 = Invoke-WebRequest -Uri 'https://browser.360.cn/' | ConvertFrom-Html
 
 # Version
 $Task.CurrentState.Version = [regex]::Match(
-  $Object1.SelectSingleNode('//*[@id="loadnew"]/em').InnerText,
+  $Object1.SelectSingleNode('/html/body/div[3]/div/em[1]').InnerText,
   '([\d\.]+)'
 ).Groups[1].Value
 
@@ -13,10 +13,10 @@ $Task.CurrentState.Installer += [ordered]@{
 
 switch (Compare-State) {
   ({ $_ -ge 1 }) {
-    $Object2 = Invoke-WebRequest -Uri 'https://bbs.360.cn/thread-15948589-1-1.html' | ConvertFrom-Html
+    $Object2 = Invoke-WebRequest -Uri 'https://bbs.360.cn/thread-16068958-1-1.html' | ConvertFrom-Html
 
     try {
-      $ReleaseNotesTitle = $Object2.SelectSingleNode('//*[@id="postmessage_118145577"]/strong[3]').InnerText
+      $ReleaseNotesTitle = $Object2.SelectSingleNode('//*[@id="postmessage_118933783"]/strong[7]').InnerText
       if ($ReleaseNotesTitle.Contains($Task.CurrentState.Version)) {
         # ReleaseTime
         $Task.CurrentState.ReleaseTime = [regex]::Match($ReleaseNotesTitle, '(\d{4}\.\d{1,2}\.\d{1,2})').Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
@@ -25,7 +25,7 @@ switch (Compare-State) {
         $Task.CurrentState.Locale += [ordered]@{
           Locale = 'zh-CN'
           Key    = 'ReleaseNotes'
-          Value  = $Object2.SelectNodes('//*[@id="postmessage_118145577"]/strong[3]/following-sibling::node()[count(.|//*[@id="postmessage_118145577"]/strong[4]/preceding-sibling::node())=count(//*[@id="postmessage_118145577"]/strong[4]/preceding-sibling::node())]') | Get-TextContent | Format-Text
+          Value  = $Object2.SelectNodes('//*[@id="postmessage_118933783"]/strong[7]/following-sibling::node()[count(.|//*[@id="postmessage_118933783"]/strong[8]/preceding-sibling::node())=count(//*[@id="postmessage_118933783"]/strong[8]/preceding-sibling::node())]') | Get-TextContent | Format-Text
         }
       } else {
         Write-Host -Object "Task $($Task.Name): No ReleaseTime and ReleaseNotes for version $($Task.CurrentState.Version)" -ForegroundColor Yellow
