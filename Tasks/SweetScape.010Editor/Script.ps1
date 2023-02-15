63570c8180077f35c2d4dccfba64f4b481455a0a
SpecterShell <spectopo@outlook.com>
2023-02-15 18:08:29 +0800

    Chore: Add SweetScape.010Editor

---
+++ Tasks/SweetScape.010Editor/Script.ps1
@@ -0,0 +1,61 @@
+$Object1 = (Invoke-RestMethod -Uri 'https://www.sweetscape.com/cgibin/010editor_check_news_5k.php').Replace('010EditorNews', 'news') | ConvertFrom-Xml
+
+# Version
+$Task.CurrentState.Version = $Object1.news.newversion
+
+# Installer
+$Task.CurrentState.Installer += [ordered]@{
+  Architecture = 'x86'
+  InstallerUrl = Get-RedirectedUrl -Uri 'https://www.sweetscape.com/download/010EditorWin32Installer.exe'
+}
+$Task.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = Get-RedirectedUrl -Uri 'https://www.sweetscape.com/download/010EditorWin64Installer.exe'
+}
+
+switch (Compare-State) {
+  ({ $_ -ge 1 }) {
+    $Object2 = Invoke-WebRequest -Uri 'https://www.sweetscape.com/010editor/release_notes.html' | ConvertFrom-Html
+
+    try {
+      $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//*[@class='contentdiv']/h2[contains(text(), '$($Task.CurrentState.Version)')]")
+      if ($ReleaseNotesTitleNode) {
+        # ReleaseTime
+        $Task.CurrentState.ReleaseTime = [datetime]::ParseExact(
+          $ReleaseNotesTitleNode.InnerText.Split('-')[-1].Trim(),
+          [string[]]@(
+            "MMMM d'st', yyyy",
+            "MMMM d'nd', yyyy",
+            "MMMM d'rd', yyyy",
+            "MMMM d'th', yyyy"
+          ),
+          (Get-Culture -Name 'en-US'),
+          [System.Globalization.DateTimeStyles]::None
+        ).ToString('yyyy-MM-dd')
+
+        # ReleaseNotes (en-US)
+        $ReleaseNotesNodes = @()
+        for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node.Name -ne 'table'; $Node = $Node.NextSibling) {
+          $ReleaseNotesNodes += $Node
+        }
+        $Task.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
+        }
+      } else {
+        Write-Host -Object "Task $($Task.Name): No ReleaseNotes for version $($Task.CurrentState.Version)" -ForegroundColor Yellow
+      }
+    } catch {
+      Write-Host -Object "Task $($Task.Name): ${_}" -ForegroundColor Yellow
+    }
+
+    Write-State
+  }
+  ({ $_ -ge 2 }) {
+    Send-VersionMessage
+  }
+  ({ $_ -ge 3 }) {
+    New-Manifest
+  }
+}
