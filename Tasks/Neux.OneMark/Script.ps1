706feb412a8ef8864a4b06ef058cf34e6a9dfba2
SpecterShell <spectopo@outlook.com>
2023-03-26 14:53:19 +1300

    Fix: Neux.OneMark Fetch English sources

--- Tasks/Neux.OneMark/Script.ps1
+++ Tasks/Neux.OneMark/Script.ps1
@@ -1,15 +1,19 @@
-# x64
-$Object1 = Invoke-RestMethod -Uri 'https://onemark.neuxlab.cn/updates/latest.x64.xml'
-# x86
-$Object2 = Invoke-RestMethod -Uri 'https://onemark.neuxlab.cn/updates/latest.xml'
+# International x64
+$Object1 = Invoke-RestMethod -Uri 'https://onemark.neux.studio/updates/latest.x64.xml'
+# International x86
+$Object2 = Invoke-RestMethod -Uri 'https://onemark.neux.studio/updates/latest.xml'
+# Chinese x64
+$Object3 = Invoke-RestMethod -Uri 'https://onemark.neuxlab.cn/updates/latest.x64.xml'
+# Chinese x86
+$Object4 = Invoke-RestMethod -Uri 'https://onemark.neuxlab.cn/updates/latest.xml'
+
+if ((@($Object1, $Object2, $Object3, $Object4) | Sort-Object -Property { $_.item.version } -Unique).Length -gt 1) {
+  Write-Host -Object "Task $($Task.Name): The versions are different between editions and/or architectures" -ForegroundColor Yellow
+  $Task.Config.Notes = '各个版本和/或架构的版本号不相同'
+}
 
 # Version
 $Task.CurrentState.Version = $Object1.item.version
-
-if ($Object1.item.version -ne $Object2.item.version) {
-  Write-Host -Object "Task $($Task.Name): The versions are different between the architectures"
-  $Task.Config.Notes = '各个架构的版本号不相同'
-}
 
 # Installer
 $Task.CurrentState.Installer += [ordered]@{
@@ -20,12 +24,27 @@ $Task.CurrentState.Installer += [ordered]@{
   Architecture = 'x64'
   InstallerUrl = $Object1.item.url
 }
+$Task.CurrentState.Installer += [ordered]@{
+  InstallerLocale = 'zh-CN'
+  Architecture    = 'x86'
+  InstallerUrl    = $Object4.item.url
+}
+$Task.CurrentState.Installer += [ordered]@{
+  InstallerLocale = 'zh-CN'
+  Architecture    = 'x64'
+  InstallerUrl    = $Object3.item.url
+}
 
 # ReleaseNotesUrl
-$ReleaseNotesUrl = $Object1.item.changelog
 $Task.CurrentState.Locale += [ordered]@{
   Key   = 'ReleaseNotesUrl'
-  Value = $ReleaseNotesUrl
+  Value = $ReleaseNotesUrl1 = $Object1.item.changelog
+}
+# ReleaseNotesUrl (zh-CN)
+$Task.CurrentState.Locale += [ordered]@{
+  InstallerLocale = 'zh-CN'
+  Key             = 'ReleaseNotesUrl'
+  Value           = $ReleaseNotesUrl2 = $Object3.item.changelog
 }
 
 switch (Compare-State) {
@@ -33,14 +52,27 @@ switch (Compare-State) {
     # RealVersion
     $Task.CurrentState.RealVersion = Get-TempFile -Uri $Task.CurrentState.Installer[0].InstallerUrl | Read-ProductVersionFromMsi
 
-    $Object3 = Invoke-WebRequest -Uri $ReleaseNotesUrl | ConvertFrom-Html
+    $Object5 = Invoke-WebRequest -Uri $ReleaseNotesUrl1 | ConvertFrom-Html
+
+    try {
+      # ReleaseNotes (en-US)
+      $Task.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key   = 'ReleaseNotes'
+        Value = $Object5.SelectNodes('//*[@id="main"]/div') | Get-TextContent | Format-Text
+      }
+    } catch {
+      Write-Host -Object "Task $($Task.Name): ${_}" -ForegroundColor Yellow
+    }
+
+    $Object6 = Invoke-WebRequest -Uri $ReleaseNotesUrl2 | ConvertFrom-Html
 
     try {
       # ReleaseNotes (zh-CN)
       $Task.CurrentState.Locale += [ordered]@{
         Locale = 'zh-CN'
         Key    = 'ReleaseNotes'
-        Value  = $Object3.SelectNodes('//*[@id="main"]/div') | Get-TextContent | Format-Text
+        Value  = $Object6.SelectNodes('//*[@id="main"]/div') | Get-TextContent | Format-Text
       }
     } catch {
       Write-Host -Object "Task $($Task.Name): ${_}" -ForegroundColor Yellow
@@ -51,7 +83,7 @@ switch (Compare-State) {
   ({ $_ -ge 2 }) {
     Send-VersionMessage
   }
-  ({ $_ -ge 3 -and $Object1.item.version -eq $Object2.item.version }) {
+  ({ $_ -ge 3 -and (@($Object1, $Object2, $Object3, $Object4) | Sort-Object -Property { $_.item.version } -Unique).Length -eq 1 }) {
     New-Manifest
   }
 }
