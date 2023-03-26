b5fb03542e1829dc810e26969b3395e77b36c102
SpecterShell <spectopo@outlook.com>
2023-03-26 15:27:07 +1300

    Chore: Add Posit.RStudio and Posit.RStudio.Pro

---
+++ Tasks/Posit.RStudio/Script.ps1
@@ -0,0 +1,55 @@
+$Object1 = (Invoke-RestMethod -Uri 'https://www.rstudio.com/wp-content/downloads.json').rstudio.open_source.stable.desktop.installer.windows
+
+# Version
+$Task.CurrentState.Version = $Object1.version
+
+# Installer
+$Task.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.url
+}
+
+# ReleaseTime
+$Task.CurrentState.ReleaseTime = $Object1.last_modified | Get-Date -Format 'yyyy-MM-dd'
+
+switch (Compare-State) {
+  ({ $_ -ge 1 }) {
+    $ReleaseNotesUrl = 'https://docs.posit.co/ide/news/'
+    $Object2 = Invoke-WebRequest -Uri $ReleaseNotesUrl | ConvertFrom-Html
+
+    try {
+      $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//*[@id='quarto-document-content']/section[contains(@id, '$($Task.CurrentState.Version.Split('+')[0])')]")
+      if ($ReleaseNotesTitleNode) {
+        # ReleaseNotes (en-US)
+        $Task.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesTitleNode.SelectNodes('./blockquote[1]/following-sibling::*') | Get-TextContent | Format-Text
+        }
+
+        # ReleaseNotesUrl
+        $Task.CurrentState.Locale += [ordered]@{
+          Key   = 'ReleaseNotesUrl'
+          Value = $ReleaseNotesUrl + '#' + $ReleaseNotesTitleNode.Attributes['id'].Value
+        }
+      } else {
+        # ReleaseNotesUrl
+        $Task.CurrentState.Locale += [ordered]@{
+          Key   = 'ReleaseNotesUrl'
+          Value = $ReleaseNotesUrl
+        }
+
+        Write-Host -Object "Task $($Task.Name): No ReleaseNotes for version $($Task.CurrentState.Version)" -ForegroundColor Yellow
+      }
+    } catch {
+      Write-Host -Object "Task $($Task.Name): ${_}" -ForegroundColor Yellow
+    }
+
+    Write-State
+  }
+  ({ $_ -ge 2 }) {
+    Send-VersionMessage
+  }
+  ({ $_ -ge 3 }) {
+    New-Manifest
+  }
+}
