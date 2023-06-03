65182ffd5f897ac2af1fbe2708d3149d2c021dcd
SpecterShell <spectopo@outlook.com>
2023-06-04 11:04:42 +1200

    Fix: YouXiao.* Remove ReleaseNotes

--- Tasks/YouXiao.YXFile/Script.ps1
+++ Tasks/YouXiao.YXFile/Script.ps1
@@ -13,26 +13,26 @@ $Task.CurrentState.Installer += [ordered]@{
 
 switch (Compare-State) {
   ({ $_ -ge 1 }) {
-    $Object = Invoke-WebRequest -Uri 'https://www.youxiao.cn/wordpress/index.php/yxfile/log/' | ConvertFrom-Html
+    # $Object = Invoke-WebRequest -Uri 'https://www.youxiao.cn/wordpress/index.php/yxfile/log/' | ConvertFrom-Html
 
-    try {
-      $ReleaseNotesTitleNode = $Object.SelectSingleNode("//*[@id='post-930']/div[2]/ul[contains(./li/text(), '$($Task.CurrentState.Version)')]")
-      if ($ReleaseNotesTitleNode) {
-        # ReleaseTime
-        $Task.CurrentState.ReleaseTime = [regex]::Match($ReleaseNotesTitleNode.InnerText, '(\d{4}年\d{1,2}月\d{1,2}日)').Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
+    # try {
+    #   $ReleaseNotesTitleNode = $Object.SelectSingleNode("//*[@id='post-930']/div[2]/ul[contains(./li/text(), '$($Task.CurrentState.Version)')]")
+    #   if ($ReleaseNotesTitleNode) {
+    #     # ReleaseTime
+    #     $Task.CurrentState.ReleaseTime = [regex]::Match($ReleaseNotesTitleNode.InnerText, '(\d{4}年\d{1,2}月\d{1,2}日)').Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
 
-        # ReleaseNotes (zh-CN)
-        $Task.CurrentState.Locale += [ordered]@{
-          Locale = 'zh-CN'
-          Key    = 'ReleaseNotes'
-          Value  = $ReleaseNotesTitleNode.SelectSingleNode('./following-sibling::ol[1]') | Get-TextContent | Format-Text
-        }
-      } else {
-        Write-Host -Object "Task $($Task.Name): No ReleaseTime and ReleaseNotes for version $($Task.CurrentState.Version)" -ForegroundColor Yellow
-      }
-    } catch {
-      Write-Host -Object "Task $($Task.Name): ${_}" -ForegroundColor Yellow
-    }
+    #     # ReleaseNotes (zh-CN)
+    #     $Task.CurrentState.Locale += [ordered]@{
+    #       Locale = 'zh-CN'
+    #       Key    = 'ReleaseNotes'
+    #       Value  = $ReleaseNotesTitleNode.SelectSingleNode('./following-sibling::ol[1]') | Get-TextContent | Format-Text
+    #     }
+    #   } else {
+    #     Write-Host -Object "Task $($Task.Name): No ReleaseTime and ReleaseNotes for version $($Task.CurrentState.Version)" -ForegroundColor Yellow
+    #   }
+    # } catch {
+    #   Write-Host -Object "Task $($Task.Name): ${_}" -ForegroundColor Yellow
+    # }
 
     Write-State
   }
