c63ab9e9c9f264f4675a1fc61fb58616db9876dc
SpecterShell <spectopo@outlook.com>
2023-06-23 23:55:50 +1200

    Chore: Add NetEase.YoudaoPokeClass and Wisedu.CAT

---
+++ Tasks/Wisedu.CAT/Script.ps1
@@ -0,0 +1,21 @@
+$Object = Invoke-WebRequest -Uri 'https://cat.wisedu.com/%e6%9c%8d%e5%8a%a1/%e5%ae%a2%e6%88%b7%e7%ab%af%e4%b8%8b%e8%bd%bd/' | ConvertFrom-Html
+
+# Installer
+$Task.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $InstallerUrl = $Object.SelectSingleNode('//*[contains(@class,"type-page")]/div/div[1]/a[1]').Attributes['href'].Value | ConvertTo-UnescapedUri
+}
+
+# Version
+$Task.CurrentState.Version = [regex]::Match($InstallerUrl, '([\d\.]+)_Setup').Groups[1].Value
+
+switch (Compare-State) {
+  ({ $_ -ge 1 }) {
+    Write-State
+  }
+  ({ $_ -ge 2 }) {
+    Send-VersionMessage
+  }
+  ({ $_ -ge 3 }) {
+    New-Manifest
+  }
+}
