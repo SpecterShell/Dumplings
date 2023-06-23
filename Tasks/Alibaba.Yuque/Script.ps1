d578be094006ed56f7cafa684e97ab6f2ee365ae
SpecterShell <spectopo@outlook.com>
2023-06-23 18:41:59 +1200

    Fix: Alibaba.Yuque Update API and remove ReleaseTime parsing

--- Tasks/Alibaba.Yuque/Script.ps1
+++ Tasks/Alibaba.Yuque/Script.ps1
@@ -1,18 +1,18 @@
-$Object = (Invoke-RestMethod -Uri 'https://app.nlark.com/yuque-desktop/v4/latest.json').stable | Where-Object -Property 'platform' -EQ -Value 'win32'
+$Object = Invoke-RestMethod -Uri 'https://app.nlark.com/yuque-desktop/v5/public/windows-stable.json'
 
 # Version
 $Task.CurrentState.Version = $Object.version
 
 # Installer
 $Task.CurrentState.Installer += [ordered]@{
-  InstallerUrl = $Object.exe_url
+  InstallerUrl = $Object.files.Where({ $_.updateType -eq 'package' }).url
 }
 
 # ReleaseNotes (zh-CN)
 $Task.CurrentState.Locale += [ordered]@{
   Locale = 'zh-CN'
   Key    = 'ReleaseNotes'
-  Value  = $Object.change_logs | Format-Text | ConvertTo-UnorderedList
+  Value  = $Object.releaseNotes | Format-Text | ConvertTo-UnorderedList
 }
 
 switch (Compare-State) {
@@ -29,17 +29,17 @@ switch (Compare-State) {
           Value = 'https://www.yuque.com/yuque/yuque-desktop/' + $ReleaseNotesUrlObject.url
         }
 
-        $Object3 = Invoke-RestMethod -Uri "https://www.yuque.com/api/docs/$($ReleaseNotesUrlObject.url)?book_id=$($Object2.doc.book_id)"
-        $Task.CurrentState.ReleaseTime = [regex]::Match(
-          $Object3.data.description,
-          '发布时间[:：]\s*(\d{4}\.\d{1,2}\.\d{1,2})'
-        ).Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
+        # $Object3 = Invoke-RestMethod -Uri "https://www.yuque.com/api/docs/$($ReleaseNotesUrlObject.url)?book_id=$($Object2.doc.book_id)"
+        # $Task.CurrentState.ReleaseTime = [regex]::Match(
+        #   $Object3.data.description,
+        #   '发布时间[:：]\s*(\d{4}\.\d{1,2}\.\d{1,2})'
+        # ).Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
       } else {
         Write-Host -Object "Task $($Task.Name): No ReleaseNotesUrl and ReleaseTime for version $($Task.CurrentState.Version)" -ForegroundColor Yellow
         # ReleaseNotesUrl
         $Task.CurrentState.Locale += [ordered]@{
           Key   = 'ReleaseNotesUrl'
-          Value = $null
+          Value = 'https://www.yuque.com/yuque/yuque-desktop/'
         }
       }
     } catch {
