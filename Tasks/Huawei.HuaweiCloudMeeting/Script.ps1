81daeca78a72fd9f6471094db1badc7deb308345
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2023-09-20 20:25:06 +1200

    Chore: Add Huawei.HuaweiCloudMeeting

---
+++ Tasks/Huawei.HuaweiCloudMeeting/Script.ps1
@@ -0,0 +1,50 @@
+# International
+$Object1 = Invoke-RestMethod -Uri "https://intl.meeting.huaweicloud.com/v1/usg/tms/softterm/version/query?userType=cloudlink-windows&currentVersion=$($Task.LastState.Version ?? '9.7.7')"
+$Version1 = $Object1.upgradeVersion
+
+# Chinese
+$Object2 = Invoke-RestMethod -Uri "https://meeting.huaweicloud.com/v1/usg/tms/softterm/version/query?userType=cloudlink-windows&currentVersion=$($Task.LastState.Version ?? '9.7.7')"
+$Version2 = $Object2.upgradeVersion
+
+if ($Version1 -ne $Version2) {
+  Write-Host -Object "Task $($Task.Name): The versions are different between editions"
+  $Task.Config.Notes = '各个版本的版本号不相同'
+}
+
+# Version
+$Task.CurrentState.Version = $Version1
+
+# Installer
+$Task.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.versionPath
+}
+$Task.CurrentState.Installer += [ordered]@{
+  InstallerLocale = 'zh-CN'
+  InstallerUrl    = $Object2.versionPath
+}
+
+# ReleaseNotes (en-US)
+$Task.CurrentState.Locale += [ordered]@{
+  Locale = 'en-US'
+  Key    = 'ReleaseNotes'
+  Value  = $Object1.versionDescriptionEn | Format-Text
+}
+
+# ReleaseNotes (zh-CN)
+$Task.CurrentState.Locale += [ordered]@{
+  Locale = 'zh-CN'
+  Key    = 'ReleaseNotes'
+  Value  = $Object2.versionDescription | Format-Text
+}
+
+switch (Compare-State) {
+  ({ $_ -ge 1 }) {
+    Write-State
+  }
+  ({ $_ -ge 2 }) {
+    Send-VersionMessage
+  }
+  ({ $_ -ge 3 -and $Version1 -eq $Version2 }) {
+    New-Manifest
+  }
+}
