f27fcb92daf9189a4b338f05849f6335f8ec2bc8
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2023-09-21 21:06:25 +1200

    Chore: Add Huawei.Welink

---
+++ Tasks/Huawei.Welink/Script.ps1
@@ -0,0 +1,45 @@
+$Object = Invoke-RestMethod -Uri 'https://api.welink.huaweicloud.com/mcloud/mag/ProxyForText/pc/appstore_appservice/api/cloudlink/patch/free/check/version/pc/1' -Method Post -Body (
+  @{
+    appId    = 'com.huawei.cloud.welink'
+    weDevice = '5'
+    weOs     = '10'
+    weVCode  = $Task.LastState.VersionCode ?? 483
+  } | ConvertTo-Json -Compress
+) -ContentType 'application/json'
+
+# Version
+$Task.CurrentState.Version = $Object.data.versionName
+
+# VersionCode
+$Task.CurrentState.VersionCode = $Object.data.versionCode
+
+# Installer
+$Task.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object.data.updateUrl
+}
+
+# ReleaseNotes (en-US)
+$Task.CurrentState.Locale += [ordered]@{
+  Locale = 'en-US'
+  Key    = 'ReleaseNotes'
+  Value  = $Object.data.tipEN | Format-Text
+}
+
+# ReleaseNotes (zh-CN)
+$Task.CurrentState.Locale += [ordered]@{
+  Locale = 'zh-CN'
+  Key    = 'ReleaseNotes'
+  Value  = $Object.data.tipZH | Format-Text
+}
+
+switch (Compare-State) {
+  ({ $_ -ge 1 }) {
+    Write-State
+  }
+  ({ $_ -ge 2 }) {
+    Send-VersionMessage
+  }
+  ({ $_ -ge 3 }) {
+    New-Manifest
+  }
+}
