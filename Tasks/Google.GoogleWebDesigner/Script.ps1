c028ddc090b9307a122df47c276ec170bb0940bd
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2023-09-22 11:41:05 +1200

    Chore: Add Google.GoogleWebDesigner

---
+++ Tasks/Google.GoogleWebDesigner/Script.ps1
@@ -0,0 +1,53 @@
+# x64
+$Object1 = Invoke-RestMethod -Uri 'https://update.googleapis.com/service/update2' -Method Post -Body @'
+<?xml version="1.0" encoding="UTF-8"?>
+<request protocol="3.0">
+  <os platform="win" version="10" arch="x64" />
+  <app appid="{811767F4-C586-4673-A41F-E9D767497222}">
+    <updatecheck />
+  </app>
+</request>
+'@
+$Version1 = $Object1.response.app.updatecheck.manifest.version
+
+# x86
+$Object2 = Invoke-RestMethod -Uri 'https://update.googleapis.com/service/update2' -Method Post -Body @'
+<?xml version="1.0" encoding="UTF-8"?>
+<request protocol="3.0">
+  <os platform="win" version="10" arch="x86" />
+  <app appid="{811767F4-C586-4673-A41F-E9D767497222}">
+    <updatecheck />
+  </app>
+</request>
+'@
+$Version2 = $Object2.response.app.updatecheck.manifest.version
+
+if ($Version1 -ne $Version2) {
+  Write-Host -Object "Task $($Task.Name): The versions are different between the architectures"
+  $Task.Config.Notes += '各个架构的版本号不相同'
+}
+
+# Version
+$Task.CurrentState.Version = $Version1
+
+# Installer
+$Task.CurrentState.Installer += [ordered]@{
+  Architecture = 'x86'
+  InstallerUrl = ($Object2.response.app.updatecheck.urls.url.codebase | Select-String -Pattern 'https://dl.google.com' -Raw -SimpleMatch) + $Object2.response.app.updatecheck.manifest.actions.action.Where({ $_.event -eq 'install' })[0].run
+}
+$Task.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = ($Object1.response.app.updatecheck.urls.url.codebase | Select-String -Pattern 'https://dl.google.com' -Raw -SimpleMatch) + $Object1.response.app.updatecheck.manifest.actions.action.Where({ $_.event -eq 'install' })[0].run
+}
+
+switch (Compare-State) {
+  ({ $_ -ge 1 }) {
+    Write-State
+  }
+  ({ $_ -ge 2 }) {
+    Send-VersionMessage
+  }
+  ({ $_ -ge 3 -and $Version1 -eq $Version2 }) {
+    New-Manifest
+  }
+}
