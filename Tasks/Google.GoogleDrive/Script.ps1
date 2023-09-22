4ff558d3294ca5649cda3df36fc377929102674e
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2023-09-22 21:25:28 +1200

    Chore: Google.GoogleDrive Force stable channel in querying

--- Tasks/Google.GoogleDrive/Script.ps1
+++ Tasks/Google.GoogleDrive/Script.ps1
@@ -1,22 +1,12 @@
-for ($i = 0; $i -lt 10; $i++) {
-  $Object = Invoke-RestMethod -Uri 'https://update.googleapis.com/service/update2' -Method Post -Body @'
+$Object = Invoke-RestMethod -Uri 'https://update.googleapis.com/service/update2' -Method Post -Body @'
 <?xml version="1.0" encoding="UTF-8"?>
 <request protocol="3.0">
-  <os platform="win" version="10" arch="x64" />
+  <os platform="win" version="10" arch="x64" cohort="1:fj3:nir@0.5,nj9@0.05,nix@0.25,nj3@0.1" />
   <app appid="{6BBAE539-2232-434A-A4E5-9A33560C6283}">
     <updatecheck />
   </app>
 </request>
 '@
-  if ($Object.response.app.cohortname -eq 'Stable') {
-    break
-  }
-  Start-Sleep 1
-}
-
-if ($Object.response.app.cohortname -ne 'Stable') {
-  throw "Task $($Task.Name): Could not fetch stable version. Current cohort is $($Object.response.app.cohortname)"
-}
 
 # Version
 $Task.CurrentState.Version = $Object.response.app.updatecheck.manifest.version
