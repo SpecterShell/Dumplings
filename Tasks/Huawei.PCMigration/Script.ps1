02ce6120675b3928df97ee8099fe810e6c7f89a3
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2023-10-01 13:00:51 +1300

    Chore: Add Huawei.PCMigration

---
+++ Tasks/Huawei.PCMigration/Script.ps1
@@ -0,0 +1,27 @@
+$Object = Invoke-WebRequest -Uri 'https://consumer.huawei.com/cn/support/pc-clone/' | ConvertFrom-Html
+
+# Version
+$Task.CurrentState.Version = $Object.SelectSingleNode('//*[@class="dows"]/span').InnerText.Trim()
+
+# Installer
+$Task.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $InstallerUrl = $Object.SelectSingleNode('//*[@class="buttoncontent"]/a').Attributes['href'].Value
+}
+
+switch (Compare-State) {
+  ({ $_ -ge 1 }) {
+    # RealVersion
+    $Task.CurrentState.RealVersion = [regex]::Match(
+      (Invoke-WebRequest -Uri $InstallerUrl -Method Head).Headers.'Content-Disposition',
+      '([\d\.]+\(.+?\))\.exe'
+    ).Groups[1].Value
+
+    Write-State
+  }
+  ({ $_ -ge 2 }) {
+    Send-VersionMessage
+  }
+  ({ $_ -ge 3 }) {
+    New-Manifest
+  }
+}
