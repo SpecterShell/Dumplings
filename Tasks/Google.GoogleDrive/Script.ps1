81b124055fab9c2f00c5f66f1bf2f2cbd1885698
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2023-10-04 10:46:22 +1300

    Fix: Google.GoogleDrive cohort

--- Tasks/Google.GoogleDrive/Script.ps1
+++ Tasks/Google.GoogleDrive/Script.ps1
@@ -1,8 +1,8 @@
 $Object = Invoke-RestMethod -Uri 'https://update.googleapis.com/service/update2' -Method Post -Body @'
 <?xml version="1.0" encoding="UTF-8"?>
 <request protocol="3.0">
-  <os platform="win" version="10" arch="x64" cohort="1:fj3:nir@0.5,nj9@0.05,nix@0.25,nj3@0.1" />
-  <app appid="{6BBAE539-2232-434A-A4E5-9A33560C6283}">
+  <os platform="win" version="10" arch="x64" />
+  <app appid="{6BBAE539-2232-434A-A4E5-9A33560C6283}" cohort="1:fj3:nir@0.5,nj9@0.05,nix@0.25,nj3@0.1">
     <updatecheck />
   </app>
 </request>
