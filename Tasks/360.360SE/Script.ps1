3e03ff27f96c46c223f5247eb254dc4019e65e8a
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2023-10-20 16:22:51 +1300

    Fix: 360.360SE Update XPath

--- Tasks/360.360SE/Script.ps1
+++ Tasks/360.360SE/Script.ps1
@@ -1,22 +1,19 @@
 $Object1 = Invoke-WebRequest -Uri 'https://browser.360.cn/' | ConvertFrom-Html
 
 # Version
-$Task.CurrentState.Version = [regex]::Match(
-  $Object1.SelectSingleNode('/html/body/div[3]/div/em[1]').InnerText,
-  '([\d\.]+)'
-).Groups[1].Value
+$Task.CurrentState.Version = [regex]::Match($Object1.SelectSingleNode('//*[@class="browser_ver"]').InnerText, '([\d\.]+)').Groups[1].Value
 
 # Installer
 $Task.CurrentState.Installer += [ordered]@{
-  InstallerUrl = $Object1.SelectSingleNode('//*[@id="loadnew"]').Attributes['href'].Value
+  InstallerUrl = "https://down.360safe.com/se/360se$($Task.CurrentState.Version).exe"
 }
 
 switch (Compare-State) {
   ({ $_ -ge 1 }) {
-    $Object2 = Invoke-WebRequest -Uri 'https://bbs.360.cn/thread-16068958-1-1.html' | ConvertFrom-Html
+    $Object2 = Invoke-WebRequest -Uri 'https://bbs.360.cn/thread-16096544-1-1.html' | ConvertFrom-Html
 
     try {
-      $ReleaseNotesTitle = $Object2.SelectSingleNode('//*[@id="postmessage_118933783"]/strong[7]').InnerText
+      $ReleaseNotesTitle = $Object2.SelectSingleNode('//*[@id="postmessage_119112293"]/strong[2]').InnerText
       if ($ReleaseNotesTitle.Contains($Task.CurrentState.Version)) {
         # ReleaseTime
         $Task.CurrentState.ReleaseTime = [regex]::Match($ReleaseNotesTitle, '(\d{4}\.\d{1,2}\.\d{1,2})').Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
@@ -25,7 +22,7 @@ switch (Compare-State) {
         $Task.CurrentState.Locale += [ordered]@{
           Locale = 'zh-CN'
           Key    = 'ReleaseNotes'
-          Value  = $Object2.SelectNodes('//*[@id="postmessage_118933783"]/strong[7]/following-sibling::node()[count(.|//*[@id="postmessage_118933783"]/strong[8]/preceding-sibling::node())=count(//*[@id="postmessage_118933783"]/strong[8]/preceding-sibling::node())]') | Get-TextContent | Format-Text
+          Value  = $Object2.SelectNodes('//*[@id="postmessage_119112293"]/strong[2]/following-sibling::node()[count(.|//*[@id="postmessage_119112293"]/strong[3]/preceding-sibling::node())=count(//*[@id="postmessage_119112293"]/strong[3]/preceding-sibling::node())]') | Get-TextContent | Format-Text
         }
       } else {
         Write-Host -Object "Task $($Task.Name): No ReleaseTime and ReleaseNotes for version $($Task.CurrentState.Version)" -ForegroundColor Yellow
