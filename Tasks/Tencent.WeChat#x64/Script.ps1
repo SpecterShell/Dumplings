53e6955dc90a3f7aad9140b099cc3522ea99c631
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2023-10-23 18:01:27 +1300

    Fix: Tencent.WeChat Save LastModified when not existed

--- Tasks/Tencent.WeChat#x64/Script.ps1
+++ Tasks/Tencent.WeChat#x64/Script.ps1
@@ -1,13 +1,11 @@
 $Uri1 = 'https://dldir1v6.qq.com/weixin/Windows/WeChatSetup.exe'
 
-if ($Task.LastState.LastModified) {
-  $Object1 = Invoke-WebRequest -Uri $Uri1 -Method Head -Headers @{'If-Modified-Since' = $Task.LastState.LastModified } -SkipHttpErrorCheck
-  if ($Object1.StatusCode -eq 304) {
-    Write-Host -Object "Task $($Task.Name): The last version $($Task.LastState.Version) is the latest, skip checking" -ForegroundColor Yellow
-    return
-  }
-  $Task.CurrentState.LastModified = $Object1.Headers.'Last-Modified'
+$Object1 = Invoke-WebRequest -Uri $Uri1 -Method Head -Headers @{'If-Modified-Since' = $Task.LastState.LastModified } -SkipHttpErrorCheck
+if ($Object1.StatusCode -eq 304) {
+  Write-Host -Object "Task $($Task.Name): The last version $($Task.LastState.Version) is the latest, skip checking" -ForegroundColor Yellow
+  return
 }
+$Task.CurrentState.LastModified = $Object1.Headers.'Last-Modified'
 
 $Path = Get-TempFile -Uri $Uri1
 
