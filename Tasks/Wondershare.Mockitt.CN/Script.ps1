cc114f77b54bacfb640091cbda3790cd94ff6216
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2023-11-02 14:15:48 +1300

    Task: Wondershare.Mockitt.CN Get info from download page

--- Tasks/Wondershare.Mockitt.CN/Script.ps1
+++ Tasks/Wondershare.Mockitt.CN/Script.ps1
@@ -1,30 +1,49 @@
-$Object = Invoke-RestMethod -Uri 'https://modao.cc/api/v2/client/desktop/check_update.json?region=CN&version=1.1.0&platform=win32&arch=x64'
+$Object1 = Invoke-WebRequest -Uri 'https://modao.cc/feature/downloads.html' | ConvertFrom-Html
 
 # Version
-$Task.CurrentState.Version = $Object.version
+$Task.CurrentState.Version = [regex]::Match(
+  $Object1.SelectSingleNode('/html/body/main/section[2]/div/div[3]').InnerText,
+  'v([\d\.]+)'
+).Groups[1].Value
 
 # Installer
 $Task.CurrentState.Installer += [ordered]@{
   Architecture = 'x86'
-  InstallerUrl = "https://cdn.modao.cc/Mockitt-win32-ia32-zh-$($Task.CurrentState.Version).exe"
+  InstallerUrl = $Object1.SelectSingleNode('//*[@id="download-win32"]').Attributes['href'].Value
 }
 $Task.CurrentState.Installer += [ordered]@{
   Architecture = 'x64'
-  InstallerUrl = "https://cdn.modao.cc/Mockitt-win32-x64-zh-$($Task.CurrentState.Version).exe"
-}
-
-# ReleaseTime
-$Task.CurrentState.ReleaseTime = $Object.pub_date.ToUniversalTime()
-
-# ReleaseNotes (zh-CN)
-$Task.CurrentState.Locale += [ordered]@{
-  Locale = 'zh-CN'
-  Key    = 'ReleaseNotes'
-  Value  = $Object.release_notes_zh | Format-Text
+  InstallerUrl = $Object1.SelectSingleNode('//*[@id="download-win64"]').Attributes['href'].Value
 }
 
 switch (Compare-State) {
   ({ $_ -ge 1 }) {
+    $Object2 = Invoke-RestMethod -Uri "https://modao.cc/api/v2/client/desktop/check_update.json?region=CN&version=$($Task.LastState.Version ?? '1.2.5')&platform=win32&arch=x64"
+
+    try {
+      if ($Object2.version -eq $Task.CurrentState.Version) {
+        # ReleaseTime
+        $Task.CurrentState.ReleaseTime = $Object2.pub_date.ToUniversalTime()
+
+        # ReleaseNotes (en-US)
+        $Task.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $Object2.release_notes_en | Format-Text
+        }
+        # ReleaseNotes (zh-CN)
+        $Task.CurrentState.Locale += [ordered]@{
+          Locale = 'zh-CN'
+          Key    = 'ReleaseNotes'
+          Value  = $Object2.release_notes_zh | Format-Text
+        }
+      } else {
+        Write-Host -Object "Task $($Task.Name): No ReleaseNotes for version $($Task.CurrentState.Version)" -ForegroundColor Yellow
+      }
+    } catch {
+      Write-Host -Object "Task $($Task.Name): ${_}" -ForegroundColor Yellow
+    }
+
     Write-State
   }
   ({ $_ -ge 2 }) {
