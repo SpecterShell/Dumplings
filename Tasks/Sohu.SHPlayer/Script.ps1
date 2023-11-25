30cdc333b78b71888d2d9148983322835c108ef0
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2023-11-25 16:57:27 +1300

    Task: Sohu.SHPlayer Get info from download page

--- Tasks/Sohu.SHPlayer/Script.ps1
+++ Tasks/Sohu.SHPlayer/Script.ps1
@@ -1,38 +1,31 @@
-$Object1 = Invoke-RestMethod -Uri 'https://p2p.hd.sohu.com.cn/update?clienttype=3&version=7.0.0.0'
+$Object1 = Invoke-WebRequest -Uri 'https://tv.sohu.com/down/index.shtml?downLoad=windows' | Read-ResponseContent -Encoding 'GBK' | ConvertFrom-Html
 
 # Version
-$Task.CurrentState.Version = $Object1.version
+$Task.CurrentState.Version = [regex]::Match(
+  $Object1.SelectSingleNode('//div[contains(@class, "down_winbox")]/div[2]/div/p/span').InnerText,
+  '版本号([\d\.]+)'
+).Groups[1].Value
 
 # Installer
 $Task.CurrentState.Installer += [ordered]@{
-  InstallerUrl = $Object1.cdn
+  InstallerUrl = Get-RedirectedUrl1st -Uri ('https:' + $Object1.SelectSingleNode('//div[contains(@class, "down_winbox")]/div[2]/a').Attributes['href'].Value)
+}
+
+# ReleaseTime
+$Task.CurrentState.ReleaseTime = [regex]::Match(
+  $Object1.SelectSingleNode('//div[contains(@class, "down_winbox")]/div[2]/div/p/span').InnerText,
+  '(\d{4}-\d{1,2}-\d{1,2})'
+).Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
+
+# ReleaseNotes (zh-CN)
+$Task.CurrentState.Locale += [ordered]@{
+  Locale = 'zh-CN'
+  Key    = 'ReleaseNotes'
+  Value  = $Object1.SelectNodes('//div[contains(@class, "down_winbox")]/div[2]/div/div') | Get-TextContent | Format-Text
 }
 
 switch (Compare-State) {
   ({ $_ -ge 1 }) {
-    $Object2 = Invoke-WebRequest -Uri 'https://tv.sohu.com/down/index.shtml?downLoad=windows' | Read-ResponseContent -Encoding 'GBK' | ConvertFrom-Html
-
-    try {
-      if ($Object2.SelectSingleNode('/html/body/div[3]/div/div[1]/div[2]/div/p/span').InnerText.Contains($Task.CurrentState.Version)) {
-        # ReleaseTime
-        $Task.CurrentState.ReleaseTime = [regex]::Match(
-          $Object2.SelectSingleNode('/html/body/div[3]/div/div[1]/div[2]/div/p/span').InnerText,
-          '(\d{4}-\d{1,2}-\d{1,2})'
-        ).Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
-
-        # ReleaseNotes (zh-CN)
-        $Task.CurrentState.Locale += [ordered]@{
-          Locale = 'zh-CN'
-          Key    = 'ReleaseNotes'
-          Value  = $Object2.SelectNodes('/html/body/div[3]/div/div[1]/div[2]/div/div') | Get-TextContent | Format-Text
-        }
-      } else {
-        Write-Host -Object "Task $($Task.Name): No ReleaseTime and ReleaseNotes for version $($Task.CurrentState.Version)" -ForegroundColor Yellow
-      }
-    } catch {
-      Write-Host -Object "Task $($Task.Name): ${_}" -ForegroundColor Yellow
-    }
-
     Write-State
   }
   ({ $_ -ge 2 }) {
