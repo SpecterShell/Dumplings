d1ae4efc5ad85c5c8b5e663ab8be8a856a309207
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2023-11-25 16:40:53 +1300

    Task: ByteDance.JianyingPro#Up Reset state

--- Tasks/ByteDance.JianyingPro#Up/Script.ps1
+++ Tasks/ByteDance.JianyingPro#Up/Script.ps1
@@ -1,21 +1,19 @@
 $Object = Invoke-RestMethod -Uri "https://is.snssdk.com/service/settings/v3/?device_platform=windows&os_version=10&aid=3704&iid=0&version_code=$($Task.LastState.VersionCode ?? '197888')"
 
 # VersionCode
-$Task.CurrentState.VersionCode = $Object.data.settings.update_reminder.lastest_stable_version
+$Task.CurrentState.VersionCode = $VersionCode = $Object.data.settings.update_reminder.lastest_stable_version
 
 # Version
-$VersionCodeBase = $Task.CurrentState.VersionCode
 $Task.CurrentState.Version = @(
-  [math]::Floor($VersionCodeBase / 256 / 256).ToString()
-  [math]::Floor($VersionCodeBase / 256 % 256).ToString()
-  [math]::Floor($VersionCodeBase % 256).ToString()
+  [math]::Floor($VersionCode / 256 / 256).ToString()
+  [math]::Floor($VersionCode / 256 % 256).ToString()
+  [math]::Floor($VersionCode % 256).ToString()
   $Object.data.settings.update_reminder.lastest_stable_builder_number.ToString()
 ) -join '.'
 
 # Installer
-$InstallerUrl = $Object.data.settings.update_reminder.lastest_stable_url
 $Task.CurrentState.Installer += [ordered]@{
-  InstallerUrl = $InstallerUrl
+  InstallerUrl = $Object.data.settings.update_reminder.lastest_stable_url
 }
 
 # ReleaseNotes (zh-CN)
