4526f9d86f22d3ac16737a369019bf5f0f80d1e0
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2023-11-25 16:23:43 +1300

    Task: Twinkstar.TwinkstarBrowser Derive x86 from x64

--- Tasks/Twinkstar.TwinkstarBrowser/Script.ps1
+++ Tasks/Twinkstar.TwinkstarBrowser/Script.ps1
@@ -1,40 +1,33 @@
-$Object1 = Invoke-WebRequest -Uri 'https://www.twinkstar.com/' -UserAgent 'Mozilla/5.0 (Windows NT 10.0)' | ConvertFrom-Html
-$Object2 = Invoke-WebRequest -Uri 'https://www.twinkstar.com/' -UserAgent 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)' | ConvertFrom-Html
+$Object1 = Invoke-WebRequest -Uri 'https://www.twinkstar.com/' -UserAgent 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)' | ConvertFrom-Html
 
 # Installer
 $Task.CurrentState.Installer += [ordered]@{
-  Architecture = 'x86'
-  InstallerUrl = $InstallerUrlX86 = $Object1.SelectSingleNode('//a[contains(@class, "download-win")]').Attributes['href'].Value
+  Architecture = 'x64'
+  InstallerUrl = $InstallerUrl = $Object1.SelectSingleNode('//a[contains(@class, "download-win")]').Attributes['href'].Value
 }
 $Task.CurrentState.Installer += [ordered]@{
-  Architecture = 'x64'
-  InstallerUrl = $InstallerUrlX64 = $Object2.SelectSingleNode('//a[contains(@class, "download-win")]').Attributes['href'].Value
+  Architecture = 'x86'
+  InstallerUrl = $InstallerUrl.Replace('.exe', '32.exe')
 }
 
 # Version
-$VersionX86 = [regex]::Match($InstallerUrlx86, 'v([\d\.]+)').Groups[1].Value
-$Task.CurrentState.Version = $VersionX64 = [regex]::Match($InstallerUrlx64, 'v([\d\.]+)').Groups[1].Value
-
-if ($VersionX86 -ne $VersionX64) {
-  Write-Host -Object "Task $($Task.Name): Distinct versions detected" -ForegroundColor Yellow
-  $Task.Config.Notes = '检测到不同的版本'
-}
+$Task.CurrentState.Version = [regex]::Match($InstallerUrl, 'v([\d\.]+)').Groups[1].Value
 
 # ReleaseTime
-$Task.CurrentState.ReleaseTime = $Object2.SelectSingleNode('//a[contains(@class, "download-win")]//div[@class="download-date"][2]').InnerText.Trim() | Get-Date -Format 'yyyy-MM-dd'
+$Task.CurrentState.ReleaseTime = $Object1.SelectSingleNode('//a[contains(@class, "download-win")]//div[@class="download-date"][2]').InnerText.Trim() | Get-Date -Format 'yyyy-MM-dd'
 
 switch (Compare-State) {
   ({ $_ -ge 1 }) {
-    $Object3 = Invoke-WebRequest -Uri 'https://bbs.twinkstar.com/forum.php?mod=viewthread&tid=4227' | ConvertFrom-Html
+    $Object2 = Invoke-WebRequest -Uri 'https://bbs.twinkstar.com/forum.php?mod=viewthread&tid=4227' | ConvertFrom-Html
 
     try {
-      $ReleaseNotesTitle = $Object3.SelectSingleNode('//*[@id="postmessage_14664"]/font[1]').InnerText
+      $ReleaseNotesTitle = $Object2.SelectSingleNode('//*[@id="postmessage_14664"]/font[1]').InnerText
       if ($ReleaseNotesTitle.Contains($Task.CurrentState.Version)) {
         # ReleaseNotes (zh-CN)
         $Task.CurrentState.Locale += [ordered]@{
           Locale = 'zh-CN'
           Key    = 'ReleaseNotes'
-          Value  = $Object3.SelectNodes('//*[@id="postmessage_14664"]/font[contains(.//text(), "changelog")][1]/following-sibling::node()[count(.|//*[@id="postmessage_14664"]/font[contains(.//text(), "下载地址")]/preceding-sibling::node())=count(//*[@id="postmessage_14664"]/font[contains(.//text(), "下载地址")]/preceding-sibling::node())]') | Get-TextContent | Format-Text
+          Value  = $Object2.SelectNodes('//*[@id="postmessage_14664"]/font[contains(.//text(), "changelog")][1]/following-sibling::node()[count(.|//*[@id="postmessage_14664"]/font[contains(.//text(), "下载地址")]/preceding-sibling::node())=count(//*[@id="postmessage_14664"]/font[contains(.//text(), "下载地址")]/preceding-sibling::node())]') | Get-TextContent | Format-Text
         }
       } else {
         Write-Host -Object "Task $($Task.Name): No ReleaseTime and ReleaseNotes for version $($Task.CurrentState.Version)" -ForegroundColor Yellow
@@ -48,7 +41,7 @@ switch (Compare-State) {
   ({ $_ -ge 2 }) {
     Send-VersionMessage
   }
-  ({ $_ -ge 3 -and $VersionX86 -eq $VersionX64 }) {
+  ({ $_ -ge 3 }) {
     New-Manifest
   }
 }
