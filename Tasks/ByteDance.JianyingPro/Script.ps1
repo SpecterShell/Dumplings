a9ef7790fc9284c001a61693bf0e39c5c79763fc
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2023-11-25 16:40:04 +1300

    Task: ByteDance.JianyingPro Fix regex

--- Tasks/ByteDance.JianyingPro/Script.ps1
+++ Tasks/ByteDance.JianyingPro/Script.ps1
@@ -1,13 +1,12 @@
 $Object = Invoke-RestMethod -Uri 'https://lf3-beecdn.bytetos.com/obj/ies-fe-bee/bee_prod/biz_80/bee_prod_80_bee_publish_3563.json'
 
 # Installer
-$InstallerUrl = $Object.windows_download_pkg.channel_default
 $Task.CurrentState.Installer += [ordered]@{
-  InstallerUrl = $InstallerUrl
+  InstallerUrl = $InstallerUrl = $Object.windows_download_pkg.channel_default
 }
 
 # Version
-$Task.CurrentState.Version = [regex]::Match($InstallerUrl, 'Jianying_([\d_]+)_jianyingpro').Groups[1].Value.Replace('_', '.')
+$Task.CurrentState.Version = [regex]::Match($InstallerUrl, '(\d+_\d+_\d+_\d+)_jianyingpro').Groups[1].Value.Replace('_', '.')
 
 # ReleaseTime
 $Task.CurrentState.ReleaseTime = [regex]::Match($Object.windows_version_and_update_date, '(\d{4}/\d{1,2}/\d{1,2})').Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
