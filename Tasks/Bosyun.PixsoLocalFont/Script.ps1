cd8062aca5c87f04db9eea506a4d2d7131f77a4b
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2023-12-08 16:58:28 +1300

    Task: Bosyun.PixsoLocalFont Fix XPath

--- Tasks/Bosyun.PixsoLocalFont/Script.ps1
+++ Tasks/Bosyun.PixsoLocalFont/Script.ps1
@@ -1,14 +1,14 @@
-$Object1 = Invoke-WebRequest -Uri 'https://pixso.cn/download/' | ConvertFrom-Html
+$Object = Invoke-WebRequest -Uri 'https://pixso.cn/download/' | ConvertFrom-Html
 
 # Version
 $Task.CurrentState.Version = [regex]::Match(
-  $Object1.SelectSingleNode('/html/body/main/section[3]/div/div/div[4]/p[2]').InnerText,
+  $Object.SelectSingleNode('//*[contains(@class, "apps-item") and contains(./div[3]/div[1]/text(), "本地字体助手")]/p[2]').InnerText,
   '([\d\.]+)'
 ).Groups[1].Value
 
 # Installer
 $Task.CurrentState.Installer += [ordered]@{
-  InstallerUrl = $Object1.SelectSingleNode('/html/body/main/section[3]/div/div/div[4]/div[3]/div[1]').Attributes['data-href'].Value
+  InstallerUrl = $Object.SelectSingleNode('//*[contains(@class, "apps-item") and contains(./div[3]/div[1]/text(), "本地字体助手")]/div[1]').Attributes['data-href'].Value
 }
 
 switch (Compare-State) {
