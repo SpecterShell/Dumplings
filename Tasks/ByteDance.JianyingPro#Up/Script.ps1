660bd4e86efc216761209b1a7678216a83c548e7
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2023-12-08 17:27:05 +1300

    Task: ByteDance.JianyingPro#Up Check if the last version is the latest

--- Tasks/ByteDance.JianyingPro#Up/Script.ps1
+++ Tasks/ByteDance.JianyingPro#Up/Script.ps1
@@ -1,5 +1,10 @@
 $Object = Invoke-RestMethod -Uri "https://is.snssdk.com/service/settings/v3/?device_platform=windows&os_version=10&aid=3704&iid=0&version_code=$($Task.LastState.VersionCode ?? '197888')"
 
+if (-not $Object.data.settings.update_reminder) {
+  Write-Host -Object "Task $($Task.Name): The last version $($Task.LastState.Version) is the latest, skip checking" -ForegroundColor Yellow
+  return
+}
+
 # VersionCode
 $Task.CurrentState.VersionCode = $VersionCode = $Object.data.settings.update_reminder.lastest_stable_version
 
