853e3d7ea1e7e27bff6ffe50b7df6d13599ccaed
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2023-12-08 16:15:30 +1300

    Task: Coldlake.StellarPlayer Remove x86 installer and switch to new API

--- Tasks/Coldlake.StellarPlayer/Script.ps1
+++ Tasks/Coldlake.StellarPlayer/Script.ps1
@@ -1,50 +1,26 @@
 # Download
-$Object1 = Invoke-RestMethod -Uri 'https://player-update.coldlake1.com/version/info' | Get-EmbeddedJson -StartsFrom 'getVersionInfo(' | ConvertFrom-Json -AsHashtable
-# Upgrade x64
+$Object1 = (Invoke-RestMethod -Uri 'https://api.stellarplayer.com/v1/cfg/getConfig?cfgKey=nZngW7WDWeVxm').data.cfgInfo.nZngW7WDWeVxm.cfgDetail | ConvertFrom-Json
+$Version1 = [regex]::Match($Object1.filename, '(\d{14})').Groups[1].Value
+
+# Upgrade
 $Object2 = Invoke-RestMethod -Uri 'https://ab.coldlake1.com/v1/abt/matcher?arch=x64&atype=show&channel=official'
 $Version2 = [regex]::Match($Object2.data, '(\d{14})').Groups[1].Value
-# Upgrade x86
-$Object3 = Invoke-RestMethod -Uri 'https://ab.coldlake1.com/v1/abt/matcher?arch=x86&atype=show&channel=official'
 
-if ((Compare-Version -ReferenceVersion $Object1.data.official.x64.full.version -DifferenceVersion $Version2) -gt 0) {
-  if ($Object2.data -ne $Object3.data) {
-    Write-Host -Object "Task $($Task.Name): Distinct versions detected" -ForegroundColor Yellow
-    $Task.Config.Notes = '检测到不同的版本'
-  } else {
-    $Identical = $True
-  }
-
+if ((Compare-Version -ReferenceVersion $Version1 -DifferenceVersion $Version2) -gt 0) {
   # Version
   $Task.CurrentState.Version = $Version2
 
   # Installer
   $Task.CurrentState.Installer += [ordered]@{
-    Architecture = 'x86'
-    InstallerUrl = "https://player-download.coldlake1.com/player/$($Task.CurrentState.Version)/Stellar_$($Task.CurrentState.Version)_official_stable_full_x86.exe"
-  }
-  $Task.CurrentState.Installer += [ordered]@{
-    Architecture = 'x64'
     InstallerUrl = "https://player-download.coldlake1.com/player/$($Task.CurrentState.Version)/Stellar_$($Task.CurrentState.Version)_official_stable_full_x64.exe"
   }
 } else {
-  if ($Object1.data.official.x64.full.version -ne $Object1.data.official.x86.full.version) {
-    Write-Host -Object "Task $($Task.Name): Distinct versions detected" -ForegroundColor Yellow
-    $Task.Config.Notes = '检测到不同的版本'
-  } else {
-    $Identical = $True
-  }
-
   # Version
-  $Task.CurrentState.Version = $Object1.data.official.x64.full.version
+  $Task.CurrentState.Version = $Version1
 
   # Installer
   $Task.CurrentState.Installer += [ordered]@{
-    Architecture = 'x86'
-    InstallerUrl = $Object1.data.official.x86.full.url
-  }
-  $Task.CurrentState.Installer += [ordered]@{
-    Architecture = 'x64'
-    InstallerUrl = $Object1.data.official.x64.full.url
+    InstallerUrl = $Object1.offical_http_address
   }
 }
 
@@ -53,14 +29,14 @@ switch (Compare-State) {
     # RealVersion
     $Task.CurrentState.RealVersion = Get-TempFile -Uri $Task.CurrentState.Installer[0].InstallerUrl | Read-ProductVersionFromExe
 
-    $Object4 = Invoke-WebRequest -Uri "https://player-update.coldlake1.com/version/gray/$($Task.CurrentState.Version)_x64.ini" | Read-ResponseContent | ConvertFrom-Ini
+    $Object3 = Invoke-WebRequest -Uri "https://player-update.coldlake1.com/version/gray/$($Task.CurrentState.Version)_x64.ini" | Read-ResponseContent | ConvertFrom-Ini
 
     try {
       # ReleaseNotes (zh-CN)
       $Task.CurrentState.Locale += [ordered]@{
         Locale = 'zh-CN'
         Key    = 'ReleaseNotes'
-        Value  = $Object4.update.info | Format-Text
+        Value  = $Object3.update.info | Format-Text
       }
     } catch {
       Write-Host -Object "Task $($Task.Name): ${_}" -ForegroundColor Yellow
@@ -71,7 +47,7 @@ switch (Compare-State) {
   ({ $_ -ge 2 }) {
     Send-VersionMessage
   }
-  ({ $_ -ge 3 -and $Identical }) {
+  ({ $_ -ge 3 }) {
     New-Manifest
   }
 }
