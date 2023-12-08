283588a14e38a5204fec3fe06c3f5e863351c804
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2023-12-08 17:17:38 +1300

    Task: ByteDance.CapCut Switch to new API

--- Tasks/ByteDance.CapCut/Script.ps1
+++ Tasks/ByteDance.CapCut/Script.ps1
@@ -1,15 +1,12 @@
-$Object1 = Invoke-WebRequest -Uri 'https://www.capcut.com/' | ConvertFrom-Html
-
-$Object2 = $Object1.SelectSingleNode('//*[@id="RENDER_DATA"]').InnerHtml.Trim() | ConvertTo-UnescapedUri | ConvertFrom-Json -AsHashtable
+$Object = Invoke-RestMethod -Uri 'https://editor-api-sg.capcut.com/service/settings/v3/?device_platform=windows&aid=359289&from_aid=359289&from_version=0.0.0'
 
 # Installer
-$InstallerUrl = $Object2.values.tccData.download_link[0].windows_download_pkg.channel_default
 $Task.CurrentState.Installer += [ordered]@{
-  InstallerUrl = $InstallerUrl
+  InstallerUrl = $InstallerUrl = $Object.data.settings.installer_downloader_config.url
 }
 
 # Version
-$Task.CurrentState.Version = [regex]::Match($InstallerUrl, 'CapCut_([\d_]+)_capcutpc').Groups[1].Value.Replace('_', '.')
+$Task.CurrentState.Version = [regex]::Match($InstallerUrl, '(\d+_\d+_\d+_\d+)_capcutpc').Groups[1].Value.Replace('_', '.')
 
 switch (Compare-State) {
   ({ $_ -ge 1 }) {
