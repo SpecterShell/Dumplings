3bd644e5fd5829a99cbe18aa6d7cd2680bdab15f
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2023-12-15 11:35:57 +1300

    Task: bshuzhang.PPLink Try to fix encoding issue

--- Tasks/bshuzhang.PPLink/Script.ps1
+++ Tasks/bshuzhang.PPLink/Script.ps1
@@ -1,6 +1,6 @@
 $Prefix = 'https://www.ppzhilian.com/download/win/'
 
-$Task.CurrentState = Invoke-RestMethod -Uri "${Prefix}latest.yml?noCache=$((New-Guid).Guid.Split('-')[0])" | ConvertFrom-Yaml | ConvertFrom-ElectronUpdater -Prefix $Prefix -Locale 'zh-CN'
+$Task.CurrentState = Invoke-WebRequest -Uri "${Prefix}latest.yml?noCache=$((New-Guid).Guid.Split('-')[0])" | Read-ResponseContent | ConvertFrom-Yaml | ConvertFrom-ElectronUpdater -Prefix $Prefix -Locale 'zh-CN'
 
 switch ($Task.Check()) {
   ({ $_ -ge 1 }) {
