274dfdfa1dc5d66f8e9c64f4cae7acbfe3ff3d9f
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2023-12-16 12:01:55 +1300

    Task: LiTang.QianJi Switch to new source and remove ReleaseTime detection

--- Tasks/LiTang.QianJi/Script.ps1
+++ Tasks/LiTang.QianJi/Script.ps1
@@ -1,35 +1,35 @@
-$Object1 = Invoke-RestMethod -Uri 'https://gitee.com/qianjigroup/qianji-public-test/raw/master/upgrade-windows.txt'
+$Object1 = Invoke-RestMethod -Uri 'https://upkit.qianji.app/qj/windows.txt'
 
 # Version
-$Task.CurrentState.Version = $Object1.versionName
+$Task.CurrentState.Version = $Object1.version.versionName
 
 # Installer
 $Task.CurrentState.Installer += [ordered]@{
-  InstallerUrl = $Object1.downloadUrl
+  InstallerUrl = $Object1.version.downloadUrl
 }
 
 # ReleaseNotes (zh-CN)
 $Task.CurrentState.Locale += [ordered]@{
   Locale = 'zh-CN'
   Key    = 'ReleaseNotes'
-  Value  = $Object1.changeLogs | Format-Text
+  Value  = $Object1.version.changeLogs | Format-Text
 }
 
 switch ($Task.Check()) {
   ({ $_ -ge 1 }) {
-    $Object2 = Invoke-WebRequest -Uri 'https://docs.qianjiapp.com/change-log/change_log_pc.html' | ConvertFrom-Html
+    # $Object2 = Invoke-WebRequest -Uri 'https://docs.qianjiapp.com/change-log/change_log_pc.html' | ConvertFrom-Html
 
-    try {
-      $ReleaseNotesNode = $Object2.SelectSingleNode("//*[@id='book-search-results']/div[1]/section/h2[contains(./text(), '$($Task.CurrentState.Version)-$($Object1.versionCode)')]")
-      if ($ReleaseNotesNode) {
-        # ReleaseTime
-        $Task.CurrentState.ReleaseTime = $ReleaseNotesNode.SelectSingleNode('./following-sibling::p[1]').InnerText | Get-Date -Format 'yyyy-MM-dd'
-      } else {
-        $Task.Logging("No ReleaseTime for version $($Task.CurrentState.Version)", 'Warning')
-      }
-    } catch {
-      $Task.Logging($_, 'Warning')
-    }
+    # try {
+    #   $ReleaseNotesNode = $Object2.SelectSingleNode("//*[@id='book-search-results']/div[1]/section/h2[contains(./text(), '$($Task.CurrentState.Version)-$($Object1.versionCode)')]")
+    #   if ($ReleaseNotesNode) {
+    #     # ReleaseTime
+    #     $Task.CurrentState.ReleaseTime = $ReleaseNotesNode.SelectSingleNode('./following-sibling::p[1]').InnerText | Get-Date -Format 'yyyy-MM-dd'
+    #   } else {
+    #     $Task.Logging("No ReleaseTime for version $($Task.CurrentState.Version)", 'Warning')
+    #   }
+    # } catch {
+    #   $Task.Logging($_, 'Warning')
+    # }
 
     $Task.Write()
   }
