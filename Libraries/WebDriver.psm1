2bf2e839a37afec1ba95a4fd093f999f18934e62
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2023-12-20 12:52:11 +1300

    Core: Update Selenium and add FirefoxDriver

--- Libraries/WebDriver.psm1
+++ Libraries/WebDriver.psm1
@@ -3,17 +3,17 @@
   Initialize WebDriver and return the object on demand
 #>
 
+Add-Type -Path (Join-Path $PSScriptRoot '..' 'Assets' 'WebDriver.dll' -Resolve)
+
 $EdgeDriver = $null
 $IsEdgeDriverLoaded = $false
-
-Add-Type -Path (Join-Path $PSScriptRoot '..' 'Assets' 'WebDriver.dll' -Resolve)
 
 function New-EdgeDriver {
   <#
   .SYNOPSIS
     Initialize an Edge Driver instance
   .PARAMETER Headless
-    Run Edge Driver in headless mode, with no windows shown
+    Run Edge Driver in headless mode, with no window shown
   #>
   param (
     [parameter(
@@ -33,7 +33,7 @@ function New-EdgeDriver {
     # https://github.com/actions/runner-images/blob/main/images/win/Windows2022-Readme.md
     $Script:EdgeDriver = [OpenQA.Selenium.Edge.EdgeDriver]::new($Env:EDGEWEBDRIVER, $EdgeOptions)
   } else {
-    $Script:EdgeDriver = [OpenQA.Selenium.Edge.EdgeDriver]::new($EdgeOptions)
+    $Script:EdgeDriver = [OpenQA.Selenium.Edge.EdgeDriver]::new((Get-Command -Name 'msedgedriver.exe').Path, $EdgeOptions)
   }
   $Script:EdgeDriver.Manage().Window.Size = [System.Drawing.Size]::new(1920, 1080)
   $Script:IsEdgeDriverLoaded = $true
@@ -65,7 +65,70 @@ function Stop-EdgeDriver {
   }
 }
 
-# Stop Edge Driver when the module is being unloaded
-$ExecutionContext.SessionState.Module.OnRemove += { Stop-EdgeDriver -ErrorAction Ignore }
+$FirefoxDriver = $null
+$IsFirefoxDriverLoaded = $false
 
-Export-ModuleMember -Function New-EdgeDriver, Get-EdgeDriver, Stop-EdgeDriver
+function New-FirefoxDriver {
+  <#
+  .SYNOPSIS
+    Initialize an Firefox Driver instance
+  .PARAMETER Headless
+    Run Firefox Driver in headless mode, with no window shown
+  #>
+  param (
+    [parameter(
+      HelpMessage = 'Run Firefox Driver in headless mode, with no windows shown'
+    )]
+    [switch]
+    $Headless
+  )
+
+  $FirefoxOptions = [OpenQA.Selenium.Firefox.FirefoxOptions]::new()
+  # Disable images downloading to speed up page loading
+  $FirefoxOptions.SetPreference('permissions.default.image', 2)
+  if ($Headless) {
+    $FirefoxOptions.AddArgument('--headless')
+  }
+  if ($Env:GECKOWEBDRIVER) {
+    # https://github.com/actions/runner-images/blob/main/images/win/Windows2022-Readme.md
+    $Script:FirefoxDriver = [OpenQA.Selenium.Firefox.FirefoxDriver]::new($Env:GECKOWEBDRIVER, $FirefoxOptions)
+  } else {
+    $Script:FirefoxDriver = [OpenQA.Selenium.Firefox.FirefoxDriver]::new((Get-Command -Name 'geckodriver.exe').Path, $FirefoxOptions)
+  }
+  $Script:FirefoxDriver.Manage().Window.Size = [System.Drawing.Size]::new(1920, 1080)
+  $Script:IsFirefoxDriverLoaded = $true
+}
+
+function Get-FirefoxDriver {
+  <#
+  .SYNOPSIS
+    Return Firefox Driver instance
+  #>
+  [OutputType([OpenQA.Selenium.Firefox.FirefoxDriver])]
+  param ()
+
+  if (-not $Script:IsFirefoxDriverLoaded) {
+    New-FirefoxDriver
+  }
+  return $Script:FirefoxDriver
+}
+
+function Stop-FirefoxDriver {
+  <#
+  .SYNOPSIS
+    Stop Firefox Driver instance
+  #>
+
+  if ($Script:IsFirefoxDriverLoaded) {
+    $Script:FirefoxDriver.Quit()
+    $Script:IsFirefoxDriverLoaded = $false
+  }
+}
+
+# Stop Firefox Driver when the module is being unloaded
+$ExecutionContext.SessionState.Module.OnRemove += {
+  Stop-EdgeDriver -ErrorAction Ignore
+  Stop-FirefoxDriver -ErrorAction Ignore
+}
+
+Export-ModuleMember -Function *
