12df0475fa595730c69ee1657b6cc73fc53bc846
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2023-12-20 15:28:35 +1300

    Core: Implement WinGet pre-installation

---
+++ Assets/InstallWinGet.ps1
@@ -0,0 +1,39 @@
+$ProgressPreference = 'SilentlyContinue'
+
+$VCLibsUri = 'https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx'
+$VCLibsPath = New-TemporaryFile
+Write-Host "Downloading ${VCLibsUri}"
+Invoke-WebRequest -Uri 'https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx' -OutFile $VCLibsPath
+
+$UILibsUri = 'https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx'
+$UILibsPath = New-TemporaryFile
+Write-Host "Downloading ${UILibsUri}"
+Invoke-WebRequest -Uri $UILibsUri -OutFile $UILibsPath
+
+$Params = @{
+  Uri         = 'https://api.github.com/repos/microsoft/winget-cli/releases/latest'
+  Headers     = @{ Accept = 'application/vnd.github+json' }
+  ContentType = 'application/json'
+}
+if ($Env:GITHUB_TOKEN) {
+  Write-Host 'GITHUB_TOKEN detected'
+  $Params['Authentication'] = 'Bearer'
+  $Params['Token'] = ConvertTo-SecureString -String $Env:GITHUB_TOKEN -AsPlainText
+}
+$WinGetRelease = Invoke-RestMethod @Params
+
+$WinGetUri = $WinGetRelease.assets.Where({ $_.name.EndsWith('.msixbundle') })[0].browser_download_url
+$WinGetPath = New-TemporaryFile
+Write-Host "Downloading ${WinGetUri}"
+Invoke-WebRequest -Uri $WinGetUri -OutFile $WinGetPath
+
+$WinGetLicenseUri = $WinGetRelease.assets.Where({ $_.name.EndsWith('_License1.xml') })[0].browser_download_url
+$WinGetLicensePath = New-TemporaryFile
+Write-Host "Downloading ${WinGetLicenseUri}"
+Invoke-WebRequest -Uri $WinGetLicenseUri -OutFile $WinGetLicensePath
+
+Write-Host 'Installing WinGet'
+Add-AppxProvisionedPackage -Online -PackagePath $WinGetPath -DependencyPackagePath $VCLibsPath, $UILibsPath -LicensePath $WinGetLicensePath
+
+Write-Host 'Cleaning environment'
+Remove-Item -Path @($VCLibsPath, $UILibsPath, $WinGetPath, $WinGetLicensePath) -Force
