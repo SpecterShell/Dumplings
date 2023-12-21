fcc62606bd2643f6e97d0c0e8dbc2599a57506e0
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2023-12-22 11:54:48 +1300

    Task: Tencent.QQ Update

--- Tasks/Tencent.QQ/Script.ps1
+++ Tasks/Tencent.QQ/Script.ps1
@@ -1,24 +1,21 @@
-$Object = Invoke-WebRequest -Uri 'https://im.qq.com/pcqq' | ConvertFrom-Html
+$Object = (Invoke-WebRequest -Uri 'https://im.qq.com/pcqq').Content | Get-EmbeddedJson -StartsFrom 'window.__INITIAL_STATE__=' | ConvertFrom-Json
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = $InstallerUrl = $Object.SelectSingleNode('//*[@class="download"][1]').Attributes['href'].Value.Replace('dldir1.qq.com', 'dldir1v6.qq.com')
+  InstallerUrl = $InstallerUrl = $Object.rainbowConfig.banner.downloadUrl.Replace('dldir1.qq.com', 'dldir1v6.qq.com')
 }
 
 # Version
-$this.CurrentState.Version = [regex]::Match($InstallerUrl, '([\d\.]+)\.exe').Groups[1].Value
+$this.CurrentState.Version = [regex]::Match($InstallerUrl, '(\d+\.\d+\.\d+\.\d+)').Groups[1].Value
 
 # ReleaseTime
-$this.CurrentState.ReleaseTime = [regex]::Match(
-  $Object.SelectSingleNode('//*[@class="banner-container__box-desc"][1]').InnerText,
-  '(\d{4}年\d{1,2}月\d{1,2}日)'
-).Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
+$this.CurrentState.ReleaseTime = $Object.rainbowConfig.banner.publishTime | Get-Date -Format 'yyyy-MM-dd'
 
 # ReleaseNotes (zh-CN)
 $this.CurrentState.Locale += [ordered]@{
   Locale = 'zh-CN'
   Key    = 'ReleaseNotes'
-  Value  = $Object.SelectNodes('//*[@class="features"]').InnerText | Format-Text
+  Value  = $Object.rainbowConfig.banner.features | Format-Text
 }
 
 switch ($this.Check()) {
