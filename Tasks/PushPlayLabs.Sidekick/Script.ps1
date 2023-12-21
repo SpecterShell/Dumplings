de34dc2b2dea85fe9076b1949280eeea18235421
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2023-12-22 01:28:19 +1300

    Task: Add PushPlayLabs.Sidekick

---
+++ Tasks/PushPlayLabs.Sidekick/Script.ps1
@@ -0,0 +1,19 @@
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $InstallerUrl = Get-RedirectedUrl -Uri 'https://api.meetsidekick.com/downloads/win'
+}
+
+# Version
+$this.CurrentState.Version = [regex]::Match($InstallerUrl, '(\d+\.\d+\.\d+\.\d+)').Groups[1].Value
+
+switch ($this.Check()) {
+  ({ $_ -ge 1 }) {
+    $this.Write()
+  }
+  ({ $_ -ge 2 }) {
+    $this.Message()
+  }
+  ({ $_ -ge 3 }) {
+    $this.Submit()
+  }
+}
