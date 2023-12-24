7c6ae1574619dd59c535a8d3691932c0926ae1f3
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2023-12-24 21:01:51 +1300

    Task: Add #FastForwardAndDeleteBranches

---
+++ Tasks/#FastForwardAndDeleteBranches/Script.ps1
@@ -0,0 +1,66 @@
+$UpstreamOwner = 'microsoft'
+$UpstreamRepo = 'winget-pkgs'
+$UpstreamBranch = 'master'
+$OriginOwner = 'SpecterShell'
+$OriginRepo = 'winget-pkgs'
+$OriginBranch = 'master'
+
+$Query = @"
+{
+  repository(owner: "${OriginOwner}", name: "${OriginRepo}") {
+    refs(
+      refPrefix: "refs/heads/"
+      last: 100
+      orderBy: { field: TAG_COMMIT_DATE, direction: ASC }
+    ) {
+      nodes {
+        name
+        associatedPullRequests(last: 1, orderBy: { field: CREATED_AT, direction: DESC }) {
+          nodes {
+            number
+            state
+            isDraft
+            url
+            timelineItems(
+              last: 1
+              itemTypes: [HEAD_REF_DELETED_EVENT, HEAD_REF_RESTORED_EVENT]
+            ) {
+              nodes {
+                __typename
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+"@
+$Object = Invoke-GitHubApi -Uri 'https://api.github.com/graphql' -Method Post -Body @{ query = $Query }
+$Branches = $Object.data.repository.refs.nodes.Where({ $_.associatedPullRequests.nodes[0].state -eq 'MERGED' }).ForEach({ $_.name })
+
+$this.Logging("$($Branches.Count) branch(es) in ${OriginOwner}/${OriginRepo} have been merged. Deleting...")
+
+$Count = 0
+foreach ($Branch in $Branches) {
+  try {
+    $this.Logging("Deleting ${Branch}")
+    Invoke-GitHubApi -Uri "https://api.github.com/repos/${OriginOwner}/${OriginRepo}/git/refs/heads/${Branch}" -Method Delete | Out-Null
+    $Count++
+  } catch {
+    $this.Logging("Failed to delete branch ${Branch}", 'Error')
+    $_ | Out-Host
+  }
+}
+
+$this.Logging("$($Branches.Count) branch(es) to delete, ${Count} deleted, $($Branches.Count - $Count) not deleted")
+
+$this.Logging("Updating ${OriginOwner}/${OriginRepo}/${OriginBranch} to ${UpstreamOwner}/${UpstreamRepo}/${UpstreamBranch}")
+
+$UpstreamSha = $Global:LocalStorage['UpstreamSha'] ??= (Invoke-GitHubApi -Uri "https://api.github.com/repos/${UpstreamOwner}/${UpstreamRepo}/git/ref/heads/${UpstreamBranch}").object.sha
+
+Invoke-GitHubApi -Uri "https://api.github.com/repos/${OriginOwner}/${OriginRepo}/git/refs/heads/${OriginBranch}" -Method Patch -Body @{
+  sha = $UpstreamSha
+} | Out-Null
+
+$this.Logging("${OriginOwner}/${OriginRepo}/${OriginBranch} has been fast-forwarded to ${UpstreamOwner}/${UpstreamRepo}/${UpstreamBranch}")
