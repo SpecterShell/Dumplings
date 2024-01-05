466bedcd8b934426286aea17620c536474dab104
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-01-05 16:43:54 +0800

    Task: Add JetBrains tasks

---
+++ Tasks/JetBrains.TeamCity/Script.ps1
@@ -0,0 +1,62 @@
+$Object1 = $LocalStorage.JetBrainsApps.TC.release
+
+# Version
+$this.CurrentState.Version = $Object1.version
+
+# Installer
+$this.CurrentState.Installer += $InstallerX64 = [ordered]@{
+  Architecture           = 'x64'
+  InstallerUrl           = $Object1.downloads.windows.link
+  AppsAndFeaturesEntries = @(
+    [ordered]@{
+      DisplayVersion = "Build $($Object1.build)"
+      ProductCode    = 'JetBrains TeamCity'
+    }
+  )
+}
+# $this.CurrentState.Installer += $InstallerARM64 = [ordered]@{
+#   Architecture           = 'arm64'
+#   InstallerUrl           = $Object1.downloads.windowsARM64.link
+#   AppsAndFeaturesEntries = @(
+#     [ordered]@{
+#       DisplayVersion = "Build $($Object1.build)"
+#       ProductCode    = 'JetBrains TeamCity'
+#     }
+#   )
+# }
+
+# ReleaseTime
+$this.CurrentState.ReleaseTime = $Object1.date | Get-Date -Format 'yyyy-MM-dd'
+
+if ($Object1.whatsnew) {
+  # ReleaseNotes (en-US)
+  $this.CurrentState.Locale += [ordered]@{
+    Locale = 'en-US'
+    Key    = 'ReleaseNotes'
+    Value  = $Object1.whatsnew | ConvertFrom-Html | Get-TextContent | Format-Text
+  }
+} else {
+  $this.Logging("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+}
+
+# ReleaseNotesUrl
+$this.CurrentState.Locale += [ordered]@{
+  Key   = 'ReleaseNotesUrl'
+  Value = $Object1.notesLink
+}
+
+switch ($this.Check()) {
+  ({ $_ -ge 1 }) {
+    # InstallerSha256
+    $InstallerX64['InstallerSha256'] = (Invoke-RestMethod -Uri $Object1.downloads.windows.checksumLink).Split()[0].ToUpper()
+    # $InstallerARM64['InstallerSha256'] = (Invoke-RestMethod -Uri $Object1.downloads.windowsARM64.checksumLink).Split()[0].ToUpper()
+
+    $this.Write()
+  }
+  ({ $_ -ge 2 }) {
+    $this.Message()
+  }
+  ({ $_ -ge 3 }) {
+    $this.Submit()
+  }
+}
