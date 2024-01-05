a4f333a5a389a65bdb88962e8f283835c305a61d
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-01-05 11:21:35 +0800

    Task: Add and fix JetBrains tasks

---
+++ Tasks/JetBrains.WebStorm.EAP/Script.ps1
@@ -0,0 +1,64 @@
+$Object1 = $LocalStorage.JetBrainsApps.WS.eap
+
+# Version
+$this.CurrentState.Version = $Object1.build
+
+# Installer
+$this.CurrentState.Installer += $InstallerX64 = [ordered]@{
+  Architecture           = 'x64'
+  InstallerUrl           = $Object1.downloads.windows.link
+  ProductCode            = "WebStorm $($Object1.build)"
+  AppsAndFeaturesEntries = @(
+    [ordered]@{
+      DisplayName = "WebStorm $($Object1.build)"
+      ProductCode = "WebStorm $($Object1.build)"
+    }
+  )
+}
+$this.CurrentState.Installer += $InstallerARM64 = [ordered]@{
+  Architecture           = 'arm64'
+  InstallerUrl           = $Object1.downloads.windowsARM64.link
+  ProductCode            = "WebStorm $($Object1.build)"
+  AppsAndFeaturesEntries = @(
+    [ordered]@{
+      DisplayName = "WebStorm $($Object1.build)"
+      ProductCode = "WebStorm $($Object1.build)"
+    }
+  )
+}
+
+# ReleaseTime
+$this.CurrentState.ReleaseTime = $Object1.date | Get-Date -Format 'yyyy-MM-dd'
+
+if ($Object1.whatsnew) {
+  # ReleaseNotes (en-US)
+  $this.CurrentState.Locale += [ordered]@{
+    Locale = 'en-US'
+    Key    = 'ReleaseNotes'
+    Value  = $Object1.whatsnew | ConvertFrom-Html | Get-TextContent | Format-Text
+  }
+} else {
+  $this.Logging("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+}
+
+# ReleaseNotesUrl
+$this.CurrentState.Locale += [ordered]@{
+  Key   = 'ReleaseNotesUrl'
+  Value = $Object1.notesLink
+}
+
+switch ($this.Check()) {
+  ({ $_ -ge 1 }) {
+    # InstallerSha256
+    $InstallerX64['InstallerSha256'] = (Invoke-RestMethod -Uri $Object1.downloads.windows.checksumLink).Split()[0].ToUpper()
+    $InstallerARM64['InstallerSha256'] = (Invoke-RestMethod -Uri $Object1.downloads.windowsARM64.checksumLink).Split()[0].ToUpper()
+
+    $this.Write()
+  }
+  ({ $_ -ge 2 }) {
+    $this.Message()
+  }
+  ({ $_ -ge 3 }) {
+    $this.Submit()
+  }
+}
