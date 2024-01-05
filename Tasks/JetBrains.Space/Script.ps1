d5dd2b1b71fabc5e9253aba9fb1124e58b45025e
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-01-05 12:07:11 +0800

    Task: Add JetBrains.Space

---
+++ Tasks/JetBrains.Space/Script.ps1
@@ -0,0 +1,15 @@
+$Prefix = 'https://download.jetbrains.com/space/'
+
+$this.CurrentState = Invoke-RestMethod -Uri "${Prefix}latest.yml?noCache=$((New-Guid).Guid.Split('-')[0])" | ConvertFrom-Yaml | ConvertFrom-ElectronUpdater -Locale 'en-US'
+
+switch ($this.Check()) {
+  ({ $_ -ge 1 }) {
+    $this.Write()
+  }
+  ({ $_ -ge 2 }) {
+    $this.Message()
+  }
+  ({ $_ -ge 3 }) {
+    $this.Submit()
+  }
+}
