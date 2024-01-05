6f8736daab19084d0935d9c068ca12a11330b6f7
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-01-05 23:58:32 +0800

    Task: Add JetBrains tasks

---
+++ Tasks/JetBrains.DataSpell.EAP/Script.ps1
@@ -0,0 +1,77 @@
+$Object1 = $LocalStorage.JetBrainsApps.DS.eap
+
+# Version
+$this.CurrentState.Version = $Object1.build
+
+# Installer
+$this.CurrentState.Installer += $InstallerX64 = [ordered]@{
+  Architecture           = 'x64'
+  InstallerUrl           = $Object1.downloads.windows.link
+  ProductCode            = "DataSpell $($Object1.build)"
+  AppsAndFeaturesEntries = @(
+    [ordered]@{
+      DisplayName = "DataSpell $($Object1.build)"
+      ProductCode = "DataSpell $($Object1.build)"
+    }
+  )
+}
+$this.CurrentState.Installer += $InstallerARM64 = [ordered]@{
+  Architecture           = 'arm64'
+  InstallerUrl           = $Object1.downloads.windowsARM64.link
+  ProductCode            = "DataSpell $($Object1.build)"
+  AppsAndFeaturesEntries = @(
+    [ordered]@{
+      DisplayName = "DataSpell $($Object1.build)"
+      ProductCode = "DataSpell $($Object1.build)"
+    }
+  )
+}
+
+# ReleaseTime
+$this.CurrentState.ReleaseTime = $Object1.date | Get-Date -Format 'yyyy-MM-dd'
+
+if ($Object1.whatsnew) {
+  # ReleaseNotes (en-US)
+  $this.CurrentState.Locale += [ordered]@{
+    Locale = 'en-US'
+    Key    = 'ReleaseNotes'
+    Value  = $Object1.whatsnew | ConvertFrom-Html | Get-TextContent | Format-Text
+  }
+} else {
+  $this.Logging("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+}
+
+if ($Object1.notesLink) {
+  # ReleaseNotesUrl
+  $this.CurrentState.Locale += [ordered]@{
+    Key   = 'ReleaseNotesUrl'
+    Value = $Object1.notesLink
+  }
+} else {
+  # ReleaseNotesUrl
+  $this.CurrentState.Locale += [ordered]@{
+    Key   = 'ReleaseNotesUrl'
+    Value = 'https://www.jetbrains.com/dataspell/whatsnew/'
+  }
+  $this.CurrentState.Locale += [ordered]@{
+    Locale = 'zh-CN'
+    Key    = 'ReleaseNotesUrl'
+    Value  = 'https://www.jetbrains.com/zh-cn/dataspell/whatsnew/'
+  }
+}
+
+switch ($this.Check()) {
+  ({ $_ -ge 1 }) {
+    # InstallerSha256
+    $InstallerX64['InstallerSha256'] = (Invoke-RestMethod -Uri $Object1.downloads.windows.checksumLink).Split()[0].ToUpper()
+    $InstallerARM64['InstallerSha256'] = (Invoke-RestMethod -Uri $Object1.downloads.windowsARM64.checksumLink).Split()[0].ToUpper()
+
+    $this.Write()
+  }
+  ({ $_ -ge 2 }) {
+    $this.Message()
+  }
+  ({ $_ -ge 3 }) {
+    $this.Submit()
+  }
+}
---
+++ Tasks/JetBrains.DataSpell/Script.ps1
@@ -0,0 +1,79 @@
+$Object1 = $LocalStorage.JetBrainsApps.DS.release
+
+# Version
+$this.CurrentState.Version = $Object1.version
+
+# Installer
+$this.CurrentState.Installer += $InstallerX64 = [ordered]@{
+  Architecture           = 'x64'
+  InstallerUrl           = $Object1.downloads.windows.link
+  ProductCode            = "DataSpell $($this.CurrentState.Version)"
+  AppsAndFeaturesEntries = @(
+    [ordered]@{
+      DisplayName    = "DataSpell $($this.CurrentState.Version)"
+      DisplayVersion = $Object1.build
+      ProductCode    = "DataSpell $($this.CurrentState.Version)"
+    }
+  )
+}
+$this.CurrentState.Installer += $InstallerARM64 = [ordered]@{
+  Architecture           = 'arm64'
+  InstallerUrl           = $Object1.downloads.windowsARM64.link
+  ProductCode            = "DataSpell $($this.CurrentState.Version)"
+  AppsAndFeaturesEntries = @(
+    [ordered]@{
+      DisplayName    = "DataSpell $($this.CurrentState.Version)"
+      DisplayVersion = $Object1.build
+      ProductCode    = "DataSpell $($this.CurrentState.Version)"
+    }
+  )
+}
+
+# ReleaseTime
+$this.CurrentState.ReleaseTime = $Object1.date | Get-Date -Format 'yyyy-MM-dd'
+
+if ($Object1.whatsnew) {
+  # ReleaseNotes (en-US)
+  $this.CurrentState.Locale += [ordered]@{
+    Locale = 'en-US'
+    Key    = 'ReleaseNotes'
+    Value  = $Object1.whatsnew | ConvertFrom-Html | Get-TextContent | Format-Text
+  }
+} else {
+  $this.Logging("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+}
+
+if ($Object1.notesLink) {
+  # ReleaseNotesUrl
+  $this.CurrentState.Locale += [ordered]@{
+    Key   = 'ReleaseNotesUrl'
+    Value = $Object1.notesLink
+  }
+} else {
+  # ReleaseNotesUrl
+  $this.CurrentState.Locale += [ordered]@{
+    Key   = 'ReleaseNotesUrl'
+    Value = 'https://www.jetbrains.com/dataspell/whatsnew/'
+  }
+  $this.CurrentState.Locale += [ordered]@{
+    Locale = 'zh-CN'
+    Key    = 'ReleaseNotesUrl'
+    Value  = 'https://www.jetbrains.com/zh-cn/dataspell/whatsnew/'
+  }
+}
+
+switch ($this.Check()) {
+  ({ $_ -ge 1 }) {
+    # InstallerSha256
+    $InstallerX64['InstallerSha256'] = (Invoke-RestMethod -Uri $Object1.downloads.windows.checksumLink).Split()[0].ToUpper()
+    $InstallerARM64['InstallerSha256'] = (Invoke-RestMethod -Uri $Object1.downloads.windowsARM64.checksumLink).Split()[0].ToUpper()
+
+    $this.Write()
+  }
+  ({ $_ -ge 2 }) {
+    $this.Message()
+  }
+  ({ $_ -ge 3 }) {
+    $this.Submit()
+  }
+}
