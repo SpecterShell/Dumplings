474fdd4b61c70f62e9007c35f8c3b48a236093bb
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-01-06 13:58:10 +0800

    Task: Add and enhance JetBrains tasks

---
+++ Tasks/JetBrains.dotUltimate/Script.ps1
@@ -0,0 +1,44 @@
+$Object1 = $LocalStorage.JetBrainsApps.RSU.release
+
+# Version
+$this.CurrentState.Version = $Object1.version
+
+# Installer
+$this.CurrentState.Installer += $Installer = [ordered]@{
+  InstallerUrl = $Object1.downloads.windows.link
+}
+
+# ReleaseTime
+$this.CurrentState.ReleaseTime = $Object1.date | Get-Date -Format 'yyyy-MM-dd'
+
+if ($Object1.whatsnew) {
+  # ReleaseNotes (en-US)
+  $this.CurrentState.Locale += [ordered]@{
+    Locale = 'en-US'
+    Key    = 'ReleaseNotes'
+    Value  = $Object1.whatsnew | ConvertFrom-Html | Get-TextContent | Format-Text
+  }
+} else {
+  $this.Logging("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+}
+
+# ReleaseNotesUrl
+$this.CurrentState.Locale += [ordered]@{
+  Key   = 'ReleaseNotesUrl'
+  Value = $Object1.notesLink
+}
+
+switch ($this.Check()) {
+  ({ $_ -ge 1 }) {
+    # InstallerSha256
+    $Installer['InstallerSha256'] = (Invoke-RestMethod -Uri $Object1.downloads.windows.checksumLink).Split()[0].ToUpper()
+
+    $this.Write()
+  }
+  ({ $_ -ge 2 }) {
+    $this.Message()
+  }
+  ({ $_ -ge 3 }) {
+    $this.Submit()
+  }
+}
