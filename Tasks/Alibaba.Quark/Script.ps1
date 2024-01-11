9c5b73c966a92aa788284f4968301a3cb9252508
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-01-11 16:06:52 +0800

    Task: Add Alibaba.Quark

---
+++ Tasks/Alibaba.Quark/Script.ps1
@@ -0,0 +1,21 @@
+$Object1 = (Invoke-WebRequest -Uri 'https://www.quark.cn/').Content | Get-EmbeddedJson -StartsFrom 'window.__wh_data__ = ' | ConvertFrom-Json
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $InstallerUrl = $Object1.page.'10005'.downloadConfig.downloadLink
+}
+
+# Version
+$this.CurrentState.Version = [regex]::Match($InstallerUrl, 'V(\d+\.\d+\.\d+\.\d+)').Groups[1].Value
+
+switch ($this.Check()) {
+  ({ $_ -ge 1 }) {
+    $this.Write()
+  }
+  ({ $_ -ge 2 }) {
+    $this.Message()
+  }
+  ({ $_ -ge 3 }) {
+    $this.Submit()
+  }
+}
