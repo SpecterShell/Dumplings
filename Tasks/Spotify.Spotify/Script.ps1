4208ce92eccbbe6f677d7eaddd94cab5b746398d
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-01-22 17:04:08 +0800

    Task: Add Spotify.Spotify

---
+++ Tasks/Spotify.Spotify/Script.ps1
@@ -0,0 +1,35 @@
+# https://github.com/amd64fox/LoaderSpot
+
+$Object1 = Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/amd64fox/LoaderSpot/main/versions.json' | Read-ResponseContent | ConvertFrom-Json -AsHashtable
+
+# Version
+$this.CurrentState.Version = $Version = $Object1.Keys | Sort-Object -Property { $_ -creplace '\d+', { $_.Value.PadLeft(20) } } | Select-Object -Last 1
+
+# RealVersion
+$this.CurrentState.RealVersion = $Object1[$Version].fullversion
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x86'
+  InstallerUrl = $Object1[$Version].links.win.x86
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = $Object1[$Version].links.win.x64
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'arm64'
+  InstallerUrl = $Object1[$Version].links.win.arm64
+}
+
+switch ($this.Check()) {
+  ({ $_ -ge 1 }) {
+    $this.Write()
+  }
+  ({ $_ -ge 2 }) {
+    $this.Message()
+  }
+  ({ $_ -ge 3 }) {
+    $this.Submit()
+  }
+}
