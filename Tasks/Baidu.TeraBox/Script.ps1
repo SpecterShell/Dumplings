6127f4f55c8b25e27c72abbb4e057c15e94d8e5e
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-01-23 19:20:56 +0800

    Task: Baidu.TeraBox Fix version

--- Tasks/Baidu.TeraBox/Script.ps1
+++ Tasks/Baidu.TeraBox/Script.ps1
@@ -1,10 +1,10 @@
 $Object1 = Invoke-RestMethod -Uri 'https://www.terabox.com/api/getsyscfg?clienttype=0&language_type=&cfg_category_keys=[]&version=0'
 
 # Version
-$this.CurrentState.Version = [regex]::Match($Object1.fe_web_guide_setting.cfg_list[0].version, '(\d+\.\d+\.\d+)\.\d+').Groups[1].Value
+$this.CurrentState.Version = $Version = [regex]::Match($Object1.fe_web_guide_setting.cfg_list[0].version, '(\d+\.\d+\.\d+\.\d+)').Groups[1].Value
 
 # RealVersion
-$this.CurrentState.RealVersion = $RealVersion = [regex]::Match($Object1.fe_web_guide_setting.cfg_list[0].version, '(\d+\.\d+\.\d+\.\d+)').Groups[1].Value
+$this.CurrentState.RealVersion = [regex]::Match($Object1.fe_web_guide_setting.cfg_list[0].version, '(\d+\.\d+\.\d+)\.\d+').Groups[1].Value
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
@@ -14,12 +14,12 @@ $this.CurrentState.Installer += [ordered]@{
 # ReleaseTime
 $this.CurrentState.ReleaseTime = $Object1.fe_web_guide_setting.cfg_list[0].update_time | Get-Date -Format 'yyyy-MM-dd'
 
-if ($LocalStorage.Contains('TeraBox') -and $LocalStorage.TeraBox.Contains($RealVersion)) {
+if ($LocalStorage.Contains('TeraBox') -and $LocalStorage.TeraBox.Contains($Version)) {
   # ReleaseNotes (en-US)
   $this.CurrentState.Locale += [ordered]@{
     Locale = 'en-US'
     Key    = 'ReleaseNotes'
-    Value  = $LocalStorage.TeraBox.$RealVersion.ReleaseNotesEN
+    Value  = $LocalStorage.TeraBox.$Version.ReleaseNotesEN
   }
 } else {
   $this.Logging("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
