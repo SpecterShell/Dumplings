f446e3f8185f71789eae6c4e6e47ba911fcc1d4c
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-01-25 14:26:01 +0800

    Task: Add Vivaldi.Vivaldi

---
+++ Tasks/Vivaldi.Vivaldi/Script.ps1
@@ -0,0 +1,68 @@
+# x86
+$Object1 = Invoke-RestMethod -Uri 'https://update.vivaldi.com/update/1.0/public/appcast.xml'
+# x64
+$Object2 = Invoke-RestMethod -Uri 'https://update.vivaldi.com/update/1.0/public/appcast.x64.xml'
+
+# Version
+$this.CurrentState.Version = $Object2.enclosure.version
+
+$Identical = $true
+if ($Object1.enclosure.version -ne $Object2.enclosure.version) {
+  $this.Logging('Distinct versions detected', 'Warning')
+  $Identical = $false
+}
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x86'
+  InstallerUrl = $Object1.enclosure.url
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = $Object2.enclosure.url
+}
+
+# ReleaseNotesUrl
+$this.CurrentState.Locale += [ordered]@{
+  Key   = 'ReleaseNotesUrl'
+  Value = $ReleaseNotesUrl = $Object2.releaseNotesLink
+}
+
+switch ($this.Check()) {
+  ({ $_ -ge 1 }) {
+    try {
+      $Object3 = Invoke-WebRequest -Uri $ReleaseNotesUrl | ConvertFrom-Html
+
+      if ($this.LastState.Contains('Version')) {
+        $ReleaseNotesTitleNode = $Object3.SelectSingleNode("/html/body/h2[contains(text(), '$($this.LastState.Version -creplace '(\d+\.\d+)\.(\d+\.\d+)', '$1 ($2)')')]")
+        if ($ReleaseNotesTitleNode) {
+          $ReleaseNotesNodes = @()
+          for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node.Name -ne 'h2'; $Node = $Node.NextSibling) {
+            $ReleaseNotesNodes += $Node
+          }
+          # ReleaseNotes (zh-CN)
+          $this.CurrentState.Locale += [ordered]@{
+            Locale = 'zh-CN'
+            Key    = 'ReleaseNotes'
+            Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
+          }
+        } else {
+          $this.Logging("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+        }
+      } else {
+        $this.Logging("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Logging($_, 'Warning')
+    }
+
+    $this.Write()
+  }
+  ({ $_ -ge 2 }) {
+    $this.Message()
+  }
+  ({ $_ -ge 3 -and $Identical }) {
+    $this.Submit()
+  }
+}
