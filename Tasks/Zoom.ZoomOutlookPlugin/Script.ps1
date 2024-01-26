8ed746300326e2f5df4dae0b461ce92dcb4b9c4b
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-01-26 23:15:16 +0800

    Task: Add Zoom.ZoomOutlookPlugin

---
+++ Tasks/Zoom.ZoomOutlookPlugin/Script.ps1
@@ -0,0 +1,24 @@
+$Object1 = Invoke-WebRequest -Uri 'https://us05web.zoom.us/product/version' -Method Post -UserAgent 'Mozilla/5.0 (ZOOM.Win 10.0 x64)' -Headers @{ 'ZM-CAP' = '8300567970761955255,6445493618999263204' } -Form @{ productName = 'outlookplugin' } | ConvertFrom-ProtoBuf
+
+# Version
+$this.CurrentState.Version = $Object1.'10'
+
+# RealVersion
+$this.CurrentState.RealVersion = $this.CurrentState.Version.Split('.')[0..2] -join '.'
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = "https://zoom.us/client/$($this.CurrentState.Version)/ZoomOutlookPluginSetup.msi"
+}
+
+switch ($this.Check()) {
+  ({ $_ -ge 1 }) {
+    $this.Write()
+  }
+  ({ $_ -ge 2 }) {
+    $this.Message()
+  }
+  ({ $_ -ge 3 }) {
+    $this.Submit()
+  }
+}
