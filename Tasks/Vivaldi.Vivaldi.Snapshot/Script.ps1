d62d5a070bddb3005d8e7e4d6bb00d645a063c6d
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-01-26 17:32:46 +0800

    Task: Add Vivaldi.Vivaldi.Snapshot

---
+++ Tasks/Vivaldi.Vivaldi.Snapshot/Script.ps1
@@ -0,0 +1,64 @@
+# x86
+$Object1 = Invoke-RestMethod -Uri 'https://update.vivaldi.com/update/1.0/win/appcast.xml'
+# x64
+$Object2 = Invoke-RestMethod -Uri 'https://update.vivaldi.com/update/1.0/win/appcast.x64.xml'
+
+# Version
+$this.CurrentState.Version = $Object2.enclosure.version
+
+$Identical = $true
+if ($Object1.enclosure.version -ne $Object2.enclosure.version) {
+  $this.Logging('Distinct versions detected', 'Warning')
+  $Identical = $false
+}
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x86'
+  InstallerUrl = $Object1.enclosure.url.Replace('snapshot-auto', 'snapshot')
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = $Object2.enclosure.url.Replace('snapshot-auto', 'snapshot')
+}
+
+# ReleaseNotesUrl
+$this.CurrentState.Locale += [ordered]@{
+  Key   = 'ReleaseNotesUrl'
+  Value = $ReleaseNotesUrl = $Object2.releaseNotesLink
+}
+
+switch ($this.Check()) {
+  ({ $_ -ge 1 }) {
+    try {
+      $Object3 = Invoke-WebRequest -Uri $ReleaseNotesUrl | ConvertFrom-Html
+
+      $ReleaseNotesTitleNode = $Object3.SelectSingleNode("/html/body/h2[contains(text(), 'Changelog since')][1]")
+      if ($ReleaseNotesTitleNode) {
+        $ReleaseNotesNodes = @()
+        for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node -and $Node.Name -ne 'h2'; $Node = $Node.NextSibling) {
+          $ReleaseNotesNodes += $Node
+        }
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Logging("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Logging($_, 'Warning')
+    }
+
+    $this.Write()
+  }
+  ({ $_ -ge 2 }) {
+    $this.Message()
+  }
+  ({ $_ -ge 3 -and $Identical }) {
+    $this.Submit()
+  }
+}
