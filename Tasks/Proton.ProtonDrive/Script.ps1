e7760562dd0180b0c7420607163c1728921a4d08
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-01-26 19:02:42 +0800

    Task: Add Proton.ProtonDrive

---
+++ Tasks/Proton.ProtonDrive/Script.ps1
@@ -0,0 +1,41 @@
+$Object1 = (Invoke-RestMethod -Uri 'https://proton.me/download/drive/windows/version.json').Releases.Where({ $_.CategoryName -eq 'Stable' })[0]
+
+# Version
+$this.CurrentState.Version = $Object1.Version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.File.Url | ConvertTo-UnescapedUri
+}
+
+# ReleaseTime
+$this.CurrentState.ReleaseTime = $Object1.ReleaseDate | Get-Date -Format 'yyyy-MM-dd'
+
+# ReleaseNotes (en-US)
+$this.CurrentState.Locale += [ordered]@{
+  Locale = 'en-US'
+  Key    = 'ReleaseNotes'
+  Value  = (
+    $Object1.ReleaseNotes | ForEach-Object -Process {
+      $TypeName = switch ($_.Type) {
+        'Improvement' { 'Improvements' }
+        'Fix' { 'Bug Fixes' }
+        'Feature' { 'New Features' }
+        Default { $_ }
+      }
+      "${TypeName}`n$($_.Notes | ConvertTo-UnorderedList)"
+    }
+  ) -join "`n" | Format-Text
+}
+
+switch ($this.Check()) {
+  ({ $_ -ge 1 }) {
+    $this.Write()
+  }
+  ({ $_ -ge 2 }) {
+    $this.Message()
+  }
+  ({ $_ -ge 3 }) {
+    $this.Submit()
+  }
+}
