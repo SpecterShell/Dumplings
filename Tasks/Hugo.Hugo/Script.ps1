4cf16e66b2cceff91e722f347a8fa4ffc084d3d5
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-01-27 23:47:05 +0800

    Task: Add Hugo.Hugo and Hugo.Hugo.Extended

---
+++ Tasks/Hugo.Hugo/Script.ps1
@@ -0,0 +1,45 @@
+$RepoOwner = 'gohugoio'
+$RepoName = 'hugo'
+
+$Object1 = Invoke-GitHubApi -Uri "https://api.github.com/repos/${RepoOwner}/${RepoName}/releases/latest"
+
+# Version
+$this.CurrentState.Version = $Object1.tag_name -creplace '^v', ''
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = $Object1.assets.Where({ $_.name.EndsWith('.zip') -and $_.name.Contains('windows') -and $_.name.Contains('amd64') -and -not $_.name.Contains('extended') })[0].browser_download_url
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'arm64'
+  InstallerUrl = $Object1.assets.Where({ $_.name.EndsWith('.zip') -and $_.name.Contains('windows') -and $_.name.Contains('arm64') -and -not $_.name.Contains('extended') })[0].browser_download_url
+}
+
+# ReleaseTime
+$this.CurrentState.ReleaseTime = $Object1.published_at.ToUniversalTime()
+
+# ReleaseNotes (zh-CN)
+$this.CurrentState.Locale += [ordered]@{
+  Locale = 'zh-CN'
+  Key    = 'ReleaseNotes'
+  Value  = ($Object1.body | ConvertFrom-Markdown).Html | ConvertFrom-Html | Get-TextContent | Format-Text
+}
+
+# ReleaseNotesUrl
+$this.CurrentState.Locale += [ordered]@{
+  Key   = 'ReleaseNotesUrl'
+  Value = $Object1.html_url
+}
+
+switch ($this.Check()) {
+  ({ $_ -ge 1 }) {
+    $this.Write()
+  }
+  ({ $_ -ge 2 }) {
+    $this.Message()
+  }
+  ({ $_ -ge 3 }) {
+    $this.Submit()
+  }
+}
