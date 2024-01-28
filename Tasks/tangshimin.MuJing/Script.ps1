da9c595f74261bc84d16787b1fc6b10e0ee22f20
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-01-28 22:01:26 +0800

    Task: Add tasks

---
+++ Tasks/tangshimin.MuJing/Script.ps1
@@ -0,0 +1,53 @@
+$RepoOwner = 'tangshimin'
+$RepoName = 'MuJing'
+
+$Object1 = Invoke-GitHubApi -Uri "https://api.github.com/repos/${RepoOwner}/${RepoName}/releases/latest"
+
+# Version
+$this.CurrentState.Version = $Object1.tag_name -creplace '^v'
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.assets.Where({ $_.name.EndsWith('.msi') })[0].browser_download_url | ConvertTo-UnescapedUri
+}
+
+# ReleaseTime
+$this.CurrentState.ReleaseTime = $Object1.published_at.ToUniversalTime()
+
+if (-not [string]::IsNullOrWhiteSpace($Object1.body)) {
+  $ReleaseNotesObject = ($Object1.body | ConvertFrom-Markdown).Html | ConvertFrom-Html
+  $ReleaseNotesNodes = [System.Collections.Generic.List[System.Object]]::new()
+  for ($Node = $ReleaseNotesObject.ChildNodes[0]; $Node -and $Node.Name -ne 'hr'; $Node = $Node.NextSibling) {
+    $ReleaseNotesNodes.Add($Node)
+  }
+  if ($ReleaseNotesNodes) {
+    # ReleaseNotes (zh-CN)
+    $this.CurrentState.Locale += [ordered]@{
+      Locale = 'zh-CN'
+      Key    = 'ReleaseNotes'
+      Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
+    }
+  } else {
+    $this.Logging("No ReleaseNotes (zh-CN) for version $($this.CurrentState.Version)", 'Warning')
+  }
+} else {
+  $this.Logging("No ReleaseNotes (zh-CN) for version $($this.CurrentState.Version)", 'Warning')
+}
+
+# ReleaseNotesUrl
+$this.CurrentState.Locale += [ordered]@{
+  Key   = 'ReleaseNotesUrl'
+  Value = $Object1.html_url
+}
+
+switch ($this.Check()) {
+  ({ $_ -ge 1 }) {
+    $this.Write()
+  }
+  ({ $_ -ge 2 }) {
+    $this.Message()
+  }
+  ({ $_ -ge 3 }) {
+    $this.Submit()
+  }
+}
