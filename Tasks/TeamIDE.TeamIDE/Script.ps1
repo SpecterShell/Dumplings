e7b4863e8602a24e725922231edea90ea97e29df
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-01-29 18:47:03 +0800

    Task: TeamIDE.TeamIDE Use latest.yml

--- Tasks/TeamIDE.TeamIDE/Script.ps1
+++ Tasks/TeamIDE.TeamIDE/Script.ps1
@@ -1,48 +1,57 @@
 $RepoOwner = 'team-ide'
 $RepoName = 'teamide'
 
-$Object1 = Invoke-GitHubApi -Uri "https://api.github.com/repos/${RepoOwner}/${RepoName}/releases/latest"
+$Object1 = Invoke-RestMethod -Uri "https://github.com/${RepoOwner}/${RepoName}/releases/latest/download/latest.yml" | ConvertFrom-Yaml
 
 # Version
-$this.CurrentState.Version = $Object1.tag_name -creplace '^v'
+$this.CurrentState.Version = $Object1.version
+
+$Prefix = "https://github.com/${RepoOwner}/${RepoName}/releases/download/v$($this.CurrentState.Version)/"
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = $Object1.assets.Where({ $_.name.EndsWith('.exe') -and $_.name.Contains('win') })[0].browser_download_url | ConvertTo-UnescapedUri
+  InstallerUrl = $Prefix + $Object1.files[0].url
 }
 
 # ReleaseTime
-$this.CurrentState.ReleaseTime = $Object1.published_at.ToUniversalTime()
-
-if (-not [string]::IsNullOrWhiteSpace($Object1.body)) {
-  $ReleaseNotesObject = ($Object1.body | ConvertFrom-Markdown).Html | ConvertFrom-Html
-  $ReleaseNotesTitleNode = $ReleaseNotesObject.SelectSingleNode('./p[text()="发布功能"]')
-  if ($ReleaseNotesTitleNode) {
-    $ReleaseNotesNodes = [System.Collections.Generic.List[System.Object]]::new()
-    for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node; $Node = $Node.NextSibling) {
-      $ReleaseNotesNodes.Add($Node)
-    }
-    # ReleaseNotes (zh-CN)
-    $this.CurrentState.Locale += [ordered]@{
-      Locale = 'zh-CN'
-      Key    = 'ReleaseNotes'
-      Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
-    }
-  } else {
-    $this.Logging("No ReleaseNotes (zh-CN) for version $($this.CurrentState.Version)", 'Warning')
-  }
-} else {
-  $this.Logging("No ReleaseNotes (zh-CN) for version $($this.CurrentState.Version)", 'Warning')
-}
+$this.CurrentState.ReleaseTime = $Object1.releaseDate | Get-Date -AsUTC
 
 # ReleaseNotesUrl
 $this.CurrentState.Locale += [ordered]@{
   Key   = 'ReleaseNotesUrl'
-  Value = $Object1.html_url
+  Value = "https://github.com/${RepoOwner}/${RepoName}/releases/tag/v$($this.CurrentState.Version)"
 }
 
 switch ($this.Check()) {
   ({ $_ -ge 1 }) {
+    try {
+      $Object2 = (Invoke-RestMethod -Uri "https://github.com/${RepoOwner}/${RepoName}/releases.atom").Where({ $_.id.EndsWith("v$($this.CurrentState.Version)") })[0]
+
+      if ($Object2.content.'#text' -ne 'No content.') {
+        $ReleaseNotesObject = $Object2.content.'#text' | ConvertFrom-Html
+        $ReleaseNotesTitleNode = $ReleaseNotesObject.SelectSingleNode('./p[text()="发布功能"]')
+        if ($ReleaseNotesTitleNode) {
+          $ReleaseNotesNodes = [System.Collections.Generic.List[System.Object]]::new()
+          for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node; $Node = $Node.NextSibling) {
+            $ReleaseNotesNodes.Add($Node)
+          }
+          # ReleaseNotes (zh-CN)
+          $this.CurrentState.Locale += [ordered]@{
+            Locale = 'zh-CN'
+            Key    = 'ReleaseNotes'
+            Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
+          }
+        } else {
+          $this.Logging("No ReleaseNotes (zh-CN) for version $($this.CurrentState.Version)", 'Warning')
+        }
+      } else {
+        $this.Logging("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Logging($_, 'Warning')
+    }
+
     $this.Write()
   }
   ({ $_ -ge 2 }) {
