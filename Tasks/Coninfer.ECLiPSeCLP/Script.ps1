f297adb77654270daa8f1d02ef2b6d5f8adce4f3
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-02-01 23:24:33 +0800

    Task: Add Coninfer.ECLiPSeCLP

---
+++ Tasks/Coninfer.ECLiPSeCLP/Script.ps1
@@ -0,0 +1,24 @@
+$Prefix = 'https://eclipseclp.org/Distribution/Builds/'
+
+$Object1 = Invoke-WebRequest -Uri "${Prefix}?C=N;O=D;V=1;F=0;P=*_*" | ConvertFrom-Html
+
+$Version = $Object1.SelectSingleNode('/html/body/ul/li[2]/a').InnerText.Trim() -replace '/$'
+
+# Version
+$this.CurrentState.Version = $Version -replace '_', ' #'
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = "${Prefix}${Version}/x86_64_nt/ECLiPSe_${Version}_x86_64_nt.exe"
+  ProductCode  = "ECLiPSe $($Version.Split('_')[0]) (64 bit)"
+}
+
+switch ($this.Check()) {
+  ({ $_ -ge 1 }) {
+    $this.Write()
+  }
+  ({ $_ -ge 2 }) {
+    $this.Message()
+  }
+}
