38e1351bb59854f00be3b2ad4f10e78c12b76a46
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-02-01 16:29:17 +0800

    Task: Add tasks

---
+++ Tasks/RProject.Rtools/Script.ps1
@@ -0,0 +1,60 @@
+$Prefix = 'https://cloud.r-project.org/bin/windows/Rtools/'
+
+$Object1 = Invoke-WebRequest -Uri $Prefix | ConvertFrom-Html
+
+# Version
+$this.CurrentState.Version = [regex]::Match(
+  $Object1.SelectSingleNode('/html/body/table/tr[1]/td[1]/a').InnerText,
+  'RTools ([\d\.]+)'
+).Groups[1].Value
+$ShortVersion = $this.CurrentState.Version.Replace('.', '')
+
+$Object2 = (Invoke-WebRequest -Uri "${Prefix}rtools${ShortVersion}/files/").Content
+$VersionMatches = [regex]::Match($Object2, "(rtools${ShortVersion}-(\d+)-\d+\.exe)")
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = "${Prefix}rtools${ShortVersion}/files/$($VersionMatches.Groups[1].Value)"
+  ProductCode  = "Rtools${ShortVersion}_is1"
+}
+
+# ReleaseNotesUrl
+$this.CurrentState.Locale += [ordered]@{
+  Key   = 'ReleaseNotesUrl'
+  Value = $ReleaseNotesUrl = "${Prefix}rtools${ShortVersion}/news.html"
+}
+
+switch ($this.Check()) {
+  ({ $_ -ge 1 }) {
+    try {
+      $Object3 = Invoke-WebRequest -Uri $ReleaseNotesUrl | ConvertFrom-Html
+
+      $ReleaseNotesTitleNode = $Object3.SelectSingleNode("/html/body/h3[contains(text(), '$($VersionMatches.Groups[2].Value)')]")
+      if ($ReleaseNotesTitleNode) {
+        # ReleaseNotes (en-US)
+        $ReleaseNotesNodes = [System.Collections.Generic.List[System.Object]]::new()
+        for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node.Name -ne 'h3'; $Node = $Node.NextSibling) {
+          $ReleaseNotesNodes.Add($Node)
+        }
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Logging("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Logging($_, 'Warning')
+    }
+
+    $this.Write()
+  }
+  ({ $_ -ge 2 }) {
+    $this.Message()
+  }
+  ({ $_ -ge 3 }) {
+    $this.Submit()
+  }
+}
---
+++ Tasks/den4b.Colors/Script.ps1
@@ -0,0 +1,41 @@
+$Object1 = Invoke-WebRequest -Uri 'https://www.den4b.com/changelogs/colors' | ConvertFrom-Html
+
+# Version
+$this.CurrentState.Version = $Object1.SelectNodes('//tbody[@class="changelogVersionBlock" and contains(./tr[1]/@class, "bg-success")][1]/tr[1]/td[1]/strong').InnerText.Trim()
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = "https://www.den4b.com/download/colors/installer/$($this.CurrentState.Version)?token=winget"
+}
+
+$this.CurrentState.ReleaseTime = $Object1.SelectSingleNode("//tbody[@id='v$($this.CurrentState.Version)']/tr[1]/td[2]").InnerText.Trim() | Get-Date -Format 'yyyy-MM-dd'
+
+switch ($this.Check()) {
+  ({ $_ -ge 1 }) {
+    try {
+      $Object2 = (Invoke-RestMethod -Uri 'https://www.den4b.com/news.atom').Where({ $_.title.Contains('Colors') -and $_.title.Contains($this.CurrentState.Version) }, 'First')
+
+      if ($Object2) {
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $Object2[0].content.'#text' | ConvertFrom-Html | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Logging("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Logging($_, 'Warning')
+    }
+
+    $this.Write()
+  }
+  ({ $_ -ge 2 }) {
+    $this.Message()
+  }
+  ({ $_ -ge 3 }) {
+    $this.Submit()
+  }
+}
