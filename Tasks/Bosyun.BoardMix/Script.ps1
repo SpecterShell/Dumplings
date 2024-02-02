81773d8d0a55123d5516c4350b238eea8f7c5882
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-02-02 13:31:07 +0800

    Task: Add Bosyun.BoardMix

---
+++ Tasks/Bosyun.BoardMix/Script.ps1
@@ -0,0 +1,15 @@
+$Prefix = 'https://api.boardmix.com/api/upgrade/desktop/bosyun/'
+
+$this.CurrentState = Invoke-WebRequest -Uri "${Prefix}latest.yml?noCache=$(Get-Random)" | Read-ResponseContent | ConvertFrom-Yaml | ConvertFrom-ElectronUpdater -Locale 'zh-CN'
+
+switch ($this.Check()) {
+  ({ $_ -ge 1 }) {
+    $this.Write()
+  }
+  ({ $_ -ge 2 }) {
+    $this.Message()
+  }
+  ({ $_ -ge 3 }) {
+    $this.Submit()
+  }
+}
