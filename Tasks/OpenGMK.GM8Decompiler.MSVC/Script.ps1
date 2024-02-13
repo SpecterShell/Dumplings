ad80f9b2fa89f22b061b70427834512b6b0b2204
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-02-13 09:43:18 +0800

    Task: OpenGMK.GM8Decompiler Add x86 installer

--- Tasks/OpenGMK.GM8Decompiler.MSVC/Script.ps1
+++ Tasks/OpenGMK.GM8Decompiler.MSVC/Script.ps1
@@ -8,6 +8,10 @@ $this.CurrentState.Version = $Object1.tag_name -creplace '^v'
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x86'
+  InstallerUrl = $Object1.assets.Where({ $_.name.EndsWith('.zip') -and $_.name.Contains('windows') -and $_.name.Contains('i686') -and $_.name.Contains('msvc') }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
+}
+$this.CurrentState.Installer += [ordered]@{
   Architecture = 'x64'
   InstallerUrl = $Object1.assets.Where({ $_.name.EndsWith('.zip') -and $_.name.Contains('windows') -and $_.name.Contains('x86_64') -and $_.name.Contains('msvc') }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
 }
