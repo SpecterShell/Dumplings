8a440e1b3c158c389d024d2eba646c84d2f8693a
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-02-16 16:59:01 +0800

    Task: MyCloudGame.AirGame

--- Tasks/MyCloudGame.AirGame/Script.ps1
+++ Tasks/MyCloudGame.AirGame/Script.ps1
@@ -4,17 +4,17 @@ $Object1 = Invoke-WebRequest -Uri "${Prefix}download.html" | ConvertFrom-Html
 
 # Version
 $this.CurrentState.Version = [regex]::Match(
-  $Object1.SelectSingleNode('//*[@id="CLIENT_VER"]').InnerText,
+  $Object1.SelectSingleNode('//*[@id="WIN_APP_VER"]').InnerText,
   '\((.+)\)'
 ).Groups[1].Value
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = $Prefix + $Object1.SelectSingleNode('//*[@id="WIN_CLIENT_LINK"]').Attributes['href'].Value
+  InstallerUrl = $Object1.SelectSingleNode('//*[@id="WIN_APP_LINK2"]').Attributes['href'].Value
 }
 $this.CurrentState.Installer += [ordered]@{
   InstallerLocale = 'zh-CN'
-  InstallerUrl    = $Object1.SelectSingleNode('//*[@id="WIN_CLIENT_LINK2"]').Attributes['href'].Value
+  InstallerUrl    = $Object1.SelectSingleNode('//*[@id="WIN_APP_LINK"]').Attributes['href'].Value
 }
 
 $ReleaseNotesTitleNode = $Object1.SelectSingleNode("//*[@id='page-top']/table/tbody/tr/td[2]/table/tbody/tr/td/h4[starts-with(./text(), 'AirGame')]").Where({ $_.InnerText.EndsWith([regex]::Match($this.CurrentState.Version, '([\d\.]+)').Groups[1].Value) }, 'First')
