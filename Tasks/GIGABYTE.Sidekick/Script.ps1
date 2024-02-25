24204c5d8fdb4792c480402bfb175e294b5d9648
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-02-25 13:17:33 +1300

    Task: Add GIGABYTE.Sidekick

---
+++ Tasks/GIGABYTE.Sidekick/Script.ps1
@@ -0,0 +1,27 @@
+$Prefix = 'https://download.gigabyte.com/FileList/Swhttp/LiveUpdate4/GCC/Sidekick/'
+
+$Object1 = Invoke-RestMethod -Uri "${Prefix}ver2.ini" | ConvertFrom-Ini
+
+# Version
+$this.CurrentState.Version = $Object1.Info.Ver
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = "${Prefix}$($Object1.Info.ApName)_$($this.CurrentState.Version).exe"
+}
+
+# ReleaseTime
+$this.CurrentState.ReleaseTime = $Object1.Info.Date | Get-Date -Format 'yyyy-MM-dd'
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Print()
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
