82369a60fed94b09952bbc57b5d40ba4c37b44d3
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-03-03 01:08:29 +1300

    Task: Add KDE.KDEConnect

---
+++ Tasks/KDE.KDEConnect/Script.ps1
@@ -0,0 +1,27 @@
+$Object1 = Invoke-WebRequest -Uri 'https://kdeconnect.kde.org/download.html' | ConvertFrom-Html
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $InstallerUrl = $Object1.SelectSingleNode('//a[contains(text(), "Offline installers")]').Attributes['href'].Value
+}
+
+# Version
+$this.CurrentState.Version = [regex]::Match($InstallerUrl, '/(\d+\.\d+\.\d+)/').Groups[1].Value
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $Object2 = Invoke-RestMethod -Uri "${InstallerUrl}.sha256"
+
+    # InstallerSha256
+    $this.CurrentState.Installer[0]['InstallerSha256'] = $Object2.Split()[0].ToUpper()
+
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Print()
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
