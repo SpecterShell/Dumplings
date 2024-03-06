9bbe1d2a36e93abfa4915b421cea9fe488caed96
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-03-06 16:06:21 +1300

    Task: NetEase.CloudMusic Add token

--- Tasks/NetEase.CloudMusic.Beta/Script.ps1
+++ Tasks/NetEase.CloudMusic.Beta/Script.ps1
@@ -2,7 +2,7 @@
 $Object1 = Invoke-RestMethod `
   -Uri 'https://interface.music.163.com/api/pc/upgrade/get' `
   -Method Post `
-  -Headers @{ Cookie = 'osver=Microsoft-Windows-10; appver=3.0.1.201499' } `
+  -Headers @{ Cookie = "osver=Microsoft-Windows-10; appver=$($this.LastState.Version ?? '3.0.6.202423'); MUSIC_A=15e860272ed0803ccf979188982b610ab086a424d18df0f04ee8b1d194962355ef2ae33b30bb5917c514d981210de4857a446f6ceddb779fec58efb075f2174dcb7ce23cca3bd6be03a4f1d7e1fccdebb72149bd3b14523943124f3fcebe94e446b14e3f0c3f8af94212382188fe1965" } `
   -Body 'action=manual&cpuBitWidth=64&e_r=false'
 $Version1 = "$($Object1.data.packageVO.appver).$($Object1.data.packageVO.buildver)"
 $InstallerUrl1 = $Object1.data.packageVO.downloadUrl
@@ -11,11 +11,10 @@ $InstallerUrl1 = $Object1.data.packageVO.downloadUrl
 $Object2 = Invoke-RestMethod `
   -Uri 'https://interface.music.163.com/api/pc/upgrade/get' `
   -Method Post `
-  -Headers @{ Cookie = 'osver=Microsoft-Windows-10; appver=3.0.1.201499' } `
+  -Headers @{ Cookie = "osver=Microsoft-Windows-10; appver=$($this.LastState.Version ?? '3.0.6.202423'); MUSIC_A=15e860272ed0803ccf979188982b610ab086a424d18df0f04ee8b1d194962355ef2ae33b30bb5917c514d981210de4857a446f6ceddb779fec58efb075f2174dcb7ce23cca3bd6be03a4f1d7e1fccdebb72149bd3b14523943124f3fcebe94e446b14e3f0c3f8af94212382188fe1965" } `
   -Body 'action=manual&cpuBitWidth=32&e_r=false'
 $Version2 = "$($Object2.data.packageVO.appver).$($Object2.data.packageVO.buildver)"
 $InstallerUrl2 = $Object2.data.packageVO.downloadUrl
-
 
 $Identical = $true
 if ($Version1 -ne $Version2) {
