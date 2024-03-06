33943be3e3c24ffea824a106bce7a5ff84c5db30
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-03-06 15:03:41 +1300

    Task: NetEase.IdentityV.CN Clean state and force submit on changed

--- Tasks/NetEase.IdentityV.CN/Script.ps1
+++ Tasks/NetEase.IdentityV.CN/Script.ps1
@@ -1,22 +1,27 @@
 $Object1 = (Invoke-WebRequest -Uri 'https://adl.netease.com/d/g/id5/c/gw?type=pc').Content
 
 # Installer
-$this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = $InstallerUrl = [regex]::Match($Object1, 'pc_link\s*=\s*"(.+?)"').Groups[1].Value
+$this.CurrentState.Installer += $Installer = [ordered]@{
+  InstallerUrl = [regex]::Match($Object1, 'pc_link\s*=\s*"(.+?)"').Groups[1].Value
 }
 
 # Version
-$this.CurrentState.Version = [regex]::Match($InstallerUrl, '(\d+)\.exe').Groups[1].Value
+$this.CurrentState.Version = [regex]::Match($Installer.InstallerUrl, '(\d+)\.exe').Groups[1].Value
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
+    $InstallerFile = Get-TempFile -Uri $Installer.InstallerUrl
+
+    # InstallerSha256
+    $Installer['InstallerSha256'] = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
     # RealVersion
-    $this.CurrentState.RealVersion = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl | Read-FileVersionFromExe
+    $this.CurrentState.RealVersion = $InstallerFile | Read-FileVersionFromExe
 
     $this.Write()
   }
   'Changed|Updated' {
     $this.Print()
     $this.Message()
+    $this.Submit()
   }
 }
