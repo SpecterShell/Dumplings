4449faa389d8704f99ab4594640281eea253adcb
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-03-06 16:59:34 +1300

    Task: Add KDE.RKWard

---
+++ Tasks/KDE.RKWard/Script.ps1
@@ -0,0 +1,47 @@
+$Object1 = Invoke-WebRequest -Uri 'https://rkward.kde.org/RKWard_on_Windows.html' | ConvertFrom-Html
+
+# Version
+$this.CurrentState.Version = [regex]::Match($Object1.SelectSingleNode('/html/body/main/ul[1]/li/text()[1]').InnerText, 'RKWard (.+)').Groups[1].Value
+
+# Installer
+$this.CurrentState.Installer += $Installer = [ordered]@{
+  InstallerUrl = $InstallerUrl = $Object1.SelectSingleNode('/html/body/main/ul[1]/li/a').Attributes['href'].Value
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $InstallerFile = Get-TempFile -Uri $Installer.InstallerUrl
+
+    # InstallerSha256
+    $Installer['InstallerSha256'] = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
+    # RealVersion
+    $this.CurrentState.RealVersion = $InstallerFile | Read-ProductVersionFromExe
+
+    try {
+      $Object2 = ((Invoke-RestMethod -Uri 'https://invent.kde.org/education/rkward/-/raw/master/ChangeLog') -split '\s+--- ').Where({ $_.Contains($this.CurrentState.Version) }, 'First')
+
+      if ($Object2) {
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $Object2[0] | Split-LineEndings | Select-Object -Skip 1 | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Print()
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
