b9cb9bea6182654cab1ddbc6bfd4279c5a116b38
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-03-09 13:47:18 +1300

    Task: Add Amazon.WorkspacesClient

---
+++ Tasks/Amazon.WorkspacesClient/Script.ps1
@@ -0,0 +1,54 @@
+$Object1 = Invoke-RestMethod -Uri 'https://d2td7dqidlhjx7.cloudfront.net/prod/global/windows/WorkSpacesAppCastx64.xml'
+
+# Version
+$this.CurrentState.Version = $Object1.enclosure.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.enclosure.url
+}
+
+# ReleaseTime
+$this.CurrentState.ReleaseTime = [datetime]::ParseExact(
+  $Object1.pubDate,
+  'ddd MMM d HH:mm:ss "PDT" yyyy',
+  (Get-Culture -Name 'en-US')
+) | ConvertTo-UtcDateTime -Id 'Pacific Standard Time'
+
+# ReleaseNotesUrl
+$this.CurrentState.Locale += [ordered]@{
+  Key   = 'ReleaseNotesUrl'
+  Value = $ReleaseNotesUrl = $Object1.releaseNotesLink
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      $Object2 = Invoke-WebRequest -Uri $ReleaseNotesUrl | ConvertFrom-Html
+
+      $ReleaseNotesTitleNode = $Object2.SelectSingleNode("/html/body/ul/li/h2[contains(text(), '$($this.CurrentState.Version.Split('.')[0..2] -join '.')')]")
+      if ($ReleaseNotesTitleNode) {
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesTitleNode.SelectNodes('./following-sibling::node()') | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Print()
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
