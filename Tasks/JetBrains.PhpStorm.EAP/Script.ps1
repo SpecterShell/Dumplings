e29711bc85043379e2f7bd0d5812eb364907493e
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-03-10 18:46:01 +1300

    Task: Add JetBrains.PhpStorm and JetBrains.PhpStorm.EAP

---
+++ Tasks/JetBrains.PhpStorm.EAP/Script.ps1
@@ -0,0 +1,78 @@
+$Object1 = $Global:DumplingsStorage.JetBrainsApps.PS.eap
+
+# Version
+$this.CurrentState.Version = $Object1.build
+
+# Installer
+$this.CurrentState.Installer += $InstallerX64 = [ordered]@{
+  Architecture           = 'x64'
+  InstallerUrl           = $Object1.downloads.windows.link
+  ProductCode            = "PhpStorm $($Object1.build)"
+  AppsAndFeaturesEntries = @(
+    [ordered]@{
+      DisplayName = "PhpStorm $($Object1.build)"
+      ProductCode = "PhpStorm $($Object1.build)"
+    }
+  )
+}
+$this.CurrentState.Installer += $InstallerARM64 = [ordered]@{
+  Architecture           = 'arm64'
+  InstallerUrl           = $Object1.downloads.windowsARM64.link
+  ProductCode            = "PhpStorm $($Object1.build)"
+  AppsAndFeaturesEntries = @(
+    [ordered]@{
+      DisplayName = "PhpStorm $($Object1.build)"
+      ProductCode = "PhpStorm $($Object1.build)"
+    }
+  )
+}
+
+# ReleaseTime
+$this.CurrentState.ReleaseTime = $Object1.date | Get-Date -Format 'yyyy-MM-dd'
+
+if ($Object1.whatsnew) {
+  # ReleaseNotes (en-US)
+  $this.CurrentState.Locale += [ordered]@{
+    Locale = 'en-US'
+    Key    = 'ReleaseNotes'
+    Value  = $Object1.whatsnew | ConvertFrom-Html | Get-TextContent | Format-Text
+  }
+} else {
+  $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+}
+
+if ($Object1.notesLink) {
+  # ReleaseNotesUrl
+  $this.CurrentState.Locale += [ordered]@{
+    Key   = 'ReleaseNotesUrl'
+    Value = $Object1.notesLink
+  }
+} else {
+  # ReleaseNotesUrl
+  $this.CurrentState.Locale += [ordered]@{
+    Key   = 'ReleaseNotesUrl'
+    Value = 'https://www.jetbrains.com/phpstorm/whatsnew/'
+  }
+  $this.CurrentState.Locale += [ordered]@{
+    Locale = 'zh-CN'
+    Key    = 'ReleaseNotesUrl'
+    Value  = 'https://www.jetbrains.com/zh-cn/phpstorm/whatsnew/'
+  }
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    # InstallerSha256
+    $InstallerX64['InstallerSha256'] = (Invoke-RestMethod -Uri $Object1.downloads.windows.checksumLink).Split()[0].ToUpper()
+    $InstallerARM64['InstallerSha256'] = (Invoke-RestMethod -Uri $Object1.downloads.windowsARM64.checksumLink).Split()[0].ToUpper()
+
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Print()
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
---
+++ Tasks/JetBrains.PhpStorm/Script.ps1
@@ -0,0 +1,80 @@
+$Object1 = $Global:DumplingsStorage.JetBrainsApps.PS.release
+
+# Version
+$this.CurrentState.Version = $Object1.version
+
+# Installer
+$this.CurrentState.Installer += $InstallerX64 = [ordered]@{
+  Architecture           = 'x64'
+  InstallerUrl           = $Object1.downloads.windows.link
+  ProductCode            = "PhpStorm $($this.CurrentState.Version)"
+  AppsAndFeaturesEntries = @(
+    [ordered]@{
+      DisplayName    = "PhpStorm $($this.CurrentState.Version)"
+      DisplayVersion = $Object1.build
+      ProductCode    = "PhpStorm $($this.CurrentState.Version)"
+    }
+  )
+}
+$this.CurrentState.Installer += $InstallerARM64 = [ordered]@{
+  Architecture           = 'arm64'
+  InstallerUrl           = $Object1.downloads.windowsARM64.link
+  ProductCode            = "PhpStorm $($this.CurrentState.Version)"
+  AppsAndFeaturesEntries = @(
+    [ordered]@{
+      DisplayName    = "PhpStorm $($this.CurrentState.Version)"
+      DisplayVersion = $Object1.build
+      ProductCode    = "PhpStorm $($this.CurrentState.Version)"
+    }
+  )
+}
+
+# ReleaseTime
+$this.CurrentState.ReleaseTime = $Object1.date | Get-Date -Format 'yyyy-MM-dd'
+
+if ($Object1.whatsnew) {
+  # ReleaseNotes (en-US)
+  $this.CurrentState.Locale += [ordered]@{
+    Locale = 'en-US'
+    Key    = 'ReleaseNotes'
+    Value  = $Object1.whatsnew | ConvertFrom-Html | Get-TextContent | Format-Text
+  }
+} else {
+  $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+}
+
+if ($Object1.notesLink) {
+  # ReleaseNotesUrl
+  $this.CurrentState.Locale += [ordered]@{
+    Key   = 'ReleaseNotesUrl'
+    Value = $Object1.notesLink
+  }
+} else {
+  # ReleaseNotesUrl
+  $this.CurrentState.Locale += [ordered]@{
+    Key   = 'ReleaseNotesUrl'
+    Value = 'https://www.jetbrains.com/phpstorm/whatsnew/'
+  }
+  $this.CurrentState.Locale += [ordered]@{
+    Locale = 'zh-CN'
+    Key    = 'ReleaseNotesUrl'
+    Value  = 'https://www.jetbrains.com/zh-cn/phpstorm/whatsnew/'
+  }
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    # InstallerSha256
+    $InstallerX64['InstallerSha256'] = (Invoke-RestMethod -Uri $Object1.downloads.windows.checksumLink).Split()[0].ToUpper()
+    $InstallerARM64['InstallerSha256'] = (Invoke-RestMethod -Uri $Object1.downloads.windowsARM64.checksumLink).Split()[0].ToUpper()
+
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Print()
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
