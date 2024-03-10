43861cc59bf30825a706deebe896fc7a9bd6981c
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-03-10 21:42:27 +1300

    Task: Add JetBrains.IntelliJIDEA.Educational

---
+++ Tasks/JetBrains.IntelliJIDEA.Educational/Script.ps1
@@ -0,0 +1,54 @@
+$Object1 = $Global:DumplingsStorage.JetBrainsApps.IIE.release
+
+# Version
+$this.CurrentState.Version = $Object1.version
+
+# Installer
+$this.CurrentState.Installer += $InstallerX64 = [ordered]@{
+  Architecture           = 'x64'
+  InstallerUrl           = $Object1.downloads.windows.link
+  ProductCode            = "IntelliJ IDEA Educational Edition $($this.CurrentState.Version)"
+  AppsAndFeaturesEntries = @(
+    [ordered]@{
+      DisplayName    = "IntelliJ IDEA Educational Edition $($this.CurrentState.Version)"
+      DisplayVersion = $Object1.build
+      ProductCode    = "IntelliJ IDEA Educational Edition $($this.CurrentState.Version)"
+    }
+  )
+}
+
+# ReleaseTime
+$this.CurrentState.ReleaseTime = $Object1.date | Get-Date -Format 'yyyy-MM-dd'
+
+if ($Object1.whatsnew) {
+  # ReleaseNotes (en-US)
+  $this.CurrentState.Locale += [ordered]@{
+    Locale = 'en-US'
+    Key    = 'ReleaseNotes'
+    Value  = $Object1.whatsnew | ConvertFrom-Html | Get-TextContent | Format-Text
+  }
+} else {
+  $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+}
+
+# ReleaseNotesUrl
+$this.CurrentState.Locale += [ordered]@{
+  Key   = 'ReleaseNotesUrl'
+  Value = $Object1.notesLink
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    # InstallerSha256
+    $InstallerX64['InstallerSha256'] = (Invoke-RestMethod -Uri $Object1.downloads.windows.checksumLink).Split()[0].ToUpper()
+
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Print()
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
