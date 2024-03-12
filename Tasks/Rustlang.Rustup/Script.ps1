06dbe241026bba1795287d48c021876418f74cb8
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-03-13 12:16:54 +1300

    Task: Add Rustlang.Rustup

---
+++ Tasks/Rustlang.Rustup/Script.ps1
@@ -0,0 +1,70 @@
+$Object1 = Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/rust-lang/rustup/master/Cargo.toml'
+
+# Version
+$this.CurrentState.Version = [regex]::Match($Object1, 'version\s*=\s*"([\d.]+)"').Groups[1].Value
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x86'
+  InstallerUrl = "https://static.rust-lang.org/rustup/archive/$($this.CurrentState.Version)/i686-pc-windows-msvc/rustup-init.exe"
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = "https://static.rust-lang.org/rustup/archive/$($this.CurrentState.Version)/x86_64-pc-windows-msvc/rustup-init.exe"
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'arm64'
+  InstallerUrl = "https://static.rust-lang.org/rustup/archive/$($this.CurrentState.Version)/aarch64-pc-windows-msvc/rustup-init.exe"
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      $Object2 = (Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/rust-lang/rustup/master/CHANGELOG.md' | ConvertFrom-Markdown).Html | ConvertFrom-Html
+
+      $ReleaseNotesTitleNode = $Object2.SelectSingleNode("/h2[contains(.//text(), '$($this.CurrentState.Version)')]")
+      if ($ReleaseNotesTitleNode) {
+        # ReleaseTime
+        $this.CurrentState.ReleaseTime = [regex]::Match($ReleaseNotesTitleNode.InnerText, '(\d{4}-\d{1,2}-\d{1,2})').Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
+
+        $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node.Name -ne 'h2'; $Node = $Node.NextSibling) { $Node }
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
+        }
+
+        # ReleaseNotesUrl
+        $this.CurrentState.Locale += [ordered]@{
+          Key   = 'ReleaseNotesUrl'
+          Value = 'https://github.com/rust-lang/rustup/blob/master/CHANGELOG.md#' + ($ReleaseNotesTitleNode.InnerText -creplace '[^a-zA-Z0-9\-\s]+', '' -creplace '\s+', '-').ToLower()
+        }
+      } else {
+        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+        # ReleaseNotesUrl
+        $this.CurrentState.Locale += [ordered]@{
+          Key   = 'ReleaseNotesUrl'
+          Value = 'https://github.com/rust-lang/rustup/blob/master/CHANGELOG.md'
+        }
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+      # ReleaseNotesUrl
+      $this.CurrentState.Locale += [ordered]@{
+        Key   = 'ReleaseNotesUrl'
+        Value = 'https://github.com/rust-lang/rustup/blob/master/CHANGELOG.md'
+      }
+    }
+
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Print()
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
