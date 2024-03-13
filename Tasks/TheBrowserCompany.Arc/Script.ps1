fade7d8f3c3d882403b2c009f93b6135f2722957
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-03-13 13:48:24 +1300

    Task: Add TheBrowserCompany.Arc

---
+++ Tasks/TheBrowserCompany.Arc/Script.ps1
@@ -0,0 +1,22 @@
+$Object1 = Invoke-RestMethod -Uri 'https://releases.arc.net/windows/prod/Arc.appinstaller'
+
+# Version
+$this.CurrentState.Version = $Object1.AppInstaller.Version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.AppInstaller.MainBundle.Uri
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Print()
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
