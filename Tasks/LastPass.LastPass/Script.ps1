afa8baa0cf2b1252da160e278f45aa1692d1382e
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-03-13 15:33:11 +1300

    Task: Add LastPass.LastPass

---
+++ Tasks/LastPass.LastPass/Script.ps1
@@ -0,0 +1,66 @@
+$Object1 = Invoke-RestMethod -Uri 'https://lastpass.com/lmiapi/check-win-installer-version' -Method Post -Body (
+  @{
+    version  = $this.LastState.Contains('Version') ? $this.LastState.Version : '4.0.0.0'
+    isManual = $true
+  } | ConvertTo-Json -Compress
+)
+
+if ($Object1.isUpdateAvailable -eq $false) {
+  $this.Log("The last version $($this.LastState.Version) is the latest, skip checking", 'Info')
+  return
+}
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $InstallerUrl = $Object1.url
+}
+
+# Version
+$this.CurrentState.Version = Get-TempFile -Uri $InstallerUrl | Read-ProductVersionFromMsi
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      $Object2 = Invoke-WebRequest -Uri 'https://lastpass.com/upgrade.php' | ConvertFrom-Html
+
+      $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//div[@class='relnotes']/table/tr/td[1]/h3[contains(text(), '$($this.CurrentState.Version.Split('.')[0..2] -join '.')')]")
+      if ($ReleaseNotesTitleNode) {
+        # ReleaseTime
+        $this.CurrentState.ReleaseTime = [datetime]::ParseExact(
+          [regex]::Match($ReleaseNotesTitleNode.InnerText, '([a-zA-Z]+\s+\d{1,2}[a-zA-Z]+\s+\d{4})').Groups[1].Value,
+          # "[string[]]" is needed here to convert "array" object to string array
+          [string[]]@(
+            "MMMM d'st' yyyy",
+            "MMMM d'nd' yyyy",
+            "MMMM d'rd' yyyy",
+            "MMMM d'th' yyyy"
+          ),
+          (Get-Culture -Name 'en-US'),
+          [System.Globalization.DateTimeStyles]::None
+        ).ToString('yyyy-MM-dd')
+
+        $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node.Name -ne 'h3'; $Node = $Node.NextSibling) { $Node }
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Print()
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
