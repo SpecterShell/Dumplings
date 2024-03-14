fea62c9ccfdbd1070f6b44d687cba6000d3df598
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-03-14 19:06:20 +1300

    Task: Add and fix Yealink.YealinkUSBConnect(.CN)

---
+++ Tasks/#Yealink.YealinkUSBConnect.CN/Script.ps1
@@ -0,0 +1,68 @@
+$OldReleaseNotesPath = Join-Path $PSScriptRoot 'Releases.yaml'
+if (Test-Path -Path $OldReleaseNotesPath) {
+  $Global:DumplingsStorage['YealinkUSBConnectCN'] = $OldReleaseNotes = Get-Content -Path $OldReleaseNotesPath -Raw | ConvertFrom-Yaml -Ordered
+} else {
+  $Global:DumplingsStorage['YealinkUSBConnectCN'] = $OldReleaseNotes = [ordered]@{}
+}
+
+$Params = @{
+  Uri         = 'https://update.yealink.com.cn/yiot-manager/api/v1/external/software/checkVersion'
+  Method      = 'Post'
+  ContentType = 'application/json;charset=UTF-8'
+  Body        = @{
+    appId          = '72bbc75337eb4930ad93a5e13e5798d1'
+    clientId       = 'H-YUC-00155d31bc02'
+    clientType     = 'software'
+    clientModel    = 'Yealink-USB-Connect'
+    clientPlatform = 'windows'
+  } | ConvertTo-Json -Compress
+}
+# en
+$Object1 = Invoke-RestMethod @Params
+# zh_CN
+$Object2 = Invoke-RestMethod @Params -Headers @{ language = 'zh_CN' }
+
+if ($Object1.data.version -ne $Object2.data.version) { throw 'Distinct versions detected' }
+
+# Version
+$this.CurrentState.Version = $Object1.data.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.data.file.url
+}
+
+# ReleaseTime
+$this.CurrentState.ReleaseTime = $ReleaseTime = $Object1.data.releaseDate | Get-Date | ConvertTo-UtcDateTime -Id UTC
+
+# ReleaseNotes (en-US)
+$this.CurrentState.Locale += [ordered]@{
+  Locale = 'en-US'
+  Key    = 'ReleaseNotes'
+  Value  = $ReleaseNotes = $Object1.data.releaseNote | Format-Text
+}
+# ReleaseNotes (zh-CN)
+$this.CurrentState.Locale += [ordered]@{
+  Locale = 'zh-CN'
+  Key    = 'ReleaseNotes'
+  Value  = $ReleaseNotesCN = $Object2.data.releaseNote | Format-Text
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $OldReleaseNotes[$this.CurrentState.Version] = [ordered]@{
+      ReleaseTime    = $ReleaseTime
+      ReleaseNotes   = $ReleaseNotes
+      ReleaseNotesCN = $ReleaseNotesCN
+    }
+    if ($Global:DumplingsPreference.Contains('EnableWrite') -and $Global:DumplingsPreference.EnableWrite) {
+      $OldReleaseNotes | ConvertTo-Yaml -OutFile $OldReleaseNotesPath -Force
+    }
+
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Print()
+    $this.Message()
+  }
+}
--- Tasks/Yealink.YealinkUSBConnect.CN/Script.ps1
+++ Tasks/Yealink.YealinkUSBConnect.CN/Script.ps1
@@ -1,27 +1,27 @@
-$Object1 = Invoke-WebRequest -Uri 'https://www.yealink.com/en/product-detail/usb-connect-management' | ConvertFrom-Html
+$Object1 = Invoke-WebRequest -Uri 'https://www.yealink.com.cn/product-detail/usb-connect-management' | ConvertFrom-Html
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = $InstallerUrl = $Object1.SelectSingleNode('/html/body/main/div/article[1]/div/div[1]/div/small/p/span/a[1]').Attributes['href'].Value
+  InstallerUrl = $InstallerUrl = $Object1.SelectSingleNode('/html/body/main/div[1]/article[1]/div/div[1]/div/small/p/span/a[1]').Attributes['href'].Value
 }
 
 # Version
 $this.CurrentState.Version = $Version = [regex]::Match($InstallerUrl, '(\d+\.\d+\.\d+\.\d+)').Groups[1].Value
 
-if ($Global:DumplingsStorage.Contains('YealinkUSBConnect') -and $Global:DumplingsStorage['YealinkUSBConnect'].Contains($Version)) {
+if ($Global:DumplingsStorage.Contains('YealinkUSBConnectCN') -and $Global:DumplingsStorage['YealinkUSBConnectCN'].Contains($Version)) {
   # ReleaseTime
-  $this.CurrentState.ReleaseTime = $Global:DumplingsStorage['YealinkUSBConnect'].$Version.ReleaseTime
+  $this.CurrentState.ReleaseTime = $Global:DumplingsStorage['YealinkUSBConnectCN'].$Version.ReleaseTime
   # ReleaseNotes (en-US)
   $this.CurrentState.Locale += [ordered]@{
     Locale = 'en-US'
     Key    = 'ReleaseNotes'
-    Value  = $Global:DumplingsStorage['YealinkUSBConnect'].$Version.ReleaseNotes
+    Value  = $Global:DumplingsStorage['YealinkUSBConnectCN'].$Version.ReleaseNotes
   }
   # ReleaseNotes (zh-CN)
   $this.CurrentState.Locale += [ordered]@{
     Locale = 'zh-CN'
     Key    = 'ReleaseNotes'
-    Value  = $Global:DumplingsStorage['YealinkUSBConnect'].$Version.ReleaseNotesCN
+    Value  = $Global:DumplingsStorage['YealinkUSBConnectCN'].$Version.ReleaseNotesCN
   }
 } else {
   $this.Log("No ReleaseTime, ReleaseTime (en-US) and ReleaseNotes (zh-CN) for version $($this.CurrentState.Version)", 'Warning')
