1a0b7d7c6ed48532f4df79a61b3a276bff7cf673
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-03-23 19:56:58 +1300

    Task: Add FortranLang.fpm

---
+++ Tasks/FortranLang.fpm/Script.ps1
@@ -0,0 +1,59 @@
+$RepoOwner = 'fortran-lang'
+$RepoName = 'fpm'
+
+$Object1 = Invoke-GitHubApi -Uri "https://api.github.com/repos/${RepoOwner}/${RepoName}/releases/latest"
+
+# Version
+$this.CurrentState.Version = $Object1.tag_name -creplace '^v'
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture  = 'x64'
+  InstallerType = 'portable'
+  InstallerUrl  = $Object1.assets.Where({ $_.name.EndsWith('.exe') -and $_.name.Contains('x86_64') -and $_.name.Contains('windows') }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture  = 'x64'
+  InstallerType = 'nullsoft'
+  InstallerUrl  = $Object1.assets.Where({ $_.name.EndsWith('.exe') -and $_.name.Contains('installer') }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
+}
+
+# ReleaseTime
+$this.CurrentState.ReleaseTime = $Object1.published_at.ToUniversalTime()
+
+if (-not [string]::IsNullOrWhiteSpace($Object1.body)) {
+  $ReleaseNotesObject = ($Object1.body | ConvertFrom-Markdown).Html | ConvertFrom-Html
+  $ReleaseNotesTitleNode = $ReleaseNotesObject.SelectSingleNode("./h2[contains(text(), `"What's Changed`")]")
+  if ($ReleaseNotesTitleNode) {
+    $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node -and -not $Node.InnerText.Contains('Full Changelog:'); $Node = $Node.NextSibling) { $Node }
+    # ReleaseNotes (en-US)
+    $this.CurrentState.Locale += [ordered]@{
+      Locale = 'en-US'
+      Key    = 'ReleaseNotes'
+      Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
+    }
+  } else {
+    $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+  }
+} else {
+  $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+}
+
+# ReleaseNotesUrl
+$this.CurrentState.Locale += [ordered]@{
+  Key   = 'ReleaseNotesUrl'
+  Value = $Object1.html_url
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Print()
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
