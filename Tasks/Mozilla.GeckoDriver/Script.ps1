988454460ca570d8de9b01386a87b7d4aa6c65e2
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-03-23 19:38:11 +1300

    Task: Add Mozilla.GeckoDriver

---
+++ Tasks/Mozilla.GeckoDriver/Script.ps1
@@ -0,0 +1,60 @@
+$RepoOwner = 'mozilla'
+$RepoName = 'geckodriver'
+
+$Object1 = Invoke-GitHubApi -Uri "https://api.github.com/repos/${RepoOwner}/${RepoName}/releases/latest"
+
+# Version
+$this.CurrentState.Version = $Object1.tag_name -creplace '^v'
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x86'
+  InstallerUrl = $Object1.assets.Where({ $_.name.EndsWith('.zip') -and $_.name.Contains('win32') }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = $Object1.assets.Where({ $_.name.EndsWith('.zip') -and $_.name.Contains('win64') }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'arm64'
+  InstallerUrl = $Object1.assets.Where({ $_.name.EndsWith('.zip') -and $_.name.Contains('win-aarch64') }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
+}
+
+# ReleaseTime
+$this.CurrentState.ReleaseTime = $Object1.published_at.ToUniversalTime()
+
+if (-not [string]::IsNullOrWhiteSpace($Object1.body)) {
+  $ReleaseNotesObject = ($Object1.body | ConvertFrom-Markdown).Html | ConvertFrom-Html
+  $ReleaseNotesTitleNode = $ReleaseNotesObject.SelectSingleNode("./h2[contains(.//text(), '$($this.CurrentState.Version)')]")
+  if ($ReleaseNotesTitleNode) {
+    # ReleaseNotes (en-US)
+    $this.CurrentState.Locale += [ordered]@{
+      Locale = 'en-US'
+      Key    = 'ReleaseNotes'
+      Value  = $ReleaseNotesTitleNode.SelectNodes('./following-sibling::node()') | Get-TextContent | Format-Text
+    }
+  } else {
+    $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+  }
+} else {
+  $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+}
+
+# ReleaseNotesUrl
+$this.CurrentState.Locale += [ordered]@{
+  Key   = 'ReleaseNotesUrl'
+  Value = $Object1.html_url
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Print()
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
