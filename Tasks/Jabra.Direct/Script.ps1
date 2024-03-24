8e26d9646dd54c535fefe45262a2d9ba00a3a369
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-03-24 13:57:44 +1300

    Task: Add Jabra.Direct

---
+++ Tasks/Jabra.Direct/Script.ps1
@@ -0,0 +1,61 @@
+$Object1 = Invoke-RestMethod -Uri 'https://jabraxpressonlineprdstor.blob.core.windows.net/jdo/jdo.json'
+
+# Version
+$this.CurrentState.Version = $Object1.WindowsVersion
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.WindowsDownload
+}
+
+# ReleaseNotesUrl
+$this.CurrentState.Locale += [ordered]@{
+  Key   = 'ReleaseNotesUrl'
+  Value = $ReleaseNotesUrl = $Object1.WindowsReleaseNotes
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+
+    # InstallerSha256
+    $this.CurrentState.Installer[0]['InstallerSha256'] = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
+    # RealVersion
+    $this.CurrentState.RealVersion = $InstallerFile | Read-ProductVersionFromExe
+    # AppsAndFeaturesEntries
+    $this.CurrentState.Installer[0]['AppsAndFeaturesEntries'] = @(
+      [ordered]@{
+        ProductCode = $this.CurrentState.Installer[0]['ProductCode'] = $InstallerFile | Read-ProductCodeFromBurn
+        UpgradeCode = $InstallerFile | Read-UpgradeCodeFromBurn
+      }
+    )
+
+    try {
+      $Object3 = Invoke-WebRequest -Uri $ReleaseNotesUrl | ConvertFrom-Html
+
+      $ReleaseNotesNode = $Object3.SelectSingleNode("//div[@class='support-release-notes-spot']//div[contains(@class, 'support-release-notes-spot__item') and contains(.//div[@class='support-release-notes-spot__item-heading'], '$($this.CurrentState.Version)')]")
+      if ($ReleaseNotesNode) {
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesNode.SelectSingleNode('.//div[@class="support-release-notes-spot__item-content"]') | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Print()
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
