2026ec8ce1869fbd4986e991d555bca2b5986fd5
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-03-25 18:04:02 +1300

    Task: Add NordSecurity.NordVPN

---
+++ Tasks/NordSecurity.NordVPN/Script.ps1
@@ -0,0 +1,29 @@
+$Object1 = (Invoke-WebRequest -Uri 'https://downloads.nordcdn.com/apps/windows/meta/NordVPN/latest/versionv2.xml' | Read-ResponseContent | ConvertFrom-Xml).channels.channel.Where({ $_.name -eq 'default' }, 'First')[0]
+
+# Version
+$this.CurrentState.Version = $Object1.releases.release.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.releases.release.update.url
+}
+
+# ReleaseNotes (en-US)
+$this.CurrentState.Locale += [ordered]@{
+  Locale = 'en-US'
+  Key    = 'ReleaseNotes'
+  Value  = $Object1.releases.release.description.'#cdata-section' | Format-Text
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Print()
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
