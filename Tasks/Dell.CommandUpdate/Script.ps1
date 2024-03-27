55d33cd2bcbb8b29b4dc1c163d53ff07e2f59c53
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-03-27 21:23:48 +1300

    Task: Add Dell.CommandUpdate and Dell.CommandUpdate.Universal

---
+++ Tasks/Dell.CommandUpdate/Script.ps1
@@ -0,0 +1,71 @@
+$Path = Get-TempFile -Uri 'https://dellupdater.dell.com/non_du/ClientService/Catalog/Platform/PrecedenceCatalog.cab'
+expand.exe -R $Path
+$Object1 = (Join-Path $Path '..' 'PrecedenceCatalog.xml' | Get-Item | Get-Content -Raw | ConvertFrom-Xml).Precedence.Demoted.SoftwareComponent.Where({ $_.identifier -eq 'aaaa47ba-9fe1-4411-b98c-389e4ddbe049' }, 'First')[0]
+
+# Version
+$this.CurrentState.Version = $Object1.vendorVersion
+
+# Installer
+$this.CurrentState.Installer += $Installer = [ordered]@{
+  InstallerUrl = 'https://dl.dell.com/' + $Object1.path
+}
+
+# ReleaseTime
+$this.CurrentState.ReleaseTime = $Object1.releaseDate | Get-Date -Format 'yyyy-MM-dd'
+
+# PackageName
+$this.CurrentState.Locale += [ordered]@{
+  Key   = 'PackageUrl'
+  Value = "https://www.dell.com/support/home/drivers/DriversDetails?driverId=$($Object1.releaseID)"
+}
+$this.CurrentState.Locale += [ordered]@{
+  Locale = 'zh-CN'
+  Key    = 'PackageUrl'
+  Value  = "https://www.dell.com/support/home/zh-cn/drivers/DriversDetails?driverId=$($Object1.releaseID)"
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $InstallerFile = Get-TempFile -Uri $Installer.InstallerUrl -UserAgent 'Microsoft-Delivery-Optimization/10.0'
+    $InstallerFileExtracted = New-TempFolder
+    7z e -aoa -ba -bd -o"${InstallerFileExtracted}" $InstallerFile
+
+    $Object2 = Join-Path $InstallerFileExtracted 'Mup.xml' | Get-Item | Get-Content -Raw | ConvertFrom-Xml
+    $NestedInstallerFile = Join-Path $InstallerFileExtracted $Object2.MUPDefinition.executable.executablename
+    $NestedInstallerFileExtracted = $NestedInstallerFile | Expand-InstallShield
+
+    $MsiInstallerFile = Join-Path $NestedInstallerFileExtracted 'DellCommandUpdate.msi'
+
+    $Installer['InstallerSha256'] = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
+    $Installer['AppsAndFeaturesEntries'] = @(
+      [ordered]@{
+        ProductCode   = $Installer['ProductCode'] = $MsiInstallerFile | Read-ProductCodeFromMsi
+        UpgradeCode   = $MsiInstallerFile | Read-UpgradeCodeFromMsi
+        InstallerType = 'msi'
+      }
+    )
+
+    try {
+      $Object3 = Join-Path $InstallerFileExtracted 'package.xml' | Get-Item | Get-Content -Raw -Encoding unicode | ConvertFrom-Xml
+
+      # ReleaseNotes (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotes'
+        Value  = $Object3.SoftwareComponent.RevisionHistory.Display.'#cdata-section' | Format-Text
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Print()
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
