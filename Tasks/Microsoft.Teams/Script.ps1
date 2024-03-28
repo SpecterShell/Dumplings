d4543e84f1f80e443b955bfaf4132352b1ec7c3b
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-03-28 13:28:43 +1300

    Task: Microsoft.Teams Select enterprise version instead of consumer version

--- Tasks/Microsoft.Teams/Script.ps1
+++ Tasks/Microsoft.Teams/Script.ps1
@@ -1,26 +1,26 @@
 $Object1 = Invoke-RestMethod -Uri 'https://config.teams.microsoft.com/config/v1/MicrosoftTeams/49?agents=TeamsBuilds&environment=prod'
 
 $Identical = $true
-if (@(@('x86', 'x64', 'arm64') | Sort-Object -Property { $Object1.TeamsBuilds.BuildSettings.WebView2.$_.latestVersion } -Unique).Count -gt 1) {
+if (@(@('x86', 'x64', 'arm64') | Sort-Object -Property { $Object1.TeamsBuilds.BuildSettings.WebView2Canary.$_.latestVersion } -Unique).Count -gt 1) {
   $this.Log('Distinct versions detected', 'Warning')
   $Identical = $false
 }
 
 # Version
-$this.CurrentState.Version = $Object1.TeamsBuilds.BuildSettings.WebView2.x64.latestVersion
+$this.CurrentState.Version = $Object1.TeamsBuilds.BuildSettings.WebView2Canary.x64.latestVersion
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
   Architecture = 'x86'
-  InstallerUrl = $Object1.TeamsBuilds.BuildSettings.WebView2.x86.buildLink
+  InstallerUrl = $Object1.TeamsBuilds.BuildSettings.WebView2Canary.x86.buildLink
 }
 $this.CurrentState.Installer += [ordered]@{
   Architecture = 'x64'
-  InstallerUrl = $Object1.TeamsBuilds.BuildSettings.WebView2.x64.buildLink
+  InstallerUrl = $Object1.TeamsBuilds.BuildSettings.WebView2Canary.x64.buildLink
 }
 $this.CurrentState.Installer += [ordered]@{
   Architecture = 'arm64'
-  InstallerUrl = $Object1.TeamsBuilds.BuildSettings.WebView2.arm64.buildLink
+  InstallerUrl = $Object1.TeamsBuilds.BuildSettings.WebView2Canary.arm64.buildLink
 }
 
 switch -Regex ($this.Check()) {
