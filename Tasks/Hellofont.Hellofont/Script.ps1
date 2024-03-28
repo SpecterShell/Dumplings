110dc4adf805e05b6f42c8d7515b9f0b190d84d0
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-03-28 17:19:05 +1300

    Task: Hellofont.Hellofont Get InstallerUrl from new API

--- Tasks/Hellofont.Hellofont/Script.ps1
+++ Tasks/Hellofont.Hellofont/Script.ps1
@@ -1,11 +1,16 @@
 $Object1 = Invoke-RestMethod -Uri 'https://back2.hellofont.cn/ziyou/ClientManagement/api/ClientVersion/LatestClientVersionItem' -Method Post -Body @{ PlatformId = 0 }
+$Object2 = Invoke-RestMethod -Uri 'https://www.hellofont.cn/api/system/client-channel?platform=windows&channel=official'
 
 # Version
 $this.CurrentState.Version = $Object1.Version
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = $Object1.Url | ConvertTo-UnescapedUri
+  InstallerUrl = $InstallerUrl = $Object2.link | ConvertTo-UnescapedUri
+}
+
+if (-not $InstallerUrl.Contains($this.CurrentState.Version)) {
+  throw "Task $($this.Name): The InstallerUrl`n${InstallerUrl}`ndoesn't contain version $($this.CurrentState.Version)"
 }
 
 # ReleaseTime
