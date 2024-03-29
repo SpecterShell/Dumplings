1ea40f89214aa11527d526e647682972b07f6a80
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-03-29 14:00:33 +1300

    Task: Add Beeper.Beeper

---
+++ Tasks/Beeper.Beeper/Script.ps1
@@ -0,0 +1,40 @@
+$Prefix = 'https://download.todesktop.com/2003241lzgn20jd/'
+
+$this.CurrentState = Invoke-RestMethod -Uri "${Prefix}latest.yml?noCache=$(Get-Random)" | ConvertFrom-Yaml | ConvertFrom-ElectronUpdater -Prefix $Prefix -Locale 'en-US'
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # TODO: Parse Notion
+      $EdgeDriver = Get-EdgeDriver
+      $EdgeDriver.Navigate().GoToUrl('https://beeper.notion.site/Beeper-Product-Changelog-cdbc7b68526d45f7b8ced8d4ba170c8d')
+      Start-Sleep -Seconds 5
+
+      $ReleaseNotesObject = $EdgeDriver.FindElement([OpenQA.Selenium.By]::XPath('//div[@class="notion-page-content"]')).GetAttribute('innerHTML') | ConvertFrom-Html
+      $ReleaseNotesTitleNode = $ReleaseNotesObject.SelectSingleNode("./div[contains(@class, 'notion-header-block') and contains(., 'v$($this.CurrentState.Version)')]")
+      if ($ReleaseNotesTitleNode) {
+        $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node -and -not $Node.Attributes['class'].Value.Contains('notion-header-block'); $Node = $Node.NextSibling) { $Node }
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Print()
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
