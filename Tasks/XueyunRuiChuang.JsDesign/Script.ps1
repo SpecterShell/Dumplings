9649c9aaefc7f832b65d4f05f37f222e308612e8
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-03-31 11:28:12 +1300

    Task: Add XueyunRuiChuang.JsDesign

---
+++ Tasks/XueyunRuiChuang.JsDesign/Script.ps1
@@ -0,0 +1,16 @@
+$Prefix = 'https://img.js.design/assets/download/'
+
+$this.CurrentState = Invoke-RestMethod -Uri "${Prefix}latest.yml?noCache=$(Get-Random)" | ConvertFrom-Yaml | ConvertFrom-ElectronUpdater -Prefix $Prefix -Locale 'zh-CN'
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
