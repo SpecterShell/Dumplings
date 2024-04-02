4c3cd8667dde54349597780c77fceefe490dbf59
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-04-02 20:47:29 +1300

    Task: Google.Chrome Add arm64 installer

--- Tasks/Google.Chrome.Beta/Script.ps1
+++ Tasks/Google.Chrome.Beta/Script.ps1
@@ -2,30 +2,41 @@
 $Object1 = Invoke-RestMethod -Uri 'https://update.googleapis.com/service/update2' -Method Post -Body @'
 <?xml version="1.0" encoding="UTF-8"?>
 <request protocol="3.0">
-  <os platform="win" version="10" arch="x64" />
+  <os platform="win" version="10.0.22000" arch="x64" />
   <app appid="{8237E44A-0054-442C-B6B6-EA0509993955}" version="" ap="x64">
     <updatecheck />
   </app>
 </request>
 '@
-$Version1 = $Object1.response.app.updatecheck.manifest.version
 
 # x86
 $Object2 = Invoke-RestMethod -Uri 'https://update.googleapis.com/service/update2' -Method Post -Body @'
 <?xml version="1.0" encoding="UTF-8"?>
 <request protocol="3.0">
-  <os platform="win" version="10" arch="x86" />
+  <os platform="win" version="10.0.22000" arch="x86" />
   <app appid="{8237E44A-0054-442C-B6B6-EA0509993955}" version="" ap="x86">
     <updatecheck />
   </app>
 </request>
 '@
-$Version2 = $Object2.response.app.updatecheck.manifest.version
 
-if ($Version1 -ne $Version2) { throw 'Distinct versions detected' }
+# arm64
+$Object3 = Invoke-RestMethod -Uri 'https://update.googleapis.com/service/update2' -Method Post -Body @'
+<?xml version="1.0" encoding="UTF-8"?>
+<request protocol="3.0">
+  <os platform="win" version="10.0.22000" arch="arm64" />
+  <app appid="{8237E44A-0054-442C-B6B6-EA0509993955}" version="" ap="arm64">
+    <updatecheck />
+  </app>
+</request>
+'@
+
+if (@(@($Object1, $Object2, $Object3) | Sort-Object -Property { $_.response.app.updatecheck.manifest.version } -Unique).Count -gt 1) {
+  throw 'Distinct versions detected'
+}
 
 # Version
-$this.CurrentState.Version = $Version1
+$this.CurrentState.Version = $Object1.response.app.updatecheck.manifest.version
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
@@ -35,6 +46,10 @@ $this.CurrentState.Installer += [ordered]@{
 $this.CurrentState.Installer += [ordered]@{
   Architecture = 'x64'
   InstallerUrl = ($Object1.response.app.updatecheck.urls.url.codebase | Select-String -Pattern 'https://dl.google.com' -Raw -SimpleMatch) + $Object1.response.app.updatecheck.manifest.actions.action.Where({ $_.event -eq 'install' }, 'First')[0].run
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'arm64'
+  InstallerUrl = ($Object3.response.app.updatecheck.urls.url.codebase | Select-String -Pattern 'https://dl.google.com' -Raw -SimpleMatch) + $Object3.response.app.updatecheck.manifest.actions.action.Where({ $_.event -eq 'install' }, 'First')[0].run
 }
 
 switch -Regex ($this.Check()) {
