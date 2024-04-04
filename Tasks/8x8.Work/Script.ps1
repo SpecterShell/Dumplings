2c9b6ffe62b371fafcf6eedc89c744477e88507d
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-04-04 16:45:54 +1300

    Task: Add 8x8.Work

---
+++ Tasks/8x8.Work/Script.ps1
@@ -0,0 +1,67 @@
+$Object1 = Invoke-RestMethod -Uri "https://cloud8.8x8.com/vos-update/public/api/v2/asset?application=work&channel=GA&platform=win32_x64&version=$($this.LastState.Contains('Version') ? $this.LastState.Version : 'v0.0.0-0-0')" -StatusCodeVariable 'StatusCode'
+
+if ($StatusCode -eq 204) {
+  $this.Log("The last version $($this.LastState.Version) is the latest, skip checking", 'Info')
+  return
+}
+
+$VersionMatches = [regex]::Match($Object1.version, 'v((\d+\.\d+)[\d\.]+)-(\d+)')
+
+# Version
+$this.CurrentState.Version = "$($VersionMatches.Groups[1].Value).$($VersionMatches.Groups[3].Value)"
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerType          = 'exe'
+  InstallerUrl           = "https://vod-updates.8x8.com/ga/work-64-exe-v$($VersionMatches.Groups[1].Value)-$($VersionMatches.Groups[3].Value).exe"
+  AppsAndFeaturesEntries = @(
+    @{
+      DisplayVersion = "$($VersionMatches.Groups[1].Value)-b$($VersionMatches.Groups[3].Value)"
+    }
+  )
+}
+$this.CurrentState.Installer += [ordered]@{
+  InstallerType = 'wix'
+  InstallerUrl  = "https://vod-updates.8x8.com/ga/work-64-msi-v$($VersionMatches.Groups[1].Value)-$($VersionMatches.Groups[3].Value).msi"
+}
+
+# ReleaseTime
+$this.CurrentState.ReleaseTime = [datetime]::ParseExact($Object1.pub_date, 'M/d/y, H:m tt', (Get-Culture -Name 'en-US')) | ConvertTo-UtcDateTime -Id 'UTC'
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # Only parse version for major updates
+      if (-not $this.LastState.Contains('Version') -or $VersionMatches.Groups[2].Value -ne ($this.LastState.Version.Split('.')[0..1] -join '.')) {
+        $Object2 = Invoke-WebRequest -Uri 'https://docs.8x8.com/8x8WebHelp/8x8-work-for-desktop/Content/workd/what-is-new.htm' | ConvertFrom-Html
+
+        $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//div[@id='mc-main-content']/h1[contains(text(), '$($VersionMatches.Groups[2].Value)')]")
+        if ($ReleaseNotesTitleNode) {
+          $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node -and $Node.Name -ne 'h1'; $Node = $Node.NextSibling) { $Node }
+          # ReleaseNotes (en-US)
+          $this.CurrentState.Locale += [ordered]@{
+            Locale = 'en-US'
+            Key    = 'ReleaseNotes'
+            Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
+          }
+        } else {
+          $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+        }
+      } else {
+        $this.Log("No ReleaseNotes #1 (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
