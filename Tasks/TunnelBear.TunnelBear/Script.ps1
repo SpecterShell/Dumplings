d6b2f612426e34b639dfbc63015d2e054f3b3e07
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-04-07 21:09:56 +1200

    Task: Add TunnelBear.TunnelBear

---
+++ Tasks/TunnelBear.TunnelBear/Script.ps1
@@ -0,0 +1,45 @@
+
+$Object1 = Invoke-RestMethod -Uri 'https://tunnelbear.s3.amazonaws.com/downloads/pc/update_windows.xml'
+
+# Version
+$this.CurrentState.Version = $Object1.item.version
+
+# Installer
+# The installer file in ./public is older than the one in ./
+# Use the latter one here
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.item.url.Replace('/public', '')
+}
+
+# ReleaseNotesUrl
+$this.CurrentState.Locale += [ordered]@{
+  Key   = 'ReleaseNotesUrl'
+  Value = $ReleaseNotesUrl = $Object1.item.changelog.Replace('/public', '')
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      $Object2 = Invoke-WebRequest -Uri $ReleaseNotesUrl | ConvertFrom-Html
+
+      # ReleaseNotes (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotes'
+        Value  = $Object2.SelectSingleNode('//*[@id="content"]') | Get-TextContent | Format-Text
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
