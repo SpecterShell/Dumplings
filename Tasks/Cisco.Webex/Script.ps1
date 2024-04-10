7f3e9c43e8b831fa6e327899fd42fd88334b7c00
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-04-10 21:47:14 +1200

    Task: Cisco.Webex Remove x86 installer

--- Tasks/Cisco.Webex/Script.ps1
+++ Tasks/Cisco.Webex/Script.ps1
@@ -1,25 +1,13 @@
-# x86
-$Object1 = Invoke-RestMethod -Uri 'https://client-upgrade-a.wbx2.com/client-upgrade/api/v1/webexteamsdesktop/upgrade/@me?channel=gold&model=win-32&r=AC805C72-1377-4FE6-B272-1FB4417B50B7'
 # x64
-$Object2 = Invoke-RestMethod -Uri 'https://client-upgrade-a.wbx2.com/client-upgrade/api/v1/webexteamsdesktop/upgrade/@me?channel=gold&model=win-64&r=AC805C72-1377-4FE6-B272-1FB4417B50B7'
-
-$Identical = $true
-if ($Object1.manifest.version -ne $Object2.manifest.version) {
-  $this.Log('Distinct versions detected', 'Warning')
-  $Identical = $false
-}
+$Object1 = Invoke-RestMethod -Uri 'https://client-upgrade-a.wbx2.com/client-upgrade/api/v1/webexteamsdesktop/upgrade/@me?channel=gold&model=win-64&r=AC805C72-1377-4FE6-B272-1FB4417B50B7'
 
 # Version
-$this.CurrentState.Version = $Object2.manifest.version
+$this.CurrentState.Version = $Object1.manifest.version
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  Architecture = 'x86'
-  InstallerUrl = $Object1.manifest.manualInstallPackageLocation
-}
-$this.CurrentState.Installer += [ordered]@{
   Architecture = 'x64'
-  InstallerUrl = $Object2.manifest.manualInstallPackageLocation
+  InstallerUrl = $Object1.manifest.manualInstallPackageLocation
 }
 
 switch -Regex ($this.Check()) {
@@ -30,7 +18,7 @@ switch -Regex ($this.Check()) {
   'Changed|Updated' {
     $this.Message()
   }
-  # ({ $_ -match 'Updated' -and $Identical }) {
-  #   $this.Submit()
-  # }
+  'Updated' {
+    $this.Submit()
+  }
 }
