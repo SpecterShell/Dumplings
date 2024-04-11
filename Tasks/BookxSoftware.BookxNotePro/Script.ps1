1482244db80ab382728c8a164db1aacd360b0572
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-04-12 11:34:24 +1200

    Task: BookxSoftware.BookxNotePro Parse ReleaseNotes

--- Tasks/BookxSoftware.BookxNotePro/Script.ps1
+++ Tasks/BookxSoftware.BookxNotePro/Script.ps1
@@ -34,6 +34,56 @@ $this.CurrentState.ReleaseTime = [regex]::Match(
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
+    try {
+      $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+      $this.CurrentState.Installer[0]['InstallerSha256'] = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
+
+      $NestedInstallerFile = $InstallerFile | Expand-TempArchive | Join-Path -ChildPath $this.CurrentState.Installer[0].NestedInstallerFiles[0].RelativeFilePath
+      $NestedInstallerFileExtracted = New-TempFolder
+      7z e -aoa -ba -bd -o"${NestedInstallerFileExtracted}" $NestedInstallerFile 'readme.txt' | Out-Host
+      $ReleaseNotesFile = Join-Path $NestedInstallerFileExtracted 'readme.txt'
+      $ReleaseNotesObject = [System.IO.StreamReader]::new($ReleaseNotesFile, [System.Text.Encoding]::GetEncoding('gb18030'))
+
+      while (-not $ReleaseNotesObject.EndOfStream) {
+        if ($ReleaseNotesObject.ReadLine().StartsWith("V$($this.CurrentState.Version)")) {
+          # ReleaseTime
+          $this.CurrentState.ReleaseTime = [datetime]::ParseExact($ReleaseNotesObject.ReadLine(), 'yyyyMMdd', $null).ToString('yyyy-MM-dd')
+          break
+        }
+      }
+      if (-not $ReleaseNotesObject.EndOfStream) {
+        $ReleaseNotesObjects = [System.Collections.Generic.List[string]]::new()
+        while (-not $ReleaseNotesObject.EndOfStream) {
+          $String = $ReleaseNotesObject.ReadLine()
+          if ($String -notmatch '^V\d+\.\d+\.\d+\.\d+') {
+            $ReleaseNotesObjects.Add($String)
+          } else {
+            break
+          }
+        }
+        # ReleaseNotes (zh-CN)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'zh-CN'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesObjects | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseTime and ReleaseNotes (zh-CN) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    } finally {
+      try {
+        if ((Test-Path -Path Variable:\ReleaseNotesObject) -and $ReleaseNotesObject) {
+          $ReleaseNotesObject.Close()
+        }
+      } catch {
+        $_ | Out-Host
+        $this.Log($_, 'Warning')
+      }
+    }
+
     $this.Print()
     $this.Write()
   }
