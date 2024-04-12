da7e6ca7f99006134ef732ccd296243afb6c949e
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-04-13 11:13:48 +1200

    Task: Add Lenovo.SLBrowser

---
+++ Tasks/Lenovo.SLBrowser/Script.ps1
@@ -0,0 +1,44 @@
+$Object1 = Invoke-RestMethod -Uri 'https://hao.lenovo.com.cn/browser-service-api/release' -Method Post -Body (
+  @{
+    browserActive  = 0
+    browserChannel = '10'
+    browserVersion = $this.LastState.Contains('Version') ? $this.LastState.Version : '9.0.3.1311'
+    osBits         = '64'
+    sn             = '0'
+  } | ConvertTo-Json -Compress
+) -ContentType 'application/json'
+$Object2 = Invoke-RestMethod -Uri "$($Object1.data.releaseUrl)SLBUpdate.ini" | ConvertFrom-Ini
+$Object3 = $Object2.files.GetEnumerator().Where({ $_.Value.Split('#')[2].Contains('\installer\') }, 'First')[0].Value.Split('#')
+
+# Version
+$this.CurrentState.Version = $Object3[1]
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl         = "$($Object1.data.releaseUrl)RootPath/SLBInstall/$($Object3[1])/$($Object3[0]).zip"
+  NestedInstallerFiles = @(
+    [ordered]@{
+      RelativeFilePath = $Object3[0]
+    }
+  )
+}
+
+# ReleaseNotes (zh-CN)
+$this.CurrentState.Locale += [ordered]@{
+  Locale = 'zh-CN'
+  Key    = 'ReleaseNotes'
+  Value  = $Object1.data.description | Format-Text
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
