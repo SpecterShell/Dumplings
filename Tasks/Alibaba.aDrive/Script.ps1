98de7715b7e39c86cbe28ded0116f90ccccd5e9d
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-04-13 19:40:29 +1200

    Task: Alibaba.aDrive Add ReleaseNotes

--- Tasks/Alibaba.aDrive/Script.ps1
+++ Tasks/Alibaba.aDrive/Script.ps1
@@ -4,6 +4,24 @@ $this.CurrentState = Invoke-RestMethod -Uri "$($Object1.url)/win32/ia32/latest.y
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
+    try {
+      $Object2 = (Invoke-WebRequest -Uri "$($Object1.url)/updateLog.json").Content | ConvertFrom-Json -AsHashtable
+
+      if ($Object2.Contains($this.CurrentState.Version)) {
+        # ReleaseNotes (zh-CN)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'zh-CN'
+          Key    = 'ReleaseNotes'
+          Value  = $Object2[$this.CurrentState.Version].logs | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseTime and ReleaseNotes (zh-CN) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
     $this.Print()
     $this.Write()
   }
