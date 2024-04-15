a37b8f3aa9f6d51457dea7ab367932f0256ee3e7
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-04-15 17:02:58 +1200

    Task: Add Google.Chrome.Beta.EXE

--- Tasks/Google.Chrome.Beta/Script.ps1
+++ Tasks/Google.Chrome.Beta/Script.ps1
@@ -1,55 +1,16 @@
-# x64
-$Object1 = Invoke-RestMethod -Uri 'https://update.googleapis.com/service/update2' -Method Post -Body @'
-<?xml version="1.0" encoding="UTF-8"?>
-<request protocol="3.0">
-  <os platform="win" version="10.0.22000" arch="x64" />
-  <app appid="{8237E44A-0054-442C-B6B6-EA0509993955}" version="" ap="x64">
-    <updatecheck />
-  </app>
-</request>
-'@
-
-# x86
-$Object2 = Invoke-RestMethod -Uri 'https://update.googleapis.com/service/update2' -Method Post -Body @'
-<?xml version="1.0" encoding="UTF-8"?>
-<request protocol="3.0">
-  <os platform="win" version="10.0.22000" arch="x86" />
-  <app appid="{8237E44A-0054-442C-B6B6-EA0509993955}" version="" ap="x86">
-    <updatecheck />
-  </app>
-</request>
-'@
-
-# arm64
-$Object3 = Invoke-RestMethod -Uri 'https://update.googleapis.com/service/update2' -Method Post -Body @'
-<?xml version="1.0" encoding="UTF-8"?>
-<request protocol="3.0">
-  <os platform="win" version="10.0.22000" arch="arm64" />
-  <app appid="{8237E44A-0054-442C-B6B6-EA0509993955}" version="" ap="arm64">
-    <updatecheck />
-  </app>
-</request>
-'@
-
-if (@(@($Object1, $Object2, $Object3) | Sort-Object -Property { $_.response.app.updatecheck.manifest.version } -Unique).Count -gt 1) {
-  throw 'Distinct versions detected'
-}
+$Object1 = Invoke-RestMethod -Uri 'https://versionhistory.googleapis.com/v1/chrome/platforms/win/channels/beta/versions'
 
 # Version
-$this.CurrentState.Version = $Object1.response.app.updatecheck.manifest.version
+$this.CurrentState.Version = $Object1.versions[0].version
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
   Architecture = 'x86'
-  InstallerUrl = ($Object2.response.app.updatecheck.urls.url.codebase | Select-String -Pattern 'https://dl.google.com' -Raw -SimpleMatch) + $Object2.response.app.updatecheck.manifest.actions.action.Where({ $_.event -eq 'install' }, 'First')[0].run
+  InstallerUrl = 'https://dl.google.com/dl/chrome/install/beta/googlechromebetastandaloneenterprise.msi'
 }
 $this.CurrentState.Installer += [ordered]@{
   Architecture = 'x64'
-  InstallerUrl = ($Object1.response.app.updatecheck.urls.url.codebase | Select-String -Pattern 'https://dl.google.com' -Raw -SimpleMatch) + $Object1.response.app.updatecheck.manifest.actions.action.Where({ $_.event -eq 'install' }, 'First')[0].run
-}
-$this.CurrentState.Installer += [ordered]@{
-  Architecture = 'arm64'
-  InstallerUrl = ($Object3.response.app.updatecheck.urls.url.codebase | Select-String -Pattern 'https://dl.google.com' -Raw -SimpleMatch) + $Object3.response.app.updatecheck.manifest.actions.action.Where({ $_.event -eq 'install' }, 'First')[0].run
+  InstallerUrl = 'https://dl.google.com/dl/chrome/install/beta/googlechromebetastandaloneenterprise64.msi'
 }
 
 switch -Regex ($this.Check()) {
