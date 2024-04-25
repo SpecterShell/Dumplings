f47867059c998e41defeb7ac4fbf9210558e5e68
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-04-25 14:34:17 +1200

    Task: Tableau.PrepBuilder Apply headers to all web requests and reorganize code

--- Tasks/Tableau.PrepBuilder/Script.ps1
+++ Tasks/Tableau.PrepBuilder/Script.ps1
@@ -1,35 +1,33 @@
-$UserAgent = 'Tableau Desktop 20233.23.1017.0948; public; libcurl-client; 64-bit; en_US; Microsoft Windows 10 Pro (Build 19045);'
+$Headers = @{
+  Accept       = '*/*'
+  'User-Agent' = 'Tableau Desktop 20233.23.1017.0948; public; libcurl-client; 64-bit; en_US; Microsoft Windows 10 Pro (Build 19045);'
+}
 
+$Object1 = Invoke-WebRequest -Uri 'https://www.tableau.com/support/releases/prep/latest' -Headers $Headers -SkipHeaderValidation
 # ReleaseNotesUrl
 $this.CurrentState.Locale += [ordered]@{
   Key   = 'ReleaseNotesUrl'
-  Value = $ReleaseNotesUrl = Get-RedirectedUrl1st -Uri 'https://www.tableau.com/support/releases/prep/latest' -Headers @{ Accept = '*/*'; 'User-Agent' = $UserAgent }
-}
-# ReleaseNotesUrl (zh-CN)
-$this.CurrentState.Locale += [ordered]@{
-  Locale = 'zh-CN'
-  Key    = 'ReleaseNotesUrl'
-  Value  = $ReleaseNotesUrlCN = Get-RedirectedUrl1st -Uri 'https://www.tableau.com/zh-cn/support/releases/prep/latest' -Headers @{ Accept = '*/*'; 'User-Agent' = $UserAgent }
+  Value = $Object1.BaseResponse.RequestMessage.RequestUri.AbsoluteUri
 }
 
-$Object1 = Invoke-WebRequest -Uri $ReleaseNotesUrl -Headers @{ Accept = '*/*' } | ConvertFrom-Html
-$Object2 = $Object1.SelectSingleNode('//script[@data-drupal-selector="drupal-settings-json"]').InnerText.Trim() | ConvertFrom-Json
-$Object3 = Invoke-RestMethod -Uri $Object2.tableauReleaseDownloadLinks.url -Headers @{ Accept = '*/*' } | Get-EmbeddedJson -StartsFrom 'jsonCallback(' | ConvertFrom-Json
+$Object2 = $Object1 | ConvertFrom-Html
+$Object3 = $Object2.SelectSingleNode('//script[@data-drupal-selector="drupal-settings-json"]').InnerText.Trim() | ConvertFrom-Json
+$Object4 = Invoke-RestMethod -Uri $Object3.tableauReleaseDownloadLinks.url -Headers $Headers -SkipHeaderValidation | Get-EmbeddedJson -StartsFrom 'jsonCallback(' | ConvertFrom-Json
 
 # Version
-$this.CurrentState.Version = $Object3.release[0].version
+$this.CurrentState.Version = $Object4.release[0].version
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = $Object3.release[0].windows_desktop_installers[0].download_link
+  InstallerUrl = $Object4.release[0].windows_desktop_installers[0].download_link
 }
 
 # ReleaseTime
-$this.CurrentState.ReleaseTime = $Object3.release[0].release_date | Get-Date -Format 'yyyy-MM-dd'
+$this.CurrentState.ReleaseTime = $Object4.release[0].release_date | Get-Date -Format 'yyyy-MM-dd'
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
-    $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl -Headers @{ Accept = '*/*' }
+    $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl -Headers $Headers -SkipHeaderValidation
 
     # InstallerSha256
     $this.CurrentState.Installer[0]['InstallerSha256'] = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
@@ -48,7 +46,7 @@ switch -Regex ($this.Check()) {
       $this.CurrentState.Locale += [ordered]@{
         Locale = 'en-US'
         Key    = 'ReleaseNotes'
-        Value  = $Object1.SelectNodes('//table[@class="table-list"]/tr/td[2]') | Get-TextContent | Format-Text
+        Value  = $Object2.SelectNodes('//table[@class="table-list"]/tr/td[2]') | Get-TextContent | Format-Text
       }
     } catch {
       $_ | Out-Host
@@ -56,13 +54,20 @@ switch -Regex ($this.Check()) {
     }
 
     try {
-      $Object4 = Invoke-WebRequest -Uri $ReleaseNotesUrlCN -Headers @{ Accept = '*/*' } | ConvertFrom-Html
+      $Object5 = Invoke-WebRequest -Uri 'https://www.tableau.com/zh-cn/support/releases/prep/latest' -Headers $Headers -SkipHeaderValidation
+      # ReleaseNotesUrl (zh-CN)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'zh-CN'
+        Key    = 'ReleaseNotesUrl'
+        Value  = $Object5.BaseResponse.RequestMessage.RequestUri.AbsoluteUri
+      }
 
+      $Object6 = $Object5 | ConvertFrom-Html
       # ReleaseNotes (zh-CN)
       $this.CurrentState.Locale += [ordered]@{
         Locale = 'zh-CN'
         Key    = 'ReleaseNotes'
-        Value  = $Object4.SelectNodes('//table[@class="table-list"]/tr/td[2]') | Get-TextContent | Format-Text
+        Value  = $Object6.SelectNodes('//table[@class="table-list"]/tr/td[2]') | Get-TextContent | Format-Text
       }
     } catch {
       $_ | Out-Host
