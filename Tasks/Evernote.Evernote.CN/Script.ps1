fc96a6b282660fa5c1b548da66882e8cf739b166
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-04-26 13:48:12 +1200

    Task: Add Evernote.Evernote.CN

---
+++ Tasks/Evernote.Evernote.CN/Script.ps1
@@ -0,0 +1,79 @@
+$Object1 = Invoke-RestMethod -Uri 'https://update.yinxiang.com/public/YXWin6/update.xml'
+
+# Version
+$this.CurrentState.Version = ($Object1.update.release3.version -split '\s+')[0]
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.update.release3.executable.url | ConvertTo-Https
+}
+
+# ReleaseNotesUrl
+$this.CurrentState.Locale += [ordered]@{
+  Key   = 'ReleaseNotesUrl'
+  Value = $ReleaseNotesUrl = $Object1.update.release3.releasenotes.Where({ $_.lang -eq 'en-us' }, 'First')[0].url
+}
+
+# ReleaseNotesUrl (zh-CN)
+$this.CurrentState.Locale += [ordered]@{
+  Locale = 'zh-CN'
+  Key    = 'ReleaseNotesUrl'
+  Value  = $ReleaseNotesUrlCN = $Object1.update.release3.releasenotes.Where({ $_.lang -eq 'zh-CN' }, 'First')[0].url
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+    $NestedInstallerFileRoot = New-TempFolder
+    7z e -aoa -ba -bd '-t#' -o"${NestedInstallerFileRoot}" $InstallerFile '2.msi' | Out-Host
+    $NestedInstallerFile = Join-Path $NestedInstallerFileRoot '2.msi'
+
+    # InstallerSha256
+    $this.CurrentState.Installer[0]['InstallerSha256'] = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
+    # AppsAndFeaturesEntries
+    $this.CurrentState.Installer[0]['AppsAndFeaturesEntries'] = @(
+      [ordered]@{
+        ProductCode   = $this.CurrentState.Installer[0]['ProductCode'] = $NestedInstallerFile | Read-ProductCodeFromMsi
+        UpgradeCode   = $NestedInstallerFile | Read-UpgradeCodeFromMsi
+        InstallerType = 'wix'
+      }
+    )
+
+    try {
+      $Object2 = Invoke-WebRequest -Uri $ReleaseNotesUrl | ConvertFrom-Html
+
+      # ReleaseNotes (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotes'
+        Value  = $Object2.SelectNodes('/html/body/node()[contains(., "Welcome to")][1]/following-sibling::node()') | Get-TextContent | Format-Text
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    try {
+      $Object3 = Invoke-WebRequest -Uri $ReleaseNotesUrlCN | ConvertFrom-Html
+
+      # ReleaseNotes (zh-CN)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'zh-CN'
+        Key    = 'ReleaseNotes'
+        Value  = $Object3.SelectNodes('/html/body/node()[contains(., "欢迎体验")][1]/following-sibling::node()') | Get-TextContent | Format-Text
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
