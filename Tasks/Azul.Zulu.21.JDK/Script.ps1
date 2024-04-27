18be50a915b8139372bd556afbcfab2da5baf471
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-04-28 01:22:18 +1200

    Task: Azul.Zulu Add architectures to installers

--- Tasks/Azul.Zulu.21.JDK/Script.ps1
+++ Tasks/Azul.Zulu.21.JDK/Script.ps1
@@ -1,10 +1,11 @@
-$Object1 = $Global:DumplingsStorage.AzulZuluBuilds | Where-Object -FilterScript { $_.distro_version[0] -eq 21 -and $_.name.Contains('jdk') -and -not $_.name.Contains('fx') } | Sort-Object -Property { $_.distro_version.ForEach({ $_.ToString().PadLeft(20) }) -join '.' } -Bottom 1
+$Object1 = $Global:DumplingsStorage.AzulZuluBuilds | Where-Object -FilterScript { $_.distro_version[0] -eq 21 -and $_.name.Contains('jdk') -and -not $_.name.Contains('fx') -and $_.name.Contains('x64') } | Sort-Object -Property { $_.distro_version.ForEach({ $_.ToString().PadLeft(20) }) -join '.' } -Bottom 1
 
 # Version
 $this.CurrentState.Version = $Object1.distro_version[0..2] -join '.'
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
   InstallerUrl = $Object1.download_url
 }
 
