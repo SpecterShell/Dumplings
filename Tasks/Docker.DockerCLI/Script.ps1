bcb12e151fec49dd6219255c2f0c203d3ada2e2c
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-05-02 12:45:21 +1200

    Task: Add Docker.DockerCLI

---
+++ Tasks/Docker.DockerCLI/Script.ps1
@@ -0,0 +1,27 @@
+$Prefix = 'https://download.docker.com/win/static/stable/x86_64/'
+
+$Object1 = Invoke-WebRequest -Uri $Prefix
+
+$InstallerName = $Object1.Links.href | Where-Object -FilterScript { $_ -ne '../' -and $_ -match 'docker-[\d\.]+\.zip' } | Sort-Object -Property { $_ -replace '\d+', { $_.Value.PadLeft(20) } } -Bottom 1
+
+# Version
+$this.CurrentState.Version = [regex]::Match($InstallerName, 'docker-([\d\.]+)\.zip').Groups[1].Value
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = $Prefix + $InstallerName
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
