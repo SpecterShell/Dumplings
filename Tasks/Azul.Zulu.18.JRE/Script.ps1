5cb63493fe3d6112fa498e5b262c8a078f98d2ed
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-05-03 22:56:40 +1200

    Task: Add Azul.Zulu

---
+++ Tasks/Azul.Zulu.18.JRE/Script.ps1
@@ -0,0 +1,23 @@
+$Object1 = $Global:DumplingsStorage.AzulZuluBuilds | Where-Object -FilterScript { $_.distro_version[0] -eq 18 -and $_.name.Contains('jre') -and -not $_.name.Contains('fx') -and $_.name.Contains('i686') } | Sort-Object -Property { $_.distro_version.ForEach({ $_.ToString().PadLeft(20) }) -join '.' } -Bottom 1
+
+# Version
+$this.CurrentState.Version = $Object1.distro_version[0..2] -join '.'
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x86'
+  InstallerUrl = $Object1.download_url
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
