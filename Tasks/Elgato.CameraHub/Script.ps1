67541349b8fd6a1239716a603e38b13c1083a586
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-05-12 21:43:49 +1200

    Task: Add Elgato.CameraHub and Elgato.ControlCenter

---
+++ Tasks/Elgato.CameraHub/Script.ps1
@@ -0,0 +1,42 @@
+$Object1 = Invoke-RestMethod -Uri 'https://gc-updates.elgato.com/windows/echw-update/final/app-version-check.json.php'
+
+# Version
+$this.CurrentState.Version = $Object1.Manual.Version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.Manual.fileURL
+}
+
+# ReleaseNotesUrl
+$this.CurrentState.Locale += [ordered]@{
+  Key   = 'ReleaseNotesUrl'
+  Value = $ReleaseNotesUrl = $Object1.Manual.ReleaseNotes.en
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      $Object2 = Invoke-WebRequest -Uri $ReleaseNotesUrl | ConvertFrom-Html
+
+      # ReleaseNotes (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotes'
+        Value  = $Object2.SelectSingleNode('/html/body') | Get-TextContent | Format-Text
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
