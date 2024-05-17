102e96ae55ca66c3be50949f8bc00ad7a41d5558
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-05-17 12:00:25 +1200

    Task: ToEverything.AFFiNE Read real version from the first installer

--- Tasks/ToEverything.AFFiNE/Script.ps1
+++ Tasks/ToEverything.AFFiNE/Script.ps1
@@ -40,6 +40,13 @@ $this.CurrentState.Locale += [ordered]@{
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
+    $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+
+    # InstallerSha256
+    $this.CurrentState.Installer[0]['InstallerSha256'] = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
+    # RealVersion
+    $this.CurrentState.RealVersion = $InstallerFile | Read-ProductVersionFromExe
+
     $this.Print()
     $this.Write()
   }
