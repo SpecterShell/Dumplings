313805edb3d413bc271d0fb1efac3528926878a8
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-05-18 13:53:31 +1200

    Task: Wondershare.Mockitt.CN Fix

--- Tasks/Wondershare.Mockitt.CN/Script.ps1
+++ Tasks/Wondershare.Mockitt.CN/Script.ps1
@@ -2,18 +2,18 @@ $Object1 = Invoke-WebRequest -Uri 'https://modao.cc/feature/downloads.html' | Co
 
 # Version
 $this.CurrentState.Version = [regex]::Match(
-  $Object1.SelectSingleNode('/html/body/main/section[2]/div/div[3]').InnerText,
+  $Object1.SelectSingleNode('/html/body/main/section[2]/div/p').InnerText,
   'v([\d\.]+)'
 ).Groups[1].Value
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
   Architecture = 'x86'
-  InstallerUrl = $Object1.SelectSingleNode('//*[@id="download-win32"]').Attributes['href'].Value
+  InstallerUrl = $Object1.SelectSingleNode('//a[contains(@class, "btn-download-pc") and contains(@href, ".exe") and contains(@href, "ia32")]').Attributes['href'].Value
 }
 $this.CurrentState.Installer += [ordered]@{
   Architecture = 'x64'
-  InstallerUrl = $Object1.SelectSingleNode('//*[@id="download-win64"]').Attributes['href'].Value
+  InstallerUrl = $Object1.SelectSingleNode('//a[contains(@class, "btn-download-pc") and contains(@href, ".exe") and contains(@href, "x64")]').Attributes['href'].Value
 }
 
 switch -Regex ($this.Check()) {
