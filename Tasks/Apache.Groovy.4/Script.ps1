3c4f09a26d926ae1d377e710610a3a3df3596f58
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-05-18 13:26:11 +1200

    Task: Add Apache.Groovy

---
+++ Tasks/Apache.Groovy.4/Script.ps1
@@ -0,0 +1,41 @@
+$Object1 = Invoke-RestMethod -Uri 'https://groovy.jfrog.io/ui/api/v1/ui/v2/treebrowser' -Method Post -Headers @{ 'X-Requested-With' = 'XMLHttpRequest' } -ContentType 'application/json' -Body @(
+  @{
+    type        = 'paginatedJunction'
+    repoType    = 'local'
+    repoKey     = 'dist-release-local'
+    path        = 'groovy-windows-installer'
+    repoPkgType = 'Generic'
+  } | ConvertTo-Json -Compress
+)
+
+# Version
+$this.CurrentState.Version = $Object1.data.text -match '^groovy-4\.[\d\.]+$' -replace '^groovy-([\d\.]+)$', '$1' |
+  Sort-Object -Property { [regex]::Replace($_, '\d+', { $args[0].Value.PadLeft(20) }) } -Bottom 1
+
+$Object2 = Invoke-RestMethod -Uri 'https://groovy.jfrog.io/ui/api/v1/ui/v2/treebrowser' -Method Post -Headers @{ 'X-Requested-With' = 'XMLHttpRequest' } -ContentType 'application/json' -Body @(
+  @{
+    type        = 'paginatedJunction'
+    repoType    = 'local'
+    repoKey     = 'dist-release-local'
+    path        = "groovy-windows-installer/groovy-$($this.CurrentState.Version)"
+    repoPkgType = 'Generic'
+  } | ConvertTo-Json -Compress
+)
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = "https://groovy.jfrog.io/artifactory/dist-release-local/$($Object2.data.Where({ $_.text.EndsWith('.msi') }, 'First')[0].path)"
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
