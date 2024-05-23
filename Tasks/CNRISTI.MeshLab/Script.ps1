d26f223bbc5e57f717ad342f85f225f2eb26ebf2
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-05-23 18:02:22 +1200

    Task: Add CNRISTI.MeshLab

---
+++ Tasks/CNRISTI.MeshLab/Script.ps1
@@ -0,0 +1,34 @@
+$RepoOwner = 'cnr-isti-vclab'
+$RepoName = 'meshlab'
+
+$Object1 = Invoke-GitHubApi -Uri "https://api.github.com/repos/${RepoOwner}/${RepoName}/releases/latest"
+
+# Version
+$this.CurrentState.Version = $Object1.tag_name -creplace '^MeshLab-'
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.assets.Where({ $_.name.EndsWith('.exe') -and $_.name.StartsWith("MeshLab$($this.CurrentState.Version)-") }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
+}
+
+# ReleaseTime
+$this.CurrentState.ReleaseTime = $Object1.published_at.ToUniversalTime()
+
+# ReleaseNotesUrl
+$this.CurrentState.Locale += [ordered]@{
+  Key   = 'ReleaseNotesUrl'
+  Value = $Object1.html_url
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
