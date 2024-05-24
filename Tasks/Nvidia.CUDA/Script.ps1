b7050d821d87e88a2a5f2add45ee17b33fc4fd2c
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-05-24 19:14:51 +1200

    Task: Add Nvidia.CUDA

---
+++ Tasks/Nvidia.CUDA/Script.ps1
@@ -0,0 +1,26 @@
+$Object1 = (Invoke-RestMethod -Uri 'https://developer.nvidia.com/cuda-downloads.json').data.releases.'Windows/x86_64/11/exe_local'
+
+# Version
+$this.CurrentState.Version = [regex]::Match($Object1.filename, 'cuda_([\d\.]+)').Groups[1].Value
+
+# RealVersion
+$this.CurrentState.RealVersion = $this.CurrentState.Version.Split('.')[0..1] -join '.'
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = ($Object1.details | ConvertFrom-Html).SelectSingleNode('//a[contains(@href, ".exe")]').Attributes['href'].Value
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
