daf0dffd606cad71bfada5967db7af4860f2d9cc
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-05-26 11:14:52 +1200

    Task: NetEase.Lingxi Remove x86 installer

--- Tasks/NetEase.Lingxi/Script.ps1
+++ Tasks/NetEase.Lingxi/Script.ps1
@@ -1,24 +1,10 @@
-# Installer
-$this.CurrentState.Installer += [ordered]@{
-  Architecture = 'x86'
-  InstallerUrl = $InstallerUrlX86 = Get-RedirectedUrl -Uri 'https://sirius-release.lx.netease.com/api/pub/client/update/download-win32'
-}
-$VersionX86 = [regex]::Matches($InstallerUrlX86, '([\d\.]+)\.exe').Groups[-1].Value
-
 $this.CurrentState.Installer += [ordered]@{
   Architecture = 'x64'
   InstallerUrl = $InstallerUrlX64 = Get-RedirectedUrl -Uri 'https://sirius-release.lx.netease.com/api/pub/client/update/download-windows'
 }
-$VersionX64 = [regex]::Matches($InstallerUrlX64, '([\d\.]+)\.exe').Groups[-1].Value
 
 # Version
-$this.CurrentState.Version = $VersionX64
-
-$Identical = $true
-if ($VersionX86 -ne $VersionX64) {
-  $this.Log('Distinct versions detected', 'Warning')
-  $Identical = $false
-}
+$this.CurrentState.Version = [regex]::Matches($InstallerUrlX64, '([\d\.]+)\.exe').Groups[-1].Value
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
@@ -28,7 +14,7 @@ switch -Regex ($this.Check()) {
   'Changed|Updated' {
     $this.Message()
   }
-  ({ $_ -match 'Updated' -and $Identical }) {
+  'Updated' {
     $this.Submit()
   }
 }
