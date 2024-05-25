c67d21250fcb773b7aa7dc7cc18921e46b526bd7
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-05-26 10:55:37 +1200

    Task: Add KiCad.KiCad

---
+++ Tasks/KiCad.KiCad/Script.ps1
@@ -0,0 +1,100 @@
+$RepoOwner = 'KiCad'
+$RepoName = 'kicad-source-mirror'
+
+$Object1 = Invoke-GitHubApi -Uri "https://api.github.com/repos/${RepoOwner}/${RepoName}/releases/latest"
+
+# Version
+$this.CurrentState.Version = $Object1.tag_name -creplace '^v'
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = $Object1.assets.Where({ $_.name.EndsWith('.exe') -and $_.name.Contains('x86_64') }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'arm64'
+  InstallerUrl = $Object1.assets.Where({ $_.name.EndsWith('.exe') -and $_.name.Contains('arm64') }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
+}
+
+# ReleaseTime
+$this.CurrentState.ReleaseTime = $Object1.published_at.ToUniversalTime()
+
+# Documentations
+$this.CurrentState.Locale += [ordered]@{
+  Key   = 'Documentations'
+  Value = @(
+    [ordered]@{
+      DocumentLabel = 'Documentation'
+      DocumentUrl   = "https://docs.kicad.org/$($this.CurrentState.Version.Split('.')[0..1] -join '.')/"
+    }
+  )
+}
+
+# Documentations (zh-CN)
+$this.CurrentState.Locale += [ordered]@{
+  Locale = 'zh-CN'
+  Key    = 'Documentations'
+  Value  = @(
+    [ordered]@{
+      DocumentLabel = '文档'
+      DocumentUrl   = "https://docs.kicad.org/$($this.CurrentState.Version.Split('.')[0..1] -join '.')/zh/"
+    }
+  )
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      $Object2 = Invoke-RestMethod -Uri 'https://downloads.kicad.org/api/v1/update' -Method Post -Body (
+        @{
+          platform        = 'windows'
+          arch            = 'amd64'
+          current_version = $this.LastState.Contains('Version') ? $this.LastState.Version : '8.0.1'
+          lang            = ''
+          last_check      = ''
+        } | ConvertTo-Json -Compress
+      ) -ContentType 'application/json'
+
+      if ($Object2.version -eq $this.CurrentState.Version) {
+        # ReleaseNotesUrl
+        $this.CurrentState.Locale += [ordered]@{
+          Key   = 'ReleaseNotesUrl'
+          Value = $ReleaseNotesUrl = $Object2.details_url
+        }
+
+        $Object3 = Invoke-WebRequest -Uri $ReleaseNotesUrl | ConvertFrom-Html
+
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $Object3.SelectSingleNode('//article/section[1]') | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotesUrl and ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+        # ReleaseNotesUrl
+        $this.CurrentState.Locale += [ordered]@{
+          Key   = 'ReleaseNotesUrl'
+          Value = 'https://www.kicad.org/blog/'
+        }
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+      # ReleaseNotesUrl
+      $this.CurrentState.Locale += [ordered]@{
+        Key   = 'ReleaseNotesUrl'
+        Value = 'https://www.kicad.org/blog/'
+      }
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
