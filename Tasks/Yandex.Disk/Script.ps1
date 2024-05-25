1f5fe6198f004ee5151eb617c2113ab249c8adad
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-05-25 22:36:32 +1200

    Task: Add Yandex.Disk

---
+++ Tasks/Yandex.Disk/Script.ps1
@@ -0,0 +1,35 @@
+# x64
+$Object1 = Invoke-RestMethod -Uri 'https://cloud-api.yandex.net/v1/disk/clients/win64/installer'
+
+# x86
+$Object2 = Invoke-RestMethod -Uri 'https://cloud-api.yandex.net/v1/disk/clients/win86/installer'
+
+if (@(@($Object1, $Object2) | Sort-Object -Property { $_.version } -Unique).Count -gt 1) {
+  throw 'Distinct versions detected'
+}
+
+# Version
+$this.CurrentState.Version = $Object1.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x86'
+  InstallerUrl = $Object2.file
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = $Object1.file
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  ({ $_ -match 'Updated' -and $Identical }) {
+    $this.Submit()
+  }
+}
