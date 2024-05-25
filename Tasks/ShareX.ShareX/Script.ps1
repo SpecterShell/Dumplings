d1eca30c689b2b054d030237314d3a9eafe2741a
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-05-25 19:21:29 +1200

    Task: Add ShareX.ShareX

---
+++ Tasks/ShareX.ShareX/Script.ps1
@@ -0,0 +1,51 @@
+$RepoOwner = 'ShareX'
+$RepoName = 'ShareX'
+
+$Object1 = Invoke-GitHubApi -Uri "https://api.github.com/repos/${RepoOwner}/${RepoName}/releases/latest"
+
+# Version
+$this.CurrentState.Version = $Object1.tag_name -creplace '^v'
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.assets.Where({ $_.name.EndsWith('.exe') -and $_.name.Contains('setup') }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
+}
+
+# ReleaseTime
+$this.CurrentState.ReleaseTime = $Object1.published_at.ToUniversalTime()
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      $Object2 = Invoke-WebRequest -Uri 'https://getsharex.com/changelog' | ConvertFrom-Html
+
+      # Remove GitHub badges which will take up two extra lines in parsed text
+      $Object2.SelectNodes('//a[@class="badge"]').ForEach({ $_.Remove() })
+
+      $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//h2[@id='v$($this.CurrentState.Version)']")
+      if ($ReleaseNotesTitleNode) {
+        $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node -and $Node.Name -ne 'h2'; $Node = $Node.NextSibling) { $Node }
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
