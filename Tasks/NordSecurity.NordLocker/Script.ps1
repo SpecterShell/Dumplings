ce2bf5b85f07714b7ebcaee6b94f67263c9e01e5
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-05-25 20:19:09 +1200

    Task: Add NordSecurity.NordLocker

---
+++ Tasks/NordSecurity.NordLocker/Script.ps1
@@ -0,0 +1,29 @@
+$Object1 = (Invoke-WebRequest -Uri 'https://downloads.nordcdn.com/apps/windows/meta/NordLocker/latest/update.xml' | Read-ResponseContent | ConvertFrom-Xml).channels.channel.Where({ $_.name -eq 'default' }, 'First')[0]
+
+# Version
+$this.CurrentState.Version = $Object1.releases.release.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.releases.release.update.url
+}
+
+# ReleaseNotes (en-US)
+$this.CurrentState.Locale += [ordered]@{
+  Locale = 'en-US'
+  Key    = 'ReleaseNotes'
+  Value  = $Object1.releases.release.description.'#cdata-section' | ConvertFrom-Json | ConvertTo-UnorderedList | Format-Text
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
