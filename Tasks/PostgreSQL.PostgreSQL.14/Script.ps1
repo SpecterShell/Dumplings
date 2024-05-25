f184440eaa42f8bb0d8d94ecc8e0b4fe644d818c
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-05-25 14:05:19 +1200

    Task: Add PostgreSQL.PostgreSQL.14

---
+++ Tasks/PostgreSQL.PostgreSQL.14/Script.ps1
@@ -0,0 +1,29 @@
+$Object1 = Invoke-WebRequest -Uri 'https://www.enterprisedb.com/downloads/postgres-postgresql-downloads' | ConvertFrom-Html
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $InstallerUrl = Get-RedirectedUrl -Uri $Object1.SelectSingleNode('//table/tbody/tr[starts-with(./td[1]/text(), "14")][1]/td[5]/a').Attributes['href'].Value
+}
+
+# Version
+$VersionMatches = [regex]::Match($InstallerUrl, '((14(\.\d+)+)-\d+)')
+$this.CurrentState.Version = $VersionMatches.Groups[1].Value
+
+# ReleaseNotesUrl
+$this.CurrentState.Locale += [ordered]@{
+  Key   = 'ReleaseNotesUrl'
+  Value = "https://www.postgresql.org/docs/release/$($VersionMatches.Groups[2].Value)/"
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
