0c3a40af6239205d31fe1a953b5c0a192bb9d173
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-05-28 13:36:15 +1200

    Task: Add DimitriVanHeesch.Doxygen

---
+++ Tasks/DimitriVanHeesch.Doxygen/Script.ps1
@@ -0,0 +1,62 @@
+$Object1 = Invoke-WebRequest -Uri 'https://www.doxygen.nl/download.html'
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $InstallerUrl = $Object1.Links | Where-Object -FilterScript { ($_ | Get-Member -Name 'href' -ErrorAction SilentlyContinue) -and $_.href.EndsWith('.exe') -and $_.href.Contains('setup') } | Select-Object -First 1 | Select-Object -ExpandProperty 'href'
+}
+
+# Version
+$this.CurrentState.Version = [regex]::Match($InstallerUrl, '-(\d+(\.\d+){2,})[-.]').Groups[1].Value
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      $Object2 = Invoke-WebRequest -Uri 'https://www.doxygen.nl/manual/changelog.html' | ConvertFrom-Html
+
+      $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//div[@class='textblock']/h2[contains(., 'Release $($this.CurrentState.Version)')]")
+      if ($ReleaseNotesTitleNode) {
+        $ReleaseNotesTimeNode = $ReleaseNotesTitleNode.SelectSingleNode('following-sibling::*[1]')
+        try {
+          # ReleaseTime
+          $this.CurrentState.ReleaseTime = [datetime]::ParseExact(
+            [regex]::Match($ReleaseNotesTimeNode.InnerText, '(\d{1,2}-\d{1,2}-\d{4})').Groups[1].Value,
+            'dd-MM-yyyy',
+            $null
+          ).ToString('yyyy-MM-dd')
+
+          # ReleaseNotes (en-US)
+          $ReleaseNotesNodes = for ($Node = $ReleaseNotesTimeNode.NextSibling; $Node -and $Node.Name -notin @('h1', 'h2'); $Node = $Node.NextSibling) { $Node }
+          $this.CurrentState.Locale += [ordered]@{
+            Locale = 'en-US'
+            Key    = 'ReleaseNotes'
+            Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
+          }
+        } catch {
+          $this.Log("No ReleaseTime for version $($this.CurrentState.Version)", 'Warning')
+
+          # ReleaseNotes (en-US)
+          $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node -and $Node.Name -notin @('h1', 'h2'); $Node = $Node.NextSibling) { $Node }
+          $this.CurrentState.Locale += [ordered]@{
+            Locale = 'en-US'
+            Key    = 'ReleaseNotes'
+            Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
+          }
+        }
+      } else {
+        $this.Log("No ReleaseTime and ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
