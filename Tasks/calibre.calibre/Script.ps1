dc00df238f0ffe786d4d8145d3a9048a49352d2c
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-05-31 23:28:55 +1200

    Task: Add calibre.calibre

---
+++ Tasks/calibre.calibre/Script.ps1
@@ -0,0 +1,45 @@
+$Object1 = Invoke-RestMethod -Uri 'http://code.calibre-ebook.com/latest'
+
+# Version
+$this.CurrentState.Version = $Object1.Trim()
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = "https://download.calibre-ebook.com/$($this.CurrentState.Version)/calibre-64bit-$($this.CurrentState.Version).msi"
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      $Object2 = Invoke-WebRequest -Uri 'https://calibre-ebook.com/whats-new' | ConvertFrom-Html
+
+      $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//div[@class='release' and contains(./h2[@class='release-title'], '$($this.CurrentState.Version.Replace('.0', ''))')]/h2[@class='release-title']")
+      if ($ReleaseNotesTitleNode) {
+        # ReleaseTime
+        $this.CurrentState.ReleaseTime = [regex]::Match($ReleaseNotesTitleNode.InnerText, '(\d{1,2} [a-zA-Z]+, \d{4})').Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
+
+        $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node -and $Node.Name -ne 'h1'; $Node = $Node.NextSibling) { $Node }
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
