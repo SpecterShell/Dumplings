56a85873a62118864cfff71e9628a0f390235d0b
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-06-01 22:38:55 +1200

    Task: Add GeoGebra.Classic

---
+++ Tasks/GeoGebra.Classic/Script.ps1
@@ -0,0 +1,42 @@
+$InstallerUrl = Get-RedirectedUrl -Uri 'https://download.geogebra.org/package/win-autoupdate'
+
+# Version
+$this.CurrentState.Version = [regex]::Match($InstallerUrl, '-(\d+-\d+-\d+)').Groups[1].Value.Replace('-', '.')
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerType          = 'exe'
+  InstallerUrl           = $InstallerUrl
+  AppsAndFeaturesEntries = @(
+    [ordered]@{
+      DisplayVersion = $this.CurrentState.Version
+    }
+  )
+}
+$this.CurrentState.Installer += $InstallerWix = [ordered]@{
+  InstallerType = 'wix'
+  InstallerUrl  = $InstallerUrl = Get-RedirectedUrl -Uri 'https://download.geogebra.org/package/win-msi6'
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $InstallerFileWix = Get-TempFile -Uri $InstallerWix.InstallerUrl
+    $InstallerWix['InstallerSha256'] = (Get-FileHash -Path $InstallerFileWix -Algorithm SHA256).Hash
+    $InstallerWix['AppsAndFeaturesEntries'] = @(
+      [ordered]@{
+        DisplayVersion = $InstallerFileWix | Read-ProductVersionFromMsi
+        ProductCode    = $InstallerWix['ProductCode'] = $InstallerFileWix | Read-ProductCodeFromMsi
+        UpgradeCode    = $InstallerFileWix | Read-UpgradeCodeFromMsi
+      }
+    )
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
