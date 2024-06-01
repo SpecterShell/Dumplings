aa9d2b32273c8d8ddb22f03858fed6258d12d1c3
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-06-01 18:25:47 +1200

    Task: Add OpenJS.NodeJS.LTS

---
+++ Tasks/OpenJS.NodeJS.LTS/Script.ps1
@@ -0,0 +1,100 @@
+$RepoOwner = 'nodejs'
+$RepoName = 'node'
+
+$Object1 = (Invoke-RestMethod -Uri 'https://nodejs.org/dist/index.json') |
+  Where-Object -FilterScript { $_.lts } |
+  # Sort-Object -Property { $_.version -replace '\d+', { $_.Value.PadLeft(20) } } |
+  Select-Object -First 1
+
+# Version
+$this.CurrentState.Version = $Object1.version -replace '^v'
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x86'
+  InstallerUrl = "https://nodejs.org/dist/v$($this.CurrentState.Version)/node-v$($this.CurrentState.Version)-x86.msi"
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = "https://nodejs.org/dist/v$($this.CurrentState.Version)/node-v$($this.CurrentState.Version)-x64.msi"
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'arm64'
+  InstallerUrl = "https://nodejs.org/dist/v$($this.CurrentState.Version)/node-v$($this.CurrentState.Version)-arm64.msi"
+}
+
+# ReleaseTime
+$this.CurrentState.ReleaseTime = $Object1.date | Get-Date -Format 'yyyy-MM-dd'
+
+# Documentations
+$this.CurrentState.Locale += [ordered]@{
+  Key   = 'Documentations'
+  Value = @(
+    [ordered]@{
+      DocumentLabel = 'Learn'
+      DocumentUrl   = 'https://nodejs.org/en/learn/'
+    }
+    [ordered]@{
+      DocumentLabel = 'Documentation'
+      DocumentUrl   = "https://nodejs.org/docs/v$($this.CurrentState.Version)/api/"
+    }
+  )
+}
+
+# Documentations (zh-CN)
+$this.CurrentState.Locale += [ordered]@{
+  Locale = 'zh-CN'
+  Key    = 'Documentations'
+  Value  = @(
+    [ordered]@{
+      DocumentLabel = '学习'
+      DocumentUrl   = 'https://nodejs.org/en/learn/'
+    }
+    [ordered]@{
+      DocumentLabel = '文档'
+      DocumentUrl   = "https://nodejs.org/docs/v$($this.CurrentState.Version)/api/"
+    }
+  )
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      $Object2 = Invoke-GitHubApi -Uri "https://api.github.com/repos/${RepoOwner}/${RepoName}/releases/tags/v$($this.CurrentState.Version)"
+
+      if (-not [string]::IsNullOrWhiteSpace($Object2.body)) {
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = ($Object2.body | ConvertFrom-Markdown).Html | ConvertFrom-Html | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+
+      # ReleaseNotesUrl
+      $this.CurrentState.Locale += [ordered]@{
+        Key   = 'ReleaseNotesUrl'
+        Value = "https://github.com/${RepoOwner}/${RepoName}/releases/tag/v$($this.CurrentState.Version)"
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+      # ReleaseNotesUrl
+      $this.CurrentState.Locale += [ordered]@{
+        Key   = 'ReleaseNotesUrl'
+        Value = $null
+      }
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
