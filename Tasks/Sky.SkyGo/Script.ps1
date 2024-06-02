2b755c7e5d127433f675c94d382e03260b6817af
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-06-02 14:05:49 +1200

    Task: Sky.SkyGo Add other locales

--- Tasks/Sky.SkyGo/Script.ps1
+++ Tasks/Sky.SkyGo/Script.ps1
@@ -1,5 +1,21 @@
 $Certificate = [System.Security.Cryptography.X509Certificates.X509Certificate2]::new([System.Convert]::FromBase64String($Global:DumplingsSecret.SkyGoPfx))
-$Object1 = Invoke-RestMethod -Uri $Global:DumplingsSecret.SkyGoUrl -Certificate $Certificate
+# United Kingdom
+$Object1 = Invoke-RestMethod -Uri ($Global:DumplingsSecret.SkyGoUrl -f 'gb') -Certificate $Certificate
+# German
+$Object2 = Invoke-RestMethod -Uri ($Global:DumplingsSecret.SkyGoUrl -f 'de') -Certificate $Certificate
+# Italy
+$Object3 = Invoke-RestMethod -Uri ($Global:DumplingsSecret.SkyGoUrl -f 'it') -Certificate $Certificate
+# Austria
+$Object4 = Invoke-RestMethod -Uri ($Global:DumplingsSecret.SkyGoUrl -f 'at') -Certificate $Certificate
+
+if (@(@($Object1, $Object2, $Object3, $Object4) | Sort-Object -Property { $_.platforms.win.version } -Unique).Count -gt 1) {
+  $this.Log('Distinct versions detected', 'Warning')
+  $this.Log("United Kingdom version: $($Object1.platforms.win.version)")
+  $this.Log("German version: $($Object2.platforms.win.version)")
+  $this.Log("Italy version: $($Object3.platforms.win.version)")
+  $this.Log("Austria version: $($Object4.platforms.win.version)")
+  $Identical = $false
+}
 
 # Version
 $this.CurrentState.Version = $Object1.platforms.win.version
@@ -7,6 +23,18 @@ $this.CurrentState.Version = $Object1.platforms.win.version
 # Installer
 $this.CurrentState.Installer += [ordered]@{
   InstallerUrl = $Object1.platforms.win.url
+}
+$this.CurrentState.Installer += [ordered]@{
+  InstallerLocale = 'de-DE'
+  InstallerUrl    = $Object2.platforms.win.url
+}
+$this.CurrentState.Installer += [ordered]@{
+  InstallerLocale = 'it-IT'
+  InstallerUrl    = $Object3.platforms.win.url
+}
+$this.CurrentState.Installer += [ordered]@{
+  InstallerLocale = 'de-AT'
+  InstallerUrl    = $Object4.platforms.win.url
 }
 
 switch -Regex ($this.Check()) {
@@ -17,7 +45,7 @@ switch -Regex ($this.Check()) {
   'Changed|Updated' {
     $this.Message()
   }
-  'Updated' {
+  ({ $_ -match 'Updated' -and $Identical }) {
     $this.Submit()
   }
 }
