99810efee4e4b508cb86770db6edae854e35775b
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-06-04 11:32:09 +1200

    Task: Add Foxit.PhantomPDF

--- Tasks/Foxit.PhantomPDF/Script.ps1
+++ Tasks/Foxit.PhantomPDF/Script.ps1
@@ -1,4 +1,4 @@
-$Object1 = Invoke-RestMethod -Uri 'https://www.foxit.com/portal/download/getdownloadform.html?retJson=1&platform=Windows&formId=trial-foxit-sign-suite-pro-individual'
+$Object1 = Invoke-RestMethod -Uri 'https://www.foxit.com/portal/download/getdownloadform.html?retJson=1&platform=Windows&formId=download-phantom-bussiness'
 
 # Version
 $this.CurrentState.Version = $Object1.package_info.version[0]
@@ -8,10 +8,10 @@ $this.CurrentState.Installer += [ordered]@{
   InstallerType = 'burn'
   InstallerUrl  = 'https://cdn01.foxitsoftware.com' + $Object1.package_info.down.Replace('_Website.exe', '.exe')
 }
-$this.CurrentState.Installer += $Installer = [ordered]@{
-  InstallerType = 'exe'
-  InstallerUrl  = 'https://cdn01.foxitsoftware.com' + $Object1.package_info.down.Replace('_Website.exe', '.exe').Replace('.exe', '_Prom.exe')
-}
+# $this.CurrentState.Installer += $Installer = [ordered]@{
+#   InstallerType = 'exe'
+#   InstallerUrl  = 'https://cdn01.foxitsoftware.com' + $Object1.package_info.down.Replace('_Website.exe', '.exe').Replace('.exe', '_Prom.exe')
+# }
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
@@ -44,7 +44,7 @@ switch -Regex ($this.Check()) {
     try {
       $Object2 = Invoke-WebRequest -Uri 'https://www.foxit.com/pdf-editor/version-history.html' | ConvertFrom-Html
 
-      $ReleaseNotesNode = $Object2.SelectSingleNode("//div[@id='tab-editor-suite-windows']//div[@id='version_$($this.CurrentState.Version)_detail']")
+      $ReleaseNotesNode = $Object2.SelectSingleNode("//div[@id='tab-editor-windows']//div[@id='version_$($this.CurrentState.Version)_detail']")
       if ($ReleaseNotesNode) {
         # ReleaseNotes (en-US)
         $this.CurrentState.Locale += [ordered]@{
