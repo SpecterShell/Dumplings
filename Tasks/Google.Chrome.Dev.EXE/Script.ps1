1c58515736fcee658ea14adf16d4e3efda1b5502
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-06-25 15:09:58 +1200

    Task: Split Google.Chrome.Dev

---
+++ Tasks/Google.Chrome.Dev.EXE/Script.ps1
@@ -0,0 +1,69 @@
+# x64
+$Object1 = Invoke-RestMethod -Uri 'https://update.googleapis.com/service/update2' -Method Post -Body @'
+<?xml version="1.0" encoding="UTF-8"?>
+<request protocol="3.0">
+  <os platform="win" version="10.0.22000" arch="x64" />
+  <app appid="{401C381F-E0DE-4B85-8BD8-3F3F14FBDA57}" version="" ap="x64">
+    <updatecheck />
+  </app>
+</request>
+'@
+
+# x86
+$Object2 = Invoke-RestMethod -Uri 'https://update.googleapis.com/service/update2' -Method Post -Body @'
+<?xml version="1.0" encoding="UTF-8"?>
+<request protocol="3.0">
+  <os platform="win" version="10.0.22000" arch="x86" />
+  <app appid="{401C381F-E0DE-4B85-8BD8-3F3F14FBDA57}" version="" ap="x86">
+    <updatecheck />
+  </app>
+</request>
+'@
+
+# arm64
+$Object3 = Invoke-RestMethod -Uri 'https://update.googleapis.com/service/update2' -Method Post -Body @'
+<?xml version="1.0" encoding="UTF-8"?>
+<request protocol="3.0">
+  <os platform="win" version="10.0.22000" arch="arm64" />
+  <app appid="{401C381F-E0DE-4B85-8BD8-3F3F14FBDA57}" version="" ap="arm64">
+    <updatecheck />
+  </app>
+</request>
+'@
+
+if (@(@($Object1, $Object2, $Object3) | Sort-Object -Property { $_.response.app.updatecheck.manifest.version } -Unique).Count -gt 1) {
+  $this.Log("x86 version: $($Object2.response.app.updatecheck.manifest.version)")
+  $this.Log("x64 version: $($Object1.response.app.updatecheck.manifest.version)")
+  $this.Log("arm64 version: $($Object3.response.app.updatecheck.manifest.version)")
+  throw 'Distinct versions detected'
+}
+
+# Version
+$this.CurrentState.Version = $Object1.response.app.updatecheck.manifest.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x86'
+  InstallerUrl = ($Object2.response.app.updatecheck.urls.url.codebase | Select-String -Pattern 'https://dl.google.com' -Raw -SimpleMatch) + $Object2.response.app.updatecheck.manifest.actions.action.Where({ $_.event -eq 'install' }, 'First')[0].run
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = ($Object1.response.app.updatecheck.urls.url.codebase | Select-String -Pattern 'https://dl.google.com' -Raw -SimpleMatch) + $Object1.response.app.updatecheck.manifest.actions.action.Where({ $_.event -eq 'install' }, 'First')[0].run
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'arm64'
+  InstallerUrl = ($Object3.response.app.updatecheck.urls.url.codebase | Select-String -Pattern 'https://dl.google.com' -Raw -SimpleMatch) + $Object3.response.app.updatecheck.manifest.actions.action.Where({ $_.event -eq 'install' }, 'First')[0].run
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
--- Tasks/Google.Chrome.Dev/Script.ps1
+++ Tasks/Google.Chrome.Dev/Script.ps1
@@ -1,69 +1,51 @@
-# x64
-$Object1 = Invoke-RestMethod -Uri 'https://update.googleapis.com/service/update2' -Method Post -Body @'
-<?xml version="1.0" encoding="UTF-8"?>
-<request protocol="3.0">
-  <os platform="win" version="10.0.22000" arch="x64" />
-  <app appid="{401C381F-E0DE-4B85-8BD8-3F3F14FBDA57}" version="" ap="x64">
-    <updatecheck />
-  </app>
-</request>
-'@
-
-# x86
-$Object2 = Invoke-RestMethod -Uri 'https://update.googleapis.com/service/update2' -Method Post -Body @'
-<?xml version="1.0" encoding="UTF-8"?>
-<request protocol="3.0">
-  <os platform="win" version="10.0.22000" arch="x86" />
-  <app appid="{401C381F-E0DE-4B85-8BD8-3F3F14FBDA57}" version="" ap="x86">
-    <updatecheck />
-  </app>
-</request>
-'@
-
-# arm64
-$Object3 = Invoke-RestMethod -Uri 'https://update.googleapis.com/service/update2' -Method Post -Body @'
-<?xml version="1.0" encoding="UTF-8"?>
-<request protocol="3.0">
-  <os platform="win" version="10.0.22000" arch="arm64" />
-  <app appid="{401C381F-E0DE-4B85-8BD8-3F3F14FBDA57}" version="" ap="arm64">
-    <updatecheck />
-  </app>
-</request>
-'@
-
-if (@(@($Object1, $Object2, $Object3) | Sort-Object -Property { $_.response.app.updatecheck.manifest.version } -Unique).Count -gt 1) {
-  $this.Log("x86 version: $($Object2.response.app.updatecheck.manifest.version)")
-  $this.Log("x64 version: $($Object1.response.app.updatecheck.manifest.version)")
-  $this.Log("arm64 version: $($Object3.response.app.updatecheck.manifest.version)")
-  throw 'Distinct versions detected'
+# Installer
+$this.CurrentState.Installer += $InstallerX86 = [ordered]@{
+  Architecture = 'x86'
+  InstallerUrl = 'https://dl.google.com/dl/chrome/install/dev/googlechromedevstandaloneenterprise.msi'
 }
+$Object1 = Invoke-WebRequest -Uri $InstallerX86.InstallerUrl -Method Head -Headers @{'If-Modified-Since' = $this.LastState['LastModifiedX86'] } -SkipHttpErrorCheck
+if ($Object1.StatusCode -eq 304) {
+  $this.Log("The version $($this.LastState.Version) from the last state is the latest, skip checking", 'Info')
+  return
+}
+
+$this.CurrentState.Installer += $InstallerX64 = [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = 'https://dl.google.com/dl/chrome/install/dev/googlechromedevstandaloneenterprise64.msi'
+}
+$Object2 = Invoke-WebRequest -Uri $InstallerX64.InstallerUrl -Method Head -Headers @{'If-Modified-Since' = $this.LastState['LastModifiedX64'] } -SkipHttpErrorCheck
+if ($Object2.StatusCode -eq 304) {
+  $this.Log("The version $($this.LastState.Version) from the last state is the latest, skip checking", 'Info')
+  return
+}
+
+$InstallerX64File = Get-TempFile -Uri $InstallerX64.InstallerUrl
+
+# InstallerSha256 + AppsAndFeaturesEntries
+$InstallerX64['InstallerSha256'] = (Get-FileHash -Path $InstallerX64File -Algorithm SHA256).Hash
+$InstallerX64['AppsAndFeaturesEntries'] = @(
+  [ordered]@{
+    ProductCode = $InstallerX64['ProductCode'] = $InstallerX64File | Read-ProductCodeFromMsi
+    UpgradeCode = $InstallerX64File | Read-UpgradeCodeFromMsi
+  }
+)
 
 # Version
-$this.CurrentState.Version = $Object1.response.app.updatecheck.manifest.version
+$this.CurrentState.Version = (Read-MsiSummaryValue -Path $InstallerX64File -Name Comments).Split(' ')[0].Trim()
 
-# Installer
-$this.CurrentState.Installer += [ordered]@{
-  Architecture = 'x86'
-  InstallerUrl = ($Object2.response.app.updatecheck.urls.url.codebase | Select-String -Pattern 'https://dl.google.com' -Raw -SimpleMatch) + $Object2.response.app.updatecheck.manifest.actions.action.Where({ $_.event -eq 'install' }, 'First')[0].run
-}
-$this.CurrentState.Installer += [ordered]@{
-  Architecture = 'x64'
-  InstallerUrl = ($Object1.response.app.updatecheck.urls.url.codebase | Select-String -Pattern 'https://dl.google.com' -Raw -SimpleMatch) + $Object1.response.app.updatecheck.manifest.actions.action.Where({ $_.event -eq 'install' }, 'First')[0].run
-}
-$this.CurrentState.Installer += [ordered]@{
-  Architecture = 'arm64'
-  InstallerUrl = ($Object3.response.app.updatecheck.urls.url.codebase | Select-String -Pattern 'https://dl.google.com' -Raw -SimpleMatch) + $Object3.response.app.updatecheck.manifest.actions.action.Where({ $_.event -eq 'install' }, 'First')[0].run
-}
+# LastModified
+$this.CurrentState.LastModifiedX86 = $Object1.Headers.'Last-Modified'[0]
+$this.CurrentState.LastModifiedX64 = $Object2.Headers.'Last-Modified'[0]
 
 switch -Regex ($this.Check()) {
-  'New|Changed|Updated' {
+  'New|Changed|Updated|Rollbacked' {
     $this.Print()
     $this.Write()
   }
-  'Changed|Updated' {
+  'Changed|Updated|Rollbacked' {
     $this.Message()
   }
-  'Updated' {
+  'Updated|Rollbacked' {
     $this.Submit()
   }
 }
