69a17fa47bde3b778b8f6fbfcc9dd9c34ea4082d
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-06-26 17:34:43 +1200

    Task: Add Obsidian.Obsidian

---
+++ Tasks/Obsidian.Obsidian/Script.ps1
@@ -0,0 +1,72 @@
+$RepoOwner = 'obsidianmd'
+$RepoName = 'obsidian-releases'
+
+$Object1 = Invoke-GitHubApi -Uri "https://api.github.com/repos/${RepoOwner}/${RepoName}/releases/latest"
+
+# Version
+$this.CurrentState.Version = $Object1.tag_name -creplace '^v'
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.assets.Where({ $_.name.EndsWith('.exe') }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.published_at.ToUniversalTime()
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    # ReleaseNotesUrl
+    $this.CurrentState.Locale += [ordered]@{
+      Key   = 'ReleaseNotesUrl'
+      Value = 'https://obsidian.md/changelog/'
+    }
+
+    try {
+      $Object2 = (Invoke-RestMethod -Uri 'https://obsidian.md/changelog.xml').Where({ $_.title.Contains($this.CurrentState.Version) -and $_.title.Contains('Desktop') }, 'First')
+
+      if ($Object2) {
+        # ReleaseNotesUrl
+        $this.CurrentState.Locale += [ordered]@{
+          Key   = 'ReleaseNotesUrl'
+          Value = $Object2[0].link.href
+        }
+      } else {
+        $this.Log("No ReleaseNotesUrl for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    try {
+      if ($Object2) {
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $Object2[0].content.'#text' | ConvertFrom-Html | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
