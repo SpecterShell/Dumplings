03dc03220da5a10c2f0e66cf390a5ffe0a333200
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-06-28 11:57:15 +1200

    Task: Add Google.AdsEditor

---
+++ Tasks/Google.AdsEditor/Script.ps1
@@ -0,0 +1,30 @@
+$Object1 = Invoke-RestMethod -Uri 'https://update.googleapis.com/service/update2' -Method Post -Body @'
+<?xml version="1.0" encoding="UTF-8"?>
+<request protocol="3.0">
+  <os platform="win" version="10.0.22000" arch="x64" />
+  <app appid="{F7A0263C-9459-4A49-BDD5-AA35E1C35151}">
+    <updatecheck />
+  </app>
+</request>
+'@
+
+# Version
+$this.CurrentState.Version = $Object1.response.app.updatecheck.manifest.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = ($Object1.response.app.updatecheck.urls.url.codebase | Select-String -Pattern 'https://dl.google.com' -Raw -SimpleMatch) + $Object1.response.app.updatecheck.manifest.packages.package.name
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
