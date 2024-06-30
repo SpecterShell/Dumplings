06d959d41f05e9110a7e39403b0fb39eb42f7b63
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-06-30 14:25:50 +1200

    Task: Add Apple.AppleApplicationSupport

---
+++ Tasks/Apple.AppleApplicationSupport.x64/Script.ps1
@@ -0,0 +1,25 @@
+$Object1 = $Global:DumplingsStorage.AppleProducts
+$Object2 = $Object1.Products.GetEnumerator().Where({ $_.Value.Contains('ServerMetadataURL') -and $_.Value.ServerMetadataURL.Contains('WINDOWS64_iCloud.smd') })[-1].Value
+$Object3 = Invoke-RestMethod -Uri $Object2.Distributions.English
+
+# Version
+$this.CurrentState.Version = $Object3.'installer-gui-script'.choice.'pkg-ref'.Where({ $_.id -eq 'AppleApplicationSupport64' }, 'First')[0].version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = $Object2.Packages.GetEnumerator().Where({ $_.URL.Contains('AppleApplicationSupport64.msi') }, 'First')[0].URL | ConvertTo-Https
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
---
+++ Tasks/Apple.AppleApplicationSupport.x86/Script.ps1
@@ -0,0 +1,25 @@
+$Object1 = $Global:DumplingsStorage.AppleProducts
+$Object2 = $Object1.Products.GetEnumerator().Where({ $_.Value.Contains('ServerMetadataURL') -and $_.Value.ServerMetadataURL.Contains('WINDOWS_iCloud.smd') })[-1].Value
+$Object3 = Invoke-RestMethod -Uri $Object2.Distributions.English
+
+# Version
+$this.CurrentState.Version = $Object3.'installer-gui-script'.choice.'pkg-ref'.Where({ $_.id -eq 'AppleApplicationSupport' }, 'First')[0].version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x86'
+  InstallerUrl = $Object2.Packages.GetEnumerator().Where({ $_.URL.Contains('AppleApplicationSupport.msi') }, 'First')[0].URL | ConvertTo-Https
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
