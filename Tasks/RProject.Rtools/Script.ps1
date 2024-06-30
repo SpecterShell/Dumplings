58c5bb5d2b2a91057f1b497ed64f5cd0f01a6941
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-06-30 16:23:17 +1200

    Task: RProject.Rtools Add arm64 installer

--- Tasks/RProject.Rtools/Script.ps1
+++ Tasks/RProject.Rtools/Script.ps1
@@ -2,23 +2,39 @@ $Prefix = 'https://cloud.r-project.org/bin/windows/Rtools/'
 
 $Object1 = Invoke-WebRequest -Uri $Prefix | ConvertFrom-Html
 
-$MinorVersion = [regex]::Match(
+$MainVersion = [regex]::Match(
   $Object1.SelectSingleNode('/html/body/table/tr[contains(./td[2]/text(), "R-release")]/td[1]/a').InnerText,
   'RTools ([\d\.]+)'
 ).Groups[1].Value
-$ShortVersion = $MinorVersion.Replace('.', '')
+$MainVersionShort = $MainVersion.Replace('.', '')
 
-$Object2 = (Invoke-WebRequest -Uri "${Prefix}rtools${ShortVersion}/files/").Content
-$VersionMatches = [regex]::Match($Object2, "(rtools${ShortVersion}-(\d+)-\d+\.exe)")
-
-# Version
-$this.CurrentState.Version = "${MinorVersion}.$($VersionMatches.Groups[2].Value)"
+$Object2 = Invoke-WebRequest -Uri "${Prefix}rtools${MainVersionShort}/files/"
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = "${Prefix}rtools${ShortVersion}/files/$($VersionMatches.Groups[1].Value)"
-  ProductCode  = "Rtools${ShortVersion}_is1"
+  Architecture = 'x64'
+  InstallerUrl = $InstallerUrlX64 = "${Prefix}rtools${MainVersionShort}/files/" + $Object2.Links.href.Where({ $_.EndsWith('.exe') -and -not $_.Contains('aarch64') }, 'First')
+  ProductCode  = "Rtools${MainVersionShort}_is1"
 }
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'arm64'
+  InstallerUrl = $InstallerUrlARM64 = "${Prefix}rtools${MainVersionShort}/files/" + $Object2.Links.href.Where({ $_.EndsWith('.exe') -and $_.Contains('aarch64') }, 'First')
+  ProductCode  = "Rtools${MainVersionShort}-aarch64_is1"
+}
+
+$BuildX64 = [regex]::Match($InstallerUrlX64, "rtools${MainVersionShort}-(\d+)").Groups[1].Value
+$BuildARM64 = [regex]::Match($InstallerUrlARM64, "rtools${MainVersionShort}-aarch64-(\d+)").Groups[1].Value
+$FullVersionX64 = "${MainVersion}.${BuildX64}"
+$FullVersionARM64 = "${MainVersion}.${BuildARM64}"
+
+if ($FullVersionX64 -ne $FullVersionARM64) {
+  $this.Log("x64 version: ${FullVersionX64}")
+  $this.Log("arm64 version: ${FullVersionARM64}")
+  throw 'Distinct versions detected'
+}
+
+# Version
+$this.CurrentState.Version = $FullVersionX64
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
@@ -26,7 +42,7 @@ switch -Regex ($this.Check()) {
       # ReleaseNotesUrl
       $this.CurrentState.Locale += [ordered]@{
         Key   = 'ReleaseNotesUrl'
-        Value = $ReleaseNotesUrl = "${Prefix}rtools${ShortVersion}/news.html"
+        Value = $ReleaseNotesUrl = "${Prefix}rtools${MainVersionShort}/news.html"
       }
     } catch {
       $_ | Out-Host
@@ -36,7 +52,7 @@ switch -Regex ($this.Check()) {
     try {
       $Object3 = Invoke-WebRequest -Uri $ReleaseNotesUrl | ConvertFrom-Html
 
-      $ReleaseNotesTitleNode = $Object3.SelectSingleNode("/html/body/h3[contains(text(), '$($VersionMatches.Groups[2].Value)')]")
+      $ReleaseNotesTitleNode = $Object3.SelectSingleNode("/html/body/h3[contains(text(), '${BuildX64}')]")
       if ($ReleaseNotesTitleNode) {
         # ReleaseNotes (en-US)
         $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node -and $Node.Name -ne 'h3'; $Node = $Node.NextSibling) { $Node }
@@ -60,7 +76,7 @@ switch -Regex ($this.Check()) {
     $this.Message()
   }
   'Updated' {
-    if ($this.LastState.Contains('Version') -and ($this.LastState.Version.Split('.')[0..1] -join '.') -eq $MinorVersion) {
+    if ($this.LastState.Contains('Version') -and ($this.LastState.Version.Split('.')[0..1] -join '.') -eq $MainVersion) {
       $this.Config.RemoveLastVersion = $true
     }
     $this.Submit()
