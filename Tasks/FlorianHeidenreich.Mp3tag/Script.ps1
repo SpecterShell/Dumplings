a9ee0a1c31010b5a2ab04e6a69ee0cf7871320ca
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-07-02 02:42:01 +1200

    Task: Add FlorianHeidenreich.Mp3tag

---
+++ Tasks/FlorianHeidenreich.Mp3tag/Script.ps1
@@ -0,0 +1,79 @@
+$Object1 = (Invoke-RestMethod -Uri 'https://download.mp3tag.de/versions.xml').Where({ $_.category -eq 'appcast' }, 'First')[0]
+
+# Version
+$this.CurrentState.Version = $Object1.enclosure.version
+$VersionShort = $this.CurrentState.Version.Replace('.', '')
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x86'
+  InstallerUrl = "https://download.mp3tag.de/mp3tagv${VersionShort}setup.exe"
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = "https://download.mp3tag.de/mp3tagv${VersionShort}-x64-setup.exe"
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.pubDate | Get-Date -AsUTC
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    try {
+      $Object2 = Invoke-WebRequest -Uri 'https://www.mp3tag.de/en/changelog.html' | ConvertFrom-Html
+      $Object3 = [System.IO.StringReader]::new($Object2.SelectSingleNode('//pre[@class="changes"]').InnerText)
+
+      while ($Object3.Peek() -ne -1) {
+        $String = $Object3.ReadLine()
+        if ($String.Contains("REL: VERSION $($this.CurrentState.Version)")) {
+          try {
+            # ReleaseTime
+            $this.CurrentState.ReleaseTime ??= [regex]::Match($String, '(\d{4}-\d{1,2}-\d{1,2})').Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
+          } catch {
+            $this.Log("No ReleaseTime for version $($this.CurrentState.Version)", 'Warning')
+          }
+          $null = $Object3.ReadLine()
+          break
+        }
+      }
+      if ($Object3.Peek() -ne -1) {
+        $ReleaseNotesObjects = [System.Collections.Generic.List[string]]::new()
+        while ($Object3.Peek() -ne -1) {
+          $String = $Object3.ReadLine()
+          if ($String -notmatch '^-+') {
+            $ReleaseNotesObjects.Add($String)
+          } else {
+            break
+          }
+        }
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = ($ReleaseNotesObjects | ForEach-Object -Process { $_.Substring(14) } | Format-Text) -replace '\n +', ' '
+        }
+      } else {
+        $this.Log("No ReleaseTime and ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+
+      $Object3.Close()
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
