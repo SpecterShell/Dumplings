3c44e682af9ed4a0d6a17bfd51f05b0367e33cb4
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-07-02 12:46:21 +1200

    Task: Add Rakuten.Viber

---
+++ Tasks/Rakuten.Viber/Script.ps1
@@ -0,0 +1,50 @@
+$Object1 = Invoke-RestMethod -Uri 'https://update.viber.com/viber/viber.php?function=ClientUpdate' -Method Post -Body @{
+  XMLDOC = @"
+<?xml version="1.0" encoding="UTF-8"?>
+<ClientUpdateRequest>
+    <ClientInfo>
+        <ViberVersion>$($this.LastState.Contains('Version') ? $this.LastState.Version : '22.4.1.0')</ViberVersion>
+        <Is64>1</Is64>
+    </ClientInfo>
+    <SystemInfo>
+        <System>Windows</System>
+        <SystemVersion>10.0.22000</SystemVersion>
+        <Arch>x86_64</Arch>
+    </SystemInfo>
+</ClientUpdateRequest>
+"@
+}
+
+if ($Object1.ClientUpdateResponse -is [string]) {
+  $this.Log("The version $($this.LastState.Version) from the last state is the latest, skip checking", 'Info')
+  return
+}
+
+# Version
+$this.CurrentState.Version = $Object1.ClientUpdateResponse.ViberVersion
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture  = 'x64'
+  InstallerType = 'burn'
+  InstallerUrl  = 'https://download.cdn.viber.com/desktop/windows/ViberSetup.exe'
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture  = 'x64'
+  InstallerType = 'wix'
+  InstallerUrl  = 'https://download.cdn.viber.com/desktop/windows/ViberSetup.msi'
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated|Rollbacked' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated|Rollbacked' {
+    $this.Message()
+  }
+  'Updated|Rollbacked' {
+    $this.CurrentState.Installer.ForEach({ $_.InstallerUrl += "?t=$(Get-Date -Format 'yyyyMMdd')" })
+    $this.Submit()
+  }
+}
