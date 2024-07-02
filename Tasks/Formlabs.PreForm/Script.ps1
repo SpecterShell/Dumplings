417cd786389d15f525841bb4ee4e23eaa449950a
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-07-02 21:00:37 +1200

    Task: Add Formlabs.PreForm

---
+++ Tasks/Formlabs.PreForm/Script.ps1
@@ -0,0 +1,32 @@
+# $Object1 = Invoke-RestMethod -Uri 'https://omaha.formlabs.com/service/update2' -Method Post -Body @"
+# <?xml version="1.0" encoding="UTF-8"?>
+# <request protocol="3.0" ismachine="1" sessionid="{$((New-Guid).Guid)}" requestid="{$((New-Guid).Guid)}">
+#   <os platform="win" version="10.0.22000" sp="" arch="x64" />
+#   <app appid="{61EFB8FF-2024-4F3C-BCCC-46A71D341A59}" version="" nextversion="" lang="" brand="" client="">
+#     <updatecheck />
+#   </app>
+# </request>
+# "@
+
+$Object1 = Invoke-WebRequest -Uri 'https://formlabs.com/download-preform-windows/'
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $InstallerUrl = $Object1.Links.Where({ $_.href.EndsWith('.exe') }, 'First')[0].href
+}
+
+# Version
+$this.CurrentState.Version = [regex]::Match($InstallerUrl, 'PreForm_win_([\d\.]+)_.+?_(\d+)[_\.]').Groups[@(1, 2)].Value | Join-String -Separator '.'
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
