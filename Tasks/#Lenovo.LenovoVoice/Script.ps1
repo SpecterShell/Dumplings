254f8c87da748db10b0e6df7d26450be79292e61
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-07-07 23:21:22 +1200

    Task: Add Lenovo.LenovoVoice

---
+++ Tasks/#Lenovo.LenovoVoice/Script.ps1
@@ -0,0 +1,45 @@
+$OldReleaseNotesPath = Join-Path $PSScriptRoot 'Releases.yaml'
+if (Test-Path -Path $OldReleaseNotesPath) {
+  $Global:DumplingsStorage['LenovoVoice'] = $OldReleaseNotes = Get-Content -Path $OldReleaseNotesPath -Raw | ConvertFrom-Yaml -Ordered
+} else {
+  $Global:DumplingsStorage['LenovoVoice'] = $OldReleaseNotes = [ordered]@{}
+}
+
+$Object1 = Invoke-WebRequest -Uri 'https://filedownload.lenovo.com/dtsw/LVA/LenovoVoice.xml' | Read-ResponseContent | ConvertFrom-Xml
+
+# Version
+$this.CurrentState.Version = $Object1.lva.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.lva.url
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated|Rollbacked' {
+    try {
+      # ReleaseNotes (zh-CN)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'zh-CN'
+        Key    = 'ReleaseNotes'
+        Value  = $ReleaseNotesCN = $Object1.lva.updateContent.item | Format-Text
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $OldReleaseNotes[$this.CurrentState.Version] = [ordered]@{
+      ReleaseNotesCN = $ReleaseNotesCN
+    }
+    if ($Global:DumplingsPreference.Contains('EnableWrite') -and $Global:DumplingsPreference.EnableWrite) {
+      $OldReleaseNotes | ConvertTo-Yaml -OutFile $OldReleaseNotesPath -Force
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated|Rollbacked' {
+    $this.Message()
+  }
+}
---
+++ Tasks/Lenovo.LenovoVoice/Script.ps1
@@ -0,0 +1,39 @@
+$EdgeDriver = Get-EdgeDriver
+$EdgeDriver.Navigate().GoToUrl('https://lva.lenovo.com.cn/')
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $InstallerUrl = $EdgeDriver.ExecuteScript('return downloadURl.lva', $null)
+}
+
+# Version
+$this.CurrentState.Version = $Version = [regex]::Match($InstallerUrl, '([\d\.]+)\.exe').Groups[1].Value
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      if ($Global:DumplingsStorage.Contains('LenovoVoice') -and $Global:DumplingsStorage['LenovoVoice'].Contains($Version)) {
+        # ReleaseNotes (zh-CN)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'zh-CN'
+          Key    = 'ReleaseNotes'
+          Value  = $Global:DumplingsStorage['LenovoVoice'].$Version.ReleaseNotesCN
+        }
+      } else {
+        $this.Log("No ReleaseNotes (zh-CN) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
