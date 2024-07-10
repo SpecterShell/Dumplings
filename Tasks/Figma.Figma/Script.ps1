659f4cfb93bd278e4a4b817b61302544f39ae535
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-07-10 20:22:10 +1200

    Task: Figma.Figma Add ARM64 installer

--- Tasks/Figma.Figma/Script.ps1
+++ Tasks/Figma.Figma/Script.ps1
@@ -5,7 +5,12 @@ $this.CurrentState.Version = [regex]::Match($Object1.Split(' ')[1], '-(\d+\.\d+\
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
   InstallerUrl = "https://desktop.figma.com/win/build/Figma-$($this.CurrentState.Version).exe"
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'arm64'
+  InstallerUrl = "https://desktop.figma.com/win-arm/build/Figma-$($this.CurrentState.Version).exe"
 }
 
 switch -Regex ($this.Check()) {
