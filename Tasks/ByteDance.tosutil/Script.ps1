ca5f1fbadf2490f3b22cadf52a8f15ddb31b8868
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-07-13 19:41:22 +1200

    Task: Add ByteDance.tosutil

---
+++ Tasks/ByteDance.tosutil/Script.ps1
@@ -0,0 +1,73 @@
+function Get-Version {
+  $this.CurrentState.Version = [regex]::Match((& $InstallerFile version), 'version: v([\d\.]+)').Groups[1].Value
+}
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl    = $InstallerUrl = 'https://tos-tools.tos-cn-beijing.volces.com/windows/tosutil'
+  InstallerSha256 = (Invoke-RestMethod -Uri "${InstallerUrl}.sha256sum").Split()[0].ToUpper()
+}
+
+# Case 0: Force submit the manifest
+if ($Global:DumplingsPreference.Contains('Force')) {
+  $this.Log('Skip checking states', 'Info')
+
+  $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl | Rename-Item -NewName { "${_}.exe" } -PassThru | Select-Object -ExpandProperty 'FullName'
+  # Version
+  Get-Version
+
+  $this.Print()
+  $this.Write()
+  $this.Message()
+  $this.Submit()
+  return
+}
+
+# Case 1: The task is newly created
+if ($this.Status.Contains('New')) {
+  $this.Log('New task', 'Info')
+
+  $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl | Rename-Item -NewName { "${_}.exe" } -PassThru | Select-Object -ExpandProperty 'FullName'
+  # Version
+  Get-Version
+
+  $this.Print()
+  $this.Write()
+  return
+}
+
+# Case 2: The SHA256 was not updated
+if ($this.CurrentState.Installer[0].InstallerUrl -eq $this.LastState.Installer[0].InstallerUrl) {
+  $this.Log("The version $($this.LastState.Version) from the last state is the latest", 'Info')
+  return
+}
+
+$InstallerFile = Get-TempFile -Uri $InstallerUrl | Rename-Item -NewName { "${_}.exe" } -PassThru | Select-Object -ExpandProperty 'FullName'
+# Version
+Get-Version
+
+# Case 3: The installer file has an invalid version
+if ([string]::IsNullOrWhiteSpace($this.CurrentState.Version)) {
+  throw 'The current state has an invalid version'
+}
+
+switch -Regex ($this.Check()) {
+  # Case 5: The SHA256 and the version were updated
+  'Updated|Rollbacked' {
+    $this.Print()
+    $this.Write()
+    $this.Message()
+    $this.Submit()
+  }
+  # Case 4: The SHA256 was updated, but the version wasn't
+  # The installer might be updated without changing the version (e.g., virus database update)
+  # Force submit the manifest even if neither the version nor the installer has changed
+  Default {
+    $this.Log('The SHA256 was changed, but the version is the same', 'Info')
+    $this.Config.IgnorePRCheck = $true
+    $this.Print()
+    $this.Write()
+    $this.Message()
+    $this.Submit()
+  }
+}
