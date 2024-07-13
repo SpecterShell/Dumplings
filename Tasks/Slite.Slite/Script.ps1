4bc22cf23d4d9b7f373fed0ee8567105bcd72e8f
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-07-13 14:44:45 +1200

    Task: Add Slite.Slite

---
+++ Tasks/Slite.Slite/Script.ps1
@@ -0,0 +1,16 @@
+$Prefix = 'https://download.todesktop.com/20062929x31pwfi/'
+
+$this.CurrentState = Invoke-RestMethod -Uri "${Prefix}latest.yml?noCache=$(Get-Random)" | ConvertFrom-Yaml | ConvertFrom-ElectronUpdater -Prefix $Prefix -Locale 'en-US'
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
