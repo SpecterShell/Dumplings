84bbdf0ab096ffc09024c6c8889b38b785d90d35
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-07-13 12:42:33 +1200

    Task: Morgen.Morgen Add release time

--- Tasks/Morgen.Morgen/Script.ps1
+++ Tasks/Morgen.Morgen/Script.ps1
@@ -17,6 +17,14 @@ $this.CurrentState.Installer += [ordered]@{
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.releaseDate | Get-Date -AsUTC
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
     $this.Print()
     $this.Write()
   }
