8c70f64e139062d8ec80a35e52ed46139f1a4589
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-07-19 11:07:38 +1200

    Task: Telerik.Fiddler.Everywhere Add release notes

--- Tasks/Telerik.Fiddler.Everywhere/Script.ps1
+++ Tasks/Telerik.Fiddler.Everywhere/Script.ps1
@@ -4,6 +4,25 @@ $this.CurrentState = Invoke-RestMethod -Uri "${Prefix}latest.yml?noCache=$(Get-R
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
+    try {
+      $Object2 = (Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/telerik/fiddler-everywhere-docs/master/release-notes/release-notes.json').Where({ $_.version -eq $this.CurrentState.Version })
+
+      if ($Object2) {
+        # ReleaseTime
+        $this.CurrentState.ReleaseTime ??= $Object2[0].date | Get-Date -Format 'yyyy-MM-dd'
+
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $Object2[0].items | ForEach-Object -Process { "$($_.type)`n$($_.entries.title | ConvertTo-UnorderedList)" } | Join-String -Separator "`n`n" | Format-Text
+        }
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
     $this.Print()
     $this.Write()
   }
