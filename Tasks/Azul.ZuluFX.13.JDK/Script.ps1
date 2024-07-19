cfe5f9f9fcedd480eecd82ffd95e450dd99a359a
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-07-19 23:14:14 +1200

    Task: Azul.ZuluFX.13.JDK Fix WinGet identifier and reset

--- Tasks/Azul.ZuluFX.13.JDK/Script.ps1
+++ Tasks/Azul.ZuluFX.13.JDK/Script.ps1
@@ -1,4 +1,4 @@
-$Object1 = $Global:DumplingsStorage.AzulZuluBuilds | Where-Object -FilterScript { $_.distro_version[0] -eq 11 -and $_.name.Contains('jdk') -and $_.name.Contains('fx') -and $_.name.Contains('x64') } | Sort-Object -Property { $_.distro_version.ForEach({ $_.ToString().PadLeft(20) }) -join '.' } -Bottom 1
+$Object1 = $Global:DumplingsStorage.AzulZuluBuilds | Where-Object -FilterScript { $_.distro_version[0] -eq 13 -and $_.name.Contains('jdk') -and $_.name.Contains('fx') -and $_.name.Contains('x64') } | Sort-Object -Property { $_.distro_version.ForEach({ $_.ToString().PadLeft(20) }) -join '.' } -Bottom 1
 
 # Version
 $this.CurrentState.Version = $Object1.distro_version -join '.'
