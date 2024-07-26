3ff15919ead5b30d056a8ea26f7a07f800fc95e0
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-07-27 00:50:34 +1200

    Task: Hellofont.Hellofont Parse version from installer URL

--- Tasks/Hellofont.Hellofont/Script.ps1
+++ Tasks/Hellofont.Hellofont/Script.ps1
@@ -1,42 +1,53 @@
-$Object1 = Invoke-RestMethod -Uri 'https://back2.hellofont.cn/ziyou/ClientManagement/api/ClientVersion/LatestClientVersionItem' -Method Post -Body @{ PlatformId = 0 }
-$Object2 = Invoke-RestMethod -Uri 'https://www.hellofont.cn/api/system/client-channel?platform=windows&channel=official'
-
-# Version
-$this.CurrentState.Version = $Object1.Version
+$Object1 = Invoke-RestMethod -Uri 'https://www.hellofont.cn/api/system/client-channel?platform=windows&channel=official'
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = $InstallerUrl = $Object2.link | ConvertTo-UnescapedUri
+  InstallerUrl = $InstallerUrl = $Object1.link | ConvertTo-UnescapedUri
 }
 
-if (-not $InstallerUrl.Contains($this.CurrentState.Version)) {
-  throw "Task $($this.Name): The InstallerUrl`n${InstallerUrl}`ndoesn't contain version $($this.CurrentState.Version)"
-}
+# Version
+$this.CurrentState.Version = [regex]::Match($InstallerUrl, '(\d+(?:\.\d+){2,})').Groups[1].Value
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
     try {
-      # ReleaseTime
-      $this.CurrentState.ReleaseTime = [datetime]::ParseExact(
-        $Object1.TimeStamp,
-        'yyyyMMddHHmmssfff',
-        $null
-      ) | ConvertTo-UtcDateTime -Id 'China Standard Time'
+      $ReleaseNotesObject = $null
 
-      $ReleaseNotesList = @()
-      if ($Object1.News) {
-        $ReleaseNotesList += '新增功能：'
-        $ReleaseNotesList += $Object1.News | ConvertTo-UnorderedList | Format-Text
+      $Object2 = (Invoke-RestMethod -Uri 'https://www.hellofont.cn/api/system/client-version').Where({ $_.PlatformId -eq 0 }, 'First')[0]
+      if ($Object2.Version -eq $this.CurrentState.Version) {
+        $ReleaseNotesObject = $Object2
+      } else {
+        $Object3 = (Invoke-RestMethod -Uri 'https://back2.hellofont.cn/ziyou/ClientManagement/api/ClientVersion/HistoryClientVersionList' -Method Post -Body @{ PlatformId = '0' }).List.Where({ $_.Version -eq $this.CurrentState.Version }, 'First')
+        if ($Object3) {
+          $ReleaseNotesObject = $Object3[0]
+        } else {
+          $this.Log("No release notes for version $($this.CurrentState.Version)", 'Warning')
+        }
       }
-      if ($Object1.Bugs) {
-        $ReleaseNotesList += '问题修复：'
-        $ReleaseNotesList += $Object1.Bugs | ConvertTo-UnorderedList | Format-Text
-      }
-      # ReleaseNotes (zh-CN)
-      $this.CurrentState.Locale += [ordered]@{
-        Locale = 'zh-CN'
-        Key    = 'ReleaseNotes'
-        Value  = $ReleaseNotesList | Format-Text
+
+      if ($ReleaseNotesObject) {
+        # ReleaseTime
+        $this.CurrentState.ReleaseTime = [datetime]::ParseExact(
+          $ReleaseNotesObject.TimeStamp,
+          'yyyyMMddHHmmssfff',
+          $null
+        ) | ConvertTo-UtcDateTime -Id 'China Standard Time'
+
+        $ReleaseNotesList = @()
+        if ($ReleaseNotesObject.News) {
+          $ReleaseNotesList += '新增功能：'
+          $ReleaseNotesList += $ReleaseNotesObject.News | ConvertTo-UnorderedList | Format-Text
+        }
+        if ($ReleaseNotesObject.Bugs) {
+          $ReleaseNotesList += '问题修复：'
+          $ReleaseNotesList += $ReleaseNotesObject.Bugs | ConvertTo-UnorderedList | Format-Text
+        }
+        # ReleaseNotes (zh-CN)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'zh-CN'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesList | Format-Text
+        }
       }
     } catch {
       $_ | Out-Host
