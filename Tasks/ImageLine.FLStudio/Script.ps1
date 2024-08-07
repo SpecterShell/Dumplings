326bad38000e094503f054cdb82137cd36886141
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-08-07 21:54:41 +1000

    Task: ImageLine.FLStudio Use HTTP for installer URL and remove release notes

--- Tasks/ImageLine.FLStudio/Script.ps1
+++ Tasks/ImageLine.FLStudio/Script.ps1
@@ -4,11 +4,11 @@ $Object1 = (Invoke-RestMethod -Uri 'http://support.image-line.com/flstudiorss/pr
 
 # Version
 $this.CurrentState.Version = $Object1.version
-$ShortVersion = $this.CurrentState.Version.Split('.')[0..2] -join '.' -replace '\.0+$'
+# $ShortVersion = $this.CurrentState.Version.Split('.')[0..2] -join '.' -replace '\.0+$'
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = $Object1.download_url.Replace('demodownload.image-line.com', 'install.image-line.com')
+  InstallerUrl = $Object1.download_url.Replace('demodownload.image-line.com', 'install.image-line.com') -replace '^https://', 'http://'
 }
 
 switch -Regex ($this.Check()) {
@@ -21,25 +21,25 @@ switch -Regex ($this.Check()) {
       $this.Log($_, 'Warning')
     }
 
-    try {
-      $Object2 = Invoke-WebRequest -Uri 'http://www.image-line.com/fl-studio-learning/fl-studio-online-manual/html/WhatsNew.htm' -UserAgent $UserAgent | ConvertFrom-Html
+    # try {
+    #   $Object2 = Invoke-WebRequest -Uri 'http://www.image-line.com/fl-studio-learning/fl-studio-online-manual/html/WhatsNew.htm' -UserAgent $UserAgent | ConvertFrom-Html
 
-      $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//td[@id='rightcol']/h3[contains(text(), '${ShortVersion}')]")
-      if ($ReleaseNotesTitleNode) {
-        $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node -and $Node.Name -ne 'h3'; $Node = $Node.NextSibling) { $Node }
-        # ReleaseNotes (en-US)
-        $this.CurrentState.Locale += [ordered]@{
-          Locale = 'en-US'
-          Key    = 'ReleaseNotes'
-          Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
-        }
-      } else {
-        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
-      }
-    } catch {
-      $_ | Out-Host
-      $this.Log($_, 'Warning')
-    }
+    #   $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//td[@id='rightcol']/h3[contains(text(), '${ShortVersion}')]")
+    #   if ($ReleaseNotesTitleNode) {
+    #     $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node -and $Node.Name -ne 'h3'; $Node = $Node.NextSibling) { $Node }
+    #     # ReleaseNotes (en-US)
+    #     $this.CurrentState.Locale += [ordered]@{
+    #       Locale = 'en-US'
+    #       Key    = 'ReleaseNotes'
+    #       Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
+    #     }
+    #   } else {
+    #     $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+    #   }
+    # } catch {
+    #   $_ | Out-Host
+    #   $this.Log($_, 'Warning')
+    # }
 
     $this.Print()
     $this.Write()
