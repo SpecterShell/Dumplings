646ad084b90099ce3bb3de719ba570ff5a54b1e2
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-08-26 19:06:38 +0800

    Task: Add Famatech.Radmin

---
+++ Tasks/Famatech.Radmin.Server/Script.ps1
@@ -0,0 +1,22 @@
+$Object1 = Invoke-WebRequest -Uri 'https://www.radmin.com/download/' | ConvertFrom-Html
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $InstallerUrl = $Object1.SelectSingleNode('//div[@class="vBlockContent" and contains(./div[@class="fl"], "Radmin Server")]/a').Attributes['href'].Value.Replace('_EN', '_ML')
+}
+
+# Version
+$this.CurrentState.Version = [regex]::Match($InstallerUrl, '(\d+(?:\.\d+){2,})').Groups[1].Value
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
