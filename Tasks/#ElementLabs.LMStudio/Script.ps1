72817bf590936c55f57ba5747a016c35075e75a5
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-08-30 20:11:48 +0800

    Task: Add ElementLabs.LMStudio

---
+++ Tasks/#ElementLabs.LMStudio/Script.ps1
@@ -0,0 +1,40 @@
+$OldReleaseNotesPath = Join-Path $PSScriptRoot 'Releases.yaml'
+if (Test-Path -Path $OldReleaseNotesPath) {
+  $Global:DumplingsStorage['LMStudio'] = $OldReleaseNotes = Get-Content -Path $OldReleaseNotesPath -Raw | ConvertFrom-Yaml -Ordered
+} else {
+  $Global:DumplingsStorage['LMStudio'] = $OldReleaseNotes = [ordered]@{}
+}
+
+$Object1 = Invoke-RestMethod -Uri "https://versions-prod.lmstudio.ai/win32/x86/$($this.LastState.Contains('Version') ? $this.LastState.Version : '0.3.2')"
+
+# Version
+$this.CurrentState.Version = $Object1.version
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated|Rollbacked' {
+    try {
+      # ReleaseNotes (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotes'
+        Value  = $ReleaseNotes = ($Object1.releaseNotes | ConvertFrom-Markdown).Html | ConvertFrom-Html | Get-TextContent | Format-Text
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $OldReleaseNotes[$this.CurrentState.Version] = [ordered]@{
+      ReleaseNotes = $ReleaseNotes
+    }
+    if ($Global:DumplingsPreference.Contains('EnableWrite') -and $Global:DumplingsPreference.EnableWrite) {
+      $OldReleaseNotes | ConvertTo-Yaml -OutFile $OldReleaseNotesPath -Force
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated|Rollbacked' {
+    $this.Message()
+  }
+}
---
+++ Tasks/ElementLabs.LMStudio/Script.ps1
@@ -0,0 +1,39 @@
+$EdgeDriver = Get-EdgeDriver -Headless
+$EdgeDriver.Navigate().GoToUrl('https://lmstudio.ai/')
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $InstallerUrl = $EdgeDriver.FindElement([OpenQA.Selenium.By]::XPath('//a[contains(@href, ".exe")]')).GetAttribute('href')
+}
+
+# Version
+$this.CurrentState.Version = $Version = [regex]::Match($InstallerUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      if ($Global:DumplingsStorage.Contains('LMStudio') -and $Global:DumplingsStorage['LMStudio'].Contains($Version)) {
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $Global:DumplingsStorage['LMStudio'].$Version.ReleaseNotes
+        }
+      } else {
+        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
