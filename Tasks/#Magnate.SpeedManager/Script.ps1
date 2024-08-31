e14b55b406dca92ebe04cd24ac9fa9516b892848
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-09-01 00:18:57 +0800

    Task: Add Magnate.SpeedManager

---
+++ Tasks/#Magnate.SpeedManager/Script.ps1
@@ -0,0 +1,40 @@
+$OldReleaseNotesPath = Join-Path $PSScriptRoot 'Releases.yaml'
+if (Test-Path -Path $OldReleaseNotesPath) {
+  $Global:DumplingsStorage['SpeedManager'] = $OldReleaseNotes = Get-Content -Path $OldReleaseNotesPath -Raw | ConvertFrom-Yaml -Ordered
+} else {
+  $Global:DumplingsStorage['SpeedManager'] = $OldReleaseNotes = [ordered]@{}
+}
+
+$Object1 = Invoke-WebRequest -Uri 'https://file2.speedtest.cn/pc/win64/version.json' | Read-ResponseContent | ConvertFrom-Json
+
+# Version
+$this.CurrentState.Version = $Object1.version
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated|Rollbacked' {
+    try {
+      # ReleaseNotes (zh-CN)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'zh-CN'
+        Key    = 'ReleaseNotes'
+        Value  = $ReleaseNotesCN = $Object1.describe | ConvertFrom-Html | Get-TextContent | Format-Text
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $OldReleaseNotes[$this.CurrentState.Version] = [ordered]@{
+      ReleaseNotesCN = $ReleaseNotesCN
+    }
+    if ($Global:DumplingsPreference.Contains('EnableWrite') -and $Global:DumplingsPreference.EnableWrite) {
+      $OldReleaseNotes | ConvertTo-Yaml -OutFile $OldReleaseNotesPath -Force
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated|Rollbacked' {
+    $this.Message()
+  }
+}
---
+++ Tasks/Magnate.SpeedManager/Script.ps1
@@ -0,0 +1,32 @@
+$Prefix = 'https://file2.speedtest.cn/pc/win64/'
+
+$this.CurrentState = Invoke-RestMethod -Uri "${Prefix}latest.yml?noCache=$(Get-Random)" | ConvertFrom-Yaml | ConvertFrom-ElectronUpdater -Prefix $Prefix -Locale 'zh-CN'
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      if ($Global:DumplingsStorage.Contains('SpeedManager') -and $Global:DumplingsStorage.SpeedManager.Contains($this.CurrentState.Version)) {
+        # ReleaseNotes (zh-CN)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'zh-CN'
+          Key    = 'ReleaseNotes'
+          Value  = $Global:DumplingsStorage.SpeedManager[$this.CurrentState.Version].ReleaseNotesCN
+        }
+      } else {
+        $this.Log("No ReleaseNotes (zh-CN) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
