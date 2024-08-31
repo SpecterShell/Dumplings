b884c10501b50eb803960b4a67fb6388b8920c53
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-08-31 19:31:13 +0800

    Task: Add PeterPawlowski.foobar2000

---
+++ Tasks/PeterPawlowski.foobar2000/Script.ps1
@@ -0,0 +1,52 @@
+$Object1 = Invoke-WebRequest -Uri 'https://www.foobar2000.org/download'
+
+# Version
+$this.CurrentState.Version = [regex]::Match($Object1.Content, 'v(\d+(?:\.\d+){1,})(?!\s+preview)').Groups[1].Value
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x86'
+  InstallerUrl = "https://www.foobar2000.org/getfile/foobar2000_v$($this.CurrentState.Version).exe"
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = "https://www.foobar2000.org/getfile/foobar2000-x64_v$($this.CurrentState.Version).exe"
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'arm64'
+  InstallerUrl = "https://www.foobar2000.org/getfile/foobar2000-arm64ec_v$($this.CurrentState.Version).exe"
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      $Object2 = Invoke-WebRequest -Uri 'https://www.foobar2000.org/changelog' | ConvertFrom-Html
+
+      $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//div[contains(@class, 'content')]/div/h2[contains(text(), '$($this.CurrentState.Version)')]")
+      if ($ReleaseNotesTitleNode) {
+        # ReleaseTime
+        $this.CurrentState.ReleaseTime = [regex]::Match($ReleaseNotesTitleNode.InnerText, '(\d{4}-\d{1,2}-\d{1,2})').Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
+
+        $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node -and $Node.Name -ne 'h2'; $Node = $Node.NextSibling) { $Node }
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
+        }
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
