25a56a55437908fd26f88af04b23f0be85b2e91a
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-09-01 17:19:33 +0800

    Task: Add THS.Futures

---
+++ Tasks/THS.Futures/Script.ps1
@@ -0,0 +1,20 @@
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $InstallerUrl = Get-RedirectedUrl1st -Uri 'https://download.10jqka.com.cn/index/download/id/580/'
+}
+
+# Version
+$this.CurrentState.Version = [regex]::Match($InstallerUrl, 'gw_?(\d+(?:\.\d+){2,})').Groups[1].Value
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
