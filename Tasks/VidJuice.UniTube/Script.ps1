d27f158a963b51175e47d389f2bf4b9233747edf
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-09-01 17:19:23 +0800

    Task: Add VidJuice.UniTube

---
+++ Tasks/VidJuice.UniTube/Script.ps1
@@ -0,0 +1,42 @@
+$Object1 = Invoke-RestMethod -Uri 'https://dl.vidjuice.com/upgrade.json'
+
+# Version
+$this.CurrentState.Version = $Object1.win.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x86'
+  InstallerUrl = $Object1.win.x86url
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = $Object1.win.x64url
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.win.date | Get-Date -Format 'yyyy-MM-dd'
+
+      # ReleaseNotes (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotes'
+        Value  = $Object1.win.change | Format-Text
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
