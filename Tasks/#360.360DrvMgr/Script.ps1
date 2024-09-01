aa62a519a3286c47a277d9c37e28c41d6777ab1b
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-09-01 23:06:30 +0800

    Task: Add 360.360DrvMgr

---
+++ Tasks/#360.360DrvMgr/Script.ps1
@@ -0,0 +1,46 @@
+$OldReleaseNotesPath = Join-Path $PSScriptRoot 'Releases.yaml'
+if (Test-Path -Path $OldReleaseNotesPath) {
+  $Global:DumplingsStorage['360DrvMgr'] = $OldReleaseNotes = Get-Content -Path $OldReleaseNotesPath -Raw | ConvertFrom-Yaml -Ordered
+} else {
+  $Global:DumplingsStorage['360DrvMgr'] = $OldReleaseNotes = [ordered]@{}
+}
+
+$Path = Get-TempFile -Uri 'https://ini.update.360safe.com/v3/360drvmgrupd.cab'
+expand.exe -R $Path | Out-Host
+$Object1 = Join-Path $Path '..' '360drvmgrupd.ini' -Resolve | Get-Item | Get-Content -Raw -Encoding 'gb18030' | ConvertFrom-Ini
+
+# Version
+$this.CurrentState.Version = $Object1.'360App1'.ver
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated|Rollbacked' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $ReleaseTime = $Object1.'360App1'.date | Get-Date -Format 'yyyy-MM-dd'
+
+      # ReleaseNotes (zh-CN)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'zh-CN'
+        Key    = 'ReleaseNotes'
+        Value  = $ReleaseNotesCN = $Object1.'360App1'.changelog | Format-Text
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $OldReleaseNotes[$this.CurrentState.Version] = [ordered]@{
+      ReleaseTime    = $ReleaseTime
+      ReleaseNotesCN = $ReleaseNotesCN
+    }
+    if ($Global:DumplingsPreference.Contains('EnableWrite') -and $Global:DumplingsPreference.EnableWrite) {
+      $OldReleaseNotes | ConvertTo-Yaml -OutFile $OldReleaseNotesPath -Force
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated|Rollbacked' {
+    $this.Message()
+  }
+}
