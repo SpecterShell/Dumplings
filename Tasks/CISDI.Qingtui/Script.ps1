7c014bafadc1579eed112d9d29910733e7e02e8d
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-09-02 23:22:34 +0800

    Task: Add CISDI.Qingtui

---
+++ Tasks/CISDI.Qingtui/Script.ps1
@@ -0,0 +1,43 @@
+$Object1 = Invoke-RestMethod -Uri 'https://web.qingtui.com/api/client/web/update' -Headers @{
+  CLIENT = 'QingTui;Windows NT 10.0, WOW64;PC;0'
+}
+
+# Version
+$this.CurrentState.Version = $Object1.data.webVersions.Where({ $_.type -eq 'winpc' })[0].lastestVer
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.data.webVersions.Where({ $_.type -eq 'winpc' })[0].lastestVerUrl
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseNotes (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotes'
+        Value  = $Object1.data.webVersions.Where({ $_.type -eq 'winpc' })[0].lastestVerNote | Format-Text
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+
+    # InstallerSha256
+    $this.CurrentState.Installer[0]['InstallerSha256'] = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
+    # RealVersion
+    $this.CurrentState.RealVersion = $InstallerFile | Read-ProductVersionFromExe
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
