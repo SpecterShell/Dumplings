5bb72d2beed1e53e26ecd00cf433c2d09b3f27a2
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-09-02 13:54:57 +0800

    Task: Add Wondershare.Ani3D, Wondershare.AniSmall, Wondershare.Virbo

---
+++ Tasks/Wondershare.Ani3D/Script.ps1
@@ -0,0 +1,14 @@
+$this.CurrentState = $Global:DumplingsStorage.WondershareUpgradeInfo['14554']
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
