a6f9a9d36628593bd79a39b0659ccdd91a0c9892
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-09-02 10:49:17 +0800

    Task: 360.360SysReset Switch to webpage source

--- Tasks/360.360SysReset/Script.ps1
+++ Tasks/360.360SysReset/Script.ps1
@@ -1,113 +1,48 @@
-function Get-ReleaseNotes {
-  try {
-    $Object2 = Invoke-WebRequest -Uri 'https://renew.360.cn/' | ConvertFrom-Html
+$Object1 = Invoke-WebRequest -Uri 'https://renew.360.cn/' | ConvertFrom-Html
 
-    $ReleaseNotesNode = $Object2.SelectSingleNode("//div[@class='log-list']/dl[contains(./dd/h3, '$($this.CurrentState.Version)')]")
-    if ($ReleaseNotesNode) {
-      if ($ReleaseNotesNode.SelectSingleNode('./dt').InnerText -match '(\d{4}-\d{1,2}-\d{1,2})') {
-        # ReleaseTime
-        $this.CurrentState.ReleaseTime = $Matches[1] | Get-Date -Format 'yyyy-MM-dd'
-      }
-
-      # ReleaseNotes (en-US)
-      $this.CurrentState.Locale += [ordered]@{
-        Locale = 'en-US'
-        Key    = 'ReleaseNotes'
-        Value  = $ReleaseNotesNode.SelectNodes('./dd/h3/following-sibling::node()[not(contains(@style, "display:none"))]') | Get-TextContent | Format-Text
-      }
-    }
-  } catch {
-    $_ | Out-Host
-    $this.Log($_, 'Warning')
-  }
-}
+# Version
+$this.CurrentState.Version = [regex]::Match($Object1.SelectSingleNode('//div[@id="txt1"]//span[contains(text(), "最新版本")]/following-sibling::span[1]').InnerText, '(\d+(?:\.\d+){3,})').Groups[1].Value
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
   InstallerUrl = 'https://down.360safe.com/sysreset/360SRInst.exe'
 }
 
-$Object1 = Invoke-WebRequest -Uri $this.CurrentState.Installer[0].InstallerUrl -Method Head
-# Last Modified
-$this.CurrentState.LastModified = $Object1.Headers.'Last-Modified'[0]
-
-# Case 0: Force submit the manifest
-if ($Global:DumplingsPreference.Contains('Force')) {
-  $this.Log('Skip checking states', 'Info')
-
-  $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
-  # Version
-  $this.CurrentState.Version = $InstallerFile | Read-ProductVersionFromExe
-  # InstallerSha256
-  $this.CurrentState.Installer[0]['InstallerSha256'] = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
-
-  Get-ReleaseNotes
-
-  $this.Print()
-  $this.Write()
-  $this.Message()
-  $this.Submit()
-  return
-}
-
-# Case 1: The task is newly created
-if ($this.Status.Contains('New')) {
-  $this.Log('New task', 'Info')
-
-  $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
-  # Version
-  $this.CurrentState.Version = $InstallerFile | Read-ProductVersionFromExe
-  # InstallerSha256
-  $this.CurrentState.Installer[0]['InstallerSha256'] = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
-
-  Get-ReleaseNotes
-
-  $this.Print()
-  $this.Write()
-  return
-}
-
-# Case 2: The Last Modified was not updated
-if ($this.CurrentState.LastModified -eq $this.LastState.LastModified) {
-  $this.Log("The version $($this.LastState.Version) from the last state is the latest", 'Info')
-  return
-}
-
-$InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
-# Version
-$this.CurrentState.Version = $InstallerFile | Read-ProductVersionFromExe
-# InstallerSha256
-$this.CurrentState.Installer[0]['InstallerSha256'] = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
-
-# Case 3: The installer file has an invalid version
-if ([string]::IsNullOrWhiteSpace($this.CurrentState.Version)) {
-  throw 'The current state has an invalid version'
-}
-
-Get-ReleaseNotes
-
-# Case 4: The Last Modified was updated, but the hash wasn't
-if ($this.CurrentState.Installer[0].InstallerSha256 -eq $this.LastState.Installer[0].InstallerSha256) {
-  $this.Log('The Last Modified was changed, but the hash is the same', 'Info')
-  $this.Write()
-  return
-}
-
 switch -Regex ($this.Check()) {
-  # Case 6: The Last Modified, hash, and version were updated
-  'Updated|Rollbacked' {
+  'New|Changed|Updated' {
+    try {
+      $Object2 = Invoke-WebRequest -Uri 'https://renew.360.cn/' | ConvertFrom-Html
+
+      $ReleaseNotesNode = $Object2.SelectSingleNode("//div[@class='log-list']/dl[contains(./dd/h3, '$($this.CurrentState.Version)')]")
+      if ($ReleaseNotesNode) {
+        if ($ReleaseNotesNode.SelectSingleNode('./dt').InnerText -match '(\d{4}-\d{1,2}-\d{1,2})') {
+          # ReleaseTime
+          $this.CurrentState.ReleaseTime = $Matches[1] | Get-Date -Format 'yyyy-MM-dd'
+        } else {
+          $this.Log("No ReleaseTime for version $($this.CurrentState.Version)", 'Warning')
+        }
+
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesNode.SelectNodes('./dd/h3/following-sibling::node()[not(contains(@style, "display:none"))]') | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseTime and ReleaseNotes (zh-CN) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
     $this.Print()
     $this.Write()
-    $this.Message()
-    $this.Submit()
   }
-  # Case 5: Both the Last Modified and the hash were updated, but the version wasn't
-  Default {
-    $this.Log('The Last Modified and the hash were changed, but the version is the same', 'Info')
-    $this.Config.IgnorePRCheck = $true
-    $this.Print()
-    $this.Write()
+  'Changed|Updated' {
     $this.Message()
+  }
+  'Updated' {
     $this.Submit()
   }
 }
