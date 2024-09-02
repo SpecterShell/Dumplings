c89da1348427e9e8ab0ebf351c98f983d5f2b9a0
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-09-02 21:55:09 +0800

    Task: Add Comix.FastMeeting

---
+++ Tasks/Comix.FastMeeting/Script.ps1
@@ -0,0 +1,70 @@
+# en-US
+$Object1 = Invoke-RestMethod -Uri 'https://collect.hst.com/checkUpdate' -Method Post -Body @{
+  content = @{
+    appkey     = '158f3069a435b314a80bdcb024f8e422'
+    curversion = $this.LastState.Contains('Version') ? $this.LastState.Version : '03.42.01.103'
+    lang       = 'en_us'
+  } | ConvertTo-Json -Compress
+}
+
+if (-not $Object1.hasNewVersion) {
+  $this.Log("The version $($this.LastState.Version) from the last state is the latest, skip checking", 'Info')
+  return
+}
+
+# Version
+$this.CurrentState.Version = $Object1.newVersion
+
+# RealVersion
+$this.CurrentState.RealVersion = $this.CurrentState.Version -replace '(?<=^|\.)0+'
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.updateUrl
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseNotes (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotes'
+        Value  = $Object1.desc | Format-Text
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    try {
+      # zh-CN
+      $Object2 = Invoke-RestMethod -Uri 'https://collect.hst.com/checkUpdate' -Method Post -Body @{
+        content = @{
+          appkey     = '158f3069a435b314a80bdcb024f8e422'
+          curversion = $this.LastState.Contains('Version') ? $this.LastState.Version : '03.42.01.103'
+          lang       = 'zh_cn'
+        } | ConvertTo-Json -Compress
+      }
+
+      # ReleaseNotes (zh-CN)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'zh-CN'
+        Key    = 'ReleaseNotes'
+        Value  = $Object2.desc | Format-Text
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
