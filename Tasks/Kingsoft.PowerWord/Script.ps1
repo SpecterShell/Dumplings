e1605537d564010f19ac2679c78d86fa7cac2c1e
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-09-03 17:42:15 +0800

    Task: Add Kingsoft.PowerWord

---
+++ Tasks/Kingsoft.PowerWord/Script.ps1
@@ -0,0 +1,42 @@
+$Object1 = Invoke-RestMethod -Uri 'http://report2.iciba.com/report/pc/versionUpdate' -Body @{
+  version = $this.LastState.Contains('Version') ? $this.LastState.Version : '2022.1.1.0141'
+}
+
+# Version
+$this.CurrentState.Version = $Object1.data.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = 'https://download.iciba.com/pc/personal2016/PowerWord.800.12012.exe'
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+
+    # InstallerSha256
+    $this.CurrentState.Installer[0]['InstallerSha256'] = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
+
+    $InstallerFileExtracted = New-TempFolder
+    7z.exe e -aoa -ba -bd -y -o"${InstallerFileExtracted}" $InstallerFile 'setup.xml' | Out-Host
+    $Object2 = Join-Path $InstallerFileExtracted 'setup.xml' | Get-Item | Get-Content -Raw
+    $Object3 = $Object2 | ConvertFrom-Xml
+
+    # PackageName
+    $this.CurrentState.Locale += [ordered]@{
+      Key   = 'PackageName'
+      Value = [regex]::Match($Object2, '(金山词霸\d{4})').Groups[1].Value
+    }
+    # ProductCode
+    $this.CurrentState.Installer[0]['ProductCode'] = "PowerWord$($Object3.config.office.info.version)"
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
