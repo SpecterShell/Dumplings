ac356b646a0ec3023b96c8991689179a433d739f
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-09-04 23:21:51 +0800

    Task: Add Tencent.QQDevTools

---
+++ Tasks/Tencent.QQDevTools/Script.ps1
@@ -0,0 +1,45 @@
+$Object1 = Invoke-WebRequest -Uri 'https://q.qq.com/wiki/tools/devtool/stable.html' | ConvertFrom-Html
+$ReleaseNotesTitleNode = $Object1.SelectSingleNode('//div[@class="content__default"]/h2[1]')
+
+# Version
+$this.CurrentState.Version = $ReleaseNotesTitleNode.SelectSingleNode('./text()').InnerText.Trim()
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl           = $ReleaseNotesTitleNode.SelectSingleNode('./following::a[contains(@href, ".exe")][1]').Attributes['href'].Value
+  AppsAndFeaturesEntries = @(
+    [ordered]@{
+      DisplayVersion = "$($this.CurrentState.Version)_x64"
+    }
+  )
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseNotes (en-US)
+      $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node -and $Node.Name -ne 'h2'; $Node = $Node.NextSibling) {
+        if (-not $Node.SelectSingleNode('.//a[contains(@href, ".exe") or contains(@href, ".dmg")]')) {
+          $Node
+        }
+      }
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotes'
+        Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
