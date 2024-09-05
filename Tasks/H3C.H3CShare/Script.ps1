a6ee56c63d523c141b924d41a596e53331fc5e53
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-09-05 14:10:16 +0800

    Task: Add H3C.H3CShare

---
+++ Tasks/H3C.H3CShare/Script.ps1
@@ -0,0 +1,84 @@
+function Get-Version {
+  $InstallerFileExtracted = New-TempFolder
+  7z.exe e -aoa -ba -bd -y -o"${InstallerFileExtracted}" $InstallerFile 'H3CClient.exe' | Out-Host
+  $this.CurrentState.Version = Join-Path $InstallerFileExtracted 'H3CClient.exe' | Read-ProductVersionFromExe
+}
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = 'https://h3c-magic.oss-cn-hangzhou.aliyuncs.com/apk/H3C%20Share%20Client%20for%20Windows.exe'
+}
+
+$Object1 = Invoke-WebRequest -Uri $this.CurrentState.Installer[0].InstallerUrl -Method Head
+# MD5
+$this.CurrentState.MD5 = $Object1.Headers.'Content-MD5'[0]
+
+# Case 0: Force submit the manifest
+if ($Global:DumplingsPreference.Contains('Force')) {
+  $this.Log('Skip checking states', 'Info')
+
+  $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+  # Version
+  Get-Version
+  # InstallerSha256
+  $this.CurrentState.Installer[0]['InstallerSha256'] = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
+
+  $this.Print()
+  $this.Write()
+  $this.Message()
+  $this.Submit()
+  return
+}
+
+# Case 1: The task is newly created
+if ($this.Status.Contains('New')) {
+  $this.Log('New task', 'Info')
+
+  $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+  # Version
+  Get-Version
+  # InstallerSha256
+  $this.CurrentState.Installer[0]['InstallerSha256'] = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
+
+  $this.Print()
+  $this.Write()
+  return
+}
+
+# Case 2: The MD5 was not updated
+if ($this.CurrentState.MD5 -eq $this.LastState.MD5) {
+  $this.Log("The version $($this.LastState.Version) from the last state is the latest", 'Info')
+  return
+}
+
+$InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+# Version
+Get-Version
+# InstallerSha256
+$this.CurrentState.Installer[0]['InstallerSha256'] = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
+
+# Case 3: The installer file has an invalid version
+if ([string]::IsNullOrWhiteSpace($this.CurrentState.Version)) {
+  throw 'The current state has an invalid version'
+}
+
+switch -Regex ($this.Check()) {
+  # Case 5: The MD5 and the version were updated
+  'Updated|Rollbacked' {
+    $this.Print()
+    $this.Write()
+    $this.Message()
+    $this.Submit()
+  }
+  # Case 4: The MD5 was updated, but the version wasn't
+  # The installer might be updated without changing the version (e.g., virus database update)
+  # Force submit the manifest even if neither the version nor the installer has changed
+  Default {
+    $this.Log('The MD5 was changed, but the version is the same', 'Info')
+    $this.Config.IgnorePRCheck = $true
+    $this.Print()
+    $this.Write()
+    $this.Message()
+    $this.Submit()
+  }
+}
