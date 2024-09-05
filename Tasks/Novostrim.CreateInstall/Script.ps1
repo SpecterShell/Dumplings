7d8aa39927a284b98985e99a65b2961aab7bd85b
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-09-05 16:34:27 +0800

    Task: Add Novostrim.CreateInstall

---
+++ Tasks/Novostrim.CreateInstall/Script.ps1
@@ -0,0 +1,45 @@
+$Object1 = Invoke-RestMethod -Uri 'https://www.createinstall.com/downloads/update.ini' | ConvertFrom-Ini
+
+# Version
+$this.CurrentState.Version = $Object1.main.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = "https://www.createinstall.com/downloads/ci-setup.$($this.CurrentState.Version).exe"
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated|Rollbacked' {
+    try {
+      $Object2 = Invoke-WebRequest -Uri 'https://www.createinstall.com/history.html' | ConvertFrom-Html
+
+      $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//div[@id='right']/h3[contains(., '$($this.CurrentState.Version)')]")
+      if ($ReleaseNotesTitleNode) {
+        # ReleaseTime
+        $this.CurrentState.ReleaseTime = [regex]::Match($ReleaseNotesTitleNode.InnerText, '([a-zA-Z]+ \d+, \d+)').Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
+
+        $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node -and $Node.Name -ne 'h3'; $Node = $Node.NextSibling) { $Node }
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated|Rollbacked' {
+    $this.Message()
+  }
+  'Updated|Rollbacked' {
+    $this.Submit()
+  }
+}
