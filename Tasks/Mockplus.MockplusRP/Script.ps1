bc97b63ca9f656aaf9d2bace64793d6bba65ecb4
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-09-05 14:22:21 +0800

    Task: Add Mockplus.MockplusRP

---
+++ Tasks/Mockplus.MockplusRP/Script.ps1
@@ -0,0 +1,16 @@
+$Prefix = 'https://img02.mockplus.cn/rp-desktop/'
+
+$this.CurrentState = Invoke-RestMethod -Uri "${Prefix}latest.yml?noCache=$(Get-Random)" | ConvertFrom-Yaml | ConvertFrom-ElectronUpdater -Prefix $Prefix -Locale 'en-US'
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated|Rollbacked' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated|Rollbacked' {
+    $this.Message()
+  }
+  'Updated|Rollbacked' {
+    $this.Submit()
+  }
+}
