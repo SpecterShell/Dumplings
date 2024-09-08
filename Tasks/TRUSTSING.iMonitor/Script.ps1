15b8c99668174a3e33b45baad55d4512a7ed7a4b
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-09-09 01:07:16 +0800

    Task: Add TRUSTSING.iMonitor

---
+++ Tasks/TRUSTSING.iMonitor/Script.ps1
@@ -0,0 +1,69 @@
+$Certificate = [System.Security.Cryptography.X509Certificates.X509Certificate2]::new([System.Convert]::FromBase64String($Global:DumplingsSecret.iMonitorPfx))
+$Object1 = Invoke-RestMethod $Global:DumplingsSecret.iMonitorUrl -Method Post -Body @(
+  @{
+    product = 'iMonitor'
+    version = $this.LastState.Contains('Version') ? $this.LastState.Version : '2.5.1.0'
+  } | ConvertTo-Json -Compress
+) -Certificate $Certificate -SkipCertificateCheck
+
+# Version
+$this.CurrentState.Version = $Object1.data.new_version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x86'
+  InstallerUrl = 'https://trustsing.com/publish/iMonitor32.exe'
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = 'https://trustsing.com/publish/iMonitor64.exe'
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    # ReleaseNotesUrl
+    $this.CurrentState.Locale += [ordered]@{
+      Key   = 'ReleaseNotesUrl'
+      Value = $ReleaseNotesUrl = 'https://trustsing.com/imonitor/'
+    }
+
+    try {
+      $Object2 = Invoke-WebRequest -Uri $ReleaseNotesUrl | ConvertFrom-Html
+
+      $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//div[@class='theme-hope-content']/h3[contains(text(), '$($this.CurrentState.Version.Split('.')[0..2] -join '.')')]")
+      if ($ReleaseNotesTitleNode) {
+        $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node -and $Node.Name -ne 'h3'; $Node = $Node.NextSibling) {
+          if (-not $Node.HasClass('button-group')) {
+            $Node
+          }
+        }
+        # ReleaseNotes (zh-CN)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'zh-CN'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
+        }
+
+        # ReleaseNotesUrl
+        $this.CurrentState.Locale += [ordered]@{
+          Key   = 'ReleaseNotesUrl'
+          Value = $ReleaseNotesUrl + '#' + $ReleaseNotesTitleNode.Attributes['id'].Value
+        }
+      } else {
+        $this.Log("No ReleaseNotes (zh-CN) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
