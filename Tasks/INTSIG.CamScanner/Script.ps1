e19352133b744cad72249b2229e999d498c4ac28
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-09-09 19:03:27 +0800

    Task: INTSIG.CamScanner Get x86 installer URL from the x64 one

--- Tasks/INTSIG.CamScanner/Script.ps1
+++ Tasks/INTSIG.CamScanner/Script.ps1
@@ -1,24 +1,15 @@
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  Architecture = 'x86'
-  InstallerUrl = $InstallerUrlX86 = Get-RedirectedUrl -Uri 'https://cc.co/16YS3j'
+  Architecture = 'x64'
+  InstallerUrl = Get-RedirectedUrl -Uri 'https://cc.co/16YS3k'
 }
 $this.CurrentState.Installer += [ordered]@{
-  Architecture = 'x64'
-  InstallerUrl = $InstallerUrlX64 = Get-RedirectedUrl -Uri 'https://cc.co/16YS3k'
-}
-
-$VersionX86 = [regex]::Match($InstallerUrlX86, 'CamScanner-(\d+(?:\.\d+)+)').Groups[1].Value
-$VersionX64 = [regex]::Match($InstallerUrlX64, 'CamScanner-(\d+(?:\.\d+)+)').Groups[1].Value
-
-if ($VersionX86 -ne $VersionX64) {
-  $this.Log("x86 version: ${VersionX86}")
-  $this.Log("x64 version: ${VersionX64}")
-  throw 'Inconsistent versions detected'
+  Architecture = 'x86'
+  InstallerUrl = $this.CurrentState.Installer[0].InstallerUrl.Replace('x64', 'x32')
 }
 
 # Version
-$this.CurrentState.Version = $VersionX64
+$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, 'CamScanner-(\d+(?:\.\d+)+)').Groups[1].Value
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
