2f786d8440cbfc3548028e062218212875fbdcd7
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-09-10 23:05:21 +0800

    Task: Morgen.Morgen Remove arm64 installer

--- Tasks/Morgen.Morgen/Script.ps1
+++ Tasks/Morgen.Morgen/Script.ps1
@@ -1,30 +1,9 @@
 $Prefix = 'https://download.todesktop.com/210203cqcj00tw1/'
 
-$Object1 = Invoke-RestMethod -Uri "${Prefix}latest.yml?noCache=$(Get-Random)" | ConvertFrom-Yaml
-
-# Version
-$this.CurrentState.Version = $Object1.version
-
-# Installer
-$this.CurrentState.Installer += [ordered]@{
-  Architecture = 'x64'
-  InstallerUrl = $Prefix + $Object1.files.Where({ $_.url.Contains('x64') }, 'First')[0].url
-}
-$this.CurrentState.Installer += [ordered]@{
-  Architecture = 'arm64'
-  InstallerUrl = $Prefix + $Object1.files.Where({ $_.url.Contains('arm64') }, 'First')[0].url
-}
+$this.CurrentState = Invoke-RestMethod -Uri "${Prefix}latest.yml?noCache=$(Get-Random)" | ConvertFrom-Yaml | ConvertFrom-ElectronUpdater -Prefix $Prefix -Locale 'en-US'
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
-    try {
-      # ReleaseTime
-      $this.CurrentState.ReleaseTime = $Object1.releaseDate | Get-Date -AsUTC
-    } catch {
-      $_ | Out-Host
-      $this.Log($_, 'Warning')
-    }
-
     $this.Print()
     $this.Write()
   }
