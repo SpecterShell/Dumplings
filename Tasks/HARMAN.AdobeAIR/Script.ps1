c5c3e3d214771e9223eb9ac444c6e5d0ce1b4989
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-09-18 23:51:11 +0800

    Task: HARMAN.AdobeAIR Parse from dynamic webpage

--- Tasks/HARMAN.AdobeAIR/Script.ps1
+++ Tasks/HARMAN.AdobeAIR/Script.ps1
@@ -1,112 +1,47 @@
-function Get-ReleaseNotes {
-  try {
-    $Object2 = (Invoke-RestMethod -Uri 'https://airsdk.harman.com/api/versions/release-notes').Where({ $_.versionName -eq $this.CurrentState.Version })
+$EdgeDriver = Get-EdgeDriver -Headless
+$EdgeDriver.Navigate().GoToUrl('https://airsdk.harman.com/runtime')
 
-    if ($Object2) {
-      # ReleaseTime
-      $this.CurrentState.ReleaseTime = $Object2[0].releasedDate | ConvertFrom-UnixTimeMilliseconds
-
-      # ReleaseNotes (en-US)
-      $this.CurrentState.Locale += [ordered]@{
-        Locale = 'en-US'
-        Key    = 'ReleaseNotes'
-        Value  = $Object2[0].releaseNotes | ForEach-Object -Process { "$($_.title): $($_.description)" } | Format-Text
-      }
-    } else {
-      $this.Log("No ReleaseTime and ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
-    }
-  } catch {
-    $_ | Out-Host
-    $this.Log($_, 'Warning')
-  }
-}
+# Version
+$this.CurrentState.Version = [regex]::Match(
+  $EdgeDriver.FindElement([OpenQA.Selenium.By]::XPath('//div[contains(@class, "miniTitle") and contains(text(), "version")]')).Text,
+  '(\d+(?:\.\d+){3,})'
+).Groups[1].Value
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = 'https://airsdk.harman.com/assets/downloads/AdobeAIR.exe'
-}
-
-$Object1 = Invoke-WebRequest -Uri $this.CurrentState.Installer[0].InstallerUrl -Method Head
-# Last Modified
-$this.CurrentState.LastModified = $Object1.Headers.'Last-Modified'[0]
-
-# Case 0: Force submit the manifest
-if ($Global:DumplingsPreference.Contains('Force')) {
-  $this.Log('Skip checking states', 'Info')
-
-  $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
-  # Version
-  $this.CurrentState.Version = $InstallerFile | Read-FileVersionFromExe
-  # InstallerSha256
-  $this.CurrentState.Installer[0]['InstallerSha256'] = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
-
-  Get-ReleaseNotes
-
-  $this.Print()
-  $this.Write()
-  $this.Message()
-  $this.Submit()
-  return
-}
-
-# Case 1: The task is newly created
-if ($this.Status.Contains('New')) {
-  $this.Log('New task', 'Info')
-
-  $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
-  # Version
-  $this.CurrentState.Version = $InstallerFile | Read-FileVersionFromExe
-  # InstallerSha256
-  $this.CurrentState.Installer[0]['InstallerSha256'] = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
-
-  Get-ReleaseNotes
-
-  $this.Print()
-  $this.Write()
-  return
-}
-
-# Case 2: The Last Modified was not updated
-if ($this.CurrentState.LastModified -eq $this.LastState.LastModified) {
-  $this.Log("The version $($this.LastState.Version) from the last state is the latest", 'Info')
-  return
-}
-
-$InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
-# Version
-$this.CurrentState.Version = $InstallerFile | Read-FileVersionFromExe
-# InstallerSha256
-$this.CurrentState.Installer[0]['InstallerSha256'] = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
-
-# Case 3: The installer file has an invalid version
-if ([string]::IsNullOrWhiteSpace($this.CurrentState.Version)) {
-  throw 'The current state has an invalid version'
-}
-
-Get-ReleaseNotes
-
-# Case 4: The Last Modified was updated, but the hash wasn't
-if ($this.CurrentState.Installer[0].InstallerSha256 -eq $this.LastState.Installer[0].InstallerSha256) {
-  $this.Log('The Last Modified was changed, but the hash is the same', 'Info')
-  $this.Write()
-  return
+  InstallerUrl = $EdgeDriver.FindElement([OpenQA.Selenium.By]::XPath('//a[contains(@class, "downloadLink") and contains(@href, ".exe")]')).GetAttribute('href')
 }
 
 switch -Regex ($this.Check()) {
-  # Case 6: The Last Modified, hash, and version were updated
-  'Updated|Rollbacked' {
+  'New|Changed|Updated' {
+    try {
+      $Object3 = (Invoke-RestMethod -Uri 'https://airsdk.harman.com/api/versions/release-notes').Where({ $_.versionName -eq $this.CurrentState.Version })
+
+      if ($Object3) {
+        # ReleaseTime
+        $this.CurrentState.ReleaseTime = $Object3[0].releasedDate | ConvertFrom-UnixTimeMilliseconds
+
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $Object3[0].releaseNotes | ForEach-Object -Process { "$($_.title): $($_.description)" } | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseTime and ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
     $this.Print()
     $this.Write()
-    $this.Message()
-    $this.Submit()
   }
-  # Case 5: Both the Last Modified and the hash were updated, but the version wasn't
-  Default {
-    $this.Log('The Last Modified and the hash were changed, but the version is the same', 'Info')
-    $this.Config.IgnorePRCheck = $true
-    $this.Print()
-    $this.Write()
+  'Changed|Updated' {
     $this.Message()
+  }
+  'Updated' {
     $this.Submit()
   }
 }
