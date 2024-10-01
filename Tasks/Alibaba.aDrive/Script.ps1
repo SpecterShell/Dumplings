3f8ea15e228003cb935da277c14cab4c576f9691
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-10-01 16:12:12 +0800

    Task: Alibaba.aDrive Update source URL

--- Tasks/Alibaba.aDrive/Script.ps1
+++ Tasks/Alibaba.aDrive/Script.ps1
@@ -1,6 +1,6 @@
 $Object1 = Invoke-RestMethod -Uri 'https://www.aliyundrive.com/desktop/version/update.json'
 
-$this.CurrentState = Invoke-RestMethod -Uri "$($Object1.url)/win32/ia32/latest.yml?noCache=$(Get-Random)" | ConvertFrom-Yaml | ConvertFrom-ElectronUpdater -Locale 'zh-CN'
+$this.CurrentState = Invoke-RestMethod -Uri "$($Object1.url)/win32/x64/latest.yml?noCache=$(Get-Random)" | ConvertFrom-Yaml | ConvertFrom-ElectronUpdater -Locale 'zh-CN'
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
