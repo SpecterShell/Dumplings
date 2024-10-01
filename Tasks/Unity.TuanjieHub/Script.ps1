2048cea60c5abc7103b35c780c5c92bb9879e546
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-10-01 14:38:27 +0800

    Task: Add Unity.Tuanjie, Unity.TuanjieHub, Unity.Unity.CN.6000

---
+++ Tasks/Unity.TuanjieHub/Script.ps1
@@ -0,0 +1,16 @@
+$Prefix = 'https://public-cdn.tuanjie.cn/hub/prod/'
+
+$this.CurrentState = Invoke-RestMethod -Uri "${Prefix}latest.yml?noCache=$(Get-Random)" | ConvertFrom-Yaml | ConvertFrom-ElectronUpdater -Prefix $Prefix -Locale 'zh-CN'
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
