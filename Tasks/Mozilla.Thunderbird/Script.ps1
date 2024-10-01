b81a5e622040d11435189d35f0156dd2f54b48f1
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-10-02 02:15:50 +0800

    Task: Mozilla.Thunderbird Rewrite

--- Tasks/Mozilla.Thunderbird/Script.ps1
+++ Tasks/Mozilla.Thunderbird/Script.ps1
@@ -1,150 +1,17 @@
 # The following locales can't be matched by WinGet properly and thus are omitted: cak (Kaqchikel)
-$Locales = @('af', 'ar', 'ast', 'be', 'bg', 'br', 'ca', 'cs', 'cy', 'da', 'de', 'dsb', 'el', 'en-CA', 'en-GB', 'en-US', 'es-AR', 'es-ES', 'es-MX', 'et', 'eu', 'fi', 'fr', 'fy-NL', 'ga-IE', 'gd', 'gl', 'he', 'hr', 'hsb', 'hu', 'hy-AM', 'id', 'is', 'it', 'ja', 'ka', 'kab', 'kk', 'ko', 'lt', 'lv', 'ms', 'multi', 'nb-NO', 'nl', 'nn-NO', 'pa-IN', 'pl', 'pt-BR', 'pt-PT', 'rm', 'ro', 'ru', 'sk', 'sl', 'sq', 'sr', 'sv-SE', 'th', 'tr', 'uk', 'uz', 'vi', 'zh-CN', 'zh-TW')
+$Locales = @('en-US', 'af', 'ar', 'ast', 'be', 'bg', 'br', 'ca', 'cs', 'cy', 'da', 'de', 'dsb', 'el', 'en-CA', 'en-GB', 'es-AR', 'es-ES', 'es-MX', 'et', 'eu', 'fi', 'fr', 'fy-NL', 'ga-IE', 'gd', 'gl', 'he', 'hr', 'hsb', 'hu', 'hy-AM', 'id', 'is', 'it', 'ja', 'ka', 'kab', 'kk', 'ko', 'lt', 'lv', 'ms', 'nb-NO', 'nl', 'nn-NO', 'pa-IN', 'pl', 'pt-BR', 'pt-PT', 'rm', 'ro', 'ru', 'sk', 'sl', 'sq', 'sr', 'sv-SE', 'th', 'tr', 'uk', 'uz', 'vi', 'zh-CN', 'zh-TW', 'multi')
 $ArchMap = [ordered]@{
-  x86 = 'win32'
-  x64 = 'win64'
-  # arm64 = 'win64-aarch64'
+  x86   = 'win32'
+  x64   = 'win64'
+  arm64 = 'win64-aarch64'
 }
 $Prefix = 'https://download-installer.cdn.mozilla.net/pub/thunderbird/releases/'
 
 $Object1 = Invoke-RestMethod -Uri 'https://product-details.mozilla.org/1.0/thunderbird_versions.json'
 
 # Version
-$this.CurrentState.Version = $Version = $Object1.THUNDERBIRD_ESR_NEXT.Replace('esr', '')
-
-$Object2 = [ordered]@{}
-Invoke-RestMethod -Uri "${Prefix}${Version}esr/SHA256SUMS" | Split-LineEndings |
-  Where-Object -FilterScript { $_ -match '(win32|win64|win64-aarch64)/' -and $_ -match '\.(exe|msix)$' -and -not $_.Contains('Thunderbird Installer') } |
-  ForEach-Object -Process {
-    $Entries = $_.Split('  ')
-    $Object2[$Entries[1]] = $Entries[0].ToUpper()
-  }
-
-# Installer
-foreach ($Arch in $ArchMap.GetEnumerator()) {
-  # $this.CurrentState.Installer += [ordered]@{
-  #   Architecture    = $Arch.Key
-  #   InstallerType   = 'exe'
-  #   InstallerUrl    = "${Prefix}${Version}esr/$($Arch.Value)/en-US/Thunderbird Setup ${Version}esr.exe"
-  #   InstallerSha256 = $Object2["$($Arch.Value)/en-US/Thunderbird Setup ${Version}esr.exe"]
-  #   ProductCode     = "Mozilla Thunderbird ${Version} ($($Arch.Key) en-US)"
-  # }
-  switch ($Locales) {
-    # # en-US is the default locale of the manifests but not listed at the beginning of the list. Add en-US locale explicitly and skip it in this process
-    # 'en-US' { continue }
-    # MSIX installers (ARM64 not available)
-    'multi' {
-      if ($Arch.Key -ne 'arm64') {
-        $this.CurrentState.Installer += [ordered]@{
-          Architecture  = $Arch.Key
-          InstallerType = 'msix'
-          InstallerUrl  = "${Prefix}${Version}esr/$($Arch.Value)/${_}/Thunderbird Setup ${Version}esr.msix"
-        }
-      }
-      continue
-    }
-    # Handle special language cases
-    # Asturian (Spain)
-    'ast' {
-      $this.CurrentState.Installer += [ordered]@{
-        InstallerLocale = 'ast-ES'
-        Architecture    = $Arch.Key
-        InstallerType   = 'exe'
-        InstallerUrl    = "${Prefix}${Version}esr/$($Arch.Value)/${_}/Thunderbird Setup ${Version}esr.exe"
-        InstallerSha256 = $Object2["$($Arch.Value)/${_}/Thunderbird Setup ${Version}esr.exe"]
-        ProductCode     = "Mozilla Thunderbird ${Version} ($($Arch.Key) ${_})"
-      }
-      continue
-    }
-    # Breton (France)
-    'br' {
-      $this.CurrentState.Installer += [ordered]@{
-        InstallerLocale = 'br-FR'
-        Architecture    = $Arch.Key
-        InstallerType   = 'exe'
-        InstallerUrl    = "${Prefix}${Version}esr/$($Arch.Value)/${_}/Thunderbird Setup ${Version}esr.exe"
-        InstallerSha256 = $Object2["$($Arch.Value)/${_}/Thunderbird Setup ${Version}esr.exe"]
-        ProductCode     = "Mozilla Thunderbird ${Version} ($($Arch.Key) ${_})"
-      }
-      continue
-    }
-    # Scottish Gaelic (United Kingdom)
-    'gd' {
-      $this.CurrentState.Installer += [ordered]@{
-        InstallerLocale = 'gd-GB'
-        Architecture    = $Arch.Key
-        InstallerType   = 'exe'
-        InstallerUrl    = "${Prefix}${Version}esr/$($Arch.Value)/${_}/Thunderbird Setup ${Version}esr.exe"
-        InstallerSha256 = $Object2["$($Arch.Value)/${_}/Thunderbird Setup ${Version}esr.exe"]
-        ProductCode     = "Mozilla Thunderbird ${Version} ($($Arch.Key) ${_})"
-      }
-      continue
-    }
-    # Kabyle (Algeria)
-    'kab' {
-      $this.CurrentState.Installer += [ordered]@{
-        InstallerLocale = 'kab-DZ'
-        Architecture    = $Arch.Key
-        InstallerType   = 'exe'
-        InstallerUrl    = "${Prefix}${Version}esr/$($Arch.Value)/${_}/Thunderbird Setup ${Version}esr.exe"
-        InstallerSha256 = $Object2["$($Arch.Value)/${_}/Thunderbird Setup ${Version}esr.exe"]
-        ProductCode     = "Mozilla Thunderbird ${Version} ($($Arch.Key) ${_})"
-      }
-      continue
-    }
-    # Serbian (Cyrillic and Latin)
-    'sr' {
-      $this.CurrentState.Installer += [ordered]@{
-        InstallerLocale = 'sr-Cyrl'
-        Architecture    = $Arch.Key
-        InstallerType   = 'exe'
-        InstallerUrl    = "${Prefix}${Version}esr/$($Arch.Value)/${_}/Thunderbird Setup ${Version}esr.exe"
-        InstallerSha256 = $Object2["$($Arch.Value)/${_}/Thunderbird Setup ${Version}esr.exe"]
-        ProductCode     = "Mozilla Thunderbird ${Version} ($($Arch.Key) ${_})"
-      }
-      $this.CurrentState.Installer += [ordered]@{
-        InstallerLocale = 'sr-Latn'
-        Architecture    = $Arch.Key
-        InstallerType   = 'exe'
-        InstallerUrl    = "${Prefix}${Version}esr/$($Arch.Value)/${_}/Thunderbird Setup ${Version}esr.exe"
-        InstallerSha256 = $Object2["$($Arch.Value)/${_}/Thunderbird Setup ${Version}esr.exe"]
-        ProductCode     = "Mozilla Thunderbird ${Version} ($($Arch.Key) ${_})"
-      }
-      continue
-    }
-    # Uzbek (Cyrillic and Latin)
-    'uz' {
-      $this.CurrentState.Installer += [ordered]@{
-        InstallerLocale = 'uz-Cyrl'
-        Architecture    = $Arch.Key
-        InstallerType   = 'exe'
-        InstallerUrl    = "${Prefix}${Version}esr/$($Arch.Value)/${_}/Thunderbird Setup ${Version}esr.exe"
-        InstallerSha256 = $Object2["$($Arch.Value)/${_}/Thunderbird Setup ${Version}esr.exe"]
-        ProductCode     = "Mozilla Thunderbird ${Version} ($($Arch.Key) ${_})"
-      }
-      $this.CurrentState.Installer += [ordered]@{
-        InstallerLocale = 'uz-Latn'
-        Architecture    = $Arch.Key
-        InstallerType   = 'exe'
-        InstallerUrl    = "${Prefix}${Version}esr/$($Arch.Value)/${_}/Thunderbird Setup ${Version}esr.exe"
-        InstallerSha256 = $Object2["$($Arch.Value)/${_}/Thunderbird Setup ${Version}esr.exe"]
-        ProductCode     = "Mozilla Thunderbird ${Version} ($($Arch.Key) ${_})"
-      }
-      continue
-    }
-    Default {
-      $this.CurrentState.Installer += [ordered]@{
-        InstallerLocale = $_
-        Architecture    = $Arch.Key
-        InstallerType   = 'exe'
-        InstallerUrl    = "${Prefix}${Version}esr/$($Arch.Value)/${_}/Thunderbird Setup ${Version}esr.exe"
-        InstallerSha256 = $Object2["$($Arch.Value)/${_}/Thunderbird Setup ${Version}esr.exe"]
-        ProductCode     = "Mozilla Thunderbird ${Version} ($($Arch.Key) ${_})"
-      }
-      continue
-    }
-  }
-}
+$OriginalVersion = $Object1.THUNDERBIRD_ESR
+$this.CurrentState.Version = $ShortVersion = $OriginalVersion.Replace('esr', '')
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
@@ -152,7 +19,7 @@ switch -Regex ($this.Check()) {
       # ReleaseNotesUrl
       $this.CurrentState.Locale += [ordered]@{
         Key   = 'ReleaseNotesUrl'
-        Value = $ReleaseNotesUrl = "https://www.thunderbird.net/thunderbird/${Version}esr/releasenotes/"
+        Value = $ReleaseNotesUrl = "https://www.thunderbird.net/thunderbird/${OriginalVersion}/releasenotes/"
       }
     } catch {
       $_ | Out-Host
@@ -160,10 +27,10 @@ switch -Regex ($this.Check()) {
     }
 
     try {
-      $Object3 = Invoke-RestMethod -Uri 'https://product-details.mozilla.org/1.0/thunderbird.json'
+      $Object2 = Invoke-RestMethod -Uri 'https://product-details.mozilla.org/1.0/thunderbird.json'
 
       # ReleaseTime
-      $this.CurrentState.ReleaseTime = $Object3.releases."thunderbird-${Version}esr".date | Get-Date -Format 'yyyy-MM-dd'
+      $this.CurrentState.ReleaseTime = $Object2.releases."thunderbird-${OriginalVersion}".date | Get-Date -Format 'yyyy-MM-dd'
 
     } catch {
       $_ | Out-Host
@@ -171,16 +38,16 @@ switch -Regex ($this.Check()) {
     }
 
     try {
-      $Object4 = Invoke-WebRequest -Uri $ReleaseNotesUrl | ConvertFrom-Html
+      $Object3 = Invoke-WebRequest -Uri $ReleaseNotesUrl | ConvertFrom-Html
 
       # Remove headers
-      $Object4.SelectNodes('/html/body/main/section[contains(./div/@class, "release-notes-container")]//div[@class="see-all-releases"]').ForEach({ $_.Remove() })
+      $Object3.SelectNodes('/html/body/main/section[contains(./div/@class, "release-notes-container")]//div[@class="see-all-releases"]').ForEach({ $_.Remove() })
 
       # ReleaseNotes (en-US)
       $this.CurrentState.Locale += [ordered]@{
         Locale = 'en-US'
         Key    = 'ReleaseNotes'
-        Value  = $Object4.SelectSingleNode('/html/body/main/section[contains(./div/@class, "release-notes-container")]') | Get-TextContent | Format-Text
+        Value  = $Object3.SelectSingleNode('/html/body/main/section[contains(./div/@class, "release-notes-container")]') | Get-TextContent | Format-Text
       }
     } catch {
       $_ | Out-Host
@@ -194,6 +61,53 @@ switch -Regex ($this.Check()) {
     $this.Message()
   }
   'Updated' {
-    $this.Submit()
+    $Object4 = [ordered]@{}
+    Invoke-RestMethod -Uri "${Prefix}${OriginalVersion}/SHA256SUMS" | Split-LineEndings |
+      Where-Object -FilterScript { $_ -match '(win32|win64|win64-aarch64)/' -and $_ -match '\.(exe|msix)$' -and -not $_.Contains('Thunderbird Installer') } |
+      ForEach-Object -Process {
+        $Entries = $_.Split('  ')
+        $Object4[$Entries[1]] = $Entries[0].ToUpper()
+      }
+
+    foreach ($Locale in $Locales) {
+      $this.CurrentState.Installer = @()
+
+      if ($Locale -eq 'multi') {
+        $this.Config.WinGetIdentifier = 'Mozilla.Thunderbird.MSIX'
+
+        foreach ($Arch in @('x86', 'x64')) {
+          # Installer
+          $this.CurrentState.Installer += [ordered]@{
+            Architecture  = $Arch
+            InstallerType = 'msix'
+            InstallerUrl  = "${Prefix}${OriginalVersion}/$($ArchMap[$Arch])/${Locale}/Thunderbird Setup ${OriginalVersion}.msix"
+          }
+        }
+      } else {
+        if ($Locale -eq 'en-US') {
+          $this.Config.WinGetIdentifier = 'Mozilla.Thunderbird'
+        } else {
+          $this.Config.WinGetIdentifier = "Mozilla.Thunderbird.${Locale}"
+        }
+
+        foreach ($Arch in @('x86', 'x64')) {
+          # Installer
+          $this.CurrentState.Installer += [ordered]@{
+            Architecture    = $Arch
+            InstallerType   = 'nullsoft'
+            InstallerUrl    = "${Prefix}${OriginalVersion}/$($ArchMap[$Arch])/${Locale}/Thunderbird Setup ${OriginalVersion}.exe"
+            InstallerSha256 = $Object4["$($ArchMap[$Arch])/${Locale}/Thunderbird Setup ${OriginalVersion}.exe"]
+            ProductCode     = "Mozilla Thunderbird ${ShortVersion} (${Arch} ${Locale})"
+          }
+        }
+      }
+
+      try {
+        $this.Submit()
+      }
+      catch {
+        $_ | Out-Host
+      }
+    }
   }
 }
