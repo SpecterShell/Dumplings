e2cc008533a546cc807881f1b572ecd3b9ad0da5
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-10-03 23:36:14 +0800

    Task: Rakuten.Viber Override InstallerUrl using ForEach-Object

--- Tasks/Rakuten.Viber/Script.ps1
+++ Tasks/Rakuten.Viber/Script.ps1
@@ -44,7 +44,13 @@ switch -Regex ($this.Check()) {
     $this.Message()
   }
   'Updated|Rollbacked' {
-    $this.CurrentState.Installer.ForEach({ $_.InstallerUrl += "?t=$(Get-Date -Format 'yyyyMMdd')" })
+    try {
+      $this.CurrentState.Installer | ForEach-Object -Process { $_.InstallerUrl += "?t=$(Get-Date -Format 'yyyyMMdd')" }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
     $this.Submit()
   }
 }
