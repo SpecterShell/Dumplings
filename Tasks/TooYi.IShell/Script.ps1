17d74751d2a9799b146ca812d8093165a0862001
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-10-03 20:53:33 +0800

    Task: Add TooYi.IShell

---
+++ Tasks/TooYi.IShell/Script.ps1
@@ -0,0 +1,37 @@
+$Object1 = Invoke-RestMethod -Uri 'http://api.ishell.cc/versions/0'
+
+# Version
+$this.CurrentState.Version = $Object1.data.vers
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.data.downUrl
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.data.createAt | Get-Date | ConvertTo-UtcDateTime -Id 'China Standard Time'
+
+      # ReleaseNotes (zh-CN)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'zh-CN'
+        Key    = 'ReleaseNotes'
+        Value  = $Object1.data.note | Format-Text
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
