c1863d58213421965778be842dc22476f853362e
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-10-03 21:07:14 +0800

    Task: Add Principle.FlowPrompter

---
+++ Tasks/Principle.FlowPrompter/Script.ps1
@@ -0,0 +1,16 @@
+$Prefix = 'https://release.flowprompter.app/flowprompter/'
+
+$this.CurrentState = Invoke-RestMethod -Uri "${Prefix}latest.yml?noCache=$(Get-Random)" | ConvertFrom-Yaml | ConvertFrom-ElectronUpdater -Prefix $Prefix -Locale 'en-US'
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
