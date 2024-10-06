f5300cfb879c0dcad94423efed97140edb594f70
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-10-06 12:03:14 +0800

    Task: Mozilla.Thunderbird Rewrite and reset

--- Tasks/Mozilla.Thunderbird/Script.ps1
+++ Tasks/Mozilla.Thunderbird/Script.ps1
@@ -1,5 +1,5 @@
-# The following locales can't be matched by WinGet properly and thus are omitted: cak (Kaqchikel)
-$Locales = @('en-US', 'af', 'ar', 'ast', 'be', 'bg', 'br', 'ca', 'cs', 'cy', 'da', 'de', 'dsb', 'el', 'en-CA', 'en-GB', 'es-AR', 'es-ES', 'es-MX', 'et', 'eu', 'fi', 'fr', 'fy-NL', 'ga-IE', 'gd', 'gl', 'he', 'hr', 'hsb', 'hu', 'hy-AM', 'id', 'is', 'it', 'ja', 'ka', 'kab', 'kk', 'ko', 'lt', 'lv', 'ms', 'nb-NO', 'nl', 'nn-NO', 'pa-IN', 'pl', 'pt-BR', 'pt-PT', 'rm', 'ro', 'ru', 'sk', 'sl', 'sq', 'sr', 'sv-SE', 'th', 'tr', 'uk', 'uz', 'vi', 'zh-CN', 'zh-TW', 'multi')
+# Unrecognized locales for WinGet: cak (Kaqchikel)
+$Locales = @('en-US', 'af', 'ar', 'ast', 'be', 'bg', 'br', 'ca', 'cak', 'cs', 'cy', 'da', 'de', 'dsb', 'el', 'en-CA', 'en-GB', 'es-AR', 'es-ES', 'es-MX', 'et', 'eu', 'fi', 'fr', 'fy-NL', 'ga-IE', 'gd', 'gl', 'he', 'hr', 'hsb', 'hu', 'hy-AM', 'id', 'is', 'it', 'ja', 'ka', 'kab', 'kk', 'ko', 'lt', 'lv', 'ms', 'nb-NO', 'nl', 'nn-NO', 'pa-IN', 'pl', 'pt-BR', 'pt-PT', 'rm', 'ro', 'ru', 'sk', 'sl', 'sq', 'sr', 'sv-SE', 'th', 'tr', 'uk', 'uz', 'vi', 'zh-CN', 'zh-TW', 'multi')
 $ArchMap = [ordered]@{
   x86   = 'win32'
   x64   = 'win64'
@@ -12,6 +12,28 @@ $Object1 = Invoke-RestMethod -Uri 'https://product-details.mozilla.org/1.0/thund
 # Version
 $OriginalVersion = $Object1.THUNDERBIRD_ESR
 $this.CurrentState.Version = $ShortVersion = $OriginalVersion.Replace('esr', '')
+
+$Object2 = [ordered]@{}
+Invoke-RestMethod -Uri "${Prefix}${OriginalVersion}/SHA256SUMS" | Split-LineEndings |
+  Where-Object -FilterScript { $_ -match '(win32|win64|win64-aarch64)/' -and $_ -match '\.(exe|msix)$' -and -not $_.Contains('Thunderbird Installer') } |
+  ForEach-Object -Process {
+    $Entries = $_.Split('  ')
+    $Object2[$Entries[1]] = $Entries[0].ToUpper()
+  }
+
+# Installer
+foreach ($Locale in @('en-US')) {
+  foreach ($Arch in @('x86', 'x64')) {
+    # Installer
+    $this.CurrentState.Installer += [ordered]@{
+      Architecture    = $Arch
+      InstallerType   = 'nullsoft'
+      InstallerUrl    = "${Prefix}${OriginalVersion}/$($ArchMap[$Arch])/${Locale}/Thunderbird Setup ${OriginalVersion}.exe"
+      InstallerSha256 = $Object2["$($ArchMap[$Arch])/${Locale}/Thunderbird Setup ${OriginalVersion}.exe"]
+      ProductCode     = "Mozilla Thunderbird ${ShortVersion} (${Arch} ${Locale})"
+    }
+  }
+}
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
@@ -27,10 +49,10 @@ switch -Regex ($this.Check()) {
     }
 
     try {
-      $Object2 = Invoke-RestMethod -Uri 'https://product-details.mozilla.org/1.0/thunderbird.json'
+      $Object3 = Invoke-RestMethod -Uri 'https://product-details.mozilla.org/1.0/thunderbird.json'
 
       # ReleaseTime
-      $this.CurrentState.ReleaseTime = $Object2.releases."thunderbird-${OriginalVersion}".date | Get-Date -Format 'yyyy-MM-dd'
+      $this.CurrentState.ReleaseTime = $Object3.releases."thunderbird-${OriginalVersion}".date | Get-Date -Format 'yyyy-MM-dd'
 
     } catch {
       $_ | Out-Host
@@ -38,16 +60,16 @@ switch -Regex ($this.Check()) {
     }
 
     try {
-      $Object3 = Invoke-WebRequest -Uri $ReleaseNotesUrl | ConvertFrom-Html
+      $Object4 = Invoke-WebRequest -Uri $ReleaseNotesUrl | ConvertFrom-Html
 
       # Remove headers
-      $Object3.SelectNodes('/html/body/main/section[contains(./div/@class, "release-notes-container")]//div[@class="see-all-releases"]').ForEach({ $_.Remove() })
+      $Object4.SelectNodes('//section[contains(./div/@class, "release-notes-container")]//div[@class="see-all-releases"]').ForEach({ $_.Remove() })
 
       # ReleaseNotes (en-US)
       $this.CurrentState.Locale += [ordered]@{
         Locale = 'en-US'
         Key    = 'ReleaseNotes'
-        Value  = $Object3.SelectSingleNode('/html/body/main/section[contains(./div/@class, "release-notes-container")]') | Get-TextContent | Format-Text
+        Value  = $Object4.SelectSingleNode('//section[contains(./div/@class, "release-notes-container")]') | Get-TextContent | Format-Text
       }
     } catch {
       $_ | Out-Host
@@ -58,14 +80,6 @@ switch -Regex ($this.Check()) {
     $this.Write()
   }
   'Updated' {
-    $Object4 = [ordered]@{}
-    Invoke-RestMethod -Uri "${Prefix}${OriginalVersion}/SHA256SUMS" | Split-LineEndings |
-      Where-Object -FilterScript { $_ -match '(win32|win64|win64-aarch64)/' -and $_ -match '\.(exe|msix)$' -and -not $_.Contains('Thunderbird Installer') } |
-      ForEach-Object -Process {
-        $Entries = $_.Split('  ')
-        $Object4[$Entries[1]] = $Entries[0].ToUpper()
-      }
-
     foreach ($Locale in $Locales) {
       $this.CurrentState.Installer = @()
 
@@ -93,7 +107,7 @@ switch -Regex ($this.Check()) {
             Architecture    = $Arch
             InstallerType   = 'nullsoft'
             InstallerUrl    = "${Prefix}${OriginalVersion}/$($ArchMap[$Arch])/${Locale}/Thunderbird Setup ${OriginalVersion}.exe"
-            InstallerSha256 = $Object4["$($ArchMap[$Arch])/${Locale}/Thunderbird Setup ${OriginalVersion}.exe"]
+            InstallerSha256 = $Object2["$($ArchMap[$Arch])/${Locale}/Thunderbird Setup ${OriginalVersion}.exe"]
             ProductCode     = "Mozilla Thunderbird ${ShortVersion} (${Arch} ${Locale})"
           }
         }
