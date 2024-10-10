4ec56fe5bfb2054d73ee553091574b32b3945206
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-10-10 14:09:41 +0800

    Task: Add Flydigi.FlydigiPCSpace, Flydigi.FlydigiSpaceStation

---
+++ Tasks/Flydigi.FlydigiPCSpace/Script.ps1
@@ -0,0 +1,44 @@
+$Object1 = Invoke-RestMethod -Uri 'https://api.flydigi.com/web/update/init?app_class_type=13'
+
+# Version
+$this.CurrentState.Version = $Object1.data.version_code
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.data.apk_url
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.data.update_time | ConvertFrom-UnixTimeSeconds
+
+      # ReleaseNotes (zh-CN)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'zh-CN'
+        Key    = 'ReleaseNotes'
+        Value  = $Object1.data.upgrade_point -replace '\d{4}\.\d{1,2}\.\d{1,2}\s*$' | Format-Text
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+
+    # InstallerSha256
+    $this.CurrentState.Installer[0]['InstallerSha256'] = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
+    # RealVersion
+    $this.CurrentState.RealVersion = $InstallerFile | Read-ProductVersionFromExe
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
---
+++ Tasks/Flydigi.FlydigiSpaceStation/Script.ps1
@@ -0,0 +1,37 @@
+$Object1 = Invoke-RestMethod -Uri 'https://api.flydigi.com/web/update/init?app_class_type=18'
+
+# Version
+$this.CurrentState.Version = $Object1.data.version_code
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.data.apk_url
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.data.update_time | ConvertFrom-UnixTimeSeconds
+
+      # ReleaseNotes (zh-CN)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'zh-CN'
+        Key    = 'ReleaseNotes'
+        Value  = $Object1.data.upgrade_point -replace '\d{4}-\d{1,2}-\d{1,2}\s*$' -replace '^更新点：' | Format-Text
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
