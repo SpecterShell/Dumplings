8a1869e83346e3f7517be95c3c41e088f821e3c6
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-10-13 16:47:27 +0800

    Task: Add Veeam.VeeamAgent

---
+++ Tasks/Veeam.VeeamAgent/Script.ps1
@@ -0,0 +1,62 @@
+$Object1 = Invoke-RestMethod -Uri 'https://autolk.veeam.com/json-rpc.php' -Method Post -Body (
+  @{
+    id     = 1
+    method = 'CheckForUpdates'
+    params = @{
+      ProductName = 'AgentWindows'
+      Version     = $this.LastState.Contains('Version') ? $this.LastState.Version : '6.1.2.134'
+    }
+  } | ConvertTo-Json -Compress
+)
+$Object2 = $Object1.result.data | ConvertFrom-Base64 | ConvertFrom-Xml
+
+# Version
+$this.CurrentState.Version = $Object2.UpdateInfo.VeeamUpdates.AgentWindows.releaseFileVersion
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object2.UpdateInfo.VeeamUpdates.AgentWindows.Url
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object2.UpdateInfo.issuetime | Get-Date -AsUTC
+
+      # ReleaseNotes (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotes'
+        Value  = $Object2.UpdateInfo.VeeamUpdates.AgentWindows.Description | Format-Text
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+    $InstallerFileExtracted = New-TempFolder
+    7z.exe e -aoa -ba -bd -y -o"${InstallerFileExtracted}" $InstallerFile 'Endpoint\Endpoint\Veeam_B&R_Endpoint_x64.msi' | Out-Host
+    $InstallerFile2 = Join-Path $InstallerFileExtracted 'Veeam_B&R_Endpoint_x64.msi'
+    # InstallerSha256
+    $this.CurrentState.Installer[0]['InstallerSha256'] = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
+    # AppsAndFeaturesEntries + ProductCode
+    $this.CurrentState.Installer[0]['AppsAndFeaturesEntries'] = @(
+      [ordered]@{
+        ProductCode   = $this.CurrentState.Installer[0]['ProductCode'] = $InstallerFile2 | Read-ProductCodeFromMsi
+        UpgradeCode   = $InstallerFile2 | Read-UpgradeCodeFromMsi
+        InstallerType = 'msi'
+      }
+    )
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
