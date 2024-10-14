b1d90fb9ead1dfd08a75795ea2983d7a41a55386
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-10-14 23:15:22 +0800

    Task: Add OK1DIX.VUSC4WIN

---
+++ Tasks/OK1DIX.VUSC4WIN/Script.ps1
@@ -0,0 +1,51 @@
+$Object1 = Invoke-WebRequest -Uri 'https://www.ok2kkw.com/vusc4win_eng.htm'
+
+# Version
+$this.CurrentState.Version = [regex]::Match($Object1.Content, 'VUSC(?:\s|&nbsp;)+4(?:\s|&nbsp;)+WIN(?:\s|&nbsp;)+(\d+(?:\.\d+)+)').Groups[1].Value
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl         = $Object1.Links.Where({ try { $_.href.EndsWith('.exe') -and $_.href.Contains('setup') } catch {} }, 'First')[0].href
+  InstallationMetadata = [ordered]@{
+    DefaultInstallLocation = '%HOMEDRIVE%\VUSC'
+    Files                  = @(
+      [ordered]@{
+        RelativeFilePath = 'VUSC.exe'
+        FileType         = 'launch'
+      }
+    )
+  }
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseNotesUrl
+      $this.CurrentState.Locale += [ordered]@{
+        Key   = 'ReleaseNotesUrl'
+        Value = $Object1.Links.Where({ try { $_.href.Contains('news') } catch {} }, 'First')[0].href
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl | Rename-Item -NewName { "${_}.exe" } -PassThru | Select-Object -ExpandProperty 'FullName'
+
+    # InstallerSha256
+    $this.CurrentState.Installer[0]['InstallerSha256'] = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
+    # InstallationMetadata > Files > FileSha256
+    Start-ThreadJob -ScriptBlock { Start-Process -FilePath $using:InstallerFile -ArgumentList '/SP-', '/VERYSILENT', '/SUPPRESSMSGBOXES', '/NORESTART' -Wait } | Wait-Job -Timeout 300 | Receive-Job | Out-Host
+    $FileSha256 = (Get-FileHash -Path (Join-Path $Env:HOMEDRIVE 'VUSC' 'VUSC.exe') -Algorithm SHA256).Hash
+    $this.CurrentState.Installer | ForEach-Object -Process { $_.InstallationMetadata.Files[0]['FileSha256'] = $FileSha256 }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
