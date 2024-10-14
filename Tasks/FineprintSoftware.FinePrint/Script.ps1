57f479328087c4691ae0df662181d0ac69f7b515
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-10-14 15:07:17 +0800

    Task: Add FineprintSoftware.FinePrint

---
+++ Tasks/FineprintSoftware.FinePrint/Script.ps1
@@ -0,0 +1,47 @@
+$UserAgent = $DumplingsDefaultUserAgent -replace 'PowerShell/\S+'
+$Object1 = Invoke-WebRequest -Uri 'https://fineprint.com/' -UserAgent $UserAgent | ConvertFrom-Html
+$Object2 = $Object1.SelectSingleNode('//div[@class="fp homeblock"]//a[contains(text(), "DOWNLOAD")]')
+
+# Version
+$this.CurrentState.Version = [regex]::Match($Object2.InnerText, 'v(\d+(?:\.\d+)+)').Groups[1].Value
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object2.Attributes['href'].Value
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      $Object3 = Invoke-WebRequest -Uri 'https://fineprint.com/fp/release-notes/' -UserAgent $UserAgent | ConvertFrom-Html
+
+      $ReleaseNotesTitleNode = $Object3.SelectSingleNode("//div[@id='content']/h3[contains(text(), 'Version $($this.CurrentState.Version)')]")
+      if ($ReleaseNotesTitleNode) {
+        # ReleaseTime
+        $this.CurrentState.ReleaseTime = [regex]::Match($ReleaseNotesTitleNode.InnerText, '([a-zA-Z]+\W+\d{1,2}\W+\d{4})').Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
+
+        $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node -and $Node.Name -ne 'h3'; $Node = $Node.NextSibling) { $Node }
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseTime and ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
