967a226e01eec1399bc5031137d4d8be2379a426
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-10-15 17:06:19 +0800

    Task: Add Oracle.OracleLinux

---
+++ Tasks/Oracle.OracleLinux.9.1/Script.ps1
@@ -0,0 +1,38 @@
+$Object1 = (Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/microsoft/WSL/master/distributions/DistributionInfo.json').distributions.Where({ $_.StoreAppId -eq '9N6CN5STZRX6' }, 'First')[0]
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = $Object1.Amd64PackageUrl
+}
+
+# Version
+$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d{6})').Groups[1].Value
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = [datetime]::ParseExact($this.CurrentState.Version, 'yyMMdd', $null).ToString('yyyy-MM-dd')
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+    $InstallerFileExtracted = New-TempFolder
+    7z.exe e -aoa -ba -bd -y -o"${InstallerFileExtracted}" $InstallerFile 'AppxManifest.xml' | Out-Host
+    $Object2 = Join-Path $InstallerFileExtracted 'AppxManifest.xml' | Get-Item | Get-Content -Raw | ConvertFrom-Xml
+    # RealVersion
+    $this.CurrentState.RealVersion = $Object2.Package.Identity.Version
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
