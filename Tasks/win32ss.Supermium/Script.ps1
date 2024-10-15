5fa31c1b7d68e0e2f30f2aff5de4bb021ffc240b
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-10-15 12:54:58 +0800

    Task: win32ss.Supermium Add RealVersion and reset

--- Tasks/win32ss.Supermium/Script.ps1
+++ Tasks/win32ss.Supermium/Script.ps1
@@ -11,7 +11,7 @@ $this.CurrentState.Installer += [ordered]@{
   Architecture = 'x86'
   InstallerUrl = $Object1.assets.Where({ $_.name.EndsWith('.exe') -and $_.name.Contains('_32') -and $_.name.Contains('setup') }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
 }
-$this.CurrentState.Installer += [ordered]@{
+$this.CurrentState.Installer += $InstallerX64 = [ordered]@{
   Architecture = 'x64'
   InstallerUrl = $Object1.assets.Where({ $_.name.EndsWith('.exe') -and $_.name.Contains('_64') -and $_.name.Contains('setup') }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
 }
@@ -36,6 +36,16 @@ switch -Regex ($this.Check()) {
       $_ | Out-Host
       $this.Log($_, 'Warning')
     }
+
+    $InstallerFile = Get-TempFile -Uri $InstallerX64.InstallerUrl | Rename-Item -NewName { "${_}.exe" } -PassThru | Select-Object -ExpandProperty 'FullName'
+
+    # InstallerSha256
+    $InstallerX64['InstallerSha256'] = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
+    # RealVersion
+    # The version can only be obtained from the ARP registry after installation
+    # Use ThreadJob with a timeout to avoid being stuck, and the script will fail in the next expression
+    Start-ThreadJob -ScriptBlock { Start-Process -FilePath $using:InstallerFile -ArgumentList '-y', '--silent', '--system-level' -Wait } | Wait-Job -Timeout 300 | Receive-Job | Out-Host
+    $this.CurrentState.RealVersion = Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Supermium' -Name 'DisplayVersion'
 
     $this.Print()
     $this.Write()
