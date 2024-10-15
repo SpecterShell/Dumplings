f6c9a4bcde24b4b57368003119d70930c23f95fa
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-10-15 18:10:41 +0800

    Task: Add Zhouqluo.Bilidown

---
+++ Tasks/Zhouqluo.Bilidown/Script.ps1
@@ -0,0 +1,37 @@
+$Object1 = Invoke-WebRequest -Uri "https://zhouql.vip/bdupdate/win32/latest.yml?noCache=$(Get-Random)" | Read-ResponseContent | ConvertFrom-Yaml
+
+# Version
+$this.CurrentState.Version = $Object1.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.files.url
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.releaseDate | Get-Date -AsUTC
+
+      # ReleaseNotes (zh-CN)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'zh-CN'
+        Key    = 'ReleaseNotes'
+        Value  = $Object1.files.desc | ConvertFrom-Html | Get-TextContent | Format-Text
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
