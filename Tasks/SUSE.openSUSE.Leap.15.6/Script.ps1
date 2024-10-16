f208a7912701cfce947efb7a1a9ce4159fdeb079
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-10-16 16:30:22 +0800

    Task: Add SUSE.openSUSE, SUSE.SUSE

---
+++ Tasks/SUSE.openSUSE.Leap.15.6/Script.ps1
@@ -0,0 +1,42 @@
+$Object1 = (Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/microsoft/WSL/master/distributions/DistributionInfo.json').distributions.Where({ $_.PackageFamilyName -eq '46932SUSE.openSUSELeap15.6_022rs5jcyhyac' }, 'First')[0]
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = $Object1.Amd64PackageUrl
+}
+# $this.CurrentState.Installer += [ordered]@{
+#   Architecture = 'arm64'
+#   InstallerUrl = $Object1.Arm64PackageUrl
+# }
+
+# Version
+$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d{6})').Groups[1].Value
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = [datetime]::ParseExact($this.CurrentState.Version, 'yyMMdd', $null).ToString('yyyy-MM-dd')
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+    $InstallerFileExtracted = New-TempFolder
+    7z.exe e -aoa -ba -bd -y -o"${InstallerFileExtracted}" $InstallerFile 'AppxManifest.xml' | Out-Host
+    $Object2 = Join-Path $InstallerFileExtracted 'AppxManifest.xml' | Get-Item | Get-Content -Raw | ConvertFrom-Xml
+    # RealVersion
+    $this.CurrentState.RealVersion = $Object2.Package.Identity.Version
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
