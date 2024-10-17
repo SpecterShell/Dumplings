d2ed4c08a745e547262de352a5402b6ae68602bf
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-10-17 11:48:52 +0800

    Task: Surfshark.Surfshark Update to x64

--- Tasks/Surfshark.Surfshark/Script.ps1
+++ Tasks/Surfshark.Surfshark/Script.ps1
@@ -1,4 +1,4 @@
-$Object1 = (Invoke-WebRequest -Uri 'https://downloads.surfshark.com/windows/meta/update.xml' | Read-ResponseContent | ConvertFrom-Xml).channels.channel.Where({ $_.name -eq 'stable' }, 'First')[0].releases.release[-1]
+$Object1 = (Invoke-WebRequest -Uri 'https://downloads.surfshark.com/windows/meta/update.xml' | Read-ResponseContent | ConvertFrom-Xml).channels.channel.Where({ $_.name -eq 'stable64' }, 'First')[0].releases.release[-1]
 
 # Version
 $this.CurrentState.Version = $Object1.version
