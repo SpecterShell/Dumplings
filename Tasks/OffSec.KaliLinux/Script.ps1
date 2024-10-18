b7753ea589abf77123d7a178f45f22a37f1d3dbf
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-10-18 21:09:52 +0800

    Task: Add OffSec.KaliLinux

---
+++ Tasks/OffSec.KaliLinux/Script.ps1
@@ -0,0 +1,27 @@
+$Object1 = (Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/microsoft/WSL/master/distributions/DistributionInfo.json').distributions.Where({ $_.PackageFamilyName -eq 'KaliLinux.54290C8133FEE_ey8k8hqnwqnmg' }, 'First')[0]
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = $Object1.Amd64PackageUrl
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'arm64'
+  InstallerUrl = $Object1.Arm64PackageUrl
+}
+
+# Version
+$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d+(?:\.\d+){2,})').Groups[1].Value
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
