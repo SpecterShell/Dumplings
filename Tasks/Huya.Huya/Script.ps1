d5c5b9425fe74395ba62f3da9efd48e82cb2e394
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-10-18 10:37:03 +0800

    Task: Huya.Huya Use CRC64 instead of MD5 and reset

--- Tasks/Huya.Huya/Script.ps1
+++ Tasks/Huya.Huya/Script.ps1
@@ -4,8 +4,8 @@ $this.CurrentState.Installer += [ordered]@{
 }
 
 $Object1 = Invoke-WebRequest -Uri $this.CurrentState.Installer[0].InstallerUrl -Method Head
-# MD5
-$this.CurrentState.MD5 = $Object1.Headers.'Content-MD5'[0]
+# Hash
+$this.CurrentState.Hash = $Object1.Headers.'x-oss-hash-crc64ecma'[0]
 
 # Case 0: Force submit the manifest
 if ($Global:DumplingsPreference.Contains('Force')) {
@@ -39,8 +39,8 @@ if ($this.Status.Contains('New')) {
   return
 }
 
-# Case 2: The MD5 was not updated
-if ($this.CurrentState.MD5 -eq $this.LastState.MD5) {
+# Case 2: The hash was not updated
+if ($this.CurrentState.Hash -eq $this.LastState.Hash) {
   $this.Log("The version $($this.LastState.Version) from the last state is the latest", 'Info')
   return
 }
@@ -57,18 +57,18 @@ if ([string]::IsNullOrWhiteSpace($this.CurrentState.Version)) {
 }
 
 switch -Regex ($this.Check()) {
-  # Case 5: The MD5 and the version were updated
+  # Case 5: The hash and the version were updated
   'Updated|Rollbacked' {
     $this.Print()
     $this.Write()
     $this.Message()
     $this.Submit()
   }
-  # Case 4: The MD5 was updated, but the version wasn't
+  # Case 4: The hash was updated, but the version wasn't
   # The installer might be updated without changing the version (e.g., virus database update)
   # Force submit the manifest even if neither the version nor the installer has changed
   Default {
-    $this.Log('The MD5 was changed, but the version is the same', 'Info')
+    $this.Log('The hash was changed, but the version is the same', 'Info')
     $this.Config.IgnorePRCheck = $true
     $this.Print()
     $this.Write()
