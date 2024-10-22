46f022033ca7927f67cc320a348c0a97a042c9d6
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-10-22 17:38:28 +0800

    Task: Add AhoyLabs.BackyardAI

---
+++ Tasks/AhoyLabs.BackyardAI/Script.ps1
@@ -0,0 +1,54 @@
+$Object1 = Invoke-RestMethod -Uri "https://update.backyard.ai/update/win32/$($this.LastState.Contains('Version') ? $this.LastState.Version : '0.29.0')" -StatusCodeVariable 'StatusCode'
+
+if ($StatusCode -eq 204) {
+  $this.Log("The version $($this.LastState.Version) from the last state is the latest, skip checking", 'Info')
+  return
+}
+
+# Version
+$this.CurrentState.Version = $Object1.name
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.url | Split-Uri -LeftPart Path
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.pub_date.ToUniversalTime()
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    try {
+      $Object2 = Invoke-WebRequest -Uri 'https://backyard.ai/changelog' | ConvertFrom-Html
+
+      $ReleaseNotesNode = $Object2.SelectSingleNode("//div[@id='$($this.CurrentState.Version)']")
+      if ($ReleaseNotesNode) {
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesNode.SelectSingleNode('./div[2]') | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
