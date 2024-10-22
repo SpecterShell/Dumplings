b84fce116a1bb45dce643be2f397167195580407
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-10-22 18:10:14 +0800

    Task: Add EEO.CamIn

---
+++ Tasks/EEO.CamIn/Script.ps1
@@ -0,0 +1,30 @@
+$Object1 = (Invoke-RestMethod -Uri 'https://www.eeo.cn/sysshare/custom/download_conf.json').Where({ $_.id -eq 63 }, 'First')[0]
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $InstallerUrl = 'https:' + $Object1.confValue
+}
+
+# Version
+$this.CurrentState.Version = [regex]::Match($InstallerUrl, '(\d+(?:\.\d+){2,})').Groups[1].Value
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.updatedTime | ConvertFrom-UnixTimeSeconds
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
