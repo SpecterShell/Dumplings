55011f5d8d43f6066a892265928eac9b84010eae
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-10-24 20:31:21 +0800

    Task: EclipseAdoptium.Temurin Add arm64 installer

--- Tasks/EclipseAdoptium.Temurin.21.JDK/Script.ps1
+++ Tasks/EclipseAdoptium.Temurin.21.JDK/Script.ps1
@@ -1,14 +1,30 @@
 $Object1 = (Invoke-WebRequest -Uri 'https://api.adoptium.net/v3/assets/latest/21/hotspot?image_type=jdk&os=windows').Content | ConvertFrom-Json -AsHashtable
 
+# x64
 $Object2 = $Object1 | Where-Object -FilterScript { $_.binary.architecture -eq 'x64' } | Select-Object -First 1
+$VersionX64 = "$($Object2.version.major).$($Object2.version.minor).$($Object2.version.security).$(($Object2.version.Contains('patch') ? $Object2.version.patch * 100 : 0) + $Object2.version.build)"
+
+# arm64
+$Object3 = $Object1 | Where-Object -FilterScript { $_.binary.architecture -eq 'aarch64' } | Select-Object -First 1
+$VersionArm64 = "$($Object3.version.major).$($Object3.version.minor).$($Object3.version.security).$(($Object3.version.Contains('patch') ? $Object3.version.patch * 100 : 0) + $Object3.version.build)"
+
+if ($VersionX64 -ne $VersionArm64) {
+  $this.Log("x64 version: ${VersionX64}")
+  $this.Log("arm64 version: ${VersionArm64}")
+  throw 'Distinct versions detected'
+}
 
 # Version
-$this.CurrentState.Version = "$($Object2.version.major).$($Object2.version.minor).$($Object2.version.security).$(($Object2.version.Contains('patch') ? $Object2.version.patch * 100 : 0) + $Object2.version.build)"
+$this.CurrentState.Version = $VersionX64
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
   Architecture = 'x64'
   InstallerUrl = $Object2.binary.installer.link
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'arm64'
+  InstallerUrl = $Object3.binary.installer.link
 }
 
 switch -Regex ($this.Check()) {
