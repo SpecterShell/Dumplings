032a4c1d297f741484c104f68f03ff96427fc5f1
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-10-30 16:26:44 +0800

    Task: Anysphere.Cursor Switch to new source and add arm64 installer

--- Tasks/Anysphere.Cursor/Script.ps1
+++ Tasks/Anysphere.Cursor/Script.ps1
@@ -1,19 +1,38 @@
-$Prefix = 'https://download.todesktop.com/230313mzl4w4u92/'
+$Object1 = Invoke-RestMethod -Uri 'https://download.todesktop.com/230313mzl4w4u92/td-latest.json'
 
-$this.CurrentState = Invoke-RestMethod -Uri "${Prefix}latest.yml?noCache=$(Get-Random)" | ConvertFrom-Yaml | ConvertFrom-ElectronUpdater -Prefix $Prefix -Locale 'en-US'
+# Version
+$this.CurrentState.Version = $Object1.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = $Object1.artifacts.nsis.x64.url
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'arm64'
+  InstallerUrl = $Object1.artifacts.nsis.arm64.url
+}
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
-    # ReleaseNotesUrl
-    $this.CurrentState.Locale += [ordered]@{
-      Key   = 'ReleaseNotesUrl'
-      Value = $ReleaseNotesUrl = 'https://changelog.cursor.com/'
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.createdAt.ToUniversalTime()
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
     }
 
     try {
-      $Object1 = Invoke-WebRequest -Uri $ReleaseNotesUrl | ConvertFrom-Html
+      # ReleaseNotesUrl
+      $this.CurrentState.Locale += [ordered]@{
+        Key   = 'ReleaseNotesUrl'
+        Value = $ReleaseNotesUrl = 'https://changelog.cursor.com/'
+      }
 
-      $ReleaseNotesTitleObject = $Object1.SelectSingleNode(".//main/article[starts-with(@id, '$($this.CurrentState.Version -replace '\.0$' -replace '\.')')]")
+      $Object2 = Invoke-WebRequest -Uri $ReleaseNotesUrl | ConvertFrom-Html
+
+      $ReleaseNotesTitleObject = $Object2.SelectSingleNode(".//main/article[starts-with(@id, '$($this.CurrentState.Version -replace '\.0$' -replace '\.')')]")
       if ($ReleaseNotesTitleObject) {
         # ReleaseNotes (en-US)
         $this.CurrentState.Locale += [ordered]@{
