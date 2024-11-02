e04d7c8ee7c09be657cd423b509e933e97d9542d
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-11-02 16:28:18 +0800

    Task: Add CTerm.CTerm

---
+++ Tasks/CTerm.CTerm/Script.ps1
@@ -0,0 +1,50 @@
+$RepoOwner = 'devcpp'
+$RepoName = 'cterm'
+
+$Object1 = Invoke-RestMethod -Uri "https://gitee.com/api/v5/repos/${RepoOwner}/${RepoName}/releases/latest" -Authentication Bearer -Token (ConvertTo-SecureString -String $Global:DumplingsSecret.GiteeToken -AsPlainText)
+
+# Version
+$this.CurrentState.Version = $Object1.tag_name -creplace '^v'
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.assets.Where({ $_.name.EndsWith('.exe') }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.created_at.ToUniversalTime()
+
+      if (-not [string]::IsNullOrWhiteSpace($Object1.body)) {
+        # ReleaseNotes (zh-CN)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'zh-CN'
+          Key    = 'ReleaseNotes'
+          Value  = $Object1.body | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotes (zh-CN) for version $($this.CurrentState.Version)", 'Warning')
+      }
+
+      # ReleaseNotesUrl
+      $this.CurrentState.Locale += [ordered]@{
+        Key   = 'ReleaseNotesUrl'
+        Value = "https://gitee.com/${RepoOwner}/${RepoName}/releases/tag/$($Object1.tag_name)"
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
