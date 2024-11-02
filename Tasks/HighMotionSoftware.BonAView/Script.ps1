ce01a8d103101b59f639a2e10bec0b1defa8c84b
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-11-02 17:01:39 +0800

    Task: Add HighMotionSoftware.BonAView

---
+++ Tasks/HighMotionSoftware.BonAView/Script.ps1
@@ -0,0 +1,49 @@
+$Object1 = Invoke-WebRequest -Uri 'https://www.highmotionsoftware.com/upd/bonaview/version' | Read-ResponseContent
+
+# Version
+$this.CurrentState.Version = $Object1.Trim()
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = 'https://www.highmotionsoftware.com/download/BonAView/file/setup-bonaview-latest.exe'
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      $Object2 = Invoke-WebRequest -Uri 'https://www.highmotionsoftware.com/download-center/bonaview' | ConvertFrom-Html
+
+      if ($Object2.SelectSingleNode('//strong[text()="Version:"]/following-sibling::text()').InnerText.Trim() -eq $this.CurrentState.Version) {
+        # ReleaseTime
+        $this.CurrentState.ReleaseTime = $Object2.SelectSingleNode('//strong[text()="Date:"]/following-sibling::text()').InnerText.Trim() | Get-Date -Format 'yyyy-MM-dd'
+      } else {
+        $this.Log("No ReleaseTime for version $($this.CurrentState.Version)", 'Warning')
+      }
+
+      $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//p[contains(./b/text(), 'BonAView v$($this.CurrentState.Version)')]")
+      if ($ReleaseNotesTitleNode) {
+        $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node -and -not ($Node.Name -eq 'p' -and $Node.InnerText.Contains('BonAView v')); $Node = $Node.NextSibling) { $Node }
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
