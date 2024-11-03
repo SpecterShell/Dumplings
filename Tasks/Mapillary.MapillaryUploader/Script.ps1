fefac2d14b68b19ff114beee7d30127af4ead202
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-11-03 15:20:26 +0800

    Task: Mapillary.MapillaryUploader Add release notes

--- Tasks/Mapillary.MapillaryUploader/Script.ps1
+++ Tasks/Mapillary.MapillaryUploader/Script.ps1
@@ -20,6 +20,26 @@ switch -Regex ($this.Check()) {
       $this.Log($_, 'Warning')
     }
 
+    try {
+      $Object2 = Invoke-WebRequest -Uri 'https://www.mapillary.com/release-notes' | ConvertFrom-Html
+
+      $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//h3[contains(text(), 'Mapillary Desktop Uploader $($this.CurrentState.Version)')]")
+      if ($ReleaseNotesTitleNode) {
+        $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node -and $Node.Name -notin @('h2', 'h3'); $Node = $Node.NextSibling) { $Node }
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
     $this.Print()
     $this.Write()
   }
