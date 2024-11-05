7dba5bde916672e4cd182d27bdbbd2e691660f3b
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-11-05 17:10:54 +0800

    Task: Add High-Logic.Scanahand

---
+++ Tasks/High-Logic.Scanahand/Script.ps1
@@ -0,0 +1,83 @@
+# x86
+$Object1 = Invoke-RestMethod -Uri 'https://www.high-logic.com/process.php' -Body @{
+  plt = 'win'
+  tar = 'x86'
+  app = 'sh'
+  ver = $this.LastState.Contains('Version') ? $this.LastState.Version : '8.0.0.319'
+  lid = '0'
+  mod = '0'
+}
+
+if ($Object1.versioninfo.haveupdate -eq 'false') {
+  $this.Log("The version $($this.LastState.Version) from the last state is the latest, skip checking", 'Info')
+  return
+}
+
+# x64
+$Object2 = Invoke-RestMethod -Uri 'https://www.high-logic.com/process.php' -Body @{
+  plt = 'win'
+  tar = 'x64'
+  app = 'sh'
+  ver = $this.LastState.Contains('Version') ? $this.LastState.Version : '8.0.0.319'
+  lid = '0'
+  mod = '0'
+}
+
+if ($Object2.versioninfo.haveupdate -eq 'false') {
+  $this.Log("The version $($this.LastState.Version) from the last state is the latest, skip checking", 'Info')
+  return
+}
+
+if ($Object1.versioninfo.installer.version -ne $Object2.versioninfo.installer.version) {
+  $this.Log("x86 version: $($Object1.versioninfo.installer.version)")
+  $this.Log("x64 version: $($Object2.versioninfo.installer.version)")
+  throw 'Inconsistent versions detected'
+}
+
+# Version
+$this.CurrentState.Version = $Object2.versioninfo.installer.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x86'
+  InstallerUrl = $Object1.versioninfo.installer.url
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = $Object2.versioninfo.installer.url
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      $Object2 = Invoke-WebRequest -Uri 'https://www.high-logic.com/font-generator/scanahand/changelog' | ConvertFrom-Html
+
+      $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//li[@class='el-item' and contains(.//h3[contains(@class, 'el-title')], 'Scanahand $($this.CurrentState.Version)')]")
+      if ($ReleaseNotesTitleNode) {
+        # ReleaseTime
+        $this.CurrentState.ReleaseTime = [regex]::Match($ReleaseNotesTitleNode.SelectSingleNode('.//h3[contains(@class, "el-title")]').InnerText, '([a-zA-Z]+\W+\d{1,2}\W+20\d{1,2})').Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
+
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesTitleNode.SelectNodes('.//div[contains(@class, "el-content")]') | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
