2356076a5ef450b97d2ea05dc9470cb482605a2c
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-11-05 17:34:11 +0800

    Task: Add HulubuluSoftware.AdvancedRenamer

---
+++ Tasks/HulubuluSoftware.AdvancedRenamer/Script.ps1
@@ -0,0 +1,53 @@
+$Object1 = Invoke-WebRequest -Uri 'https://www.advancedrenamer.com/download' | ConvertFrom-Html
+$Object2 = $Object1.SelectSingleNode('//div[@class="siteblock" and contains(., "Advanced Renamer 4")]')
+
+# Version
+$this.CurrentState.Version = [regex]::Match($Object1.InnerText, 'Advanced Renamer (4(?:\.\d+)+)').Groups[1].Value
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = Join-Uri 'https://www.advancedrenamer.com/' $Object2.SelectSingleNode('.//a[@class="btn_download" and contains(@href, ".exe")]').Attributes['href'].Value
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = [regex]::Match($Object2.InnerText, '(20\d{1,2}-\d{1,2}-\d{1,2})').Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    try {
+      $Object3 = Invoke-WebRequest -Uri 'https://www.advancedrenamer.com/whatsnew_v4' | ConvertFrom-Html
+
+      $ReleaseNotesTitleNode = $Object3.SelectSingleNode("//div[@class='siteblock' and contains(./h2/text(), 'Version $($this.CurrentState.Version)')]/h2")
+      if ($ReleaseNotesTitleNode) {
+        # ReleaseTime
+        $this.CurrentState.ReleaseTime ??= [regex]::Match($ReleaseNotesTitleNode.SelectSingleNode('./following-sibling::text()').InnerText, '(20\d{1,2}-\d{1,2}-\d{1,2})').Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
+
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesTitleNode.SelectNodes('./following-sibling::br[1]/following-sibling::node()') | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
