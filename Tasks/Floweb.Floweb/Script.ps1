f0c6b9a3f72d76f173164c3754a73cfe8511aeee
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-11-06 21:26:09 +0800

    Task: Add Floweb.Floweb

---
+++ Tasks/Floweb.Floweb/Script.ps1
@@ -0,0 +1,56 @@
+$Object1 = Invoke-RestMethod -Uri 'https://download.once.work/floweb-latest.json'
+
+# Version
+$this.CurrentState.Version = $Object1.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.platforms.'windows-x86_64'.url | Split-Uri -LeftPart Path
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.pub_date.ToUniversalTime()
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    try {
+      $Object2 = Invoke-RestMethod -Uri 'https://download.once.work/floweb-version.json'
+
+      $ReleaseNotesObject = $Object2.changes.Where({ $_.type.StartsWith("v$($this.CurrentState.Version)") }, 'First')
+      if ($ReleaseNotesObject) {
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesObject.description.en | Format-Text
+        }
+
+        # ReleaseNotes (zh-CN)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'zh-CN'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesObject.description.'zh_CN' | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotes for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
