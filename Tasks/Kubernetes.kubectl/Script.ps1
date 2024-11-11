a0d0e53d5ac3557ce7fc626482bf6c5e83059d06
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-11-11 12:09:08 +0800

    Task: Add Kubernetes.kubectl

---
+++ Tasks/Kubernetes.kubectl/Script.ps1
@@ -0,0 +1,31 @@
+$Object1 = Invoke-WebRequest -Uri 'https://cdn.dl.k8s.io/release/stable.txt'
+
+# Version
+$this.CurrentState.Version = $Object1.Content.Trim() -replace '^v'
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x86'
+  InstallerUrl = "https://dl.k8s.io/release/v$($this.CurrentState.Version)/bin/windows/386/kubectl.exe"
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = "https://dl.k8s.io/release/v$($this.CurrentState.Version)/bin/windows/amd64/kubectl.exe"
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'arm64'
+  InstallerUrl = "https://dl.k8s.io/release/v$($this.CurrentState.Version)/bin/windows/arm64/kubectl.exe"
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
