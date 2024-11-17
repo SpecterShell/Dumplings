7920988167e0d056c8abb582a3a0606ea47bec47
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-11-17 13:23:47 +0800

    Task: GeoGebra.Classic Remove DisplayVersion

--- Tasks/GeoGebra.Classic/Script.ps1
+++ Tasks/GeoGebra.Classic/Script.ps1
@@ -1,30 +1,18 @@
-# Version + Installer
+# Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerType          = 'exe'
-  InstallerUrl           = $InstallerUrl = Get-RedirectedUrl -Uri 'https://download.geogebra.org/package/win-autoupdate'
-  AppsAndFeaturesEntries = @(
-    [ordered]@{
-      DisplayVersion = $this.CurrentState.Version = [regex]::Match($InstallerUrl, '-(\d+-\d+-\d+)').Groups[1].Value.Replace('-', '.')
-    }
-  )
+  InstallerType = 'exe'
+  InstallerUrl  = $InstallerUrl = Get-RedirectedUrl -Uri 'https://download.geogebra.org/package/win-autoupdate'
 }
-$this.CurrentState.Installer += $InstallerWix = [ordered]@{
+$this.CurrentState.Installer += [ordered]@{
   InstallerType = 'wix'
   InstallerUrl  = $InstallerUrl -replace '\.exe$', '.msi'
 }
 
+# Version
+$this.CurrentState.Version = [regex]::Match($InstallerUrl, '-(\d+-\d+-\d+)').Groups[1].Value.Replace('-', '.')
+
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
-    $InstallerFileWix = Get-TempFile -Uri $InstallerWix.InstallerUrl
-    $InstallerWix['InstallerSha256'] = (Get-FileHash -Path $InstallerFileWix -Algorithm SHA256).Hash
-    $InstallerWix['AppsAndFeaturesEntries'] = @(
-      [ordered]@{
-        DisplayVersion = $InstallerFileWix | Read-ProductVersionFromMsi
-        ProductCode    = $InstallerWix['ProductCode'] = $InstallerFileWix | Read-ProductCodeFromMsi
-        UpgradeCode    = $InstallerFileWix | Read-UpgradeCodeFromMsi
-      }
-    )
-
     $this.Print()
     $this.Write()
   }
