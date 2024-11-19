f6804bf5ceda00f9dc60fb99f3513704fe46d2b2
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-11-19 19:38:10 +0800

    Task: Add ScreenCloud.ScreenCloudPlayer

---
+++ Tasks/ScreenCloud.ScreenCloudPlayer/Script.ps1
@@ -0,0 +1,49 @@
+$Prefix = 'https://release.screen.cloud/player/desktop/channel/stable/'
+
+$Object1 = Invoke-RestMethod -Uri "${Prefix}latest.yml?noCache=$(Get-Random)" | ConvertFrom-Yaml
+
+# Version
+$this.CurrentState.Version = $Object1.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture  = 'x86'
+  InstallerType = 'nullsoft'
+  InstallerUrl  = $Prefix + $Object1.files.Where({ $_.url.Contains('ia32') }, 'First')[0].url
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture  = 'x64'
+  InstallerType = 'nullsoft'
+  InstallerUrl  = $Prefix + $Object1.files.Where({ $_.url.Contains('x64') }, 'First')[0].url
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture  = 'x86'
+  InstallerType = 'wix'
+  InstallerUrl  = $Prefix + $Object1.files.Where({ $_.url.Contains('ia32') }, 'First')[0].url -replace '\.exe$', '.msi'
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture  = 'x64'
+  InstallerType = 'wix'
+  InstallerUrl  = $Prefix + $Object1.files.Where({ $_.url.Contains('x64') }, 'First')[0].url -replace '\.exe$', '.msi'
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.releaseDate | Get-Date -AsUTC
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
