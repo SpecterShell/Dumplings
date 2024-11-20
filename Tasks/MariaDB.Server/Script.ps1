7c05a67f088dac098e6e4efb52e6423a240fe00c
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-11-20 16:34:44 +0800

    Task: MariaDB.Server Fix

--- Tasks/MariaDB.Server/Script.ps1
+++ Tasks/MariaDB.Server/Script.ps1
@@ -1,10 +1,9 @@
 $Object1 = Invoke-RestMethod -Uri 'https://downloads.mariadb.org/rest-api/mariadb/all-releases/'
 
 # Version
-$this.CurrentState.Version = $Object1.releases.Where({ $_.status -eq 'stable' }, 'First')[0].release_number
+$this.CurrentState.Version = $Version = $Object1.releases.Where({ $_.status -eq 'stable' }, 'First')[0].release_number
 
-$Object2 = (Invoke-RestMethod -Uri "https://downloads.mariadb.org/rest-api/mariadb/$($this.CurrentState.Version)/?os=Windows&cpu=x86_64").release_data[$this.CurrentState.Version]
-
+$Object2 = (Invoke-RestMethod -Uri "https://downloads.mariadb.org/rest-api/mariadb/${Version}/?os=Windows&cpu=x86_64").release_data.$Version
 # Installer
 $this.CurrentState.Installer += [ordered]@{
   InstallerUrl = $Object2.files.Where({ $_.package_type -eq 'MSI Package' }, 'First')[0].file_download_url
