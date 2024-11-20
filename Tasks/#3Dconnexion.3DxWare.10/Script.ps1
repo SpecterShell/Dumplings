2e2da8e44810b1536b1a53f77edb699e1720b5a1
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-11-20 13:47:43 +0800

    Task: Fix conditions

--- Tasks/#3Dconnexion.3DxWare.10/Script.ps1
+++ Tasks/#3Dconnexion.3DxWare.10/Script.ps1
@@ -35,14 +35,25 @@ switch -Regex ($this.Check()) {
     if ($Global:DumplingsPreference.Contains('EnableWrite') -and $Global:DumplingsPreference.EnableWrite) {
       $OldReleases | ConvertTo-Yaml -OutFile $OldReleasesPath -Force
     }
-
+  }
+  'New' {
     $this.Print()
     $this.Write()
   }
   { $_ -match 'Changed' -and $_ -notmatch 'Updated|Rollbacked' } {
+    $this.Print()
+    $this.Write()
     $this.Message()
   }
-  { $_ -match 'Updated|Rollbacked' -and -not $OldReleases.Contains($this.CurrentState.Version) } {
+  'Updated' {
+    $this.Print()
+    $this.Write()
+    if (-not $OldReleases.Contains($this.CurrentState.Version)) {
+      $this.Message()
+    }
+  }
+  { $_ -match 'Rollbacked' -and -not $OldReleases.Contains($this.CurrentState.Version) } {
+    $this.Print()
     $this.Message()
   }
 }
