3eab962a71e0fe4debddb5700327d1b95b651418
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-11-21 13:36:34 +0800

    Task: Add Meta.Messenger

---
+++ Tasks/Meta.Messenger/Script.ps1
@@ -0,0 +1,39 @@
+$Object1 = Invoke-RestMethod -Uri 'https://www.facebook.com/messenger/desktop/zeratul/update.xml?target=zeratul&platform=win'
+
+# Version
+$this.CurrentState.Version = $Object1.enclosure.shortVersionString
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = 'https://www.messenger.com/messenger/desktop/downloadV2/?platform=win'
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseNotes (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotes'
+        Value  = $Object1.description | Format-Text
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+
+    # InstallerSha256
+    $this.CurrentState.Installer[0]['InstallerSha256'] = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
+    # RealVersion
+    $this.CurrentState.RealVersion = ($InstallerFile | Read-ProductVersionFromExe).Split('.')[0..2] -join '.'
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+    $this.Submit()
+  }
+}
