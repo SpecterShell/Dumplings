a68fd53f138088e705afa80cf2cc954598cfef4b
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-11-22 19:09:13 +0800

    Task: Add NetSurf.NetSurf

---
+++ Tasks/NetSurf.NetSurf/Script.ps1
@@ -0,0 +1,51 @@
+$Object1 = Invoke-WebRequest -Uri 'https://www.netsurf-browser.org/downloads/windows/' | ConvertFrom-Html
+$Object2 = $Object1.SelectSingleNode('//p[contains(@class, "downloadfirst")]/a')
+
+# Version
+$this.CurrentState.Version = [regex]::Match($Object2.InnerText, 'NetSurf (\d+(?:\.\d+)+) for Windows').Groups[1].Value
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object2.Attributes['href'].Value
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = [regex]::Match($Object2.InnerText, '(\d{1,2}\W+[a-zA-Z]+\W+20\d{2})').Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    try {
+      $Object3 = (Invoke-RestMethod -Uri 'https://download.netsurf-browser.org/netsurf/releases/ChangeLog.txt' | ConvertFrom-Markdown).Html | ConvertFrom-Html
+
+      $ReleaseNotesTitleNode = $Object3.SelectSingleNode("//h2[contains(text(), 'NetSurf $($this.CurrentState.Version)')]")
+      if ($ReleaseNotesTitleNode) {
+        $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node -and $Node.Name -ne 'h2'; $Node = $Node.NextSibling) { $Node }
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotes (en-US) for the version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
