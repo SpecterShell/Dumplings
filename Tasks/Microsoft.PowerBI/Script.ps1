399af140a9d955a974aee9f0777c5b0ec82aff71
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-11-28 11:24:52 +0800

    Task: Microsoft.PowerBI Detect ETag update and reset

--- Tasks/Microsoft.PowerBI/Script.ps1
+++ Tasks/Microsoft.PowerBI/Script.ps1
@@ -7,10 +7,6 @@ for ($Date = $StartDate; $Date -gt $EndDate; $Date = $Date.AddMonths(-1)) {
   $this.Log("Probing ${InstallerUrl}", 'Info')
   $Object1 = Invoke-WebRequest -Uri $InstallerUrl -Method Head -SkipHttpErrorCheck
   if ($Object1.StatusCode -eq 200) {
-    $InstallerFile = Get-TempFile -Uri $InstallerUrl
-
-    # Version
-    $this.CurrentState.Version = $InstallerFile | Read-ProductVersionFromExe
     # Installer
     $this.CurrentState.Installer += [ordered]@{
       Architecture = 'x64'
@@ -26,18 +22,120 @@ for ($Date = $StartDate; $Date -gt $EndDate; $Date = $Date.AddMonths(-1)) {
 
 if (-not $Found) {
   $this.Log("The version $($this.LastState.Version) from the last state is the latest", 'Info')
-  $this.CurrentState = $this.LastState
+  # Installer
+  $this.CurrentState.Installer = $this.LastState.Installer
+  # LastDate
+  $this.CurrentState.LastDate = $this.LastState.LastDate
 }
 
+$Object2 = Invoke-WebRequest -Uri $this.CurrentState.Installer[0].InstallerUrl -Method Head
+$ETag = $Object2.Headers.ETag[0]
+
+# Case 0: Force submit the manifest
+if ($Global:DumplingsPreference.Contains('Force')) {
+  $this.Log('Skip checking states', 'Info')
+
+  # ETag
+  $this.CurrentState.ETag = @($ETag)
+
+  $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+  # Version
+  $this.CurrentState.Version = $InstallerFile | Read-ProductVersionFromExe
+  # InstallerSha256
+  $this.CurrentState.Installer[0]['InstallerSha256'] = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
+  # AppsAndFeaturesEntries + ProductCode
+  $this.CurrentState.Installer[0]['AppsAndFeaturesEntries'] = @(
+    [ordered]@{
+      ProductCode = $this.CurrentState.Installer[0]['ProductCode'] = $InstallerFile | Read-UpgradeCodeFromBurn
+      UpgradeCode = $InstallerFile | Read-UpgradeCodeFromBurn
+    }
+  )
+
+  $this.Print()
+  $this.Write()
+  $this.Message()
+  $this.Submit()
+  return
+}
+
+# Case 1: The task is newly created
+if ($this.Status.Contains('New')) {
+  $this.Log('New task', 'Info')
+
+  # ETag
+  $this.CurrentState.ETag = @($ETag)
+
+  $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+  # Version
+  $this.CurrentState.Version = $InstallerFile | Read-ProductVersionFromExe
+  # InstallerSha256
+  $this.CurrentState.Installer[0]['InstallerSha256'] = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
+  # AppsAndFeaturesEntries + ProductCode
+  $this.CurrentState.Installer[0]['AppsAndFeaturesEntries'] = @(
+    [ordered]@{
+      ProductCode = $this.CurrentState.Installer[0]['ProductCode'] = $InstallerFile | Read-UpgradeCodeFromBurn
+      UpgradeCode = $InstallerFile | Read-UpgradeCodeFromBurn
+    }
+  )
+
+  $this.Print()
+  $this.Write()
+  return
+}
+
+# Case 2: The ETag was not updated
+if ($ETag -in $this.LastState.ETag) {
+  $this.Log("The version $($this.LastState.Version) from the last state is the latest", 'Info')
+  return
+}
+
+$InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+# Version
+$this.CurrentState.Version = $InstallerFile | Read-ProductVersionFromExe
+# InstallerSha256
+$this.CurrentState.Installer[0]['InstallerSha256'] = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
+# AppsAndFeaturesEntries + ProductCode
+$this.CurrentState.Installer[0]['AppsAndFeaturesEntries'] = @(
+  [ordered]@{
+    ProductCode = $this.CurrentState.Installer[0]['ProductCode'] = $InstallerFile | Read-UpgradeCodeFromBurn
+    UpgradeCode = $InstallerFile | Read-UpgradeCodeFromBurn
+  }
+)
+
+# Case 3: The installer file has an invalid version
+if ([string]::IsNullOrWhiteSpace($this.CurrentState.Version)) {
+  throw 'The current state has an invalid version'
+}
+
+# Case 4: The ETag was updated, but the hash wasn't
+if ($this.CurrentState.Installer[0].InstallerSha256 -eq $this.LastState.Installer[0].InstallerSha256) {
+  $this.Log('The ETag was changed, but the hash is the same', 'Info')
+
+  # ETag
+  $this.CurrentState.ETag = $this.LastState.ETag + $ETag
+
+  $this.Write()
+  return
+}
+
+# ETag
+$this.CurrentState.ETag = @($ETag)
+
 switch -Regex ($this.Check()) {
-  'New|Changed|Updated' {
+  # Case 6: The ETag, hash, and version were updated
+  'Updated|Rollbacked' {
     $this.Print()
     $this.Write()
-  }
-  'Changed|Updated' {
     $this.Message()
+    $this.Submit()
   }
-  'Updated' {
+  # Case 5: Both the ETag and the hash were updated, but the version wasn't
+  Default {
+    $this.Log('The ETag and the hash were changed, but the version is the same', 'Info')
+    $this.Config.IgnorePRCheck = $true
+    $this.Print()
+    $this.Write()
+    $this.Message()
     $this.Submit()
   }
 }
