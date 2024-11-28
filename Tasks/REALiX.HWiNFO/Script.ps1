37ff4ab985b4457112857de9696d41135d1f13a4
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-11-28 14:45:44 +0800

    Task: Add REALiX.HWiNFO

---
+++ Tasks/REALiX.HWiNFO/Script.ps1
@@ -0,0 +1,58 @@
+$ProjectName = 'hwinfo'
+$RootPath = '/Windows_Installer'
+
+$Object1 = Invoke-RestMethod -Uri "https://sourceforge.net/projects/${ProjectName}/rss?path=${RootPath}"
+$Assets = $Object1.Where({ $_.title.'#cdata-section' -match "^$([regex]::Escape($RootPath))/hwi64_\d+\.exe$" })
+
+# Version
+$RawVersion = [regex]::Match($Assets[0].title.'#cdata-section', 'hwi64_(\d+)\.exe').Groups[1].Value
+$this.CurrentState.Version = $RawVersion.Insert($RawVersion.Length - 2, '.')
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Assets[0].link | ConvertTo-UnescapedUri
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = [datetime]::ParseExact(
+        $Assets[0].pubDate,
+        'ddd, dd MMM yyyy HH:mm:ss "UT"',
+        (Get-Culture -Name 'en-US')
+      ) | ConvertTo-UtcDateTime -Id 'UTC'
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    try {
+      $Object2 = Invoke-WebRequest -Uri 'https://www.hwinfo.com/version-history/' | ConvertFrom-Html
+
+      $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//*[@id='tab3']//div[contains(@class, 'version-released') and contains(., 'v$($this.CurrentState.Version)')]")
+      if ($ReleaseNotesTitleNode) {
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesTitleNode.SelectNodes('./following-sibling::node()') | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotesUrl for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
