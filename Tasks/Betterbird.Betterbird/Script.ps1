20ad398df85bd70a1b7deeecfc367be30da25ed0
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-11-29 21:34:07 +0800

    Task: Betterbird.Betterbird Fix version pattern, get installer names from hash file

--- Tasks/Betterbird.Betterbird/Script.ps1
+++ Tasks/Betterbird.Betterbird/Script.ps1
@@ -9,30 +9,30 @@ $Prefix = 'https://www.betterbird.eu/downloads/'
 $Object1 = Invoke-WebRequest -Uri "${Prefix}index.php"
 
 # Version
-$VersionMatches = [regex]::Match($Object1.Content, 'Betterbird (((\d+)(?:\.\d+)+)-[a-zA-Z0-9]+)')
+$VersionMatches = [regex]::Match($Object1.Content, 'Betterbird (((\d+)(?:\.\d+)+)[a-zA-Z0-9\-]+)')
 $OriginalVersion = $VersionMatches.Groups[1].Value
 $MajorVersion = $VersionMatches.Groups[3].Value
 $this.CurrentState.Version = $ShortVersion = $VersionMatches.Groups[2].Value
 
 $Object2 = [ordered]@{}
 Invoke-RestMethod -Uri "${Prefix}sha256-${MajorVersion}.txt" | Split-LineEndings |
-  Where-Object -FilterScript { $_ -match [regex]::Escape($OriginalVersion) -and $_ -match '\.exe$' } |
+  Where-Object -FilterScript { $_.Contains($OriginalVersion) -and $_.EndsWith('.exe') } |
   ForEach-Object -Process {
     $Entries = $_ -split '\s+'
     $Object2[$Entries[1].TrimStart('*')] = $Entries[0].ToUpper()
   }
 
 # Installer
-# https://www.betterbird.eu/downloads/WindowsInstaller/betterbird-115.16.1-bb34.nl.win64.installer.exe
 foreach ($Locale in $Locales) {
   foreach ($Arch in @('x64')) {
+    $Installer = $Object2.GetEnumerator().Where({ $_.Key.Contains($Locale) -and $_.Key.Contains($ArchMap[$Arch]) }, 'First')[0]
     # Installer
     $this.CurrentState.Installer += [ordered]@{
       InstallerLocale = $Locale
       Architecture    = $Arch
       InstallerType   = 'nullsoft'
-      InstallerUrl    = "${Prefix}WindowsInstaller/betterbird-${OriginalVersion}.${Locale}.$($ArchMap[$Arch]).installer.exe"
-      InstallerSha256 = $Object2["betterbird-${OriginalVersion}.${Locale}.$($ArchMap[$Arch]).installer.exe"]
+      InstallerUrl    = "${Prefix}WindowsInstaller/$($Installer.Key)"
+      InstallerSha256 = $Installer.Value
       ProductCode     = "Betterbird ${ShortVersion} (${Arch} ${Locale})"
     }
   }
