949ac9275c7e3933be3eb01f79e65146137cd766
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-11-30 11:39:19 +0800

    Task: Add 3TSoftwareLabs.Studio3T

---
+++ Tasks/3TSoftwareLabs.Studio3T/Script.ps1
@@ -0,0 +1,82 @@
+$Object1 = Invoke-RestMethod -Uri 'https://update.studio3t.com/win-x64/updates.xml'
+
+# Version
+$this.CurrentState.Version = $Object1.updateDescriptor.entry.newVersion
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = "https://download.studio3t.com/studio-3t/windows/$($this.CurrentState.Version)/studio-3t-x64.zip"
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseNotesUrl
+      $this.CurrentState.Locale += [ordered]@{
+        Key   = 'ReleaseNotesUrl'
+        Value = 'https://files.studio3t.com/changelog/changelog.txt'
+      }
+
+      $ReleaseNotesUrl = "https://studio3t.com/whats-new/release-$($this.CurrentState.Version.Split('.')[0..1] -join '-')/"
+      Invoke-WebRequest -Uri $ReleaseNotesUrl -Method Head | Out-Null
+
+      # ReleaseNotesUrl
+      $this.CurrentState.Locale += [ordered]@{
+        Key   = 'ReleaseNotesUrl'
+        Value = $ReleaseNotesUrl
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    try {
+      $Object2 = [System.IO.StreamReader]::new((Invoke-WebRequest -Uri 'https://files.studio3t.com/changelog/changelog.txt').RawContentStream)
+
+      while (-not $Object2.EndOfStream) {
+        $String = $Object2.ReadLine()
+        if ($String -match "^$([regex]::Escape($this.CurrentState.Version))") {
+          try {
+            # ReleaseTime
+            $this.CurrentState.ReleaseTime = [regex]::Match($String, '(\d{1,2}-[a-zA-Z]+-20\d{2})').Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
+          } catch {
+            $this.Log("No ReleaseTime for version $($this.CurrentState.Version)", 'Warning')
+          }
+          $null = $Object2.ReadLine()
+          break
+        }
+      }
+      if (-not $Object2.EndOfStream) {
+        $ReleaseNotesObjects = [System.Collections.Generic.List[string]]::new()
+        while (-not $Object2.EndOfStream) {
+          $String = $Object2.ReadLine()
+          if ($String -notmatch '^\d+(\.\d+)+ ') {
+            $ReleaseNotesObjects.Add($String)
+          } else {
+            break
+          }
+        }
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesObjects | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseTime and ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
