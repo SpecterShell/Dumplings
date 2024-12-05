55b06b696467a8193dc0208f1e9298bd24a970a9
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-12-05 23:29:32 +0800

    Task: Adobe.Connect Remove x86 installer

--- Tasks/Adobe.Connect.MSI/Script.ps1
+++ Tasks/Adobe.Connect.MSI/Script.ps1
@@ -1,20 +1,20 @@
 $Object1 = Invoke-RestMethod -Uri "https://my.adobeconnect.com/common/UpdateDescr.xml?noCache=$(Get-Random)"
 
-if ($Object1.UpdateDescr.Windows.Release[0].Version -ne $Object1.UpdateDescr.Win32.Release[0].Version) {
-  $this.Log("x86 version: $($Object1.UpdateDescr.Win32.Release[0].Version)")
-  $this.Log("x64 version: $($Object1.UpdateDescr.Windows.Release[0].Version)")
-  throw 'Inconsistent versions detected'
-}
+# if ($Object1.UpdateDescr.Windows.Release[0].Version -ne $Object1.UpdateDescr.Win32.Release[0].Version) {
+#   $this.Log("x86 version: $($Object1.UpdateDescr.Win32.Release[0].Version)")
+#   $this.Log("x64 version: $($Object1.UpdateDescr.Windows.Release[0].Version)")
+#   throw 'Inconsistent versions detected'
+# }
 
 # Version
 $this.CurrentState.Version = $Object1.UpdateDescr.Windows.Release[0].Version -replace '^20'
 
 # Installer
-$this.CurrentState.Installer += [ordered]@{
-  Architecture  = 'x86'
-  InstallerType = 'msi'
-  InstallerUrl  = Get-RedirectedUrl1st -Uri 'https://www.adobe.com/go/Connect11_32msi'
-}
+# $this.CurrentState.Installer += [ordered]@{
+#   Architecture  = 'x86'
+#   InstallerType = 'msi'
+#   InstallerUrl  = Get-RedirectedUrl1st -Uri 'https://www.adobe.com/go/Connect11_32msi'
+# }
 $this.CurrentState.Installer += [ordered]@{
   Architecture  = 'x64'
   InstallerType = 'msi'
--- Tasks/Adobe.Connect/Script.ps1
+++ Tasks/Adobe.Connect/Script.ps1
@@ -1,25 +1,25 @@
 $Object1 = Invoke-RestMethod -Uri "https://my.adobeconnect.com/common/UpdateDescr.xml?noCache=$(Get-Random)"
 
-if ($Object1.UpdateDescr.Windows.Release[0].Version -ne $Object1.UpdateDescr.Win32.Release[0].Version) {
-  $this.Log("x86 version: $($Object1.UpdateDescr.Win32.Release[0].Version)")
-  $this.Log("x64 version: $($Object1.UpdateDescr.Windows.Release[0].Version)")
-  throw 'Inconsistent versions detected'
-}
+# if ($Object1.UpdateDescr.Windows.Release[0].Version -ne $Object1.UpdateDescr.Win32.Release[0].Version) {
+#   $this.Log("x86 version: $($Object1.UpdateDescr.Win32.Release[0].Version)")
+#   $this.Log("x64 version: $($Object1.UpdateDescr.Windows.Release[0].Version)")
+#   throw 'Inconsistent versions detected'
+# }
 
 # Version
 $this.CurrentState.Version = $Object1.UpdateDescr.Windows.Release[0].Version
 
 # Installer
-$this.CurrentState.Installer += [ordered]@{
-  Architecture           = 'x86'
-  InstallerType          = 'exe'
-  InstallerUrl           = Get-RedirectedUrl1st -Uri 'https://www.adobe.com/go/Connect11_32AppStandalone'
-  AppsAndFeaturesEntries = @(
-    [ordered]@{
-      DisplayVersion = "$($this.CurrentState.Version).32"
-    }
-  )
-}
+# $this.CurrentState.Installer += [ordered]@{
+#   Architecture           = 'x86'
+#   InstallerType          = 'exe'
+#   InstallerUrl           = Get-RedirectedUrl1st -Uri 'https://www.adobe.com/go/Connect11_32AppStandalone'
+#   AppsAndFeaturesEntries = @(
+#     [ordered]@{
+#       DisplayVersion = "$($this.CurrentState.Version).32"
+#     }
+#   )
+# }
 $this.CurrentState.Installer += [ordered]@{
   Architecture           = 'x64'
   InstallerType          = 'exe'
