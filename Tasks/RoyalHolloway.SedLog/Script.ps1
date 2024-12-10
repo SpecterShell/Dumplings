d0c7fbd415e81bf6c6c15ddfe7569b0a0b3f11e7
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-12-10 12:01:27 +0800

    Task: Add RoyalHolloway.SedLog

---
+++ Tasks/RoyalHolloway.SedLog/Script.ps1
@@ -0,0 +1,22 @@
+$Object1 = Invoke-WebRequest -Uri 'https://sedlog.rhul.ac.uk/download.html'
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $InstallerUrl = $Object1.Links.Where({ try { $_.href.EndsWith('.exe') -and $_.href.Contains('setup') } catch {} }, 'First')[0].href
+}
+
+# Version
+$this.CurrentState.Version = [regex]::Match($InstallerUrl, 'sedlog-(\d+(\.\d+)+)[-.]').Groups[1].Value
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
