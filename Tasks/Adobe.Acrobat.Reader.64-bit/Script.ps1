95089cd9f135e9e01cf64e7a0a3fd3323983681d
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-12-13 17:25:29 +0800

    Task: Add Adobe.Acrobat

---
+++ Tasks/Adobe.Acrobat.Reader.64-bit/Script.ps1
@@ -0,0 +1,31 @@
+$Object1 = Invoke-RestMethod -Uri 'https://rdc.adobe.io/reader/products?lang=mui&site=otherversions&os=Windows%2011&api_key=dc-get-adobereader-cdn'
+$Object2 = $Object1.products.reader.Where({ $_.displayName.Contains('64bit') }, 'First')[0]
+$Object3 = Invoke-RestMethod -Uri 'https://rdc.adobe.io/reader/downloadUrl' -Body @{
+  name    = $Object2.displayName
+  os      = 'Windows 11'
+  site    = 'enterprise'
+  lang    = 'mui'
+  api_key = 'dc-get-adobereader-cdn'
+}
+
+# Version
+$this.CurrentState.Version = $Object2.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = $Object3.downloadURL
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
