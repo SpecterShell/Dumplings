b9dc2a8d2b93a5b71eb8a345f0a1a5e43afb350a
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-12-14 21:49:58 +0800

    Task: Figma.Figma Add WiX installer

--- Tasks/Figma.Figma/Script.ps1
+++ Tasks/Figma.Figma/Script.ps1
@@ -5,16 +5,29 @@ $this.CurrentState.Version = $Object1.version
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  Architecture = 'x64'
-  InstallerUrl = "https://desktop.figma.com/win/build/Figma-$($this.CurrentState.Version).exe"
+  Architecture  = 'x64'
+  InstallerType = 'exe'
+  InstallerUrl  = "https://desktop.figma.com/win/build/Figma-$($this.CurrentState.Version).exe"
 }
 $this.CurrentState.Installer += [ordered]@{
-  Architecture = 'arm64'
-  InstallerUrl = "https://desktop.figma.com/win-arm/build/Figma-$($this.CurrentState.Version).exe"
+  Architecture  = 'arm64'
+  InstallerType = 'exe'
+  InstallerUrl  = "https://desktop.figma.com/win-arm/build/Figma-$($this.CurrentState.Version).exe"
+}
+$this.CurrentState.Installer += $InstallerX64WiX = [ordered]@{
+  Architecture  = 'x64'
+  InstallerType = 'wix'
+  InstallerUrl  = "https://desktop.figma.com/win/build/Figma-$($this.CurrentState.Version).msi"
 }
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
+    $InstallerFileX64WiX = Get-TempFile -Uri $InstallerX64WiX.InstallerUrl
+    # InstallerSha256
+    $InstallerX64WiX['InstallerSha256'] = (Get-FileHash -Path $InstallerFileX64WiX -Algorithm SHA256).Hash
+    # ProductCode
+    $InstallerX64WiX['ProductCode'] = "$($InstallerFileX64WiX | Read-ProductCodeFromMsi).msq"
+
     $this.Print()
     $this.Write()
   }
