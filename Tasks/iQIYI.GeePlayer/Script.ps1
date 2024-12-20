f2c4ab99e7f1cb5b5d0d1514f11a7d46ae926bfd
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-12-20 11:00:48 +0800

    Task: iQIYI.GeePlayer Update

--- Tasks/iQIYI.GeePlayer/Script.ps1
+++ Tasks/iQIYI.GeePlayer/Script.ps1
@@ -1,12 +1,12 @@
 $Object1 = Invoke-WebRequest -Uri 'https://www.iqiyi.com/appstore.html' | ConvertFrom-Html
-$Object2 = $Object1.SelectSingleNode('//li[contains(@class, "app-item") and contains(.//h3[@class="main-title"], "爱奇艺万能播放器")]')
+$Object2 = $Object1.SelectSingleNode('//li[contains(@class, "app-item") and contains(.//h3[@class="main-title"], "爱奇艺万能播放")]')
 
 # Version
 $this.CurrentState.Version = [regex]::Match($Object2.SelectSingleNode('.//p[@class="sub-ver"]').InnerText, '(\d+\.\d+\.\d+\.\d+)').Groups[1].Value
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = $Object2.SelectSingleNode('.//a[@class="mod-btn"]').Attributes['href'].Value -replace '^https://', 'http://'
+  InstallerUrl = $Object2.SelectSingleNode('.//a[@class="mod-btn"]').Attributes['href'].Value
 }
 
 switch -Regex ($this.Check()) {
