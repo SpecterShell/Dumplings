c261e5b70a96f6966b0364c677ce69908ba23a44
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-12-25 15:49:15 +0800

    Task: Add ALE.Rainbow

---
+++ Tasks/ALE.Rainbow/Script.ps1
@@ -0,0 +1,16 @@
+$Prefix = 'https://openrainbow.com/downloads/'
+
+$this.CurrentState = Invoke-RestMethod -Uri "${Prefix}latest_desktop-x64.yml?noCache=$(Get-Random)" | ConvertFrom-Yaml | ConvertFrom-ElectronUpdater -Prefix $Prefix -Locale 'en-US'
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
