02290137d440b7bce5b38b48e099bb46e279f9e2
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-12-25 12:03:29 +0800

    Task: NitroSoftware.NitroPro Switch to update source

--- Tasks/NitroSoftware.NitroPro/Script.ps1
+++ Tasks/NitroSoftware.NitroPro/Script.ps1
@@ -1,43 +1,35 @@
-$Object1 = Invoke-WebRequest -Uri 'https://www.gonitro.com/pro/try/trial/download/thanks'
+$Object1 = Invoke-RestMethod -Uri 'https://downloads.gonitro.com/rss_feed/windows/14/retail/pro.rss'
+
+# Version
+$this.CurrentState.Version = $Object1.enclosure.version
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = $InstallerUrl = $Object1.Links.Where({ try { $_.href.EndsWith('.msi') } catch {} }, 'First')[0].href
+  InstallerType = 'wix'
+  InstallerUrl  = "https://downloads.gonitro.com/professional_$($this.CurrentState.Version)/en/retail/nitro_pro14_ba_x64.msi"
 }
-
-# Version
-$this.CurrentState.Version = [regex]::Match($InstallerUrl, '(\d+(\.\d+){2,})').Groups[1].Value
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
     try {
-      $Object2 = Invoke-WebRequest -Uri 'https://www.gonitro.com/product-details/release-notes' | ConvertFrom-Html
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.pubDate | Get-Date -AsUTC
 
-      if ($Object2.SelectSingleNode("//span[@class='versionLabel']/following-sibling::text()[string()='$($this.CurrentState.Version)']")) {
-        $ReleaseNotesNode = $Object2.SelectSingleNode('//*[contains(@class, "AccordionSection-content")][1]')
-        $ReleaseTimeNode = $ReleaseNotesNode.SelectSingleNode('./*[contains(.//*, "Release date")]')
-
-        if ($ReleaseTimeNode) {
-          # ReleaseTime
-          $this.CurrentState.ReleaseTime = [regex]::Match($ReleaseTimeNode.InnerText, '([a-zA-Z]+ \d{1,2}, \d{4})').Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
-
-          # ReleaseNotes (en-US)
-          $this.CurrentState.Locale += [ordered]@{
-            Locale = 'en-US'
-            Key    = 'ReleaseNotes'
-            Value  = $ReleaseTimeNode.SelectNodes('./following-sibling::node()') | Get-TextContent | Format-Text
-          }
-        } else {
-          $this.Log("No ReleaseTime for version $($this.CurrentState.Version)", 'Warning')
-          # ReleaseNotes (en-US)
-          $this.CurrentState.Locale += [ordered]@{
-            Locale = 'en-US'
-            Key    = 'ReleaseNotes'
-            Value  = $ReleaseNotesNode | Get-TextContent | Format-Text
-          }
+      $ReleaseNotesObject = $Object1.description.'#cdata-section' | ConvertFrom-Html
+      if ($ReleaseTimeNode = $ReleaseNotesObject.SelectSingleNode('/html/body/*[contains(., "Release Date")]')) {
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseTimeNode.SelectNodes('./following-sibling::node()') | Get-TextContent | Format-Text
         }
       } else {
-        $this.Log("No ReleaseTime and ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesObject.SelectSingleNode('/html/body') | Get-TextContent | Format-Text
+        }
       }
     } catch {
       $_ | Out-Host
