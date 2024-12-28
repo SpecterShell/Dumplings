4a7182c92d640430c2fcca5f9bca5c7de2397819
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-12-29 00:48:30 +0800

    Task: ALE.Rainbow Add installers, fix version and reset

--- Tasks/ALE.Rainbow/Script.ps1
+++ Tasks/ALE.Rainbow/Script.ps1
@@ -1,9 +1,78 @@
 $Prefix = 'https://openrainbow.com/downloads/'
+# x86 user
+$Object1 = Invoke-RestMethod -Uri "${Prefix}latest_desktop.yml?noCache=$(Get-Random)" | ConvertFrom-Yaml
+# x86 machine
+$Object2 = Invoke-RestMethod -Uri "${Prefix}latest_desktop_machine.yml?noCache=$(Get-Random)" | ConvertFrom-Yaml
+# x64 user
+$Object3 = Invoke-RestMethod -Uri "${Prefix}latest_desktop-x64.yml?noCache=$(Get-Random)" | ConvertFrom-Yaml
+# x64 machine
+$Object4 = Invoke-RestMethod -Uri "${Prefix}latest_desktop_machine-x64.yml?noCache=$(Get-Random)" | ConvertFrom-Yaml
 
-$this.CurrentState = Invoke-RestMethod -Uri "${Prefix}latest_desktop-x64.yml?noCache=$(Get-Random)" | ConvertFrom-Yaml | ConvertFrom-ElectronUpdater -Prefix $Prefix -Locale 'en-US'
+if (@(@($Object1, $Object2, $Object3, $Object4) | Sort-Object -Property { $_.version } -Unique).Count -gt 1) {
+  $this.Log("x86 user version: $($Object1.version)")
+  $this.Log("x86 machine version: $($Object2.version)")
+  $this.Log("x64 user version: $($Object3.version)")
+  $this.Log("x64 machine version: $($Object4.version)")
+  throw 'Inconsistent versions detected'
+}
+
+# Version
+$this.CurrentState.Version = $Object3.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture  = 'x86'
+  InstallerType = 'inno'
+  Scope         = 'user'
+  InstallerUrl  = $Prefix + $Object1.files[0].url
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture  = 'x86'
+  InstallerType = 'inno'
+  Scope         = 'machine'
+  InstallerUrl  = $Prefix + $Object2.files[0].url
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture  = 'x86'
+  InstallerType = 'msi'
+  Scope         = 'machine'
+  InstallerUrl  = $Prefix + $Object2.files[0].url.Replace('_Electron', '_Offline').Replace('.exe', '.msi')
+}
+$this.CurrentState.Installer += $Installer = [ordered]@{
+  Architecture  = 'x64'
+  InstallerType = 'inno'
+  Scope         = 'user'
+  InstallerUrl  = $Prefix + $Object3.files[0].url
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture  = 'x64'
+  InstallerType = 'inno'
+  Scope         = 'machine'
+  InstallerUrl  = $Prefix + $Object4.files[0].url
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture  = 'x64'
+  InstallerType = 'msi'
+  Scope         = 'machine'
+  InstallerUrl  = $Prefix + $Object4.files[0].url.Replace('_Electron', '_Offline').Replace('.exe', '.msi')
+}
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object3.releaseDate | Get-Date -AsUTC
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $InstallerFile = Get-TempFile -Uri $Installer.InstallerUrl
+    # InstallerSha256
+    $Installer['InstallerSha256'] = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
+    # RealVersion
+    $this.CurrentState.RealVersion = $InstallerFile | Read-ProductVersionFromExe
+
     $this.Print()
     $this.Write()
   }
