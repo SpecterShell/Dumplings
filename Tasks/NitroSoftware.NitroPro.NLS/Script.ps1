7cd490b4f2289b4c06c9b480aeab1bdc3c8e7ada
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-12-30 11:38:38 +0800

    Task: Add NitroSoftware.NitroPro.NLS

---
+++ Tasks/NitroSoftware.NitroPro.NLS/Script.ps1
@@ -0,0 +1,48 @@
+$Object1 = Invoke-RestMethod -Uri 'https://downloads.gonitro.com/rss_feed/windows/14/nls/pro.rss'
+
+# Version
+$this.CurrentState.Version = $Object1.enclosure.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerType = 'wix'
+  InstallerUrl  = "https://downloads.gonitro.com/professional_$($this.CurrentState.Version)/en/nls/nitro_pro14_x64.msi"
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.pubDate | Get-Date -AsUTC
+
+      $ReleaseNotesObject = $Object1.description.'#cdata-section' | ConvertFrom-Html
+      if ($ReleaseTimeNode = $ReleaseNotesObject.SelectSingleNode('/html/body/*[contains(., "Release Date")]')) {
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseTimeNode.SelectNodes('./following-sibling::node()') | Get-TextContent | Format-Text
+        }
+      } else {
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesObject.SelectSingleNode('/html/body') | Get-TextContent | Format-Text
+        }
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
