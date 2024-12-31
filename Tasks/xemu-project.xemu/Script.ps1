bba07d29c5e1ff9230e7a9a727dba130c1a53cc3
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2024-12-31 14:37:38 +0800

    Task: xemu-project.xemu Restrict architecture

--- Tasks/xemu-project.xemu/Script.ps1
+++ Tasks/xemu-project.xemu/Script.ps1
@@ -8,7 +8,8 @@ $this.CurrentState.Version = $Object1.tag_name -creplace '^v'
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = $Object1.assets.Where({ $_.name.Contains('win') -and $_.name.Contains('release') -and -not $_.name.Contains('pdb') }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
+  Architecture = 'x64'
+  InstallerUrl = $Object1.assets.Where({ $_.name.Contains('win') -and $_.name.Contains('x86_64') -and $_.name.Contains('release') -and -not $_.name.Contains('pdb') }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
 }
 
 switch -Regex ($this.Check()) {
