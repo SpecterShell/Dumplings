116a3ee66ac58358f35c7d3c84ad0ab056580f0a
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-01-11 15:34:05 +0800

    Task Ruihu.Apifox: Update

--- Tasks/Ruihu.Apifox/Script.ps1
+++ Tasks/Ruihu.Apifox/Script.ps1
@@ -1,7 +1,47 @@
-$this.CurrentState = Invoke-RestMethod -Uri "https://cdn.apifox.cn/download/latest.yml?noCache=$(Get-Random)" | ConvertFrom-Yaml | ConvertFrom-ElectronUpdater -Locale 'zh-CN'
+# x86
+$Object1 = Invoke-RestMethod -Uri "https://api.apifox.cn/api/v1/configs/client-updates/$($this.LastState.Contains('Version') ? $this.LastState.Version : '2.6.43')/win32/latest.yml?noCache=$(Get-Random)" -Headers @{
+  'x-system-version'                  = '10.0.22000'
+  'x-running-under-arm64-translation' = 'false'
+  'x-platform'                        = 'win32'
+  'x-arch'                            = 'x86'
+} | ConvertFrom-Yaml
+# x64
+$Object2 = Invoke-RestMethod -Uri "https://api.apifox.cn/api/v1/configs/client-updates/$($this.LastState.Contains('Version') ? $this.LastState.Version : '2.6.43')/win64/latest.yml?noCache=$(Get-Random)" -Headers @{
+  'x-system-version'                  = '10.0.22000'
+  'x-running-under-arm64-translation' = 'false'
+  'x-platform'                        = 'win64'
+  'x-arch'                            = 'x64'
+} | ConvertFrom-Yaml
+
+if ($Object1.version -ne $Object2.version) {
+  $this.Log("x86 version: $($Object1.version)")
+  $this.Log("x64 version: $($Object2.version)")
+  throw 'Inconsistent versions detected'
+}
+
+# Version
+$this.CurrentState.Version = $Object2.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x86'
+  InstallerUrl = $Object1.files[0].url
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = $Object2.files[0].url
+}
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object2.releaseDate | Get-Date -AsUTC
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
     try {
       $Object1 = Invoke-WebRequest -Uri 'https://apifox.com/help/app/changelog/' | ConvertFrom-Html
 
