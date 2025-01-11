b8b0c623f23ae8b420d62e38733f61788805fa30
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-01-11 12:27:55 +0800

    Task: Add Comfy.ComfyUI-Desktop

---
+++ Tasks/Comfy.ComfyUI-Desktop/Script.ps1
@@ -0,0 +1,31 @@
+$Object1 = Invoke-RestMethod -Uri 'https://download.todesktop.com/241012ess7yxs0e/td-latest.json'
+
+# Version
+$this.CurrentState.Version = $Object1.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = $Object1.artifacts.nsis.x64.url | ConvertTo-UnescapedUri
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.createdAt.ToUniversalTime()
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
