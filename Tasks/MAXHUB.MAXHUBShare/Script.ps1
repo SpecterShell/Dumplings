0ac487053412fa33c312d4e952911949c3ac5d0e
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-01-11 20:41:54 +0800

    Task: Add MAXHUB.MAXHUBShare

---
+++ Tasks/MAXHUB.MAXHUBShare/Script.ps1
@@ -0,0 +1,52 @@
+$Object1 = Invoke-RestMethod -Uri 'https://myou.cvte.com/api/v1/update' -Body @{
+  appKey      = 'bf4165392bdd7ca1234e0b72faa20eabe6fbea4e'
+  versionCode = $this.LastState.Contains('Version') ? $this.LastState.Version : '6.5.2.14'
+  platform    = 'windows_app'
+}
+
+if (-not $Object1.hasUpdate) {
+  $this.Log("The version $($this.LastState.Version) from the last state is the latest, skip checking", 'Info')
+  return
+}
+
+# Version
+$this.CurrentState.Version = $Object1.targetVersionCode
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.downloadUrl
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.updateStartTime | Get-Date | ConvertTo-UtcDateTime -Id 'China Standard Time'
+
+      # ReleaseNotes (zh-CN)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'zh-CN'
+        Key    = 'ReleaseNotes'
+        Value  = $Object1.updateDesc | Format-Text
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+    # InstallerSha256
+    $this.CurrentState.Installer[0]['InstallerSha256'] = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
+    # RealVersion
+    $this.CurrentState.RealVersion = $InstallerFile | Read-ProductVersionFromExe
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
