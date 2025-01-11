692a797008a187047a6e007cd36a06bcc6f97103
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-01-11 23:17:00 +0800

    Task: Add Seewo.EasiNote5C, Seewo.SeewoHugoKanban, Seewo.SeewoYiQiXueStudent, Seewo.SeewoYiQiXueTeacher

---
+++ Tasks/Seewo.SeewoHugoKanban/Script.ps1
@@ -0,0 +1,22 @@
+$Object1 = $Global:DumplingsStorage.SeewoApps['SeewoHugoKanbanWebApp']
+
+# Version
+$this.CurrentState.Version = $Object1.softInfos[0].softVersion
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.softInfos[0].downloadUrl
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
---
+++ Tasks/Seewo.SeewoYiQiXueStudent/Script.ps1
@@ -0,0 +1,22 @@
+$Object1 = $Global:DumplingsStorage.SeewoApps['SeewoYiQiXueStudent']
+
+# Version
+$this.CurrentState.Version = $Object1.softInfos.Where({ $_.useSystem -eq 1 }, 'First')[0].softVersion
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.softInfos.Where({ $_.useSystem -eq 1 }, 'First')[0].downloadUrl
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
