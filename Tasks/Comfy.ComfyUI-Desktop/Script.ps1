548944c666b1f8a93c7ccc049d20cdb26ae8721c
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-01-11 13:40:57 +0800

    Task Comfy.ComfyUI-Desktop: Add release notes

--- Tasks/Comfy.ComfyUI-Desktop/Script.ps1
+++ Tasks/Comfy.ComfyUI-Desktop/Script.ps1
@@ -19,6 +19,32 @@ switch -Regex ($this.Check()) {
       $this.Log($_, 'Warning')
     }
 
+    try {
+      $RepoOwner = 'Comfy-Org'
+      $RepoName = 'desktop'
+
+      $Object2 = Invoke-GitHubApi -Uri "https://api.github.com/repos/${RepoOwner}/${RepoName}/releases/tags/v$($this.CurrentState.Version)"
+
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object2.published_at.ToUniversalTime()
+
+      if (-not [string]::IsNullOrWhiteSpace($Object2.body)) {
+        $ReleaseNotesObject = ($Object2.body | ConvertFrom-Markdown).Html | ConvertFrom-Html
+        $ReleaseNotesNodes = for ($Node = $ReleaseNotesObject.ChildNodes[0]; $Node -and -not ($Node.Name -eq 'h3' -and $Node.InnerText.Contains('Download Latest:')); $Node = $Node.NextSibling) { $Node }
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
     $this.Print()
     $this.Write()
   }
