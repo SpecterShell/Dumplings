800446c86fe1a0ebc7870b2633881d98ce23201b
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-01-11 19:05:18 +0800

    Task: Add InspectDevTools.Inspect

---
+++ Tasks/InspectDevTools.Inspect/Script.ps1
@@ -0,0 +1,35 @@
+$Object1 = Invoke-RestMethod -Uri 'https://download.todesktop.com/231013edhal6kvv/td-latest.json'
+
+# Version
+$this.CurrentState.Version = $Object1.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = $Object1.artifacts.nsis.x64.url | ConvertTo-UnescapedUri
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'arm64'
+  InstallerUrl = $Object1.artifacts.nsis.arm64.url | ConvertTo-UnescapedUri
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.createdAt.ToUniversalTime()
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
