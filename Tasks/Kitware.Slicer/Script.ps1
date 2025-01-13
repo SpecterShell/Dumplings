0c79ac63a74620085d00cfa8994e86ac495bb72d
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-01-13 15:30:07 +0800

    Task: Add Kitware.Slicer

---
+++ Tasks/Kitware.Slicer/Script.ps1
@@ -0,0 +1,30 @@
+$Object1 = Invoke-RestMethod -Uri 'https://download.slicer.org/find?os=win&stability=release'
+
+# Version
+$this.CurrentState.Version = $Object1.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = Join-Uri 'https://download.slicer.org/' $Object1.download_url
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.build_date.ToUniversalTime()
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
