29cd26faaa0691bf81c8924fb31f13dd779fde5c
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-01-15 14:34:00 +0800

    Task: Add Zultys.ZAC

---
+++ Tasks/Zultys.ZAC/Script.ps1
@@ -0,0 +1,43 @@
+$Object1 = Invoke-WebRequest -Uri 'https://www.zultys.com/zac-download/'
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x86'
+  InstallerUrl = [regex]::Match($Object1.Content, 'https://.+?ZAC_x86.+?\.exe').Value
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = $InstallerUrl = [regex]::Match($Object1.Content, 'https://.+?ZAC_x64.+?\.exe').Value
+}
+
+# Version
+$this.CurrentState.Version = [regex]::Match($InstallerUrl, '(\d+(?:\.\d+){2,})').Groups[1].Value
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    foreach ($Installer in $this.CurrentState.Installer) {
+      $InstallerFile = Get-TempFile -Uri $Installer.InstallerUrl
+      $InstallerFileExtracted = $InstallerFile | Expand-InstallShield
+      $InstallerFile2 = Join-Path $InstallerFileExtracted 'ZAC.msi'
+      # InstallerSha256
+      $Installer.InstallerSha256 = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
+      # AppsAndFeaturesEntries + ProductCode
+      $Installer.AppsAndFeaturesEntries = @(
+        [ordered]@{
+          ProductCode   = $Installer.ProductCode = $InstallerFile2 | Read-ProductCodeFromMsi
+          UpgradeCode   = $InstallerFile2 | Read-UpgradeCodeFromMsi
+          InstallerType = 'msi'
+        }
+      )
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
