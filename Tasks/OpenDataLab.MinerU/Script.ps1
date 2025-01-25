a98ec9d77f4b40450958e3799b61fa7c6a818f47
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-01-25 15:45:53 +0800

    Task: Add OpenDataLab.MinerU

---
+++ Tasks/OpenDataLab.MinerU/Script.ps1
@@ -0,0 +1,16 @@
+$Prefix = 'https://webpub.shlab.tech/MinerU/latest/'
+
+$this.CurrentState = Invoke-RestMethod -Uri "${Prefix}latest.yml?noCache=$(Get-Random)" | ConvertFrom-Yaml | ConvertFrom-ElectronUpdater -Prefix $Prefix -Locale 'zh-CN'
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
