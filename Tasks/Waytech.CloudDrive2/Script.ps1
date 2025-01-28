abae370270e0c521cfbfb3cb113504918a1179d9
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-01-28 22:02:42 +0800

    Task Waytech.CloudDrive2: Add Arm64 installer

--- Tasks/Waytech.CloudDrive2/Script.ps1
+++ Tasks/Waytech.CloudDrive2/Script.ps1
@@ -11,6 +11,10 @@ $this.CurrentState.Installer += [ordered]@{
   Architecture = 'x64'
   InstallerUrl = $Object1.assets.Where({ $_.name.EndsWith('.exe') -and $_.name.Contains('Setup') -and $_.name.Contains('X64') }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
 }
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'arm64'
+  InstallerUrl = $Object1.assets.Where({ $_.name.EndsWith('.exe') -and $_.name.Contains('Setup') -and $_.name.Contains('Arm64') }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
+}
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
