14a0f5143254f35a7d21e16f03f6d41c5162e3a3
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-01-30 10:05:14 +0800

    Task Google.GoogleDrive: Add release notes

--- Tasks/Google.GoogleDrive/Script.ps1
+++ Tasks/Google.GoogleDrive/Script.ps1
@@ -18,6 +18,52 @@ $this.CurrentState.Installer += [ordered]@{
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
+    try {
+      $Object2 = Invoke-WebRequest -Uri 'https://support.google.com/a/answer/7577057' | ConvertFrom-Html
+
+      $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//div[@class='cc']/p[contains(., 'Version $($this.CurrentState.Version.Split('.')[0..1] -join '.')')]")
+      if ($ReleaseNotesTitleNode) {
+        # ReleaseTime
+        $this.CurrentState.ReleaseTime = [regex]::Match($ReleaseNotesTitleNode.SelectSingleNode('./preceding-sibling::h2').InnerText, '([a-zA-Z]+\W+\d{1,2}\W+20\d{2})').Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
+
+        $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node -and $Node.Name -ne 'h2'; $Node = $Node.NextSibling) { $Node }
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    try {
+      $Object3 = Invoke-WebRequest -Uri 'https://support.google.com/a/answer/7577057?hl=zh-Hans' | ConvertFrom-Html
+
+      $ReleaseNotesCNTitleNode = $Object3.SelectSingleNode("//div[@class='cc']/p[contains(., '版本 $($this.CurrentState.Version.Split('.')[0..1] -join '.')')]")
+      if ($ReleaseNotesCNTitleNode) {
+        # ReleaseTime
+        $this.CurrentState.ReleaseTime ??= [regex]::Match($ReleaseNotesCNTitleNode.SelectSingleNode('./preceding-sibling::h2').InnerText, '(20\d{2}\s*年\s*\d{1,2}\s*月\s*\d{1,2}\s*日)').Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
+
+        $ReleaseNotesCNNodes = for ($Node = $ReleaseNotesCNTitleNode.NextSibling; $Node -and $Node.Name -ne 'h2'; $Node = $Node.NextSibling) { $Node }
+        # ReleaseNotes (zh-CN)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'zh-CN'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesCNNodes | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotes (zh-CN) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
     $this.Print()
     $this.Write()
   }
