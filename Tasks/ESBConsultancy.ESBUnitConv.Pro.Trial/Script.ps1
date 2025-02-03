48905095aa8c142f8afdbe138fc283041b5d7671
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-02-03 17:43:01 +0800

    Task: Add ESBConsultancy

---
+++ Tasks/ESBConsultancy.ESBUnitConv.Pro.Trial/Script.ps1
@@ -0,0 +1,37 @@
+$Object1 = Invoke-WebRequest -Uri 'https://www.esbconsult.com/esbcalc/esbunitconvpro.htm'
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.Links.Where({ try { $_.href.EndsWith('.zip') -and -not $_.href.Contains('port') -and -not $_.href.Contains('nosetup') } catch {} }, 'First')[0].href
+}
+
+# Version
+$this.CurrentState.Version = [regex]::Match($Object1.Content, 'ESBUnitConv&trade; Pro Trial v(\d+(?:\.\d+)+)').Groups[1].Value
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = [regex]::Match($Object1.Content, '(\d{1,2}\W+[a-zA-Z]+\W+20\d{2})').Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+    $InstallerFile2 = $InstallerFile | Expand-TempArchive | Join-Path -ChildPath 'esbunitconv_setup.exe'
+    # InstallerSha256
+    $this.CurrentState.Installer[0]['InstallerSha256'] = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
+    # RealVersion
+    $this.CurrentState.RealVersion = $InstallerFile2 | Read-ProductVersionFromExe
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
