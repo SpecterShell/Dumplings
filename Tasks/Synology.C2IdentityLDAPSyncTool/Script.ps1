be9b289d0a9a64c0a7e7f7f5ebcfc65014e5eafd
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-02-03 23:23:50 +0800

    Task: Add Synology.C2IdentityLDAPSyncTool

---
+++ Tasks/Synology.C2IdentityLDAPSyncTool/Script.ps1
@@ -0,0 +1,44 @@
+$Object1 = Invoke-RestMethod -Uri 'https://utyautoupdate.synology.com/getUpdate/C2IdentityLDAPSyncTool?os=windows&bits=64&include_beta=false'
+
+# Version
+$this.CurrentState.Version = "$($Object1.version.major).$($Object1.version.minor).$($Object1.version.hotfix)-$($Object1.version.build_number)"
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = $Object1.installer.url
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.release_date | Get-Date -Format 'yyyy-MM-dd'
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+    # RealVersion
+    $this.CurrentState.RealVersion = $InstallerFile | Read-ProductVersionFromMsi
+    # InstallerSha256
+    $this.CurrentState.Installer[0]['InstallerSha256'] = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
+    # AppsAndFeaturesEntries + ProductCode
+    $this.CurrentState.Installer[0]['AppsAndFeaturesEntries'] = @(
+      [ordered]@{
+        ProductCode = $this.CurrentState.Installer[0]['ProductCode'] = $InstallerFile | Read-ProductCodeFromMsi
+        UpgradeCode = $InstallerFile | Read-UpgradeCodeFromMsi
+      }
+    )
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
