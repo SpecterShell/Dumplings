a91965298eb5172634968220167f8cbf6a3ca54b
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-02-03 12:24:14 +0800

    Task: Add WalkMe.WalkMeEditor

---
+++ Tasks/WalkMe.WalkMeEditor/Script.ps1
@@ -0,0 +1,38 @@
+$Object1 = Invoke-WebRequest -Uri 'https://www.walkme.com/download/'
+
+# Installer
+$this.CurrentState.Installer += $InstallerEXE = [ordered]@{
+  InstallerType = 'exe'
+  Scope         = 'user'
+  InstallerUrl  = $Object1.Links.Where({ try { $_.href.EndsWith('.exe') -and $_.href.Contains('PerUserInstallers') } catch {} }, 'First')[0].href
+}
+$this.CurrentState.Installer += $InstallerWiX = [ordered]@{
+  InstallerType = 'wix'
+  Scope         = 'machine'
+  InstallerUrl  = $Object1.Links.Where({ try { $_.href.EndsWith('.msi') -and $_.href.Contains('PerMachineInstallers') } catch {} }, 'First')[0].href
+}
+
+$VersionEXE = [regex]::Match($InstallerEXE.InstallerUrl, '(\d+(?:\.\d+){2,})').Groups[1].Value
+$VersionWiX = [regex]::Match($InstallerWiX.InstallerUrl, '(\d+(?:\.\d+){2,})').Groups[1].Value
+
+if ($VersionEXE -ne $VersionWiX) {
+  $this.Log("User version: ${VersionEXE}")
+  $this.Log("Machine version: ${VersionWiX}")
+  throw 'Inconsistent versions detected'
+}
+
+# Version
+$this.CurrentState.Version = $VersionEXE
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
