686f82d41814ba71dd8f478df5de99eae1fd90b0
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-02-07 23:02:57 +0800

    Task: Add KDE.digiKam

---
+++ Tasks/KDE.digiKam/Script.ps1
@@ -0,0 +1,64 @@
+$Object1 = Invoke-RestMethod -Uri 'https://www.digikam.org/release.yml' | ConvertFrom-Yaml
+
+# Version
+$this.CurrentState.Version = $Object1.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = Join-Uri 'https://download.kde.org/stable/digikam/' $Object1.win.b64.file.Replace('.mirrorlist', '')
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    try {
+      # ReleaseNotesUrl
+      $this.CurrentState.Locale += [ordered]@{
+        Key   = 'ReleaseNotesUrl'
+        Value = 'https://www.digikam.org/news/'
+      }
+
+      $Object2 = (Invoke-RestMethod -Uri 'https://www.digikam.org/news/index.xml').Where({ $_.title.Contains($this.CurrentState.Version) }, 'First')
+
+      if ($Object2) {
+        # ReleaseTime
+        $this.CurrentState.ReleaseTime = $Object2[0].pubDate | Get-Date -AsUTC
+
+        $ReleaseNotesObject = $Object2[0].description | ConvertFrom-Html
+        $ReleaseNotesNodes = for ($Node = $ReleaseNotesObject.ChildNodes[0]; $Node -and -not ($Node.Attributes.Contains('id') -and $Node.Attributes['id'].Value -eq 'download'); $Node = $Node.NextSibling) { $Node }
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
+        }
+
+        # ReleaseNotesUrl
+        $this.CurrentState.Locale += [ordered]@{
+          Key   = 'ReleaseNotesUrl'
+          Value = $Object2[0].link
+        }
+      } else {
+        $this.Log("No ReleaseTime, ReleaseNotes (en-US) and ReleaseNotesUrl for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
