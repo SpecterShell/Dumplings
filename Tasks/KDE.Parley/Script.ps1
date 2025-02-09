7a8b8ae5eaa2bdd9a3a1de170f55751ae63b207b
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-02-09 12:48:23 +0800

    Task: Add KDE.Bomber, KDE.KTurtle, KDE.Parley

---
+++ Tasks/KDE.Parley/Script.ps1
@@ -0,0 +1,35 @@
+$Prefix = 'https://cdn.kde.org/ci-builds/education/parley/master/windows/'
+
+$Object1 = Invoke-WebRequest -Uri $Prefix
+
+$InstallerName = $Object1.Links.Where({ try { $_.href.EndsWith('.exe') -and $_.href.Contains('x86_64') } catch {} }, 'First')[0].href
+$VersionMatches = [regex]::Match($InstallerName, 'master-(?<Build>\d+)')
+
+# Version
+$this.CurrentState.Version = $VersionMatches.Groups['Build'].Value
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = Join-Uri $Prefix $InstallerName
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl | Rename-Item -NewName { "${_}.exe" } -PassThru | Select-Object -ExpandProperty 'FullName'
+    # InstallerSha256
+    $this.CurrentState.Installer[0]['InstallerSha256'] = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
+    # RealVersion
+    Start-ThreadJob -ScriptBlock { Start-Process -FilePath $using:InstallerFile -ArgumentList '/S' -Wait } | Wait-Job -Timeout 300 | Receive-Job | Out-Host
+    $this.CurrentState.RealVersion = Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\parley' -Name 'DisplayVersion'
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
