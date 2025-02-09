db452a79351fbb6f01aecfe0f1a6f79d2cbb9a91
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-02-09 11:37:36 +0800

    Task dotPDN.PaintDotNet: Get release notes from official site

--- Tasks/dotPDN.PaintDotNet/Script.ps1
+++ Tasks/dotPDN.PaintDotNet/Script.ps1
@@ -59,81 +59,84 @@ switch -Regex ($this.Check()) {
     }
 
     $InstallerFileX64 = Get-TempFile -Uri $InstallerX64.InstallerUrl
-    $NestedInstallerFileX64 = $InstallerFileX64 | Expand-TempArchive | Join-Path -ChildPath $InstallerX64.NestedInstallerFiles[0].RelativeFilePath
-    $ExtractedNestedInstallerFileX64 = New-TempFolder
-    7z.exe e -aoa -ba -bd -y -o"${ExtractedNestedInstallerFileX64}" $NestedInstallerFileX64 'x64\PaintDotNet_x64.msi' | Out-Host
-    $RealInstallerFileX64 = Join-Path $ExtractedNestedInstallerFileX64 'PaintDotNet_x64.msi'
-
+    $InstallerFileX642 = $InstallerFileX64 | Expand-TempArchive | Join-Path -ChildPath $InstallerX64.NestedInstallerFiles[0].RelativeFilePath
+    $InstallerFileX642Extracted = New-TempFolder
+    7z.exe e -aoa -ba -bd -y -o"${InstallerFileX642Extracted}" $InstallerFileX642 'x64\PaintDotNet_x64.msi' | Out-Host
+    $InstallerFileX643 = Join-Path $InstallerFileX642Extracted 'PaintDotNet_x64.msi'
+    # InstallerSha256
     $InstallerX64['InstallerSha256'] = (Get-FileHash -Path $InstallerFileX64 -Algorithm SHA256).Hash
+    # AppsAndFeaturesEntries + ProductCode
     $InstallerX64['AppsAndFeaturesEntries'] = @(
       [ordered]@{
-        ProductCode   = $InstallerX64['ProductCode'] = $RealInstallerFileX64 | Read-ProductCodeFromMsi
-        UpgradeCode   = $RealInstallerFileX64 | Read-UpgradeCodeFromMsi
+        ProductCode   = $InstallerX64['ProductCode'] = $InstallerFileX643 | Read-ProductCodeFromMsi
+        UpgradeCode   = $InstallerFileX643 | Read-UpgradeCodeFromMsi
         InstallerType = 'wix'
       }
     )
 
     $InstallerFileARM64 = Get-TempFile -Uri $InstallerARM64.InstallerUrl
-    $NestedInstallerFileARM64 = $InstallerFileARM64 | Expand-TempArchive | Join-Path -ChildPath $InstallerARM64.NestedInstallerFiles[0].RelativeFilePath
-    $ExtractedNestedInstallerFileARM64 = New-TempFolder
-    7z.exe e -aoa -ba -bd -y -o"${ExtractedNestedInstallerFileARM64}" $NestedInstallerFileARM64 'arm64\PaintDotNet_arm64.msi' | Out-Host
-    $RealInstallerFileARM64 = Join-Path $ExtractedNestedInstallerFileARM64 'PaintDotNet_arm64.msi'
-
+    $InstallerFileARM642 = $InstallerFileARM64 | Expand-TempArchive | Join-Path -ChildPath $InstallerARM64.NestedInstallerFiles[0].RelativeFilePath
+    $InstallerFileARM642Extracted = New-TempFolder
+    7z.exe e -aoa -ba -bd -y -o"${InstallerFileARM642Extracted}" $InstallerFileARM642 'arm64\PaintDotNet_arm64.msi' | Out-Host
+    $InstallerFileARM643 = Join-Path $InstallerFileARM642Extracted 'PaintDotNet_arm64.msi'
+    # InstallerSha256
     $InstallerARM64['InstallerSha256'] = (Get-FileHash -Path $InstallerFileARM64 -Algorithm SHA256).Hash
+    # AppsAndFeaturesEntries + ProductCode
     $InstallerARM64['AppsAndFeaturesEntries'] = @(
       [ordered]@{
-        ProductCode   = $InstallerARM64['ProductCode'] = $RealInstallerFileARM64 | Read-ProductCodeFromMsi
-        UpgradeCode   = $RealInstallerFileARM64 | Read-UpgradeCodeFromMsi
+        ProductCode   = $InstallerARM64['ProductCode'] = $InstallerFileARM643 | Read-ProductCodeFromMsi
+        UpgradeCode   = $InstallerFileARM643 | Read-UpgradeCodeFromMsi
         InstallerType = 'wix'
       }
     )
 
     try {
-      $Object2 = Invoke-RestMethod -Uri 'https://www.getpaint.net/updates/v9/manifest.os1000.x64.json'
+      $Object2 = [System.IO.StringReader]::new((Invoke-WebRequest -Uri 'https://getpaint.net/roadmap.html' | ConvertFrom-Html | Get-TextContent))
 
-      if ($Object2.releases[0].'display-name'.Contains($this.CurrentState.Version)) {
-        # ReleaseNotesUrl
-        $this.CurrentState.Locale += [ordered]@{
-          Key   = 'ReleaseNotesUrl'
-          Value = $ReleaseNotesUrl = $Object2.releases[0].'more-info-url'
-        }
-      } else {
-        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
-        # ReleaseNotesUrl
-        $this.CurrentState.Locale += [ordered]@{
-          Key   = 'ReleaseNotesUrl'
-          Value = $Object1.html_url
-        }
-      }
-    } catch {
-      $_ | Out-Host
-      $this.Log($_, 'Warning')
-      # ReleaseNotesUrl
-      $this.CurrentState.Locale += [ordered]@{
-        Key   = 'ReleaseNotesUrl'
-        Value = $Object1.html_url
-      }
-    }
-
-    try {
-      if (Test-Path -Path Variable:\ReleaseNotesUrl) {
-        $Object3 = Invoke-WebRequest -Uri $ReleaseNotesUrl | ConvertFrom-Html
-
-        $ReleaseNotesTitleNode = $Object3.SelectSingleNode('//div[@data-role="commentContent"]/h2[contains(text(), "Change Log")]')
-        $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node; $Node = $Node.NextSibling) { $Node }
-        if ($ReleaseNotesNodes) {
-          # ReleaseNotes (en-US)
-          $this.CurrentState.Locale += [ordered]@{
-            Locale = 'en-US'
-            Key    = 'ReleaseNotes'
-            Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
+      while ($Object2.Peek() -ne -1) {
+        $String = $Object2.ReadLine()
+        if ($String -match "^Paint\.NET $([regex]::Escape($this.CurrentState.Version))") {
+          try {
+            # ReleaseTime
+            $this.CurrentState.ReleaseTime ??= [datetime]::ParseExact(
+              [regex]::Match($String, '([a-zA-Z]+\W+\d{1,2}(st|nd|rd|th)\W+20\d{2})').Groups[1].Value,
+              # "[string[]]" is needed here to convert "array" object to string array
+              [string[]]@(
+                "MMMM d'st', yyyy",
+                "MMMM d'nd', yyyy",
+                "MMMM d'rd', yyyy",
+                "MMMM d'th', yyyy"
+              ),
+              (Get-Culture -Name 'en-US'),
+              [System.Globalization.DateTimeStyles]::None
+            ).ToString('yyyy-MM-dd')
+          } catch {
+            $this.Log("No ReleaseTime for version $($this.CurrentState.Version)", 'Warning')
           }
-        } else {
-          $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+          break
+        }
+      }
+      if ($Object2.Peek() -ne -1) {
+        $ReleaseNotesObjects = [System.Collections.Generic.List[string]]::new()
+        while ($Object2.Peek() -ne -1) {
+          $String = $Object2.ReadLine()
+          if ($String -notmatch '^Paint\.NET (\d+(?:\.\d+)+)') {
+            $ReleaseNotesObjects.Add($String)
+          } else {
+            break
+          }
+        }
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesObjects | Format-Text
         }
       } else {
-        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+        $this.Log("No ReleaseTime and ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
       }
+
+      $Object2.Close()
     } catch {
       $_ | Out-Host
       $this.Log($_, 'Warning')
