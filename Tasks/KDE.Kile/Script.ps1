b8ea966af982ef3760f8321502f386fca491028a
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-02-11 01:32:13 +0800

    Add KDE.Kile

---
+++ Tasks/KDE.Kile/Script.ps1
@@ -0,0 +1,41 @@
+$ProjectName = 'kile'
+$RootPath = '/unstable'
+
+$Object1 = Invoke-RestMethod -Uri "https://sourceforge.net/projects/${ProjectName}/rss?path=${RootPath}"
+$Assets = $Object1.Where({ $_.title.'#cdata-section' -match "^$([regex]::Escape($RootPath))/.+/kile-\d+(?:\.\d+)+-\d+.+\.exe$" })
+
+# Version
+$this.CurrentState.Version = [regex]::Match($Assets[0].title.'#cdata-section', 'kile-(\d+(?:\.\d+)+-\d+)').Groups[1].Value
+
+# RealVersion
+$this.CurrentState.RealVersion = [regex]::Match($Assets[0].title.'#cdata-section', 'kile-(\d+(?:\.\d+)+)-\d+').Groups[1].Value
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Assets[0].link | ConvertTo-UnescapedUri
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = [datetime]::ParseExact(
+        $Assets[0].pubDate,
+        'ddd, dd MMM yyyy HH:mm:ss "UT"',
+        (Get-Culture -Name 'en-US')
+      ) | ConvertTo-UtcDateTime -Id 'UTC'
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
