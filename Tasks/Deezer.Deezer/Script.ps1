38893efcac6935fb6e99186ff52dfaf4626f17f8
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-02-15 17:04:32 +0800

    Add Deezer.Deezer

---
+++ Tasks/Deezer.Deezer/Script.ps1
@@ -0,0 +1,31 @@
+$Object1 = Invoke-RestMethod -Uri 'https://www.deezer.com/desktop/update/latest.yml?architecture=x86&platform=win32&platformVersion=10.0.22000' | ConvertFrom-Yaml
+
+# Version
+$this.CurrentState.Version = $Object1.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x86'
+  InstallerUrl = "https://www.deezer.com/desktop/update/artifact-win32-x86-$($this.CurrentState.Version)"
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.releaseDate | Get-Date -AsUTC
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
