ac8449d86b3b09dde018647ea4237345b638bfa6
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-02-16 17:25:08 +0800

    Add Xiph.FLAC

---
+++ Tasks/Xiph.FLAC/Script.ps1
@@ -0,0 +1,83 @@
+$RepoOwner = 'xiph'
+$RepoName = 'flac'
+
+$Object1 = Invoke-GitHubApi -Uri "https://api.github.com/repos/${RepoOwner}/${RepoName}/releases/latest"
+
+# Version
+$this.CurrentState.Version = $Object1.tag_name -creplace '^v'
+
+# Installer
+$Asset = $Object1.assets.Where({ $_.name.EndsWith('.zip') -and $_.name.Contains('win') }, 'First')[0]
+$this.CurrentState.Installer += [ordered]@{
+  Architecture         = 'x86'
+  InstallerUrl         = $Asset.browser_download_url | ConvertTo-UnescapedUri
+  NestedInstallerFiles = @(
+    [ordered]@{
+      RelativeFilePath     = "$($Asset.name | Split-Path -LeafBase)\Win32\flac.exe"
+      PortableCommandAlias = 'flac'
+    }
+    [ordered]@{
+      RelativeFilePath     = "$($Asset.name | Split-Path -LeafBase)\Win32\metaflac.exe"
+      PortableCommandAlias = 'metaflac'
+    }
+  )
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture         = 'x64'
+  InstallerUrl         = $Asset.browser_download_url | ConvertTo-UnescapedUri
+  NestedInstallerFiles = @(
+    [ordered]@{
+      RelativeFilePath     = "$($Asset.name | Split-Path -LeafBase)\Win64\flac.exe"
+      PortableCommandAlias = 'flac'
+    }
+    [ordered]@{
+      RelativeFilePath     = "$($Asset.name | Split-Path -LeafBase)\Win64\metaflac.exe"
+      PortableCommandAlias = 'metaflac'
+    }
+  )
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.published_at.ToUniversalTime()
+
+      if (-not [string]::IsNullOrWhiteSpace($Object1.body)) {
+        $ReleaseNotesObject = $Object1.body | Convert-MarkdownToHtml -Extensions 'advanced', 'emojis', 'hardlinebreak'
+        $ReleaseNotesTitleNode = $ReleaseNotesObject.SelectSingleNode('./h2[text()="Changes"]')
+        if ($ReleaseNotesTitleNode) {
+          $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node -and $Node.Name -ne 'h2'; $Node = $Node.NextSibling) { $Node }
+          # ReleaseNotes (en-US)
+          $this.CurrentState.Locale += [ordered]@{
+            Locale = 'en-US'
+            Key    = 'ReleaseNotes'
+            Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
+          }
+        } else {
+          # ReleaseNotes (en-US)
+          $this.CurrentState.Locale += [ordered]@{
+            Locale = 'en-US'
+            Key    = 'ReleaseNotes'
+            Value  = $ReleaseNotesObject | Get-TextContent | Format-Text
+          }
+        }
+      } else {
+        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
