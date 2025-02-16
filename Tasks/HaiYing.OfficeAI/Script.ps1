9d979dc802d2c77c636a8e67f19c70826d01dfaa
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-02-16 19:54:53 +0800

    HaiYing.OfficeAI: Fix log content

--- Tasks/HaiYing.OfficeAI/Script.ps1
+++ Tasks/HaiYing.OfficeAI/Script.ps1
@@ -34,7 +34,7 @@ switch -Regex ($this.Check()) {
           Value  = $ReleaseNotesTitleNode.SelectNodes('./following-sibling::node()') | Get-TextContent | Format-Text
         }
       } else {
-        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+        $this.Log("No ReleaseTime and ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
       }
     } catch {
       $_ | Out-Host
