d97d61fc2324cd332eb2197cf4be58bf9b058500
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-02-17 14:45:11 +0800

    Reqwey.OllaMate: Fix case

--- Tasks/Reqwey.OllaMate/Script.ps1
+++ Tasks/Reqwey.OllaMate/Script.ps1
@@ -10,7 +10,7 @@ $this.CurrentState.Version = $Object1.tag_name -creplace '^v'
 $this.CurrentState.Installer += [ordered]@{
   Architecture  = 'x64'
   InstallerType = 'nullsoft'
-  InstallerUrl  = $Object1.assets.Where({ $_.name.EndsWith('.exe') -and $_.name.Contains('x64') -and $_.name.Contains('Setup') }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
+  InstallerUrl  = $Object1.assets.Where({ $_.name.EndsWith('.exe') -and $_.name.Contains('x64') -and $_.name.Contains('setup') }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
 }
 $this.CurrentState.Installer += [ordered]@{
   Architecture  = 'x64'
