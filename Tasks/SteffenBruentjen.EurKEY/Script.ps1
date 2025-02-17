7702d85b9d6f449016da61973c06564663d2fa56
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-02-18 00:31:02 +0800

    Add SteffenBruentjen.EurKEY

---
+++ Tasks/SteffenBruentjen.EurKEY/Script.ps1
@@ -0,0 +1,93 @@
+$Prefix = 'https://eurkey.steffen.bruentjen.eu/'
+
+$Object1 = Invoke-WebRequest -Uri "${Prefix}download.html"
+$Installer = $Object1.Links.Where({ try { $_.href.EndsWith('.zip') -and $_.href.Contains('windows') -and -not $_.href.Contains('beta') } catch {} }, 'First')[0]
+
+# Version
+$this.CurrentState.Version = [regex]::Match($Installer.outerHTML, 'version (\d+(?:\.\d+)+)').Groups[1].Value
+
+# Installer
+$this.CurrentState.Installer += $InstallerX86 = [ordered]@{
+  Architecture = 'x86'
+  InstallerUrl = Join-Uri $Prefix $Installer.href
+}
+$this.CurrentState.Installer += $InstallerX64 = [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = Join-Uri $Prefix $Installer.href
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+    # InstallerSha256
+    foreach ($Installer in $this.CurrentState.Installer) {
+      $Installer['InstallerSha256'] = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
+    }
+    $InstallerFileExtracted = New-TempFolder
+    7z.exe e -aoa -ba -bd -y -o"${InstallerFileExtracted}" $InstallerFile 'eurkey_i386.msi' 'eurkey_amd64.msi' | Out-Host
+    $InstallerFile2 = Join-Path $InstallerFileExtracted 'eurkey_i386.msi'
+    # RealVersion
+    $this.CurrentState.RealVersion = $InstallerFile2 | Read-ProductVersionFromMsi
+    # AppsAndFeaturesEntries + ProductCode
+    $InstallerX86['AppsAndFeaturesEntries'] = @(
+      [ordered]@{
+        ProductCode = $InstallerX86['ProductCode'] = $InstallerFile2 | Read-ProductCodeFromMsi
+        UpgradeCode = $InstallerFile2 | Read-UpgradeCodeFromMsi
+      }
+    )
+    $InstallerFile3 = Join-Path $InstallerFileExtracted 'eurkey_amd64.msi'
+    # AppsAndFeaturesEntries + ProductCode
+    $InstallerX64['AppsAndFeaturesEntries'] = @(
+      [ordered]@{
+        ProductCode = $InstallerX64['ProductCode'] = $InstallerFile3 | Read-ProductCodeFromMsi
+        UpgradeCode = $InstallerFile3 | Read-UpgradeCodeFromMsi
+      }
+    )
+
+    try {
+      $Object2 = Invoke-WebRequest -Uri 'https://eurkey.steffen.bruentjen.eu/changelog.html' | ConvertFrom-Html
+      $Object3 = [System.IO.StringReader]::new($Object2.SelectSingleNode('//pre').InnerText)
+
+      while ($Object3.Peek() -ne -1) {
+        $String = $Object3.ReadLine()
+        if ($String -match "^Version $([regex]::Escape($this.CurrentState.Version))$") {
+          $null = $Object3.ReadLine()
+          break
+        }
+      }
+      if ($Object3.Peek() -ne -1) {
+        $ReleaseNotesObjects = [System.Collections.Generic.List[string]]::new()
+        while ($Object3.Peek() -ne -1) {
+          $String = $Object3.ReadLine()
+          if ($String -notmatch '^Version \d+(\.\d+)+$') {
+            $ReleaseNotesObjects.Add($String)
+          } else {
+            break
+          }
+        }
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesObjects | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+
+      $Object3.Close()
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
