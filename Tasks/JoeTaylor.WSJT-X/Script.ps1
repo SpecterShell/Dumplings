3d4923129da2191f5e8994a040b0d1ff291344b5
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-02-18 13:22:25 +0800

    Add JoeTaylor.WSJT-X

---
+++ Tasks/JoeTaylor.WSJT-X/Script.ps1
@@ -0,0 +1,143 @@
+$ProjectName = 'wsjt'
+$RootPath = ''
+
+$Object1 = Invoke-RestMethod -Uri "https://sourceforge.net/projects/${ProjectName}/rss?path=${RootPath}"
+$Assets = $Object1.Where({ $_.title.'#cdata-section' -match "^$([regex]::Escape($RootPath))/wsjtx-(\d+(?:\.\d+)+)/wsjtx-.+\.exe$" })
+
+# Version
+$this.CurrentState.Version = [regex]::Match($Assets[0].title.'#cdata-section', "^$([regex]::Escape($RootPath))/wsjtx-(\d+(?:\.\d+)+)/").Groups[1].Value
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x86'
+  InstallerUrl = $Assets.Where({ $_.title.'#cdata-section'.EndsWith('.exe') -and $_.title.'#cdata-section'.Contains('win32') }, 'First')[0].link | ConvertTo-UnescapedUri
+  ProductCode  = "wsjtx $($this.CurrentState.Version)"
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = $Assets.Where({ $_.title.'#cdata-section'.EndsWith('.exe') -and $_.title.'#cdata-section'.Contains('win64') }, 'First')[0].link | ConvertTo-UnescapedUri
+  ProductCode  = "wsjtx $($this.CurrentState.Version)"
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = [datetime]::ParseExact(
+        $Assets.Where({ $_.title.'#cdata-section'.EndsWith('.exe') -and $_.title.'#cdata-section'.Contains('win64') }, 'First')[0].pubDate,
+        'ddd, dd MMM yyyy HH:mm:ss "UT"',
+        (Get-Culture -Name 'en-US')
+      ) | ConvertTo-UtcDateTime -Id 'UTC'
+
+      # Documentations
+      $this.CurrentState.Locale += [ordered]@{
+        Key   = 'Documentations'
+        Value = @(
+          [ordered]@{
+            DocumentLabel = 'User Guide'
+            DocumentUrl   = "https://wsjt.sourceforge.io/wsjtx-doc/wsjtx-main-$($this.CurrentState.Version).html"
+          }
+          [ordered]@{
+            DocumentLabel = 'Q65 Mode Quick-Start Guide'
+            DocumentUrl   = 'https://wsjt.sourceforge.io/Q65_Quick_Start.pdf'
+          }
+          [ordered]@{
+            DocumentLabel = 'SuperFox Mode User Guide'
+            DocumentUrl   = 'https://wsjt.sourceforge.io/SuperFox_User_Guide.pdf'
+          }
+          [ordered]@{
+            DocumentLabel = 'WSJT-X and MAP65 Quick-Start Guide'
+            DocumentUrl   = 'https://wsjt.sourceforge.io/WSJTX_2.5.0_MAP65_3.0_Quick_Start.pdf'
+          }
+          [ordered]@{
+            DocumentLabel = 'FT8 DXpedition Mode User Guide'
+            DocumentUrl   = 'https://wsjt.sourceforge.io/FT8_DXpedition_Mode.pdf'
+          }
+        )
+      }
+      # Documentations (zh-CN)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'zh-CN'
+        Key    = 'Documentations'
+        Value  = @(
+          [ordered]@{
+            DocumentLabel = '用户指南'
+            DocumentUrl   = "https://wsjt.sourceforge.io/wsjtx-doc/wsjtx-main-$($this.CurrentState.Version).html"
+          }
+          [ordered]@{
+            DocumentLabel = 'Q65 模式 快速入门'
+            DocumentUrl   = 'https://wsjt.sourceforge.io/Q65_Quick_Start.pdf'
+          }
+          [ordered]@{
+            DocumentLabel = 'SuperFox 模式 使用指南'
+            DocumentUrl   = 'https://wsjt.sourceforge.io/SuperFox_User_Guide.pdf'
+          }
+          [ordered]@{
+            DocumentLabel = 'WSJT-X 和 MAP65 快速入门'
+            DocumentUrl   = 'https://wsjt.sourceforge.io/WSJTX_2.5.0_MAP65_3.0_Quick_Start.pdf'
+          }
+          [ordered]@{
+            DocumentLabel = 'FT8 远征模式 使用指南'
+            DocumentUrl   = 'https://wsjt.sourceforge.io/FT8_DXpedition_Mode_Chinese.pdf'
+          }
+        )
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    try {
+      # ReleaseNotesUrl
+      $this.CurrentState.Locale += [ordered]@{
+        Key   = 'ReleaseNotesUrl'
+        Value = $ReleaseNotesUrl = "https://wsjt.sourceforge.io/wsjtx-doc/Release_Notes_$($this.CurrentState.Version).txt"
+      }
+
+      try {
+        $Object2 = [System.IO.StreamReader]::new((Invoke-WebRequest -Uri $ReleaseNotesUrl).RawContentStream)
+
+        while (-not $Object2.EndOfStream) {
+          if ($Object2.ReadLine() -match "Release: WSJT-X $([regex]::Escape($this.CurrentState.Version))") {
+            1..3 | ForEach-Object { $null = $Object2.ReadLine() }
+            break
+          }
+        }
+        if (-not $Object2.EndOfStream) {
+          $ReleaseNotesObjects = [System.Collections.Generic.List[string]]::new()
+          while (-not $Object2.EndOfStream) {
+            $String = $Object2.ReadLine()
+            if ($String -notmatch 'Release: WSJT-X \d+(?:\.\d+)+') {
+              $ReleaseNotesObjects.Add($String)
+            } else {
+              break
+            }
+          }
+          # ReleaseNotes (en-US)
+          $this.CurrentState.Locale += [ordered]@{
+            Locale = 'en-US'
+            Key    = 'ReleaseNotes'
+            Value  = $ReleaseNotesObjects | Format-Text
+          }
+        } else {
+          $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+        }
+      } catch {
+        $_ | Out-Host
+        $this.Log($_, 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
