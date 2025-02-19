87f1c5800bbee5cb8135f9fa19475d70fb60d457
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-02-19 15:16:48 +0800

    Add Tailscale.Tailscale

---
+++ Tasks/Tailscale.Tailscale/Script.ps1
@@ -0,0 +1,72 @@
+$Prefix = 'https://pkgs.tailscale.com/stable/'
+
+$Object1 = Invoke-RestMethod -Uri "${Prefix}?mode=json&os=windows"
+
+# Version
+$this.CurrentState.Version = $Object1.Version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerType = 'burn'
+  InstallerUrl  = Join-Uri $Prefix "tailscale-setup-full-$($this.CurrentState.Version).exe"
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture  = 'x86'
+  InstallerType = 'wix'
+  InstallerUrl  = Join-Uri $Prefix $Object1.Installer.'x86.msi'
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture  = 'x64'
+  InstallerType = 'wix'
+  InstallerUrl  = Join-Uri $Prefix $Object1.Installer.'amd64.msi'
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture  = 'arm64'
+  InstallerType = 'wix'
+  InstallerUrl  = Join-Uri $Prefix $Object1.Installer.'arm64.msi'
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseNotesUrl
+      $this.CurrentState.Locale += [ordered]@{
+        Key   = 'ReleaseNotesUrl'
+        Value = 'https://tailscale.com/changelog'
+      }
+
+      $Object2 = (Invoke-RestMethod -Uri 'https://tailscale.com/changelog/index.xml').Where({ $_.title -eq "Tailscale v$($this.CurrentState.Version)" }, 'First')
+      if ($Object2) {
+        # ReleaseTime
+        $this.CurrentState.ReleaseTime = $Object2[0].pubDate | Get-Date -AsUTC
+
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $Object2[0].description | ConvertFrom-Html | Get-TextContent | Format-Text
+        }
+
+        # ReleaseNotesUrl
+        $this.CurrentState.Locale += [ordered]@{
+          Key   = 'ReleaseNotesUrl'
+          Value = $Object2[0].link
+        }
+      } else {
+        $this.Log("No ReleaseNotes for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
