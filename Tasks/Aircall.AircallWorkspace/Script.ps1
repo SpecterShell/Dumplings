8968d389abfe4e101a849734a11bbfdf73efe7cc
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-02-20 00:31:54 +0800

    Add Aircall.AircallWorkspace

---
+++ Tasks/Aircall.AircallWorkspace/Script.ps1
@@ -0,0 +1,47 @@
+$Object1 = Invoke-WebRequest -Uri 'https://download-electron.aircall.io/aircall-workspace/RELEASES' | Read-ResponseContent | ConvertFrom-SquirrelReleases | Where-Object -FilterScript { -not $_.IsDelta } | Sort-Object -Property { $_.Version -creplace '\d+', { $_.Value.PadLeft(20) } } -Bottom 1
+
+# Version
+$this.CurrentState.Version = $Object1.Version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture           = 'x64'
+  InstallerType          = 'exe'
+  InstallerUrl           = "https://download-electron.aircall.io/aircall-workspace/Aircall-Workspace-$($this.CurrentState.Version)-x64.exe"
+  AppsAndFeaturesEntries = @(
+    [ordered]@{
+      DisplayName    = 'Aircall-Workspace'
+      DisplayVersion = $this.CurrentState.Version
+    }
+  )
+}
+$this.CurrentState.Installer += $InstallerWiX = [ordered]@{
+  Architecture  = 'x64'
+  InstallerType = 'wix'
+  InstallerUrl  = "https://download-electron.aircall.io/aircall-workspace/Aircall-Workspace-$($this.CurrentState.Version)-x64.msi"
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $InstallerWiXFile = Get-TempFile -Uri $InstallerWiX.InstallerUrl
+    # InstallerSha256
+    $InstallerWiX['InstallerSha256'] = (Get-FileHash -Path $InstallerWiXFile -Algorithm SHA256).Hash
+    # AppsAndFeaturesEntries + ProductCode
+    $InstallerWiX['AppsAndFeaturesEntries'] = @(
+      [ordered]@{
+        DisplayVersion = $InstallerWiXFile | Read-ProductVersionFromMsi
+        ProductCode    = $InstallerWiX['ProductCode'] = $InstallerWiXFile | Read-ProductCodeFromMsi
+        UpgradeCode    = $InstallerWiXFile | Read-UpgradeCodeFromMsi
+      }
+    )
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
