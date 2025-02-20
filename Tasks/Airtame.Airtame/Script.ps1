58a24e681796d144bf46d232c9eaabeced9782f2
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-02-20 23:08:03 +0800

    Add Airtame.Airtame

---
+++ Tasks/Airtame.Airtame/Script.ps1
@@ -0,0 +1,37 @@
+$Prefix = 'https://downloads.airtame.com/app/latest/win/'
+
+$Object1 = Invoke-RestMethod -Uri "${Prefix}latest.yml?noCache=$(Get-Random)" | ConvertFrom-Yaml
+
+# Version
+$this.CurrentState.Version = $Object1.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerType = 'nullsoft'
+  InstallerUrl  = Join-Uri $Prefix $Object1.files[0].url
+}
+$this.CurrentState.Installer += [ordered]@{
+  InstallerType = 'msi'
+  InstallerUrl  = (Join-Uri $Prefix $Object1.files[0].url) -replace '\.exe$', '.msi'
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.releaseDate | Get-Date -AsUTC
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
