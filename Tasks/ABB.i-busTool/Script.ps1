e1b98264f89f1e2259fa672c56511eacf0be0aee
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-02-20 13:46:49 +0800

    Add ABB.i-busTool

---
+++ Tasks/ABB.i-busTool/Script.ps1
@@ -0,0 +1,22 @@
+# $Object1 = Invoke-WebRequest -Uri 'http://www.knx-gebaeudesysteme.de/sto_g/i-bus-tool/_NEW2_/ABB/UpdateDescription.xml' | Read-ResponseContent | ConvertFrom-Xml
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = Get-RedirectedUrl1st -Uri 'https://search.abb.com/library/Download.aspx?DocumentID=9AKK106354A1779&Action=Launch' | ConvertTo-UnescapedUri
+}
+
+# Version
+$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d+(?:\.\d+){2,})').Groups[1].Value
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
