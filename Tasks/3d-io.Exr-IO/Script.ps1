38c22b5b57bf88f28567a09885b9c0e47fc81eef
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-02-22 11:05:33 +0800

    Add 3d-io.Exr-IO

---
+++ Tasks/3d-io.Exr-IO/Script.ps1
@@ -0,0 +1,23 @@
+# $Object1 = Invoke-RestMethod -Uri 'https://www.exr-io.com/UpdateCheck.php'
+$Object1 = Invoke-WebRequest -Uri 'https://www.exr-io.com/'
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.Links.Where({ try { $_.href.EndsWith('.exe') } catch {} }, 'First')[0].href
+}
+
+# Version
+$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d+(?:\.\d+){2})').Groups[1].Value
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
