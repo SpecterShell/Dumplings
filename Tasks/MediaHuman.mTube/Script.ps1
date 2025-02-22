44379070dac7231cef8e6c09c5bee6d6108e7046
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-02-22 14:57:18 +0800

    Add MediaHuman.mTube

---
+++ Tasks/MediaHuman.mTube/Script.ps1
@@ -0,0 +1,30 @@
+$Object1 = Invoke-WebRequest -Uri 'https://www.mediahuman.com/mtube/' | ConvertFrom-Html
+
+# Version
+$this.CurrentState.Version = $Object1.SelectSingleNode('//span[@itemprop="softwareVersion"]').InnerText
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = 'https://www.mediahuman.com/files/mTube.exe'
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = [regex]::Match($Object1.SelectSingleNode('.//span[@itemprop="datePublished"]').InnerText, '([a-zA-Z]+\W+\d{1,2}\W+20\d{2})').Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
