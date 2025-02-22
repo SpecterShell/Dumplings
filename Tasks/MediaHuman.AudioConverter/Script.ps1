fd1fbaef779e070ab2a4aa54f194fdf2375effb9
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-02-22 14:19:03 +0800

    Add MediaHuman.AudioConverter

---
+++ Tasks/MediaHuman.AudioConverter/Script.ps1
@@ -0,0 +1,45 @@
+$Object1 = [regex]::Match(
+  (Invoke-WebRequest -Uri 'https://www.mediahuman.com/files/AudioConverter.xml').Content,
+  '(?s)(<VersionInfo>.+?</VersionInfo>)'
+).Groups[1].Value | ConvertFrom-Xml
+
+# Version
+$this.CurrentState.Version = $Object1.VersionInfo.current
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x86'
+  InstallerUrl = $Object1.VersionInfo.url.win.'#text' | ConvertTo-Https
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = $Object1.VersionInfo.url.win64.'#text' | ConvertTo-Https
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = [datetime]::ParseExact($Object1.VersionInfo.date, 'dd/MM/yyyy', $null).ToString('yyyy-MM-dd')
+
+      # ReleaseNotes (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotes'
+        Value  = $Object1.VersionInfo.changes | Format-Text
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
