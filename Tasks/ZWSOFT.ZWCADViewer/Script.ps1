9a6e655ff217e5cbe4102e09baace85fe7332df7
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-02-23 10:59:20 +0800

    Add ZWSOFT.ZWCADViewer

---
+++ Tasks/ZWSOFT.ZWCADViewer/Script.ps1
@@ -0,0 +1,38 @@
+$Object1 = Invoke-WebRequest -Uri 'https://upgrade-online.zwsoft.cn/zwcadviewer/Viewer/ZwServerUpdateConfig.xml' | Read-ResponseContent | ConvertFrom-Xml
+
+# Version
+$this.CurrentState.Version = $Object1.ServerConfigs.ZWCADViewer_X64.'zh-CN'.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = $Object1.ServerConfigs.ZWCADViewer_X64.'zh-CN'.mspSourceUrl
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseNotes (zh-CN)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'zh-CN'
+        Key    = 'ReleaseNotes'
+        Value  = $Object1.ServerConfigs.ZWCADViewer_X64.'zh-CN'.content.Replace('^', "`n") | Format-Text
+      }
+
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = [regex]::Match($Object1.ServerConfigs.ZWCADViewer_X64.'zh-CN'.vernum, '(20\d{2}\.\d{2}\.\d{2})').Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
