e7299dc2d32d247ddab5479d78aa3aad7af02a47
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-02-26 11:59:26 +0800

    RARLab.WinRAR: Skip PR check if only the installers are changed

--- Tasks/RARLab.WinRAR/Script.ps1
+++ Tasks/RARLab.WinRAR/Script.ps1
@@ -52,6 +52,9 @@ switch -Regex ($this.Check()) {
     $this.Print()
     $this.Write()
   }
+  { $_.Contains('Changed') -and -not $_.Contains('Updated') } {
+    $this.Config.IgnorePRCheck = $true
+  }
   'Changed|Updated' {
     $this.Message()
     $this.Submit()
