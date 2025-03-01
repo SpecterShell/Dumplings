f21d49f9a80079d7fdf75902a8d250d05b6acdbe
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-03-01 12:13:24 +0800

    BtbN.FFmpeg: Use ComplexVersion and remove 5.1 branch

--- Tasks/BtbN.FFmpeg/Script.ps1
+++ Tasks/BtbN.FFmpeg/Script.ps1
@@ -6,7 +6,7 @@ $TargetDate = (Get-Date -AsUTC).AddDays(1).ToString('yyyy-MM-01').ToDateTime($nu
 $Object1 = (Invoke-GitHubApi -Uri "https://api.github.com/repos/${RepoOwner}/${RepoName}/releases").Where({ $_.tag_name.Contains($TargetDate.ToString('yyyy-MM-dd')) }, 'First')[0]
 
 # Version
-$this.CurrentState.Version = $Object1.tag_name -creplace '^autobuild-'
+$this.CurrentState.Version = [ComplexVersion]($Object1.tag_name -creplace '^autobuild-')
 
 # Installer
 $Asset = $Object1.assets.Where({ $_.name.EndsWith('.zip') -and $_.name.Contains('win64') -and $_.name.Contains('-gpl') -and -not $_.name.Contains('shared') -and $_.name.Contains('N-') }, 'First')[0]
@@ -54,7 +54,7 @@ switch -Regex ($this.Check()) {
   'Updated' {
     foreach ($License in @('GPL', 'LGPL')) {
       foreach ($Shared in @($false, $true)) {
-        foreach ($Branch in @('master', '5.1', '6.1', '7.1')) {
+        foreach ($Branch in @('master', '6.1', '7.1')) {
           $this.Config.WinGetIdentifier = "BtbN.FFmpeg.${License}$($Shared ? '.Shared' : '')$($Branch -eq 'master' ? '' : ".${Branch}")"
 
           $Asset = $Object1.assets.Where({ $_.name.EndsWith('.zip') -and $_.name.Contains('win64') -and $_.name.Contains("-$($License.ToLower())") -and ($Shared -eq $_.name.Contains('shared')) -and ($Branch -eq 'master' ? $_.name.Contains('N-') : $_.name.Contains("n${Branch}")) }, 'First')[0]
