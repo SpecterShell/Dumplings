086b570f38a1e9d1f13e5813ebd1785ef75faedb
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-03-02 10:30:11 +0800

    Add ZWSOFT.ZWCAD.Mechanical.2025

---
+++ Tasks/ZWSOFT.ZWCAD.Mechanical.2025/Script.ps1
@@ -0,0 +1,37 @@
+$Object1 = (Invoke-RestMethod -Uri 'https://www.zwsoft.cn/index.php?g=Api&m=Common&a=getDownloadCenterProduct').data.Where({ $_.title -eq '中望CAD机械版 2025' }, 'First')[0]
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = (($Object1.download[0].url.Split('/') | Sort-Object -Property { $_.Length } -Bottom 1) -replace '(?<!=)$', '=' | ConvertFrom-Base64) -replace '^\d+' -replace 'https?://dl\.zwsoft\.cn', 'https://upgrade-online.zwsoft.cn'
+}
+
+# Version
+$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, 'Release(\d+(?:\.\d+)+)').Groups[1].Value
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.updateDate | Get-Date -Format 'yyyy-MM-dd'
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+
+    # InstallerSha256
+    $this.CurrentState.Installer[0]['InstallerSha256'] = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
+    # RealVersion
+    $this.CurrentState.RealVersion = $InstallerFile | Read-ProductVersionFromExe
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
