c6f30b1b4c346a9855841dce8a1a7089d7e84d0d
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-03-02 10:59:32 +0800

    Add Sogelink.Appeee

---
+++ Tasks/Sogelink.Appeee/Script.ps1
@@ -0,0 +1,23 @@
+$Object1 = Invoke-WebRequest -Uri 'https://web.appeee.nl/Files/UpdateWinApp/appeee/RELEASES' | Read-ResponseContent | ConvertFrom-SquirrelReleases | Where-Object -FilterScript { -not $_.IsDelta } | Sort-Object -Property { [RawVersion]$_.Version } -Bottom 1
+
+# Version
+$this.CurrentState.Version = $Object1.Version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerType = 'exe'
+  InstallerUrl  = 'https://web.appeee.nl/Files/UpdateWinApp/appeee/AppeeeSetup.exe'
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
