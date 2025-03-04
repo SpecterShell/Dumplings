8386e84c9e09335e3a0cbb44ee9e90016efed201
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-03-04 11:34:43 +0800

    Add SignerLabs.Klee

---
+++ Tasks/SignerLabs.Klee.CPU/Script.ps1
@@ -0,0 +1,43 @@
+$Prefix = 'https://dvnr1hi9fanyr.cloudfront.net/client-release/Windows-CPU/'
+
+$Object1 = Invoke-RestMethod -Uri "${Prefix}latest.yml" | ConvertFrom-Yaml
+
+# Version
+$this.CurrentState.Version = $Object1.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = Join-Uri $Prefix $Object1.files[0].url
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.releaseDate | Get-Date -AsUTC
+
+      # ReleaseNotes
+      if ($Object1.Contains('releaseNotes')) {
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $Object1.releaseNotes | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
