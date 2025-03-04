ad0785cb13daae21e3548560d37dfe0797e5d853
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-03-04 19:40:52 +0800

    Add OOMOL.OOMOLStudio

---
+++ Tasks/OOMOL.OOMOLStudio/Script.ps1
@@ -0,0 +1,58 @@
+$Object1 = Invoke-RestMethod -Uri "https://update.oomol.com/api/update/win32-x64-user/stable/$($this.LastState.Contains('Hash') ? $this.LastState.Hash : '507dabb43c68dc48d32ba20158801b2f94aa7c2b')" -StatusCodeVariable 'StatusCode'
+
+if ($StatusCode -eq 204) {
+  $this.Log("The version $($this.LastState.Version) from the last state is the latest, skip checking", 'Info')
+  return
+}
+
+# Version
+$this.CurrentState.Version = $Object1.productVersion
+
+# Hash
+$this.CurrentState.Hash = $Object1.hash
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture    = 'x64'
+  Scope           = 'user'
+  InstallerUrl    = $Object1.url
+  InstallerSha256 = $Object1.sha256hash.ToUpper()
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.timestamp | ConvertFrom-UnixTimeMilliseconds
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    try {
+      $Object2 = Invoke-RestMethod -Uri 'https://oomol.com/updates/rss.xml' | Where-Object -FilterScript { $_.title.'#cdata-section'.Contains($this.CurrentState.Version) } | Select-Object -First 1
+
+      if ($Object2) {
+        # ReleaseNotesUrl
+        $this.CurrentState.Locale += [ordered]@{
+          Key   = 'ReleaseNotesUrl'
+          Value = $Object2.link
+        }
+      } else {
+        $this.Log("No ReleaseNotesUrl for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
