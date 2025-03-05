ec4780e43c8a2e168169fa7f8d87882199fc68c5
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-03-05 11:51:58 +0800

    Add Revopoint.RevoScan.MetroX

---
+++ Tasks/#Revopoint.RevoScan.MetroX/Script.ps1
@@ -0,0 +1,96 @@
+$OldReleasesPath = Join-Path $PSScriptRoot 'Releases.yaml'
+if (Test-Path -Path $OldReleasesPath) {
+  $Global:DumplingsStorage['RevoScanMetroX'] = $OldReleases = Get-Content -Path $OldReleasesPath -Raw | ConvertFrom-Yaml -Ordered
+} else {
+  $Global:DumplingsStorage['RevoScanMetroX'] = $OldReleases = [ordered]@{}
+}
+
+# en-US
+$Object1 = Invoke-RestMethod -Uri 'https://api.infly3d.com/software/version/info' -Method Post -Body (
+  @{
+    deploy                = 'test'
+    sn_code               = 'FFFFFFFFFFFFFM051'
+    user_version_string   = $this.LastState.Contains('Version') ? "v$($this.LastState.Version)" : 'v5.4.12.1526'
+    version_desc_language = 'en_US'
+  } | ConvertTo-Json -Compress
+) -ContentType 'application/json'
+
+if ($Object1.code -eq 1000) {
+  $this.Log("The version $($this.LastState.Version) from the last state is the latest, skip checking", 'Info')
+  return
+}
+if ($Object1.code -ne 200) { throw $Object1.msg }
+
+$VersionEN = $Object1.data.target_version -replace '^v'
+
+# zh-CN
+$Object2 = Invoke-RestMethod -Uri 'https://api.infly3d.com/software/version/info' -Method Post -Body (
+  @{
+    deploy                = 'test'
+    sn_code               = 'FFFFFFFFFFFFFM051'
+    user_version_string   = $this.LastState.Contains('Version') ? "v$($this.LastState.Version)" : 'v5.4.12.1526'
+    version_desc_language = 'zh_CN'
+  } | ConvertTo-Json -Compress
+) -ContentType 'application/json'
+
+if ($Object2.code -eq 1000) {
+  $this.Log("The version $($this.LastState.Version) from the last state is the latest, skip checking", 'Info')
+  return
+}
+if ($Object2.code -ne 200) { throw $Object2.msg }
+
+$VersionCN = $Object2.data.target_version -replace '^v'
+
+if ($VersionEN -ne $VersionCN) {
+  $this.Log("en-US version: ${VersionEN}")
+  $this.Log("zh-CN version: ${VersionCN}")
+  throw 'Inconsistent versions detected'
+}
+
+# Version
+$this.CurrentState.Version = $VersionEN
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated|Rollbacked' {
+    # ReleaseNotes (en-US)
+    $this.CurrentState.Locale += [ordered]@{
+      Locale = 'en-US'
+      Key    = 'ReleaseNotes'
+      Value  = $ReleaseNotes = $Object1.data.version_desc | Format-Text
+    }
+    # ReleaseNotes (zh-CN)
+    $this.CurrentState.Locale += [ordered]@{
+      Locale = 'zh-CN'
+      Key    = 'ReleaseNotes'
+      Value  = $ReleaseNotesCN = $Object2.data.version_desc | Format-Text
+    }
+
+    $OldReleases[$this.CurrentState.Version] = [ordered]@{
+      ReleaseNotes   = $ReleaseNotes
+      ReleaseNotesCN = $ReleaseNotesCN
+    }
+    if ($Global:DumplingsPreference.Contains('EnableWrite') -and $Global:DumplingsPreference.EnableWrite) {
+      $OldReleases | ConvertTo-Yaml -OutFile $OldReleasesPath -Force
+    }
+  }
+  'New' {
+    $this.Print()
+    $this.Write()
+  }
+  { $_ -match 'Changed' -and $_ -notmatch 'Updated|Rollbacked' } {
+    $this.Print()
+    $this.Write()
+    $this.Message()
+  }
+  'Updated' {
+    $this.Print()
+    $this.Write()
+    if (-not $OldReleases.Contains($this.CurrentState.Version)) {
+      $this.Message()
+    }
+  }
+  { $_ -match 'Rollbacked' -and -not $OldReleases.Contains($this.CurrentState.Version) } {
+    $this.Print()
+    $this.Message()
+  }
+}
---
+++ Tasks/Revopoint.RevoScan.MetroX/Script.ps1
@@ -0,0 +1,48 @@
+$Object1 = Invoke-WebRequest -Uri 'https://www.revopoint3d.com/pages/support-download' | ConvertFrom-Html
+$Object2 = $Object1.SelectSingleNode('//*[@id="rs5m"]//div[@class="softwareitem" and contains(., "Windows")]')
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object2.SelectSingleNode('.//a[contains(@href, ".exe")]').Attributes['href'].Value
+}
+
+# Version
+$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d+(?:\.\d+){2,})').Groups[1].Value
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = [regex]::Match($Object2.InnerText, '(20\d{2}-\d{1,2}-\d{1,2})').Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
+
+      if ($Global:DumplingsStorage.Contains('RevoScanMetroX') -and $Global:DumplingsStorage.RevoScanMetroX.Contains($this.CurrentState.Version)) {
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $Global:DumplingsStorage.RevoScanMetroX[$this.CurrentState.Version].ReleaseNotes
+        }
+        # ReleaseNotes (zh-CN)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'zh-CN'
+          Key    = 'ReleaseNotes'
+          Value  = $Global:DumplingsStorage.RevoScanMetroX[$this.CurrentState.Version].ReleaseNotesCN
+        }
+      } else {
+        $this.Log("No ReleaseNotes (en-US) and ReleaseNotes (zh-CN) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
