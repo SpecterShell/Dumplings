7eb3f29c6acf789b587a00344d2748aa5eec8e2b
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-03-05 13:55:02 +0800

    Add AdsPower.AdsPower

---
+++ Tasks/AdsPower.AdsPower/Script.ps1
@@ -0,0 +1,70 @@
+# x86
+$Object1 = Invoke-RestMethod -Uri 'https://api.adspower.net/client/sys-client-version/get-latest-version?system=com_win32'
+# x64
+$Object2 = Invoke-RestMethod -Uri 'https://api.adspower.net/client/sys-client-version/get-latest-version?system=com_win64'
+
+if ($Object1.data.version -ne $Object2.data.version) {
+  $this.Log("x86 version: $($Object1.data.version)", 'Warning')
+  $this.Log("x64 version: $($Object2.data.version)", 'Warning')
+  throw 'Inconsistent versions detected'
+}
+
+# Version
+$this.CurrentState.Version = $Object2.data.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x86'
+  InstallerUrl = $Object1.data.url
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = $Object2.data.url
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseNotes (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotes'
+        Value  = $Object1.data.release.'en-US' | Format-Text
+      }
+      # ReleaseNotes (zh-CN)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'zh-CN'
+        Key    = 'ReleaseNotes'
+        Value  = $Object1.data.release.'zh-CN' | Format-Text
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    try {
+      $Prefix = $Object1.data.latest
+
+      $Object3 = Invoke-RestMethod -Uri "${Prefix}latest.yml" | ConvertFrom-Yaml
+
+      if ($Object3.version -eq $this.CurrentState.Version) {
+        # ReleaseTime
+        $this.CurrentState.ReleaseTime = $Object3.releaseDate | Get-Date -AsUTC
+      } else {
+        $this.Log("No ReleaseTime for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
