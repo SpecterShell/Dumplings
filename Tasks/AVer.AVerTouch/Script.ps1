bce32d2107721d9d369ee2c9f9b40c323bc2622f
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-03-06 22:11:56 +0800

    Add AVer.AVerTouch

---
+++ Tasks/AVer.AVerTouch/Script.ps1
@@ -0,0 +1,49 @@
+$Object1 = Invoke-RestMethod -Uri 'http://download.aver.com/AVerTouchWindows/check4Update/update.cfg' | ConvertFrom-Ini
+
+# Version
+$this.CurrentState.Version = $Object1.OTA.Version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.OTA.Path
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.OTA.ReleaseDate | Get-Date -Format 'yyyy-MM-dd'
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    foreach ($Installer in $this.CurrentState.Installer) {
+      $InstallerFile = Get-TempFile -Uri $Installer.InstallerUrl
+      $InstallerFileExtracted = New-TempFolder
+      7z.exe e -aoa -ba -bd -y -o"${InstallerFileExtracted}" $InstallerFile 'AVerTouchQt.exe' | Out-Host
+      $InstallerFile2 = Join-Path $InstallerFileExtracted 'AVerTouchQt.exe'
+      $InstallerFile2Extracted = $InstallerFile2 | Expand-InstallShield
+      $InstallerFile3 = Join-Path $InstallerFile2Extracted 'AVerTouch.msi'
+      # InstallerSha256
+      $Installer.InstallerSha256 = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
+      # AppsAndFeaturesEntries + ProductCode
+      $Installer.AppsAndFeaturesEntries = @(
+        [ordered]@{
+          ProductCode   = $Installer.ProductCode = $InstallerFile3 | Read-ProductCodeFromMsi
+          UpgradeCode   = $InstallerFile3 | Read-UpgradeCodeFromMsi
+          InstallerType = 'msi'
+        }
+      )
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
