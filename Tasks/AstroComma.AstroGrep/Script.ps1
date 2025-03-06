f5c2f8c4e5054448d2c8a843fd798b17f55e8bd8
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-03-06 15:07:55 +0800

    Add AstroComma.AstroGrep

---
+++ Tasks/AstroComma.AstroGrep/Script.ps1
@@ -0,0 +1,74 @@
+$ProjectName = 'astrogrep'
+$RootPath = '/AstroGrep (Win32)'
+$PatternPath = 'AstroGrep v(\d+(?:\.\d+)+)'
+$PatternFilename = 'AstroGrep_Setup.+\.exe'
+
+$Object1 = Invoke-RestMethod -Uri "https://sourceforge.net/projects/${ProjectName}/rss?path=${RootPath}"
+$Assets = $Object1.Where({ $_.title.'#cdata-section' -match "^$([regex]::Escape($RootPath))/${PatternPath}/${PatternFilename}$" })
+
+# Version
+$this.CurrentState.Version = [regex]::Match($Assets[0].title.'#cdata-section', "^$([regex]::Escape($RootPath))/${PatternPath}/").Groups[1].Value
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Assets[0].link | ConvertTo-UnescapedUri
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = [datetime]::ParseExact(
+        $Assets[0].pubDate,
+        'ddd, dd MMM yyyy HH:mm:ss "UT"',
+        (Get-Culture -Name 'en-US')
+      ) | ConvertTo-UtcDateTime -Id 'UTC'
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    try {
+      $Object2 = Invoke-WebRequest -Uri 'https://astrogrep.sourceforge.net/news/' | ConvertFrom-Html
+
+      $ReleaseNotesNode = $Object2.SelectSingleNode(".//div[@class='sectionContent' and contains(./span[1], 'AstroGrep v$($this.CurrentState.Version)')]")
+      if ($ReleaseNotesNode) {
+        $ReleaseTimeNode = $ReleaseNotesNode.SelectSingleNode('./div[@class="sectionDetails"]')
+        if ($ReleaseTimeNode -and $ReleaseTimeNode.InnerText -match '([a-zA-Z]+\W+\d{1,2}\W+20\d{2})') {
+          # ReleaseTime
+          $this.CurrentState.ReleaseTime ??= $Matches[1] | Get-Date -Format 'yyyy-MM-dd'
+
+          # ReleaseNotes (en-US)
+          $this.CurrentState.Locale += [ordered]@{
+            Locale = 'en-US'
+            Key    = 'ReleaseNotes'
+            Value  = $ReleaseTimeNode.SelectNodes('./following-sibling::node()') | Get-TextContent | Format-Text
+          }
+        } else {
+          $this.Log("No ReleaseTime (en-US) for version $($this.CurrentState.Version)", 'Warning')
+
+          # ReleaseNotes (en-US)
+          $this.CurrentState.Locale += [ordered]@{
+            Locale = 'en-US'
+            Key    = 'ReleaseNotes'
+            Value  = $ReleaseNotesNode.SelectNodes('./span[1]/following-sibling::node()') | Get-TextContent | Format-Text
+          }
+        }
+      } else {
+        $this.Log("No ReleaseTime and ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
