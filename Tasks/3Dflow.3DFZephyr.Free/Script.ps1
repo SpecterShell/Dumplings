2e8ad05294511b994b7b9ef7bca9eea2d902fe7f
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-03-07 11:37:29 +0800

    3Dflow.3DFZephyr.Free: Parse version from installer URL

--- Tasks/3Dflow.3DFZephyr.Free/Script.ps1
+++ Tasks/3Dflow.3DFZephyr.Free/Script.ps1
@@ -1,5 +1,5 @@
 $Object1 = Invoke-RestMethod -Uri 'http://3dflow.net/zephyr-network/networkMessage.php' -Method Post -Body @{
-  action       = 'checkUpdates'
+  action       = 'getInstaller'
   architecture = 'x64'
   product      = '3DF Zephyr Free'
   ver          = $this.LastState.Contains('Version') ? $this.LastState.Version : '7.529'
@@ -11,29 +11,61 @@ if ($Object1.Contains('NO_UPDATE_AVAILABLE')) {
 }
 
 # Version
-$this.CurrentState.Version = [regex]::Match($Object1, 'Version (\d+(?:\.\d+)+)').Groups[1].Value
-
-$Object2 = Invoke-RestMethod -Uri 'http://3dflow.net/zephyr-network/networkMessage.php' -Method Post -Body @{
-  action       = 'getInstaller'
-  architecture = 'x64'
-  product      = '3DF Zephyr Free'
-  ver          = $this.LastState.Contains('Version') ? $this.LastState.Version : '7.529'
-}
+$this.CurrentState.Version = [regex]::Match($Object1, '(\d+(?:\.\d+)+)').Groups[1].Value
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
   Architecture = 'x64'
-  InstallerUrl = $Object2.Trim() | ConvertTo-UnescapedUri
+  InstallerUrl = $Object1.Trim() | ConvertTo-UnescapedUri
 }
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
     try {
-      # ReleaseNotes (en-US)
-      $this.CurrentState.Locale += [ordered]@{
-        Locale = 'en-US'
-        Key    = 'ReleaseNotes'
-        Value  = $Object1 | Split-LineEndings | Select-Object -Skip 2 | Format-Text
+      $Object2 = Invoke-RestMethod -Uri 'http://3dflow.net/zephyr-network/networkMessage.php' -Method Post -Body @{
+        action       = 'checkUpdates'
+        architecture = 'x64'
+        product      = '3DF Zephyr Free'
+        ver          = $this.LastState.Contains('Version') ? $this.LastState.Version : '7.529'
+      }
+
+      if (-not $Object2.Contains('NO_UPDATE_AVAILABLE')) {
+        $Object3 = [System.IO.StringReader]::new($Object2.Split('|')[2])
+
+        while ($Object3.Peek() -ne -1) {
+          $String = $Object3.ReadLine()
+          if ($String.StartsWith('Version') -and $String.Contains($this.CurrentState.Version)) {
+            break
+          }
+        }
+        if ($Object3.Peek() -ne -1) {
+          $ReleaseNotesObjects = [System.Collections.Generic.List[string]]::new()
+          while ($Object3.Peek() -ne -1) {
+            $String = $Object3.ReadLine()
+            if (-not $String.StartsWith('Version')) {
+              $ReleaseNotesObjects.Add($String)
+            } else {
+              break
+            }
+          }
+          # ReleaseNotes (en-US)
+          $this.CurrentState.Locale += [ordered]@{
+            Locale = 'en-US'
+            Key    = 'ReleaseNotes'
+            Value  = $ReleaseNotesObjects | Format-Text
+          }
+        } else {
+          # ReleaseNotes (en-US)
+          $this.CurrentState.Locale += [ordered]@{
+            Locale = 'en-US'
+            Key    = 'ReleaseNotes'
+            Value  = $Object2.Split('|')[2] | Format-Text
+          }
+        }
+
+        $Object3.Close()
+      } else {
+        $this.Log("No ReleaseNotes (en-US) for version $($this.LastState.Version)", 'Warning')
       }
     } catch {
       $_ | Out-Host
