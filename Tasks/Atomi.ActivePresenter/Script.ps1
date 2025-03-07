8fb2c4626d6f9cd11df01317ac4ce677a0729c93
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-03-07 14:31:01 +0800

    Add Atomi.ActivePresenter

---
+++ Tasks/Atomi.ActivePresenter/Script.ps1
@@ -0,0 +1,60 @@
+$Prefix = 'https://atomisystems.com/updates/ActivePresenter/v9/'
+
+$Object1 = Invoke-RestMethod -Uri "${Prefix}update.manifest"
+
+# Version
+$this.CurrentState.Version = "$($Object1.versions[0].major).$($Object1.versions[0].minor).$($Object1.versions[0].build)"
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = "https://cdn.atomisystems.com/apdownloads/ActivePresenter_v$($this.CurrentState.Version)_setup.exe"
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.versions[0].packages[0].date | Get-Date -Format 'yyyy-MM-dd'
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    try {
+      # ReleaseNotesUrl
+      $this.CurrentState.Locale += [ordered]@{
+        Key   = 'ReleaseNotesUrl'
+        Value = $ReleaseNotesUrl = Join-Uri $Prefix $Object1.versions[0].releasenotes
+      }
+
+      $Object2 = Invoke-WebRequest -Uri $ReleaseNotesUrl | ConvertFrom-Html
+
+      $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//*[@class='v-title' and contains(., '$($this.CurrentState.Version)')]")
+      if ($ReleaseNotesTitleNode) {
+        # ReleaseTime
+        $this.CurrentState.ReleaseTime = [regex]::Match($ReleaseNotesTitleNode.InnerText, '(20\d{2}\.\d{1,2}\.\d{1,2})').Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
+
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesTitleNode.SelectSingleNode('./following-sibling::*[@class="v-content"]') | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseTime and ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
