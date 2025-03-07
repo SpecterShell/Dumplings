4cc1120559ba8bf8fd657e1a5300ae172030e31b
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-03-07 15:27:56 +0800

    Add MatthiasZronek.BenchMate

---
+++ Tasks/MatthiasZronek.BenchMate/Script.ps1
@@ -0,0 +1,48 @@
+$Object1 = Invoke-WebRequest -Uri 'https://benchmate.org/'
+
+# Version
+$this.CurrentState.Version = [regex]::Match($Object1.Content, 'BenchMate\W+(\d+(\.\d+)+)').Groups[1].Value
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = "https://dl.benchmate.org/bm-$($this.CurrentState.Version).exe"
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      $Object3 = Invoke-WebRequest -Uri 'https://benchmate.org/changelog/all' | ConvertFrom-Html
+
+      $ReleaseNotesTitleNode = $Object3.SelectSingleNode("//*[contains(@id, '$($this.CurrentState.Version)')]")
+      if ($ReleaseNotesTitleNode) {
+        # ReleaseTime
+        $this.CurrentState.ReleaseTime = [datetime]::ParseExact(
+          [regex]::Match($ReleaseNotesTitleNode.InnerText, '(\d{1,2}\.\d{1,2}\.20\d{2})').Groups[1].Value,
+          'dd.MM.yyyy',
+          $null
+        ).ToString('yyyy-MM-dd')
+
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesTitleNode.SelectNodes('./following-sibling::node()') | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseTime and ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
