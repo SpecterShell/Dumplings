5106cef5c0b77b738992e49abb9834cc365689e7
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-03-08 11:16:36 +0800

    Add ArcticLine.JetScreenshot

---
+++ Tasks/ArcticLine.JetScreenshot/Script.ps1
@@ -0,0 +1,44 @@
+$Object1 = @"
+<xml>
+$((Invoke-WebRequest -Uri 'http://www.jetScreenshot.com/getnews.php' -Method Post -Body @{ UserID = '' } | Read-ResponseContent -Encoding 'windows-1252').Replace('<br>', "`n"))
+</xml>
+"@ | ConvertFrom-Xml
+
+# Version
+$this.CurrentState.Version = $Object1.xml.LASTVERSION
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = 'https://jetscreenshot.com/jetScreenshot-setup.exe'
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseNotes (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotes'
+        Value  = $Object1.xml.NEWFEATURES | Format-Text
+      }
+    } catch {
+      $this.Log($_, 'Warning')
+      $_ | Out-Host
+    }
+
+    $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+    # InstallerSha256
+    $this.CurrentState.Installer[0]['InstallerSha256'] = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
+    # RealVersion
+    $this.CurrentState.RealVersion = $InstallerFile | Read-ProductVersionFromExe
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
