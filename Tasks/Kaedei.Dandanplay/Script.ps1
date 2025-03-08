0d2e156e7619903e7a52275d120e6e168f80b753
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-03-08 17:24:15 +0800

    Kaedei.Dandanplay: Switch to GitHub source and reset

--- Tasks/Kaedei.Dandanplay/Script.ps1
+++ Tasks/Kaedei.Dandanplay/Script.ps1
@@ -1,37 +1,43 @@
-$Object1 = Invoke-WebRequest -Uri 'https://www.dandanplay.com/' | ConvertFrom-Html
+$RepoOwner = 'kaedei'
+$RepoName = 'dandanplay'
+
+$Object1 = Invoke-GitHubApi -Uri "https://api.github.com/repos/${RepoOwner}/${RepoName}/releases/latest"
 
 # Version
-$this.CurrentState.Version = [regex]::Match($Object1.SelectSingleNode('//div[@class="memo" and contains(., "版本")]').InnerText, '(\d+(?:\.\d+){2,})').Groups[1].Value
+$this.CurrentState.Version = $Object1.tag_name -creplace '^windows-'
+
+# RealVersion
+$this.CurrentState.RealVersion = $this.CurrentState.Version.Split('.')[0..2] -join '.'
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
   Architecture = 'x64'
-  InstallerUrl = "https://dandan.sakurateam.top/dandanplay-x64_$($this.CurrentState.Version).exe"
+  InstallerUrl = $Object1.assets.Where({ $_.name.EndsWith('.exe') -and $_.name.Contains('x64') }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
 }
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
     try {
       # ReleaseTime
-      $this.CurrentState.ReleaseTime = [regex]::Match(
-        $Object1.SelectSingleNode('//div[@class="memo" and contains(text(), "发布日期")]').InnerText,
-        '(\d{4}年\d{1,2}月\d{1,2}日)'
-      ).Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
-    } catch {
-      $_ | Out-Host
-      $this.Log($_, 'Warning')
-    }
+      $this.CurrentState.ReleaseTime = $Object1.published_at.ToUniversalTime()
 
-    try {
-      $Object2 = Invoke-WebRequest -Uri 'https://www.dandanplay.com/blog.html' | ConvertFrom-Html
-
-      $ReleaseNotesNode = $Object2.SelectSingleNode("//div[@id='blogs']/ul/li[contains(./div/h4/text(), 'Windows版') and contains(./div/h4/text(), '$($this.CurrentState.Version)')]/div/p")
-      if ($ReleaseNotesNode) {
-        # ReleaseNotes (zh-CN)
-        $this.CurrentState.Locale += [ordered]@{
-          Locale = 'zh-CN'
-          Key    = 'ReleaseNotes'
-          Value  = $ReleaseNotesNode | Get-TextContent | Format-Text
+      if (-not [string]::IsNullOrWhiteSpace($Object1.body)) {
+        $ReleaseNotesObject = $Object1.body | Convert-MarkdownToHtml -Extensions 'advanced', 'emojis', 'hardlinebreak'
+        $ReleaseNotesTitleNode = $ReleaseNotesObject.SelectSingleNode("./p[contains(., '$($this.CurrentState.RealVersion)')]")
+        if ($ReleaseNotesTitleNode) {
+          # ReleaseNotes (zh-CN)
+          $this.CurrentState.Locale += [ordered]@{
+            Locale = 'zh-CN'
+            Key    = 'ReleaseNotes'
+            Value  = $ReleaseNotesTitleNode.SelectNodes('./following-sibling::node()') | Get-TextContent | Format-Text
+          }
+        } else {
+          # ReleaseNotes (zh-CN)
+          $this.CurrentState.Locale += [ordered]@{
+            Locale = 'zh-CN'
+            Key    = 'ReleaseNotes'
+            Value  = $ReleaseNotesObject | Get-TextContent | Format-Text
+          }
         }
       } else {
         $this.Log("No ReleaseNotes (zh-CN) for version $($this.CurrentState.Version)", 'Warning')
