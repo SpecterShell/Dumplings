ffbbd3372059560449b5e00528f9250a484fab79
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-03-09 15:55:13 +0800

    Add Algento.Botim

---
+++ Tasks/Algento.Botim/Script.ps1
@@ -0,0 +1,51 @@
+# Should be a POST request, but use GET for convenience
+$Object1 = Invoke-RestMethod -Uri 'https://web.botim.me/user/signup2/checkversion.json' -Headers @{ 'AES-HASH' = 'xxxxx' } -Body @{
+  data = @{
+    'devicetype' = 4
+    'version'    = $this.LastState.Contains('Version') ? $this.LastState.Version : '1.7.4'
+  } | ConvertTo-Json -Compress
+} -ContentType 'application/json'
+
+if ($Object1.code -eq 0) {
+  $this.Log("The version $($this.LastState.Version) from the last state is the latest, skip checking", 'Info')
+  return
+}
+
+$Object2 = $Object1.data | ConvertFrom-Json
+$Object3 = Invoke-RestMethod -Uri $Object2.desktopupgradeymlurl | ConvertFrom-Yaml
+
+# Version
+$this.CurrentState.Version = $Object3.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = Join-Uri $Object2.desktopupgradeymlurl $Object3.files[0].url
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object3.releaseDate | Get-Date -AsUTC
+
+      # ReleaseNotes (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotes'
+        Value  = $Object2.upgradeinfo | Format-Text
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
