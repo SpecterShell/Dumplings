c85b15392c0ba44a694a58859f160f59d704ce6f
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-03-11 17:19:29 +0800

    BartelsMedia.MacroRecorder: Parse version from installer and reset

--- Tasks/BartelsMedia.MacroRecorder/Script.ps1
+++ Tasks/BartelsMedia.MacroRecorder/Script.ps1
@@ -1,48 +1,113 @@
-$Object1 = Invoke-RestMethod -Uri 'https://www.macrorecorder.com/update3.php?os=win'
+function Read-Installer {
+  $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
 
-# Version
-$this.CurrentState.Version = $Object1.xml.winclient.versions.minorupdate
+  # Version
+  $this.CurrentState.Version = $InstallerFile | Read-ProductVersionFromExe
+  # InstallerSha256
+  $this.CurrentState.Installer[0]['InstallerSha256'] = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
+}
+
+function Get-ReleaseNotes {
+  try {
+    $Object2 = Invoke-WebRequest -Uri 'https://www.macrorecorder.com/download/changelog/' | ConvertFrom-Html
+
+    $ReleaseNotesNode = $Object2.SelectSingleNode("//tr[contains(./td[2]/text(), '$($this.CurrentState.Version)')]")
+    if ($ReleaseNotesNode) {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = [datetime]::ParseExact(
+        [regex]::Match($ReleaseNotesNode.SelectSingleNode('./td[1]').InnerText, '(\d{1,2}/\d{1,2}/20\d{2})').Groups[1].Value,
+        'MM/dd/yyyy',
+        $null
+      ).ToString('yyyy-MM-dd')
+
+      # ReleaseNotes (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotes'
+        Value  = $ReleaseNotesNode.SelectSingleNode('./td[3]') | Get-TextContent | Format-Text
+      }
+    } else {
+      $this.Log("No ReleaseTime and ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+    }
+  } catch {
+    $_ | Out-Host
+    $this.Log($_, 'Warning')
+  }
+}
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
   InstallerUrl = 'https://www.macrorecorder.com/MacroRecorderSetup.exe'
 }
 
+$Object1 = Invoke-WebRequest -Uri $this.CurrentState.Installer[0].InstallerUrl -Method Head
+# Last Modified
+$this.CurrentState.LastModified = $Object1.Headers.'Last-Modified'[0]
+
+# Case 0: Force submit the manifest
+if ($Global:DumplingsPreference.Contains('Force')) {
+  $this.Log('Skip checking states', 'Info')
+
+  Read-Installer
+  Get-ReleaseNotes
+
+  $this.Print()
+  $this.Write()
+  $this.Message()
+  $this.Submit()
+  return
+}
+
+# Case 1: The task is new
+if ($this.Status.Contains('New')) {
+  $this.Log('New task', 'Info')
+
+  Read-Installer
+  Get-ReleaseNotes
+
+  $this.Print()
+  $this.Write()
+  return
+}
+
+# Case 2: The Last Modified is unchanged
+if ([datetime]$this.CurrentState.LastModified -le [datetime]$this.LastState.LastModified) {
+  $this.Log("The version $($this.LastState.Version) from the last state is the latest", 'Info')
+  return
+}
+
+Read-Installer
+
+# Case 3: The current state has an invalid version
+if ([string]::IsNullOrWhiteSpace($this.CurrentState.Version)) {
+  throw 'The current state has an invalid version'
+}
+
+Get-ReleaseNotes
+
+# Case 4: The Last Modified has changed, but the SHA256 is not
+if ($this.CurrentState.Installer[0].InstallerSha256 -eq $this.LastState.Installer[0].InstallerSha256) {
+  $this.Log('The Last Modified has changed, but the SHA256 is not', 'Info')
+
+  $this.Write()
+  return
+}
+
 switch -Regex ($this.Check()) {
-  'New|Changed|Updated' {
-    try {
-      $Object2 = Invoke-WebRequest -Uri 'https://www.macrorecorder.com/download/changelog/' | ConvertFrom-Html
-
-      $ReleaseNotesNode = $Object2.SelectSingleNode("//tr[contains(./td[2]/text(), '$($this.CurrentState.Version)')]")
-      if ($ReleaseNotesNode) {
-        # ReleaseTime
-        $this.CurrentState.ReleaseTime = [datetime]::ParseExact(
-          [regex]::Match($ReleaseNotesNode.SelectSingleNode('./td[1]').InnerText, '(\d{1,2}/\d{1,2}/20\d{2})').Groups[1].Value,
-          'MM/dd/yyyy',
-          $null
-        ).ToString('yyyy-MM-dd')
-
-        # ReleaseNotes (en-US)
-        $this.CurrentState.Locale += [ordered]@{
-          Locale = 'en-US'
-          Key    = 'ReleaseNotes'
-          Value  = $ReleaseNotesNode.SelectSingleNode('./td[3]') | Get-TextContent | Format-Text
-        }
-      } else {
-        $this.Log("No ReleaseTime and ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
-      }
-    } catch {
-      $_ | Out-Host
-      $this.Log($_, 'Warning')
-    }
-
+  # Case 6: The Last Modified, the SHA256 and the version have changed
+  'Updated|Rollbacked' {
     $this.Print()
     $this.Write()
-  }
-  'Changed|Updated' {
     $this.Message()
+    $this.Submit()
   }
-  'Updated' {
+  # Case 5: The Last Modified and the SHA256 have changed, but the version is not
+  Default {
+    $this.Log('The Last Modified and the SHA256 have changed, but the version is not', 'Info')
+    $this.Config.IgnorePRCheck = $true
+    $this.Print()
+    $this.Write()
+    $this.Message()
     $this.Submit()
   }
 }
