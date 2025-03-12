4193747258c0bd64cbe01d41a6788edef1836fbd
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-03-12 18:07:58 +0800

    XK72.Charles: Read MSIX version

--- Tasks/XK72.Charles/Script.ps1
+++ Tasks/XK72.Charles/Script.ps1
@@ -19,6 +19,10 @@ $this.CurrentState.Installer += [ordered]@{
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
+    $WinGetInstallerFiles[$this.CurrentState.Installer[0].InstallerUrl] = $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+    # RealVersion
+    $this.CurrentState.RealVersion = $InstallerFile | Read-ProductVersionFromMSIX
+
     try {
       $Object3 = Invoke-WebRequest -Uri 'https://www.charlesproxy.com/documentation/version-history/' | ConvertFrom-Html
 
