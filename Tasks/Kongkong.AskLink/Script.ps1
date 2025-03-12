71f5dfa8ccfcb0c51327f0501367ea97795254bd
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-03-12 12:38:51 +0800

    Add Kongkong.AskLink

---
+++ Tasks/Kongkong.AskLink/Script.ps1
@@ -0,0 +1,34 @@
+$Object1 = (Invoke-RestMethod -Uri 'https://api01.asklink.com/download/list').data.Where({ $_.clientType -eq 'WIN' }, 'First')[0]
+
+# Version
+$this.CurrentState.Version = [regex]::Match($Object1.name, '(\d+(?:\.\d+)+)').Groups[1].Value
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.url
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseNotes (zh-CN)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'zh-CN'
+        Key    = 'ReleaseNotes'
+        Value  = $Object1.remark | Format-Text
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
