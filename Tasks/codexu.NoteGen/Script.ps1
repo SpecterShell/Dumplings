ee24498c3bb232b3f1de6d24c6af71c4ed6d28cc
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-03-12 12:43:58 +0800

    codexu.NoteGen: Fix version pattern

--- Tasks/codexu.NoteGen/Script.ps1
+++ Tasks/codexu.NoteGen/Script.ps1
@@ -4,7 +4,7 @@ $RepoName = 'note-gen'
 $Object1 = Invoke-GitHubApi -Uri "https://api.github.com/repos/${RepoOwner}/${RepoName}/releases/latest"
 
 # Version
-$this.CurrentState.Version = $Object1.tag_name -creplace '^v'
+$this.CurrentState.Version = $Object1.tag_name -creplace '^app-v'
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
