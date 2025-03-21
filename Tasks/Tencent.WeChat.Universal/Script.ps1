7d906e461659d057e75ff78f7e4a872314d8b8a1
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-03-21 11:26:01 +0800

    Tencent.WeChat.Universal: Update installer version parser and remove alternative link detection

--- Tasks/Tencent.WeChat.Universal/Script.ps1
+++ Tasks/Tencent.WeChat.Universal/Script.ps1
@@ -1,5 +1,18 @@
-$Uri1 = 'https://dldir1v6.qq.com/weixin/Universal/Windows/WeChatWin.exe'
-$Object1 = Invoke-WebRequest -Uri $Uri1 -Method Head
+function Read-Installer {
+  $this.InstallerFiles[$this.CurrentState.Installer[0].InstallerUrl] = $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+  $InstallerFileExtracted = New-TempFolder
+  7z.exe e -aoa -ba -bd -y -o"${InstallerFileExtracted}" $InstallerFile 'install.7z' | Out-Host
+  $InstallerFile2 = Join-Path $InstallerFileExtracted 'install.7z'
+  # Version
+  $this.CurrentState.Version = [regex]::Match((7z.exe l -ba -slt $InstallerFile2), 'Path = (\d+\.\d+\.\d+\.\d+)').Groups[1].Value
+}
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = 'https://dldir1v6.qq.com/weixin/Universal/Windows/WeChatWin.exe'
+}
+
+$Object1 = Invoke-WebRequest -Uri $this.CurrentState.Installer[0].InstallerUrl -Method Head
 # Hash
 $this.CurrentState.Hash = $Object1.Headers.'X-COS-META-MD5'[0]
 
@@ -7,32 +20,7 @@ $this.CurrentState.Hash = $Object1.Headers.'X-COS-META-MD5'[0]
 if ($Global:DumplingsPreference.Contains('Force')) {
   $this.Log('Skip checking states', 'Info')
 
-  $this.InstallerFiles[$Uri1] = $InstallerFile = Get-TempFile -Uri $Uri1
-  # Version
-  $this.CurrentState.Version = [regex]::Match((7z.exe l -ba -slt $InstallerFile), 'Path = (\d+\.\d+\.\d+\.\d+)').Groups[1].Value
-
-  try {
-    $Uri2 = "https://dldir1v6.qq.com/weixin/Universal/Windows/WeChatWin$($this.CurrentState.Version).exe"
-    $Object2 = Invoke-WebRequest -Uri $Uri2 -Method Head
-    # Installer
-    $this.CurrentState.Installer += [ordered]@{
-      Architecture = 'x64'
-      InstallerUrl = $Uri2
-    }
-    # Hash alternative
-    $this.CurrentState.HashA = $Object2.Headers.'X-COS-META-MD5'[0]
-    # Mode
-    $this.CurrentState.Mode = $true
-  } catch {
-    $this.Log("${Uri2} doesn't exist, fallback to ${Uri1}", 'Warning')
-    # Installer
-    $this.CurrentState.Installer += [ordered]@{
-      Architecture = 'x64'
-      InstallerUrl = $Uri1
-    }
-    # Mode
-    $this.CurrentState.Mode = $false
-  }
+  Read-Installer
 
   $this.Print()
   $this.Write()
@@ -41,150 +29,45 @@ if ($Global:DumplingsPreference.Contains('Force')) {
   return
 }
 
-# Case 1: The task is newly created
+# Case 1: The task is new
 if ($this.Status.Contains('New')) {
   $this.Log('New task', 'Info')
 
-  $this.InstallerFiles[$Uri1] = $InstallerFile = Get-TempFile -Uri $Uri1
-  # Version
-  $this.CurrentState.Version = [regex]::Match((7z.exe l -ba -slt $InstallerFile), 'Path = (\d+\.\d+\.\d+\.\d+)').Groups[1].Value
-
-  try {
-    $Uri2 = "https://dldir1v6.qq.com/weixin/Universal/Windows/WeChatWin$($this.CurrentState.Version).exe"
-    $Object2 = Invoke-WebRequest -Uri $Uri2 -Method Head
-    # Installer
-    $this.CurrentState.Installer += [ordered]@{
-      Architecture = 'x64'
-      InstallerUrl = $Uri2
-    }
-    # Hash alternative
-    $this.CurrentState.HashA = $Object2.Headers.'X-COS-META-MD5'[0]
-    # Mode
-    $this.CurrentState.Mode = $true
-  } catch {
-    $this.Log("${Uri2} doesn't exist, fallback to ${Uri1}", 'Warning')
-    # Installer
-    $this.CurrentState.Installer += [ordered]@{
-      Architecture = 'x64'
-      InstallerUrl = $Uri1
-    }
-    # Mode
-    $this.CurrentState.Mode = $false
-  }
+  Read-Installer
 
   $this.Print()
   $this.Write()
   return
 }
 
+# Case 2: The hash is unchanged
 if ($this.CurrentState.Hash -eq $this.LastState.Hash) {
-  # Version
-  $this.CurrentState.Version = $this.LastState.Version
-  # If the alternative installer URL exists, don't fallback to the main one
-  if ($this.LastState.Mode -eq $true) {
-    # Installer
-    $this.CurrentState.Installer += [ordered]@{
-      Architecture = 'x64'
-      InstallerUrl = $Uri2 = $this.LastState.Installer[0].InstallerUrl
-    }
-    # Mode
-    $this.CurrentState.Mode = $true
+  $this.Log("The version $($this.LastState.Version) from the last state is the latest", 'Info')
+  return
+}
 
-    $Object2 = Invoke-WebRequest -Uri $Uri2 -Method Head
-    # Hash alternative
-    $this.CurrentState.HashA = $Object2.Headers.'X-COS-META-MD5'[0]
+Read-Installer
 
-    # Case 2: The main and the alternative hash are not updated
-    if ($this.CurrentState.MD5A -eq $this.LastState.MD5A) {
-      $this.Log("The version $($this.LastState.Version) from the last state is the latest", 'Info')
-      return
-    }
+# Case 3: The current state has an invalid version
+if ([string]::IsNullOrWhiteSpace($this.CurrentState.Version)) {
+  throw 'The current state has an invalid version'
+}
 
-    Get-ReleaseNotes
-
-    # Case 3: The main hash is not updated, but the alternative one has
-    $this.Log('The alternative hash has updated', 'Info')
+switch -Regex ($this.Check()) {
+  # Case 5: The hash and the version have changed
+  'Updated|Rollbacked' {
+    $this.Print()
+    $this.Write()
+    $this.Message()
+    $this.Submit()
+  }
+  # Case 4: The hash has changed, but the version is not
+  Default {
+    $this.Log('The hash has changed, but the version is not', 'Info')
     $this.Config.IgnorePRCheck = $true
     $this.Print()
     $this.Write()
     $this.Message()
     $this.Submit()
-    return
-  } else {
-    try {
-      $Uri2 = "https://dldir1v6.qq.com/weixin/Universal/Windows/WeChatWin$($this.CurrentState.Version).exe"
-      $Object2 = Invoke-WebRequest -Uri $Uri2 -Method Head
-      # Installer
-      $this.CurrentState.Installer += [ordered]@{
-        Architecture = 'x64'
-        InstallerUrl = $Uri2
-      }
-      # Hash alternative
-      $this.CurrentState.HashA = $Object2.Headers.'X-COS-META-MD5'[0]
-      # Mode
-      $this.CurrentState.Mode = $true
-
-      Get-ReleaseNotes
-
-      # Case 4: Detected an alternative installer URL
-      $this.Log('Detected an alternative installer URL', 'Info')
-      $this.Print()
-      $this.Write()
-      return
-    } catch {
-      # Case 5: The main MD5 was not updated, and the alternative installer URL doesn't exist
-      return
-    }
-  }
-} else {
-  $this.InstallerFiles[$Uri1] = $InstallerFile = Get-TempFile -Uri $Uri1
-  # Version
-  $this.CurrentState.Version = [regex]::Match((7z.exe l -ba -slt $InstallerFile), 'Path = (\d+\.\d+\.\d+\.\d+)').Groups[1].Value
-
-  try {
-    # The main hash has updated, and the alternative installer URL exists
-    $Uri2 = "https://dldir1v6.qq.com/weixin/Universal/Windows/WeChatWin$($this.CurrentState.Version).exe"
-    $Object2 = Invoke-WebRequest -Uri $Uri2 -Method Head
-    # Installer
-    $this.CurrentState.Installer += [ordered]@{
-      Architecture = 'x64'
-      InstallerUrl = $Uri2
-    }
-    # Hash alternative
-    $this.CurrentState.HashA = $Object2.Headers.'X-COS-META-MD5'[0]
-    # Mode
-    $this.CurrentState.Mode = $true
-  } catch {
-    # The main hash has updated, but the alternative installer URL does not exist
-    $this.Log("${Uri2} does not exist, fallback to ${Uri1}", 'Warning')
-    # Installer
-    $this.CurrentState.Installer += [ordered]@{
-      Architecture = 'x64'
-      InstallerUrl = $Uri1
-    }
-    # Mode
-    $this.CurrentState.Mode = $false
-  }
-
-  switch -Regex ($this.Check()) {
-    # Case 7: The installer URL has updated
-    'Changed|Updated|Rollbacked' {
-      $this.Print()
-      $this.Write()
-      $this.Message()
-    }
-    # Case 8: The hash and the version have updated
-    'Updated|Rollbacked' {
-      $this.Submit()
-    }
-    # Case 6: The hash has updated, but the version is not
-    Default {
-      $this.Log('The hash has updated, but the version is not', 'Info')
-      $this.Config.IgnorePRCheck = $true
-      $this.Print()
-      $this.Write()
-      $this.Message()
-      $this.Submit()
-    }
   }
 }
