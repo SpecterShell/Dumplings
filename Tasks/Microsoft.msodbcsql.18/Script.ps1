af34f4cf4a628c6d506d47bf67735285d18a90e1
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-03-21 11:53:53 +0800

    Microsoft.msodbcsql.18: Fix

--- Tasks/Microsoft.msodbcsql.18/Script.ps1
+++ Tasks/Microsoft.msodbcsql.18/Script.ps1
@@ -1,7 +1,7 @@
 $LocaleMap = [ordered]@{
   'en-US' = 0x409
   'de-DE' = 0x407
-  'es-ES' = 0xC0A
+  'es-ES' = 0x40A # 0xC0A
   'fr-FR' = 0x40C
   'it-IT' = 0x410
   'ja-JP' = 0x411
@@ -11,6 +11,11 @@ $LocaleMap = [ordered]@{
   'zh-CN' = 0x804
   'zh-TW' = 0x404
 }
+$ArchMap = [ordered]@{
+  'x86'   = 'x86'
+  'x64'   = 'x64'
+  'arm64' = 'ARM64'
+}
 
 $Object1 = Invoke-WebRequest -Uri 'https://learn.microsoft.com/en-us/sql/connect/odbc/windows/release-notes-odbc-sql-server-windows' | ConvertFrom-Html
 $Object2 = $Object1.SelectSingleNode('//div[contains(@class, "content")]/h2[starts-with(text(), "18.")][1]')
@@ -19,24 +24,14 @@ $Object2 = $Object1.SelectSingleNode('//div[contains(@class, "content")]/h2[star
 $this.CurrentState.Version = [regex]::Match($Object2.SelectSingleNode('./following::text()[contains(., "Version number:")]').InnerText, 'Version number:\s+(\d+(?:\.\d+)+)').Groups[1].Value
 
 # Installer
-$InstallerUrl = Get-RedirectedUrl -Uri $Object2.SelectSingleNode('./following::a[contains(@href, "linkid") and contains(text(), "x64")][1]').Attributes['href'].Value
-foreach ($Locale in $LocaleMap.Keys) {
-  foreach ($Arch in @('x86', 'x64', 'arm64')) {
-    $Installer = [ordered]@{}
-
-    if ($Locale -ne 'en-US') {
-      $Installer['InstallerLocale'] = $Locale
+foreach ($Arch in $ArchMap.Keys) {
+  $InstallerUrl = $Object2.SelectSingleNode("./following::a[contains(@href, 'linkid') and contains(text(), '$($ArchMap[$Arch])')][1]").Attributes['href'].Value
+  foreach ($Locale in $LocaleMap.Keys) {
+    $this.CurrentState.Installer += [ordered]@{
+      InstallerLocale = $Locale
+      Architecture    = $Arch
+      InstallerUrl    = "${InstallerUrl}&clcid=$($LocaleMap[$Locale])"
     }
-
-    $Installer['Architecture'] = $Arch
-
-    if ($Arch -eq 'x64') {
-      $Installer['InstallerUrl'] = $InstallerUrl -replace "$($LocaleMap.Values -join '|')", $LocaleMap[$Locale] -replace "$($LocaleMap.Keys -join '|')", $Locale
-    } else {
-      $Installer['InstallerUrl'] = $InstallerUrl -replace "$($LocaleMap.Values -join '|')", $LocaleMap[$Locale] -replace "$($LocaleMap.Keys -join '|')", $Locale -replace 'x64|amd64', $Arch
-    }
-
-    $this.CurrentState.Installer += $Installer
   }
 }
 
@@ -57,6 +52,11 @@ switch -Regex ($this.Check()) {
     $this.Message()
   }
   'Updated' {
+    # There are too many installers to check, so we defer it to the submit phase to save time
+    foreach ($Installer in $this.CurrentState.Installer) {
+      $Installer.InstallerUrl = Get-RedirectedUrl -Uri $Installer.InstallerUrl
+    }
+
     $this.Submit()
   }
 }
