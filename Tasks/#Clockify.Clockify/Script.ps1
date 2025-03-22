7ed9e812242646761a779220acfe8071fb219f59
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-03-22 17:08:54 +0800

    Add Clockify.Clockify

---
+++ Tasks/#Clockify.Clockify/Script.ps1
@@ -0,0 +1,58 @@
+$OldReleasesPath = Join-Path $PSScriptRoot 'Releases.yaml'
+if (Test-Path -Path $OldReleasesPath) {
+  $Global:DumplingsStorage['Clockify'] = $OldReleases = Get-Content -Path $OldReleasesPath -Raw | ConvertFrom-Yaml -Ordered
+} else {
+  $Global:DumplingsStorage['Clockify'] = $OldReleases = [ordered]@{}
+}
+
+$Object1 = Invoke-RestMethod -Uri 'https://clockify-resources.s3.eu-central-1.amazonaws.com/downloads/softwareUpdate/appCast.xml'
+
+# Version
+$this.CurrentState.Version = $Object1.enclosure.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.enclosure.url
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated|Rollbacked' {
+    # ReleaseTime
+    $this.CurrentState.ReleaseTime = $Object1.pubDate | Get-Date -AsUTC
+
+    # ReleaseNotes (en-US)
+    $this.CurrentState.Locale += [ordered]@{
+      Locale = 'en-US'
+      Key    = 'ReleaseNotes'
+      Value  = $ReleaseNotes = Invoke-WebRequest -Uri $Object1.releaseNotesLink | Read-ResponseContent | ConvertFrom-Html | Get-TextContent | Format-Text
+    }
+
+    $OldReleases[$this.CurrentState.Version] = [ordered]@{
+      ReleaseTime  = $this.CurrentState.ReleaseTime
+      ReleaseNotes = $ReleaseNotes
+    }
+    if ($Global:DumplingsPreference.Contains('EnableWrite') -and $Global:DumplingsPreference.EnableWrite) {
+      $OldReleases | ConvertTo-Yaml -OutFile $OldReleasesPath -Force
+    }
+  }
+  'New' {
+    $this.Print()
+    $this.Write()
+  }
+  { $_ -match 'Changed' -and $_ -notmatch 'Updated|Rollbacked' } {
+    $this.Print()
+    $this.Write()
+    $this.Message()
+  }
+  'Updated' {
+    $this.Print()
+    $this.Write()
+    if (-not $OldReleases.Contains($this.CurrentState.Version)) {
+      $this.Message()
+    }
+  }
+  { $_ -match 'Rollbacked' -and -not $OldReleases.Contains($this.CurrentState.Version) } {
+    $this.Print()
+    $this.Message()
+  }
+}
