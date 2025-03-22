2caf52d784db56c94c0bb4d8f950489a5db438b8
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-03-22 15:32:19 +0800

    Add Dedoose.Dedoose

---
+++ Tasks/#Dedoose.Dedoose/Script.ps1
@@ -0,0 +1,49 @@
+$OldReleasesPath = Join-Path $PSScriptRoot 'Releases.yaml'
+if (Test-Path -Path $OldReleasesPath) {
+  $Global:DumplingsStorage['Dedoose'] = $OldReleases = Get-Content -Path $OldReleasesPath -Raw | ConvertFrom-Yaml -Ordered
+} else {
+  $Global:DumplingsStorage['Dedoose'] = $OldReleases = [ordered]@{}
+}
+
+$Object1 = Invoke-RestMethod -Uri 'https://app.dedoose.com/_assets/xml/ClientVersions.xml'
+
+# Version
+$this.CurrentState.Version = $Object1.DedooseClientVersions.windows
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated|Rollbacked' {
+    # ReleaseNotes (en-US)
+    $this.CurrentState.Locale += [ordered]@{
+      Locale = 'en-US'
+      Key    = 'ReleaseNotes'
+      Value  = $ReleaseNotes = $Object1.DedooseClientVersions.ReleaseNotes.ReleaseNote.Where({ $_.Version -eq '10.0.23' }, 'First').'#text' | ConvertFrom-Html | Get-TextContent | Format-Text
+    }
+
+    $OldReleases[$this.CurrentState.Version] = [ordered]@{
+      ReleaseNotes = $ReleaseNotes
+    }
+    if ($Global:DumplingsPreference.Contains('EnableWrite') -and $Global:DumplingsPreference.EnableWrite) {
+      $OldReleases | ConvertTo-Yaml -OutFile $OldReleasesPath -Force
+    }
+  }
+  'New' {
+    $this.Print()
+    $this.Write()
+  }
+  { $_ -match 'Changed' -and $_ -notmatch 'Updated|Rollbacked' } {
+    $this.Print()
+    $this.Write()
+    $this.Message()
+  }
+  'Updated' {
+    $this.Print()
+    $this.Write()
+    if (-not $OldReleases.Contains($this.CurrentState.Version)) {
+      $this.Message()
+    }
+  }
+  { $_ -match 'Rollbacked' -and -not $OldReleases.Contains($this.CurrentState.Version) } {
+    $this.Print()
+    $this.Message()
+  }
+}
---
+++ Tasks/Dedoose.Dedoose/Script.ps1
@@ -0,0 +1,38 @@
+$Object1 = Invoke-WebRequest -Uri 'https://www.dedoose.com/resources/articledetail/dedoose-desktop-app'
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = 'https:' + $Object1.Links.Where({ try { $_.href.EndsWith('.exe') -and $_.href.Contains('Setup') } catch {} }, 'First')[0].href
+}
+
+# Version
+$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      if ($Global:DumplingsStorage.Contains('Dedoose') -and $Global:DumplingsStorage.Dedoose.Contains($this.CurrentState.Version)) {
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $Global:DumplingsStorage.Dedoose[$this.CurrentState.Version].ReleaseNotes
+        }
+      } else {
+        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
