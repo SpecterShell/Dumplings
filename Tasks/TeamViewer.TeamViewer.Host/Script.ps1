0a1cd142af6d556a40aacad9a692681fdc4309cf
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-03-24 17:43:20 +0800

    TeamViewer.TeamViewer.Host: Parse version from installer and reset

--- Tasks/TeamViewer.TeamViewer.Host/Script.ps1
+++ Tasks/TeamViewer.TeamViewer.Host/Script.ps1
@@ -1,42 +1,90 @@
-$Object1 = Invoke-WebRequest -Uri 'https://download.teamviewer.com/download/update/TVHVersion15.txt'
-# $Object1 = Invoke-WebRequest -Uri 'https://download.teamviewer.com/download/update/TVHMSIVersion15.txt'
-
-# Version
-$this.CurrentState.Version = $Object1.Content.Trim()
+function Read-Installer {
+  $this.InstallerFiles[$this.CurrentState.Installer[0].InstallerUrl] = $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+  # Version
+  $this.CurrentState.Version = ($InstallerFile | Read-FileVersionRawFromExe).ToString(3)
+  # Installer
+  $this.CurrentState.Installer += [ordered]@{
+    Architecture        = 'x86'
+    InstallerType       = 'zip'
+    NestedInstallerType = 'wix'
+    InstallerUrl        = "https://download.teamviewer.com/download/version_15x/update/Update_msi_Host_$($this.CurrentState.Version).zip"
+  }
+  $this.CurrentState.Installer += [ordered]@{
+    Architecture        = 'x64'
+    InstallerType       = 'zip'
+    NestedInstallerType = 'wix'
+    InstallerUrl        = "https://download.teamviewer.com/download/version_15x/update/Update_msi_Host_$($this.CurrentState.Version)_x64.zip"
+  }
+}
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
   Architecture  = 'x86'
   InstallerType = 'nullsoft'
-  InstallerUrl  = "https://download.teamviewer.com/download/version_$($this.CurrentState.Version.Split('.')[0])x/TeamViewer_Host_Setup.exe"
+  InstallerUrl  = 'https://download.teamviewer.com/download/version_15x/TeamViewer_Host_Setup.exe'
 }
 $this.CurrentState.Installer += [ordered]@{
   Architecture  = 'x64'
   InstallerType = 'nullsoft'
-  InstallerUrl  = "https://download.teamviewer.com/download/version_$($this.CurrentState.Version.Split('.')[0])x/TeamViewer_Host_Setup_x64.exe"
+  InstallerUrl  = 'https://download.teamviewer.com/download/version_15x/TeamViewer_Host_Setup_x64.exe'
 }
-$this.CurrentState.Installer += [ordered]@{
-  Architecture        = 'x86'
-  InstallerType       = 'zip'
-  NestedInstallerType = 'wix'
-  InstallerUrl        = "https://download.teamviewer.com/download/version_$($this.CurrentState.Version.Split('.')[0])x/update/Update_msi_Host_$($this.CurrentState.Version).zip"
+
+$Object1 = Invoke-WebRequest -Uri $this.CurrentState.Installer[0].InstallerUrl -Method Head
+# Hash
+$this.CurrentState.Hash = $Object1.Headers.'Content-MD5'[0]
+
+# Case 0: Force submit the manifest
+if ($Global:DumplingsPreference.Contains('Force')) {
+  $this.Log('Skip checking states', 'Info')
+
+  Read-Installer
+
+  $this.Print()
+  $this.Write()
+  $this.Message()
+  $this.Submit()
+  return
 }
-$this.CurrentState.Installer += [ordered]@{
-  Architecture        = 'x64'
-  InstallerType       = 'zip'
-  NestedInstallerType = 'wix'
-  InstallerUrl        = "https://download.teamviewer.com/download/version_$($this.CurrentState.Version.Split('.')[0])x/update/Update_msi_Host_$($this.CurrentState.Version)_x64.zip"
+
+# Case 1: The task is new
+if ($this.Status.Contains('New')) {
+  $this.Log('New task', 'Info')
+
+  Read-Installer
+
+  $this.Print()
+  $this.Write()
+  return
+}
+
+# Case 2: The hash is unchanged
+if ($this.CurrentState.Hash -eq $this.LastState.Hash) {
+  $this.Log("The version $($this.LastState.Version) from the last state is the latest", 'Info')
+  return
+}
+
+Read-Installer
+
+# Case 3: The current state has an invalid version
+if ([string]::IsNullOrWhiteSpace($this.CurrentState.Version)) {
+  throw 'The current state has an invalid version'
 }
 
 switch -Regex ($this.Check()) {
-  'New|Changed|Updated' {
+  # Case 5: The hash and the version have changed
+  'Updated|Rollbacked' {
     $this.Print()
     $this.Write()
-  }
-  'Changed|Updated' {
     $this.Message()
+    $this.Submit()
   }
-  'Updated' {
+  # Case 4: The hash has changed, but the version is not
+  Default {
+    $this.Log('The hash has changed, but the version is not', 'Info')
+    $this.Config.IgnorePRCheck = $true
+    $this.Print()
+    $this.Write()
+    $this.Message()
     $this.Submit()
   }
 }
