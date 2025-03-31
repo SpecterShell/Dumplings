cbb3815c8111af3dd82dea45bbcf59e6e942eb54
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-03-31 18:20:15 +0800

    GnuCash.GnuCash: Remove hashes from release notes

--- Tasks/GnuCash.GnuCash/Script.ps1
+++ Tasks/GnuCash.GnuCash/Script.ps1
@@ -19,7 +19,7 @@ switch -Regex ($this.Check()) {
 
       if (-not [string]::IsNullOrWhiteSpace($Object1.body)) {
         $ReleaseNotesObject = $Object1.body | Convert-MarkdownToHtml -Extensions 'advanced', 'emojis', 'hardlinebreak'
-        $ReleaseNotesNodes = for ($Node = $ReleaseNotesObject.ChildNodes[0]; $Node -and -not ($Node.Name -eq 'h3' -and $Node.InnerText.Contains('Getting GnuCash')); $Node = $Node.NextSibling) { $Node }
+        $ReleaseNotesNodes = for ($Node = $ReleaseNotesObject.ChildNodes[0]; $Node -and -not $Node.InnerText.Contains('Getting GnuCash for'); $Node = $Node.NextSibling) { $Node }
         # ReleaseNotes (en-US)
         $this.CurrentState.Locale += [ordered]@{
           Locale = 'en-US'
