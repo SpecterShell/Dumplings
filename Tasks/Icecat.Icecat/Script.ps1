ca53a4222fd64e8d6cc3965158047b553141317b
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-04-02 11:44:15 +0800

    Icecat.Icecat: Build InstallerUrl from template

--- Tasks/Icecat.Icecat/Script.ps1
+++ Tasks/Icecat.Icecat/Script.ps1
@@ -6,12 +6,12 @@ $this.CurrentState.Version = [regex]::Match($Object1.Content, 'Icecat Version:(\
 # Installer
 $this.CurrentState.Installer += [ordered]@{
   Architecture = 'x64'
-  InstallerUrl = Join-Uri 'https://icecatbrowser.org/' $Object1.Links.Where({ try { $_.href.EndsWith('.exe') -and $_.href.Contains('installer') -and -not $_.href.Contains('aarch64') } catch {} }, 'First')[0].href
+  InstallerUrl = "https://icecatbrowser.org/assets/icecat/$($this.CurrentState.Version)/icecat-$($this.CurrentState.Version).en-US.win64.installer.exe"
   ProductCode  = "IceCat $($this.CurrentState.Version) (x64 en-US)"
 }
 $this.CurrentState.Installer += [ordered]@{
   Architecture = 'arm64'
-  InstallerUrl = Join-Uri 'https://icecatbrowser.org/' $Object1.Links.Where({ try { $_.href.EndsWith('.exe') -and $_.href.Contains('installer') -and $_.href.Contains('aarch64') } catch {} }, 'First')[0].href
+  InstallerUrl = "https://icecatbrowser.org/assets/icecat/$($this.CurrentState.Version)/icecat-$($this.CurrentState.Version).en-US.win64-aarch64.installer.exe"
   ProductCode  = "IceCat $($this.CurrentState.Version) (AArch64 en-US)"
 }
 
