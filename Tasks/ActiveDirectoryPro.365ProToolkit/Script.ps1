2d737c5cc7197432e6ef823decad63c2aa902b36
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-04-04 12:19:55 +0800

    ActiveDirectoryPro.365ProToolkit: Add ProductCode

--- Tasks/ActiveDirectoryPro.365ProToolkit/Script.ps1
+++ Tasks/ActiveDirectoryPro.365ProToolkit/Script.ps1
@@ -7,7 +7,9 @@ function Read-Installer {
   $this.CurrentState.Version = $InstallerFile2 | Read-ProductVersionFromMsi
   # InstallerSha256
   $this.CurrentState.Installer[0]['InstallerSha256'] = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
-  # AppsAndFeaturesEntries + ProductCode
+  # ProductCode
+  $this.CurrentState.Installer[0]['ProductCode'] = $InstallerFile2 | Read-ProductCodeFromMsi
+  # AppsAndFeaturesEntries
   $this.CurrentState.Installer[0]['AppsAndFeaturesEntries'] = @(
     [ordered]@{
       UpgradeCode   = $InstallerFile2 | Read-UpgradeCodeFromMsi
