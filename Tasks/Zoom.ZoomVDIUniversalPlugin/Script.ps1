9858c6ea7d8ccc77503d65d8e3c2b94624f44587
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-04-05 01:17:16 +0800

    Zoom.ZoomVDIUniversalPlugin: Update base version to 6.3, remove x86 installer and reset

--- Tasks/Zoom.ZoomVDIUniversalPlugin/Script.ps1
+++ Tasks/Zoom.ZoomVDIUniversalPlugin/Script.ps1
@@ -3,7 +3,7 @@ $Object1 = Invoke-WebRequest -Uri 'https://zoom.us/product/version' -UserAgent '
   platform    = 'Universal'
   os          = 'Win32'
   pluginOS    = 'win'
-  cv          = $this.Status.Contains('New') ? '6.1.10' : ($this.LastState.Version.Split('.')[0..2] -join '.')
+  cv          = $this.Status.Contains('New') ? '6.3.10' : ($this.LastState.Version.Split('.')[0..2] -join '.')
 } | ConvertFrom-ProtoBuf
 $Object2 = $Object1['12'] | ConvertFrom-Json
 
@@ -14,10 +14,10 @@ $this.CurrentState.Version = [regex]::Match($Object2.x64.url, '(\d+\.\d+\.\d+\.\
 $this.CurrentState.RealVersion = $this.CurrentState.Version.Split('.')[@(0, 1, 3)] -join '.'
 
 # Installer
-$this.CurrentState.Installer += [ordered]@{
-  Architecture = 'x86'
-  InstallerUrl = Join-Uri 'https://zoom.us/' $Object2.url
-}
+# $this.CurrentState.Installer += [ordered]@{
+#   Architecture = 'x86'
+#   InstallerUrl = Join-Uri 'https://zoom.us/' $Object2.url
+# }
 $this.CurrentState.Installer += [ordered]@{
   Architecture = 'x64'
   InstallerUrl = Join-Uri 'https://zoom.us/' $Object2.x64.url
