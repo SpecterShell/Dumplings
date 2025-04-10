c969f2cb9e833c80d65fc6ad2e4f784de512effe
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-04-10 17:19:17 +0800

    NextChat.NextChat: Update repo name

--- Tasks/NextChat.NextChat/Script.ps1
+++ Tasks/NextChat.NextChat/Script.ps1
@@ -1,5 +1,5 @@
 $RepoOwner = 'ChatGPTNextWeb'
-$RepoName = 'ChatGPT-Next-Web'
+$RepoName = 'NextChat'
 
 $Object1 = Invoke-GitHubApi -Uri "https://api.github.com/repos/${RepoOwner}/${RepoName}/releases/latest"
 
