58d22b4b7d5defe1f2f4bca8e0a16da903ca42bb
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-04-10 18:49:54 +0800

    ZWSOFT.NetworkLicenseManager: Update

--- Tasks/ZWSOFT.NetworkLicenseManager/Script.ps1
+++ Tasks/ZWSOFT.NetworkLicenseManager/Script.ps1
@@ -1,15 +1,17 @@
 $Object1 = $Global:DumplingsStorage.ZWSOFTApps.data.Where({ $_.title -eq '网络服务套件' }, 'First')[0]
+$Object2 = Invoke-WebRequest -Uri $Object1.download.Where({ $_.name.Contains('32位') }, 'First')[0].url
+$Object3 = Invoke-WebRequest -Uri $Object1.download.Where({ $_.name.Contains('64位') }, 'First')[0].url
 
 # Installer
 $this.CurrentState.Installer += $InstallerX86 = [ordered]@{
   Architecture = 'x86'
-  InstallerUrl = ($Object1.download.Where({ $_.name.Contains('32位') }, 'First')[0].url.Split('/') | Sort-Object -Property { $_.Length } -Bottom 1 | ConvertFrom-Base64) -replace '^\d+' -replace 'https?://dl\.zwsoft\.cn', 'https://upgrade-online.zwsoft.cn'
+  InstallerUrl = Join-Uri 'https://upgrade-online.zwsoft.cn' ([regex]::Match($Object2.Content, 'https://download\.zwsoft\.cn/\d+/[a-zA-Z0-9]+([^"]+)').Groups[1].Value)
 }
 $VersionX86 = [regex]::Match($InstallerX86.InstallerUrl, '_(\d+(?:\.\d+)+)[_.]').Groups[1].Value
 
 $this.CurrentState.Installer += $InstallerX64 = [ordered]@{
   Architecture = 'x64'
-  InstallerUrl = ($Object1.download.Where({ $_.name.Contains('64位') }, 'First')[0].url.Split('/') | Sort-Object -Property { $_.Length } -Bottom 1 | ConvertFrom-Base64) -replace '^\d+' -replace 'https?://dl\.zwsoft\.cn', 'https://upgrade-online.zwsoft.cn'
+  InstallerUrl = Join-Uri 'https://upgrade-online.zwsoft.cn' ([regex]::Match($Object3.Content, 'https://download\.zwsoft\.cn/\d+/[a-zA-Z0-9]+([^"]+)').Groups[1].Value)
 }
 $VersionX64 = [regex]::Match($InstallerX64.InstallerUrl, '_(\d+(?:\.\d+)+)[_.]').Groups[1].Value
 
