4a39855dacaaa47f747e86203b8a8d48a81c45ee
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-04-11 20:41:04 +0800

    Add BrinkSoftware

---
+++ Tasks/BrinkSoftware.IbisVoorInfra/Script.ps1
@@ -0,0 +1,39 @@
+$Object1 = Invoke-WebRequest -Uri 'https://www.ibis.nl/downloads-0'
+$InstallerLink = $Object1.Links.Where({ try { $_.href.EndsWith('.exe') -and $_.OuterHTML.Contains('Ibis voor Infra') } catch {} }, 'First')[0]
+
+# Version
+$this.CurrentState.Version = [regex]::Match($InstallerLink.OuterHtml, '(\d+(?:\.\d+){2,})').Groups[1].Value
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $InstallerLink.href
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    foreach ($Installer in $this.CurrentState.Installer) {
+      $this.InstallerFiles[$Installer.InstallerUrl] = $InstallerFile = Get-TempFile -Uri $Installer.InstallerUrl
+      $InstallerFileExtracted = $InstallerFile | Expand-InstallShield
+      $InstallerFile2 = Join-Path $InstallerFileExtracted 'Ibis voor Infra.msi'
+      # ProductCode
+      $Installer['ProductCode'] = $InstallerFile2 | Read-ProductCodeFromMsi
+      # AppsAndFeaturesEntries
+      $Installer['AppsAndFeaturesEntries'] = @(
+        [ordered]@{
+          UpgradeCode   = $InstallerFile2 | Read-UpgradeCodeFromMsi
+          InstallerType = 'msi'
+        }
+      )
+      Remove-Item -Path $InstallerFileExtracted -Recurse -Force -ErrorAction 'Continue' -ProgressAction 'SilentlyContinue'
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
