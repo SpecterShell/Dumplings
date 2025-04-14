07980040447ad966a4e5d4eed277952dbc14a057
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-04-14 10:44:44 +0800

    Add ChefSoftware.Inspec

---
+++ Tasks/ChefSoftware.Inspec/Script.ps1
@@ -0,0 +1,51 @@
+$Object1 = Invoke-RestMethod -Uri 'https://omnitruck.chef.io/stable/inspec/metadata/?p=windows&pv=10.0.22000&v=latest&m=x86_64&prerelease=false&nightlies=false' -Headers @{
+  Accept = 'application/json'
+}
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.url
+}
+
+# Version
+$this.CurrentState.Version = [regex]::Match($Object1.url, '(\d+(?:\.\d+)+-\d+)').Groups[1].Value.Replace('-', '.')
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      $ReleaseNotesRaw = Invoke-RestMethod -Uri "https://packages.chef.io/release-notes/inspec/$($Object1.version).md" | Convert-MarkdownToHtml | Get-TextContent
+      if ($ReleaseNotesRaw -match 'Release Date: (\d{1,2}\W+[a-zA-Z]+\W+20\d{2})\.') {
+        # ReleaseTime
+        $this.CurrentState.ReleaseTime = $Matches[1] | Get-Date -Format 'yyyy-MM-dd'
+
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesRaw -replace 'Release Date: (\d{1,2}\W+[a-zA-Z]+\W+20\d{2})\.' | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseTime for version $($this.CurrentState.Version)", 'Warning')
+
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesRaw | Format-Text
+        }
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
