74b108bd762880ff70f9c9ac4f51c7ed85b2480f
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-04-15 20:05:27 +0800

    Add secrypt.digiSealreader

---
+++ Tasks/secrypt.digiSealreader/Script.ps1
@@ -0,0 +1,138 @@
+function Read-Installer {
+  $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+  # Version
+  $this.CurrentState.Version = $InstallerFile | Read-ProductVersionFromExe
+  # InstallerSha256
+  $this.CurrentState.Installer[0]['InstallerSha256'] = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
+  Remove-Item -Path $InstallerFile -Recurse -Force -ErrorAction 'Continue' -ProgressAction 'SilentlyContinue'
+}
+
+function Get-ReleaseNotes {
+  try {
+    $Object2 = Invoke-WebRequest -Uri 'http://www.secrypt.de/downloads/1hd82znzdnfabwms/secrypt/digiSeal_office/update_info/digiSeal_reader_update_info.html' | ConvertFrom-Html
+    $Object3 = $Object2.SelectSingleNode('/html/body').InnerText | ConvertFrom-Base64 | ConvertFrom-Xml
+
+    $Object4 = [System.IO.StringReader]::new($Object3.'Update-Info'.Component.Usermsg.language.Where({ $_.name -eq 'EN' }, 'First')[0].message.'#text')
+
+    while ($Object4.Peek() -ne -1) {
+      $String = $Object4.ReadLine()
+      if ($String.StartsWith("v.$($this.CurrentState.Version)")) {
+        if ($String -match '(20\d{6})') {
+          # ReleaseTime
+          $this.CurrentState.ReleaseTime = [datetime]::ParseExact($Matches[1], 'yyyyMMdd', $null).ToString('yyyy-MM-dd')
+        } else {
+          $this.Log("No ReleaseTime for version $($this.CurrentState.Version)", 'Warning')
+        }
+        break
+      }
+    }
+    if ($Object4.Peek() -ne -1) {
+      $ReleaseNotesObjects = [System.Collections.Generic.List[string]]::new()
+      while ($Object4.Peek() -ne -1) {
+        $String = $Object4.ReadLine()
+        if ($String -match '^_+$') { break }
+        $ReleaseNotesObjects.Add($String)
+      }
+      # ReleaseNotes (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotes'
+        Value  = $ReleaseNotesObjects | Format-Text
+      }
+    } else {
+      $this.Log("No ReleaseTime and ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+    }
+
+    $Object4.Close()
+  } catch {
+    $_ | Out-Host
+    $this.Log($_, 'Warning')
+  }
+}
+
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = 'https://www.secrypt.de/downloads/setup_digiSeal_reader.exe'
+}
+
+$Object1 = Invoke-WebRequest -Uri $this.CurrentState.Installer[0].InstallerUrl -Method Head
+$ETag = $Object1.Headers.ETag[0]
+
+# Case 0: Force submit the manifest
+if ($Global:DumplingsPreference.Contains('Force')) {
+  $this.Log('Skip checking states', 'Info')
+
+  # ETag
+  $this.CurrentState.ETag = @($ETag)
+
+  Read-Installer
+  Get-ReleaseNotes
+
+  $this.Print()
+  $this.Write()
+  $this.Message()
+  $this.Submit()
+  return
+}
+
+# Case 1: The task is new
+if ($this.Status.Contains('New')) {
+  $this.Log('New task', 'Info')
+
+  # ETag
+  $this.CurrentState.ETag = @($ETag)
+
+  Read-Installer
+  Get-ReleaseNotes
+
+  $this.Print()
+  $this.Write()
+  return
+}
+
+# Case 2: The ETag is unchanged
+if ($ETag -in $this.LastState.ETag) {
+  $this.Log("The version $($this.LastState.Version) from the last state is the latest (Global)", 'Info')
+  return
+}
+
+Read-Installer
+
+# Case 3: The current state has an invalid version
+if ([string]::IsNullOrWhiteSpace($this.CurrentState.Version)) {
+  throw 'The current state has an invalid version'
+}
+
+Get-ReleaseNotes
+
+# Case 4: The ETag has changed, but the SHA256 is not
+if ($this.CurrentState.Installer[0].InstallerSha256 -eq $this.LastState.Installer[0].InstallerSha256) {
+  $this.Log('The ETag has changed, but the SHA256 is not', 'Info')
+
+  # ETag
+  $this.CurrentState.ETag = $this.LastState.ETag + $ETag
+
+  $this.Write()
+  return
+}
+
+# ETag
+$this.CurrentState.ETag = @($ETag)
+
+switch -Regex ($this.Check()) {
+  # Case 6: The ETag, the SHA256 and the version have changed
+  'Updated|Rollbacked' {
+    $this.Print()
+    $this.Write()
+    $this.Message()
+    $this.Submit()
+  }
+  # Case 5: The ETag and the SHA256 have changed, but the version is not
+  Default {
+    $this.Log('The ETag and the SHA256 have changed, but the version is not', 'Info')
+    $this.Config.IgnorePRCheck = $true
+    $this.Print()
+    $this.Write()
+    $this.Message()
+    $this.Submit()
+  }
+}
