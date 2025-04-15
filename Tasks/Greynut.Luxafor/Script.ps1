1ba4f73cdc815318dfd8179722e6fa31cdc163af
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-04-15 20:55:43 +0800

    Greynut.Luxafor: Remove download source

--- Tasks/Greynut.Luxafor/Script.ps1
+++ Tasks/Greynut.Luxafor/Script.ps1
@@ -1,12 +1,12 @@
-$Object1 = Invoke-WebRequest -Uri 'https://www.luxaformanual.com/'
+$Object1 = Invoke-RestMethod -Uri 'https://api.luxafor.com/versioning/api/luxafor_blz159is6sp00pugtpqi0fvq/latest'
+
+# Version
+$this.CurrentState.Version = $Object1.version
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = $Object1.Links.Where({ try { $_.href.EndsWith('.msi') -and $_.href.Contains('LuxaforApp') } catch {} }, 'First')[0].href
+  InstallerUrl = "https://api.luxafor.com/versioning/download/luxafor_blz159is6sp00pugtpqi0fvq/$($this.CurrentState.Version)/LuxaforApp-Installer-$($this.CurrentState.Version).msi"
 }
-
-# Version
-$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
@@ -17,20 +17,6 @@ switch -Regex ($this.Check()) {
     $this.Message()
   }
   'Updated' {
-    $ToSubmit = $false
-
-    $Mutex = [System.Threading.Mutex]::new($false, 'DumplingsSubmitLockLuxafor')
-    $Mutex.WaitOne(30000) | Out-Null
-    if (-not $Global:DumplingsStorage.Contains("Luxafor-$($this.CurrentState.Version)-ToSubmit")) {
-      $Global:DumplingsStorage["Luxafor-$($this.CurrentState.Version)-ToSubmit"] = $ToSubmit = $true
-    }
-    $Mutex.ReleaseMutex()
-    $Mutex.Dispose()
-
-    if ($ToSubmit) {
-      $this.Submit()
-    } else {
-      $this.Log('Another task is submitting manifests for this package', 'Warning')
-    }
+    $this.Submit()
   }
 }
