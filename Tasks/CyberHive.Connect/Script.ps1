51c78253da128f9303d12247a81ba0708a206ad4
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-04-16 10:57:58 +0800

    CyberHive.Connect: Get installer from release notes page

--- Tasks/CyberHive.Connect/Script.ps1
+++ Tasks/CyberHive.Connect/Script.ps1
@@ -1,32 +1,46 @@
-$Object1 = Invoke-RestMethod -Uri 'https://pkgs.cyberhive.com/windows/connect-win.xml'
-
-# Version
-$this.CurrentState.Version = $Object1.version
-
-$Object2 = Invoke-WebRequest -Uri $Object1.link
+$Object1 = Invoke-WebRequest -Uri 'https://docs.cyberhive.com/connect/release-notes/'
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = $Object2.Links.Where({ try { $_.href.EndsWith('.exe') -and $_.href.Contains($this.CurrentState.Version) } catch {} }, 'First')[0].href
+  InstallerUrl = $Object1.Links.Where({ try { $_.href.EndsWith('.exe') } catch {} }, 'First')[0].href
 }
+
+# Version
+$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
     try {
-      # ReleaseTime
-      $this.CurrentState.ReleaseTime = $Object1.pubDate | Get-Date -AsUTC
+      $Object2 = $Object1 | ConvertFrom-Html
 
-      # ReleaseNotes
-      $this.CurrentState.Locale += [ordered]@{
-        Locale = 'en-US'
-        Key    = 'ReleaseNotes'
-        Value  = $Object1.description.'#cdata-section' | ConvertFrom-Html | Get-TextContent | Format-Text
-      }
-
-      # ReleaseNotesUrl
-      $this.CurrentState.Locale += [ordered]@{
-        Key   = 'ReleaseNotesUrl'
-        Value = $Object1.link
+      $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//h2[contains(text(), '$($this.CurrentState.Version)')]")
+      if ($ReleaseNotesTitleNode) {
+        $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node -and $Node.Name -ne 'h2'; $Node = $Node.NextSibling) {
+          if ($Node.InnerText -match '(\d{1,2}[a-zA-Z]+\W+[a-zA-Z]+\W+20\d{2})') {
+            # ReleaseTime
+            $this.CurrentState.ReleaseTime = [datetime]::ParseExact(
+              $Matches[1],
+              [string[]]@(
+                "d'st' MMM yyyy", "d'st' MMMM yyyy",
+                "d'nd' MMM yyyy", "d'nd' MMMM yyyy",
+                "d'rd' MMM yyyy", "d'rd' MMMM yyyy",
+                "d'th' MMM yyyy", "d'th' MMMM yyyy"
+              ),
+              (Get-Culture -Name 'en-US'),
+              [System.Globalization.DateTimeStyles]::None
+            ).ToString('yyyy-MM-dd')
+          } else {
+            $Node
+          }
+        }
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseTime and ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
       }
     } catch {
       $_ | Out-Host
