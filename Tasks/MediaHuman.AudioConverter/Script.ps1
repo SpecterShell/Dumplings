e9358c4dd7fc58c5301ce566804acabde8b020a9
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-04-18 18:09:48 +0800

    MediaHuman: Read version from installers

--- Tasks/MediaHuman.AudioConverter/Script.ps1
+++ Tasks/MediaHuman.AudioConverter/Script.ps1
@@ -1,45 +1,126 @@
-$Object1 = [regex]::Match(
-  (Invoke-WebRequest -Uri 'https://www.mediahuman.com/files/AudioConverter.xml').Content,
-  '(?s)(<VersionInfo>.+?</VersionInfo>)'
-).Groups[1].Value | ConvertFrom-Xml
-
-# Version
-$this.CurrentState.Version = $Object1.VersionInfo.current
-
-# Installer
-$this.CurrentState.Installer += [ordered]@{
-  Architecture = 'x86'
-  InstallerUrl = $Object1.VersionInfo.url.win.'#text' | ConvertTo-Https
-}
-$this.CurrentState.Installer += [ordered]@{
-  Architecture = 'x64'
-  InstallerUrl = $Object1.VersionInfo.url.win64.'#text' | ConvertTo-Https
+function Read-Installer {
+  $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+  # Version
+  $this.CurrentState.Version = $InstallerFile | Read-ProductVersionFromExe
+  # InstallerSha256
+  $this.CurrentState.Installer[0]['InstallerSha256'] = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
+  Remove-Item -Path $InstallerFile -Recurse -Force -ErrorAction 'Continue' -ProgressAction 'SilentlyContinue'
 }
 
-switch -Regex ($this.Check()) {
-  'New|Changed|Updated' {
-    try {
-      # ReleaseTime
-      $this.CurrentState.ReleaseTime = [datetime]::ParseExact($Object1.VersionInfo.date, 'dd/MM/yyyy', $null).ToString('yyyy-MM-dd')
+function Get-ReleaseNotes {
+  try {
+    $Object2 = Invoke-WebRequest -Uri 'https://www.mediahuman.com/audio-converter/changelog.html' | ConvertFrom-Html
 
+    $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//h4[contains(text(), 'Version $($this.CurrentState.Version)')]")
+    if ($ReleaseNotesTitleNode) {
+      $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node -and $Node.Name -ne 'h4'; $Node = $Node.NextSibling) {
+        if ($Node.InnerText -match '([a-zA-Z]+\W+\d{1,2}\W+20\d{2})') {
+          # ReleaseTime
+          $this.CurrentState.ReleaseTime = $Matches[1] | Get-Date -Format 'yyyy-MM-dd'
+        } else {
+          $Node
+        }
+      }
       # ReleaseNotes (en-US)
       $this.CurrentState.Locale += [ordered]@{
         Locale = 'en-US'
         Key    = 'ReleaseNotes'
-        Value  = $Object1.VersionInfo.changes | Format-Text
+        Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
       }
-    } catch {
-      $_ | Out-Host
-      $this.Log($_, 'Warning')
+    } else {
+      $this.Log("No ReleaseTime and ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
     }
+  } catch {
+    $_ | Out-Host
+    $this.Log($_, 'Warning')
+  }
+}
 
+# Installer
+$this.CurrentState.Installer += $this.CurrentState.Installer[0] = [ordered]@{
+  Architecture = 'x86'
+  InstallerUrl = 'https://www.mediahuman.com/files/MHAudioConverter.exe'
+}
+$Object1 = Invoke-WebRequest -Uri $this.CurrentState.Installer[0].InstallerUrl -Method Head
+# Last Modified
+$this.CurrentState.LastModified = $Object1.Headers.'Last-Modified'[0]
+
+$this.CurrentState.Installer += $InstallerX64 = [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = 'https://www.mediahuman.com/files/MHAudioConverter-x64.exe'
+}
+$Object2 = Invoke-WebRequest -Uri $InstallerX64.InstallerUrl -Method Head
+# Last Modified
+$this.CurrentState.LastModifiedX64 = $Object2.Headers.'Last-Modified'[0]
+
+# Case 0: Force submit the manifest
+if ($Global:DumplingsPreference.Contains('Force')) {
+  $this.Log('Skip checking states', 'Info')
+
+  Read-Installer
+  Get-ReleaseNotes
+
+  $this.Print()
+  $this.Write()
+  $this.Message()
+  $this.Submit()
+  return
+}
+
+# Case 1: The task is new
+if ($this.Status.Contains('New')) {
+  $this.Log('New task', 'Info')
+
+  Read-Installer
+  Get-ReleaseNotes
+
+  $this.Print()
+  $this.Write()
+  return
+}
+
+# Case 2: The Last Modified is unchanged
+if ([datetime]$this.CurrentState.LastModified -le [datetime]$this.LastState.LastModified) {
+  $this.Log("The version $($this.LastState.Version) from the last state is the latest (WiX)", 'Info')
+  return
+}
+if ([datetime]$this.CurrentState.LastModifiedX64 -le [datetime]$this.LastState.LastModifiedX64) {
+  $this.Log("The version $($this.LastState.Version) from the last state is the latest (EXE)", 'Info')
+  return
+}
+
+Read-Installer
+
+# Case 3: The current state has an invalid version
+if ([string]::IsNullOrWhiteSpace($this.CurrentState.Version)) {
+  throw 'The current state has an invalid version'
+}
+
+Get-ReleaseNotes
+
+# Case 4: The Last Modified has changed, but the SHA256 is not
+if ($this.CurrentState.Installer[0].InstallerSha256 -eq $this.LastState.Installer[0].InstallerSha256) {
+  $this.Log('The Last Modified has changed, but the SHA256 is not', 'Info')
+
+  $this.Write()
+  return
+}
+
+switch -Regex ($this.Check()) {
+  # Case 6: The Last Modified, the SHA256 and the version have changed
+  'Updated|Rollbacked' {
     $this.Print()
     $this.Write()
-  }
-  'Changed|Updated' {
     $this.Message()
+    $this.Submit()
   }
-  'Updated' {
+  # Case 5: The Last Modified and the SHA256 have changed, but the version is not
+  Default {
+    $this.Log('The Last Modified and the SHA256 have changed, but the version is not', 'Info')
+    $this.Config.IgnorePRCheck = $true
+    $this.Print()
+    $this.Write()
+    $this.Message()
     $this.Submit()
   }
 }
--- Tasks/MediaHuman.LyricsFinder/Script.ps1
+++ Tasks/MediaHuman.LyricsFinder/Script.ps1
@@ -1,41 +1,113 @@
-$Object1 = @"
-<VersionInfos>
-$((Invoke-WebRequest -Uri 'https://www.mediahuman.com/files/LyricsFinder.xml').Content)
-</VersionInfos>
-"@ | ConvertFrom-Xml
-
-# Version
-$this.CurrentState.Version = $Object1.VersionInfos.VersionInfo[0].current
-
-# Installer
-$this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = $Object1.VersionInfos.VersionInfo[0].url.win[0].'#text'
+function Read-Installer {
+  $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+  # Version
+  $this.CurrentState.Version = $InstallerFile | Read-ProductVersionFromExe
+  # InstallerSha256
+  $this.CurrentState.Installer[0]['InstallerSha256'] = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
+  Remove-Item -Path $InstallerFile -Recurse -Force -ErrorAction 'Continue' -ProgressAction 'SilentlyContinue'
 }
 
-switch -Regex ($this.Check()) {
-  'New|Changed|Updated' {
-    try {
-      # ReleaseTime
-      $this.CurrentState.ReleaseTime = [datetime]::ParseExact($Object1.VersionInfos.VersionInfo[0].date, 'dd/MM/yyyy', $null).ToString('yyyy-MM-dd')
+function Get-ReleaseNotes {
+  try {
+    $Object2 = Invoke-WebRequest -Uri 'https://www.mediahuman.com/lyrics-finder/changelog.html' | ConvertFrom-Html
 
+    $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//h4[contains(text(), 'Version $($this.CurrentState.Version)')]")
+    if ($ReleaseNotesTitleNode) {
+      $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node -and $Node.Name -ne 'h4'; $Node = $Node.NextSibling) {
+        if ($Node.InnerText -match '([a-zA-Z]+\W+\d{1,2}\W+20\d{2})') {
+          # ReleaseTime
+          $this.CurrentState.ReleaseTime = $Matches[1] | Get-Date -Format 'yyyy-MM-dd'
+        } else {
+          $Node
+        }
+      }
       # ReleaseNotes (en-US)
       $this.CurrentState.Locale += [ordered]@{
         Locale = 'en-US'
         Key    = 'ReleaseNotes'
-        Value  = $Object1.VersionInfos.VersionInfo[0].changes | Format-Text
+        Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
       }
-    } catch {
-      $_ | Out-Host
-      $this.Log($_, 'Warning')
+    } else {
+      $this.Log("No ReleaseTime and ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
     }
+  } catch {
+    $_ | Out-Host
+    $this.Log($_, 'Warning')
+  }
+}
 
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = 'https://www.mediahuman.com/files/LyricsFinder.exe'
+}
+$Object1 = Invoke-WebRequest -Uri $this.CurrentState.Installer[0].InstallerUrl -Method Head
+# Last Modified
+$this.CurrentState.LastModified = $Object1.Headers.'Last-Modified'[0]
+
+# Case 0: Force submit the manifest
+if ($Global:DumplingsPreference.Contains('Force')) {
+  $this.Log('Skip checking states', 'Info')
+
+  Read-Installer
+  Get-ReleaseNotes
+
+  $this.Print()
+  $this.Write()
+  $this.Message()
+  $this.Submit()
+  return
+}
+
+# Case 1: The task is new
+if ($this.Status.Contains('New')) {
+  $this.Log('New task', 'Info')
+
+  Read-Installer
+  Get-ReleaseNotes
+
+  $this.Print()
+  $this.Write()
+  return
+}
+
+# Case 2: The Last Modified is unchanged
+if ([datetime]$this.CurrentState.LastModified -le [datetime]$this.LastState.LastModified) {
+  $this.Log("The version $($this.LastState.Version) from the last state is the latest (WiX)", 'Info')
+  return
+}
+
+Read-Installer
+
+# Case 3: The current state has an invalid version
+if ([string]::IsNullOrWhiteSpace($this.CurrentState.Version)) {
+  throw 'The current state has an invalid version'
+}
+
+Get-ReleaseNotes
+
+# Case 4: The Last Modified has changed, but the SHA256 is not
+if ($this.CurrentState.Installer[0].InstallerSha256 -eq $this.LastState.Installer[0].InstallerSha256) {
+  $this.Log('The Last Modified has changed, but the SHA256 is not', 'Info')
+
+  $this.Write()
+  return
+}
+
+switch -Regex ($this.Check()) {
+  # Case 6: The Last Modified, the SHA256 and the version have changed
+  'Updated|Rollbacked' {
     $this.Print()
     $this.Write()
-  }
-  'Changed|Updated' {
     $this.Message()
+    $this.Submit()
   }
-  'Updated' {
+  # Case 5: The Last Modified and the SHA256 have changed, but the version is not
+  Default {
+    $this.Log('The Last Modified and the SHA256 have changed, but the version is not', 'Info')
+    $this.Config.IgnorePRCheck = $true
+    $this.Print()
+    $this.Write()
+    $this.Message()
     $this.Submit()
   }
 }
