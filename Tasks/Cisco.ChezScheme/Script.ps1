5c9e875b9a929c23989d55a8fad1e748ed80be12
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-04-19 16:52:29 +0800

    Add Cisco.ChezScheme

---
+++ Tasks/Cisco.ChezScheme/Script.ps1
@@ -0,0 +1,39 @@
+$RepoOwner = 'cisco'
+$RepoName = 'ChezScheme'
+
+$Object1 = Invoke-GitHubApi -Uri "https://api.github.com/repos/${RepoOwner}/${RepoName}/releases/latest"
+
+# Version
+$this.CurrentState.Version = $Object1.tag_name -creplace '^v'
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.assets.Where({ $_.name.EndsWith('.exe') }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.published_at.ToUniversalTime()
+
+      # ReleaseNotesUrl
+      $this.CurrentState.Locale += [ordered]@{
+        Key   = 'ReleaseNotesUrl'
+        Value = "https://cisco.github.io/ChezScheme/release_notes/v$($this.CurrentState.Version)/release_notes.html"
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
