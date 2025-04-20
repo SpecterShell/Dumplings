cb061abf46694ced59b9cf325f9af713d0541d8a
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-04-20 12:42:06 +0800

    Add and move Altova

---
+++ Tasks/Altova.MobileTogetherServer.2025/Script.ps1
@@ -0,0 +1,64 @@
+$Object1 = Invoke-WebRequest -Uri 'https://www.altova.com/thankyou?ProductCode=TS&EditionCode=Z&InstallerType=Product&Lang=multi&OperatingSystem=win32'
+
+# Installer
+$InstallerUrl = $Object1.Links.Where({ try { $_.href.EndsWith('.exe') } catch {} }, 'First')[0].href
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x86'
+  InstallerUrl = $InstallerUrl
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = $InstallerUrl -replace '\.exe$', '_x64.exe'
+}
+
+# Version
+$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '/MT_(\d+(?:\.\d+)+)/').Groups[1].Value
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    foreach ($Installer in $this.CurrentState.Installer) {
+      $this.Log("Processing $($Installer.Architecture) installer", 'Verbose')
+
+      $this.InstallerFiles[$Installer.InstallerUrl] = $InstallerFile = Get-TempFile -Uri $Installer.InstallerUrl
+      $InstallerFileExtracted = New-TempFolder
+      7z.exe e -aoa -ba -bd -y '-t#' -o"${InstallerFileExtracted}" $InstallerFile '*.cab' | Out-Host
+      $InstallerFile2 = Join-Path $InstallerFileExtracted '*.cab' | Get-Item -Force | Select-Object -First 1
+      $InstallerFile2Extracted = New-TempFolder
+      7z.exe e -aoa -ba -bd -y -o"${InstallerFile2Extracted}" $InstallerFile2 '*.msi' | Out-Host
+      $InstallerFile3 = Join-Path $InstallerFile2Extracted '*.msi' | Get-Item -Force | Select-Object -First 1
+      # RealVersion
+      $this.CurrentState.RealVersion = $InstallerFile3 | Read-ProductVersionFromMsi
+      # ProductCode
+      $Installer.ProductCode = $InstallerFile3 | Read-ProductCodeFromMsi
+      Remove-Item -Path $InstallerFile2Extracted -Recurse -Force -ErrorAction 'Continue' -ProgressAction 'SilentlyContinue'
+      Remove-Item -Path $InstallerFileExtracted -Recurse -Force -ErrorAction 'Continue' -ProgressAction 'SilentlyContinue'
+    }
+
+    try {
+      $Object2 = Invoke-WebRequest -Uri 'https://www.altova.com/releasenotes/getnotes' -Method Post -Body @{
+        category = 'mobile'
+        product  = 'MobileTogetherServer'
+        version  = "mt$($this.CurrentState.Version)"
+      } | ConvertFrom-Html
+
+      # ReleaseNotes (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotes'
+        Value  = $Object2.SelectNodes('/table/tbody/tr').ForEach({ "[$($_.SelectSingleNode('./td[@class="component"]').InnerText)] $($_.SelectSingleNode('./td[@class="summary"]').InnerText)" }) | Format-Text
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
