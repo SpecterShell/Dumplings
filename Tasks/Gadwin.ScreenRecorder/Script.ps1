15c1f7fc2d4e01f375e78db481421c8f86577987
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-04-21 11:25:05 +0800

    Add Gadwin.PrintScreen, Gadwin.PrintScreen.Pro, Gadwin.ScreenRecorder

---
+++ Tasks/Gadwin.ScreenRecorder/Script.ps1
@@ -0,0 +1,39 @@
+$Object1 = (Invoke-RestMethod -Uri 'https://www.gadwin.com/download/version_info.xml').Version_info.Product.Where({ $_.name -eq 'Gadwin ScreenRecorder' }, 'First')[0]
+
+# Version
+$this.CurrentState.Version = $Object1.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x86'
+  InstallerUrl = $Object1.url_x86 | ConvertTo-Https
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = $Object1.url_x64 | ConvertTo-Https
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = [datetime]::ParseExact($Object1.upload, 'MM.dd.yy', $null).ToString('yyyy-MM-dd')
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.InstallerFiles[$this.CurrentState.Installer[0].InstallerUrl] = $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+    # RealVersion
+    $this.CurrentState.RealVersion = $InstallerFile | Read-ProductVersionFromMsi
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
