75fbe46b403d5b158fdb9a58e38d9bbbcec1bc8d
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-04-21 18:20:39 +0800

    Add Evoluent.EvoluentMouseManager

---
+++ Tasks/Evoluent.EvoluentMouseManager/Script.ps1
@@ -0,0 +1,34 @@
+$Object1 = Invoke-RestMethod -Uri 'https://evoluent.com/download/update.json'
+
+# Version
+$this.CurrentState.Version = $Object1.version -join '.'
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.url
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseNotes (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotes'
+        Value  = $Object1.changelog | ConvertTo-UnorderedList | Format-Text
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
