ce35313949431fa763e5e8724f35aff3f335a986
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-04-23 18:12:50 +0800

    Add DaimlerTruck.FleetboardCockpit

---
+++ Tasks/DaimlerTruck.FleetboardCockpit/Script.ps1
@@ -0,0 +1,34 @@
+$Object1 = Invoke-WebRequest -Uri 'https://www.fleetboard.info/digital-solutions/fleetboard-cockpit/cockpit-releases/'
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.Links.Where({ try { $_.href.EndsWith('.exe') } catch {} }, 'First')[0].href
+}
+
+# Version
+$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d+(?:_\d+)+)').Groups[1].Value.Replace('_', '.')
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = [datetime]::ParseExact(
+        [regex]::Match($Object1.Content, '(\d{1,2}\.\d{1,2}\.20\d{2})').Groups[1].Value,
+        'dd.MM.yyyy',
+        $null
+      ).ToString('yyyy-MM-dd')
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
