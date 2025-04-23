122f7567605004bdf947191d07eb59e93c2be6f7
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-04-23 11:11:33 +0800

    Add CCL.NetLogo

---
+++ Tasks/CCL.NetLogo/Script.ps1
@@ -0,0 +1,60 @@
+$Prefix = 'https://ccl.northwestern.edu/netlogo/oldversions.shtml'
+
+$Object1 = Invoke-WebRequest -Uri $Prefix
+
+$Prefix = Join-Uri $Prefix ($Object1.Links.Where({ try { $_.outerHTML -match 'NetLogo (\d+(?:\.\d+)+)' } catch {} }).href | Sort-Object -Property { $_ -replace '\d+', { $_.Value.PadLeft(20) } } -Bottom 1)
+
+$Object2 = Invoke-WebRequest -Uri $Prefix
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x86'
+  InstallerUrl = Join-Uri $Prefix $Object2.Links.Where({ try { $_.href.EndsWith('-32.msi') } catch {} }, 'First')[0].href
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = Join-Uri $Prefix $Object2.Links.Where({ try { $_.href.EndsWith('-64.msi') } catch {} }, 'First')[0].href
+}
+
+# Version
+$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      $ReleaseNotesUrl = 'https://ccl.northwestern.edu/netlogo/docs/versions.html'
+      $Object2 = Invoke-WebRequest -Uri $ReleaseNotesUrl | ConvertFrom-Html
+
+      $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//h2[contains(./a/text(), 'Version $($this.CurrentState.Version)')]")
+      if ($ReleaseNotesTitleNode) {
+        # ReleaseNotes (en-US)
+        $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node -and $Node.Name -ne 'h2'; $Node = $Node.NextSibling) { $Node }
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
+        }
+
+        # ReleaseNotesUrl
+        $this.CurrentState.Locale += [ordered]@{
+          Key   = 'ReleaseNotesUrl'
+          Value = $ReleaseNotesUrl + $ReleaseNotesTitleNode.SelectSingleNode('./a').Attributes['href'].Value
+        }
+      } else {
+        $this.Log("No ReleaseTime and ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
