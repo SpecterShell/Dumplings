35ea869e3d8f92657bcd088de520d4ce48e530c8
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-04-23 22:19:11 +0800

    Add Dynare.Dynare

---
+++ Tasks/Dynare.Dynare/Script.ps1
@@ -0,0 +1,55 @@
+$Prefix = 'https://www.dynare.org/release/windows/'
+$Object1 = Invoke-WebRequest -Uri $Prefix
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = Join-Uri $Prefix ($Object1.Links.Where({ try { $_.href -match '^dynare-(\d+(?:\.\d+)+)-win\.exe$' } catch {} }).href | Sort-Object -Property { $_ -replace '\d+', { $_.Value.PadLeft(20) } } -Bottom 1)
+}
+
+# Version
+$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseNotesUrl
+      $this.CurrentState.Locale += [ordered]@{
+        Key   = 'ReleaseNotesUrl'
+        Value = 'https://www.dynare.org/news'
+      }
+
+      $Object2 = (Invoke-RestMethod -Uri 'https://www.dynare.org/feed.xml').Where({ $_.title.'#text'.Contains($this.CurrentState.Version) }, 'First')
+
+      if ($Object2) {
+        # ReleaseTime
+        $this.CurrentState.ReleaseTime = $Object2[0].updated | Get-Date -AsUTC
+
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $Object2[0].content.'#cdata-section' | ConvertFrom-Html | Get-TextContent | Format-Text
+        }
+        # ReleaseNotesUrl
+        $this.CurrentState.Locale += [ordered]@{
+          Key   = 'ReleaseNotesUrl'
+          Value = $Object2[0].link.href
+        }
+      } else {
+        $this.Log("No ReleaseTime, ReleaseNotes (en-US) and ReleaseNotesUrl for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
