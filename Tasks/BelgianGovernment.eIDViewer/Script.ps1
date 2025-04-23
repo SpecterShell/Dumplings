e339f9e3c66340e9a8eeff6120f2aa3feb9dad2b
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-04-23 12:30:54 +0800

    BelgianGovernment.eIDViewer: Decode URL and reset

--- Tasks/BelgianGovernment.eIDViewer/Script.ps1
+++ Tasks/BelgianGovernment.eIDViewer/Script.ps1
@@ -2,7 +2,7 @@ $Object1 = Invoke-WebRequest -Uri 'https://eid.belgium.be/en/download/47/license
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = $Object1.Links.Where({ try { $_.href.EndsWith('.msi') } catch {} }, 'First')[0].href
+  InstallerUrl = $Object1.Links.Where({ try { $_.href.EndsWith('.msi') } catch {} }, 'First')[0].href | ConvertTo-UnescapedUri
 }
 
 # Version
@@ -10,6 +10,10 @@ $this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].Inst
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
+    $this.InstallerFiles[$this.CurrentState.Installer[0].InstallerUrl] = $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+    # RealVersion
+    $this.CurrentState.RealVersion = $InstallerFile | Read-ProductVersionFromMsi
+
     $this.Print()
     $this.Write()
   }
