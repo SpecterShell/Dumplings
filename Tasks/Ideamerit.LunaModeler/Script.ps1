011d9ef12225fb3d10434ce32d1943a079c4980d
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-04-24 14:09:18 +0800

    Add Ideamerit

---
+++ Tasks/Ideamerit.LunaModeler/Script.ps1
@@ -0,0 +1,23 @@
+$Object1 = Invoke-WebRequest -Uri 'https://www.datensen.com/download.html'
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = $Object1.Links.Where({ try { $_.href.EndsWith('.exe') -and $_.href.Contains('Luna%20Modeler') -and $_.href.Contains('x64') } catch {} }, 'First')[0].href | ConvertTo-UnescapedUri
+}
+
+# Version
+$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
---
+++ Tasks/Ideamerit.MeteorModeler/Script.Alt.ps1
@@ -0,0 +1,23 @@
+$Object1 = Invoke-RestMethod -Uri "https://www.datensen.com/api/MeteorModeler/v3/$($this.Status.Contains('New') ? '9.1.0' : $this.LastState.Version)/t/trial/d/0"
+
+# Version
+$this.CurrentState.Version = $Object1.data[1]
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = "https://www.datensen.com/downloads/Meteor Modeler-$($this.CurrentState.Version)-x64.exe"
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
