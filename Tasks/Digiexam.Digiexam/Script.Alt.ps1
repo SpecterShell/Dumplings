2c7e66fb1805af97d2763c4c7b05ee6ce6a9b911
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-04-25 16:15:18 +0800

    Add Digiexam.Digiexam

---
+++ Tasks/Digiexam.Digiexam/Script.Alt.ps1
@@ -0,0 +1,29 @@
+$Object1 = Invoke-WebRequest -Uri 'https://storage.googleapis.com/digiexam/clients/win/RELEASES' | Read-ResponseContent | ConvertFrom-SquirrelReleases | Where-Object -FilterScript { -not $_.IsDelta } | Sort-Object -Property { $_.Version -creplace '\d+', { $_.Value.PadLeft(20) } } -Bottom 1
+
+# Version
+$this.CurrentState.Version = $Object1.Version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture  = 'x64'
+  InstallerType = 'exe'
+  InstallerUrl  = 'https://www.digiexam.com/hubfs/client/Digiexam_Win_x86_64.exe'
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture  = 'x64'
+  InstallerType = 'wix'
+  InstallerUrl  = 'https://www.digiexam.com/hubfs/client/managed/Digiexam_Managed_Win_x86_64.msi'
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
