9f91a753cf4263720af4829ad1bdfcbaf6d7182a
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-04-25 16:14:56 +0800

    Dialpad.Dialpad: Fixes

--- Tasks/Dialpad.Dialpad/Script.ps1
+++ Tasks/Dialpad.Dialpad/Script.ps1
@@ -1,11 +1,11 @@
-# x64
+# x86
 $Object1 = Invoke-WebRequest -Uri 'https://storage.googleapis.com/dialpad_native/stable/win32/ia32/RELEASES' | Read-ResponseContent | ConvertFrom-SquirrelReleases | Where-Object -FilterScript { -not $_.IsDelta } | Sort-Object -Property { $_.Version -creplace '\d+', { $_.Value.PadLeft(20) } } -Bottom 1
-# arm64
+# x64
 $Object2 = Invoke-WebRequest -Uri 'https://storage.googleapis.com/dialpad_native/stable/win32/x64/RELEASES' | Read-ResponseContent | ConvertFrom-SquirrelReleases | Where-Object -FilterScript { -not $_.IsDelta } | Sort-Object -Property { $_.Version -creplace '\d+', { $_.Value.PadLeft(20) } } -Bottom 1
 
 if ($Object1.Version -ne $Object2.Version) {
-  $this.Log("x64 version: $($Object1.Version)")
-  $this.Log("arm64 version: $($Object2.Version)")
+  $this.Log("x86 version: $($Object1.Version)")
+  $this.Log("x64 version: $($Object2.Version)")
   throw 'Inconsistent versions detected'
 }
 
