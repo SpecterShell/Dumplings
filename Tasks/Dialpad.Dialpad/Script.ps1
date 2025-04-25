4171cd68396dd2311fe2696c356c5f1fc8764657
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-04-25 12:12:00 +0800

    Dialpad.Dialpad: Add x86 installer

--- Tasks/Dialpad.Dialpad/Script.ps1
+++ Tasks/Dialpad.Dialpad/Script.ps1
@@ -1,9 +1,23 @@
-$Object1 = Invoke-WebRequest -Uri 'https://storage.googleapis.com/dialpad_native/stable/win32/x64/RELEASES' | Read-ResponseContent | ConvertFrom-SquirrelReleases | Where-Object -FilterScript { -not $_.IsDelta } | Sort-Object -Property { $_.Version -creplace '\d+', { $_.Value.PadLeft(20) } } -Bottom 1
+# x64
+$Object1 = Invoke-WebRequest -Uri 'https://storage.googleapis.com/dialpad_native/stable/win32/ia32/RELEASES' | Read-ResponseContent | ConvertFrom-SquirrelReleases | Where-Object -FilterScript { -not $_.IsDelta } | Sort-Object -Property { $_.Version -creplace '\d+', { $_.Value.PadLeft(20) } } -Bottom 1
+# arm64
+$Object2 = Invoke-WebRequest -Uri 'https://storage.googleapis.com/dialpad_native/stable/win32/x64/RELEASES' | Read-ResponseContent | ConvertFrom-SquirrelReleases | Where-Object -FilterScript { -not $_.IsDelta } | Sort-Object -Property { $_.Version -creplace '\d+', { $_.Value.PadLeft(20) } } -Bottom 1
+
+if ($Object1.Version -ne $Object2.Version) {
+  $this.Log("x64 version: $($Object1.Version)")
+  $this.Log("arm64 version: $($Object2.Version)")
+  throw 'Inconsistent versions detected'
+}
 
 # Version
-$this.CurrentState.Version = $Object1.Version
+$this.CurrentState.Version = $Object2.Version
 
 # Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture  = 'x86'
+  InstallerType = 'exe'
+  InstallerUrl  = "https://storage.googleapis.com/dialpad_native/stable/win32/ia32/DialpadSetup-$($this.CurrentState.Version).exe"
+}
 $this.CurrentState.Installer += [ordered]@{
   Architecture  = 'x64'
   InstallerType = 'exe'
