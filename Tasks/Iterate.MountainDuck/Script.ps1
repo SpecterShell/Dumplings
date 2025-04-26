aee8e851ace99721abc032f81b7ddd53332f98fb
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-04-26 19:06:07 +0800

    Add Iterate.MountainDuck

---
+++ Tasks/Iterate.MountainDuck/Script.ps1
@@ -0,0 +1,50 @@
+$Object1 = Invoke-RestMethod -Uri 'https://version-cdn.mountainduck.io/4/windows/changelog.rss'
+
+# Version
+$this.CurrentState.Version = [regex]::Match($Object1.enclosure.url, '(\d+(?:\.\d+)+)').Groups[1].Value
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerType = 'burn'
+  InstallerUrl  = $Object1.enclosure.url | ConvertTo-UnescapedUri
+}
+$this.CurrentState.Installer += [ordered]@{
+  InstallerType = 'wix'
+  InstallerUrl  = $Object1.enclosure.url -replace '\.exe$', '.msi' | ConvertTo-UnescapedUri
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      $Object2 = Invoke-WebRequest -Uri 'https://mountainduck.io/changelog/' | ConvertFrom-Html
+      $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//tr[contains(./td[1], '$($this.CurrentState.Version.Split('.')[0..2] -join '.')')]")
+      if ($ReleaseNotesTitleNode) {
+        # ReleaseTime
+        $this.CurrentState.ReleaseTime = [regex]::Match($ReleaseNotesTitleNode.SelectSingleNode('./td[1]').InnerText, '(\d{1,2}\W+[a-zA-Z]+\W+20\d{2})').Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
+
+        # Add brackets around labels
+        $ReleaseNotesTitleNode.SelectNodes('./td[2]//span[contains(@class, "label")]').ForEach({ $_.InnerHtml = "[$($_.InnerText)]" })
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesTitleNode.SelectSingleNode('./td[2]') | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
