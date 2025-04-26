e4bd5ae7b8a7526e6d74ac28b41dee49c742bfa3
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-04-26 14:30:43 +0800

    Add Novacura.FlowClient, Novacura.FlowServerManager and replace NuGet URL for Novacura.FlowStudio

--- Tasks/Novacura.FlowClient/Script.ps1
+++ Tasks/Novacura.FlowClient/Script.ps1
@@ -1,4 +1,4 @@
-$Object1 = Invoke-RestMethod -Uri 'https://pkgs.dev.azure.com/NC-FLOW/c34daccf-1296-47b5-8591-66ab5e0dc7cb/_packaging/bd2d042a-4006-4a45-99a1-7d4f37ab6a56/nuget/v3/registrations2-semver2/novacura.flow.studio/index.json'
+$Object1 = Invoke-RestMethod -Uri 'https://pkgs.dev.azure.com/NC-FLOW/c34daccf-1296-47b5-8591-66ab5e0dc7cb/_packaging/bd2d042a-4006-4a45-99a1-7d4f37ab6a56/nuget/v3/registrations2-semver2/novacura.web.forms/index.json'
 
 # Version
 $this.CurrentState.Version = $Object1.items.items[0].catalogEntry.version
