2fb0a0400fe7f36668e44c0a584e572f35a1b4c8
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-04-26 14:59:57 +0800

    Add Genesys.GenesysCloud

---
+++ Tasks/Genesys.GenesysCloud/Script.ps1
@@ -0,0 +1,53 @@
+$Object1 = Invoke-RestMethod -Uri 'https://apps.mypurecloud.com/directory-windows/appcast.xml'
+
+# Version
+$this.CurrentState.Version = $Object1.enclosure.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerType = 'burn'
+  InstallerUrl  = $Object1.enclosure.url
+}
+$this.CurrentState.Installer += [ordered]@{
+  InstallerType = 'wix'
+  InstallerUrl  = $Object1.enclosure.url -replace '\.exe$', '.msi'
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.pubDate | Get-Date -AsUTC
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    try {
+      $Object2 = Invoke-WebRequest -Uri 'https://help.mypurecloud.com/release-notes-home/genesys-cloud-for-windows-desktop-app-release-notes/' | ConvertFrom-Html
+      $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//div[contains(@class, 'accordion') and contains(./div[@class='accordion__header'], '$($this.CurrentState.Version)')]")
+      if ($ReleaseNotesTitleNode) {
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesTitleNode.SelectSingleNode('./div[@class="accordion__content"]') | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
