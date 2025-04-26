c68dc8a9208a0068d92efdf1ae67177e61ebc10b
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-04-26 12:15:26 +0800

    Add XnSoft.XnViewMP

---
+++ Tasks/XnSoft.XnViewMP/Script.ps1
@@ -0,0 +1,31 @@
+$Prefix = 'https://download.xnview.com/old_versions/XnView_MP/'
+
+$Object1 = Invoke-WebRequest -Uri "${Prefix}?C=N;O=D;V=1;P=*win*x64*.exe;F=0"
+
+$InstallerName = $Object1.Links[1].href
+
+# Version
+$this.CurrentState.Version = [regex]::Match($InstallerName, '-([\d\.]+)[-\.]').Groups[1].Value
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = "${Prefix}${InstallerName}"
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.InstallerFiles[$this.CurrentState.Installer[0].InstallerUrl] = $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+    # RealVersion
+    $this.CurrentState.RealVersion = $InstallerFile | Read-ProductVersionFromExe
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
