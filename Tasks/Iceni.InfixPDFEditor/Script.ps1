a7ae3d017465b91c55a4f978e63fde7460abdf96
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-04-27 17:06:40 +0800

    Add Iceni.InfixPDFEditor

---
+++ Tasks/Iceni.InfixPDFEditor/Script.ps1
@@ -0,0 +1,50 @@
+$Object1 = Invoke-WebRequest -Uri 'https://www.iceni.com/infix_download.htm'
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.Links.Where({ try { $_.href.EndsWith('.exe') } catch {} }, 'First')[0].href
+}
+
+# Version
+$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.InstallerFiles[$this.CurrentState.Installer[0].InstallerUrl] = $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+    # RealVersion
+    $this.CurrentState.RealVersion = $InstallerFile | Read-ProductVersionFromExe
+
+    try {
+      $Object2 = Invoke-WebRequest -Uri 'https://www.iceni.com/infix_change_history7.htm' | ConvertFrom-Html
+      $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//div[contains(@class, 'panel-heading') and contains(., '$($this.CurrentState.Version)')]")
+      if ($ReleaseNotesTitleNode) {
+        # ReleaseTime
+        $this.CurrentState.ReleaseTime = [regex]::Match($ReleaseNotesTitleNode.InnerText, '(\d{1,2}\W+[a-zA-Z]+\W+20\d{2})').Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
+
+        # Remove release notes prefix symbols
+        $ReleaseNotesTitleNode.SelectNodes('./following-sibling::node()//i[contains(@class, "fa ")]').ForEach({ $_.Remove() })
+
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesTitleNode.SelectNodes('./following-sibling::node()') | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
