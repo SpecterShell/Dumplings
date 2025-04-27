93c4e1893a43bcb06a96340fadf9d680dcdfa646
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-04-27 16:14:12 +0800

    Add Cypherix.Cryptainer.Personal

---
+++ Tasks/Cypherix.Cryptainer.Personal/Script.ps1
@@ -0,0 +1,27 @@
+$Prefix = 'https://www.cypherix.com/cryptainer-pe-download-center.html'
+$Object1 = Invoke-WebRequest -Uri $Prefix
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = Join-Uri $Prefix $Object1.Links.Where({ try { $_.href.EndsWith('.exe') } catch {} }, 'First')[0].href
+}
+
+# Version
+$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    if ($this.CurrentState.Version.Split('.')[0] -ne $this.Config.WinGetIdentifier.Split('.')[-1]) {
+      $this.Log("The WinGet package needs to be updated to the version $($this.CurrentState.Version.Split('.')[0])", 'Error')
+    } else {
+      $this.Submit()
+    }
+  }
+}
