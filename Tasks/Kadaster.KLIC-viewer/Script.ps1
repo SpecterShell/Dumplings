e729afa6e304021cbfc43022f751c3c7ac023ec7
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-04-28 19:04:32 +0800

    Add Kadaster.KLIC-viewer

---
+++ Tasks/Kadaster.KLIC-viewer/Script.ps1
@@ -0,0 +1,23 @@
+$Prefix = 'https://www.kadaster.nl/-/klic-viewer-desktopvarianten'
+$Object1 = Invoke-WebRequest -Uri $Prefix
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = Join-Uri $Prefix $Object1.Links.Where({ try { $_.outerHTML.Contains('.exe') -and $_.href.Contains('setup') } catch {} }, 'First')[0].href
+}
+
+# Version
+$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d+(?:-\d+)+)').Groups[1].Value.Replace('-', '.')
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
