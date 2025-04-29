2f91cec144fff624942e8a715e0e61efce56834a
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-04-29 14:27:54 +0800

    Add LiquidFiles.LiquidFilesOutlookPlugin

---
+++ Tasks/LiquidFiles.LiquidFilesOutlookPlugin/Script.ps1
@@ -0,0 +1,30 @@
+$Object1 = Invoke-RestMethod -Uri 'https://license.liquidfiles.com/download/clients.json'
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.outlook.admin_msi.url
+}
+
+# Version
+$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.outlook.admin_msi.date | Get-Date -Format 'yyyy-MM-dd'
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
