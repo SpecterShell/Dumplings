a86d226e1f78a6416db1df554080fdd473815a76
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-04-29 15:14:02 +0800

    Add MicroDicom.DICOMViewer

---
+++ Tasks/MicroDicom.DICOMViewer/Script.ps1
@@ -0,0 +1,57 @@
+$Prefix = 'https://www.microdicom.com/downloads.html'
+$Object1 = Invoke-WebRequest -Uri $Prefix
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x86'
+  InstallerUrl = $InstallerUrlX86 = Join-Uri $Prefix $Object1.Links.Where({ try { $_.href.EndsWith('.exe') -and -not $_.href.Contains('ShellExtension') -and $_.href.Contains('win32') } catch {} }, 'First')[0].href
+}
+$VersionX86 = [regex]::Match($InstallerUrlX86, '(\d+(?:\.\d+)+)').Groups[1].Value
+
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = $InstallerUrlX64 = Join-Uri $Prefix $Object1.Links.Where({ try { $_.href.EndsWith('.exe') -and -not $_.href.Contains('ShellExtension') -and $_.href.Contains('x64') } catch {} }, 'First')[0].href
+}
+$VersionX64 = [regex]::Match($InstallerUrlX64, '(\d+(?:\.\d+)+)').Groups[1].Value
+
+if ($VersionX86 -ne $VersionX64) {
+  $this.Log("x86 version: ${VersionX86}")
+  $this.Log("x64 version: ${VersionX64}")
+  throw 'Inconsistent versions detected'
+}
+
+# Version
+$this.CurrentState.Version = $VersionX64
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      $Object2 = Invoke-WebRequest -Uri 'https://www.microdicom.com/dicom-viewer/history.html' | ConvertFrom-Html
+
+      $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//div[@itemprop='articleBody']/*[text()='$($this.CurrentState.Version)']")
+      if ($ReleaseNotesTitleNode) {
+        $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node -and $Node.Name -ne 'h2' -and $Node.InnerText -notmatch '^\d+(?:\.\d+)+$'; $Node = $Node.NextSibling) { $Node }
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
