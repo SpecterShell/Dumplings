432cbc6f7aae832f64df17b6e89ebed3d943c4ea
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-05-03 17:52:38 +0800

    Kubus.BIMcollabInstaller: Switch to MSI installer

--- Tasks/Kubus.BIMcollabInstaller/Script.ps1
+++ Tasks/Kubus.BIMcollabInstaller/Script.ps1
@@ -5,27 +5,14 @@ $this.CurrentState.Version = $Object1.BIMcollabInstaller.Version
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = $Object1.BIMcollabInstaller.URL
+  InstallerUrl = $Object1.BIMcollabInstaller.URL -replace '\.exe$', '.msi'
 }
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
     $this.InstallerFiles[$this.CurrentState.Installer[0].InstallerUrl] = $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl | Rename-Item -NewName { "${_}.exe" } -PassThru | Select-Object -ExpandProperty 'FullName'
-    $InstallerFileExtracted = New-TempFolder
-    Start-Process -FilePath $InstallerFile -ArgumentList @('/extract', $InstallerFileExtracted) -Wait
-    $InstallerFile2 = Get-ChildItem -Path $InstallerFileExtracted -Include '*.msi' -Recurse | Select-Object -First 1
     # RealVersion
-    $this.CurrentState.RealVersion = $InstallerFile2 | Read-ProductVersionFromMsi
-    # ProductCode
-    $this.CurrentState.Installer[0]['ProductCode'] = $InstallerFile2 | Read-ProductCodeFromMsi
-    # AppsAndFeaturesEntries
-    $this.CurrentState.Installer[0]['AppsAndFeaturesEntries'] = @(
-      [ordered]@{
-        UpgradeCode   = $InstallerFile2 | Read-UpgradeCodeFromMsi
-        InstallerType = 'msi'
-      }
-    )
-    Remove-Item -Path $InstallerFileExtracted -Recurse -Force -ErrorAction 'Continue' -ProgressAction 'SilentlyContinue'
+    $this.CurrentState.RealVersion = $InstallerFile | Read-ProductVersionFromMsi
 
     $this.Print()
     $this.Write()
