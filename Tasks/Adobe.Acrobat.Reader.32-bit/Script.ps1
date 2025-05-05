7295c0aeaf6b9adc735ccd6bcd875bc6f3ebbd49
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-05-05 18:15:48 +0800

    Adobe.Acrobat.Reader: Add submit lock

--- Tasks/Adobe.Acrobat.Reader.32-bit/Script.ps1
+++ Tasks/Adobe.Acrobat.Reader.32-bit/Script.ps1
@@ -26,6 +26,20 @@ switch -Regex ($this.Check()) {
     $this.Message()
   }
   'Updated' {
-    $this.Submit()
+    $ToSubmit = $false
+
+    $Mutex = [System.Threading.Mutex]::new($false, 'DumplingsSubmitLockAcrobatReaderX86')
+    $Mutex.WaitOne(30000) | Out-Null
+    if (-not $Global:DumplingsStorage.Contains("AcrobatReaderX86-$($this.CurrentState.Version)-ToSubmit")) {
+      $Global:DumplingsStorage["AcrobatReaderX86-$($this.CurrentState.Version)-ToSubmit"] = $ToSubmit = $true
+    }
+    $Mutex.ReleaseMutex()
+    $Mutex.Dispose()
+
+    if ($ToSubmit) {
+      $this.Submit()
+    } else {
+      $this.Log('Another task is submitting manifests for this package', 'Warning')
+    }
   }
 }
--- Tasks/Adobe.Acrobat.Reader.64-bit/Script.ps1
+++ Tasks/Adobe.Acrobat.Reader.64-bit/Script.ps1
@@ -26,6 +26,20 @@ switch -Regex ($this.Check()) {
     $this.Message()
   }
   'Updated' {
-    $this.Submit()
+    $ToSubmit = $false
+
+    $Mutex = [System.Threading.Mutex]::new($false, 'DumplingsSubmitLockAcrobatReaderX64')
+    $Mutex.WaitOne(30000) | Out-Null
+    if (-not $Global:DumplingsStorage.Contains("AcrobatReaderX64-$($this.CurrentState.Version)-ToSubmit")) {
+      $Global:DumplingsStorage["AcrobatReaderX64-$($this.CurrentState.Version)-ToSubmit"] = $ToSubmit = $true
+    }
+    $Mutex.ReleaseMutex()
+    $Mutex.Dispose()
+
+    if ($ToSubmit) {
+      $this.Submit()
+    } else {
+      $this.Log('Another task is submitting manifests for this package', 'Warning')
+    }
   }
 }
