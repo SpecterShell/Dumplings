569372af4ea78ecb1eb839b4640944a47fd13de4
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-05-05 17:53:56 +0800

    Adobe.Acrobat.Reader.64-bit: Add update source

---
+++ Tasks/Adobe.Acrobat.Reader.64-bit#Up/Script.ps1
@@ -0,0 +1,25 @@
+# Version
+$RawVersion = [regex]::Match(
+  (Get-TempFile -Uri 'https://armmf.adobe.com/arm-manifests/win/AcrobatDCx64Manifest3.msi' | Read-MsiProperty -Query "SELECT FileSequences FROM ProductUpdatesEx WHERE ProductCode='{AC76BA86-1033-FF00-7760-BC15014EA700}'"), # DESC doesn't work here. Assume the latest version comes first.
+  '(\d{10})'
+).Groups[1].Value
+$this.CurrentState.Version = $RawVersion.Insert(5, '.').Insert(2, '.')
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = "https://ardownload2.adobe.com/pub/adobe/acrobat/win/AcrobatDC/${RawVersion}/AcroRdrDCx64${RawVersion}_MUI.exe"
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
