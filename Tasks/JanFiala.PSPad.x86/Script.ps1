4810b58e66ef561d83107ca0ce74043bb5271e1d
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-05-06 12:24:01 +0800

    Add JanFiala.PSPad

---
+++ Tasks/JanFiala.PSPad.x86/Script.ps1
@@ -0,0 +1,50 @@
+$Object1 = Invoke-RestMethod -Uri 'http://www.pspad.com/version.php'
+
+# Version
+$VersionMatches = [regex]::Match($Object1, '^(\d+(?:\.\d+)+) \((\d+)\)')
+$this.CurrentState.Version = "$($VersionMatches.Groups[1].Value).$($VersionMatches.Groups[2].Value)"
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = "https://pspad.poradna.net/release/pspad$($VersionMatches.Groups[1].Value.Replace('.', ''))_setup.exe"
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      $Object2 = Invoke-WebRequest -Uri 'https://www.pspad.com/en/whatsnew.htm' | ConvertFrom-Html
+
+      $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//h4[contains(text(), 'v$($VersionMatches.Groups[1].Value)')]")
+      if ($ReleaseNotesTitleNode) {
+        # ReleaseTime
+        $this.CurrentState.ReleaseTime = [datetime]::ParseExact(
+          [regex]::Match($ReleaseNotesTitleNode.InnerText, '(\d{1,2}/\d{1,2}/20\d{2})').Groups[1].Value,
+          'MM/dd/yyyy',
+          $null
+        ).ToUniversalTime()
+
+        $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node -and $Node.Name -ne 'h4'; $Node = $Node.NextSibling) { $Node }
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
