106197ef21cae3ac1e4635bb9afc5049a5fdf112
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-05-07 00:48:10 +0800

    Add DBeaver.DBeaver.Team

---
+++ Tasks/DBeaver.DBeaver.Team/Script.ps1
@@ -0,0 +1,46 @@
+$Object1 = Invoke-RestMethod -Uri 'https://dbeaver.com/product/dbeaver-te-version.xml'
+
+# Version
+$this.CurrentState.Version = $Object1.version.number
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  Scope        = 'user'
+  InstallerUrl = "https://downloads.dbeaver.net/team/$($this.CurrentState.Version)/dbeaver-te-$($this.CurrentState.Version)-x86_64-setup.exe"
+  ProductCode  = 'DBeaverTeam (current user)'
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  Scope        = 'machine'
+  InstallerUrl = "https://downloads.dbeaver.net/team/$($this.CurrentState.Version)/dbeaver-te-$($this.CurrentState.Version)-x86_64-setup.exe"
+  ProductCode  = 'DBeaverTeam'
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = [datetime]::ParseExact($Object1.version.date, 'dd.MM.yyyy', $null).ToString('yyyy-MM-dd')
+
+      # ReleaseNotes (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotes'
+        Value  = $Object1.version.'release-notes'.'#cdata-section' | Format-Text
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
