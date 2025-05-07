7853d7a97a85177ef1edd18c4abb9c06044b3fc1
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-05-07 12:19:49 +0800

    ElementLabs.LMStudio: Add arm64 installer, fix version pattern, parse real version from installer, reset

--- Tasks/ElementLabs.LMStudio/Script.ps1
+++ Tasks/ElementLabs.LMStudio/Script.ps1
@@ -3,21 +3,30 @@ $EdgeDriver.Navigate().GoToUrl('https://lmstudio.ai/')
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
   InstallerUrl = $InstallerUrl = $EdgeDriver.FindElement([OpenQA.Selenium.By]::XPath('//a[contains(@href, ".exe")]')).GetAttribute('href')
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'arm64'
+  InstallerUrl = $InstallerUrl.Replace('x64', 'arm64')
 }
 
 # Version
-$this.CurrentState.Version = [regex]::Match($InstallerUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
+$this.CurrentState.Version = [regex]::Match($InstallerUrl, '(\d+(?:\.\d+)+(-\d+)?)').Groups[1].Value
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
+    $this.InstallerFiles[$this.CurrentState.Installer[0].InstallerUrl] = $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+    # RealVersion
+    $this.CurrentState.RealVersion = $InstallerFile | Read-ProductVersionFromExe
+
     try {
-      if ($Global:DumplingsStorage.Contains('LMStudio') -and $Global:DumplingsStorage.LMStudio.Contains($this.CurrentState.Version)) {
+      if ($Global:DumplingsStorage.Contains('LMStudio') -and $Global:DumplingsStorage.LMStudio.Contains($this.CurrentState.RealVersion)) {
         # ReleaseNotes (en-US)
         $this.CurrentState.Locale += [ordered]@{
           Locale = 'en-US'
           Key    = 'ReleaseNotes'
-          Value  = $Global:DumplingsStorage.LMStudio[$this.CurrentState.Version].ReleaseNotes
+          Value  = $Global:DumplingsStorage.LMStudio[$this.CurrentState.RealVersion].ReleaseNotes
         }
       } else {
         $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
