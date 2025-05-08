259feddc7c780cf82d372ae26b10bc6890c30ba9
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-05-08 12:07:33 +0800

    Add ADInstruments.kuraCloud

---
+++ Tasks/ADInstruments.kuraCloud/Script.ps1
@@ -0,0 +1,23 @@
+$Prefix = 'https://updates.adinstruments.com/'
+$Object1 = Invoke-RestMethod -Uri "${Prefix}kcd-win.json"
+
+# Version
+$this.CurrentState.Version = $Object1.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = Join-Uri $Prefix $Object1.filename
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
