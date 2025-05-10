30ca0791f02c1f19b87abf44a6a5382e8bcad37c
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-05-11 00:55:23 +0800

    Freedom.Freedom: Switch to Last Modified and reset

--- Tasks/Freedom.Freedom/Script.ps1
+++ Tasks/Freedom.Freedom/Script.ps1
@@ -1,14 +1,11 @@
 $Uri1 = 'https://cdn.freedom.to/installers/FreedomSetup.exe'
 $Object1 = Invoke-WebRequest -Uri $Uri1 -Method Head
-# ETag
-$ETag = $Object1.Headers.ETag[0]
+# Hash
+$this.CurrentState.LastModified = $Object1.Headers.'Last-Modified'[0]
 
 # Case 0: Force submit the manifest
 if ($Global:DumplingsPreference.Contains('Force')) {
   $this.Log('Skip checking states', 'Info')
-
-  # ETag
-  $this.CurrentState.ETag = @($ETag)
 
   $InstallerFile = Get-TempFile -Uri $Uri1
   # Version
@@ -49,9 +46,6 @@ if ($Global:DumplingsPreference.Contains('Force')) {
 if ($this.Status.Contains('New')) {
   $this.Log('New task', 'Info')
 
-  # ETag
-  $this.CurrentState.ETag = @($ETag)
-
   $InstallerFile = Get-TempFile -Uri $Uri1
   # Version
   $this.CurrentState.Version = $InstallerFile | Read-ProductVersionFromExe
@@ -85,15 +79,13 @@ if ($this.Status.Contains('New')) {
   return
 }
 
-if ($ETag -in $this.LastState.ETag) {
-  # The ETag is unchanged, so let's check if the alternative installer URL exists
+if ([datetime]$this.CurrentState.LastModified -le [datetime]$this.LastState.LastModified) {
+  # The Last Modified is unchanged, so let's check if the alternative installer URL exists
   if ($this.LastState.Mode -eq $true) {
     # If the alternative installer URL exists, don't fallback to the main one
     $this.Log("The version $($this.LastState.Version) from the last state is the latest", 'Info')
     return
   } else {
-    # ETag
-    $this.CurrentState.ETag = $this.LastState.ETag
     # Version
     $this.CurrentState.Version = $this.LastState.Version
     # InstallerSha256
@@ -110,18 +102,18 @@ if ($ETag -in $this.LastState.ETag) {
       # Mode
       $this.CurrentState.Mode = $true
 
-      # Case 2: The ETag is unchanged, but an alternative installer URL is detected
+      # Case 2: The Last Modified is unchanged, but an alternative installer URL is detected
       $this.Log('An alternative installer URL is detected', 'Info')
       $this.Print()
       $this.Write()
       return
     } catch {
-      # Case 3: The ETag is unchanged, and the alternative installer URL doesn't exist
+      # Case 3: The Last Modified is unchanged, and the alternative installer URL doesn't exist
       return
     }
   }
 } else {
-  # The ETag has changed, but there is a chance that the hash is actually unchanged, so let's check it
+  # The Last Modified has changed, but there is a chance that the hash is actually unchanged, so let's check it
   $InstallerFile = Get-TempFile -Uri $Uri1
   # Version
   $this.CurrentState.Version = $InstallerFile | Read-ProductVersionFromExe
@@ -129,12 +121,10 @@ if ($ETag -in $this.LastState.ETag) {
   $this.CurrentState.InstallerSha256 = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
 
   if ($this.CurrentState.InstallerSha256 -eq $this.LastState.InstallerSha256) {
-    $this.Log('The ETag has changed, but the SHA256 is not', 'Info')
-    # ETag
-    $this.CurrentState.ETag = $this.LastState.ETag + $ETag
+    $this.Log('The Last Modified has changed, but the SHA256 is not', 'Info')
 
     if ($this.LastState.Mode -eq $true) {
-      # Case 4: The ETag has changed, but the SHA256 is not, and the alternative installer URL already exists
+      # Case 4: The Last Modified has changed, but the SHA256 is not, and the alternative installer URL already exists
       $this.Log("The version $($this.LastState.Version) from the last state is the latest", 'Info')
 
       # Installer
@@ -146,7 +136,7 @@ if ($ETag -in $this.LastState.ETag) {
       return
     } else {
       try {
-        # Case 5: The ETag has changed, but the SHA256 is not, while an alternative installer URL is detected
+        # Case 5: The Last Modified has changed, but the SHA256 is not, while an alternative installer URL is detected
         $Uri2 = "https://cdn.freedom.to/installers/win/FreedomSetup-$($this.CurrentState.Version).exe"
         $null = Invoke-WebRequest -Uri $Uri2 -Method Head
         # Installer
@@ -162,8 +152,7 @@ if ($ETag -in $this.LastState.ETag) {
         $this.Write()
         return
       } catch {
-        # Case 6: The ETag has changed, but the SHA256 is not, and the alternative installer URL doesn't exist
-
+        # Case 6: The Last Modified has changed, but the SHA256 is not, and the alternative installer URL doesn't exist
         # Installer
         $this.CurrentState.Installer += [ordered]@{
           InstallerUrl    = $Uri1
@@ -178,12 +167,10 @@ if ($ETag -in $this.LastState.ETag) {
       }
     }
   } else {
-    $this.Log('Both the ETag and the SHA256 have changed', 'Info')
-    # ETag
-    $this.CurrentState.ETag = @($ETag)
+    $this.Log('Both the Last Modified and the SHA256 have changed', 'Info')
 
     try {
-      # The ETag and the SHA256 have changed, while the alternative installer URL exists
+      # The Last Modified and the SHA256 have changed, while the alternative installer URL exists
       $Uri2 = "https://cdn.freedom.to/installers/win/FreedomSetup-$($this.CurrentState.Version).exe"
       $null = Invoke-WebRequest -Uri $Uri2 -Method Head
       # Installer
@@ -194,7 +181,7 @@ if ($ETag -in $this.LastState.ETag) {
       # Mode
       $this.CurrentState.Mode = $true
     } catch {
-      # The ETag and the SHA256 have changed, but the alternative installer URL doesn't exist
+      # The Last Modified and the SHA256 have changed, but the alternative installer URL doesn't exist
       $this.Log("${Uri2} doesn't exist, fallback to ${Uri1}", 'Warning')
       # Installer
       $this.CurrentState.Installer += [ordered]@{
@@ -214,13 +201,13 @@ if ($ETag -in $this.LastState.ETag) {
       $this.Write()
       $this.Message()
     }
-    # Case 9: The ETag, the SHA256 and the version have changed
+    # Case 9: The Last Modified, the SHA256 and the version have changed
     'Updated|Rollbacked' {
       $this.Submit()
     }
-    # Case 7: The ETag and the SHA256 have changed, but the version is not
+    # Case 7: The Last Modified and the SHA256 have changed, but the version is not
     Default {
-      $this.Log('The ETag and the SHA256 have changed, but the version is not', 'Info')
+      $this.Log('The Last Modified and the SHA256 have changed, but the version is not', 'Info')
       $this.Config.IgnorePRCheck = $true
       $this.Print()
       $this.Write()
