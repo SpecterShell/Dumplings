9dc73ac4a026d9e9ea4fbb32dfd2dbb05ad89378
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-05-11 13:03:33 +0800

    Philips.SpeechLive: Fixes

--- Tasks/Philips.SpeechLive/Script.ps1
+++ Tasks/Philips.SpeechLive/Script.ps1
@@ -1,99 +1,33 @@
-function Read-Installer {
-  $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
-  # Version
-  $this.CurrentState.Version = $InstallerFile | Read-ProductVersionFromMsi
-  # InstallerSha256
-  $this.CurrentState.Installer[0]['InstallerSha256'] = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
-  # ProductCode
-  $this.CurrentState.Installer[0]['ProductCode'] = $InstallerFile | Read-ProductCodeFromMsi
-  # AppsAndFeaturesEntries
-  $this.CurrentState.Installer[0]['AppsAndFeaturesEntries'] = @(
-    [ordered]@{
-      UpgradeCode = $InstallerFile | Read-UpgradeCodeFromMsi
-    }
-  )
-  Remove-Item -Path $InstallerFile -Recurse -Force -ErrorAction 'Continue' -ProgressAction 'SilentlyContinue'
-}
+$Prefix = 'https://speech-desktop-app-update.speech.com/sl-desktop-app-updates/win64-builder/'
 
+$Object1 = Invoke-RestMethod -Uri "${Prefix}latest.yml" | ConvertFrom-Yaml
+
+# Version
+$this.CurrentState.Version = $Object1.version
+
+# Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = 'https://speech-desktop-app-update.speech.com/sl-desktop-app-updates/latest/speechlive-desktop-windows-win64.msi'
+  InstallerType = 'nullsoft'
+  InstallerUrl  = Join-Uri $Prefix $Object1.files[0].url
 }
-
-$Object1 = Invoke-WebRequest -Uri $this.CurrentState.Installer[0].InstallerUrl -Method Head
-$ETag = $Object1.Headers.ETag[0]
-
-# Case 0: Force submit the manifest
-if ($Global:DumplingsPreference.Contains('Force')) {
-  $this.Log('Skip checking states', 'Info')
-
-  # ETag
-  $this.CurrentState.ETag = @($ETag)
-
-  Read-Installer
-
-  $this.Print()
-  $this.Write()
-  $this.Message()
-  $this.Submit()
-  return
-}
-
-# Case 1: The task is new
-if ($this.Status.Contains('New')) {
-  $this.Log('New task', 'Info')
-
-  # ETag
-  $this.CurrentState.ETag = @($ETag)
-
-  Read-Installer
-
-  $this.Print()
-  $this.Write()
-  return
-}
-
-# Case 2: The ETag is unchanged
-if ($ETag -in $this.LastState.ETag) {
-  $this.Log("The version $($this.LastState.Version) from the last state is the latest (Global)", 'Info')
-  return
-}
-
-Read-Installer
-
-# Case 3: The current state has an invalid version
-if ([string]::IsNullOrWhiteSpace($this.CurrentState.Version)) {
-  throw 'The current state has an invalid version'
-}
-
-# Case 4: The ETag has changed, but the SHA256 is not
-if ($this.CurrentState.Installer[0].InstallerSha256 -eq $this.LastState.Installer[0].InstallerSha256) {
-  $this.Log('The ETag has changed, but the SHA256 is not', 'Info')
-
-  # ETag
-  $this.CurrentState.ETag = $this.LastState.ETag + $ETag
-
-  $this.Write()
-  return
-}
-
-# ETag
-$this.CurrentState.ETag = @($ETag)
 
 switch -Regex ($this.Check()) {
-  # Case 6: The ETag, the SHA256 and the version have changed
-  'Updated|Rollbacked' {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.releaseDate | Get-Date -AsUTC
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
     $this.Print()
     $this.Write()
-    $this.Message()
-    $this.Submit()
   }
-  # Case 5: The ETag and the SHA256 have changed, but the version is not
-  Default {
-    $this.Log('The ETag and the SHA256 have changed, but the version is not', 'Info')
-    $this.Config.IgnorePRCheck = $true
-    $this.Print()
-    $this.Write()
+  'Changed|Updated' {
     $this.Message()
+  }
+  'Updated' {
     $this.Submit()
   }
 }
