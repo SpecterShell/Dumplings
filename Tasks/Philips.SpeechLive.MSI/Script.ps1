37a7efbe9d13f3814898203f0331e241a8c80e8f
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-05-11 13:02:59 +0800

    Add Philips.SpeechLive

---
+++ Tasks/Philips.SpeechLive.MSI/Script.ps1
@@ -0,0 +1,33 @@
+$Prefix = 'https://speech-desktop-app-update.speech.com/sl-desktop-app-updates/win64-builder/'
+
+$Object1 = Invoke-RestMethod -Uri "${Prefix}latest.yml" | ConvertFrom-Yaml
+
+# Version
+$this.CurrentState.Version = $Object1.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerType = 'nullsoft'
+  InstallerUrl  = Join-Uri $Prefix $Object1.files[0].url
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.releaseDate | Get-Date -AsUTC
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
