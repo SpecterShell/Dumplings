5aa430f26c70c91501de0900bcd5ad787f67e09e
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-05-12 23:22:18 +0800

    Add PowerSoftware.AnyBurn

---
+++ Tasks/PowerSoftware.AnyBurn.Pro/Script.ps1
@@ -0,0 +1,63 @@
+$Object1 = Invoke-RestMethod -Uri 'http://anyburn.com/version.ini' | ConvertFrom-Ini
+
+# Version
+$this.CurrentState.Version = $Object1.Version.Number
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x86'
+  InstallerUrl = $Object1.Version.ProUrl.Replace('www.anyburn.com', 'anyburn.com')
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = $Object1.Version.'ProUrl.X64'.Replace('www.anyburn.com', 'anyburn.com')
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'arm64'
+  InstallerUrl = $Object1.Version.'ProUrl.X64'.Replace('www.anyburn.com', 'anyburn.com') -replace 'x64', 'arm64'
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.Version.Date | Get-Date -Format 'yyyy-MM-dd'
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    try {
+      $Object2 = Invoke-WebRequest -Uri 'https://anyburn.com/tutorials/history.htm' | ConvertFrom-Html
+
+      $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//b[contains(text(), '$($this.CurrentState.Version)')]")
+      if ($ReleaseNotesTitleNode) {
+        # ReleaseNotes (en-US)
+        $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node; $Node = $Node.NextSibling) {
+          if ($Node.InnerText -notmatch '[a-zA-Z]+\W+\d{1,2}\W+20\d{2}') {
+            $Node
+          }
+        }
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseTime and ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
