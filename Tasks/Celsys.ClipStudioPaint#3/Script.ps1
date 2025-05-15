e77cf4b22eb8fd801ba10d34de55839107c23d70
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-05-15 16:01:16 +0800

    Celsys.ClipStudioPaint: Merge multiple editions into one task

---
+++ Tasks/Celsys.ClipStudioPaint#3/Script.ps1
@@ -0,0 +1,22 @@
+$Object1 = Invoke-WebRequest -Uri 'https://www.clipstudio.net/en/dl/v3/'
+
+# Version
+$this.CurrentState.Version = [regex]::Match($Object1.Content, 'Version (\d+(?:\.\d+){2,})').Groups[1].Value
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = Get-RedirectedUrl -Uri 'https://www.clipstudio.net/gd?id=csp-install-win_v3'
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
