4c6a3d0d1ee7fef6099f1c92ef9f375f589a7906
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-05-15 17:11:14 +0800

    JLC: Parse release notes as text, add arm64 installer

--- Tasks/JLC.EasyEDA.Pro/Script.ps1
+++ Tasks/JLC.EasyEDA.Pro/Script.ps1
@@ -17,23 +17,38 @@ switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
     try {
       $Object2 = Invoke-WebRequest -Uri 'https://pro.easyeda.com/page/update-record' | ConvertFrom-Html
+      $Object3 = [System.IO.StringReader]::new(($Object2.SelectSingleNode('//*[contains(@class, "doc-body-left")]') | Get-TextContent))
 
-      $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//*[contains(@class, 'doc-body-left')]/p[contains(./strong/text(), '$($this.CurrentState.Version)')]")
-      if ($ReleaseNotesTitleNode) {
-        $ReleaseNotesTimeNode = $ReleaseNotesTitleNode.SelectSingleNode('./following-sibling::p[1]')
-        # ReleaseTime
-        $this.CurrentState.ReleaseTime = ($ReleaseNotesTimeNode.InnerText | ConvertTo-HtmlDecodedText).Trim() | Get-Date -Format 'yyyy-MM-dd'
-
-        $ReleaseNotesNodes = for ($Node = $ReleaseNotesTimeNode.NextSibling; $Node -and -not $Node.SelectSingleNode('./strong'); $Node = $Node.NextSibling) { $Node }
+      while ($Object3.Peek() -ne -1) {
+        $String = $Object3.ReadLine()
+        if ($String -match "^v?$([regex]::Escape($this.CurrentState.Version))") {
+          break
+        }
+      }
+      if ($Object3.Peek() -ne -1) {
+        $ReleaseNotesObjects = [System.Collections.Generic.List[string]]::new()
+        while ($Object3.Peek() -ne -1) {
+          $String = $Object3.ReadLine()
+          if ($String -match '^(20\d{2}\.\d{1,2}\.\d{1,2})') {
+            # ReleaseTime
+            $this.CurrentState.ReleaseTime = $Matches[1] | Get-Date -Format 'yyyy-MM-dd'
+          } elseif ($String -notmatch '^v?(\d+(?:\.\d+)+)') {
+            $ReleaseNotesObjects.Add($String)
+          } else {
+            break
+          }
+        }
         # ReleaseNotes (en-US)
         $this.CurrentState.Locale += [ordered]@{
           Locale = 'en-US'
           Key    = 'ReleaseNotes'
-          Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
+          Value  = $ReleaseNotesObjects | Format-Text
         }
       } else {
         $this.Log("No ReleaseTime and ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
       }
+
+      $Object3.Close()
     } catch {
       $_ | Out-Host
       $this.Log($_, 'Warning')
