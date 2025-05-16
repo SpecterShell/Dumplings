a7024bc9f0cb8b37206ff5ce19c4bfe795e8678a
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-05-16 17:54:24 +0800

    HP.HPClientManagementScriptLibrary: Add ReleaseNotes

--- Tasks/HP.HPClientManagementScriptLibrary/Script.ps1
+++ Tasks/HP.HPClientManagementScriptLibrary/Script.ps1
@@ -10,6 +10,29 @@ $this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].Inst
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
+    try {
+      $Object2 = Invoke-WebRequest -Uri 'https://developers.hp.com/hp-client-management/doc/changelog' | ConvertFrom-Html
+
+      $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//h3[contains(text(), '[$($this.CurrentState.Version)]')]")
+      if ($ReleaseNotesTitleNode) {
+        # ReleaseTime
+        $this.CurrentState.ReleaseTime = [regex]::Match($ReleaseNotesTitleNode.InnerText, '([a-zA-Z]+\W+\d{1,2}\W+20\d{2})').Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
+
+        # ReleaseNotes (en-US)
+        $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node -and $Node.Name -ne 'h3'; $Node = $Node.NextSibling) { $Node }
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseTime and ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
     $this.Print()
     $this.Write()
   }
