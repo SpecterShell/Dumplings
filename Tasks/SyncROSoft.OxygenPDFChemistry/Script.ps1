0141a8b9bccd0e19727da5e014fdc002668d6f8b
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-05-19 14:32:40 +0800

    Add SyncROSoft.OxygenPDFChemistry

---
+++ Tasks/SyncROSoft.OxygenPDFChemistry/Script.ps1
@@ -0,0 +1,43 @@
+# Version
+$this.CurrentState.Version = $Global:DumplingsStorage.SyncROSoftApps.checkVersion.builds.currentVersion
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = "https://archives.oxygenxml.com/Oxygen/Chemistry/InstData$($this.CurrentState.Version)/oxygen-pdf-chemistry.exe"
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Global:DumplingsStorage.SyncROSoftApps.checkVersion.builds.build[0].pubDate | Get-Date -AsUTC
+
+      # ReleaseNotesUrl
+      $this.CurrentState.Locale += [ordered]@{
+        Key   = 'ReleaseNotesUrl'
+        Value = $ReleaseNotesUrl = "https://www.oxygenxml.com/pdf_chemistry/whatisnew$($this.CurrentState.Version).html"
+      }
+
+      $Object1 = Invoke-WebRequest -Uri $ReleaseNotesUrl | ConvertFrom-Html
+
+      # ReleaseNotes (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotes'
+        Value  = $Object1.SelectNodes('//div[@class="date"]/following-sibling::node()') | Get-TextContent | Format-Text
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
