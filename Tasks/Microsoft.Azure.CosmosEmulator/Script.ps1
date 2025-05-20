c6f7484743b62d2fcc4c5f05b7b51ce12e3994ae
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-05-20 22:24:54 +0800

    Microsoft.Azure.CosmosEmulator: Add ReleaseNotes

--- Tasks/Microsoft.Azure.CosmosEmulator/Script.ps1
+++ Tasks/Microsoft.Azure.CosmosEmulator/Script.ps1
@@ -8,6 +8,26 @@ $this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].Inst
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
+    try {
+      $Object2 = Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/MicrosoftDocs/azure-databases-docs/HEAD/articles/cosmos-db/emulator-release-notes.md' | Convert-MarkdownToHtml
+
+      $ReleaseNotesTitleNode = $Object2.SelectSingleNode("/h3[contains(., '$($this.CurrentState.Version)')]")
+      if ($ReleaseNotesTitleNode) {
+        $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node -and $Node.Name -ne 'h3'; $Node = $Node.NextSibling) { $Node }
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
     $this.Print()
     $this.Write()
   }
