ee66f0e76ec171e2bbf60ccb3a992a9f6823b51c
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-05-20 10:57:22 +0800

    Add StandBySoft.MOOSProjectViewer

---
+++ Tasks/StandBySoft.MOOSProjectViewer/Script.ps1
@@ -0,0 +1,23 @@
+$Object1 = Invoke-RestMethod -Uri 'http://www.moosprojectviewer.com/version.xml'
+$Object2 = $Object1.products.product.Where({ $_.name -eq 'MOOS' }, 'First')[0]
+
+# Version
+$this.CurrentState.Version = $Object2.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object2.'viewer-link-windows'
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
