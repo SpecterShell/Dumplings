a77054af01f368b7c3258824dd028ca2034175f9
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-05-21 13:28:01 +0800

    IDEA.ReadPaper: Switch to download source

--- Tasks/IDEA.ReadPaper/Script.ps1
+++ Tasks/IDEA.ReadPaper/Script.ps1
@@ -1,27 +1,15 @@
 $Object1 = Invoke-RestMethod -Uri 'https://readpaper.com/api/microService-app-aiKnowledge/aiKnowledge/client/v1/getDownloadUrls' -Method Post -ContentType 'application/json' -Body '{}'
 
-$Prefix = $Object1.data.urls.windowsClient | Split-Uri -Parent
-
-$Object2 = Invoke-RestMethod -Uri "${Prefix}latest.yml" | ConvertFrom-Yaml
-
-# Version
-$this.CurrentState.Version = $Object2.version
-
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = Join-Uri $Prefix $Object2.files[0].url
+  InstallerUrl = $Object1.data.urls.windowsClient
 }
+
+# Version
+$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d+(\.\d+)+)').Groups[1].Value
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
-    try {
-      # ReleaseTime
-      $this.CurrentState.ReleaseTime = $Object2.releaseDate | Get-Date -AsUTC
-    } catch {
-      $_ | Out-Host
-      $this.Log($_, 'Warning')
-    }
-
     $this.Print()
     $this.Write()
   }
