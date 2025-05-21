508a6e5c3c3281c4de20f46e07385688866858f6
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-05-21 15:19:19 +0800

    Add ADInstruments.LtSampling

---
+++ Tasks/ADInstruments.LtSampling/Script.ps1
@@ -0,0 +1,27 @@
+$Object1 = Invoke-WebRequest -Uri 'https://lt-sampling.kuracloud.com/lt-sampling.appinstaller' | Read-ResponseContent | ConvertFrom-Xml
+
+# Version
+$this.CurrentState.Version = $Object1.AppInstaller.Version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerType = 'msix'
+  InstallerUrl  = $Object1.AppInstaller.MainPackage.Uri
+}
+$this.CurrentState.Installer += [ordered]@{
+  InstallerType = 'msi'
+  InstallerUrl  = $Object1.AppInstaller.MainPackage.Uri -replace '\.msix$', '.msi'
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
