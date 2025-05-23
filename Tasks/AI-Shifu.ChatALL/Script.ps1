f7a92326093315e5ceb2bc14eb7fbdc997799a14
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-05-23 10:49:29 +0800

    AI-Shifu.ChatALL: Remove x86 installer

--- Tasks/AI-Shifu.ChatALL/Script.ps1
+++ Tasks/AI-Shifu.ChatALL/Script.ps1
@@ -8,10 +8,6 @@ $this.CurrentState.Version = $Object1.tag_name -creplace '^v'
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  Architecture = 'x86'
-  InstallerUrl = $Object1.assets.Where({ $_.name.EndsWith('.exe') -and $_.name.Contains('win') -and $_.name.Contains('ia32') }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
-}
-$this.CurrentState.Installer += [ordered]@{
   Architecture = 'x64'
   InstallerUrl = $Object1.assets.Where({ $_.name.EndsWith('.exe') -and $_.name.Contains('win') -and $_.name.Contains('x64') }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
 }
