790a4d7f3f56e62e731eef8193ef5d7667422e53
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-05-26 15:35:04 +0800

    Add MITMediaLab.Scratch.3

---
+++ Tasks/MITMediaLab.Scratch.3/Script.ps1
@@ -0,0 +1,20 @@
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = Get-RedirectedUrl -Uri 'https://downloads.scratch.mit.edu/desktop/Scratch Setup.exe' | ConvertTo-UnescapedUri
+}
+
+# Version
+$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
