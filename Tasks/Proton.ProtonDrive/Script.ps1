46133adea3f0e33eb044a2ccea967016ca5c3e7f
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-06-06 08:16:33 +0800

    Proton.ProtonDrive: Add arm64 installer

--- Tasks/Proton.ProtonDrive/Script.ps1
+++ Tasks/Proton.ProtonDrive/Script.ps1
@@ -1,11 +1,25 @@
-$Object1 = (Invoke-WebRequest -Uri 'https://proton.me/download/drive/windows/version.json' | Read-ResponseContent | ConvertFrom-Json -AsHashtable).Releases.Where({ $_.CategoryName -eq 'Stable' }, 'First')[0]
+# x64
+$Object1 = (Invoke-WebRequest -Uri 'https://proton.me/download/drive/windows/x64/v1/version.json' | Read-ResponseContent | ConvertFrom-Json -AsHashtable).Releases.Where({ $_.CategoryName -eq 'Stable' }, 'First')[0]
+# arm64
+$Object2 = (Invoke-WebRequest -Uri 'https://proton.me/download/drive/windows/arm64/v1/version.json' | Read-ResponseContent | ConvertFrom-Json -AsHashtable).Releases.Where({ $_.CategoryName -eq 'Stable' }, 'First')[0]
+
+if ($Object1.Version -ne $Object2.Version) {
+  $this.Log("x64 version: $($Object1.Version)")
+  $this.Log("arm64 version: $($Object2.Version)")
+  throw 'Inconsistent versions detected'
+}
 
 # Version
 $this.CurrentState.Version = $Object1.Version
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
   InstallerUrl = $Object1.File.Url | ConvertTo-UnescapedUri
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'arm64'
+  InstallerUrl = $Object2.File.Url | ConvertTo-UnescapedUri
 }
 
 switch -Regex ($this.Check()) {
