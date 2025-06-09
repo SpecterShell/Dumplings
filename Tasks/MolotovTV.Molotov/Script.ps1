0e89ff07d9fc423d36205ada0b16b6093fa1b17c
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-06-09 17:23:49 +0800

    Add MolotovTV.Molotov

---
+++ Tasks/MolotovTV.Molotov/Script.ps1
@@ -0,0 +1,23 @@
+$Object1 = Invoke-RestMethod -Uri 'https://desktop-app.molotov.tv/stable/win32/x64/manifest.json'
+
+# Version
+$this.CurrentState.Version = $Object1.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = $Object1.url | ConvertTo-UnescapedUri
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
