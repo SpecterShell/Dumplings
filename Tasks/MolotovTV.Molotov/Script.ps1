f4cac01484d5147192bab980911eccadef995bfd
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-06-09 17:33:42 +0800

    MolotovTV.Molotov: Add x86 installer

--- Tasks/MolotovTV.Molotov/Script.ps1
+++ Tasks/MolotovTV.Molotov/Script.ps1
@@ -1,12 +1,25 @@
-$Object1 = Invoke-RestMethod -Uri 'https://desktop-app.molotov.tv/stable/win32/x64/manifest.json'
+# x86
+$Object1 = Invoke-RestMethod -Uri 'https://desktop-app.molotov.tv/stable/win32/ia32/manifest.json'
+# x64
+$Object2 = Invoke-RestMethod -Uri 'https://desktop-app.molotov.tv/stable/win32/x64/manifest.json'
+
+if ($Object1.version -ne $Object2.version) {
+  $this.Log("x86 version: $($Object1.version)")
+  $this.Log("x64 version: $($Object2.version)")
+  throw 'Inconsistent versions detected'
+}
 
 # Version
-$this.CurrentState.Version = $Object1.version
+$this.CurrentState.Version = $Object2.version
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  Architecture = 'x64'
+  Architecture = 'x86'
   InstallerUrl = $Object1.url | ConvertTo-UnescapedUri
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = $Object2.url | ConvertTo-UnescapedUri
 }
 
 switch -Regex ($this.Check()) {
