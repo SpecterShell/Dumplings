3f7b92e759b4882afba2ba211562c5928a7b4dbf
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-06-10 19:32:59 +0800

    Add Lebo.Lebo

---
+++ Tasks/Lebo.Lebo/Script.ps1
@@ -0,0 +1,40 @@
+$Prefix = 'https://download.mirrorcast.cn/release/electron/packages/prod/'
+
+$Object1 = Invoke-RestMethod -Uri "${Prefix}23303-x64-latest-win32.yml" | ConvertFrom-Yaml
+
+# Version
+$this.CurrentState.Version = $Object1.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = Join-Uri $Prefix $Object1.files[0].url
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.releaseDate | Get-Date -AsUTC
+
+      # ReleaseNotes (zh-CN)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'zh-CN'
+        Key    = 'ReleaseNotes'
+        Value  = $Object1.releaseNotes | Format-Text
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
