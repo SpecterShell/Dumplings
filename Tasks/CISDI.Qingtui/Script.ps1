659379c02ff5f0aa09b7a22db8fcf43cb349e971
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-06-10 21:18:14 +0800

    CISDI.Qingtui: Switch to download source, reset

--- Tasks/CISDI.Qingtui/Script.ps1
+++ Tasks/CISDI.Qingtui/Script.ps1
@@ -1,33 +1,13 @@
-$Object1 = Invoke-RestMethod -Uri 'https://web.qingtui.com/api/client/web/update' -Headers @{
-  CLIENT = "QingTui/$($this.Status.Contains('New') ? '70002' : $this.LastState.Version);Windows NT 10.0, WOW64;PC;0"
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = Get-RedirectedUrl -Uri 'https://web.qingtui.com/team/update?ab=qt&client=win'
 }
 
 # Version
-$this.CurrentState.Version = $Object1.data.webVersions.Where({ $_.type -eq 'winpc' })[0].lastestVer
-
-# Installer
-$this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = $Object1.data.webVersions.Where({ $_.type -eq 'winpc' })[0].lastestVerUrl
-}
+$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
-    try {
-      # ReleaseNotes (zh-CN)
-      $this.CurrentState.Locale += [ordered]@{
-        Locale = 'zh-CN'
-        Key    = 'ReleaseNotes'
-        Value  = $Object1.data.webVersions.Where({ $_.type -eq 'winpc' })[0].lastestVerNote | Format-Text
-      }
-    } catch {
-      $_ | Out-Host
-      $this.Log($_, 'Warning')
-    }
-
-    $this.InstallerFiles[$this.CurrentState.Installer[0].InstallerUrl] = $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
-    # RealVersion
-    $this.CurrentState.RealVersion = $InstallerFile | Read-ProductVersionFromExe
-
     $this.Print()
     $this.Write()
   }
