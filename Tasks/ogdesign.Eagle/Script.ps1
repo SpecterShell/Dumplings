83c4fdbb54cc60ed84a935c532f7c2f60b17ea4c
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-06-13 00:19:00 +0800

    ogdesign.Eagle: Update

--- Tasks/ogdesign.Eagle/Script.ps1
+++ Tasks/ogdesign.Eagle/Script.ps1
@@ -1,4 +1,4 @@
-$Object1 = Invoke-RestMethod -Uri 'https://eagle.cool/check-for-update'
+$Object1 = Invoke-RestMethod -Uri 'https://core.eagle.cool/check-for-update'
 
 # Installer
 $InstallerUrl = 'https:' + $Object1.links.windows
@@ -16,22 +16,17 @@ $this.CurrentState.Version = "$($VersionMatches.Groups[1].Value)-$($VersionMatch
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
-    $this.InstallerFiles[$Installer.InstallerUrl] = $InstallerFile = Get-TempFile -Uri $Installer.InstallerUrl
-    # RealVersion
-    $this.CurrentState.RealVersion = $InstallerFile | Read-ProductVersionFromExe
-
     try {
-      $Object2 = Invoke-WebRequest -Uri 'https://eagle.cool/' | ConvertFrom-Html
-
       # ReleaseTime
-      $this.CurrentState.ReleaseTime = [regex]::Match(
-        $Object2.SelectSingleNode('//*[@id="hero"]/div/div/div/div/div[3]/text()[2]').InnerText,
-        '(\d{4}/\d{1,2}/\d{1,2})'
-      ).Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
+      $this.CurrentState.ReleaseTime = $Object1.publishedDate.ToUniversalTime()
     } catch {
       $_ | Out-Host
       $this.Log($_, 'Warning')
     }
+
+    $this.InstallerFiles[$Installer.InstallerUrl] = $InstallerFile = Get-TempFile -Uri $Installer.InstallerUrl
+    # RealVersion
+    $this.CurrentState.RealVersion = $InstallerFile | Read-ProductVersionFromExe
 
     $this.Print()
     $this.Write()
