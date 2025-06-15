7b1bd09d7989288ce32d74b9b5181e910e135c06
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-06-15 13:59:54 +0800

    Add Thorlabs.ThorlabsDeviceSDK

---
+++ Tasks/Thorlabs.ThorlabsDeviceSDK/Script.ps1
@@ -0,0 +1,52 @@
+$Object1 = Invoke-RestMethod -Uri 'https://www.thorlabs.com/software_pages/check_updates.cfm?ItemID=SC30'
+
+# Version
+$this.CurrentState.Version = $Object1.ItemID.SoftwarePkg.VersionNumber
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.ItemID.SoftwarePkg.DownloadLink
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.ItemID.SoftwarePkg.ReleaseDate | Get-Date -Format 'yyyy-MM-dd'
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    foreach ($Installer in $this.CurrentState.Installer) {
+      $this.InstallerFiles[$Installer.InstallerUrl] = $InstallerFile = Get-TempFile -Uri $Installer.InstallerUrl
+      $InstallerFileExtracted = New-TempFolder
+      7z.exe e -aoa -ba -bd -y -o"${InstallerFileExtracted}" $InstallerFile 'SC30_Software_Package\Components\TL_DeviceSDK\TL_DeviceSDK_Setup.exe' | Out-Host
+      $InstallerFile2 = Join-Path $InstallerFileExtracted 'TL_DeviceSDK_Setup.exe'
+      $InstallerFile2Extracted = $InstallerFile2 | Expand-InstallShield
+      $InstallerFile3 = Join-Path $InstallerFile2Extracted 'TL_DeviceSDK.msi'
+      # RealVersion
+      $this.CurrentState.RealVersion = $InstallerFile3 | Read-ProductVersionFromMsi
+      # ProductCode
+      $Installer['ProductCode'] = $InstallerFile3 | Read-ProductCodeFromMsi
+      # AppsAndFeaturesEntries
+      $Installer['AppsAndFeaturesEntries'] = @(
+        [ordered]@{
+          UpgradeCode   = $InstallerFile3 | Read-UpgradeCodeFromMsi
+          InstallerType = 'msi'
+        }
+      )
+      Remove-Item -Path $InstallerFile2Extracted -Recurse -Force -ErrorAction 'Continue' -ProgressAction 'SilentlyContinue'
+      Remove-Item -Path $InstallerFileExtracted -Recurse -Force -ErrorAction 'Continue' -ProgressAction 'SilentlyContinue'
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
