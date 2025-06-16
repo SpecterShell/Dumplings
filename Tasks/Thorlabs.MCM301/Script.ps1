103f1cea9c9d84b0b79cba8b3ea7b117cf85f3ee
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-06-16 11:06:35 +0800

    Add Thorlabs.MCM301

---
+++ Tasks/Thorlabs.MCM301/Script.ps1
@@ -0,0 +1,90 @@
+$Object1 = Invoke-RestMethod -Uri 'https://www.thorlabs.com/software_pages/check_updates.cfm?ItemID=MCM301'
+
+# Version
+$this.CurrentState.Version = $Object1.ItemID.SoftwarePkg.VersionNumber
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl         = $Object1.ItemID.SoftwarePkg.DownloadLink.Replace('\', '/')
+  NestedInstallerFiles = @(
+    [ordered]@{
+      RelativeFilePath = "70-0157-$($this.CurrentState.Version)_MCM301_Customer_UI.zip"
+    }
+  )
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.ItemID.SoftwarePkg.ReleaseDate | Get-Date -Format 'yyyy-MM-dd'
+
+      # LicenseUrl (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'LicenseUrl'
+        Value  = "https://www.thorlabs.com/Software/THO/MCM301/software/v$($this.CurrentState.Version)/License.zip"
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    try {
+      # ReleaseNotesUrl (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotesUrl'
+        Value  = $null
+      }
+
+      $ReleaseNotesUrl = $Object1.ItemID.SoftwarePkg.ChangeLog.Replace('\', '/')
+      $Object2 = [System.IO.StreamReader]::new((Invoke-WebRequest -Uri $ReleaseNotesUrl).RawContentStream)
+
+      # ReleaseNotesUrl (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotesUrl'
+        Value  = $ReleaseNotesUrl
+      }
+
+      while (-not $Object2.EndOfStream) {
+        $String = $Object2.ReadLine()
+        if ($String.Contains("Release V$($this.CurrentState.Version)")) {
+          break
+        }
+      }
+      if (-not $Object2.EndOfStream) {
+        $ReleaseNotesObjects = [System.Collections.Generic.List[string]]::new()
+        while (-not $Object2.EndOfStream) {
+          $String = $Object2.ReadLine()
+          if ($String -notmatch 'Release V\d+(\.\d+)+ ') {
+            $ReleaseNotesObjects.Add($String)
+          } else {
+            break
+          }
+        }
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesObjects | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
