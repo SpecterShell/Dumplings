e16ea0f51c3601349c4f6ebcabfabfc1d49bf0a6
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-06-16 11:24:50 +0800

    Add Thorlabs.Joystick

---
+++ Tasks/Thorlabs.Joystick/Script.ps1
@@ -0,0 +1,38 @@
+$Object1 = Invoke-RestMethod -Uri 'https://www.thorlabs.com/software_pages/check_updates.cfm?ItemID=MCMK3'
+
+# Version
+$this.CurrentState.Version = $Object1.ItemID.SoftwarePkg.VersionNumber
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.ItemID.SoftwarePkg.DownloadLink
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.ItemID.SoftwarePkg.ReleaseDate | Get-Date -Format 'yyyy-MM-dd'
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.InstallerFiles[$this.CurrentState.Installer[0].InstallerUrl] = $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl | Rename-Item -NewName { "${_}.exe" } -PassThru | Select-Object -ExpandProperty 'FullName'
+    # RealVersion
+    Start-ThreadJob -ScriptBlock {
+      winget install --id 'Microsoft.DotNet.DesktopRuntime.6' --source 'winget' --architecture 'x86' --exact --accept-package-agreements --accept-source-agreements --force | Out-Host
+      Start-Process -FilePath $using:InstallerFile -ArgumentList '/S' -Wait
+    } | Wait-Job -Timeout 300 | Receive-Job | Out-Host
+    $this.CurrentState.RealVersion = Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Joystick' -Name 'DisplayVersion'
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
