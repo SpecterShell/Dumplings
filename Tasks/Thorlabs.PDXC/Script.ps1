30a92402763b2baaeece14c591d208020162b93e
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-06-16 10:55:11 +0800

    Add Thorlabs.PDXC

---
+++ Tasks/Thorlabs.PDXC/Script.ps1
@@ -0,0 +1,42 @@
+$Object1 = Invoke-RestMethod -Uri 'https://www.thorlabs.com/software_pages/check_updates.cfm?ItemID=PDXC'
+
+# Version
+$this.CurrentState.Version = $Object1.ItemID.SoftwarePkg.VersionNumber
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl         = $InstallerUrl = $Object1.ItemID.SoftwarePkg.DownloadLink
+  NestedInstallerFiles = @(
+    [ordered]@{
+      RelativeFilePath = "$($InstallerUrl | Split-Path -LeafBase).exe"
+    }
+  )
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.ItemID.SoftwarePkg.ReleaseDate | Get-Date -Format 'yyyy-MM-dd'
+
+      # LicenseUrl (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'LicenseUrl'
+        Value  = "https://www.thorlabs.com/Software/THO/PDXC/v$($this.CurrentState.Version)/License.zip"
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
