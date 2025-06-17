ad657a4b69e6761d77cbaf81cd81b00dff25aedb
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-06-17 15:59:53 +0800

    Add PureDevSoftware.10x

---
+++ Tasks/PureDevSoftware.10x/Script.ps1
@@ -0,0 +1,48 @@
+$Object1 = Invoke-RestMethod -Uri 'https://10xeditor.com/update.php'
+
+# Version
+$this.CurrentState.Version = [regex]::Match($Object1, 'php_output:(\d+(?:\.\d+)+)').Groups[1].Value
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = "https://www.puredevsoftware.com/download.php?file=10x_prev_versions/10xInstaller_$($this.CurrentState.Version -replace '\.', '_').exe&type=exe"
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      $Object2 = Invoke-WebRequest -Uri 'https://10xeditor.com/versions.htm' | ConvertFrom-Html
+
+      $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//table[@class='release_version_table']//tr[contains(., 'Version ($($this.CurrentState.Version.Split('.')[0..2] -join '.'))')]")
+      if ($ReleaseNotesTitleNode) {
+        # ReleaseTime
+        $this.CurrentState.ReleaseTime = [datetime]::ParseExact(
+          [regex]::Match($ReleaseNotesTitleNode.InnerText, '(\d{1,2}/\d{1,2}/20\d{2})').Groups[1].Value,
+          'dd/MM/yyyy',
+          $null
+        ).ToString('yyyy-MM-dd')
+
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesTitleNode.SelectNodes('./following-sibling::node()') | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseTime and ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
