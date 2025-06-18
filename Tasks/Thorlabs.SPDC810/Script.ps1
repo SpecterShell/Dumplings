4a3b59d104fe0f00e11c1a92646fba4072ec4d30
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-06-18 17:06:54 +0800

    Add Thorlabs.SPDC810

---
+++ Tasks/Thorlabs.SPDC810/Script.ps1
@@ -0,0 +1,35 @@
+$Object1 = Invoke-RestMethod -Uri 'https://www.thorlabs.com/software_pages/check_updates.cfm?ItemID=SPDC810'
+
+# Version
+$this.CurrentState.Version = $Object1.ItemID.SoftwarePkg.VersionNumber
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl         = $InstallerUrl = $Object1.ItemID.SoftwarePkg.DownloadLink
+  NestedInstallerFiles = @(
+    [ordered]@{
+      RelativeFilePath = "$($InstallerUrl | Split-Path -LeafBase).exe"
+    }
+  )
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.ItemID.SoftwarePkg.ReleaseDate | Get-Date -Format 'yyyy-MM-dd'
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
