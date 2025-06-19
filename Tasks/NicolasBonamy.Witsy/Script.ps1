925dd799acd7667311a6d20f16c5c1ba10c2087e
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-06-19 13:51:33 +0800

    NicolasBonamy.Witsy: Add installer architecture

--- Tasks/NicolasBonamy.Witsy/Script.ps1
+++ Tasks/NicolasBonamy.Witsy/Script.ps1
@@ -8,6 +8,7 @@ $this.CurrentState.Version = $Object1.tag_name -creplace '^v'
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
   InstallerUrl = $Object1.assets.Where({ $_.name.Contains('.exe') -and $_.name.Contains('x64') -and $_.name.Contains('Setup') }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
 }
 
