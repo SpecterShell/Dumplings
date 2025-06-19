02dc2a9bb4938a4b7312db997738975efc16cdc6
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-06-19 18:48:55 +0800

    Bosyun.PixsoLocalFont: Parse prefix and prepend it to InstallerUrl

--- Tasks/Bosyun.PixsoLocalFont/Script.ps1
+++ Tasks/Bosyun.PixsoLocalFont/Script.ps1
@@ -1,15 +1,15 @@
-$Object1 = Invoke-WebRequest -Uri 'https://pixso.cn/download/' | ConvertFrom-Html
+$Object1 = Invoke-WebRequest -Uri 'https://pixso.cn/download/'
+$Object2 = $Object1 | ConvertFrom-Html
 
-# Version
-$this.CurrentState.Version = [regex]::Match(
-  $Object1.SelectSingleNode('//*[contains(@class, "apps-item") and contains(./div[3]/div[1]/text(), "本地字体助手")]/p[2]').InnerText,
-  '([\d\.]+)'
-).Groups[1].Value
+$Prefix = [regex]::Match($Object1.Content, 'window\.location\.href\s*=\s*"([^"]+)"\s*\+\s*_href').Groups[1].Value
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = Join-Uri 'https://cms-oss.pixso.cn' $Object1.SelectSingleNode('//*[contains(@class, "apps-item") and contains(./div[3]/div[1]/text(), "本地字体助手")]/div[1]').Attributes['data-href'].Value
+  InstallerUrl = Join-Uri $Prefix $Object2.SelectSingleNode('//*[contains(@class, "apps-item") and contains(., "本地字体助手")]//*[contains(@data-href, ".exe")]').Attributes['data-href'].Value
 }
+
+# Version
+$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
