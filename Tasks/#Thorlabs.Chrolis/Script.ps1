118026d336e2f515b61c7e818b9a47891bbe2eef
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-06-19 10:32:52 +0800

    Add Thorlabs.Chrolis

---
+++ Tasks/#Thorlabs.Chrolis/Script.ps1
@@ -0,0 +1,1 @@
+$Global:DumplingsStorage.ChrolisApps = Invoke-RestMethod -Uri 'https://www.thorlabs.com/software_pages/check_updates.cfm?ItemID=CHROLIS'
---
+++ Tasks/Thorlabs.Chrolis/Script.ps1
@@ -0,0 +1,45 @@
+$Object1 = $Global:DumplingsStorage.ChrolisApps
+
+# Version
+$this.CurrentState.Version = $Object1.ItemID.SoftwarePkg.VersionNumber
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl         = $InstallerUrl = $Object1.ItemID.SoftwarePkg.DownloadLink
+  NestedInstallerFiles = @(
+    [ordered]@{
+      RelativeFilePath = "$($InstallerUrl | Split-Path -LeafBase)\CHROLIS_Installer.exe"
+    }
+  )
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.ItemID.SoftwarePkg.ReleaseDate | Get-Date -Format 'yyyy-MM-dd'
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    foreach ($Installer in $this.CurrentState.Installer) {
+      $this.InstallerFiles[$Installer.InstallerUrl] = $InstallerFile = Get-TempFile -Uri $Installer.InstallerUrl
+      $InstallerFileExtracted = New-TempFolder
+      7z.exe e -aoa -ba -bd -y -o"${InstallerFileExtracted}" $InstallerFile $this.CurrentState.Installer[0].NestedInstallerFiles[0].RelativeFilePath | Out-Host
+      $InstallerFile2 = Join-Path $InstallerFileExtracted 'CHROLIS_Installer.exe'
+      # RealVersion
+      $this.CurrentState.RealVersion = $InstallerFile2 | Read-FileVersionFromExe
+      Remove-Item -Path $InstallerFileExtracted -Recurse -Force -ErrorAction 'Continue' -ProgressAction 'SilentlyContinue'
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
