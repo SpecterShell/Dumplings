d7a62e5949d1a6ef9a57ba9cc9071f1ed9152cd0
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-06-20 15:15:03 +0800

    Thorlabs.CCT: Add ReleaseNotes

--- Tasks/Thorlabs.CCT/Script.ps1
+++ Tasks/Thorlabs.CCT/Script.ps1
@@ -2,7 +2,7 @@ $Object1 = Invoke-RestMethod -Uri 'https://www.thorlabs.com/software_pages/check
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = $InstallerUrl = $Object1.ItemID.SoftwarePkg.DownloadLink
+  InstallerUrl = $Object1.ItemID.SoftwarePkg.DownloadLink
 }
 
 # Version
@@ -15,12 +15,50 @@ switch -Regex ($this.Check()) {
       $this.CurrentState.Locale += [ordered]@{
         Locale = 'en-US'
         Key    = 'ReleaseNotesUrl'
-        Value  = Join-Uri $InstallerUrl 'ChangeLog.txt'
+        Value  = $null
+      }
+
+      $ReleaseNotesUrl = $Object1.ItemID.SoftwarePkg.ChangeLog
+      $Object2 = [System.IO.StreamReader]::new((Invoke-WebRequest -Uri $ReleaseNotesUrl).RawContentStream)
+
+      # ReleaseNotesUrl (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotesUrl'
+        Value  = $ReleaseNotesUrl
+      }
+
+      while (-not $Object2.EndOfStream) {
+        $String = $Object2.ReadLine()
+        if ($String.Contains("v$($this.CurrentState.Version.Split('.')[0..1] -join '.')")) {
+          $null = $Object2.ReadLine() # Skip the next line
+          break
+        }
+      }
+      if (-not $Object2.EndOfStream) {
+        $ReleaseNotesObjects = [System.Collections.Generic.List[string]]::new()
+        while (-not $Object2.EndOfStream) {
+          $String = $Object2.ReadLine()
+          if ($String -notmatch 'v\d+(\.\d+)+') {
+            $ReleaseNotesObjects.Add($String)
+          } else {
+            break
+          }
+        }
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesObjects | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
       }
     } catch {
       $_ | Out-Host
       $this.Log($_, 'Warning')
     }
+
     $this.Print()
     $this.Write()
   }
