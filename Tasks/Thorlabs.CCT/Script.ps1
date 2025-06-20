6d03ee576b717d3ccc0b0b5ceb68f7f7e96b80e1
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-06-20 14:47:08 +0800

    Thorlabs.CCT: Add ReleaseNotesUrl

--- Tasks/Thorlabs.CCT/Script.ps1
+++ Tasks/Thorlabs.CCT/Script.ps1
@@ -2,7 +2,7 @@ $Object1 = Invoke-RestMethod -Uri 'https://www.thorlabs.com/software_pages/check
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = $Object1.ItemID.SoftwarePkg.DownloadLink
+  InstallerUrl = $InstallerUrl = $Object1.ItemID.SoftwarePkg.DownloadLink
 }
 
 # Version
@@ -10,6 +10,17 @@ $this.CurrentState.Version = [regex]::Matches($this.CurrentState.Installer[0].In
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
+    try {
+      # ReleaseNotesUrl (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotesUrl'
+        Value  = Join-Uri $InstallerUrl 'ChangeLog.txt'
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
     $this.Print()
     $this.Write()
   }
