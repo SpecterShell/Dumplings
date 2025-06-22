e543d059c34d857938210194c0a1612456332cb9
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-06-22 20:01:34 +0800

    Add Steady.Steady

---
+++ Tasks/Steady.Steady/Script.ps1
@@ -0,0 +1,32 @@
+$Object1 = Invoke-RestMethod -Uri 'https://download.todesktop.com/250228ns15hrp4z/td-latest.json'
+
+# Version
+$this.CurrentState.Version = $Object1.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture  = 'x64'
+  InstallerType = 'nullsoft'
+  InstallerUrl  = $Object1.artifacts.nsis.x64.url | ConvertTo-UnescapedUri
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.createdAt.ToUniversalTime()
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
