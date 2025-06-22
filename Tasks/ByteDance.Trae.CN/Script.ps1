8e68a4eb2d6324f4cc16548ca86fc7b5602cf2c5
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-06-22 12:42:31 +0800

    ByteDance.Trae: Switch to download source

--- Tasks/ByteDance.Trae.CN/Script.ps1
+++ Tasks/ByteDance.Trae.CN/Script.ps1
@@ -1,18 +1,19 @@
-$Object1 = Invoke-RestMethod -Uri 'https://tron.jiyunhudong.com/api/sdk/check_update?pid=7409949320595642651&branch=release_desktop_yoma_win32_cn&buildId=&uid='
+$Object1 = Invoke-RestMethod -Uri 'https://api.trae.ai/icube/api/v1/native/version/trae/cn/latest'
+# $Object1 = Invoke-RestMethod -Uri 'https://api.marscode.cn/icube/api/v1/native/version/trae/cn/latest'
 
 # Version
 $this.CurrentState.Version = $Object1.data.manifest.win32.version
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = $Object1.data.manifest.win32.urls.Where({ $_.region -eq 'va' }, 'First')[0].x64 | ConvertTo-UnescapedUri
+  InstallerUrl = $Object1.data.manifest.win32.download.Where({ $_.region -eq 'va' }, 'First')[0].x64 | ConvertTo-UnescapedUri
 }
 # $this.CurrentState.Installer += [ordered]@{
-#   InstallerUrl = $Object1.data.manifest.win32.urls.Where({ $_.region -eq 'sg' }, 'First')[0].x64 | ConvertTo-UnescapedUri
+#   InstallerUrl = $Object1.data.manifest.win32.download.Where({ $_.region -eq 'sg' }, 'First')[0].x64 | ConvertTo-UnescapedUri
 # }
 $this.CurrentState.Installer += [ordered]@{
   InstallerLocale = 'zh-CN'
-  InstallerUrl    = $Object1.data.manifest.win32.urls.Where({ $_.region -eq 'cn' }, 'First')[0].x64 | ConvertTo-UnescapedUri
+  InstallerUrl    = $Object1.data.manifest.win32.download.Where({ $_.region -eq 'cn' }, 'First')[0].x64 | ConvertTo-UnescapedUri
 }
 
 switch -Regex ($this.Check()) {
@@ -20,13 +21,6 @@ switch -Regex ($this.Check()) {
     try {
       # ReleaseTime
       $this.CurrentState.ReleaseTime = $Object1.data.manifest.win32.extra.uploadDate | ConvertFrom-UnixTimeMilliseconds
-
-      # ReleaseNotes (zh-CN)
-      $this.CurrentState.Locale += [ordered]@{
-        Locale = 'zh-CN'
-        Key    = 'ReleaseNotes'
-        Value  = $Object1.data.releaseNote | Format-Text
-      }
     } catch {
       $_ | Out-Host
       $this.Log($_, 'Warning')
--- Tasks/ByteDance.Trae/Script.ps1
+++ Tasks/ByteDance.Trae/Script.ps1
@@ -1,18 +1,19 @@
-$Object1 = Invoke-RestMethod -Uri 'https://tron.jiyunhudong.com/api/sdk/check_update?pid=7409949320595642651&branch=release_desktop_win32_i18n&buildId=&uid='
+$Object1 = Invoke-RestMethod -Uri 'https://api.trae.ai/icube/api/v1/native/version/trae/latest'
+# $Object1 = Invoke-RestMethod -Uri 'https://api.marscode.cn/icube/api/v1/native/version/trae/latest'
 
 # Version
 $this.CurrentState.Version = $Object1.data.manifest.win32.version
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = $Object1.data.manifest.win32.urls.Where({ $_.region -eq 'va' }, 'First')[0].x64
+  InstallerUrl = $Object1.data.manifest.win32.download.Where({ $_.region -eq 'va' }, 'First')[0].x64
 }
 # $this.CurrentState.Installer += [ordered]@{
-#   InstallerUrl = $Object1.data.manifest.win32.urls.Where({ $_.region -eq 'sg' }, 'First')[0].x64
+#   InstallerUrl = $Object1.data.manifest.win32.download.Where({ $_.region -eq 'sg' }, 'First')[0].x64
 # }
 $this.CurrentState.Installer += [ordered]@{
   InstallerLocale = 'zh-CN'
-  InstallerUrl    = $Object1.data.manifest.win32.urls.Where({ $_.region -eq 'cn' }, 'First')[0].x64
+  InstallerUrl    = $Object1.data.manifest.win32.download.Where({ $_.region -eq 'cn' }, 'First')[0].x64
 }
 
 switch -Regex ($this.Check()) {
@@ -20,13 +21,6 @@ switch -Regex ($this.Check()) {
     try {
       # ReleaseTime
       $this.CurrentState.ReleaseTime = $Object1.data.manifest.win32.extra.uploadDate | ConvertFrom-UnixTimeMilliseconds
-
-      # ReleaseNotes (en-US)
-      $this.CurrentState.Locale += [ordered]@{
-        Locale = 'en-US'
-        Key    = 'ReleaseNotes'
-        Value  = $Object1.data.releaseNote | Format-Text
-      }
     } catch {
       $_ | Out-Host
       $this.Log($_, 'Warning')
