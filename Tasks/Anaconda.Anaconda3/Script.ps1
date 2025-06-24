f023a82eac553c53816bce7bc92feff029f25d91
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-06-24 10:44:50 +0800

    Anaconda: Update release notes source

--- Tasks/Anaconda.Anaconda3/Script.ps1
+++ Tasks/Anaconda.Anaconda3/Script.ps1
@@ -25,35 +25,35 @@ switch -Regex ($this.Check()) {
     ).Groups[1].Value
     $this.CurrentState.Installer[0]['ProductCode'] = "Anaconda3 $($this.CurrentState.Version) (Python ${PythonVersion} 64-bit)"
 
-    # ReleaseNotesUrl
-    $this.CurrentState.Locale += [ordered]@{
-      Key   = 'ReleaseNotesUrl'
-      Value = $ReleaseNotesUrl = 'https://docs.anaconda.com/free/anaconda/release-notes/'
-    }
-
     try {
+      # ReleaseNotesUrl
+      $this.CurrentState.Locale += [ordered]@{
+        Key   = 'ReleaseNotesUrl'
+        Value = $ReleaseNotesUrl = 'https://www.anaconda.com/docs/getting-started/anaconda/release-notes'
+      }
+
       $Object2 = Invoke-WebRequest -Uri $ReleaseNotesUrl | ConvertFrom-Html
 
-      $ReleaseNotesNode = $Object2.SelectSingleNode("//section[@id='anaconda-release-notes']/section[contains(./h2/text(), '$($this.CurrentState.Version)')]")
-      if ($ReleaseNotesNode) {
+      $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//div[@id='anaconda-$($this.CurrentState.Version.Replace('.', '-'))']")
+      if ($ReleaseNotesTitleNode) {
         # ReleaseTime
-        $this.CurrentState.ReleaseTime = [regex]::Match($ReleaseNotesNode.SelectSingleNode('./h2/text()').InnerText, '\(([a-zA-Z]+\W+\d{1,2}\W+\d{4})').Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
+        $this.CurrentState.ReleaseTime = [regex]::Match($ReleaseNotesTitleNode.SelectSingleNode('./div[1]').InnerText, '([a-zA-Z]+\W+\d{1,2}\W+20\d{2})').Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
 
         # Remove link marks
-        $ReleaseNotesNode.SelectNodes('.//a[@class="headerlink"]').ForEach({ $_.Remove() })
+        $ReleaseNotesTitleNode.SelectNodes('.//a[starts-with(@href, "#")]').ForEach({ $_.Remove() })
         # Remove "Packages"
-        $ReleaseNotesNode.SelectNodes('.//details').ForEach({ $_.Remove() })
+        $ReleaseNotesTitleNode.SelectNodes('.//div[contains(@class, "accordion")]').ForEach({ $_.Remove() })
         # ReleaseNotes (en-US)
         $this.CurrentState.Locale += [ordered]@{
           Locale = 'en-US'
           Key    = 'ReleaseNotes'
-          Value  = $ReleaseNotesNode.SelectNodes('./h2/following-sibling::node()') | Get-TextContent | Format-Text
+          Value  = $ReleaseNotesTitleNode.SelectNodes('./div[2]') | Get-TextContent | Format-Text
         }
 
         # ReleaseNotesUrl
         $this.CurrentState.Locale += [ordered]@{
           Key   = 'ReleaseNotesUrl'
-          Value = $ReleaseNotesUrl + '#' + ($ReleaseNotesNode.SelectSingleNode('./h2/text()').InnerText.ToLower() -creplace '[^a-zA-Z0-9]+', '-').Trim('-')
+          Value = $ReleaseNotesUrl + '#' + $ReleaseNotesTitleNode.Attributes['id'].Value
         }
       } else {
         $this.Log("No ReleaseTime, ReleaseNotes (en-US) and ReleaseNotesUrl for version $($this.CurrentState.Version)", 'Warning')
