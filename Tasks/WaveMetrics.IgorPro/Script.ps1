fb46da8047306370c930cad70659fd88066b0a6e
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-06-26 14:05:22 +0800

    WaveMetrics.IgorPro: Rewrite

--- Tasks/WaveMetrics.IgorPro/Script.ps1
+++ Tasks/WaveMetrics.IgorPro/Script.ps1
@@ -3,6 +3,10 @@ $Object1 = Invoke-RestMethod -Uri 'https://updates.wavemetrics.com/Updaters/PHP/
 # ja
 $Object2 = Invoke-RestMethod -Uri 'https://updates.wavemetrics.com/Updaters/PHP/igor9updatecheck.php?language=ja&wantBeta=0' | ConvertFrom-Ini
 
+if ($Object1.Contains('IgorExtraInfo') -and -not $Object1.IgorExtraInfo.InfoKeys.Contains('Beta')) {
+  $this.Log('Next major version available', 'Warning')
+}
+
 if ($Object1.main.Version -ne $Object2.main.Version) {
   $this.Log("en version: $($Object1.main.Version)")
   $this.Log("ja version: $($Object2.main.Version)")
@@ -16,14 +20,29 @@ $this.CurrentState.Version = $Object1.main.Version
 $this.CurrentState.Installer += [ordered]@{
   InstallerLocale = 'en'
   InstallerUrl    = $Object1.main.InstallerPath
+  ProductCode     = $Object1.main.ProductCode
 }
 $this.CurrentState.Installer += [ordered]@{
-  InstallerLocale = 'ja'
-  InstallerUrl    = $Object2.main.InstallerPath
+  InstallerLocale        = 'ja'
+  InstallerUrl           = $Object2.main.InstallerPath
+  ProductCode            = $Object2.main.ProductCode
+  AppsAndFeaturesEntries = @(
+    [ordered]@{
+      DisplayName = "$($Object2.main.DisplayName) $($this.CurrentState.Version)"
+    }
+  )
 }
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = [regex]::Match($Object1.main.DisplayVersion, '(20\d{2}-\d{1,2}-\d{1,2})').Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
     try {
       # ReleaseNotesUrl (en-US)
       $this.CurrentState.Locale += [ordered]@{
