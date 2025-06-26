04ef34780e682520cd5a9d9cf9b734c43b2f14f0
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-06-26 19:19:28 +0800

    Add Tenable.Nessus

---
+++ Tasks/Tenable.Nessus/Script.ps1
@@ -0,0 +1,79 @@
+$Object1 = (Invoke-WebRequest -Uri 'https://www.tenable.com/downloads/api/v2/pages/nessus').Content | ConvertFrom-Json -AsHashtable
+$Object2 = $Object1.releases[$Object1.releases.latest.Keys[0]]
+# x86
+$Object3 = $Object2.Where({ $_.os -eq 'Windows' -and $_.arch -eq 'i386' -and -not $_.file_url.Contains('latest') }, 'First')[0]
+# x64
+$Object4 = $Object2.Where({ $_.os -eq 'Windows' -and $_.arch -eq 'x86_64' -and -not $_.file_url.Contains('latest') }, 'First')[0]
+
+if ($Object3.version -ne $Object4.version) {
+  $this.Log("x86 version: $($Object3.version)")
+  $this.Log("x64 version: $($Object4.version)")
+  throw 'Inconsistent versions detected'
+}
+
+# Version
+$this.CurrentState.Version = $Object3.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x86'
+  InstallerUrl = $Object3.file_url
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = $Object4.file_url
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.InstallerFiles[$this.CurrentState.Installer[0].InstallerUrl] = $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+    # RealVersion
+    $this.CurrentState.RealVersion = $InstallerFile | Read-ProductVersionFromMsi
+
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object4.product_release_date.ToUniversalTime()
+
+      # ReleaseNotesUrl (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotesUrl'
+        Value  = 'https://docs.tenable.com/release-notes/Content/nessus/nessus.htm'
+      }
+
+      $ReleaseNotesUrl = "https://docs.tenable.com/release-notes/Content/nessus/$($this.CurrentState.ReleaseTime.Year).htm"
+      $Object5 = Invoke-WebRequest -Uri $ReleaseNotesUrl | ConvertFrom-Html
+      $ReleaseNotesTitleNode = $Object5.SelectSingleNode("//h2[contains(., 'Tenable Nessus $($this.CurrentState.Version)')]")
+      if ($ReleaseNotesTitleNode) {
+        # ReleaseNotesUrl (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotesUrl'
+          Value  = $ReleaseNotesUrl
+        }
+
+        # ReleaseNotes (en-US)
+        $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node -and $Node.Name -ne 'h2'; $Node = $Node.NextSibling) { $Node }
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
