6423311aa296e9cd90ddcfc4d4c0f5c31d3207c8
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-06-28 19:49:43 +0800

    Icecat.Icecat: Parse version from installer, reset

--- Tasks/Icecat.Icecat/Script.ps1
+++ Tasks/Icecat.Icecat/Script.ps1
@@ -1,17 +1,29 @@
 $Object1 = Invoke-WebRequest -Uri 'https://icecatbrowser.org/all_downloads.html'
 
+$InstallerX64Url = $Object1.Links.Where({ try { $_.href.EndsWith('.exe') -and $_.href.Contains('win64') -and -not $_.href.Contains('aarch64') } catch {} }, 'First')[0].href
+$VersionX64 = [regex]::Match($InstallerX64Url, '(\d+(?:\.\d+)+)').Groups[1].Value
+
+$InstallerArm64Url = $Object1.Links.Where({ try { $_.href.EndsWith('.exe') -and $_.href.Contains('aarch64') } catch {} }, 'First')[0].href
+$VersionArm64 = [regex]::Match($InstallerArm64Url, '(\d+(?:\.\d+)+)').Groups[1].Value
+
+if ($VersionX64 -ne $VersionArm64) {
+  $this.Log("x64 version: ${VersionX64}")
+  $this.Log("ARM64 version: ${VersionArm64}")
+  throw 'Inconsistent versions detected'
+}
+
 # Version
-$this.CurrentState.Version = [regex]::Match($Object1.Content, 'Icecat Version:(\d+(?:\.\d+)+)').Groups[1].Value
+$this.CurrentState.Version = $VersionX64
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
   Architecture = 'x64'
-  InstallerUrl = "https://icecatbrowser.org/assets/icecat/$($this.CurrentState.Version)/icecat-$($this.CurrentState.Version).en-US.win64.installer.exe"
+  InstallerUrl = $InstallerX64Url
   ProductCode  = "IceCat $($this.CurrentState.Version) (x64 en-US)"
 }
 $this.CurrentState.Installer += [ordered]@{
   Architecture = 'arm64'
-  InstallerUrl = "https://icecatbrowser.org/assets/icecat/$($this.CurrentState.Version)/icecat-$($this.CurrentState.Version).en-US.win64-aarch64.installer.exe"
+  InstallerUrl = $InstallerArm64Url
   ProductCode  = "IceCat $($this.CurrentState.Version) (AArch64 en-US)"
 }
 
