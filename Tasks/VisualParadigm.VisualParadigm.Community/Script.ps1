a106efa24a6031c0e2951ec1f5e160f8e5c8f347
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-06-29 15:29:02 +0800

    Add VisualParadigm

---
+++ Tasks/VisualParadigm.VisualParadigm.Community/Script.ps1
@@ -0,0 +1,66 @@
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = 'https://www.visual-paradigm.com/downloads/vpce/Visual_Paradigm_CE_Win64.exe'
+}
+
+$VersionMatches = [regex]::Match((Get-RedirectedUrl -Uri $this.CurrentState.Installer[0].InstallerUrl), '/vpce(\d+(?:\.\d+)+)/(\d+)/')
+# Version
+$this.CurrentState.Version = "$($VersionMatches.Groups[1].Value).$($VersionMatches.Groups[2].Value)"
+# RealVersion
+$this.CurrentState.RealVersion = $VersionMatches.Groups[1].Value
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = [datetime]::ParseExact($VersionMatches.Groups[2].Value, 'yyyyMMdd', $null).ToString('yyyy-MM-dd')
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    try {
+      # ReleaseNotesUrl
+      $this.CurrentState.Locale += [ordered]@{
+        Key   = 'ReleaseNotesUrl'
+        Value = $null
+      }
+
+      $ReleaseNotesUrl = "https://www.visual-paradigm.com/aboutus/newsreleases/vp$($VersionMatches.Groups[1].Value.Replace('.', '')).jsp"
+
+      $Object1 = Invoke-WebRequest -Uri $ReleaseNotesUrl | ConvertFrom-Html
+
+      # ReleaseNotesUrl (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotesUrl'
+        Value  = $ReleaseNotesUrl
+      }
+      # ReleaseNotesUrl (zh-CN)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'zh-CN'
+        Key    = 'ReleaseNotesUrl'
+        Value  = "https://www.visual-paradigm.com/cn/aboutus/newsreleases/vp$($VersionMatches.Groups[1].Value.Replace('.', '')).jsp"
+      }
+
+      # ReleaseNotes (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotes'
+        Value  = $Object1.SelectSingleNode('//td[@class="content"]') | Get-TextContent | Format-Text
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
---
+++ Tasks/VisualParadigm.VisualParadigm/Script.ps1
@@ -0,0 +1,66 @@
+$VersionMatches = [regex]::Match((Get-RedirectedUrl -Uri 'https://www.visual-paradigm.com/downloads/vp/Visual_Paradigm_Win64.exe'), '/vp(\d+(?:\.\d+)+)/(\d+)/')
+# Version
+$this.CurrentState.Version = "$($VersionMatches.Groups[1].Value).$($VersionMatches.Groups[2].Value)"
+# RealVersion
+$this.CurrentState.RealVersion = $VersionMatches.Groups[1].Value
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = "https://www.visual-paradigm.com/downloads/vp$($VersionMatches.Groups[1].Value)/Visual_Paradigm_Win64.exe"
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = [datetime]::ParseExact($VersionMatches.Groups[2].Value, 'yyyyMMdd', $null).ToString('yyyy-MM-dd')
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    try {
+      # ReleaseNotesUrl
+      $this.CurrentState.Locale += [ordered]@{
+        Key   = 'ReleaseNotesUrl'
+        Value = $null
+      }
+
+      $ReleaseNotesUrl = "https://www.visual-paradigm.com/aboutus/newsreleases/vp$($VersionMatches.Groups[1].Value.Replace('.', '')).jsp"
+
+      $Object1 = Invoke-WebRequest -Uri $ReleaseNotesUrl | ConvertFrom-Html
+
+      # ReleaseNotesUrl (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotesUrl'
+        Value  = $ReleaseNotesUrl
+      }
+      # ReleaseNotesUrl (zh-CN)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'zh-CN'
+        Key    = 'ReleaseNotesUrl'
+        Value  = "https://www.visual-paradigm.com/cn/aboutus/newsreleases/vp$($VersionMatches.Groups[1].Value.Replace('.', '')).jsp"
+      }
+
+      # ReleaseNotes (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotes'
+        Value  = $Object1.SelectSingleNode('//td[@class="content"]') | Get-TextContent | Format-Text
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
