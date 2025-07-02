a69bc941b604abbda94e41043011edc836914c49
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-07-02 13:33:40 +0800

    ZWSOFT: Filter products in query

--- Tasks/#ZWSOFT/Script.ps1
+++ Tasks/#ZWSOFT/Script.ps1
@@ -1,1 +1,5 @@
-$Global:DumplingsStorage.ZWSOFTApps = Invoke-RestMethod -Uri 'https://www.zwsoft.cn/index.php?g=Api&m=Common&a=getDownloadCenterProduct'
+$Global:DumplingsStorage.ZWSOFTApps = Invoke-RestMethod -Uri 'https://www.zwsoft.cn/index.php?g=Api&m=Common&a=getDownloadCenterProduct' -Method Post -Body @{
+  environment    = 'pc'
+  system         = 'Windows'
+  page           = '1'
+}
