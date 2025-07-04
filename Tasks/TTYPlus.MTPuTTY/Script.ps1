650a24cebb1f226ebdefc31cd2e789e655f785ec
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-07-04 14:34:25 +0800

    Add TTYPlus.MTPuTTY

---
+++ Tasks/TTYPlus.MTPuTTY/Script.ps1
@@ -0,0 +1,134 @@
+function Read-Installer {
+  $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+  # Version
+  $this.CurrentState.Version = $InstallerFile | Read-ProductVersionFromExe
+  # InstallerSha256
+  $this.CurrentState.Installer[0]['InstallerSha256'] = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
+  Remove-Item -Path $InstallerFile -Recurse -Force -ErrorAction 'Continue' -ProgressAction 'SilentlyContinue'
+}
+
+function Get-ReleaseNotes {
+  try {
+    $Object2 = Invoke-WebRequest -Uri 'https://ttyplus.com/multi-tabbed-putty/changelog/' | ConvertFrom-Html
+    $Object3 = [System.IO.StringReader]::new($Object2.SelectSingleNode('//textarea').InnerText)
+
+    while ($Object3.Peek() -ne -1) {
+      $String = $Object3.ReadLine()
+      if ($String -match "^Ver $([regex]::Escape($this.CurrentState.Version))") {
+        $null = $Object3.ReadLine()
+        break
+      }
+    }
+    if ($Object3.Peek() -ne -1) {
+      $ReleaseNotesObjects = [System.Collections.Generic.List[string]]::new()
+      while ($Object3.Peek() -ne -1) {
+        $String = $Object3.ReadLine()
+        if ($String -notmatch '^Ver \d+(\.\d+)+$') {
+          $ReleaseNotesObjects.Add($String)
+        } else {
+          break
+        }
+      }
+      # ReleaseNotes (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotes'
+        Value  = $ReleaseNotesObjects | Format-Text
+      }
+    } else {
+      $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+    }
+
+    $Object3.Close()
+  } catch {
+    $_ | Out-Host
+    $this.Log($_, 'Warning')
+  }
+}
+
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = 'https://ttyplus.com/download/mtputty_setup.exe'
+}
+
+$Object1 = Invoke-WebRequest -Uri $this.CurrentState.Installer[0].InstallerUrl -Method Head
+$ETag = $Object1.Headers.ETag[0]
+
+# Case 0: Force submit the manifest
+if ($Global:DumplingsPreference.Contains('Force')) {
+  $this.Log('Skip checking states', 'Info')
+
+  # ETag
+  $this.CurrentState.ETag = @($ETag)
+
+  Read-Installer
+  Get-ReleaseNotes
+
+  $this.Print()
+  $this.Write()
+  $this.Message()
+  $this.Submit()
+  return
+}
+
+# Case 1: The task is new
+if ($this.Status.Contains('New')) {
+  $this.Log('New task', 'Info')
+
+  # ETag
+  $this.CurrentState.ETag = @($ETag)
+
+  Read-Installer
+  Get-ReleaseNotes
+
+  $this.Print()
+  $this.Write()
+  return
+}
+
+# Case 2: The ETag is unchanged
+if ($ETag -in $this.LastState.ETag) {
+  $this.Log("The version $($this.LastState.Version) from the last state is the latest (Global)", 'Info')
+  return
+}
+
+Read-Installer
+
+# Case 3: The current state has an invalid version
+if ([string]::IsNullOrWhiteSpace($this.CurrentState.Version)) {
+  throw 'The current state has an invalid version'
+}
+
+Get-ReleaseNotes
+
+# Case 4: The ETag has changed, but the SHA256 is not
+if ($this.CurrentState.Installer[0].InstallerSha256 -eq $this.LastState.Installer[0].InstallerSha256) {
+  $this.Log('The ETag has changed, but the SHA256 is not', 'Info')
+
+  # ETag
+  $this.CurrentState.ETag = $this.LastState.ETag + $ETag
+
+  $this.Write()
+  return
+}
+
+# ETag
+$this.CurrentState.ETag = @($ETag)
+
+switch -Regex ($this.Check()) {
+  # Case 6: The ETag, the SHA256 and the version have changed
+  'Updated|Rollbacked' {
+    $this.Print()
+    $this.Write()
+    $this.Message()
+    $this.Submit()
+  }
+  # Case 5: The ETag and the SHA256 have changed, but the version is not
+  default {
+    $this.Log('The ETag and the SHA256 have changed, but the version is not', 'Info')
+    $this.Config.IgnorePRCheck = $true
+    $this.Print()
+    $this.Write()
+    $this.Message()
+    $this.Submit()
+  }
+}
