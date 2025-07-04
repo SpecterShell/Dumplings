6a69f429fd91b2bf10444c65c4ffd7721e5992b6
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-07-04 14:10:12 +0800

    Add Vivi.Vivi

---
+++ Tasks/Vivi.Vivi/Script.ps1
@@ -0,0 +1,67 @@
+# Installer
+$this.CurrentState.Installer += $InstallerX86 = [ordered]@{
+  Architecture = 'x86'
+  InstallerUrl = Get-RedirectedUrl -Uri 'https://api.vivi.io/windows-msi'
+}
+$VersionX86 = [regex]::Match($InstallerX86.InstallerUrl, '(\d+(\.\d+)+)').Groups[1].Value
+
+$this.CurrentState.Installer += $InstallerX64 = [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = Get-RedirectedUrl -Uri 'https://api.vivi.io/windows-msi64'
+}
+$VersionX64 = [regex]::Match($InstallerX64.InstallerUrl, '(\d+(\.\d+)+)').Groups[1].Value
+
+if ($VersionX86 -ne $VersionX64) {
+  $this.Log("x86 version: ${VersionX86}")
+  $this.Log("x64 version: ${VersionX64}")
+  throw 'Inconsistent versions detected'
+}
+
+# Version
+$this.CurrentState.Version = $VersionX64
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseNotesUrl (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotesUrl'
+        Value  = $ReleaseNotesUrl = 'https://vivi.atlassian.net/wiki/spaces/VRB/overview'
+      }
+
+      $Object1 = Invoke-WebRequest -Uri $ReleaseNotesUrl
+      if ($ReleaseNotesUrlLink = $Object1.Links.Where({ try { $_.href.Contains("App+Release+$($this.CurrentState.Version)") } catch {} }, 'First')) {
+        # ReleaseNotesUrl (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotesUrl'
+          Value  = $ReleaseNotesUrl = Join-Uri $ReleaseNotesUrl $ReleaseNotesUrlLink[0].href
+        }
+
+        $Object2 = Invoke-WebRequest -Uri $ReleaseNotesUrl | ConvertFrom-Html
+
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $Object2.SelectSingleNode('//div[@class="ak-renderer-document"]') | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotesUrl (en-US) and ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
