e9bf04841eebc27d3a9b53373d5182c3c30a80a6
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-07-06 18:01:57 +0800

    Add PaperCutSoftware.MobilityPrint

---
+++ Tasks/PaperCutSoftware.MobilityPrint/Script.ps1
@@ -0,0 +1,24 @@
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $InstallerUrl = Get-RedirectedUrl -Uri 'https://www.papercut.com/api/product/mobility-print/latest/server/windows/'
+}
+
+# Version
+$this.CurrentState.Version = [regex]::Match($InstallerUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.InstallerFiles[$this.CurrentState.Installer[0].InstallerUrl] = $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+    # RealVersion
+    $this.CurrentState.RealVersion = $InstallerFile | Read-ProductVersionFromExe
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
