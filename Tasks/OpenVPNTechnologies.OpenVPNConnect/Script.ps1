cd269becc3481c01b2e4a000d62a36b4b220a55e
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-07-08 01:01:49 +0800

    OpenVPNTechnologies.OpenVPNConnect: Return if the source does not exist

--- Tasks/OpenVPNTechnologies.OpenVPNConnect/Script.ps1
+++ Tasks/OpenVPNTechnologies.OpenVPNConnect/Script.ps1
@@ -1,17 +1,24 @@
-$Object1 = (Invoke-WebRequest -Uri "https://packages.openvpn.net/connect/v3/updates/$($this.Status.Contains('New') ? '3.6.0' : $this.LastState.Version)/MSI.txt") -replace '(?s)(?<=</SoftwareUpdate>).+' | ConvertFrom-Xml
+$Object1 = Invoke-WebRequest -Uri "https://packages.openvpn.net/connect/v3/updates/$($this.Status.Contains('New') ? '3.6.0' : $this.LastState.Version)/MSI.txt" -MaximumRetryCount 0 -SkipHttpErrorCheck
+
+if ($Object1.StatusCode -ne 200) {
+  $this.Log("The version $($this.LastState.Version) from the last state is the latest, skip checking", 'Info')
+  return
+}
+
+$Object2 = $Object1.Content -replace '(?s)(?<=</SoftwareUpdate>).+' | ConvertFrom-Xml
 
 # Version
-$this.CurrentState.Version = $Object1.SoftwareUpdate.Version
+$this.CurrentState.Version = $Object2.SoftwareUpdate.Version
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
   Architecture = 'x86'
-  InstallerUrl = $Object1.SoftwareUpdate.ContentURLx86
+  InstallerUrl = $Object2.SoftwareUpdate.ContentURLx86
 }
 # x64
 $this.CurrentState.Installer += [ordered]@{
   Architecture = 'x64'
-  InstallerUrl = $Object1.SoftwareUpdate.ContentURL
+  InstallerUrl = $Object2.SoftwareUpdate.ContentURL
 }
 
 switch -Regex ($this.Check()) {
@@ -21,7 +28,7 @@ switch -Regex ($this.Check()) {
       $this.CurrentState.Locale += [ordered]@{
         Locale = 'en-US'
         Key    = 'ReleaseNotes'
-        Value  = '"' + $Object1.SoftwareUpdate.ReleaseNotes + '"' | ConvertFrom-Json | Format-Text
+        Value  = '"' + $Object2.SoftwareUpdate.ReleaseNotes + '"' | ConvertFrom-Json | Format-Text
       }
     } catch {
       $_ | Out-Host
