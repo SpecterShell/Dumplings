442783d89b36c5ad8b9aaf48cf18ad10e967a042
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-07-07 13:37:27 +0800

    Add Makeblock.xToolCreativeSpace

---
+++ Tasks/Makeblock.xToolCreativeSpace/Script.ps1
@@ -0,0 +1,58 @@
+$Object1 = (
+  Invoke-WebRequest -Uri 'https://api.xtool.com/efficacy/v1/package/version/latest' -Method Post -Body (
+    @{
+      domain         = 'xcs'
+      region         = 'en'
+      contentId      = 'xcs_win'
+      contentVersion = $this.Status.Contains('New') ? '2.7.21' : $this.LastState.Version
+      extraFilter    = @()
+    } | ConvertTo-Json -Compress
+  ) -ContentType 'application/json'
+).Content | ConvertFrom-Json -AsHashtable
+
+if ($Object1.data.Count -eq 0) {
+  $this.Log("The version $($this.LastState.Version) from the last state is the latest, skip checking", 'Info')
+  return
+}
+
+# Version
+$this.CurrentState.Version = $Object1.data.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.data.contents[0].url
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.data.publishTime | ConvertFrom-UnixTimeSeconds
+
+      # ReleaseNotes (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotes'
+        Value  = $Object1.data.description.en | Format-Text
+      }
+      # ReleaseNotes (zh-CN)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'zh-CN'
+        Key    = 'ReleaseNotes'
+        Value  = $Object1.data.description.zh | Format-Text
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
