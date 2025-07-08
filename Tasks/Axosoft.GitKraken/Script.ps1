f79c5e2dc22bbb424da088d2362708071b697c8f
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-07-08 20:01:30 +0800

    Axosoft.GitKraken: Update source

--- Tasks/Axosoft.GitKraken/Script.ps1
+++ Tasks/Axosoft.GitKraken/Script.ps1
@@ -1,11 +1,12 @@
-$Object1 = Invoke-WebRequest -Uri 'https://release.axocdn.com/windows/RELEASES' | Read-ResponseContent | ConvertFrom-SquirrelReleases | Where-Object -FilterScript { -not $_.IsDelta } | Sort-Object -Property { $_.Version -creplace '\d+', { $_.Value.PadLeft(20) } } -Bottom 1
+$Object1 = Invoke-WebRequest -Uri 'https://release.gitkraken.com/windows/RELEASES' | Read-ResponseContent | ConvertFrom-SquirrelReleases | Where-Object -FilterScript { -not $_.IsDelta } | Sort-Object -Property { $_.Version -creplace '\d+', { $_.Value.PadLeft(20) } } -Bottom 1
 
 # Version
 $this.CurrentState.Version = $Object1.Version
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = "https://release.axocdn.com/windows/GitKrakenSetup-$($this.CurrentState.Version).exe"
+  Architecture = 'x64'
+  InstallerUrl = "https://api.gitkraken.dev/releases/production/windows/x64/$($this.CurrentState.Version)/GitKrakenSetup.exe"
 }
 
 switch -Regex ($this.Check()) {
