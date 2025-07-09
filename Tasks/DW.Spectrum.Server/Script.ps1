136fae1d00903443a826edde3d14a9b768031ed8
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-07-09 10:53:23 +0800

    DW: Specify release notes page encoding, remove unnecessary nodes, exclude password from release notes

--- Tasks/DW.Spectrum.Server/Script.ps1
+++ Tasks/DW.Spectrum.Server/Script.ps1
@@ -27,14 +27,21 @@ switch -Regex ($this.Check()) {
         Value  = $ReleaseNotesUrl = $Object1.releaseNotes
       }
 
-      $Object2 = Invoke-WebRequest -Uri $ReleaseNotesUrl | ConvertFrom-Html
+      $Object2 = Invoke-WebRequest -Uri $ReleaseNotesUrl | Read-ResponseContent -Encoding 'windows-1252' | ConvertFrom-Html
 
       # Replace all Wingdings characters with their Unicode equivalents
       $Object2.SelectNodes('//span[contains(@style, "font-family:Wingdings")]').ForEach({ if ($_.InnerText -match 'l|o') { $_.InnerHtml = '<span>-</span>' } })
-      $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//p[contains(., 'DW Spectrum Release note for $($this.CurrentState.Version)')]")
+      # Remove o:p nodes
+      $Object2.SelectNodes('//*[local-name() = "o:p"]').ForEach({ $_.Remove() })
+      $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//div[@class='WordSection1']/*[contains(., 'DW Spectrum Release note for $($this.CurrentState.Version)')]")
       if ($ReleaseNotesTitleNode) {
         # ReleaseNotes (en-US)
-        $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node -and -not $Node.InnerText.Contains('DW Spectrum Release note for '); $Node = $Node.NextSibling) { $Node }
+        $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node -and $Node.InnerText -notmatch 'DW Spectrum Release note for '; $Node = $Node.NextSibling) {
+          # Skip nodes that contain 'Password:' in their text
+          if (-not $Node.InnerText.Contains('Password:')) {
+            $Node
+          }
+        }
         $this.CurrentState.Locale += [ordered]@{
           Locale = 'en-US'
           Key    = 'ReleaseNotes'
