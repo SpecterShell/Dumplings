10cfe0cdc46ff419d2d7f37a7371b7ab79a1b194
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-07-09 10:24:19 +0800

    LobeHub.LobeHub: Also capture h2 nodes of release notes

--- Tasks/LobeHub.LobeHub/Script.ps1
+++ Tasks/LobeHub.LobeHub/Script.ps1
@@ -19,7 +19,7 @@ switch -Regex ($this.Check()) {
 
       if (-not [string]::IsNullOrWhiteSpace($Object1.body)) {
         $ReleaseNotesObject = $Object1.body | Convert-MarkdownToHtml -Extensions 'advanced', 'emojis', 'hardlinebreak'
-        $ReleaseNotesTitleNode = $ReleaseNotesObject.SelectSingleNode("./h3[contains(., '$($this.CurrentState.Version)')]")
+        $ReleaseNotesTitleNode = $ReleaseNotesObject.SelectSingleNode("./*[self::h2 or self::h3][contains(., '$($this.CurrentState.Version)')]")
         if ($ReleaseNotesTitleNode) {
           $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node -and $Node.Name -ne 'details'; $Node = $Node.NextSibling) { $Node }
           # ReleaseNotes (en-US)
@@ -29,7 +29,12 @@ switch -Regex ($this.Check()) {
             Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
           }
         } else {
-          $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+          # ReleaseNotes (en-US)
+          $this.CurrentState.Locale += [ordered]@{
+            Locale = 'en-US'
+            Key    = 'ReleaseNotes'
+            Value  = $ReleaseNotesObject | Get-TextContent | Format-Text
+          }
         }
       } else {
         $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
