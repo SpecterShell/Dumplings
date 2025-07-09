ea888c27f2f26a12aa6ff27eeaf584a51c342ddb
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-07-09 14:47:03 +0800

    Add Adobe.DNGConverter

---
+++ Tasks/Adobe.DNGConverter/Script.ps1
@@ -0,0 +1,34 @@
+# Installer
+$this.CurrentState.Installer += $InstallerX64 = [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = Get-RedirectedUrl1st -Uri 'https://www.adobe.com/go/dng_converter_win' -Method GET
+}
+$VersionX64 = [regex]::Match($InstallerX64.InstallerUrl, 'x64_(\d+(?:_\d+)+)').Groups[1].Value.Replace('_', '.')
+
+$this.CurrentState.Installer += $InstallerARM64 = [ordered]@{
+  Architecture = 'arm64'
+  InstallerUrl = Get-RedirectedUrl1st -Uri 'https://www.adobe.com/go/dng_converter_winarm' -Method GET
+}
+$VersionArm64 = [regex]::Match($InstallerARM64.InstallerUrl, 'arm64_(\d+(?:_\d+)+)').Groups[1].Value.Replace('_', '.')
+
+if ($VersionX64 -ne $VersionArm64) {
+  $this.Log("x64 version: ${VersionX64}")
+  $this.Log("ARM64 version: ${VersionArm64}")
+  throw 'Inconsistent versions detected'
+}
+
+# Version
+$this.CurrentState.Version = $VersionX64
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
