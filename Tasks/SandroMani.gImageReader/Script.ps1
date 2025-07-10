dc28c496012285819f99d332402a26872298d427
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-07-10 13:42:07 +0800

    SandroMani.gImageReader: Manually detect the latest stable release, reset

--- Tasks/SandroMani.gImageReader/Script.ps1
+++ Tasks/SandroMani.gImageReader/Script.ps1
@@ -1,7 +1,7 @@
 $RepoOwner = 'manisandro'
 $RepoName = 'gImageReader'
 
-$Object1 = Invoke-GitHubApi -Uri "https://api.github.com/repos/${RepoOwner}/${RepoName}/releases/latest"
+$Object1 = (Invoke-GitHubApi -Uri "https://api.github.com/repos/${RepoOwner}/${RepoName}/releases").Where({ -not $_.prerelease -and -not $_.tag_name.Contains('master') }, 'First')[0]
 
 # Version
 $this.CurrentState.Version = $Object1.tag_name -creplace '^v'
