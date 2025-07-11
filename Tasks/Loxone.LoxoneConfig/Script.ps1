a418315b9aa3ce8142e7d510a75367b6b76e8761
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-07-11 19:56:16 +0800

    Add Loxone.LoxoneConfig

---
+++ Tasks/Loxone.LoxoneConfig/Script.ps1
@@ -0,0 +1,49 @@
+$Object1 = Invoke-RestMethod -Uri 'http://update.loxone.com/updatecheck.xml'
+
+# Version
+$this.CurrentState.Version = $Object1.Miniserversoftware.Release.Version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.Miniserversoftware.Release.Path | ConvertTo-Https
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.InstallerFiles[$this.CurrentState.Installer[0].InstallerUrl] = $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+    $InstallerFileExtracted = New-TempFolder
+    7z.exe e -aoa -ba -bd -y -o"${InstallerFileExtracted}" $InstallerFile 'LoxoneConfigSetup.exe' | Out-Host
+    $InstallerFile2 = Join-Path $InstallerFileExtracted 'LoxoneConfigSetup.exe'
+    # RealVersion
+    $this.CurrentState.RealVersion = $InstallerFile2 | Read-ProductVersionFromExe
+    Remove-Item -Path $InstallerFileExtracted -Recurse -Force -ErrorAction 'Continue' -ProgressAction 'SilentlyContinue'
+
+    try {
+      $Object2 = Invoke-WebRequest -Uri 'https://www.loxone.com/enen/support/changelog-config/' | ConvertFrom-Html
+
+      $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//h5[contains(text(), '$($this.CurrentState.Version -replace '0+(?!\.|$)')')]")
+      if ($ReleaseNotesTitleNode) {
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesTitleNode.SelectNodes('./following-sibling::node()') | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
