3648197043b487d2cc5d813af11a9f5f42d38d1b
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-07-14 13:54:59 +0800

    Datacomp.BIMvision: Switch to update source

--- Tasks/Datacomp.BIMvision/Script.ps1
+++ Tasks/Datacomp.BIMvision/Script.ps1
@@ -1,12 +1,27 @@
-$Object1 = Invoke-WebRequest -Uri 'https://bimvision.eu/download/'
+$Object1 = Invoke-RestMethod -Uri 'http://store.bimvision.eu/WebService/BIMVision.svc' -Method Post -Headers @{
+  SOAPAction = 'http://datacomp.com.pl/IBIMVision/GetVersionInfo'
+} -Body @'
+<?xml version="1.0"?>
+<SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <SOAP-ENV:Body>
+    <GetVersionInfo xmlns="http://datacomp.com.pl">
+      <client>
+        <Branding xmlns="http://schemas.datacontract.org/2004/07/BIMVisionPluginStore.Web.WebService">BV</Branding>
+        <Test xmlns="http://schemas.datacontract.org/2004/07/BIMVisionPluginStore.Web.WebService">false</Test>
+      </client>
+    </GetVersionInfo>
+  </SOAP-ENV:Body>
+</SOAP-ENV:Envelope>
+'@ -ContentType 'text/xml; charset=utf-8'
+$Object2 = $Object1.Envelope.Body.GetVersionInfoResponse.GetVersionInfoResult
+
+# Version
+$this.CurrentState.Version = "$($Object2.latest.Major).$($Object2.latest.Minor).$($Object2.latest.Release)"
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = $Object1.Links.Where({ try { $_.href.Contains('.exe') } catch {} }, 'First')[0].href | Split-Uri -LeftPart Path | ConvertTo-UnescapedUri
+  InstallerUrl = $Object2.latest.DownloadLink | ConvertTo-UnescapedUri
 }
-
-# Version
-$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
