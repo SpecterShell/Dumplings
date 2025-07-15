a800aef353167e5b2bc81bdcfbb81dcaa325a1d1
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-07-15 10:11:00 +0800

    BlenderFoundation.Blender: Parse the latest version from the releases

--- Tasks/BlenderFoundation.Blender/Script.ps1
+++ Tasks/BlenderFoundation.Blender/Script.ps1
@@ -1,12 +1,20 @@
+$Prefix = 'https://download.blender.org/release/'
+
+$Object1 = Invoke-WebRequest -Uri $Prefix
+
+$Prefix += $Object1.Links | Where-Object -FilterScript { try { $_.href -match 'Blender\d+(?:\.\d+)+/' } catch {} } | Select-Object -ExpandProperty 'href' | Sort-Object -Property { [version][regex]::Match($_, '(\d+(?:\.\d+)+)').Groups[1].Value } -Bottom 1
+
+$Object2 = Invoke-WebRequest -Uri $Prefix
+
+$InstallerName = $Object2.Links | Where-Object -FilterScript { try { $_.href.EndsWith('.msi') -and $_.href.Contains('x64') } catch {} } | Select-Object -ExpandProperty 'href' | Sort-Object -Property { [version][regex]::Match($_, '(\d+(?:\.\d+)+)').Groups[1].Value } -Bottom 1
+
 # Version
-$this.CurrentState.Version = [regex]::Match(
-  (curl -fsSLA $DumplingsInternetExplorerUserAgent 'https://www.blender.org/download/' | Join-String -Separator "`n"),
-  'blender-([\w\.]+)-windows'
-).Groups[1].Value
+$this.CurrentState.Version = [regex]::Match($InstallerName, '(\d+(?:\.\d+)+)').Groups[1].Value
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = "https://download.blender.org/release/Blender$($this.CurrentState.Version.Split('.')[0..1] -join '.')/blender-$($this.CurrentState.Version)-windows-x64.msi"
+  Architecture = 'x64'
+  InstallerUrl = Join-Uri $Prefix $InstallerName
 }
 
 switch -Regex ($this.Check()) {
