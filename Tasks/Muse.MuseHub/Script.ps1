c9f7ce83e7ecf322ba4aeda764c1c2bbb7930516
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-07-15 10:50:48 +0800

    Muse.MuseHub: Update

--- Tasks/Muse.MuseHub/Script.ps1
+++ Tasks/Muse.MuseHub/Script.ps1
@@ -1,4 +1,15 @@
-$Query = @'
+function Read-Installer {
+  $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+  # Version
+  $this.CurrentState.Version = $InstallerFile | Read-ProductVersionFromExe
+  # InstallerSha256
+  $this.CurrentState.Installer[0]['InstallerSha256'] = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
+  Remove-Item -Path $InstallerFile -Recurse -Force -ErrorAction 'Continue' -ProgressAction 'SilentlyContinue'
+}
+
+function Get-ReleaseNotes {
+  try {
+    $Query = @'
 {
   product(
     id: "ca479cc2-0fb8-4ebf-8131-dcac2bfdc485"
@@ -18,49 +29,111 @@ $Query = @'
 }
 '@
 
-$Object1 = Invoke-RestMethod -Uri 'https://cosmos-customer-webservice.azurewebsites.net/graphql' -Method Post -Body (
-  @{ query = $Query } | ConvertTo-Json -Compress
-) -ContentType 'application/json'
+    $Object2 = Invoke-RestMethod -Uri 'https://cosmos-customer-webservice.azurewebsites.net/graphql' -Method Post -Body (
+      @{ query = $Query } | ConvertTo-Json -Compress
+    ) -ContentType 'application/json'
 
-# Version
-$this.CurrentState.Version = $Object1.data.product.assets.version
-
-$Object2 = Invoke-RestMethod -Uri $Object1.data.product.assets.packageFileRefreshUrl
-
-# Installer
-$this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = $Object2.Trim() | Split-Uri -LeftPart Path
-}
-
-switch -Regex ($this.Check()) {
-  'New|Changed|Updated' {
-    try {
+    if ($Object2.data.product.whatIsNew.Contains($this.CurrentState.Version.Split('.')[0..2] -join '.')) {
       # ReleaseTime
-      $this.CurrentState.ReleaseTime = $Object1.data.product.releaseDate.ToUniversalTime()
+      $this.CurrentState.ReleaseTime = $Object2.data.product.releaseDate.ToUniversalTime()
 
       # ReleaseNotes (en-US)
       $this.CurrentState.Locale += [ordered]@{
         Locale = 'en-US'
         Key    = 'ReleaseNotes'
-        Value  = $Object1.data.product.whatIsNew | Convert-MarkdownToHtml | Get-TextContent | Format-Text
+        Value  = $Object2.data.product.whatIsNew | Convert-MarkdownToHtml -Extensions 'advanced', 'emojis', 'hardlinebreak' | Get-TextContent | Format-Text
       }
-    } catch {
-      $_ | Out-Host
-      $this.Log($_, 'Warning')
+    } else {
+      $this.Log("No ReleaseTime and ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
     }
+  } catch {
+    $_ | Out-Host
+    $this.Log($_, 'Warning')
+  }
+}
 
-    $this.InstallerFiles[$this.CurrentState.Installer[0].InstallerUrl] = $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
-    $InstallerFileExtracted = New-TempFolder
-    7z.exe e -aoa -ba -bd -y -o"${InstallerFileExtracted}" $InstallerFile 'MuseHub.exe' | Out-Host
-    $InstallerFile2 = Join-Path $InstallerFileExtracted 'MuseHub.exe'
-    # RealVersion
-    $this.CurrentState.RealVersion = $InstallerFile2 | Read-ProductVersionFromExe
-    Remove-Item -Path $InstallerFileExtracted -Recurse -Force -ErrorAction 'Continue' -ProgressAction 'SilentlyContinue'
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = 'https://muse-cdn.com/Muse_Hub.exe'
+}
 
+$Object1 = Invoke-WebRequest -Uri $this.CurrentState.Installer[0].InstallerUrl -Method Head
+$ETag = $Object1.Headers.ETag[0]
+
+# Case 0: Force submit the manifest
+if ($Global:DumplingsPreference.Contains('Force')) {
+  $this.Log('Skip checking states', 'Info')
+
+  # ETag
+  $this.CurrentState.ETag = @($ETag)
+
+  Read-Installer
+  Get-ReleaseNotes
+
+  $this.Print()
+  $this.Write()
+  $this.Message()
+  $this.Submit()
+  return
+}
+
+# Case 1: The task is new
+if ($this.Status.Contains('New')) {
+  $this.Log('New task', 'Info')
+
+  # ETag
+  $this.CurrentState.ETag = @($ETag)
+
+  Read-Installer
+  Get-ReleaseNotes
+
+  $this.Print()
+  $this.Write()
+  return
+}
+
+# Case 2: The ETag is unchanged
+if ($ETag -in $this.LastState.ETag) {
+  $this.Log("The version $($this.LastState.Version) from the last state is the latest (Global)", 'Info')
+  return
+}
+
+Read-Installer
+
+# Case 3: The current state has an invalid version
+if ([string]::IsNullOrWhiteSpace($this.CurrentState.Version)) {
+  throw 'The current state has an invalid version'
+}
+
+Get-ReleaseNotes
+
+# Case 4: The ETag has changed, but the SHA256 is not
+if ($this.CurrentState.Installer[0].InstallerSha256 -eq $this.LastState.Installer[0].InstallerSha256) {
+  $this.Log('The ETag has changed, but the SHA256 is not', 'Info')
+
+  # ETag
+  $this.CurrentState.ETag = $this.LastState.ETag + $ETag
+
+  $this.Write()
+  return
+}
+
+# ETag
+$this.CurrentState.ETag = @($ETag)
+
+switch -Regex ($this.Check()) {
+  # Case 6: The ETag, the SHA256 and the version have changed
+  'Updated|Rollbacked' {
     $this.Print()
     $this.Write()
+    $this.Message()
+    $this.Submit()
   }
-  'Changed|Updated' {
+  # Case 5: The ETag and the SHA256 have changed, but the version is not
+  default {
+    $this.Log('The ETag and the SHA256 have changed, but the version is not', 'Info')
+    $this.Config.IgnorePRCheck = $true
+    $this.Print()
+    $this.Write()
     $this.Message()
     $this.Submit()
   }
