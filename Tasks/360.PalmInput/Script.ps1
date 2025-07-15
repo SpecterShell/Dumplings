dde9081a262e6edf7db9a7fc46d4a6ed35b52ae8
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-07-15 10:09:40 +0800

    360.PalmInput: Switch to new source

--- Tasks/360.PalmInput/Script.ps1
+++ Tasks/360.PalmInput/Script.ps1
@@ -1,45 +1,24 @@
-$Object1 = Invoke-WebRequest -Uri 'https://www.xinshuru.com/' | ConvertFrom-Html
+$Object1 = Invoke-RestMethod -Uri 'https://www.xinshuru.com/xinshuru/data/updatelogs/pc.json'
 
 # Version
-$this.CurrentState.Version = [regex]::Match(
-  $Object1.SelectSingleNode("//*[@class='win']//*[@class='desc']").InnerText,
-  '版本：V(\d+\.\d+\.\d+\.\d+)'
-).Groups[1].Value
+$this.CurrentState.Version = $Object1.winlogs[0].version -replace '^v'
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = 'https:' + $Object1.SelectSingleNode("//*[@class='win']//*[@class='download']").Attributes['href'].Value
+  InstallerUrl = 'https:' + $Object1.wininfo.downloadurl
 }
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
     try {
       # ReleaseTime
-      $this.CurrentState.ReleaseTime = [regex]::Match(
-        $Object1.SelectSingleNode("//*[@class='win']//*[@class='desc']").InnerText,
-        '更新日期：(\d{4}/\d{1,2}/\d{1,2})'
-      ).Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
-    } catch {
-      $_ | Out-Host
-      $this.Log($_, 'Warning')
-    }
+      $this.CurrentState.ReleaseTime = $Object1.winlogs[0].date | Get-Date -Format 'yyyy-MM-dd'
 
-    try {
-      $Object2 = Invoke-WebRequest -Uri 'https://www.xinshuru.com/win_record.html' | ConvertFrom-Html
-
-      $ReleaseNotesObject = $Object2.SelectSingleNode("//*[@class='history' and contains(.//*[@class='latest'], '$($this.CurrentState.Version.Split('.')[0..2] -join '.')')]")
-      if ($ReleaseNotesObject) {
-        # ReleaseTime
-        $this.CurrentState.ReleaseTime ??= $ReleaseNotesObject.SelectSingleNode('.//*[@class="htime"]') | Get-TextContent | Get-Date -Format 'yyyy-MM-dd'
-
-        # ReleaseNotes (zh-CN)
-        $this.CurrentState.Locale += [ordered]@{
-          Locale = 'zh-CN'
-          Key    = 'ReleaseNotes'
-          Value  = $ReleaseNotesObject.SelectNodes('.//*[@class="latest"]/following-sibling::node()') | Get-TextContent | Format-Text
-        }
-      } else {
-        $this.Log("No ReleaseTime and ReleaseNotes (zh-CN) for version $($this.CurrentState.Version)", 'Warning')
+      # ReleaseNotes (zh-CN)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'zh-CN'
+        Key    = 'ReleaseNotes'
+        Value  = $Object1.winlogs[0].features | ConvertTo-OrderedList | Format-Text
       }
     } catch {
       $_ | Out-Host
