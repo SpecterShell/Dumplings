8e7a5bcb3330d8e511983c257baa44995ada377e
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-07-16 13:58:39 +0800

    Add EuropeanCommission.EUROMOD

---
+++ Tasks/EuropeanCommission.EUROMOD/Script.ps1
@@ -0,0 +1,68 @@
+$Prefix = 'https://euromod-web.jrc.ec.europa.eu/download-euromod'
+$Object1 = Invoke-WebRequest -Uri $Prefix
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture         = 'x64'
+  InstallerType        = 'zip'
+  InstallerUrl         = $InstallerUrl = Join-Uri $Prefix $Object1.Links.Where({ try { $_.href.Contains('EUROMOD') -and $_.href.EndsWith('.zip') -and $_.href.Contains('installer') -and $_.href.Contains('64bit') -and -not $_.href.Contains('latest') } catch {} }, 'First')[0].href
+  NestedInstallerFiles = @(
+    [ordered]@{
+      RelativeFilePath = "$($InstallerUrl | Split-Path -LeafBase).exe"
+    }
+  )
+}
+
+# Version
+$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseNotesUrl (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotesUrl'
+        Value  = $Prefix
+      }
+      # ReleaseNotesUrl (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotesUrl'
+        Value  = Join-Uri $Prefix $Object1.Links.Where({ try { $_.href.EndsWith('.pdf') -and $_.href.Contains('Whatsnew') -and $_.href.Contains($this.CurrentState.Version) } catch {} }, 'First')[0].href
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    foreach ($Installer in $this.CurrentState.Installer) {
+      $this.InstallerFiles[$Installer.InstallerUrl] = $InstallerFile = Get-TempFile -Uri $Installer.InstallerUrl
+      $InstallerFileExtracted = New-TempFolder
+      7z.exe x -aoa -ba -bd -y -o"${InstallerFileExtracted}" $InstallerFile $Installer.NestedInstallerFiles[0].RelativeFilePath | Out-Host
+      $InstallerFile2 = Join-Path $InstallerFileExtracted $Installer.NestedInstallerFiles[0].RelativeFilePath
+      $InstallerFile2Extracted = $InstallerFile2 | Expand-InstallShield
+      $InstallerFile3 = Join-Path $InstallerFile2Extracted 'EUROMOD.msi'
+      # ProductCode
+      $Installer['ProductCode'] = $InstallerFile3 | Read-ProductCodeFromMsi
+      # AppsAndFeaturesEntries
+      $Installer['AppsAndFeaturesEntries'] = @(
+        [ordered]@{
+          UpgradeCode   = $InstallerFile3 | Read-UpgradeCodeFromMsi
+          InstallerType = 'msi'
+        }
+      )
+      Remove-Item -Path $InstallerFile2Extracted -Recurse -Force -ErrorAction 'Continue' -ProgressAction 'SilentlyContinue'
+      Remove-Item -Path $InstallerFileExtracted -Recurse -Force -ErrorAction 'Continue' -ProgressAction 'SilentlyContinue'
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
