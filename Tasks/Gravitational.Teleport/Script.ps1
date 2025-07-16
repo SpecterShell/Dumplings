de43e3296ce07bfd8195470a97045a30e5622e80
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-07-16 15:41:31 +0800

    Add Gravitational.Teleport

---
+++ Tasks/Gravitational.Teleport/Script.ps1
@@ -0,0 +1,63 @@
+$Object1 = Invoke-WebRequest -Uri 'https://goteleport.com/download/' | ConvertFrom-Html
+$Object2 = $Object1.SelectSingleNode('//script[@id="__NEXT_DATA__"]').InnerHtml | ConvertFrom-Json
+
+# Version
+$this.CurrentState.Version = $Object2.props.pageProps.initialDownloads[0].versions[0].version
+
+# Installer
+$Asset = $Object2.props.pageProps.initialDownloads[0].versions[0].assets.Where({ $_.os -eq 'windows' -and $_.arch -eq 'amd64' -and $_.description -eq 'Windows (64-bit, tsh client only)' }, 'First')[0]
+$this.CurrentState.Installer += [ordered]@{
+  Architecture    = 'x64'
+  InstallerUrl    = $Asset.publicUrl | ConvertTo-UnescapedUri
+  InstallerSha256 = $Asset.sha256.Trim().ToUpper()
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseNotes (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotes'
+        Value  = $Object2.props.pageProps.initialDownloads[0].versions[0].notesMd | Convert-MarkdownToHtml -Extensions 'advanced', 'emojis', 'hardlinebreak' | Get-TextContent | Format-Text
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    try {
+      $RepoOwner = 'gravitational'
+      $RepoName = 'teleport'
+
+      # ReleaseNotesUrl
+      $this.CurrentState.Locale += [ordered]@{
+        Key   = 'ReleaseNotesUrl'
+        Value = "https://github.com/${RepoOwner}/${RepoName}/releases"
+      }
+
+      $Object2 = Invoke-GitHubApi -Uri "https://api.github.com/repos/${RepoOwner}/${RepoName}/releases/tags/v$($this.CurrentState.Version)"
+
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object2.published_at.ToUniversalTime()
+
+      # ReleaseNotesUrl
+      $this.CurrentState.Locale += [ordered]@{
+        Key   = 'ReleaseNotesUrl'
+        Value = $Object2.html_url
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
