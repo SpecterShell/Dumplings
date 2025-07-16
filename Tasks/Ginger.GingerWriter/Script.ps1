16b6b792204080895ec74009f54777552070f18d
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-07-16 14:21:59 +0800

    Add Ginger.GingerWriter

---
+++ Tasks/Ginger.GingerWriter/Script.ps1
@@ -0,0 +1,30 @@
+$Object1 = Invoke-RestMethod -Uri 'https://downloads.gingersoftware.com/files/windows/update3.xml'
+
+# Version
+$this.CurrentState.Version = $Object1.enclosure.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.enclosure.url
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.pubDate | Get-Date -AsUTC
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
