181d7204d28daa5a18b985d2ca00e9994fd8f518
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-07-17 23:20:17 +0800

    Wanpath.MyWorkDriveClient: Update

--- Tasks/Wanpath.MyWorkDriveClient/Script.ps1
+++ Tasks/Wanpath.MyWorkDriveClient/Script.ps1
@@ -1,113 +1,62 @@
-function Read-Installer {
-  $this.InstallerFiles[$this.CurrentState.Installer[0].InstallerUrl] = $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
-  $InstallerFileExtracted = New-TempFolder
-  7z.exe e -aoa -ba -bd -y -o"${InstallerFileExtracted}" $InstallerFile 'Files\MyWorkDrive.Installer.msi' | Out-Host
-  $InstallerFile2 = Join-Path $InstallerFileExtracted 'MyWorkDrive.Installer.msi'
-  # Version
-  $this.CurrentState.Version = $InstallerFile2 | Read-ProductVersionFromMsi
-  # ProductCode
-  $this.CurrentState.Installer[0]['ProductCode'] = $InstallerFile2 | Read-ProductCodeFromMsi
-  # AppsAndFeaturesEntries
-  $this.CurrentState.Installer[0]['AppsAndFeaturesEntries'] = @(
-    [ordered]@{
-      UpgradeCode   = $InstallerFile2 | Read-UpgradeCodeFromMsi
-      InstallerType = 'msi'
-    }
-  )
-  Remove-Item -Path $InstallerFileExtracted -Recurse -Force -ErrorAction 'Continue' -ProgressAction 'SilentlyContinue'
-}
-
-function Get-ReleaseNotes {
-  try {
-    $Object2 = Invoke-WebRequest -Uri 'https://www.myworkdrive.com/support/windows-client-release-notes/' | ConvertFrom-Html
-
-    $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//text()[contains(., 'MyWorkDrive Windows Client $($this.CurrentState.Version)') or contains(., 'MyWorkDrive Windows Client $($this.CurrentState.Version.Replace('.', '-'))')]")
-    if ($ReleaseNotesTitleNode) {
-      # ReleaseTime
-      $this.CurrentState.ReleaseTime = [regex]::Match($ReleaseNotesTitleNode.InnerText, '(\d{1,2}\W+[a-zA-Z]+\W+20\d{2})').Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
-
-      # ReleaseNotes (en-US)
-      $this.CurrentState.Locale += [ordered]@{
-        Locale = 'en-US'
-        Key    = 'ReleaseNotes'
-        Value  = $ReleaseNotesTitleNode.SelectNodes('./following-sibling::node()') | Get-TextContent | Format-Text
-      }
-    } else {
-      $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
-    }
-  } catch {
-    $_ | Out-Host
-    $this.Log($_, 'Warning')
-  }
-}
-
-$Object1 = Invoke-WebRequest -Uri 'https://www.myworkdrive.com/download/download-windows-client-production/' | ConvertFrom-Html
+$Object1 = Invoke-WebRequest -Uri 'https://www.myworkdrive.com/download' | ConvertFrom-Html
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = $Object1.SelectSingleNode('//a[contains(@class, "wpdm-download-link")]').Attributes['data-downloadurl'].Value -replace '&refresh=[^&]+' -replace '(?<=\?)refresh=[^&]+'
+  InstallerUrl = $Object1.SelectSingleNode('//input[contains(@value, ".exe")]').Attributes['value'].Value
 }
 
-$Object2 = Invoke-WebRequest -Uri $this.CurrentState.Installer[0].InstallerUrl -Method Head
-# Hash
-$this.CurrentState.Hash = $Object2.Headers.'Content-MD5'[0]
-
-# Case 0: Force submit the manifest
-if ($Global:DumplingsPreference.Contains('Force')) {
-  $this.Log('Skip checking states', 'Info')
-
-  Read-Installer
-  Get-ReleaseNotes
-
-  $this.Print()
-  $this.Write()
-  $this.Message()
-  $this.Submit()
-  return
-}
-
-# Case 1: The task is new
-if ($this.Status.Contains('New')) {
-  $this.Log('New task', 'Info')
-
-  Read-Installer
-  Get-ReleaseNotes
-
-  $this.Print()
-  $this.Write()
-  return
-}
-
-# Case 2: The hash is unchanged
-if ($this.CurrentState.Hash -eq $this.LastState.Hash) {
-  $this.Log("The version $($this.LastState.Version) from the last state is the latest", 'Info')
-  return
-}
-
-Read-Installer
-
-# Case 3: The current state has an invalid version
-if ([string]::IsNullOrWhiteSpace($this.CurrentState.Version)) {
-  throw 'The current state has an invalid version'
-}
-
-Get-ReleaseNotes
+# Version
+$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d+(?:-\d+)+)').Groups[1].Value.Replace('-', '.')
 
 switch -Regex ($this.Check()) {
-  # Case 5: The hash and the version have changed
-  'Updated|Rollbacked' {
+  'New|Changed|Updated' {
+    $this.InstallerFiles[$this.CurrentState.Installer[0].InstallerUrl] = $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+    $InstallerFileExtracted = New-TempFolder
+    7z.exe e -aoa -ba -bd -y -o"${InstallerFileExtracted}" $InstallerFile 'Files\MyWorkDrive.Installer.msi' | Out-Host
+    $InstallerFile2 = Join-Path $InstallerFileExtracted 'MyWorkDrive.Installer.msi'
+    # Version
+    $this.CurrentState.Version = $InstallerFile2 | Read-ProductVersionFromMsi
+    # ProductCode
+    $this.CurrentState.Installer[0]['ProductCode'] = $InstallerFile2 | Read-ProductCodeFromMsi
+    # AppsAndFeaturesEntries
+    $this.CurrentState.Installer[0]['AppsAndFeaturesEntries'] = @(
+      [ordered]@{
+        UpgradeCode   = $InstallerFile2 | Read-UpgradeCodeFromMsi
+        InstallerType = 'msi'
+      }
+    )
+    Remove-Item -Path $InstallerFileExtracted -Recurse -Force -ErrorAction 'Continue' -ProgressAction 'SilentlyContinue'
+
+    try {
+      $Object2 = Invoke-WebRequest -Uri 'https://www.myworkdrive.com/kb/articles/windows-client-release-notes' | ConvertFrom-Html
+
+      $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//text()[contains(., 'MyWorkDrive Windows Client $($this.CurrentState.Version)') or contains(., 'MyWorkDrive Windows Client $($this.CurrentState.Version.Replace('.', '-'))')]")
+      if ($ReleaseNotesTitleNode) {
+        # ReleaseTime
+        $this.CurrentState.ReleaseTime = [regex]::Match($ReleaseNotesTitleNode.InnerText, '(\d{1,2}\W+[a-zA-Z]+\W+20\d{2})').Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
+
+        # ReleaseNotes (en-US)
+        $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node -and $Node.Name -ne 'h2'; $Node = $Node.NextSibling) { $Node }
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
     $this.Print()
     $this.Write()
-    $this.Message()
-    $this.Submit()
   }
-  # Case 4: The hash has changed, but the version is not
-  default {
-    $this.Log('The hash has changed, but the version is not', 'Info')
-    $this.Config.IgnorePRCheck = $true
-    $this.Print()
-    $this.Write()
+  'Changed|Updated' {
     $this.Message()
+  }
+  'Updated' {
     $this.Submit()
   }
 }
