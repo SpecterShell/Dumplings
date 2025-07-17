b3b0b12fc8b3aec6cef5420b1f37d98c5bafd410
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-07-17 23:19:56 +0800

    ThomsonReuters.E-TranscriptBundleViewer: Update

--- Tasks/ThomsonReuters.E-TranscriptBundleViewer/Script.ps1
+++ Tasks/ThomsonReuters.E-TranscriptBundleViewer/Script.ps1
@@ -1,15 +1,26 @@
-$Object1 = Invoke-WebRequest -Uri 'https://legal.thomsonreuters.com/en/support/software-downloads/reallegal/e-transcript-bundle-viewer'
+$Prefix = 'https://www.thomsonreuters.com/en-us/help/legal-products-software-downloads'
+$Object1 = Invoke-WebRequest -Uri $Prefix | ConvertFrom-Html
+$Object2 = $Object1.SelectSingleNode('//div[@data-prop-page-title="Legal products software downloads"]').Attributes['data-prop-table-data-props'].DeEntitizeValue | ConvertFrom-Json
+$Object3 = $Object2.Where({ $_.tableData[0].value -eq 'E-Transcript Bundle Viewer' }, 'First')[0]
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = $Object1.Links.Where({ try { $_.href.EndsWith('.exe') } catch {} }, 'First')[0].href
+  InstallerUrl = Join-Uri $Prefix $Object3.tableData[3].link
 }
 
 # Version
-$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
+$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d+(?:_\d+)+)').Groups[1].Value.Replace('_', '.')
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object3.tableData[1].value | Get-Date -Format 'yyyy-MM-dd'
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
     foreach ($Installer in $this.CurrentState.Installer) {
       $this.InstallerFiles[$Installer.InstallerUrl] = $InstallerFile = Get-TempFile -Uri $Installer.InstallerUrl
       $InstallerFileExtracted = $InstallerFile | Expand-InstallShield
