b0c0f568a0477e16d5aa9ebf87fbd20e8e661dea
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-07-17 23:19:37 +0800

    QNAP.QVRProClient: Remove x86 installer

--- Tasks/QNAP.QVRProClient/Script.ps1
+++ Tasks/QNAP.QVRProClient/Script.ps1
@@ -1,33 +1,33 @@
 # x86
-$Object1 = $Global:DumplingsStorage.QNAPApps.docRoot.utility.application.Where({ $_.applicationName -eq 'com.qnap.qvrproclient' }, 'First')[0].platform.Where({ $_.platformName -eq 'WindowsX86' }, 'First')[0].software
-$VersionX86 = "$($Object1.version).$($Object1.buildNumber)"
+# $Object1 = $Global:DumplingsStorage.QNAPApps.docRoot.utility.application.Where({ $_.applicationName -eq 'com.qnap.qvrproclient' }, 'First')[0].platform.Where({ $_.platformName -eq 'WindowsX86' }, 'First')[0].software
+# $VersionX86 = "$($Object1.version).$($Object1.buildNumber)"
 # x64
 $Object2 = $Global:DumplingsStorage.QNAPApps.docRoot.utility.application.Where({ $_.applicationName -eq 'com.qnap.qvrproclient' }, 'First')[0].platform.Where({ $_.platformName -eq 'WindowsX64' }, 'First')[0].software
 $VersionX64 = "$($Object2.version).$($Object2.buildNumber)"
 
-if ($VersionX86 -ne $VersionX64) {
-  $this.Log("x86 version: ${VersionX86}")
-  $this.Log("x64 version: ${VersionX64}")
-  throw 'Inconsistent versions detected'
-}
+# if ($VersionX86 -ne $VersionX64) {
+#   $this.Log("x86 version: ${VersionX86}")
+#   $this.Log("x64 version: ${VersionX64}")
+#   throw 'Inconsistent versions detected'
+# }
 
 # Version
 $this.CurrentState.Version = $VersionX64
 
 # Installer
-$this.CurrentState.Installer += [ordered]@{
-  Architecture = 'x86'
-  InstallerUrl = $Object1.downloadURL[0]
-}
+# $this.CurrentState.Installer += [ordered]@{
+#   Architecture = 'x86'
+#   InstallerUrl = $Object1.downloadURL[0]
+# }
 $this.CurrentState.Installer += [ordered]@{
   Architecture = 'x64'
   InstallerUrl = $Object2.downloadURL[0]
 }
-$this.CurrentState.Installer += [ordered]@{
-  InstallerLocale = 'zh-CN'
-  Architecture    = 'x86'
-  InstallerUrl    = $Object1.downloadURL[0].Replace('//download.qnap.com/', '//download.qnap.com.cn/')
-}
+# $this.CurrentState.Installer += [ordered]@{
+#   InstallerLocale = 'zh-CN'
+#   Architecture    = 'x86'
+#   InstallerUrl    = $Object1.downloadURL[0].Replace('//download.qnap.com/', '//download.qnap.com.cn/')
+# }
 $this.CurrentState.Installer += [ordered]@{
   InstallerLocale = 'zh-CN'
   Architecture    = 'x64'
@@ -38,14 +38,14 @@ switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
     try {
       # ReleaseTime
-      $this.CurrentState.ReleaseTime = $Object1.releaseDate | Get-Date -AsUTC
+      $this.CurrentState.ReleaseTime = $Object2.releaseDate | Get-Date -AsUTC
     } catch {
       $_ | Out-Host
       $this.Log($_, 'Warning')
     }
 
     try {
-      $Object3 = (Invoke-RestMethod -Uri 'https://www.qnap.com.cn/api/v1/release-notes/utility/QVR%20Pro%20Client?locale=en').result.notes.Windows.Where({ $_.version -eq $Object1.version }, 'First')
+      $Object3 = (Invoke-RestMethod -Uri 'https://www.qnap.com.cn/api/v1/release-notes/utility/QVR%20Pro%20Client?locale=en').result.notes.Windows.Where({ $_.version -eq $Object2.version }, 'First')
 
       if ($Object3) {
         # ReleaseNotes (en-US)
@@ -63,7 +63,7 @@ switch -Regex ($this.Check()) {
     }
 
     try {
-      $Object4 = (Invoke-RestMethod -Uri 'https://www.qnap.com.cn/api/v1/release-notes/utility/QVR%20Pro%20Client?locale=zh-cn').result.notes.Windows.Where({ $_.version -eq $Object1.version }, 'First')
+      $Object4 = (Invoke-RestMethod -Uri 'https://www.qnap.com.cn/api/v1/release-notes/utility/QVR%20Pro%20Client?locale=zh-cn').result.notes.Windows.Where({ $_.version -eq $Object2.version }, 'First')
 
       if ($Object4 -and $Object4[0].note -ne $Object3[0].note) {
         # ReleaseNotes (zh-CN)
