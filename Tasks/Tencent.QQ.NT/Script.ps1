66bc95b8c76b74a910b9c157960b44093d5e9fa9
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-07-18 11:31:55 +0800

    Tencent.QQ: Fixes

--- Tasks/Tencent.QQ.NT/Script.ps1
+++ Tasks/Tencent.QQ.NT/Script.ps1
@@ -1,5 +1,5 @@
 $Object1 = Invoke-WebRequest -Uri 'https://im.qq.com/pcqq/index.shtml'
-$Object2 = Invoke-RestMethod -Uri ([regex]::Match($Object1.Content, 'rainbowConfigUrl\s*=\s*"(.+?)"').Groups[1].Value) | Get-EmbeddedJson -StartsFrom 'var params= ' | ConvertFrom-Json
+$Object2 = Invoke-RestMethod -Uri ('https:' + [regex]::Match($Object1.Content, 'rainbowConfigUrl\s*=\s*"(.+?)"').Groups[1].Value) | Get-EmbeddedJson -StartsFrom 'var params= ' | ConvertFrom-Json
 
 # Installer
 $this.CurrentState.Installer += $InstallerX86 = [ordered]@{
@@ -34,7 +34,7 @@ switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
     try {
       # ReleaseTime
-      $this.CurrentState.ReleaseTime = $Object2.ntPublishTime | Get-Date -Format 'yyyy-MM-dd'
+      $this.CurrentState.ReleaseTime = $Object2.updateDate | Get-Date -Format 'yyyy-MM-dd'
     } catch {
       $_ | Out-Host
       $this.Log($_, 'Warning')
@@ -48,15 +48,15 @@ switch -Regex ($this.Check()) {
       # Only parse version for major updates
       if (-not $this.Status.Contains('New') -or ($this.CurrentState.Version.Split('.')[0..2] -join '.') -ne ($this.LastState.Version.Split('.')[0..2] -join '.')) {
         $Object3 = Invoke-WebRequest -Uri 'https://im.qq.com/pcqq/support.html'
-        $Object4 = Invoke-RestMethod -Uri ([regex]::Match($Object3.Content, 'rainbowConfigUrl\s*=\s*"(.+?)"').Groups[1].Value) | Get-EmbeddedJson -StartsFrom 'var params= ' | ConvertFrom-Json
+        $Object4 = Invoke-RestMethod -Uri ('https:' + [regex]::Match($Object3.Content, 'rainbowConfigUrl\s*=\s*"(.+?)"').Groups[1].Value) | Get-EmbeddedJson -StartsFrom 'var params= ' | ConvertFrom-Json
 
-        $ReleaseNotesObject = $Object4.ntLogs.Where({ $_.version.EndsWith($Object2.ntVersion) }, 'First')
+        $ReleaseNotesObject = $Object4.log.Where({ $_.version.EndsWith($Object2.version) }, 'First')
         if ($ReleaseNotesObject) {
           # ReleaseNotes (zh-CN)
           $this.CurrentState.Locale += [ordered]@{
             Locale = 'zh-CN'
             Key    = 'ReleaseNotes'
-            Value  = $ReleaseNotesObject[0].features.text | Format-Text
+            Value  = $ReleaseNotesObject[0].feature | Format-Text
           }
         } else {
           $this.Log("No ReleaseNotes (zh-CN) for version $($this.CurrentState.Version)", 'Warning')
