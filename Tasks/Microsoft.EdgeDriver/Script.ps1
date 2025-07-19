73e12d9bd820584adec977a3ee70d665f255f481
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-07-19 09:59:36 +0800

    Microsoft.EdgeDriver: Update source

--- Tasks/Microsoft.EdgeDriver/Script.ps1
+++ Tasks/Microsoft.EdgeDriver/Script.ps1
@@ -1,38 +1,20 @@
-$Object1 = (Invoke-WebRequest -Uri 'https://msedgedriver.azureedge.net/LATEST_STABLE' | Read-ResponseContent).Trim()
+$Object1 = (Invoke-WebRequest -Uri 'https://msedgedriver.microsoft.com/LATEST_STABLE' | Read-ResponseContent).Trim()
 
 # Version
 $this.CurrentState.Version = $Object1
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  Architecture         = 'x86'
-  InstallerUrl         = "https://msedgedriver.azureedge.net/$($this.CurrentState.Version)/edgedriver_win32.zip"
-  NestedInstallerFiles = @(
-    [ordered]@{
-      RelativeFilePath     = 'msedgedriver.exe'
-      PortableCommandAlias = 'msedgedriver'
-    }
-  )
+  Architecture = 'x86'
+  InstallerUrl = "https://msedgedriver.microsoft.com/$($this.CurrentState.Version)/edgedriver_win32.zip"
 }
 $this.CurrentState.Installer += [ordered]@{
-  Architecture         = 'x64'
-  InstallerUrl         = "https://msedgedriver.azureedge.net/$($this.CurrentState.Version)/edgedriver_win64.zip"
-  NestedInstallerFiles = @(
-    [ordered]@{
-      RelativeFilePath     = 'msedgedriver.exe'
-      PortableCommandAlias = 'msedgedriver'
-    }
-  )
+  Architecture = 'x64'
+  InstallerUrl = "https://msedgedriver.microsoft.com/$($this.CurrentState.Version)/edgedriver_win64.zip"
 }
 $this.CurrentState.Installer += [ordered]@{
-  Architecture         = 'arm64'
-  InstallerUrl         = "https://msedgedriver.azureedge.net/$($this.CurrentState.Version)/edgedriver_arm64.zip"
-  NestedInstallerFiles = @(
-    [ordered]@{
-      RelativeFilePath     = 'msedgedriver.exe'
-      PortableCommandAlias = 'msedgedriver'
-    }
-  )
+  Architecture = 'arm64'
+  InstallerUrl = "https://msedgedriver.microsoft.com/$($this.CurrentState.Version)/edgedriver_arm64.zip"
 }
 
 switch -Regex ($this.Check()) {
