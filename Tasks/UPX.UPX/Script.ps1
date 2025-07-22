abf24b33ae3a58f84b6b857ddc24f17abea338fc
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-07-22 11:19:50 +0800

    UPX.UPX: Enhance release notes parsing

--- Tasks/UPX.UPX/Script.ps1
+++ Tasks/UPX.UPX/Script.ps1
@@ -35,30 +35,44 @@ switch -Regex ($this.Check()) {
     try {
       # ReleaseTime
       $this.CurrentState.ReleaseTime = $Object1.published_at.ToUniversalTime()
-
-      # ReleaseNotesUrl
-      $this.CurrentState.Locale += [ordered]@{
-        Key   = 'ReleaseNotesUrl'
-        Value = $ReleaseNotesUrl = 'https://upx.github.io/upx-news.txt'
-      }
     } catch {
       $_ | Out-Host
       $this.Log($_, 'Warning')
     }
 
     try {
-      $Object2 = Invoke-RestMethod -Uri $ReleaseNotesUrl
+      $Object2 = [System.IO.StreamReader]::new((Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/upx/upx/HEAD/NEWS').RawContentStream)
 
-      $ReleaseNotesObject = ($Object2 -split '(?m)(?=^Changes in [\d\.]+ \(.+?\):\n)').Where({ $_.Contains($this.CurrentState.Version) }, 'First')[0]
-      if ($ReleaseNotesObject) {
+      while (-not $Object2.EndOfStream) {
+        $String = $Object2.ReadLine()
+        if ($String -match "^Changes in $([regex]::Escape($this.CurrentState.Version))") {
+          if ($String -match '(\d{1,2}\W+[a-zA-Z]+\W+20\d{2})') {
+            # ReleaseTime
+            $this.CurrentState.ReleaseTime = $Matches[1] | Get-Date -Format 'yyyy-MM-dd'
+          } else {
+            $this.Log("No ReleaseTime for version $($this.CurrentState.Version)", 'Warning')
+          }
+          break
+        }
+      }
+      if (-not $Object2.EndOfStream) {
+        $ReleaseNotesObjects = [System.Collections.Generic.List[string]]::new()
+        while (-not $Object2.EndOfStream) {
+          $String = $Object2.ReadLine()
+          if ($String -notmatch '^Changes in \d+(\.\d+)+ ') {
+            $ReleaseNotesObjects.Add($String -replace '^  ')
+          } else {
+            break
+          }
+        }
         # ReleaseNotes (en-US)
         $this.CurrentState.Locale += [ordered]@{
           Locale = 'en-US'
           Key    = 'ReleaseNotes'
-          Value  = $ReleaseNotesObject -creplace '^Changes in [\d\.]+ \(.+?\):\n' | Format-Text
+          Value  = $ReleaseNotesObjects | Format-Text
         }
       } else {
-        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+        $this.Log("No ReleaseTime and ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
       }
     } catch {
       $_ | Out-Host
