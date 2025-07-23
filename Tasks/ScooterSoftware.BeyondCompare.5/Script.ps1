fba041c8b248f7fa7d21b55389e6cad03ce8dffd
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-07-23 10:48:23 +0800

    ScooterSoftware.BeyondCompare.5: Add user translated installers

--- Tasks/ScooterSoftware.BeyondCompare.5/Script.ps1
+++ Tasks/ScooterSoftware.BeyondCompare.5/Script.ps1
@@ -5,24 +5,72 @@ $this.CurrentState.Version = $Object1.Update.latestversion -replace '\s+build\s+
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerLocale = 'en-US'
+  InstallerLocale = 'en'
   InstallerUrl    = "https://www.scootersoftware.com/files/BCompare-$($this.CurrentState.Version).exe"
 }
 $this.CurrentState.Installer += [ordered]@{
-  InstallerLocale = 'de-DE'
+  InstallerLocale = 'de'
   InstallerUrl    = "https://www.scootersoftware.com/files/BCompare-de-$($this.CurrentState.Version).exe"
 }
 $this.CurrentState.Installer += [ordered]@{
-  InstallerLocale = 'fr-FR'
+  InstallerLocale = 'fr'
   InstallerUrl    = "https://www.scootersoftware.com/files/BCompare-fr-$($this.CurrentState.Version).exe"
 }
 $this.CurrentState.Installer += [ordered]@{
-  InstallerLocale = 'ja-JP'
+  InstallerLocale = 'ja'
   InstallerUrl    = "https://www.scootersoftware.com/files/BCompare-jp-$($this.CurrentState.Version).exe"
 }
 $this.CurrentState.Installer += [ordered]@{
   InstallerLocale = 'zh-CN'
   InstallerUrl    = "https://www.scootersoftware.com/files/BCompare-zh-$($this.CurrentState.Version).exe"
+}
+$this.CurrentState.Installer += [ordered]@{
+  InstallerLocale = 'cs'
+  InstallerUrl    = "https://www.scootersoftware.com/files/BCompare-cs-$($this.CurrentState.Version).exe"
+}
+$this.CurrentState.Installer += [ordered]@{
+  InstallerLocale = 'es'
+  InstallerUrl    = "https://www.scootersoftware.com/files/BCompare-es-$($this.CurrentState.Version).exe"
+}
+$this.CurrentState.Installer += [ordered]@{
+  InstallerLocale = 'ga'
+  InstallerUrl    = "https://www.scootersoftware.com/files/BCompare-ga-$($this.CurrentState.Version).exe"
+}
+$this.CurrentState.Installer += [ordered]@{
+  InstallerLocale = 'id'
+  InstallerUrl    = "https://www.scootersoftware.com/files/BCompare-id-$($this.CurrentState.Version).exe"
+}
+$this.CurrentState.Installer += [ordered]@{
+  InstallerLocale = 'it'
+  InstallerUrl    = "https://www.scootersoftware.com/files/BCompare-it-$($this.CurrentState.Version).exe"
+}
+$this.CurrentState.Installer += [ordered]@{
+  InstallerLocale = 'ko'
+  InstallerUrl    = "https://www.scootersoftware.com/files/BCompare-kr-$($this.CurrentState.Version).exe"
+}
+$this.CurrentState.Installer += [ordered]@{
+  InstallerLocale = 'nl'
+  InstallerUrl    = "https://www.scootersoftware.com/files/BCompare-nl-$($this.CurrentState.Version).exe"
+}
+$this.CurrentState.Installer += [ordered]@{
+  InstallerLocale = 'pt'
+  InstallerUrl    = "https://www.scootersoftware.com/files/BCompare-pt-$($this.CurrentState.Version).exe"
+}
+$this.CurrentState.Installer += [ordered]@{
+  InstallerLocale = 'ru'
+  InstallerUrl    = "https://www.scootersoftware.com/files/BCompare-ru-$($this.CurrentState.Version).exe"
+}
+$this.CurrentState.Installer += [ordered]@{
+  InstallerLocale = 'sl'
+  InstallerUrl    = "https://www.scootersoftware.com/files/BCompare-sl-$($this.CurrentState.Version).exe"
+}
+$this.CurrentState.Installer += [ordered]@{
+  InstallerLocale = 'sv'
+  InstallerUrl    = "https://www.scootersoftware.com/files/BCompare-se-$($this.CurrentState.Version).exe"
+}
+$this.CurrentState.Installer += [ordered]@{
+  InstallerLocale = 'zh-TW'
+  InstallerUrl    = "https://www.scootersoftware.com/files/BCompare-cn-$($this.CurrentState.Version).exe"
 }
 
 switch -Regex ($this.Check()) {
