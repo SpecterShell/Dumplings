b005c65b41ad96757948a715f4d20defe61d6f3a
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-07-24 20:39:26 +0800

    Add ECSoftware.Help+ManualTranslationAssistant

---
+++ Tasks/ECSoftware.Help+ManualTranslationAssistant/Script.ps1
@@ -0,0 +1,52 @@
+$Object1 = Invoke-WebRequest -Uri 'https://www.helpandmanual.com/downloads.html'
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.Links.Where({ try { $_.href.EndsWith('.exe') -and $_.outerHTML -match 'H\+M Translation Assistant (\d+(?:\.\d+)+)' } catch {} }, 'First')[0].href
+}
+
+# Version
+$this.CurrentState.Version = $Matches[1]
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      $Object2 = Invoke-RestMethod -Uri 'https://www.helpandmanual.com/news/feed/'
+
+      $ReleaseNotesObject = $Object2.Where({ $_.title.Contains("Translation Assistant $($this.CurrentState.Version)") }, 'First')
+      if ($ReleaseNotesObject) {
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesObject[0].encoded.'#cdata-section' | ConvertFrom-Html | Get-TextContent | Format-Text
+        }
+
+        # ReleaseNotesUrl (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotesUrl'
+          Value  = $ReleaseNotesObject[0].link
+        }
+      } else {
+        $this.Log("No ReleaseNotes (en-US) and ReleaseNotesUrl (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    if ($this.CurrentState.Version.Split('.')[0] -ne $this.Config.WinGetIdentifier.Split('.')[-1]) {
+      $this.Log('Major version update. The WinGet package needs to be updated', 'Error')
+    } else {
+      $this.Submit()
+    }
+  }
+}
