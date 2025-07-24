32d300080de07bea3e9d117429985f89321bd7e3
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-07-24 14:24:43 +0800

    Add VictronEnergy.DataControl

---
+++ Tasks/VictronEnergy.DataControl/Script.ps1
@@ -0,0 +1,23 @@
+$Prefix = 'https://www.victronenergy.com/support-and-downloads/software'
+$Object1 = Invoke-WebRequest -Uri $Prefix
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = Join-Uri $Prefix $Object1.Links.Where({ try { $_.href.EndsWith('.zip') -and $_.href.Contains('VictronEnergyDataControl') -and $_.href.Contains('Setup') } catch {} }, 'First')[0].href
+}
+
+# Version
+$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
