c5376a473cdcab8265ed1b9a687739064a08aa2e
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-07-26 18:55:43 +0800

    Add StephenRessler.BridgeDesigner

---
+++ Tasks/StephenRessler.BridgeDesigner/Script.ps1
@@ -0,0 +1,36 @@
+$ProjectName = 'wpbdc'
+$RootPath = '/Current Release/jre/'
+$PatternPath = ''
+$PatternFilename = 'setup[^\d]*(\d+)[^\d]*\.exe'
+
+$Object1 = Invoke-RestMethod -Uri "https://sourceforge.net/projects/${ProjectName}/rss?path=${RootPath}"
+$Assets = $Object1.Where({ $_.title.'#cdata-section' -match "^$([regex]::Escape($RootPath))${PatternPath}${PatternFilename}$" })
+
+# Version
+$this.CurrentState.Version = [regex]::Match($Assets[0].title.'#cdata-section', "^$([regex]::Escape($RootPath))${PatternPath}${PatternFilename}").Groups[1].Value
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Assets[0].link | ConvertTo-UnescapedUri
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = [datetime]::ParseExact($Assets[0].pubDate, 'ddd, dd MMM yyyy HH:mm:ss "UT"', (Get-Culture -Name 'en-US')) | ConvertTo-UtcDateTime -Id 'UTC'
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Log('The real version is unknown. Get the real version from the program about window', 'Warning')
+  }
+}
