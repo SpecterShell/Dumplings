c7c051aa084c8f2c1f23175a815657ef43ab2515
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-07-27 10:37:06 +0800

    Add CREALOGIX.PayEye

---
+++ Tasks/CREALOGIX.PayEye/Script.ps1
@@ -0,0 +1,22 @@
+$Object1 = Invoke-WebRequest -Uri 'https://support.crealogix.com/en/products/payeye/'
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.Links.Where({ try { $_.href.EndsWith('.exe') } catch {} }, 'First')[0].href
+}
+
+# Version
+$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
