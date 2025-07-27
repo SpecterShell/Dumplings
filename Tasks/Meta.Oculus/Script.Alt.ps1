f862d2dbbc70410a4a97edcf426dc904c5eb71fb
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-07-27 22:00:15 +0800

    Meta.Oculus: Switch to new source, reset

--- Tasks/Meta.Oculus/Script.Alt.ps1
+++ Tasks/Meta.Oculus/Script.Alt.ps1
@@ -1,25 +1,18 @@
-$Object1 = Invoke-RestMethod -Uri 'https://www.oculus.com/sparkle-updates/1582076955407037/'
+$Object1 = Invoke-RestMethod -Uri 'https://graph.oculus.com/pc_gestalt_version?access_token=OC|1582076955407037|'
 
 # Version
-$this.CurrentState.Version = $Object1.enclosure.version
+$this.CurrentState.Version = $Object1.version
 
 # Installer
-$InstallerUrlBuilder = [System.UriBuilder]::new($Object1.enclosure.url)
+$InstallerUrlBuilder = [System.UriBuilder]::new($Object1.uri)
 $InstallerUrlBuilder.Host = 'securecdn.oculus.com'
+$InstallerUrlBuilder.Query = $InstallerUrlBuilder.Query -replace '&?fcl=[^&]+'
 $this.CurrentState.Installer += [ordered]@{
   InstallerUrl = $InstallerUrlBuilder.ToString().Replace(':443', '')
 }
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
-    try {
-      # ReleaseTime
-      $this.CurrentState.ReleaseTime = $Object1.pubDate | Get-Date -AsUTC
-    } catch {
-      $_ | Out-Host
-      $this.Log($_, 'Warning')
-    }
-
     $this.InstallerFiles[$this.CurrentState.Installer[0].InstallerUrl] = $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
     # RealVersion
     $this.CurrentState.RealVersion = ($InstallerFile | Read-ProductVersionFromExe).Split('.')[0..2] -join '.'
