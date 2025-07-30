52685bbe884a84292bbd0b7c2adfbe7d85b63ca7
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-07-30 21:22:54 +0800

    Add IPEVO.CamControl

---
+++ Tasks/IPEVO.CamControl/Script.ps1
@@ -0,0 +1,29 @@
+$Object1 = Invoke-WebRequest -Uri 'https://us.ipevo.com/pages/camcontrol-download' | ConvertFrom-Html
+
+$InstallerUrl = $Object1.SelectSingleNode("//text()[.='Windows 7 / 8 / 10 / 11']/following::a[contains(., 'Download')]").Attributes['href'].Value | ConvertTo-HtmlDecodedText
+
+# Version
+$this.CurrentState.Version = [regex]::Match((Get-RedirectedUrl -Uri $InstallerUrl), '(\d+(?:\.\d+)+)').Groups[1].Value
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl         = $InstallerUrl
+  NestedInstallerFiles = @(
+    [ordered]@{
+      RelativeFilePath = "Annotator_v$($this.CurrentState.Version).msi"
+    }
+  )
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
