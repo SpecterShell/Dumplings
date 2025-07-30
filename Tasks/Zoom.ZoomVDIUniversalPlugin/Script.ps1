d16ac48b3eec19abc6f51f20751d6dfa619f3522
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-07-30 22:39:43 +0800

    Zoom.ZoomVDIUniversalPlugin: Parse real version from installer

--- Tasks/Zoom.ZoomVDIUniversalPlugin/Script.ps1
+++ Tasks/Zoom.ZoomVDIUniversalPlugin/Script.ps1
@@ -10,9 +10,6 @@ $Object2 = $Object1['12'] | ConvertFrom-Json
 # Version
 $this.CurrentState.Version = [regex]::Match($Object2.x64.url, '(\d+\.\d+\.\d+\.\d+)').Groups[1].Value
 
-# RealVersion
-$this.CurrentState.RealVersion = $this.CurrentState.Version.Split('.')[@(0, 1, 3)] -join '.'
-
 # Installer
 # $this.CurrentState.Installer += [ordered]@{
 #   Architecture = 'x86'
@@ -29,6 +26,10 @@ $this.CurrentState.Installer += [ordered]@{
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
+    $this.InstallerFiles[$this.CurrentState.Installer[0].InstallerUrl] = $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+    # RealVersion
+    $this.CurrentState.RealVersion = $InstallerFile | Read-ProductVersionFromMsi
+
     $this.Print()
     $this.Write()
   }
