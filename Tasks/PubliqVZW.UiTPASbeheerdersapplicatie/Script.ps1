3fdbdb6b93c834549664fbe3c656a71f84e45f42
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-07-31 00:18:16 +0800

    Add PubliqVZW.UiTPASbeheerdersapplicatie

---
+++ Tasks/PubliqVZW.UiTPASbeheerdersapplicatie/Script.ps1
@@ -0,0 +1,26 @@
+$Object1 = Invoke-WebRequest -Uri 'https://files.uitpas.be/uitpas_browser.html'
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.Links.Where({ try { $_.href.EndsWith('.msi') } catch {} }, 'First')[0].href
+}
+
+# Version
+$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.InstallerFiles[$this.CurrentState.Installer[0].InstallerUrl] = $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+    # RealVersion
+    $this.CurrentState.RealVersion = $InstallerFile | Read-ProductVersionFromMsi
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
