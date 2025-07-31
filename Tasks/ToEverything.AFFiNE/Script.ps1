213b9b7058ac1c170c561c58424d83c200255059
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-07-31 21:42:31 +0800

    ToEverything.AFFiNE: Add arm64 installer

--- Tasks/ToEverything.AFFiNE/Script.ps1
+++ Tasks/ToEverything.AFFiNE/Script.ps1
@@ -17,6 +17,16 @@ $this.CurrentState.Installer += [ordered]@{
   InstallerType = 'nullsoft'
   InstallerUrl  = $Object1.assets.Where({ $_.name.EndsWith('.exe') -and $_.name.Contains('nsis') -and $_.name.Contains('x64') }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
 }
+$this.CurrentState.Installer += [ordered]@{
+  Architecture  = 'arm64'
+  InstallerType = 'exe'
+  InstallerUrl  = $Object1.assets.Where({ $_.name.EndsWith('.exe') -and -not $_.name.Contains('nsis') -and $_.name.Contains('arm64') }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture  = 'arm64'
+  InstallerType = 'nullsoft'
+  InstallerUrl  = $Object1.assets.Where({ $_.name.EndsWith('.exe') -and $_.name.Contains('nsis') -and $_.name.Contains('arm64') }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
+}
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
