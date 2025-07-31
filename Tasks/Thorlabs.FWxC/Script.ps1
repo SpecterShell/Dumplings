650327cacbc92598180cc20dad9b972611eb4803
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-07-31 14:23:58 +0800

    Thorlabs.FWxC: Fix

--- Tasks/Thorlabs.FWxC/Script.ps1
+++ Tasks/Thorlabs.FWxC/Script.ps1
@@ -1,11 +1,11 @@
 $Object1 = Invoke-RestMethod -Uri 'https://www.thorlabs.com/software_pages/check_updates.cfm?ItemID=FW102C'
 
 # Version
-$this.CurrentState.Version = $Object1.ItemID.SoftwarePkg[0].VersionNumber
+$this.CurrentState.Version = $Object1.ItemID.SoftwarePkg.VersionNumber
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerUrl         = $InstallerUrl = $Object1.ItemID.SoftwarePkg[0].DownloadLink
+  InstallerUrl         = $InstallerUrl = $Object1.ItemID.SoftwarePkg.DownloadLink
   NestedInstallerFiles = @(
     [ordered]@{
       RelativeFilePath = "$($InstallerUrl | Split-Path -LeafBase).exe"
@@ -17,7 +17,7 @@ switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
     try {
       # ReleaseTime
-      $this.CurrentState.ReleaseTime = $Object1.ItemID.SoftwarePkg[0].ReleaseDate | Get-Date -Format 'yyyy-MM-dd'
+      $this.CurrentState.ReleaseTime = $Object1.ItemID.SoftwarePkg.ReleaseDate | Get-Date -Format 'yyyy-MM-dd'
     } catch {
       $_ | Out-Host
       $this.Log($_, 'Warning')
