6b6934065f7765f06c2272ca5c0b3ddc97133c64
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-08-02 23:04:53 +0800

    Add ApacheFriends.Xampp

---
+++ Tasks/ApacheFriends.Xampp.8.1/Script.ps1
@@ -0,0 +1,67 @@
+$ProjectName = 'xampp'
+$RootPath = '/XAMPP Windows/'
+$PatternPath = '8\.1(?:\.\d+)+/'
+$PatternFilename = 'xampp-windows-x64-(\d+(?:\.\d+)+-\d+)-.+-installer\.exe'
+
+$Object1 = Invoke-RestMethod -Uri "https://sourceforge.net/projects/${ProjectName}/rss?path=${RootPath}"
+$Assets = $Object1.Where({ $_.title.'#cdata-section' -match "^$([regex]::Escape($RootPath))${PatternPath}${PatternFilename}$" })
+
+# Version
+$this.CurrentState.Version = [regex]::Match($Assets[0].title.'#cdata-section', "^$([regex]::Escape($RootPath))${PatternPath}${PatternFilename}").Groups[1].Value
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Assets[0].link | ConvertTo-UnescapedUri
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = [datetime]::ParseExact($Assets[0].pubDate, 'ddd, dd MMM yyyy HH:mm:ss "UT"', (Get-Culture -Name 'en-US')) | ConvertTo-UtcDateTime -Id 'UTC'
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    try {
+      # ReleaseNotesUrl (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotesUrl'
+        Value  = 'https://www.apachefriends.org/blog'
+      }
+
+      $Object2 = (Invoke-RestMethod -Uri 'https://www.apachefriends.org/feed.xml').Where({ $_.title.Contains($this.CurrentState.Version.Split('-')[0]) }, 'First')
+
+      if ($Object2) {
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $Object2[0].content.'#text' | ConvertFrom-Html | Get-TextContent | Format-Text
+        }
+        # ReleaseNotesUrl (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotesUrl'
+          Value  = $Object2[0].link.href
+        }
+      } else {
+        $this.Log("No ReleaseTime, ReleaseNotes (en-US) and ReleaseNotesUrl for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
