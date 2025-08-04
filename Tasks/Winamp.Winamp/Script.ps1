c3e283d26b78669114c3d892c5e627f04b024f6f
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-08-04 15:12:34 +0800

    Add Winamp.Winamp

---
+++ Tasks/Winamp.Winamp/Script.ps1
@@ -0,0 +1,27 @@
+$Object1 = (Invoke-RestMethod -Uri 'http://client.winamp.com/update/latest-version.php').Split()
+
+# Version
+$this.CurrentState.Version = $Object1[0].Trim()
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1[1].Trim() | Split-Uri -LeftPart 'Path'
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.InstallerFiles[$this.CurrentState.Installer[0].InstallerUrl] = $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl | Rename-Item -NewName { "${_}.exe" } -PassThru | Select-Object -ExpandProperty 'FullName'
+    # RealVersion
+    Start-ThreadJob -ScriptBlock { Start-Process -FilePath $using:InstallerFile -ArgumentList '/S' -Wait } | Wait-Job -Timeout 300 | Receive-Job | Out-Host
+    $this.CurrentState.RealVersion = Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Winamp' -Name 'DisplayVersion'
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
