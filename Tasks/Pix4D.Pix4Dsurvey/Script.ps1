4fdc9f8fba4fd1bb03e07c304e7c9f1df9dac2ab
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-08-15 23:32:30 +0800

    Pix4D.Pix4Dsurvey: Fix release notes parsing

--- Tasks/Pix4D.Pix4Dsurvey/Script.ps1
+++ Tasks/Pix4D.Pix4Dsurvey/Script.ps1
@@ -11,12 +11,12 @@ switch -Regex ($this.Check()) {
     try {
       $Object2 = Invoke-WebRequest -Uri 'https://support.pix4d.com/hc/en-us/articles/360033539032' | ConvertFrom-Html
 
-      $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//table[contains(./tbody/tr[2]/td[1]/text(), '$($this.CurrentState.Version)')]")
+      $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//table[contains(./tbody/tr[2]/td[1]/text(), '$($this.CurrentState.Version -replace '(\.0+)$')')]")
       if ($ReleaseNotesTitleNode) {
         # ReleaseTime
         $this.CurrentState.ReleaseTime = [regex]::Match($ReleaseNotesTitleNode.InnerText, '([a-zA-Z]+\W+\d{1,2}\W+20\d{2})').Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
 
-        $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node -and $Node.Name -ne 'h3'; $Node = $Node.NextSibling) {
+        $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node -and -not ($Node.Name -eq 'h3' -and $Node.InnerText.Contains('Version')) -and $Node.Name -ne 'hr'; $Node = $Node.NextSibling) {
           if (-not $Node.SelectSingleNode("./strong[contains(., '$($this.CurrentState.Version)')]")) {
             $Node
           }
