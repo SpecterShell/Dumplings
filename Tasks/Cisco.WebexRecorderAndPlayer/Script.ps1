e1861a5301229601478393b3a0c9fb6c2e93fa93
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-08-15 17:51:39 +0800

    Cisco.WebexRecorderAndPlayer: Switch to Last-Modified header entry for update detection

--- Tasks/Cisco.WebexRecorderAndPlayer/Script.ps1
+++ Tasks/Cisco.WebexRecorderAndPlayer/Script.ps1
@@ -21,15 +21,13 @@ $this.CurrentState.Installer += [ordered]@{
   InstallerUrl = $Object1.Links.Where({ try { $_.href.EndsWith('.msi') -and $_.href.Contains('atrecply') } catch {} }, 'First')[0].href
 }
 
-$Object2 = Invoke-WebRequest -Uri $this.CurrentState.Installer[0].InstallerUrl -Method Head
-$ETag = $Object2.Headers.ETag[0]
+$Object1 = Invoke-WebRequest -Uri $this.CurrentState.Installer[0].InstallerUrl -Method Head
+# Last Modified
+$this.CurrentState.LastModified = $Object1.Headers.'Last-Modified'[0]
 
 # Case 0: Force submit the manifest
 if ($Global:DumplingsPreference.Contains('Force')) {
   $this.Log('Skip checking states', 'Info')
-
-  # ETag
-  $this.CurrentState.ETag = @($ETag)
 
   Read-Installer
 
@@ -44,9 +42,6 @@ if ($Global:DumplingsPreference.Contains('Force')) {
 if ($this.Status.Contains('New')) {
   $this.Log('New task', 'Info')
 
-  # ETag
-  $this.CurrentState.ETag = @($ETag)
-
   Read-Installer
 
   $this.Print()
@@ -54,9 +49,12 @@ if ($this.Status.Contains('New')) {
   return
 }
 
-# Case 2: The ETag is unchanged
-if ($ETag -in $this.LastState.ETag) {
+# Case 2: The Last Modified is unchanged
+if ([datetime]$this.CurrentState.LastModified -eq [datetime]$this.LastState.LastModified) {
   $this.Log("The version $($this.LastState.Version) from the last state is the latest", 'Info')
+  return
+} elseif ([datetime]$this.CurrentState.LastModified -lt [datetime]$this.LastState.LastModified) {
+  $this.Log("The last modified datetime from the current state `"$($this.CurrentState.LastModified)`" is older than the one from the last state `"$($this.LastState.LastModified)`"", 'Warning')
   return
 }
 
@@ -67,31 +65,25 @@ if ([string]::IsNullOrWhiteSpace($this.CurrentState.Version)) {
   throw 'The current state has an invalid version'
 }
 
-# Case 4: The ETag has changed, but the SHA256 is not
+# Case 4: The Last Modified has updated, but the SHA256 is not
 if ($this.CurrentState.Installer[0].InstallerSha256 -eq $this.LastState.Installer[0].InstallerSha256) {
-  $this.Log('The ETag has changed, but the SHA256 is not', 'Info')
-
-  # ETag
-  $this.CurrentState.ETag = $this.LastState.ETag + $ETag
+  $this.Log('The Last Modified has changed, but the SHA256 is not', 'Info')
 
   $this.Write()
   return
 }
 
-# ETag
-$this.CurrentState.ETag = @($ETag)
-
 switch -Regex ($this.Check()) {
-  # Case 6: The ETag, the SHA256 and the version have changed
+  # Case 6: The Last Modified, the SHA256 and the version have changed
   'Updated|Rollbacked' {
     $this.Print()
     $this.Write()
     $this.Message()
     $this.Submit()
   }
-  # Case 5: The ETag and the SHA256 have changed, but the version is not
-  Default {
-    $this.Log('The ETag and the SHA256 have changed, but the version is not', 'Info')
+  # Case 5: The Last Modified and the SHA256 have changed, but the version is not
+  default {
+    $this.Log('The Last Modified and the SHA256 have changed, but the version is not', 'Info')
     $this.Config.IgnorePRCheck = $true
     $this.Print()
     $this.Write()
