bcf99a9e7073304ada6c0dcbe0f6646806321eb1
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-08-20 10:41:35 +0800

    Add ModelScope.FlowBench

---
+++ Tasks/ModelScope.FlowBench/Script.ps1
@@ -0,0 +1,27 @@
+$Object1 = Invoke-RestMethod -Uri 'https://modelscope.cn/api/v1/aigc/flowbench/deps'
+
+# Version
+$this.CurrentState.Version = $Object1.Data.Preview.LatestVersion
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerType = 'msix'
+  InstallerUrl  = $Object1.Data.Preview.Versions.($this.CurrentState.Version).Win
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.InstallerFiles[$this.CurrentState.Installer[0].InstallerUrl] = $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+    # RealVersion
+    $this.CurrentState.RealVersion = $InstallerFile | Read-ProductVersionFromExe
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
