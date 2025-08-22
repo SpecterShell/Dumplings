b7ad4d4a38e192bc7bb1b05f5c31c23d160dc94c
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-08-22 17:49:38 +0800

    Add FellowInsights.Fellow

---
+++ Tasks/FellowInsights.Fellow/Script.ps1
@@ -0,0 +1,20 @@
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = Get-RedirectedUrl -Uri 'https://fellow.app/desktop/download/win32/latest/'
+}
+
+# Version
+$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
