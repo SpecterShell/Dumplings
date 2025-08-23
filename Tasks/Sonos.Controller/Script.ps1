b1553e1b7524bfe1289256873d9fa3264a1e9617
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-08-23 13:55:08 +0800

    Add Sonos

---
+++ Tasks/Sonos.Controller/Script.ps1
@@ -0,0 +1,36 @@
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = Get-RedirectedUrl -Uri 'https://www.sonos.com/redir/controller_software_pc2'
+}
+
+# Version
+$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d+(?:\.\d+)+-\d+)').Groups[1].Value.Replace('-', '.')
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    foreach ($Installer in $this.CurrentState.Installer) {
+      $this.InstallerFiles[$Installer.InstallerUrl] = $InstallerFile = Get-TempFile -Uri $Installer.InstallerUrl
+      $InstallerFileExtracted = $InstallerFile | Expand-InstallShield
+      $InstallerFile2 = Join-Path $InstallerFileExtracted 'Sonos.msi'
+      # ProductCode
+      $Installer['ProductCode'] = $InstallerFile2 | Read-ProductCodeFromMsi
+      # AppsAndFeaturesEntries
+      $Installer['AppsAndFeaturesEntries'] = @(
+        [ordered]@{
+          UpgradeCode   = $InstallerFile2 | Read-UpgradeCodeFromMsi
+          InstallerType = 'msi'
+        }
+      )
+      Remove-Item -Path $InstallerFileExtracted -Recurse -Force -ErrorAction 'Continue' -ProgressAction 'SilentlyContinue'
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
