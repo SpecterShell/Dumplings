736cc9ffc7397444a5d4908bbb1a431b3f956dfa
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-08-23 14:28:04 +0800

    Add Trimble.TrimbleConnect

---
+++ Tasks/Trimble.TrimbleConnect/Script.ps1
@@ -0,0 +1,75 @@
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = Get-RedirectedUrl1st -Uri 'https://download.tekla.com/download-trimble-connect'
+}
+
+# Version
+$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    foreach ($Installer in $this.CurrentState.Installer) {
+      $this.InstallerFiles[$Installer.InstallerUrl] = $InstallerFile = Get-TempFile -Uri $Installer.InstallerUrl
+      $InstallerFileExtracted = $InstallerFile | Expand-InstallShield
+      $InstallerFile2 = Join-Path $InstallerFileExtracted 'Trimble Connect.msi'
+      # ProductCode
+      $Installer['ProductCode'] = $InstallerFile2 | Read-ProductCodeFromMsi
+      # AppsAndFeaturesEntries
+      $Installer['AppsAndFeaturesEntries'] = @(
+        [ordered]@{
+          UpgradeCode   = $InstallerFile2 | Read-UpgradeCodeFromMsi
+          InstallerType = 'msi'
+        }
+      )
+      Remove-Item -Path $InstallerFileExtracted -Recurse -Force -ErrorAction 'Continue' -ProgressAction 'SilentlyContinue'
+    }
+
+    try {
+      # ReleaseNotesUrl (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotesUrl'
+        Value  = $ReleaseNotesUrl = 'https://community.trimble.com/communities/supportcommunity?CommunityKey=2ba352bf-190c-4712-8473-09e331f39bf2'
+      }
+
+      $Object2 = Invoke-WebRequest -Uri $ReleaseNotesUrl
+      if ($ReleaseNotesUrlLink = $Object2.Links.Where({ try { $_.outerHTML.Contains('Trimble Connect') -and $_.outerHTML.Contains($this.CurrentState.Version.Split('.')[0..2] -join '.') } catch {} }, 'First')) {
+        # ReleaseNotesUrl (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotesUrl'
+          Value  = $ReleaseNotesUrl = Join-Uri $ReleaseNotesUrl $ReleaseNotesUrlLink[0].href
+        }
+
+        $Object3 = Invoke-WebRequest -Uri $ReleaseNotesUrl | ConvertFrom-Html
+
+        # ReleaseTime
+        $this.CurrentState.ReleaseTime = [regex]::Match($Object3.SelectSingleNode('//div[@id="MainCopy_ctl10_EntryTitlePanel"]//h2').InnerText, '([a-zA-Z]+\W+\d{1,2}\W+20\d{2})').Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
+
+        # Remove tags
+        $Object3.SelectNodes('//a[@class="user-content-hashtag"]').ForEach({ $_.Remove() })
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $Object3.SelectSingleNode('//div[@id="MainCopy_ctl17_DetailPanel"]/div[last()]/div[last()]') | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotesUrl (en-US), ReleaseNotes (en-US) and ReleaseTime for version $($this.CurrentState.Version)", 'Warning')
+      }
+
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
