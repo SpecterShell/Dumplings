dcad0cea6ae45ac1cac9df7eaf1761a9d8597ba9
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-08-24 13:20:12 +0800

    Add TexasInstruments.TI-Admin

---
+++ Tasks/TexasInstruments.TI-Admin/Script.ps1
@@ -0,0 +1,23 @@
+$Prefix = 'https://education.ti.com/en/software/details/en/86107044888141eaa25d80f1ad625628/ti-admin'
+$Object1 = Invoke-WebRequest -Uri $Prefix
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = Join-Uri $Prefix $Object1.Links.Where({ try { $_.href.EndsWith('.exe') } catch {} }, 'First')[0].href
+}
+
+# Version
+$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
