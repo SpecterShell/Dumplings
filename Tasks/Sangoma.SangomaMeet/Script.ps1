38d01633c3f4b46717e1b76029b828b189b064fb
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-08-29 12:28:55 +0800

    Sangoma.SangomaMeet: Extract version from redirected feed URL

--- Tasks/Sangoma.SangomaMeet/Script.ps1
+++ Tasks/Sangoma.SangomaMeet/Script.ps1
@@ -1,23 +1,27 @@
-$Object1 = Invoke-RestMethod -Uri 'https://meet.sangoma.com/apps/desktop/latest/windows/latest.yml' | ConvertFrom-Yaml
+$Object1 = Invoke-WebRequest -Uri 'https://meet.sangoma.com/apps/desktop/latest/windows/latest.yml'
+$Object2 = $Object1 | Read-ResponseContent | ConvertFrom-Yaml
 
 # Version
-$this.CurrentState.Version = $Object1.version
+$this.CurrentState.Version = [regex]::Match($Object1.BaseResponse.RequestMessage.RequestUri.AbsoluteUri, '(\d+(?:\.\d+)+(?:-\d+)?)').Groups[1].Value
+
+# RealVersion
+$this.CurrentState.RealVersion = $Object2.version
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
   Architecture = 'x86'
-  InstallerUrl = Join-Uri "https://meet.sangoma.com/apps/desktop/$($this.CurrentState.Version)/windows/" $Object1.files.Where({ $_.url.Contains('ia32') }, 'First')[0].url | Split-Uri -LeftPart Path
+  InstallerUrl = Join-Uri "https://meet.sangoma.com/apps/desktop/$($this.CurrentState.Version)/windows/" $Object2.files.Where({ $_.url.Contains('ia32') }, 'First')[0].url | Split-Uri -LeftPart Path
 }
 $this.CurrentState.Installer += [ordered]@{
   Architecture = 'x64'
-  InstallerUrl = Join-Uri "https://meet.sangoma.com/apps/desktop/$($this.CurrentState.Version)/windows/" $Object1.files.Where({ $_.url.Contains('x64') }, 'First')[0].url | Split-Uri -LeftPart Path
+  InstallerUrl = Join-Uri "https://meet.sangoma.com/apps/desktop/$($this.CurrentState.Version)/windows/" $Object2.files.Where({ $_.url.Contains('x64') }, 'First')[0].url | Split-Uri -LeftPart Path
 }
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
     try {
       # ReleaseTime
-      $this.CurrentState.ReleaseTime = $Object1.releaseDate | Get-Date -AsUTC
+      $this.CurrentState.ReleaseTime = $Object2.releaseDate | Get-Date -AsUTC
     } catch {
       $_ | Out-Host
       $this.Log($_, 'Warning')
