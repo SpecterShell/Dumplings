6255047d7b810f086df52909b8499b3254ed38cd
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-08-29 12:38:52 +0800

    GoTo.GoTo: Add machine installer

--- Tasks/GoTo.GoTo/Script.ps1
+++ Tasks/GoTo.GoTo/Script.ps1
@@ -1,13 +1,25 @@
 $Prefix = 'https://goto-desktop.goto.com/'
-
 $Object1 = Invoke-RestMethod -Uri "${Prefix}latest.yml" | ConvertFrom-Yaml
+
+$PrefixMachine = 'https://goto-desktop.goto.com/machine/'
+$Object2 = Invoke-RestMethod -Uri "${PrefixMachine}latest.yml" | ConvertFrom-Yaml
+
+if ($Object1.version -ne $Object2.version) {
+  $this.Log("Inconsistent versions: User: $($Object1.version), Machine: $($Object2.version)", 'Error')
+  return
+}
 
 # Version
 $this.CurrentState.Version = $Object1.version
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
+  Scope        = 'user'
   InstallerUrl = Join-Uri $Prefix $Object1.files[0].url
+}
+$this.CurrentState.Installer += [ordered]@{
+  Scope        = 'machine'
+  InstallerUrl = Join-Uri $PrefixMachine $Object2.files[0].url
 }
 
 switch -Regex ($this.Check()) {
