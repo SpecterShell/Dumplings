0ff9b9522b054f4436c954143f089095574e61c8
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-08-29 11:36:59 +0800

    TheBrowserCompany.Arc: Fix release notes parsing

--- Tasks/TheBrowserCompany.Arc/Script.ps1
+++ Tasks/TheBrowserCompany.Arc/Script.ps1
@@ -11,6 +11,7 @@ if ($Object1.AppInstaller.Version -ne $Object2.AppInstaller.Version) {
 
 # Version
 $this.CurrentState.Version = $Object1.AppInstaller.Version
+$ShortVersion = $this.CurrentState.Version.Split('.')[0..2] -join '.' -replace '(\.0+)+$'
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
@@ -27,7 +28,7 @@ switch -Regex ($this.Check()) {
     try {
       $Object3 = curl -fsSLA $DumplingsInternetExplorerUserAgent 'https://arc.net/windows/release-notes' | Join-String -Separator "`n" | ConvertFrom-Html
 
-      $ReleaseNotesTitleNode = $Object3.SelectSingleNode("//p[contains(text(), 'V$($this.CurrentState.Version.Split('.')[0..2] -join '.')')]")
+      $ReleaseNotesTitleNode = $Object3.SelectSingleNode("//p[contains(text(), 'V${ShortVersion}') or contains(text(), 'V.${ShortVersion}')]")
       if ($ReleaseNotesTitleNode) {
         # ReleaseTime
         $this.CurrentState.ReleaseTime = [regex]::Match($ReleaseNotesTitleNode.SelectSingleNode('./preceding-sibling::h2[1]').InnerText, '([a-zA-Z]+\W+\d{1,2}\W+20\d{2})').Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
