49fc3687b6c1dc6a80e77f498f65af5e726f705f
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-08-30 11:45:21 +0800

    Lebo.Lebo: Switch to download source

--- Tasks/Lebo.Lebo#5/Script.ps1
+++ Tasks/Lebo.Lebo#5/Script.ps1
@@ -1,28 +1,13 @@
-$Object1 = (Invoke-WebRequest -Uri 'http://hotupgrade.hpplay.cn:8999/Author/UpdateApp' -Body @{
-    appid       = '10619'
-    model       = 'Windows'
-    api_version = $this.Status.Contains('New') ? '50566' : $this.LastState.VersionCode
-    mac         = '00:00:00:00:00:00'
-    tid         = '1'
-    uid         = '1'
-  }).Content | ConvertFrom-Json -AsHashtable
-
-if ($Object1.data.Count -eq 0) {
-  $this.Log("The version $($this.LastState.Version) from the last state is the latest, skip checking", 'Info')
-  return
-}
+$Object1 = (Invoke-RestMethod -Uri 'https://www.lebo.cn/downUrl.json').result.Where({ $_.type -eq 'pc' }, 'First')[0]
 
 # Version
-$this.CurrentState.Version = $Object1.data.cversion
-
-# VersionCode
-$this.CurrentState.VersionCode = $Object1.data.aversion
+$this.CurrentState.Version = $Object1.version
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
   Query             = @{}
   Architecture      = 'x86'
-  InstallerUrl      = $Object1.data.furl
+  InstallerUrl      = $Object1.url
   InstallerSwitches = @{}
   ProductCode       = 'PCCast'
 }
@@ -31,14 +16,7 @@ switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
     try {
       # ReleaseTime
-      $this.CurrentState.ReleaseTime = $Object1.data.updatetime | ConvertFrom-UnixTimeMilliseconds
-
-      # ReleaseNotes (zh-CN)
-      $this.CurrentState.Locale += [ordered]@{
-        Locale = 'zh-CN'
-        Key    = 'ReleaseNotes'
-        Value  = $Object1.data.upNotes | Format-Text
-      }
+      $this.CurrentState.ReleaseTime = $Object1.prompt | Get-Date -Format 'yyyy-MM-dd'
     } catch {
       $_ | Out-Host
       $this.Log($_, 'Warning')
