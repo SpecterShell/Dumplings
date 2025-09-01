e1483d88c97ed180d725477af780a7a08583c96d
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-09-01 11:39:35 +0800

    Add AceBIT.PasswordDepot.18.Corporate

---
+++ Tasks/AceBIT.PasswordDepot.18.Corporate/Script.ps1
@@ -0,0 +1,45 @@
+$Object1 = (Invoke-RestMethod -Uri 'https://www.password-depot.de/download/pdepot18.info').versioninfo.version.Where({ $_.edition -eq 'CORPORATE' }, 'First')[0]
+
+# Version
+$this.CurrentState.Version = $Object1.number
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = Join-Uri 'https://www.password-depot.de/download/v18/' $Object1.setupfile
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      $Object2 = Invoke-WebRequest -Uri 'https://www.password-depot.de/en/resources/whats-new.htm' | ConvertFrom-Html
+
+      $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//h2[contains(., '$($this.CurrentState.Version)')]")
+      if ($ReleaseNotesTitleNode) {
+        # ReleaseTime
+        $this.CurrentState.ReleaseTime = [datetime]::ParseExact([regex]::Match($ReleaseNotesTitleNode.InnerText, '(\d{1,2}\.\d{1,2}\.20\d{2})').Groups[1].Value, 'dd.MM.yyyy', $null).ToString('yyyy-MM-dd')
+
+        # ReleaseNotes (en-US)
+        $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node -and $Node.Name -ne 'h2'; $Node = $Node.NextSibling) { $Node }
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseTime and ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
