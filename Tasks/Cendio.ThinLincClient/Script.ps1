5ff7456bf2e72132376a55e5a8bb3be01e3901b3
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-09-02 13:49:51 +0800

    Add Cendio.ThinLincClient

---
+++ Tasks/Cendio.ThinLincClient/Script.ps1
@@ -0,0 +1,55 @@
+$Object1 = Invoke-WebRequest -Uri 'https://www.cendio.com/thinlinc/download/'
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.Links.Where({ try { $_.href.EndsWith('.exe') } catch {} }, 'First')[0].href
+}
+
+# Version
+$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseNotesUrl (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotesUrl'
+        Value  = $ReleaseNotesUrl = 'https://www.cendio.com/thinlinc/docs/relnotes/'
+      }
+
+      $Object2 = Invoke-WebRequest -Uri $ReleaseNotesUrl
+      if ($ReleaseNotesUrlLink = $Object2.Links.Where({ try { $_.href.Contains($this.CurrentState.Version) } catch {} }, 'First')) {
+        # ReleaseNotesUrl (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotesUrl'
+          Value  = $ReleaseNotesUrl = $ReleaseNotesUrlLink[0].href
+        }
+
+        $Object3 = Invoke-WebRequest -Uri $ReleaseNotesUrl | ConvertFrom-Html
+
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $Object3.SelectSingleNode('//div[@class="content-text"]') | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotesUrl and ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
