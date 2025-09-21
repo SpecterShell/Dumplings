b489a348fe5733e6116a3bf241a72ae53c1d2dc3
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-09-21 11:33:13 +0800

    Add DSwiss.SecureSafe

---
+++ Tasks/DSwiss.SecureSafe/Script.ps1
@@ -0,0 +1,24 @@
+# $Object1 = Invoke-RestMethod -Uri 'https://sync.securesafe.com/api/sync/1/public/version/win'
+
+$Object1 = Invoke-WebRequest -Uri 'https://www.securesafe.com/en/product/passwords-files'
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.Links.Where({ try { $_.href.EndsWith('.exe') } catch {} }, 'First')[0].href
+}
+
+# Version
+$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
