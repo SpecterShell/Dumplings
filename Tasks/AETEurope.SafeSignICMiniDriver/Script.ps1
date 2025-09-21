a4fe2f11564fa9737a09d07e560409a8e3879728
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-09-21 17:25:03 +0800

    Add AETEurope.SafeSignICMiniDriver

---
+++ Tasks/AETEurope.SafeSignICMiniDriver/Script.ps1
@@ -0,0 +1,33 @@
+$Object1 = Invoke-WebRequest -Uri 'https://www.uziregister.nl/uzi-pas/activeer-en-installeer-uw-uzi-pas/overzicht-safesign-software'
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.Links.Where({ try { $_.href.EndsWith('.zip') -and $_.outerHTML.Contains('Windows 64-bit') } catch {} }, 'First')[0].href
+}
+
+# Version
+$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    foreach ($Installer in $this.CurrentState.Installer) {
+      $this.InstallerFiles[$Installer.InstallerUrl] = $InstallerFile = Get-TempFile -Uri $Installer.InstallerUrl
+      $ZipFile = [System.IO.Compression.ZipFile]::OpenRead($InstallerFile)
+      $Installer['NestedInstallerFiles'] = @(
+        [ordered]@{
+          RelativeFilePath = $ZipFile.Entries.Where({ $_.FullName.EndsWith('.msi') }, 'First')[0].FullName.Replace('/', '\')
+        }
+      )
+      $ZipFile.Dispose()
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
