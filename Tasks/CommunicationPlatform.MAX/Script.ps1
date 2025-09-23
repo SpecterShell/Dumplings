04be8536ba1350a12bbfac25524d47181d1de39c
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-09-23 16:11:40 +0800

    Add CommunicationPlatform.MAX

---
+++ Tasks/CommunicationPlatform.MAX/Script.ps1
@@ -0,0 +1,22 @@
+$Object1 = Invoke-RestMethod -Uri 'https://download.max.ru/win/release/update.xml'
+
+# Version
+$this.CurrentState.Version = $Object1.enclosure.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.enclosure.url
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
