7c17252b976073b20ab588632cc9971d2a5b443f
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-09-24 15:19:47 +0800

    AMustInEveryOffice.ASAPUtilities: Skip removing download links from the release notes

--- Tasks/AMustInEveryOffice.ASAPUtilities.Business/Script.ps1
+++ Tasks/AMustInEveryOffice.ASAPUtilities.Business/Script.ps1
@@ -27,19 +27,11 @@ switch -Regex ($this.Check()) {
       $Object3 = (Invoke-RestMethod -Uri 'https://www.asap-utilities.com/rss.php').Where({ $_.title.Contains($this.CurrentState.Version) }, 'First')
 
       if ($Object3) {
-        $ReleaseNotesObject = $Object3[0].description.'#cdata-section' | ConvertFrom-Html
-
-        # Remove download section
-        $SectionTitleNode = $ReleaseNotesObject.SelectSingleNode('./h3[contains(text(), "Download Now")]')
-        $Nodes = for ($Node = $SectionTitleNode.NextSibling; $Node -and $Node.Name -ne 'h3'; $Node = $Node.NextSibling) { $Node }
-        $SectionTitleNode.Remove()
-        $Nodes.ForEach({ $_.Remove() })
-
         # ReleaseNotes (en-US)
         $this.CurrentState.Locale += [ordered]@{
           Locale = 'en-US'
           Key    = 'ReleaseNotes'
-          Value  = $ReleaseNotesObject | Get-TextContent | Format-Text
+          Value  = $Object3[0].description.'#cdata-section' | ConvertFrom-Html | Get-TextContent | Format-Text
         }
       } else {
         $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
--- Tasks/AMustInEveryOffice.ASAPUtilities.HomeStudent/Script.ps1
+++ Tasks/AMustInEveryOffice.ASAPUtilities.HomeStudent/Script.ps1
@@ -1,6 +1,6 @@
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = $Global:DumplingsStorage.ASAPUtilitiesDownloadPage.Links.Where({ try { $_.href.EndsWith('.exe') -and $_.href.Contains('HS') } catch {} }, 'First')[0].href
+  InstallerUrl = $Global:DumplingsStorage.ASAPUtilitiesDownloadPage.Links.Where({ try { $_.href.EndsWith('.exe') -and $_.href.Contains('HS') } catch {} }, 'First')[0].href.Replace('server2.asap-utilities.com', 'server1.asap-utilities.com')
 }
 
 # Version
@@ -12,19 +12,11 @@ switch -Regex ($this.Check()) {
       $Object2 = (Invoke-RestMethod -Uri 'https://www.asap-utilities.com/rss.php').Where({ $_.title.Contains($this.CurrentState.Version) }, 'First')
 
       if ($Object2) {
-        $ReleaseNotesObject = $Object2[0].description.'#cdata-section' | ConvertFrom-Html
-
-        # Remove download section
-        $SectionTitleNode = $ReleaseNotesObject.SelectSingleNode('./h3[contains(text(), "Download Now")]')
-        $Nodes = for ($Node = $SectionTitleNode.NextSibling; $Node -and $Node.Name -ne 'h3'; $Node = $Node.NextSibling) { $Node }
-        $SectionTitleNode.Remove()
-        $Nodes.ForEach({ $_.Remove() })
-
         # ReleaseNotes (en-US)
         $this.CurrentState.Locale += [ordered]@{
           Locale = 'en-US'
           Key    = 'ReleaseNotes'
-          Value  = $ReleaseNotesObject | Get-TextContent | Format-Text
+          Value  = $Object2[0].description.'#cdata-section' | ConvertFrom-Html | Get-TextContent | Format-Text
         }
       } else {
         $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
