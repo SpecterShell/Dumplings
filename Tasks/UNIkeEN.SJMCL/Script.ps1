006b5e5b158c1156f1bd185d62ce3ca265e64094
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-09-28 17:46:13 +0800

    Add UNIkeEN.SJMCL

---
+++ Tasks/UNIkeEN.SJMCL/Script.ps1
@@ -0,0 +1,60 @@
+$RepoOwner = 'UNIkeEN'
+$RepoName = 'SJMCL'
+
+$Object1 = Invoke-GitHubApi -Uri "https://api.github.com/repos/${RepoOwner}/${RepoName}/releases/latest"
+
+# Version
+$this.CurrentState.Version = $Object1.tag_name -creplace '^v'
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x86'
+  InstallerUrl = Join-Uri 'https://mc.sjtu.cn/sjmcl/releases/' $Object1.assets.Where({ $_.name.EndsWith('.msi') -and $_.name.Contains('i686') }, 'First')[0].name
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = Join-Uri 'https://mc.sjtu.cn/sjmcl/releases/' $Object1.assets.Where({ $_.name.EndsWith('.msi') -and $_.name.Contains('x86_64') }, 'First')[0].name
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'arm64'
+  InstallerUrl = Join-Uri 'https://mc.sjtu.cn/sjmcl/releases/' $Object1.assets.Where({ $_.name.EndsWith('.msi') -and $_.name.Contains('aarch64') }, 'First')[0].name
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.published_at.ToUniversalTime()
+
+      if (-not [string]::IsNullOrWhiteSpace($Object1.body)) {
+        # ReleaseNotes (zh-CN)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'zh-CN'
+          Key    = 'ReleaseNotes'
+          Value  = $Object1.body | Convert-MarkdownToHtml -Extensions 'advanced', 'emojis', 'hardlinebreak' | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotes (zh-CN) for version $($this.CurrentState.Version)", 'Warning')
+      }
+
+      # ReleaseNotesUrl (zh-CN)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'zh-CN'
+        Key    = 'ReleaseNotesUrl'
+        Value  = $Object1.html_url
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
