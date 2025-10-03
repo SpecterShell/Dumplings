a9656171eac8ac9e9a1e8c57314b7d438f3b3675
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-10-03 12:26:28 +0800

    Add IBM.Semeru.25

---
+++ Tasks/IBM.Semeru.25.JRE/Script.ps1
@@ -0,0 +1,49 @@
+$RepoOwner = 'ibmruntimes'
+$RepoName = 'semeru25-binaries'
+
+$Object1 = Invoke-GitHubApi -Uri "https://api.github.com/repos/${RepoOwner}/${RepoName}/releases/latest"
+
+$VersionMatches = [regex]::Match($Object1.tag_name, 'jdk-(?<Major>\d+)(?:\.(?<Minor>\d+))?(?:\.(?<Patch>\d+))?\+(?<Build>\d+)')
+
+$VersionBuilder = [System.Text.StringBuilder]::new($VersionMatches.Groups['Major'].Value).Append('.')
+$VersionBuilder = $VersionMatches.Groups['Minor'].Success ? $VersionBuilder.Append($VersionMatches.Groups['Minor'].Value) : $VersionBuilder.Append('0')
+$VersionBuilder = $VersionBuilder.Append('.')
+$VersionBuilder = $VersionMatches.Groups['Patch'].Success ? $VersionBuilder.Append($VersionMatches.Groups['Patch'].Value) : $VersionBuilder.Append('0')
+$VersionBuilder = $VersionBuilder.Append('.').Append($VersionMatches.Groups['Build'].Value)
+
+# Version
+$this.CurrentState.Version = $VersionBuilder.ToString()
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = $Object1.assets.Where({ $_.name.EndsWith('.msi') -and $_.name.Contains('jre') }, 'First').browser_download_url | ConvertTo-UnescapedUri
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.published_at.ToUniversalTime()
+
+      # ReleaseNotesUrl (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotesUrl'
+        Value  = $Object1.html_url
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
