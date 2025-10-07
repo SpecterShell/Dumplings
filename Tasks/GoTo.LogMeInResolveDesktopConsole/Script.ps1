b506c2c20a9339edd739c11e0041dd592a768551
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-10-07 10:27:46 +0800

    Add GoTo.LogMeInResolveDesktopConsole

---
+++ Tasks/GoTo.LogMeInResolveDesktopConsole/Script.ps1
@@ -0,0 +1,41 @@
+$Object1 = (Invoke-RestMethod -Uri 'https://cdn.console.gotoresolve.com/dtc/update.json').infos.Where({ $_.product -eq 'resolve' }, 'First')[0].updates[-1]
+
+# Version
+$this.CurrentState.Version = $Object1.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.downloadUrl
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      $Object2 = Invoke-WebRequest -Uri 'https://support.logmein.com/resolve/help/whats-new-in-the-logmein-resolve-desktop-technician-console' | ConvertFrom-Html
+
+      $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//h2[contains(text(), '$($this.CurrentState.Version)')]")
+      if ($ReleaseNotesTitleNode) {
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesTitleNode.SelectNodes('./following-sibling::node()') | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
