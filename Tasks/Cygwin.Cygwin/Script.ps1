d0b7b98a5e91a443a65e4b659ed93350953a2499
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-10-21 17:41:59 +0800

    Add Cygwin.Cygwin

---
+++ Tasks/Cygwin.Cygwin/Script.ps1
@@ -0,0 +1,29 @@
+$Object1 = Invoke-RestMethod -Uri 'https://mirrors.kernel.org/sourceware/cygwin/x86_64/setup.ini'
+
+# Version
+$this.CurrentState.Version = [regex]::Match($Object1, '(?s)@ cygwin.+?version: (\S+)').Groups[1].Value
+
+$SetupVersion = [regex]::Match($Object1, '(?m)setup-version: (\S+)').Groups[1].Value
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x86'
+  InstallerUrl = "https://cygwin.com/setup/setup-${SetupVersion}.x86.exe"
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = "https://cygwin.com/setup/setup-${SetupVersion}.x86_64.exe"
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
