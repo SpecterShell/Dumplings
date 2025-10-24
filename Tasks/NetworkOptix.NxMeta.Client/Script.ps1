105a7561e9fc7cd2a8152126273f9afb84746529
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-10-24 11:34:03 +0800

    Add NetworkOptix.NxMeta

---
+++ Tasks/NetworkOptix.NxMeta.Client/Script.ps1
@@ -0,0 +1,57 @@
+$Object1 = $Global:DumplingsStorage.NxMetaApps
+
+# Version
+$this.CurrentState.Version = $Object1.releases.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = "$($Object1.updatesPrefix)/$($Object1.releases.buildNumber)/$($Object1.releases.platforms.Where({ $_.name -eq 'windows' })[0].files.Where({ $_.platform -eq 'windows_x64' -and $_.appType -eq 'client' }, 'First')[0].path)"
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.releases.date | Get-Date -Format 'yyyy-MM-dd'
+
+      # ReleaseNotesUrl (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotesUrl'
+        Value  = $Object1.releases.releaseNotes
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    try {
+      $Object2 = Invoke-RestMethod -Uri 'https://meta.nxvms.com/api/utils/downloads/history'
+
+      $ReleaseNotesObject = $Object2.releases.Where({ $_.version -eq $this.CurrentState.Version }, 'First')
+      if ($ReleaseNotesObject) {
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesObject[0].releaseNotes | ConvertFrom-Html | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseTime and ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
