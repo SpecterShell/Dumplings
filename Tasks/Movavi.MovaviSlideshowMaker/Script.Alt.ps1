bb0ffbbf852996f948e5eb8cf35438ed313fc18f
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-11-01 14:06:41 +0800

    Add Movavi.MovaviSlideshowMaker

---
+++ Tasks/Movavi.MovaviSlideshowMaker/Script.Alt.ps1
@@ -0,0 +1,23 @@
+$Object1 = Invoke-RestMethod -Uri 'https://dl.movavi.com/content/wi/resources/release/movavi/ssm/ssm_win_x64/resources/config.json'
+
+# Version
+$this.CurrentState.Version = $Object1.data.Product.version.DefaultValue
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.data.Installers[0].url.DefaultValue
+  ProductCode  = "Movavi Slideshow Maker $($this.CurrentState.Version.Split('.')[0])"
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
