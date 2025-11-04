588ff2640726c8ff22008c0a0ccd742c7ee8d703
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-11-04 11:01:54 +0800

    SonicWall.NetExtender: Remove arm64 installer

--- Tasks/SonicWall.NetExtender/Script.ps1
+++ Tasks/SonicWall.NetExtender/Script.ps1
@@ -12,9 +12,9 @@ $InstallerX64MSIUrl = $Object4.link.href.Trim()
 $VersionX64MSI = [regex]::Match($InstallerX64MSIUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
 
 # arm64 MSI
-$Object5 = $Object2.SelectTokens('$..cta_group[?(@.link.href =~ /NetExtender-arm64.+\.msi/)]').Where({ $true }, 'Last')[0].ToString() | ConvertFrom-Json
-$InstallerARM64MSIUrl = $Object5.link.href.Trim()
-$VersionARM64MSI = [regex]::Match($InstallerARM64MSIUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
+# $Object5 = $Object2.SelectTokens('$..cta_group[?(@.link.href =~ /NetExtender-arm64.+\.msi/)]').Where({ $true }, 'Last')[0].ToString() | ConvertFrom-Json
+# $InstallerARM64MSIUrl = $Object5.link.href.Trim()
+# $VersionARM64MSI = [regex]::Match($InstallerARM64MSIUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
 
 # x86 EXE
 $Object6 = $Object2.SelectTokens('$..cta_group[?(@.link.href =~ /NXSetupU-x86.+\.exe/)]').Where({ $true }, 'Last')[0].ToString() | ConvertFrom-Json
@@ -32,10 +32,10 @@ $VersionX64EXE = [regex]::Match($InstallerX64EXEUrl, '(\d+(?:\.\d+)+)').Groups[1
 # $VersionARM64EXE = [regex]::Match($InstallerARM64EXEUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
 
 # if (@(@($VersionX86MSI, $VersionX64MSI, $VersionARM64MSI, $VersionX86EXE, $VersionX64EXE, $VersionARM64EXE) | Sort-Object -Unique).Count -gt 1) {
-if (@(@($VersionX86MSI, $VersionX64MSI, $VersionARM64MSI, $VersionX86EXE, $VersionX64EXE) | Sort-Object -Unique).Count -gt 1) {
+if (@(@($VersionX86MSI, $VersionX64MSI, $VersionX86EXE, $VersionX64EXE) | Sort-Object -Unique).Count -gt 1) {
   $this.Log("x86 MSI version: ${VersionX86MSI}")
   $this.Log("x64 MSI version: ${VersionX64MSI}")
-  $this.Log("arm64 MSI version: ${VersionARM64MSI}")
+  # $this.Log("arm64 MSI version: ${VersionARM64MSI}")
   $this.Log("x86 EXE version: ${VersionX86EXE}")
   $this.Log("x64 EXE version: ${VersionX64EXE}")
   # $this.Log("arm64 EXE version: ${VersionARM64EXE}")
@@ -56,11 +56,11 @@ $this.CurrentState.Installer += [ordered]@{
   InstallerType = 'wix'
   InstallerUrl  = $InstallerX64MSIUrl
 }
-$this.CurrentState.Installer += [ordered]@{
-  Architecture  = 'arm64'
-  InstallerType = 'wix'
-  InstallerUrl  = $InstallerARM64MSIUrl
-}
+# $this.CurrentState.Installer += [ordered]@{
+#   Architecture  = 'arm64'
+#   InstallerType = 'wix'
+#   InstallerUrl  = $InstallerARM64MSIUrl
+# }
 $this.CurrentState.Installer += [ordered]@{
   Architecture  = 'x86'
   InstallerType = 'nullsoft'
