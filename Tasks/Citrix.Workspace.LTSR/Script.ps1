84f6172e19b79547999e1a78a931afb228c94ecf
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-11-14 19:52:33 +0800

    Add Citrix.Workspace.LTSR

---
+++ Tasks/Citrix.Workspace.LTSR/Script.ps1
@@ -0,0 +1,32 @@
+$Prefix = 'https://downloadplugins.citrix.com/ReceiverUpdates/Prod'
+
+$Object1 = (Invoke-RestMethod -Uri "${Prefix}/catalog_win2.xml").Catalog.CatalogVersion.Installers.Where({ $_.name -eq '1CDF566D-B2C7-47CA-802F-6283C862E1D6' -or $_.name -eq 'WorkspaceApp' }, 'First')[0].Installer.Where({ $_.Stream -eq 'LTSR' }) | Sort-Object -Property { $_.Version -replace '\d+', { $_.Value.PadLeft(20) } } -Bottom 1
+
+# Version
+$this.CurrentState.Version = $Object1.Version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = "${Prefix}$($Object1.DownloadURL)"
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.StartDate | Get-Date -Format 'yyyy-MM-dd'
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
