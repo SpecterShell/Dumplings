f2f71c92da5b3f37ba592f931277223897177b72
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-11-15 14:42:55 +0800

    Add Movavi.MovaviAcademic

---
+++ Tasks/Movavi.MovaviAcademic/Script.ps1
@@ -0,0 +1,58 @@
+$Object1 = Invoke-RestMethod -Uri 'https://content.movavi.com/sparkle/aca/win/x86_64/appcast/appcast.xml'
+
+# Version
+$this.CurrentState.Version = $Object1.enclosure.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.enclosure.url
+  ProductCode  = "Movavi Academic $($this.CurrentState.Version.Split('.')[0])"
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.pubDate | Get-Date -AsUTC
+
+      <# The release notes URL redirects to home page
+      # ReleaseNotesUrl (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotesUrl'
+        Value  = $ReleaseNotesUrl = $Object1.releaseNotesLink.Where({ try { $_.lang -eq 'en' } catch {} }, 'First')[0].'#text'
+      }
+
+      $Object2 = Invoke-WebRequest -Uri $ReleaseNotesUrl | ConvertFrom-Html
+
+      $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//h3[contains(., '$($this.CurrentState.Version)')]")
+      if ($ReleaseNotesTitleNode) {
+        # Remove prefix '-' in list items
+        $Object2.SelectNodes('//li').ForEach({ $_.InnerHtml = $_.InnerHtml -replace '^-\s*' })
+
+        # ReleaseNotes (en-US)
+        $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node -and $Node.Name -ne 'h3'; $Node = $Node.NextSibling) { $Node }
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+      #>
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
