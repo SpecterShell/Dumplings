181c62610166592e6be10a51d220d6420101b34c
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-11-18 17:13:05 +0800

    Trimble.TrimbleConnect: Remove ReleaseNotes

--- Tasks/Trimble.TrimbleConnect/Script.ps1
+++ Tasks/Trimble.TrimbleConnect/Script.ps1
@@ -24,45 +24,6 @@ switch -Regex ($this.Check()) {
       Remove-Item -Path $InstallerFileExtracted -Recurse -Force -ErrorAction 'Continue' -ProgressAction 'SilentlyContinue'
     }
 
-    try {
-      # ReleaseNotesUrl (en-US)
-      $this.CurrentState.Locale += [ordered]@{
-        Locale = 'en-US'
-        Key    = 'ReleaseNotesUrl'
-        Value  = $ReleaseNotesUrl = 'https://community.trimble.com/communities/supportcommunity?CommunityKey=2ba352bf-190c-4712-8473-09e331f39bf2'
-      }
-
-      $Object2 = Invoke-WebRequest -Uri $ReleaseNotesUrl
-      if ($ReleaseNotesUrlLink = $Object2.Links.Where({ try { $_.outerHTML.Contains('Trimble Connect') -and $_.outerHTML.Contains($this.CurrentState.Version.Split('.')[0..2] -join '.') } catch {} }, 'First')) {
-        # ReleaseNotesUrl (en-US)
-        $this.CurrentState.Locale += [ordered]@{
-          Locale = 'en-US'
-          Key    = 'ReleaseNotesUrl'
-          Value  = $ReleaseNotesUrl = Join-Uri $ReleaseNotesUrl $ReleaseNotesUrlLink[0].href
-        }
-
-        $Object3 = Invoke-WebRequest -Uri $ReleaseNotesUrl | ConvertFrom-Html
-
-        # ReleaseTime
-        $this.CurrentState.ReleaseTime = [regex]::Match($Object3.SelectSingleNode('//div[@id="MainCopy_ctl10_EntryTitlePanel"]//h2').InnerText, '([a-zA-Z]+\W+\d{1,2}\W+20\d{2})').Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
-
-        # Remove tags
-        $Object3.SelectNodes('//a[@class="user-content-hashtag"]').ForEach({ $_.Remove() })
-        # ReleaseNotes (en-US)
-        $this.CurrentState.Locale += [ordered]@{
-          Locale = 'en-US'
-          Key    = 'ReleaseNotes'
-          Value  = $Object3.SelectSingleNode('//div[@id="MainCopy_ctl17_DetailPanel"]/div[last()]/div[last()]') | Get-TextContent | Format-Text
-        }
-      } else {
-        $this.Log("No ReleaseNotesUrl (en-US), ReleaseNotes (en-US) and ReleaseTime for version $($this.CurrentState.Version)", 'Warning')
-      }
-
-    } catch {
-      $_ | Out-Host
-      $this.Log($_, 'Warning')
-    }
-
     $this.Print()
     $this.Write()
   }
