59c7776426a3a672e41eeff330cfd9acc041c1b5
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-11-21 00:31:10 +0800

    GtxFury.FlyClash: Fix installer filter

--- Tasks/GtxFury.FlyClash/Script.ps1
+++ Tasks/GtxFury.FlyClash/Script.ps1
@@ -8,7 +8,7 @@ $this.CurrentState.Version = $Object1.tag_name -creplace '^v'
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = $Object1.assets.Where({ $_.name.EndsWith('.exe') -and $_.name.Contains('Setup') }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
+  InstallerUrl = $Object1.assets.Where({ $_.name.EndsWith('.exe') -and $_.name -match 'Setup' }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
 }
 
 switch -Regex ($this.Check()) {
