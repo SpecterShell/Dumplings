c4dea59a7cf1a9062003e8497b98b4ee2885a974
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-11-21 00:32:42 +0800

    Tencent.TencentVideo: Update

--- Tasks/Tencent.TencentVideo/Script.ps1
+++ Tasks/Tencent.TencentVideo/Script.ps1
@@ -1,30 +1,28 @@
-$Object1 = Invoke-WebRequest -Uri 'https://v.qq.com/download.html' | ConvertFrom-Html
+$Prefix = 'https://v.qq.com/download.html'
+$Object1 = Invoke-WebRequest -Uri $Prefix | ConvertFrom-Html
+$Object2 = Invoke-RestMethod -Uri (Join-Uri $Prefix $Object1.SelectSingleNode('//script[contains(@src, "index") and contains(@src, ".js")]').Attributes['src'].Value)
+$SchemaKey = [regex]::Match($Object2, 'schemaid:"downloadpage_config",schemakey:"([^"]+)"').Groups[1].Value
+$Object3 = (Invoke-RestMethod -Uri "https://cache.wuji.qq.com/x/api/wuji_cache/object?appid=vqqcom&schemaid=downloadpage_config&schemakey=${SchemaKey}").data.Where({ $_.app -eq 'Windows' }, 'First')[0]
 
 # Version
-$this.CurrentState.Version = [regex]::Match(
-  $Object1.SelectSingleNode('//*[@id="mod_container"]/div[2]/div[1]/div[1]/div/span[1]').InnerText,
-  'V([\d\.]+)'
-).Groups[1].Value
+$this.CurrentState.Version = $Object3.version
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = $Object1.SelectSingleNode('//*[@id="mod_container"]/div[2]/div[1]/div[1]/a').Attributes['href'].Value.Replace('dldir1.qq.com', 'dldir1v6.qq.com')
+  InstallerUrl = $Object3.downloadLink
 }
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
     try {
       # ReleaseTime
-      $this.CurrentState.ReleaseTime = [regex]::Match(
-        $Object1.SelectSingleNode('//*[@id="mod_container"]/div[2]/div[1]/div[1]/div/span[3]').InnerText,
-        '(\d{4}-\d{1,2}-\d{1,2})'
-      ).Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
+      $this.CurrentState.ReleaseTime = $Object3.updateDate.ToUniversalTime()
 
       # ReleaseNotes (zh-CN)
       $this.CurrentState.Locale += [ordered]@{
         Locale = 'zh-CN'
         Key    = 'ReleaseNotes'
-        Value  = $Object1.SelectSingleNode('//*[@id="mod_container"]/div[2]/div[1]/div[1]/div/span[4]/div/ul') | Get-TextContent | Format-Text
+        Value  = $Object3.versionDetail | Format-Text
       }
     } catch {
       $_ | Out-Host
