8e33defcf882ba15033e8841bdb60d548577e31f
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-12-06 13:57:56 +0800

    BitSum.ProcessLasso.Beta: Detect installer updates by the Last-Modified header, reset

--- Tasks/BitSum.ProcessLasso.Beta/Script.ps1
+++ Tasks/BitSum.ProcessLasso.Beta/Script.ps1
@@ -15,7 +15,8 @@ $this.CurrentState.Installer += $InstallerX86 = [ordered]@{
   InstallerUrl = $Object1.Links.Where({ try { $_.href.EndsWith('.exe') -and $_.href.Contains('beta') -and $_.href.Contains('32') } catch {} }, 'First')[0].href
 }
 $Object2 = Invoke-WebRequest -Uri $InstallerX86.InstallerUrl -Method Head
-$ETag = $Object2.Headers.ETag[0]
+# Last Modified
+$this.CurrentState.LastModified = $Object2.Headers.'Last-Modified'[0]
 
 # x64
 $this.CurrentState.Installer += $InstallerX64 = [ordered]@{
@@ -23,15 +24,12 @@ $this.CurrentState.Installer += $InstallerX64 = [ordered]@{
   InstallerUrl = $Object1.Links.Where({ try { $_.href.EndsWith('.exe') -and $_.href.Contains('beta') -and $_.href.Contains('64') } catch {} }, 'First')[0].href
 }
 $Object3 = Invoke-WebRequest -Uri $InstallerX64.InstallerUrl -Method Head
-$ETagX64 = $Object3.Headers.ETag[0]
+# Last Modified
+$this.CurrentState.LastModifiedX64 = $Object3.Headers.'Last-Modified'[0]
 
 # Case 0: Force submit the manifest
 if ($Global:DumplingsPreference.Contains('Force')) {
   $this.Log('Skip checking states', 'Info')
-
-  # ETag
-  $this.CurrentState.ETag = @($ETag)
-  $this.CurrentState.ETagX64 = @($ETagX64)
 
   Read-Installer
 
@@ -46,10 +44,6 @@ if ($Global:DumplingsPreference.Contains('Force')) {
 if ($this.Status.Contains('New')) {
   $this.Log('New task', 'Info')
 
-  # ETag
-  $this.CurrentState.ETag = @($ETag)
-  $this.CurrentState.ETagX64 = @($ETagX64)
-
   Read-Installer
 
   $this.Print()
@@ -57,13 +51,19 @@ if ($this.Status.Contains('New')) {
   return
 }
 
-# Case 2: The ETag is unchanged
-if ($ETag -in $this.LastState.ETag) {
-  $this.Log("The version $($this.LastState.Version) from the last state is the latest (MSI)", 'Info')
+# Case 2: The Last Modified is unchanged
+if ([datetime]$this.CurrentState.LastModified -eq [datetime]$this.LastState.LastModified) {
+  $this.Log("The version $($this.LastState.Version) from the last state is the latest (x86)", 'Info')
+  return
+} elseif ([datetime]$this.CurrentState.LastModified -lt [datetime]$this.LastState.LastModified) {
+  $this.Log("The last modified datetime from the current state `"$($this.CurrentState.LastModified)`" is older than the one from the last state `"$($this.LastState.LastModified)`" (x86)", 'Warning')
   return
 }
-if ($ETagX64 -in $this.LastState.ETagX64) {
-  $this.Log("The version $($this.LastState.Version) from the last state is the latest (EXE)", 'Info')
+if ([datetime]$this.CurrentState.LastModifiedX64 -eq [datetime]$this.LastState.LastModifiedX64) {
+  $this.Log("The version $($this.LastState.Version) from the last state is the latest (x64)", 'Info')
+  return
+} elseif ([datetime]$this.CurrentState.LastModifiedX64 -lt [datetime]$this.LastState.LastModifiedX64) {
+  $this.Log("The last modified datetime from the current state `"$($this.CurrentState.LastModifiedX64)`" is older than the one from the last state `"$($this.LastState.LastModifiedX64)`" (x64)", 'Warning')
   return
 }
 
@@ -74,33 +74,25 @@ if ([string]::IsNullOrWhiteSpace($this.CurrentState.Version)) {
   throw 'The current state has an invalid version'
 }
 
-# Case 4: The ETag has changed, but the SHA256 is not
+# Case 4: The Last Modified has changed, but the SHA256 is not
 if ($this.CurrentState.Installer[0].InstallerSha256 -eq $this.LastState.Installer[0].InstallerSha256) {
-  $this.Log('The ETag has changed, but the SHA256 is not', 'Info')
-
-  # ETag
-  $this.CurrentState.ETag = $this.LastState.ETag + $ETag
-  $this.CurrentState.ETagX64 = $this.LastState.ETagX64 + $ETagX64
+  $this.Log('The Last Modified has changed, but the SHA256 is not', 'Info')
 
   $this.Write()
   return
 }
 
-# ETag
-$this.CurrentState.ETag = @($ETag)
-$this.CurrentState.ETagX64 = @($ETagX64)
-
 switch -Regex ($this.Check()) {
-  # Case 6: The ETag, the SHA256 and the version have changed
+  # Case 6: The Last Modified, the SHA256 and the version have changed
   'Updated|Rollbacked' {
     $this.Print()
     $this.Write()
     $this.Message()
     $this.Submit()
   }
-  # Case 5: The ETag and the SHA256 have changed, but the version is not
+  # Case 5: The Last Modified and the SHA256 have changed, but the version is not
   default {
-    $this.Log('The ETag and the SHA256 have changed, but the version is not', 'Info')
+    $this.Log('The Last Modified and the SHA256 have changed, but the version is not', 'Info')
     $this.Config.IgnorePRCheck = $true
     $this.Print()
     $this.Write()
