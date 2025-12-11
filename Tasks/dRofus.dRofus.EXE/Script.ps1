4873b8d4387f3baf9c8e02a307ecd473cc703cc5
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-12-12 00:11:03 +0800

    dRofus.dRofus.EXE: Fix version scheme

--- Tasks/dRofus.dRofus.EXE/Script.ps1
+++ Tasks/dRofus.dRofus.EXE/Script.ps1
@@ -1,9 +1,9 @@
 $Object1 = Invoke-WebRequest -Uri 'https://deploy.drofus.com/stable/RELEASES' | Read-ResponseContent | ConvertFrom-SquirrelReleases | Where-Object -FilterScript { -not $_.IsDelta } | Sort-Object -Property { $_.Version -creplace '\d+', { $_.Value.PadLeft(20) } } -Bottom 1
 
-$VersionMatches = [regex]::Match($Object1.Filename, '(\d+(?:\.\d+)+)(?:-stable)?-(\d+)')
+$VersionMatches = [regex]::Match($Object1.Filename, '(\d+(?:\.\d+)+)(?:-stable)?(-(\d+))?')
 
 # Version
-$this.CurrentState.Version = "$($VersionMatches.Groups[1].Value).$($VersionMatches.Groups[2].Value)"
+$this.CurrentState.Version = "$($VersionMatches.Groups[1].Value).$($VersionMatches.Groups[2].Success ? $VersionMatches.Groups[2].Value : '0')"
 
 # RealVersion
 $this.CurrentState.RealVersion = $VersionMatches.Groups[1].Value
