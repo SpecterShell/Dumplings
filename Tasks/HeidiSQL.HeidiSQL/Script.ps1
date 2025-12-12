beba67a4990d507c6e69f424859ef81240d8faa7
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-12-12 10:41:41 +0800

    HeidiSQL.HeidiSQL: Switch to download page

--- Tasks/HeidiSQL.HeidiSQL/Script.ps1
+++ Tasks/HeidiSQL.HeidiSQL/Script.ps1
@@ -1,45 +1,35 @@
-$Object1 = Invoke-RestMethod -Uri 'https://www.heidisql.com/updatecheck.php?bits=64' | ConvertFrom-Ini
+$Object1 = Invoke-GitHubApi -Uri 'https://api.github.com/repos/HeidiSQL/HeidiSQL/releases/latest'
 
 # Version
-$this.CurrentState.Version = $Object1.Release.Version
+$this.CurrentState.Version = $Object1.tag_name -replace '^v'
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  Architecture = 'x64'
-  InstallerUrl = $Object1.Release.URL
+  InstallerUrl = $Object1.assets.Where({ $_.name.EndsWith('.exe') -and $_.name -match 'Setup' }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
 }
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
     try {
       # ReleaseTime
-      $this.CurrentState.ReleaseTime = $Object1.Release.Date | Get-Date -Format 'yyyy-MM-dd'
-    } catch {
-      $_ | Out-Host
-      $this.Log($_, 'Warning')
-    }
+      $this.CurrentState.ReleaseTime = $Object1.published_at.ToUniversalTime()
 
-    try {
-      $Object2 = (Invoke-RestMethod -Uri 'https://www.heidisql.com/rss.php?c=1,7').Where({ $_.title.Contains($this.CurrentState.Version) }, 'First')
-
-      if ($Object2) {
-        $ReleaseNotesObject = $Object2[0].content.'#text' | ConvertFrom-Html
-        # Remove the download link
-        $ReleaseNotesObject.SelectNodes('/p[contains(./a/@href, "download.php")]').ForEach({ $_.Remove() })
+      if (-not [string]::IsNullOrWhiteSpace($Object1.body)) {
         # ReleaseNotes (en-US)
         $this.CurrentState.Locale += [ordered]@{
           Locale = 'en-US'
           Key    = 'ReleaseNotes'
-          Value  = $ReleaseNotesObject | Get-TextContent | Format-Text
-        }
-
-        # ReleaseNotesUrl
-        $this.CurrentState.Locale += [ordered]@{
-          Key   = 'ReleaseNotesUrl'
-          Value = $Object2[0].link.href
+          Value  = $Object1.body | Convert-MarkdownToHtml -Extensions 'advanced', 'emojis', 'hardlinebreak' | Get-TextContent | Format-Text
         }
       } else {
         $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+
+      # ReleaseNotesUrl (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotesUrl'
+        Value  = $Object1.html_url
       }
     } catch {
       $_ | Out-Host
