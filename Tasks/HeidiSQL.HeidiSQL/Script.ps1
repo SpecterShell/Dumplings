cd593cac3dff60fbebd875e8d5a053b15d2cc07f
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-12-14 17:54:44 +0800

    HeidiSQL.HeidiSQL: Parse version from installer URL

--- Tasks/HeidiSQL.HeidiSQL/Script.ps1
+++ Tasks/HeidiSQL.HeidiSQL/Script.ps1
@@ -1,12 +1,12 @@
 $Object1 = Invoke-GitHubApi -Uri 'https://api.github.com/repos/HeidiSQL/HeidiSQL/releases/latest'
 
-# Version
-$this.CurrentState.Version = $Object1.tag_name -replace '^v'
-
 # Installer
 $this.CurrentState.Installer += [ordered]@{
   InstallerUrl = $Object1.assets.Where({ $_.name.EndsWith('.exe') -and $_.name -match 'Setup' }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
 }
+
+# Version
+$this.CurrentState.Version = [regex]::Matches($this.CurrentState.Installer[0].InstallerUrl, '(\d+(?:\.\d+)+)')[-1].Groups[1].Value
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
