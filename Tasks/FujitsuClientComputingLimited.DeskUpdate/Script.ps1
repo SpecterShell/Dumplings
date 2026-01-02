7b1c876684ed63a93c4bed006f9f7fb21eaffc52
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-01-02 16:54:02 +0800

    Add FujitsuClientComputingLimited.DeskUpdate

---
+++ Tasks/FujitsuClientComputingLimited.DeskUpdate/Script.ps1
@@ -0,0 +1,30 @@
+$Object1 = Invoke-RestMethod -Uri "https://webdownloads2.ts.fujitsu.com/deskupdate_5_1/data/api/v1/update/$($this.Status.Contains('New') ? '5.2.75.0' : $this.LastState.Version)"
+
+# Version
+$this.CurrentState.Version = $Object1.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = "https://support.ts.fujitsu.com/Download/StreamFileToBrowser.asp?SoftwareGUID=$($Object1.updateSetupPackage.softwareID.ToUpper())"
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.versionDate.ToUniversalTime()
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
