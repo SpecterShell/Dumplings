b445e441ffe50d667808b7bf3876b719336762dd
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-01-05 20:13:31 +0800

    coolcoala.KoalaClash: Update repo name, add en-US locale to ReleaseNotesUrl

--- Tasks/coolcoala.KoalaClash/Script.ps1
+++ Tasks/coolcoala.KoalaClash/Script.ps1
@@ -1,5 +1,5 @@
 $RepoOwner = 'coolcoala'
-$RepoName = 'clash-verge-rev-lite'
+$RepoName = 'koala-clash'
 
 $Object1 = Invoke-GitHubApi -Uri "https://api.github.com/repos/${RepoOwner}/${RepoName}/releases/latest"
 
@@ -40,10 +40,11 @@ switch -Regex ($this.Check()) {
         $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
       }
 
-      # ReleaseNotesUrl
+      # ReleaseNotesUrl (en-US)
       $this.CurrentState.Locale += [ordered]@{
-        Key   = 'ReleaseNotesUrl'
-        Value = $Object1.html_url
+        Locale = 'en-US'
+        Key    = 'ReleaseNotesUrl'
+        Value  = $Object1.html_url
       }
     } catch {
       $_ | Out-Host
