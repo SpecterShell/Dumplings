4ba821fd7284deb9e6aeaccccde381961dd87ac5
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-01-05 20:57:10 +0800

    docmirror.dev-sidecar: Add the version prefix to remove, fix installer filter

--- Tasks/docmirror.dev-sidecar/Script.ps1
+++ Tasks/docmirror.dev-sidecar/Script.ps1
@@ -4,7 +4,7 @@ $RepoName = 'dev-sidecar'
 $Object1 = Invoke-GitHubApi -Uri "https://api.github.com/repos/${RepoOwner}/${RepoName}/releases/latest"
 
 # Version
-$this.CurrentState.Version = $Object1.tag_name -creplace '^v'
+$this.CurrentState.Version = $Object1.tag_name -replace '^v' -replace 'release-'
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
@@ -13,7 +13,7 @@ $this.CurrentState.Installer += [ordered]@{
 }
 $this.CurrentState.Installer += [ordered]@{
   Architecture = 'x64'
-  InstallerUrl = $Object1.assets.Where({ $_.name.EndsWith('.exe') -and $_.name.Contains('x86_64') }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
+  InstallerUrl = $Object1.assets.Where({ $_.name.EndsWith('.exe') -and $_.name -match 'x64|x86_64' }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
 }
 $this.CurrentState.Installer += [ordered]@{
   Architecture = 'arm64'
