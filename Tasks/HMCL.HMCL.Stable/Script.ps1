3b77b1fe4f1f9f3d359b55c67c825cef03c15897
neveler <55753029+neveler@users.noreply.github.com>
2026-01-08 20:38:00 +0800

    update

--- Tasks/HMCL.HMCL.Stable/Script.ps1
+++ Tasks/HMCL.HMCL.Stable/Script.ps1
@@ -8,21 +8,27 @@ $this.CurrentState.Version = $Object1.tag_name -creplace '^v'
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  Architecture = 'neutral'
-  InstallerUrl = $Object1.assets.Where({ $_.name.EndsWith('.exe') }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
+  Architecture  = 'neutral'
+  InstallerType = 'portable'
+  InstallerUrl  = $Object1.assets.Where({ $_.name.EndsWith('.exe') }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
 }
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
+    # ReleaseTime
     try {
-      # ReleaseTime
       $this.CurrentState.ReleaseTime = $Object1.published_at.ToUniversalTime()
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
 
-      # ReleaseNotesUrl
+    # ReleaseNotesUrl
+    try {
       $this.CurrentState.Locale += [ordered]@{
         Locale = 'en-US'
         Key    = 'ReleaseNotesUrl'
-        Value  = "https://docs.hmcl.net/changelog/stable.html#HMCL-${this.CurrentState.Version}"
+        Value  = "https://docs.hmcl.net/changelog/stable.html#HMCL-$($this.CurrentState.Version)"
       }
     } catch {
       $_ | Out-Host
