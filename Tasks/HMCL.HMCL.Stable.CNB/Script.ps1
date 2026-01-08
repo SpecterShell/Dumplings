ee19aecc1f76a461e02afc9c08e101de197f3f05
neveler <55753029+neveler@users.noreply.github.com>
2026-01-08 20:57:59 +0800

    update

---
+++ Tasks/HMCL.HMCL.Stable.CNB/Script.ps1
@@ -0,0 +1,47 @@
+$RepoOwner = 'HMCL-dev'
+$RepoName = 'HMCL'
+
+$Object1 = Invoke-GitHubApi -Uri "https://api.github.com/repos/${RepoOwner}/${RepoName}/releases/latest"
+
+# Version
+$this.CurrentState.Version = $Object1.tag_name -creplace '^v'
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture  = 'neutral'
+  InstallerType = 'portable'
+  InstallerUrl  = "https://cnb.cool/HMCL-dev/HMCL/-/releases/download/v$($this.CurrentState.Version)/HMCL-$($this.CurrentState.Version).exe"
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    # ReleaseTime
+    try {
+      $this.CurrentState.ReleaseTime = $Object1.published_at.ToUniversalTime()
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    # ReleaseNotesUrl
+    try {
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotesUrl'
+        Value  = "https://docs.hmcl.net/changelog/stable.html#HMCL-$($this.CurrentState.Version)"
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
