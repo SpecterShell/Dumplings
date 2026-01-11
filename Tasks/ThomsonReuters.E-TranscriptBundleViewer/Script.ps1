3d264c5cabca18f1893c5efd91a7771079912b4b
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-01-11 14:38:09 +0800

    ThomsonReuters.E-TranscriptBundleViewer: Update

--- Tasks/ThomsonReuters.E-TranscriptBundleViewer/Script.ps1
+++ Tasks/ThomsonReuters.E-TranscriptBundleViewer/Script.ps1
@@ -1,11 +1,16 @@
 $Prefix = 'https://www.thomsonreuters.com/en-us/help/legal-products-software-downloads'
 $Object1 = Invoke-WebRequest -Uri $Prefix | ConvertFrom-Html
-$Object2 = $Object1.SelectSingleNode('//div[@data-prop-page-title="Legal products software downloads"]').Attributes['data-prop-table-data-props'].DeEntitizeValue | ConvertFrom-Json
-$Object3 = $Object2.Where({ $_.tableData[0].value -eq 'E-Transcript Bundle Viewer' }, 'First')[0]
+$Object2 = $Object1.SelectSingleNode('//div[@data-prop-page-title="Legal products software downloads"]')
+$Object3 = $Object2.Attributes['data-prop-table-data-props'].DeEntitizeValue | ConvertFrom-Json
+$Object4 = $Object3.Where({ $_.tableData[0].value -eq 'E-Transcript Bundle Viewer' }, 'First')[0]
+$Object5 = Invoke-RestMethod -Uri "$($Object2.Attributes['data-prop-file-download-url'].DeEntitizeValue)/files/download/$([uri]::EscapeDataString($Object4.tableData[3].link))" -Headers @{
+  Referer       = 'https://www.thomsonreuters.com/'
+  Authorization = 'Bearer thomson-reuters-help-center-external-user'
+}
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = Join-Uri $Prefix $Object3.tableData[3].link
+  InstallerUrl = $Object5.url
 }
 
 # Version
@@ -15,7 +20,7 @@ switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
     try {
       # ReleaseTime
-      $this.CurrentState.ReleaseTime = $Object3.tableData[1].value | Get-Date -Format 'yyyy-MM-dd'
+      $this.CurrentState.ReleaseTime = $Object4.tableData[1].value | Get-Date -Format 'yyyy-MM-dd'
     } catch {
       $_ | Out-Host
       $this.Log($_, 'Warning')
