0c97834f148fec7d84fe3e58aaf99dfd78146728
Charlie Chen <56779163+SpecterShell@users.noreply.github.com>
2026-01-12 22:55:59 +0800

    Rewrite script

--- Tasks/JeffreyPfau.mGBA/Script.ps1
+++ Tasks/JeffreyPfau.mGBA/Script.ps1
@@ -1,16 +1,16 @@
 $Object1 = Invoke-GitHubApi -Uri 'https://api.github.com/repos/mgba-emu/mgba/releases/latest'
 
 # Version
-$this.CurrentState.Version = $Object1.tag_name
+$this.CurrentState.Version = $Object1.tag_name -replace '^v'
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  Architecture  = 'x86'
-  InstallerUrl  = $Object1.assets.Where({ $_.name.EndsWith('-win32-installer.exe') }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
+  Architecture = 'x86'
+  InstallerUrl = $Object1.assets.Where({ $_.name.EndsWith('.exe') -and $_.name -match 'installer' -and $_.name -match 'win32' }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
 }
 $this.CurrentState.Installer += [ordered]@{
-  Architecture  = 'x64'
-  InstallerUrl  = $Object1.assets.Where({ $_.name.EndsWith('-win64-installer.exe') }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
+  Architecture = 'x64'
+  InstallerUrl = $Object1.assets.Where({ $_.name.EndsWith('.exe') -and $_.name -match 'installer' -and $_.name -match 'win64' }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
 }
 
 switch -Regex ($this.Check()) {
@@ -18,6 +18,46 @@ switch -Regex ($this.Check()) {
     try {
       # ReleaseTime
       $this.CurrentState.ReleaseTime = $Object1.published_at.ToUniversalTime()
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    try {
+      $Object2 = [System.IO.StreamReader]::new((Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/mgba-emu/mgba/HEAD/CHANGES').RawContentStream)
+
+      while (-not $Object2.EndOfStream) {
+        $String = $Object2.ReadLine()
+        if ($String -match "^$([regex]::Escape($this.CurrentState.Version)):") {
+          # try {
+          #   # ReleaseTime
+          #   $this.CurrentState.ReleaseTime ??= [regex]::Match($String, '(20\d{2}-\d{1,2}-\d{1,2})').Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
+          # } catch {
+          #   $this.Log("No ReleaseTime for version $($this.CurrentState.Version)", 'Warning')
+          # }
+          $null = $Object2.ReadLine()
+          break
+        }
+      }
+      if (-not $Object2.EndOfStream) {
+        $ReleaseNotesObjects = [System.Collections.Generic.List[string]]::new()
+        while (-not $Object2.EndOfStream) {
+          $String = $Object2.ReadLine()
+          if ($String -notmatch '^\d+(\.\d+)+:') {
+            $ReleaseNotesObjects.Add($String)
+          } else {
+            break
+          }
+        }
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesObjects | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
     } catch {
       $_ | Out-Host
       $this.Log($_, 'Warning')
@@ -33,3 +73,4 @@ switch -Regex ($this.Check()) {
     $this.Submit()
   }
 }
+
