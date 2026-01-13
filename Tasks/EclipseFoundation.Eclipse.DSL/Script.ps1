d356927d2a9712e5f37ae257d012f431c0691cb8
neveler <55753029+neveler@users.noreply.github.com>
2026-01-14 01:27:09 +0800

    Add EclipseFoundation.Eclipse (#68)
    Co-authored-by: SpecterShell <56779163+SpecterShell@users.noreply.github.com>

---
+++ Tasks/EclipseFoundation.Eclipse.DSL/Script.ps1
@@ -0,0 +1,25 @@
+$this.CurrentState.Version = $Global:DumplingsStorage.EclipseVersion
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/$($this.CurrentState.Version)/R/eclipse-dsl-$($this.CurrentState.Version)-R-win32-x86_64.zip"
+}
+
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'arm64'
+  InstallerUrl = "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/$($this.CurrentState.Version)/R/eclipse-dsl-$($this.CurrentState.Version)-R-win32-aarch64.zip"
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
