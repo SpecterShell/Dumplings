fd4db3e7a4d4f50eaa3f27649a27b6ecb16a27c0
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-01-14 01:24:58 +0800

    Prefetch version, use automatic mirror links

--- Tasks/EclipseFoundation.Eclipse.RCP/Script.ps1
+++ Tasks/EclipseFoundation.Eclipse.RCP/Script.ps1
@@ -1,16 +1,14 @@
-$Object1 = Invoke-RestMethod -Uri 'https://download.eclipse.org/technology/epp/downloads/release/release.xml'
-
-$this.CurrentState.Version = ($Object1.packages.present -split "/")[0]
+$this.CurrentState.Version = $Global:DumplingsStorage.EclipseVersion
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
   Architecture = 'x64'
-  InstallerUrl = "https://download.eclipse.org/technology/epp/downloads/release/$($this.CurrentState.Version)/R/eclipse-rcp-$($this.CurrentState.Version)-R-win32-x86_64.zip"
+  InstallerUrl = "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/$($this.CurrentState.Version)/R/eclipse-rcp-$($this.CurrentState.Version)-R-win32-x86_64.zip"
 }
 
 $this.CurrentState.Installer += [ordered]@{
   Architecture = 'arm64'
-  InstallerUrl = "https://download.eclipse.org/technology/epp/downloads/release/$($this.CurrentState.Version)/R/eclipse-rcp-$($this.CurrentState.Version)-R-win32-aarch64.zip"
+  InstallerUrl = "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/$($this.CurrentState.Version)/R/eclipse-rcp-$($this.CurrentState.Version)-R-win32-aarch64.zip"
 }
 
 switch -Regex ($this.Check()) {
