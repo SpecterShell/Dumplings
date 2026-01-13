7cb61b2bbc5ad27b9dda7fce815b853b2cf4574e
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-01-14 01:28:55 +0800

    EclipseFoundation.Eclipse: Fix InstallerUrl

--- Tasks/EclipseFoundation.Eclipse.DSL/Script.ps1
+++ Tasks/EclipseFoundation.Eclipse.DSL/Script.ps1
@@ -3,12 +3,12 @@ $this.CurrentState.Version = $Global:DumplingsStorage.EclipseVersion
 # Installer
 $this.CurrentState.Installer += [ordered]@{
   Architecture = 'x64'
-  InstallerUrl = "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/$($this.CurrentState.Version)/R/eclipse-dsl-$($this.CurrentState.Version)-R-win32-x86_64.zip"
+  InstallerUrl = "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/$($this.CurrentState.Version)/R/eclipse-dsl-$($this.CurrentState.Version)-R-win32-x86_64.zip&r=1"
 }
 
 $this.CurrentState.Installer += [ordered]@{
   Architecture = 'arm64'
-  InstallerUrl = "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/$($this.CurrentState.Version)/R/eclipse-dsl-$($this.CurrentState.Version)-R-win32-aarch64.zip"
+  InstallerUrl = "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/$($this.CurrentState.Version)/R/eclipse-dsl-$($this.CurrentState.Version)-R-win32-aarch64.zip&r=1"
 }
 
 switch -Regex ($this.Check()) {
