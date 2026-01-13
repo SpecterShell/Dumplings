6398cb6cb1e4e24047a1c809b5a18f70de19249f
neveler <55753029+neveler@users.noreply.github.com>
2026-01-13 22:04:13 +0800

    Add EclipseFoundation.Eclipse

---
+++ Tasks/EclipseFoundation.Eclipse.JEE/Script.ps1
@@ -0,0 +1,27 @@
+$Object1 = Invoke-RestMethod -Uri 'https://download.eclipse.org/technology/epp/downloads/release/release.xml'
+
+$this.CurrentState.Version = ($Object1.packages.present -split "/")[0]
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = "https://download.eclipse.org/technology/epp/downloads/release/$($this.CurrentState.Version)/R/eclipse-jee-$($this.CurrentState.Version)-R-win32-x86_64.zip"
+}
+
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'arm64'
+  InstallerUrl = "https://download.eclipse.org/technology/epp/downloads/release/$($this.CurrentState.Version)/R/eclipse-jee-$($this.CurrentState.Version)-R-win32-aarch64.zip"
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
