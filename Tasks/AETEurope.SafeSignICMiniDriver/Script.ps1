d8753b2bf9b4fbe482d8778964edeb42a920f679
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-01-15 21:02:37 +0800

    AETEurope.SafeSignICMiniDriver: Detect installer in the nested webpage

--- Tasks/AETEurope.SafeSignICMiniDriver/Script.ps1
+++ Tasks/AETEurope.SafeSignICMiniDriver/Script.ps1
@@ -1,8 +1,18 @@
-$Object1 = Invoke-WebRequest -Uri 'https://www.uziregister.nl/uzi-pas/activeer-en-installeer-uw-uzi-pas/overzicht-safesign-software'
+$Prefix = 'https://www.uziregister.nl/uzi-pas/activeer-en-installeer-uw-uzi-pas/overzicht-safesign-software'
+$Object1 = Invoke-WebRequest -Uri $Prefix
 
-# Installer
-$this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = $Object1.Links.Where({ try { $_.href.EndsWith('.zip') -and $_.outerHTML.Contains('Windows 64-bit') } catch {} }, 'First')[0].href
+$Link = Join-Uri $Prefix $Object1.Links.Where({ try { $_.outerHTML.Contains('Windows 64-bit') } catch {} }, 'First')[0].href
+if ($Link -match '\.(zip|exe|msi)') {
+  # Installer
+  $this.CurrentState.Installer += [ordered]@{
+    InstallerUrl = $Link
+  }
+} else {
+  $Object2 = Invoke-WebRequest -Uri ($Prefix = $Link)
+  # Installer
+  $this.CurrentState.Installer += [ordered]@{
+    InstallerUrl = Join-Uri $Prefix $Object2.Links.Where({ try { $_.href.EndsWith('.zip') -and $_.outerHTML.Contains('Windows 64-bit') } catch {} }, 'First')[0].href
+  }
 }
 
 # Version
