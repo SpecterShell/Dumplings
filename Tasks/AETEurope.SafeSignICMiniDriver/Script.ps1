a34806a88ad30cc284798b65f12cf6aea6989b09
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-01-15 21:12:45 +0800

    AETEurope.SafeSignICMiniDriver: Enhance detection logic

--- Tasks/AETEurope.SafeSignICMiniDriver/Script.ps1
+++ Tasks/AETEurope.SafeSignICMiniDriver/Script.ps1
@@ -2,16 +2,17 @@ $Prefix = 'https://www.uziregister.nl/uzi-pas/activeer-en-installeer-uw-uzi-pas/
 $Object1 = Invoke-WebRequest -Uri $Prefix
 
 $Link = Join-Uri $Prefix $Object1.Links.Where({ try { $_.outerHTML.Contains('Windows 64-bit') } catch {} }, 'First')[0].href
-if ($Link -match '\.(zip|exe|msi)') {
-  # Installer
-  $this.CurrentState.Installer += [ordered]@{
-    InstallerUrl = $Link
-  }
-} else {
+$Object2 = Invoke-WebRequest -Uri $Link -Method Head
+if ($Object2.Headers.'Content-Type' -match 'text/html') {
   $Object2 = Invoke-WebRequest -Uri ($Prefix = $Link)
   # Installer
   $this.CurrentState.Installer += [ordered]@{
     InstallerUrl = Join-Uri $Prefix $Object2.Links.Where({ try { $_.href.EndsWith('.zip') -and $_.outerHTML.Contains('Windows 64-bit') } catch {} }, 'First')[0].href
+  }
+} else {
+  # Installer
+  $this.CurrentState.Installer += [ordered]@{
+    InstallerUrl = $Link
   }
 }
 
