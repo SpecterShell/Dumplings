75a0cd952279b747eb9315a1950cdbd702197b91
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-01-30 16:42:11 +0800

    GoTo.LogMeInResolveDesktopConsole: Get article ID for API from web page

--- Tasks/GoTo.LogMeInResolveDesktopConsole/Script.ps1
+++ Tasks/GoTo.LogMeInResolveDesktopConsole/Script.ps1
@@ -11,10 +11,11 @@ $this.CurrentState.Installer += [ordered]@{
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
     try {
-      $Object2 = Invoke-RestMethod -Uri 'https://support.logmein.com/api2/article/id_7631261c-344b-4b7a-9eff-fb40fd5f9b8b_goto_resolve/en'
-      $Object3 = $Object2.contentText | ConvertFrom-Html
+      $Object2 = Invoke-WebRequest -Uri 'https://support.logmein.com/resolve/help/whats-new-in-the-logmein-resolve-desktop-technician-console' | ConvertFrom-Html
+      $Object3 = Invoke-RestMethod -Uri "https://support.logmein.com/api2/article/$($Object2.SelectSingleNode('//meta[@name="article-immutable-id"]').Attributes['content'].Value)/en"
+      $Object4 = $Object3.contentText | ConvertFrom-Html
 
-      $ReleaseNotesTitleNode = $Object3.SelectSingleNode("//h2[contains(text(), '$($this.CurrentState.Version)')]")
+      $ReleaseNotesTitleNode = $Object4.SelectSingleNode("//h2[contains(text(), '$($this.CurrentState.Version)')]")
       if ($ReleaseNotesTitleNode) {
         # ReleaseNotes (en-US)
         $this.CurrentState.Locale += [ordered]@{
