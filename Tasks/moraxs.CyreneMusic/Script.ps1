c5e78f31b79bea9f9b4633bfcfe0973e08dc9c5a
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-01-30 15:40:47 +0800

    moraxs.CyreneMusic: Fix InstallerType

--- Tasks/moraxs.CyreneMusic/Script.ps1
+++ Tasks/moraxs.CyreneMusic/Script.ps1
@@ -5,7 +5,7 @@ $this.CurrentState.Version = $Object1.tag_name -replace '^v'
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerType = 'nullsoft'
+  InstallerType = 'inno'
   InstallerUrl  = $Object1.assets.Where({ $_.name.EndsWith('.exe') -and $_.name -match 'setup' }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
 }
 
