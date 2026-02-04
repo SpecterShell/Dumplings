c6031ae46ef2519a831d71c654512b54b7c8be87
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-02-04 11:58:32 +0800

    VoSoft.VicDiary: Update

--- Tasks/VoSoft.VicDiary/Script.ps1
+++ Tasks/VoSoft.VicDiary/Script.ps1
@@ -1,37 +1,52 @@
 $RepoOwner = 'vo-soft'
-$RepoName = 'vic-diary-releases'
+$RepoName = 'vic-diary-release'
 
-$Object1 = Invoke-GitHubApi -Uri "https://api.github.com/repos/${RepoOwner}/${RepoName}/releases/latest"
-
-# Version
-$this.CurrentState.Version = $Object1.tag_name -creplace '^v'
+$Object1 = Invoke-RestMethod -Uri "https://gitee.com/api/v5/repos/${RepoOwner}/${RepoName}/releases/latest" -Authentication Bearer -Token (ConvertTo-SecureString -String $Global:DumplingsSecret.GiteeToken -AsPlainText)
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = $Object1.assets.Where({ $_.name.EndsWith('.exe') -and $_.name.Contains('setup') }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
+  InstallerUrl = $Object1.assets.Where({ $_.name.EndsWith('.exe') -and $_.name -match 'setup' }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
 }
+
+# Version
+$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
     try {
-      # ReleaseTime
-      $this.CurrentState.ReleaseTime = $Object1.published_at.ToUniversalTime()
+      # ReleaseNotesUrl (zh-CN)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'zh-CN'
+        Key    = 'ReleaseNotesUrl'
+        Value  = "https://gitee.com/${RepoOwner}/${RepoName}/releases/tag/$($Object1.tag_name)"
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
 
-      if (-not [string]::IsNullOrWhiteSpace($Object1.body)) {
+    try {
+      $Object2 = Invoke-RestMethod -Uri "https://gitee.com/api/v5/repos/${RepoOwner}/${RepoName}/releases/tags/v$($this.CurrentState.Version)" -Authentication Bearer -Token (ConvertTo-SecureString -String $Global:DumplingsSecret.GiteeToken -AsPlainText)
+
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object2.created_at.ToUniversalTime()
+
+      if (-not [string]::IsNullOrWhiteSpace($Object2.body)) {
         # ReleaseNotes (zh-CN)
         $this.CurrentState.Locale += [ordered]@{
           Locale = 'zh-CN'
           Key    = 'ReleaseNotes'
-          Value  = $Object1.body | Convert-MarkdownToHtml -Extensions 'advanced', 'emojis', 'hardlinebreak' | Get-TextContent | Format-Text
+          Value  = $Object2.body | Convert-MarkdownToHtml -Extensions 'advanced', 'emojis', 'hardlinebreak' | Get-TextContent | Format-Text
         }
       } else {
         $this.Log("No ReleaseNotes (zh-CN) for version $($this.CurrentState.Version)", 'Warning')
       }
 
-      # ReleaseNotesUrl
+      # ReleaseNotesUrl (zh-CN)
       $this.CurrentState.Locale += [ordered]@{
-        Key   = 'ReleaseNotesUrl'
-        Value = $Object1.html_url
+        Locale = 'zh-CN'
+        Key    = 'ReleaseNotesUrl'
+        Value  = "https://gitee.com/${RepoOwner}/${RepoName}/releases/tag/$($Object2.tag_name)"
       }
     } catch {
       $_ | Out-Host
