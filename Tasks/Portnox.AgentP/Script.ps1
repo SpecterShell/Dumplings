9f43eea529cc6eaf48683097eb4c288d228d2128
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-02-04 12:16:34 +0800

    Portnox.AgentP: Update

--- Tasks/Portnox.AgentP/Script.ps1
+++ Tasks/Portnox.AgentP/Script.ps1
@@ -1,19 +1,9 @@
 $HttpClient = [System.Net.Http.HttpClient]::new()
 
 # Installer
-$this.CurrentState.Installer += $InstallerX86 = [ordered]@{
-  Architecture = 'x86'
-  InstallerUrl = 'https://clear.portnox.com/enduser/DownloadAgentPForOsAndPackageType?osType=2&packageType=Windows_x86'
-}
-$HttpRequest = [System.Net.Http.HttpRequestMessage]::new([System.Net.Http.HttpMethod]::Get, $InstallerX86.InstallerUrl)
-$HttpResponse = $HttpClient.Send($HttpRequest, [System.Net.Http.HttpCompletionOption]::ResponseHeadersRead)
-$VersionX86 = [regex]::Match($HttpResponse.Content.Headers.ContentDisposition.FileName, '(\d+(?:\.\d+)+)').Groups[1].Value
-$HttpRequest.Dispose()
-$HttpResponse.Dispose()
-
 $this.CurrentState.Installer += $InstallerX64 = [ordered]@{
   Architecture = 'x64'
-  InstallerUrl = 'https://clear.portnox.com/enduser/DownloadAgentPForOsAndPackageType?osType=2&packageType=Windows_x64'
+  InstallerUrl = 'https://clear.portnox.com/enduser/DownloadAgentPForOsAndPackageType?osType=2&packageType=Windows_x64&architecture=X64'
 }
 $HttpRequest = [System.Net.Http.HttpRequestMessage]::new([System.Net.Http.HttpMethod]::Get, $InstallerX64.InstallerUrl)
 $HttpResponse = $HttpClient.Send($HttpRequest, [System.Net.Http.HttpCompletionOption]::ResponseHeadersRead)
@@ -21,8 +11,18 @@ $VersionX64 = [regex]::Match($HttpResponse.Content.Headers.ContentDisposition.Fi
 $HttpRequest.Dispose()
 $HttpResponse.Dispose()
 
-if ($VersionX86 -ne $VersionX64) {
-  $this.Log("Inconsistent versions: x86: ${VersionX86}, x64: ${VersionX64}", 'Error')
+$this.CurrentState.Installer += $InstallerARM64 = [ordered]@{
+  Architecture = 'arm64'
+  InstallerUrl = 'https://clear.portnox.com/enduser/DownloadAgentPForOsAndPackageType?osType=2&packageType=Windows_x64&architecture=Arm64'
+}
+$HttpRequest = [System.Net.Http.HttpRequestMessage]::new([System.Net.Http.HttpMethod]::Get, $InstallerARM64.InstallerUrl)
+$HttpResponse = $HttpClient.Send($HttpRequest, [System.Net.Http.HttpCompletionOption]::ResponseHeadersRead)
+$VersionARM64 = [regex]::Match($HttpResponse.Content.Headers.ContentDisposition.FileName, '(\d+(?:\.\d+)+)').Groups[1].Value
+$HttpRequest.Dispose()
+$HttpResponse.Dispose()
+
+if ($VersionX64 -ne $VersionARM64) {
+  $this.Log("Inconsistent versions: x64: ${VersionX64}, arm64: ${VersionARM64}", 'Error')
   return
 }
 
