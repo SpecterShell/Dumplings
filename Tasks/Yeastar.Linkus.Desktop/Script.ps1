5930b46630d90fc187006df8e43365b38099cd43
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-02-04 15:03:52 +0800

    Add Yeastar.Linkus

---
+++ Tasks/Yeastar.Linkus.Desktop/Script.ps1
@@ -0,0 +1,32 @@
+$Prefix = 'https://image.yeastar.com/Linkus/Release/PC/'
+
+$Object1 = Invoke-RestMethod -Uri "${Prefix}latest-client.yml" | ConvertFrom-Yaml
+
+# Version
+$this.CurrentState.Version = $Object1.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = Join-Uri $Prefix $Object1.files[0].url
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.releaseDate | Get-Date -AsUTC
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
