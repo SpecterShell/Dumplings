e976ac218b7c4a7e1eaaa9cf1fb63d7663ea5847
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-02-05 12:28:45 +0800

    Add Discord.Discord.arm64

---
+++ Tasks/Discord.Discord.Canary.arm64/Script.ps1
@@ -0,0 +1,21 @@
+$Object1 = Invoke-RestMethod -Uri 'https://updates.discord.com/distributions/app/manifests/latest?channel=canary&platform=win&arch=arm64'
+
+# Version
+$this.CurrentState.Version = $Object1.full.host_version -join '.'
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = Join-Uri $Object1.full.url 'DiscordCanarySetup.exe'
+}
+
+switch -Regex ($this.Check()) {
+  'New|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Updated' {
+    $this.Message()
+    $this.Submit()
+  }
+}
