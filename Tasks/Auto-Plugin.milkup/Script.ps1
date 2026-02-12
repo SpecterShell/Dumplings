2b6dd96a596109aa15988bc13316c78e1c6d07a7
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-02-12 21:35:56 +0800

    Auto-Plugin.milkup: Strip off one more prefix from the version string

--- Tasks/Auto-Plugin.milkup/Script.ps1
+++ Tasks/Auto-Plugin.milkup/Script.ps1
@@ -4,7 +4,7 @@ $RepoName = 'milkup'
 $Object1 = Invoke-GitHubApi -Uri "https://api.github.com/repos/${RepoOwner}/${RepoName}/releases/latest"
 
 # Version
-$this.CurrentState.Version = $Object1.tag_name -replace '^Beta-v'
+$this.CurrentState.Version = $Object1.tag_name -replace '^Beta-v' -replace '^v'
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
