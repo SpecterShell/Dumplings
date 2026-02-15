25b59efb5b315ec2e15f17640bd7792884375d8e
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-02-15 22:27:38 +0800

    buxuku.SmartSub: Remove sub packages

--- Tasks/buxuku.SmartSub/Script.ps1
+++ Tasks/buxuku.SmartSub/Script.ps1
@@ -9,7 +9,7 @@ $this.CurrentState.Version = $Object1.tag_name -creplace '^v'
 # Installer
 $this.CurrentState.Installer += [ordered]@{
   Architecture = 'x64'
-  InstallerUrl = $Object1.assets.Where({ $_.name.EndsWith('.exe') -and $_.name.Contains('x64') -and $_.name.Contains('no-cuda') }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
+  InstallerUrl = $Object1.assets.Where({ $_.name.EndsWith('.exe') -and $_.name.Contains('x64') }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
 }
 
 switch -Regex ($this.Check()) {
@@ -19,17 +19,19 @@ switch -Regex ($this.Check()) {
       $this.CurrentState.ReleaseTime = $Object1.published_at.ToUniversalTime()
 
       if (-not [string]::IsNullOrWhiteSpace($Object1.body)) {
+        $ReleaseNotesObject = $Object1.body | Convert-MarkdownToHtml -Extensions 'advanced', 'emojis', 'hardlinebreak'
         # ReleaseNotes (en-US)
+        $ReleaseNotesNodes = for ($Node = $ReleaseNotesObject.ChildNodes[0]; $Node -and -not ($Node.Name -match 'h\d' -and $Node.InnerText -match '下载'); $Node = $Node.NextSibling) { $Node }
         $this.CurrentState.Locale += [ordered]@{
           Locale = 'en-US'
           Key    = 'ReleaseNotes'
-          Value  = $Object1.body | Convert-MarkdownToHtml -Extensions 'advanced', 'emojis', 'hardlinebreak' | Get-TextContent | Format-Text
+          Value  = $ReleaseNotes = $ReleaseNotesNodes | Get-TextContent | Format-Text
         }
         # ReleaseNotes (zh-CN)
         $this.CurrentState.Locale += [ordered]@{
           Locale = 'zh-CN'
           Key    = 'ReleaseNotes'
-          Value  = $Object1.body | Convert-MarkdownToHtml -Extensions 'advanced', 'emojis', 'hardlinebreak' | Get-TextContent | Format-Text
+          Value  = $ReleaseNotes
         }
       } else {
         $this.Log("No ReleaseNotes (en-US) and ReleaseNotes (zh-CN) for version $($this.CurrentState.Version)", 'Warning')
@@ -50,46 +52,8 @@ switch -Regex ($this.Check()) {
   }
   'Changed|Updated' {
     $this.Message()
-    $this.MessageEnabled = $false
   }
   'Updated' {
-    $WinGetIdentifierPrefix = $this.Config.WinGetIdentifier
-
-    $this.Config.WinGetIdentifier = "${WinGetIdentifierPrefix}.CPU"
-    $this.CurrentState.Installer = @(
-      [ordered]@{
-        Architecture = 'x64'
-        InstallerUrl = $Object1.assets.Where({ $_.name.EndsWith('.exe') -and $_.name.Contains('x64') -and $_.name.Contains('no-cuda') }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
-      }
-    )
-
-    try {
-      $this.Submit()
-    } catch {
-      $_ | Out-Host
-      $this.Log($_, 'Warning')
-    }
-
-    foreach ($Branch in @('11.8', '12.2', '12.4')) {
-      foreach ($Optimized in @($false, $true)) {
-        $this.Config.WinGetIdentifier = "${WinGetIdentifierPrefix}.CUDA.${Branch}$($Optimized ? '.Optimized' : '')"
-        $this.CurrentState.Installer = @(
-          [ordered]@{
-            Architecture = 'x64'
-            InstallerUrl = $Object1.assets.Where({ $_.name.EndsWith('.exe') -and $_.name.Contains('x64') -and ($_.name.Contains("_${Branch}") -and ($Optimized ? $_.name.Contains('optimized') : $_.name.Contains('generic'))) }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
-          }
-        )
-
-        try {
-          $this.Submit()
-        } catch {
-          $_ | Out-Host
-          $this.Log($_, 'Warning')
-        }
-      }
-    }
-  }
-  'Changed|Updated' {
-    $this.Message()
+    $this.Submit()
   }
 }
