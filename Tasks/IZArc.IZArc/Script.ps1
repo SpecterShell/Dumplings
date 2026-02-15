d5047e486e265e3a66d8c1144b0a72ed45eccc2c
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-02-15 22:20:20 +0800

    IZArc.IZArc: Update prefix and release notes parser

--- Tasks/IZArc.IZArc/Script.ps1
+++ Tasks/IZArc.IZArc/Script.ps1
@@ -1,4 +1,4 @@
-$Prefix = 'https://www.izarc.org/download/'
+$Prefix = 'https://www.izarc.org/downloads/'
 $Object1 = Invoke-RestMethod -Uri "${Prefix}update.ini" | ConvertFrom-Ini
 
 # Version
@@ -12,15 +12,23 @@ $this.CurrentState.Installer += [ordered]@{
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
     try {
-      $Object2 = Invoke-WebRequest -Uri 'https://www.izarc.org/news' | ConvertFrom-Html
-      $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//div[contains(@class, 'sppb-column-addons')]/div[contains(., '$($this.CurrentState.Version)')]")
-      if ($ReleaseNotesTitleNode) {
-        $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node -and -not $Node.SelectSingleNode('./div[contains(@class, "sppb-addon-divider-wrap")]'); $Node = $Node.NextSibling) { $Node }
+      $EdgeDriver = Get-EdgeDriver -Headless
+      $EdgeDriver.Navigate().GoToUrl('https://www.izarc.org/news')
+
+      $ReleaseNotesNode = [OpenQA.Selenium.Support.UI.WebDriverWait]::new($EdgeDriver, [timespan]::FromSeconds(30)).Until(
+        [System.Func[OpenQA.Selenium.IWebDriver, OpenQA.Selenium.IWebElement]] {
+          param([OpenQA.Selenium.IWebDriver]$WebDriver)
+          try { $WebDriver.FindElement([OpenQA.Selenium.By]::XPath("//article[contains(.//h2, '$($this.CurrentState.Version)')]")) } catch {}
+        }
+      ).GetAttribute('innerHTML') | ConvertFrom-Html
+      if ($ReleaseNotesNode) {
+        # Remove unnecessary list prefixes
+        $ReleaseNotesNode.SelectNodes('.//span[@class="text-primary"]').ForEach({ $_.Remove() })
         # ReleaseNotes (en-US)
         $this.CurrentState.Locale += [ordered]@{
           Locale = 'en-US'
           Key    = 'ReleaseNotes'
-          Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
+          Value  = $ReleaseNotesNode.SelectSingleNode('.//ul') | Get-TextContent | Format-Text
         }
       } else {
         $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
