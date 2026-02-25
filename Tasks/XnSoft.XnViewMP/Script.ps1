9cdee80f16f6a6c83f7d3a6f94f06738a4463553
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-02-26 00:52:39 +0800

    XnSoft.XnViewMP: Add zip+portable installer

--- Tasks/XnSoft.XnViewMP/Script.ps1
+++ Tasks/XnSoft.XnViewMP/Script.ps1
@@ -1,21 +1,35 @@
 $Prefix = 'https://download.xnview.com/old_versions/XnView_MP/'
 
-$Object1 = Invoke-WebRequest -Uri "${Prefix}?C=N;O=D;V=1;P=*win*x64*.exe;F=0"
-
-$InstallerName = $Object1.Links[1].href
-
-# Version
-$this.CurrentState.Version = [regex]::Match($InstallerName, '-([\d\.]+)[-\.]').Groups[1].Value
+$Object1 = Invoke-WebRequest -Uri "${Prefix}?C=N;O=D;V=1;F=0"
 
 # Installer
-$this.CurrentState.Installer += [ordered]@{
-  Architecture = 'x64'
-  InstallerUrl = "${Prefix}${InstallerName}"
+$this.CurrentState.Installer += $InstallerEXE = [ordered]@{
+  Architecture  = 'x64'
+  InstallerType = 'inno'
+  InstallerUrl  = "${Prefix}$($Object1.Links.Where({ try { $_.href -like 'XnView_MP-*-win-x64.exe' } catch {} }, 'First')[0].href)"
 }
+$VersionEXE = [regex]::Match($InstallerEXE.InstallerUrl, 'XnView_MP-(\d+(?:\.\d+)+)-win-x64\.exe').Groups[1].Value
+
+$this.CurrentState.Installer += $InstallerZIP = [ordered]@{
+  Architecture        = 'x64'
+  InstallerType       = 'zip'
+  NestedInstallerType = 'portable'
+  InstallerUrl        = "${Prefix}$($Object1.Links.Where({ try { $_.href -like 'XnView_MP-*-win-x64.zip' } catch {} }, 'First')[0].href)"
+}
+$VersionZIP = [regex]::Match($InstallerZIP.InstallerUrl, 'XnView_MP-(\d+(?:\.\d+)+)-win-x64\.zip').Groups[1].Value
+
+
+if ($VersionEXE -ne $VersionZIP) {
+  $this.Log("Inconsistent versions: EXE: ${VersionEXE}, ZIP: ${VersionZIP}", 'Error')
+  return
+}
+
+# Version
+$this.CurrentState.Version = $VersionEXE
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
-    $this.InstallerFiles[$this.CurrentState.Installer[0].InstallerUrl] = $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+    $this.InstallerFiles[$InstallerEXE.InstallerUrl] = $InstallerFile = Get-TempFile -Uri $InstallerEXE.InstallerUrl
     # RealVersion
     $this.CurrentState.RealVersion = $InstallerFile | Read-ProductVersionFromExe
 
