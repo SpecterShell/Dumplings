b2341542382079f131bbbcf9230f0541de39e6e0
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-02-28 13:29:39 +0800

    Splunk.UniversalForwarder: Remove x86 installers

--- Tasks/Splunk.UniversalForwarder/Script.ps1
+++ Tasks/Splunk.UniversalForwarder/Script.ps1
@@ -1,12 +1,12 @@
 $Object1 = Invoke-WebRequest -Uri 'https://www.splunk.com/en_us/download/universal-forwarder.html' | ConvertFrom-Html
 
 # Installer
-$Asset = $Object1.SelectSingleNode('//a[@data-platform="windows" and @data-arch="x86"]')
-$this.CurrentState.Installer += [ordered]@{
-  Architecture = 'x86'
-  InstallerUrl = $Asset.Attributes['data-link'].Value
-}
-$VersionX86 = $Asset.Attributes['data-version'].Value
+# $Asset = $Object1.SelectSingleNode('//a[@data-platform="windows" and @data-arch="x86"]')
+# $this.CurrentState.Installer += [ordered]@{
+#   Architecture = 'x86'
+#   InstallerUrl = $Asset.Attributes['data-link'].Value
+# }
+# $VersionX86 = $Asset.Attributes['data-version'].Value
 
 $Asset = $Object1.SelectSingleNode('//a[@data-platform="windows" and @data-arch="x86_64"]')
 $this.CurrentState.Installer += [ordered]@{
@@ -15,10 +15,10 @@ $this.CurrentState.Installer += [ordered]@{
 }
 $VersionX64 = $Asset.Attributes['data-version'].Value
 
-if ($VersionX86 -ne $VersionX64) {
-  $this.Log("Inconsistent versions: x86: ${VersionX86}, x64: ${VersionX64}", 'Error')
-  return
-}
+# if ($VersionX86 -ne $VersionX64) {
+#   $this.Log("Inconsistent versions: x86: ${VersionX86}, x64: ${VersionX64}", 'Error')
+#   return
+# }
 
 # Version
 $this.CurrentState.Version = $VersionX64
