105c33d9c011c7b7a72940488a4d6a7c07308d88
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-03-11 18:21:39 +0800

    Revamp the script, remove the state

--- Tasks/NDI.NDIRuntime/Script.ps1
+++ Tasks/NDI.NDIRuntime/Script.ps1
@@ -1,4 +1,3 @@
-# The Runtime & Tools are updated together, so we can check the same JSON as NDI Tools for the version number.
 $Object1 = Invoke-RestMethod -Uri 'https://downloads.ndi.tv/Tools/ndi_tools_win_current_version.json'
 
 # Version
@@ -14,31 +13,7 @@ switch -Regex ($this.Check()) {
     try {
       $Object2 = Invoke-WebRequest -Uri 'https://docs.ndi.video/all/using-ndi/ndi-tools/release-notes' | ConvertFrom-Html
 
-      $VersionParts = $this.CurrentState.Version.Split('.')
-      $VersionFull = $this.CurrentState.Version
-      $VersionSimplified = $VersionFull -replace '(?:\.0)+$', ''
-      $VersionMajor = if ($VersionParts.Length -ge 2) { "$($VersionParts[0]).$($VersionParts[1])" } else { $VersionParts[0] }
-      $VersionCandidates = @($VersionFull, $VersionSimplified, $VersionMajor) | Where-Object { $_ } | Select-Object -Unique
-
-      $ReleaseNotesTitleNode = $null
-      foreach ($VersionCandidate in $VersionCandidates) {
-        $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//h2[@id='ndi-$VersionCandidate']")
-        if ($ReleaseNotesTitleNode) {
-          break
-        }
-      }
-
-      if (-not $ReleaseNotesTitleNode) {
-        $ReleaseNotesH2Nodes = $Object2.SelectNodes('//h2')
-        foreach ($VersionCandidate in $VersionCandidates) {
-          $VersionRegex = "^\s*NDI\s+$([regex]::Escape($VersionCandidate))\s*$"
-          $ReleaseNotesTitleNode = $ReleaseNotesH2Nodes | Where-Object { $_.InnerText -match $VersionRegex } | Select-Object -First 1
-          if ($ReleaseNotesTitleNode) {
-            break
-          }
-        }
-      }
-
+      $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//h2[contains(., '$($this.CurrentState.Version.Split('.')[0..2] -join '.')')]")
       if ($ReleaseNotesTitleNode) {
         # Remove pseudo nodes
         $Object2.SelectNodes("//li/div[contains(.//@style, `"pseudoBefore`")]").ForEach({ $_.Remove() })
