1dea7dfca70ebd4b2da15949a0c45ecd9a7e82f9
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-03-12 14:44:55 +0800

    Ramplio.CatLight: Add ReleaseNotes (en-US)

--- Tasks/Ramplio.CatLight/Script.Alt.ps1
+++ Tasks/Ramplio.CatLight/Script.Alt.ps1
@@ -9,6 +9,38 @@ $this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].Inst
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
+    try {
+      # ReleaseNotesUrl (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotesUrl'
+        Value  = 'https://support.catlight.io/s1-general/updates'
+      }
+
+      $Object2 = (Invoke-RestMethod -Uri 'https://support.catlight.io/s1-general/updates?format=rss').Where({ $_.title.Contains($this.CurrentState.Version.Split('.')[0..1] -join '.') }, 'First')
+
+      if ($Object2) {
+        # ReleaseNotesUrl (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotesUrl'
+          Value  = $Object2[0].link
+        }
+
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $Object2[0].encoded.'#cdata-section' | ConvertFrom-Html | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotesUrl, ReleaseTime and ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
     $this.Print()
     $this.Write()
   }
