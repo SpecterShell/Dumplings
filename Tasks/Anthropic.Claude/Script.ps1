ec95200f11bf02fc540eef3f64a6111879248b98
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-03-19 11:13:06 +0800

    Anthropic.Claude: Add MSIX installers

--- Tasks/Anthropic.Claude/Script.ps1
+++ Tasks/Anthropic.Claude/Script.ps1
@@ -1,23 +1,37 @@
 # Installer
-$this.CurrentState.Installer += $InstallerX64 = [ordered]@{
+$this.CurrentState.Installer += $InstallerX64EXE = [ordered]@{
   Architecture = 'x64'
   InstallerUrl = curl -fsSA $DumplingsInternetExplorerUserAgent -w '%{redirect_url}' 'https://claude.ai/api/desktop/win32/x64/exe/latest/redirect' | Select-Object -Last 1
 }
-$VersionX64 = [regex]::Match($InstallerX64.InstallerUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
+$VersionX64EXE = [regex]::Match($InstallerX64EXE.InstallerUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
 
-$this.CurrentState.Installer += $InstallerArm64 = [ordered]@{
+$this.CurrentState.Installer += $InstallerArm64EXE = [ordered]@{
   Architecture = 'arm64'
   InstallerUrl = curl -fsSA $DumplingsInternetExplorerUserAgent -w '%{redirect_url}' 'https://claude.ai/api/desktop/win32/arm64/exe/latest/redirect' | Select-Object -Last 1
 }
-$VersionArm64 = [regex]::Match($InstallerArm64.InstallerUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
+$VersionArm64EXE = [regex]::Match($InstallerArm64EXE.InstallerUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
 
-if ($VersionX64 -ne $VersionArm64) {
-  $this.Log("Inconsistent versions: x64: $VersionX64, arm64: $VersionArm64", 'Error')
+$this.CurrentState.Installer += $InstallerX64MSIX = [ordered]@{
+  Architecture  = 'x64'
+  InstallerType = 'msix'
+  InstallerUrl  = curl -fsSA $DumplingsInternetExplorerUserAgent -w '%{redirect_url}' 'https://claude.ai/api/desktop/win32/x64/msix/latest/redirect' | Select-Object -Last 1
+}
+$VersionX64MSIX = [regex]::Match($InstallerX64MSIX.InstallerUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
+
+$this.CurrentState.Installer += $InstallerArm64MSIX = [ordered]@{
+  Architecture  = 'arm64'
+  InstallerType = 'msix'
+  InstallerUrl  = curl -fsSA $DumplingsInternetExplorerUserAgent -w '%{redirect_url}' 'https://claude.ai/api/desktop/win32/arm64/msix/latest/redirect' | Select-Object -Last 1
+}
+$VersionArm64MSIX = [regex]::Match($InstallerArm64MSIX.InstallerUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
+
+if (@(@($VersionX64EXE, $VersionArm64EXE, $VersionX64MSIX, $VersionArm64MSIX) | Sort-Object -Unique).Count -gt 1) {
+  $this.Log("Inconsistent versions: x64 EXE: $VersionX64EXE, arm64 EXE: $VersionArm64EXE, x64 MSIX: $VersionX64MSIX, arm64 MSIX: $VersionArm64MSIX", 'Error')
   return
 }
 
 # Version
-$this.CurrentState.Version = $VersionX64
+$this.CurrentState.Version = $VersionX64EXE
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
