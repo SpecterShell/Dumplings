095444735840fe6822400c1457e11b33da37cb1a
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-04-03 14:21:35 +0800

    Fix InstallerType

--- Tasks/OhRudi.S3PR/Script.ps1
+++ Tasks/OhRudi.S3PR/Script.ps1
@@ -5,7 +5,7 @@ $this.CurrentState.Version = $Object1.tag_name -replace '^v'
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerType = 'exe'
+  InstallerType = 'portable'
   InstallerUrl  = $Object1.assets.Where({ $_.name.EndsWith('.exe') }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
 }
 
