9cb01c8bb357f494f7ad9aecc37545df8efc094a
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-04-05 14:12:25 +0800

    Add PaloAltoNetworks.PrismaAccessBrowser

---
+++ Tasks/PaloAltoNetworks.PrismaAccessBrowser/Script.ps1
@@ -0,0 +1,43 @@
+# x86
+$Object1 = (Invoke-RestMethod -Uri 'https://releases.talon-sec.com/api/v1/appcast.xml?appid={dfef2477-4f0e-454b-bc0d-03ce61074e4c}&platform=win&architecture=x64&channel=packaged')[-1]
+# x64
+$Object2 = (Invoke-RestMethod -Uri 'https://releases.talon-sec.com/api/v1/appcast.xml?appid={dfef2477-4f0e-454b-bc0d-03ce61074e4c}&platform=win&architecture=arm64&channel=packaged')[-1]
+
+if ($Object1.enclosure.shortVersionString -ne $Object2.enclosure.shortVersionString) {
+  $this.Log("Inconsistent versions: x86: $($Object1.enclosure.shortVersionString), x64: $($Object2.enclosure.shortVersionString)", 'Error')
+  return
+}
+
+# Version
+$this.CurrentState.Version = $Object2.enclosure.shortVersionString
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x86'
+  InstallerUrl = $Object1.enclosure.url
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = $Object2.enclosure.url
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object2.pubDate | Get-Date -AsUTC
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
