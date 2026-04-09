02d874fa05dba3955d9a4c029628bcfd1896635f
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-04-09 11:46:42 +0800

    ZWSOFT: Fetch one more page of download data

--- Tasks/#ZWSOFT/Script.ps1
+++ Tasks/#ZWSOFT/Script.ps1
@@ -3,3 +3,11 @@ $Global:DumplingsStorage.ZWSOFTApps = Invoke-RestMethod -Uri 'https://www.zwsoft
   system         = 'Windows'
   page           = '1'
 }
+
+$Object1 = Invoke-RestMethod -Uri 'https://www.zwsoft.cn/index.php?g=Api&m=Common&a=getDownloadCenterProduct' -Method Post -Body @{
+  environment    = 'pc'
+  system         = 'Windows'
+  page           = '2'
+}
+
+$Global:DumplingsStorage.ZWSOFTApps.data += $Object1.data
