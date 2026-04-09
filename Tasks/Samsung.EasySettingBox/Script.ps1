3b7ca1d72d582497840fb0c4942d86e803bfe83d
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-04-09 11:30:43 +0800

    Samsung.EasySettingBox: Remove x86 installer

--- Tasks/Samsung.EasySettingBox/Script.ps1
+++ Tasks/Samsung.EasySettingBox/Script.ps1
@@ -1,25 +1,25 @@
 $Object1 = Invoke-RestMethod -Uri 'https://www.samsung.com/ie/support/model/LS27CG552EUXEN/' | ConvertFrom-Html
 $Object2 = $Object1.SelectSingleNode('//*[@data-sdf-prop="contents"]').InnerHtml | ConvertFrom-Json
 # x86
-$Object3 = $Object2.softwares.Where({ $_.description.Contains('Easy setting box') -and $_.description.Contains('32bit') -and $_.fileName.EndsWith('.msi') }, 'First')[0]
-$VersionX86 = [regex]::Match($Object3.fileVersion, '(\d+(?:\.\d+)+)').Groups[1].Value
+# $Object3 = $Object2.softwares.Where({ $_.description.Contains('Easy setting box') -and $_.description.Contains('32bit') -and $_.fileName.EndsWith('.msi') }, 'First')[0]
+# $VersionX86 = [regex]::Match($Object3.fileVersion, '(\d+(?:\.\d+)+)').Groups[1].Value
 # x64
 $Object4 = $Object2.softwares.Where({ $_.description.Contains('Easy setting box') -and $_.description.Contains('64bit') -and $_.fileName.EndsWith('.msi') }, 'First')[0]
 $VersionX64 = [regex]::Match($Object4.fileVersion, '(\d+(?:\.\d+)+)').Groups[1].Value
 
-if ($VersionX86 -ne $VersionX64) {
-  $this.Log("Inconsistent versions: x86: ${VersionX86}, x64: ${VersionX64}", 'Error')
-  return
-}
+# if ($VersionX86 -ne $VersionX64) {
+#   $this.Log("Inconsistent versions: x86: ${VersionX86}, x64: ${VersionX64}", 'Error')
+#   return
+# }
 
 # Version
 $this.CurrentState.Version = $VersionX64
 
 # Installer
-$this.CurrentState.Installer += [ordered]@{
-  Architecture = 'x86'
-  InstallerUrl = Join-Uri 'https://downloadcenter.samsung.com/content/' $Object3.filePath
-}
+# $this.CurrentState.Installer += [ordered]@{
+#   Architecture = 'x86'
+#   InstallerUrl = Join-Uri 'https://downloadcenter.samsung.com/content/' $Object3.filePath
+# }
 $this.CurrentState.Installer += [ordered]@{
   Architecture = 'x64'
   InstallerUrl = Join-Uri 'https://downloadcenter.samsung.com/content/' $Object4.filePath
@@ -29,7 +29,7 @@ switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
     try {
       # ReleaseTime
-      $this.CurrentState.ReleaseTime = $Object3.fileModifiedDateCalendar | ConvertFrom-UnixTimeMilliseconds
+      $this.CurrentState.ReleaseTime = $Object4.fileModifiedDateCalendar | ConvertFrom-UnixTimeMilliseconds
     } catch {
       $_ | Out-Host
       $this.Log($_, 'Warning')
