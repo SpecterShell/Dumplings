19b13fb12c527443210150437bb577b9ef1e892c
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-04-21 19:16:41 +0800

    KaringX.ClashMi: Parse version from installer

--- Tasks/KaringX.ClashMi/Script.ps1
+++ Tasks/KaringX.ClashMi/Script.ps1
@@ -13,6 +13,10 @@ $this.CurrentState.Installer += [ordered]@{
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
+    $this.InstallerFiles[$this.CurrentState.Installer[0].InstallerUrl] = $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+    # RealVersion
+    $this.CurrentState.RealVersion = $InstallerFile | Read-ProductVersionFromExe
+
     try {
       # ReleaseTime
       $this.CurrentState.ReleaseTime = $Object1.published_at.ToUniversalTime()
