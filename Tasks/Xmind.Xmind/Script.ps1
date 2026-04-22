5d0639976e17ef04ad7bcc73e0a4fb2ab249a635
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-04-22 12:15:21 +0800

    Xmind.Xmind: Remove ReleaseNotes

--- Tasks/Xmind.Xmind/Script.ps1
+++ Tasks/Xmind.Xmind/Script.ps1
@@ -27,50 +27,6 @@ switch -Regex ($this.Check()) {
     # RealVersion
     $this.CurrentState.RealVersion = $InstallerFile | Read-ProductVersionFromExe
 
-    try {
-      $Object1 = Invoke-WebRequest -Uri 'https://xmind.app/desktop/release-notes/' | ConvertFrom-Html
-
-      $ReleaseNotesTitleNode = $Object1.SelectSingleNode("//div[@class='version-block' and contains(./h3/text(), '$($this.CurrentState.Version)')]")
-      if ($ReleaseNotesTitleNode) {
-        # ReleaseTime
-        $this.CurrentState.ReleaseTime = $ReleaseNotesTitleNode.SelectSingleNode('./h5').InnerText.Trim() | Get-Date -Format 'yyyy-MM-dd'
-
-        # ReleaseNotes (en-US)
-        $this.CurrentState.Locale += [ordered]@{
-          Locale = 'en-US'
-          Key    = 'ReleaseNotes'
-          Value  = $ReleaseNotesTitleNode.SelectNodes('./h5/following-sibling::node()') | Get-TextContent | Format-Text
-        }
-      } else {
-        $this.Log("No ReleaseTime and ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
-      }
-    } catch {
-      $_ | Out-Host
-      $this.Log($_, 'Warning')
-    }
-
-    try {
-      $Object2 = Invoke-WebRequest -Uri 'https://xmind.cn/desktop/release-notes/' | ConvertFrom-Html
-
-      $ReleaseNotesCNTitleNode = $Object2.SelectSingleNode("//div[@class='version-block' and contains(./h3/text(), '$($this.CurrentState.Version)')]")
-      if ($ReleaseNotesCNTitleNode) {
-        # ReleaseTime
-        $this.CurrentState.ReleaseTime ??= $ReleaseNotesCNTitleNode.SelectSingleNode('./h5').InnerText.Trim() | Get-Date -Format 'yyyy-MM-dd'
-
-        # ReleaseNotes (zh-CN)
-        $this.CurrentState.Locale += [ordered]@{
-          Locale = 'zh-CN'
-          Key    = 'ReleaseNotes'
-          Value  = $ReleaseNotesCNTitleNode.SelectNodes('./h5/following-sibling::node()') | Get-TextContent | Format-Text
-        }
-      } else {
-        $this.Log("No ReleaseTime and ReleaseNotes (zh-CN) for version $($this.CurrentState.Version)", 'Warning')
-      }
-    } catch {
-      $_ | Out-Host
-      $this.Log($_, 'Warning')
-    }
-
     $this.Print()
     $this.Write()
   }
