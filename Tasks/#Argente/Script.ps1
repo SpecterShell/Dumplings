888adbff0a185e508886b9a39c47dc510003fd31
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-04-30 22:48:17 +0800

    Argente: Fix hardware ID in requests

--- Tasks/#Argente/Script.ps1
+++ Tasks/#Argente/Script.ps1
@@ -1,6 +1,6 @@
 # x86
 $Global:DumplingsStorage.ArgenteX86 = Invoke-RestMethod -Uri 'https://argenteutilities.com/service/updates.php?hardware=1&arquitectura=X86&hardware=0000000000000000000000000000000000000000000000000000000000000000'
 # x64
-$Global:DumplingsStorage.ArgenteX64 = Invoke-RestMethod -Uri 'https://argenteutilities.com/service/updates.php?hardware=1&arquitectura=X64&hardware=00000000000000000000000000000000000000000000000000000000000₀₀₀₀'
+$Global:DumplingsStorage.ArgenteX64 = Invoke-RestMethod -Uri 'https://argenteutilities.com/service/updates.php?hardware=1&arquitectura=X64&hardware=0000000000000000000000000000000000000000000000000000000000000000'
 # arm64
-$Global:DumplingsStorage.ArgenteARM64 = Invoke-RestMethod -Uri 'https://argenteutilities.com/service/updates.php?hardware=1&arquitectura=ARM64&hardware=₀₀₀₀₀₀₀₀₀₀₀₀₀₀₀₀₀₀₀₀₀₀₀₀₀₀₀₀₁'
+$Global:DumplingsStorage.ArgenteARM64 = Invoke-RestMethod -Uri 'https://argenteutilities.com/service/updates.php?hardware=1&arquitectura=ARM64&hardware=0000000000000000000000000000000000000000000000000000000000000000'
