107ef968bc096528015522b4196418964ab86094
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-04-30 20:40:44 +0800

    Add Argente

--- Tasks/Argente.Utilities/Script.ps1
+++ Tasks/Argente.Utilities/Script.ps1
@@ -1,29 +1,23 @@
-# x86
-$Object1 = Invoke-RestMethod -Uri 'https://argenteutilities.com/service/updates.php?hardware=1&arquitectura=X86&hardware=0000000000000000000000000000000000000000000000000000000000000000'
-# x64
-$Object2 = Invoke-RestMethod -Uri 'https://argenteutilities.com/service/updates.php?hardware=1&arquitectura=X64&hardware=0000000000000000000000000000000000000000000000000000000000000000'
-# arm64
-$Object3 = Invoke-RestMethod -Uri 'https://argenteutilities.com/service/updates.php?hardware=1&arquitectura=ARM64&hardware=0000000000000000000000000000000000000000000000000000000000000000'
 # x86 EXE
-$Object4 = $Object1.data.Where({ $_.name -eq 'Argente Utilities' -and $_.installer -eq 'utilitiesx86' }, 'First')[0]
+$Object1 = $Global:DumplingsStorage.ArgenteX86.data.Where({ $_.name -eq 'Argente Utilities' -and $_.installer -eq 'utilitiesx86' }, 'First')[0]
 # x64 EXE
-$Object5 = $Object2.data.Where({ $_.name -eq 'Argente Utilities' -and $_.installer -eq 'utilitiesx64' }, 'First')[0]
+$Object2 = $Global:DumplingsStorage.ArgenteX64.data.Where({ $_.name -eq 'Argente Utilities' -and $_.installer -eq 'utilitiesx64' }, 'First')[0]
 # arm64 EXE
-$Object6 = $Object3.data.Where({ $_.name -eq 'Argente Utilities' -and $_.installer -eq 'utilitiesarm64' }, 'First')[0]
+$Object3 = $Global:DumplingsStorage.ArgenteARM64.data.Where({ $_.name -eq 'Argente Utilities' -and $_.installer -eq 'utilitiesarm64' }, 'First')[0]
 # x86 Portable
-$Object7 = $Object1.data.Where({ $_.name -eq 'Argente Utilities' -and $_.installer -eq 'utilitiesx86portable' }, 'First')[0]
+$Object4 = $Global:DumplingsStorage.ArgenteX86.data.Where({ $_.name -eq 'Argente Utilities' -and $_.installer -eq 'utilitiesx86portable' }, 'First')[0]
 # x64 Portable
-$Object8 = $Object2.data.Where({ $_.name -eq 'Argente Utilities' -and $_.installer -eq 'utilitiesx64portable' }, 'First')[0]
+$Object5 = $Global:DumplingsStorage.ArgenteX64.data.Where({ $_.name -eq 'Argente Utilities' -and $_.installer -eq 'utilitiesx64portable' }, 'First')[0]
 # arm64 Portable
-$Object9 = $Object3.data.Where({ $_.name -eq 'Argente Utilities' -and $_.installer -eq 'utilitiesarm64portable' }, 'First')[0]
+$Object6 = $Global:DumplingsStorage.ArgenteARM64.data.Where({ $_.name -eq 'Argente Utilities' -and $_.installer -eq 'utilitiesarm64portable' }, 'First')[0]
 
-if (@(@($Object4.version, $Object5.version, $Object6.version, $Object7.version, $Object8.version, $Object9.version) | Sort-Object -Unique).Count -gt 1) {
-  $this.Log("Inconsistent versions: x86 EXE: $($Object4.version), x64 EXE: $($Object5.version), arm64 EXE: $($Object6.version), x86 Portable: $($Object7.version), x64 Portable: $($Object8.version), arm64 Portable: $($Object9.version)", 'Error')
+if (@(@($Object1.version, $Object2.version, $Object3.version, $Object4.version, $Object5.version, $Object6.version) | Sort-Object -Unique).Count -gt 1) {
+  $this.Log("Inconsistent versions: x86 EXE: $($Object1.version), x64 EXE: $($Object2.version), arm64 EXE: $($Object3.version), x86 Portable: $($Object4.version), x64 Portable: $($Object5.version), arm64 Portable: $($Object6.version)", 'Error')
   return
 }
 
 # Version
-$this.CurrentState.Version = $Object5.version
+$this.CurrentState.Version = $Object2.version
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
@@ -60,15 +54,15 @@ $this.CurrentState.Installer += [ordered]@{
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
     try {
-      $Object10 = Invoke-WebRequest -Uri 'https://argenteutilities.com/en/blog' | ConvertFrom-Html
+      $Object7 = Invoke-WebRequest -Uri 'https://argenteutilities.com/en/blog' | ConvertFrom-Html
 
-      $ReleaseNotesTitleNode = $Object10.SelectSingleNode("//h3[contains(text(), '$($this.CurrentState.Version)')]")
+      $ReleaseNotesTitleNode = $Object7.SelectSingleNode("//h3[contains(text(), '$($this.CurrentState.Version)')]")
       if ($ReleaseNotesTitleNode) {
         # ReleaseTime
         $this.CurrentState.ReleaseTime = [regex]::Match($ReleaseNotesTitleNode.SelectSingleNode('./following-sibling::ul').InnerText, '(\d{1,2}\W+[a-zA-Z]+\W+20\d{2})').Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
 
         # Remove download buttons
-        $Object10.SelectNodes('//div[contains(@class, "more")]').ForEach({ $_.Remove() })
+        $Object7.SelectNodes('//div[contains(@class, "more")]').ForEach({ $_.Remove() })
         # ReleaseNotes (en-US)
         $this.CurrentState.Locale += [ordered]@{
           Locale = 'en-US'
