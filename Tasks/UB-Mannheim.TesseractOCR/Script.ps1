9230142b63e92f162c776be5a4be5f87d4caaf51
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-04-30 21:05:41 +0800

    UB-Mannheim.TesseractOCR: Switch to GitHub source

--- Tasks/UB-Mannheim.TesseractOCR/Script.ps1
+++ Tasks/UB-Mannheim.TesseractOCR/Script.ps1
@@ -1,47 +1,36 @@
-$Prefix = 'https://digi.bib.uni-mannheim.de/tesseract/'
-
-$Object1 = (Invoke-WebRequest -Uri "${Prefix}?C=M;O=D;F=0;P=*.exe").Content
+$Object1 = Invoke-GitHubApi -Uri 'https://api.github.com/repos/UB-Mannheim/tesseract/releases/latest'
 
 # Version
-$this.CurrentState.Version = [regex]::Match($Object1, 'tesseract-ocr-w64-setup-([\d\.]+)\.exe').Groups[1].Value
+$this.CurrentState.Version = $Object1.tag_name -replace '^v'
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  Architecture = 'x86'
-  InstallerUrl = "${Prefix}tesseract-ocr-w32-setup-$($this.CurrentState.Version).exe"
-}
-$this.CurrentState.Installer += [ordered]@{
   Architecture = 'x64'
-  InstallerUrl = "${Prefix}tesseract-ocr-w64-setup-$($this.CurrentState.Version).exe"
+  InstallerUrl = $Object1.assets.Where({ $_.name.EndsWith('.exe') -and $_.name.Contains('w64') }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
 }
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
     try {
       # ReleaseTime
-      $this.CurrentState.ReleaseTime = [datetime]::ParseExact(
-        [regex]::Match($this.CurrentState.Version, '(\d{8})').Groups[1].Value,
-        'yyyyMMdd',
-        $null
-      ).ToString('yyyy-MM-dd')
-    } catch {
-      $_ | Out-Host
-      $this.Log($_, 'Warning')
-    }
+      $this.CurrentState.ReleaseTime = $Object1.published_at.ToUniversalTime()
 
-    try {
-      $Object2 = Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/wiki/UB-Mannheim/tesseract/Home.md' | Convert-MarkdownToHtml
-
-      # ReleaseNotes (en-US)
-      $ReleaseNotesNode = $Object2.SelectSingleNode("/ul[2]/li[contains(./text(), '$([regex]::Match($this.CurrentState.Version, '(\d+\.\d+\.\d+)').Groups[1].Value)')]")
-      if ($ReleaseNotesNode) {
+      if (-not [string]::IsNullOrWhiteSpace($Object1.body)) {
+        # ReleaseNotes (en-US)
         $this.CurrentState.Locale += [ordered]@{
           Locale = 'en-US'
           Key    = 'ReleaseNotes'
-          Value  = $ReleaseNotesNode.SelectSingleNode('./text()').InnerText | Format-Text
+          Value  = $Object1.body | Convert-MarkdownToHtml -Extensions 'advanced', 'emojis', 'hardlinebreak' | Get-TextContent | Format-Text
         }
       } else {
         $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+
+      # ReleaseNotesUrl (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotesUrl'
+        Value  = $Object1.html_url
       }
     } catch {
       $_ | Out-Host
