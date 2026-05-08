2b0af02acaa02bb5087b42530857cf99404ae550
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-05-08 16:58:12 +0800

    Move badlogic.pi to EarendilWorks.pi

--- Tasks/EarendilWorks.pi/Script.ps1
+++ Tasks/EarendilWorks.pi/Script.ps1
@@ -1,4 +1,4 @@
-$Object1 = Invoke-GitHubApi -Uri 'https://api.github.com/repos/badlogic/pi-mono/releases/latest'
+$Object1 = Invoke-GitHubApi -Uri 'https://api.github.com/repos/earendil-works/pi/releases/latest'
 
 # Version
 $this.CurrentState.Version = $Object1.tag_name -replace '^v'
