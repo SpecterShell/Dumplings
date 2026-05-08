fcbfd8ba43bef0e3ee1a8c4673de4f8b443508e8
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-05-08 18:07:58 +0800

    AceBIT: Remove ReleaseTime. Fix release notes parsing

--- Tasks/AceBIT.PasswordDepotEnterpriseServer.18/Script.ps1
+++ Tasks/AceBIT.PasswordDepotEnterpriseServer.18/Script.ps1
@@ -11,19 +11,10 @@ function Get-ReleaseNotes {
   try {
     $Object2 = Invoke-WebRequest -Uri 'https://www.password-depot.de/en/resources/whats-new.htm' | ConvertFrom-Html
 
-    $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//*[@id='tabs-2']//h2[contains(., '$($this.CurrentState.Version)')]")
+    $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//*[@id='content-server']//h3[contains(., '$($this.CurrentState.Version)')]")
     if ($ReleaseNotesTitleNode) {
-      # ReleaseTime
-      if ($ReleaseNotesTitleNode.InnerText -match '(\d{1,2}\.\d{1,2}\.20\d{2})') {
-        $this.CurrentState.ReleaseTime = [datetime]::ParseExact($Matches[1], 'dd.MM.yyyy', $null).ToString('yyyy-MM-dd')
-      } elseif ($ReleaseNotesTitleNode.InnerText -match '(\d{1,2}\W+[a-zA-Z]+\W+20\d{2})') {
-        $this.CurrentState.ReleaseTime = $Matches[1] | Get-Date -Format 'yyyy-MM-dd'
-      } else {
-        $this.Log("No ReleaseTime for version $($this.CurrentState.Version)", 'Warning')
-      }
-
       # ReleaseNotes (en-US)
-      $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node -and $Node.Name -ne 'h2'; $Node = $Node.NextSibling) { $Node }
+      $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node -and $Node.Name -ne 'h3'; $Node = $Node.NextSibling) { $Node }
       $this.CurrentState.Locale += [ordered]@{
         Locale = 'en-US'
         Key    = 'ReleaseNotes'
