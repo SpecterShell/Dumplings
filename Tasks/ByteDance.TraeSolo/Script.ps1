8fa42715d37e848967def47dc13f01133b731eb6
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-05-09 19:17:36 +0800

    Add ByteDance.TraeSolo

---
+++ Tasks/ByteDance.TraeSolo/Script.ps1
@@ -0,0 +1,60 @@
+$Object1 = Invoke-RestMethod -Uri 'https://api.trae.ai/icube/api/v1/native/version/trae/latest'
+# $Object1 = Invoke-RestMethod -Uri 'https://api.marscode.cn/icube/api/v1/native/version/trae/latest'
+
+# Version
+$this.CurrentState.Version = $Object1.data.solo.win32.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.data.solo.win32.download.Where({ $_.region -eq 'va' }, 'First')[0].x64
+}
+# $this.CurrentState.Installer += [ordered]@{
+#   InstallerUrl = $Object1.data.solo.win32.download.Where({ $_.region -eq 'sg' }, 'First')[0].x64
+# }
+$this.CurrentState.Installer += [ordered]@{
+  InstallerLocale = 'zh-CN'
+  InstallerUrl    = $Object1.data.solo.win32.download.Where({ $_.region -eq 'cn' }, 'First')[0].x64
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.data.solo.win32.extra.uploadDate | ConvertFrom-UnixTimeMilliseconds
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    try {
+      $EdgeDriver = Get-EdgeDriver -Headless
+      $EdgeDriver.Navigate().GoToUrl('https://www.trae.ai/changelog')
+
+      $ReleaseNotesNode = [OpenQA.Selenium.Support.UI.WebDriverWait]::new($EdgeDriver, [timespan]::FromSeconds(30)).Until(
+        [System.Func[OpenQA.Selenium.IWebDriver, OpenQA.Selenium.IWebElement]] {
+          param([OpenQA.Selenium.IWebDriver]$WebDriver)
+          try { $WebDriver.FindElement([OpenQA.Selenium.By]::XPath("//section[contains(@class, 'versionEntry') and contains(.//div[contains(@class, 'metadataColumn')], '$($this.CurrentState.Version)') and contains(.//div[contains(@class, 'metadataColumn')], 'SOLO')]")) } catch {}
+        }
+      ).GetAttribute('innerHTML') | ConvertFrom-Html
+
+      # ReleaseNotes (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotes'
+        Value  = $ReleaseNotesNode.SelectSingleNode('.//div[contains(@class, "contentColumn")]') | Get-TextContent | Format-Text
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
