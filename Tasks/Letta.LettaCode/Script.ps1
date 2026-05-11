7d2b0ac688b5e692aeaa7be5a6098ea5efbcaad1
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-05-11 23:23:18 +0800

    Add Letta.LettaCode

---
+++ Tasks/Letta.LettaCode/Script.ps1
@@ -0,0 +1,65 @@
+$Object1 = Invoke-RestMethod -Uri 'https://download.todesktop.com/260305dtu2nh5/td-latest.json'
+
+# Version
+$this.CurrentState.Version = $Object1.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture  = 'x64'
+  InstallerType = 'nullsoft'
+  # InstallerUrl  = $Object1.artifacts.nsis.x64.url | ConvertTo-UnescapedUri
+  InstallerUrl  = "https://download.letta.com/builds/$([regex]::Match($Object1.artifacts.nsis.x64.path, 'Build ([a-zA-Z0-9]+)').Groups[1].Value)/windows/nsis/x64"
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.createdAt.ToUniversalTime()
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    try {
+      # ReleaseNotesUrl (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotesUrl'
+        Value  = 'https://github.com/letta-ai/letta-code/releases'
+      }
+
+      $Object2 = Invoke-GitHubApi -Uri "https://api.github.com/repos/letta-ai/letta-code/releases/tags/v$($this.CurrentState.Version)"
+
+      if (-not [string]::IsNullOrWhiteSpace($Object2.body)) {
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $Object2.body | Convert-MarkdownToHtml -Extensions 'advanced', 'emojis', 'hardlinebreak' | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+
+      # ReleaseNotesUrl (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotesUrl'
+        Value  = "https://github.com/letta-ai/letta-code/releases/tag/v$($this.CurrentState.Version)"
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
