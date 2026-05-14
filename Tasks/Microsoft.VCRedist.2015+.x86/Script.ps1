1f1038ce1415a5e45abdbcb9285398ef422c790f
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-05-14 22:10:49 +0800

    Microsoft.VCRedist.2015+: Switch to aka.ms source

--- Tasks/Microsoft.VCRedist.2015+.x86/Script.ps1
+++ Tasks/Microsoft.VCRedist.2015+.x86/Script.ps1
@@ -1,76 +1,99 @@
-$ManifestFile = $Global:DumplingsStorage.VisualStudioManifestFile
-
-$StreamReader = [System.IO.StreamReader]::new($ManifestFile)
-$JsonTextReader = [Newtonsoft.Json.JsonTextReader]::new($StreamReader)
-
-$PackageId = 'Microsoft.VisualCpp.Redist.14'
-$PackageChip = 'x86'
-
-$Mode = 0
-
-while ($JsonTextReader.Read()) {
-  if ($Mode -eq 0) {
-    if ($JsonTextReader.TokenType -eq [Newtonsoft.Json.JsonToken]::PropertyName -and $JsonTextReader.Value -eq 'id') {
-      if ($JsonTextReader.Read() -and $JsonTextReader.TokenType -eq [Newtonsoft.Json.JsonToken]::String -and $JsonTextReader.Value -eq $PackageId) {
-        $Mode = 1
-      }
+function Read-Installer {
+  $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+  # Version
+  $this.CurrentState.Version = $InstallerFile | Read-ProductVersionFromExe
+  # InstallerSha256
+  $this.CurrentState.Installer[0]['InstallerSha256'] = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
+  # ProductCode
+  $this.CurrentState.Installer[0]['ProductCode'] = $InstallerFile | Read-ProductCodeFromBurn
+  # AppsAndFeaturesEntries
+  $this.CurrentState.Installer[0]['AppsAndFeaturesEntries'] = @(
+    [ordered]@{
+      UpgradeCode = $InstallerFile | Read-UpgradeCodeFromBurn
     }
-  } elseif ($Mode -eq 1) {
-    if ($JsonTextReader.TokenType -eq [Newtonsoft.Json.JsonToken]::PropertyName) {
-      switch ($JsonTextReader.Value) {
-        'version' {
-          if ($JsonTextReader.Read() -and $JsonTextReader.TokenType -eq [Newtonsoft.Json.JsonToken]::String) {
-            # Version
-            $this.CurrentState.Version = $JsonTextReader.Value
-          }
-        }
-        'chip' {
-          if ($JsonTextReader.Read() -and $JsonTextReader.TokenType -eq [Newtonsoft.Json.JsonToken]::String -and $JsonTextReader.Value -eq $PackageChip) {
-            $Mode = 2
-          }
-        }
-      }
-    }
-  } elseif ($Mode -eq 2) {
-    if ($JsonTextReader.TokenType -eq [Newtonsoft.Json.JsonToken]::PropertyName -and $JsonTextReader.Value -eq 'fileName') {
-      if ($JsonTextReader.Read() -and $JsonTextReader.TokenType -eq [Newtonsoft.Json.JsonToken]::String -and $JsonTextReader.Value -eq 'VC_redist.x86.exe') {
-        $Mode = 3
-      }
-    }
-  } elseif ($Mode -eq 3) {
-    if ($JsonTextReader.TokenType -eq [Newtonsoft.Json.JsonToken]::PropertyName -and $JsonTextReader.Value -eq 'url') {
-      if ($JsonTextReader.Read() -and $JsonTextReader.TokenType -eq [Newtonsoft.Json.JsonToken]::String) {
-        # Installer
-        $this.CurrentState.Installer += [ordered]@{
-          Architecture = 'x86'
-          InstallerUrl = $JsonTextReader.Value
-        }
-        break
-      }
-    }
-  }
+  )
+  Remove-Item -Path $InstallerFile -Recurse -Force -ErrorAction 'Continue' -ProgressAction 'SilentlyContinue'
 }
 
-$JsonTextReader.Close()
-$StreamReader.Close()
-
-if ($Mode -ne 3) {
-  throw "The package ${PackageId} with architecture ${PackageArch} was not found"
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = Get-RedirectedUrl -Uri 'https://aka.ms/vc14/vc_redist.x86.exe'
 }
+
+$Object1 = Invoke-WebRequest -Uri $this.CurrentState.Installer[0].InstallerUrl -Method Head
+$ETag = $Object1.Headers.ETag[0]
+
+# Case 0: Force submit the manifest
+if ($Global:DumplingsPreference.Contains('Force')) {
+  $this.Log('Skip checking states', 'Info')
+
+  # ETag
+  $this.CurrentState.ETag = @($ETag)
+
+  Read-Installer
+
+  $this.Print()
+  $this.Write()
+  $this.Message()
+  $this.Submit()
+  return
+}
+
+# Case 1: The task is new
+if ($this.Status.Contains('New')) {
+  $this.Log('New task', 'Info')
+
+  # ETag
+  $this.CurrentState.ETag = @($ETag)
+
+  Read-Installer
+
+  $this.Print()
+  $this.Write()
+  return
+}
+
+# Case 2: The ETag is unchanged
+if ($ETag -in $this.LastState.ETag) {
+  $this.Log("The version $($this.LastState.Version) from the last state is the latest (Global)", 'Info')
+  return
+}
+
+Read-Installer
+
+# Case 3: The current state has an invalid version
+if ([string]::IsNullOrWhiteSpace($this.CurrentState.Version)) {
+  throw 'The current state has an invalid version'
+}
+
+# Case 4: The ETag has changed, but the SHA256 is not
+if ($this.CurrentState.Installer[0].InstallerSha256 -eq $this.LastState.Installer[0].InstallerSha256) {
+  $this.Log('The ETag has changed, but the SHA256 is not', 'Info')
+
+  # ETag
+  $this.CurrentState.ETag = $this.LastState.ETag + $ETag
+
+  $this.Write()
+  return
+}
+
+# ETag
+$this.CurrentState.ETag = @($ETag)
 
 switch -Regex ($this.Check()) {
-  'New|Changed|Updated' {
-    $this.InstallerFiles[$this.CurrentState.Installer[0].InstallerUrl] = $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
-    # RealVersion
-    $this.CurrentState.RealVersion = $InstallerFile | Read-ProductVersionFromExe
-
+  # Case 6: The ETag, the SHA256 and the version have changed
+  'Updated|Rollbacked' {
     $this.Print()
     $this.Write()
-  }
-  'Changed|Updated' {
     $this.Message()
+    $this.Submit()
   }
-  'Updated' {
+  # Case 5: The ETag and the SHA256 have changed, but the version is not
+  default {
+    $this.Log('The ETag and the SHA256 have changed, but the version is not', 'Info')
+    $this.Config.IgnorePRCheck = $true
+    $this.Print()
+    $this.Write()
+    $this.Message()
     $this.Submit()
   }
 }
