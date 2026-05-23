58945b1fac24da963d7d6bd773bae1717d374988
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-05-23 17:18:39 +0800

    Add ErlichLiu.Proma

---
+++ Tasks/ErlichLiu.Proma/Script.ps1
@@ -0,0 +1,69 @@
+$Object1 = Invoke-RestMethod -Uri 'https://api.proma.cool/api/v1/releases/latest'
+
+# Version
+$this.CurrentState.Version = $Object1.release.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = $Object1.release.platforms.'win-x64'.downloadUrl
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.release.releaseDate.ToUniversalTime()
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    try {
+      # ReleaseNotesUrl (zh-CN)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'zh-CN'
+        Key    = 'ReleaseNotesUrl'
+        Value  = 'https://github.com/ErlichLiu/Proma/releases'
+      }
+
+      $Object2 = Invoke-GitHubApi -Uri "https://api.github.com/repos/ErlichLiu/Proma/releases/tags/v$($this.CurrentState.Version)"
+
+      if (-not [string]::IsNullOrWhiteSpace($Object2.body)) {
+        $ReleaseNotesObject = $Object2.body | Convert-MarkdownToHtml -Extensions 'advanced', 'emojis', 'hardlinebreak'
+        $ReleaseNotesNodes = for ($Node = $ReleaseNotesObject.ChildNodes[0]; $Node -and -not ($Node.Name -eq 'h2' -and $Node.InnerText.Contains('下载')); $Node = $Node.NextSibling) { $Node }
+        if ($ReleaseNotesNodes) {
+          # ReleaseNotes (zh-CN)
+          $this.CurrentState.Locale += [ordered]@{
+            Locale = 'zh-CN'
+            Key    = 'ReleaseNotes'
+            Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
+          }
+        } else {
+          $this.Log("No ReleaseNotes (zh-CN) for version $($this.CurrentState.Version)", 'Warning')
+        }
+      } else {
+        $this.Log("No ReleaseNotes (zh-CN) for version $($this.CurrentState.Version)", 'Warning')
+      }
+
+      # ReleaseNotesUrl (zh-CN)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'zh-CN'
+        Key    = 'ReleaseNotesUrl'
+        Value  = "https://github.com/ErlichLiu/Proma/releases/tag/v$($this.CurrentState.Version)"
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
