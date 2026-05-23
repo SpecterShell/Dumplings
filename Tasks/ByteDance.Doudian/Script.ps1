4ed6f9cbb06414623c133e0a66e04c15f5ca11ff
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-05-23 19:33:00 +0800

    Add ByteDance.Doudian

---
+++ Tasks/ByteDance.Doudian/Script.ps1
@@ -0,0 +1,35 @@
+$Object1 = Invoke-RestMethod -Uri "https://api.toutiaoapi.com/service/settings/v3/?aid=430651&device_platform=pc&from_version=$($this.Status.Contains('New') ? '1.1.7' : $this.CurrentState.Version)"
+
+# Version
+$this.CurrentState.Version = $Object1.data.settings.doudian_pc_update.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = $Object1.data.settings.doudian_pc_update.url
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseNotes (zh-CN)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'zh-CN'
+        Key    = 'ReleaseNotes'
+        Value  = $Object1.data.settings.doudian_pc_update.notes
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
