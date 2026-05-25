99e15450f4353b9094007a01259530d9b31818f5
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-05-25 11:21:17 +0800

    ESEngine.Reasonix: Fix

--- Tasks/ESEngine.Reasonix/Script.ps1
+++ Tasks/ESEngine.Reasonix/Script.ps1
@@ -66,21 +66,7 @@ switch -Regex ($this.Check()) {
     $this.Message()
   }
   'Updated' {
-    $ToSubmit = $false
-
-    $Mutex = [System.Threading.Mutex]::new($false, 'DumplingsSubmitLockAnthropicClaudeCode')
-    $Mutex.WaitOne(30000) | Out-Null
-    if (-not $Global:DumplingsStorage.Contains("AnthropicClaudeCode-$($this.CurrentState.Version)-ToSubmit")) {
-      $Global:DumplingsStorage["AnthropicClaudeCode-$($this.CurrentState.Version)-ToSubmit"] = $ToSubmit = $true
-    }
-    $Mutex.ReleaseMutex()
-    $Mutex.Dispose()
-
-    if ($ToSubmit) {
-      $this.Submit()
-    } else {
-      $this.Log('Another task is submitting manifests for this package', 'Warning')
-    }
+    $this.Submit()
   }
 }
 
