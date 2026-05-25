b67f7244d70db7b37f201704f03446bb65ce732e
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-05-25 23:34:00 +0800

    Add MoonshotAI.KimiCodeCLI

---
+++ Tasks/MoonshotAI.KimiCodeCLI/Script.ps1
@@ -0,0 +1,32 @@
+$Prefix = 'https://code.kimi.com/kimi-code/'
+
+$Object1 = Invoke-WebRequest -Uri "${Prefix}latest" | Read-ResponseContent
+
+# Version
+$this.CurrentState.Version = $Object1.Trim()
+
+$Prefix += $this.CurrentState.Version + '/'
+$Object2 = Invoke-RestMethod -Uri "${Prefix}manifest.json"
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = Join-Uri $Prefix $Object2.platforms.'win32-x64'.filename
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'arm64'
+  InstallerUrl = Join-Uri $Prefix $Object2.platforms.'win32-arm64'.filename
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
