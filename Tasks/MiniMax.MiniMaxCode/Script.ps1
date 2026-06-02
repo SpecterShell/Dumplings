4347e24ab84ca7ac7344670a13e8e4cf3b972134
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-06-02 11:14:16 +0800

    MiniMax.MiniMaxCode: Add installers

--- Tasks/MiniMax.MiniMaxCode/Script.ps1
+++ Tasks/MiniMax.MiniMaxCode/Script.ps1
@@ -1,13 +1,25 @@
-$Prefix = 'https://filecdn.minimax.chat/public/minimax-agent-prod/release/'
+$Prefix1 = 'https://file.cdn.minimax.io/public/minimax-agent-prod/release/'
+$Object1 = Invoke-RestMethod -Uri "${Prefix1}latest.yml" | ConvertFrom-Yaml
 
-$Object1 = Invoke-RestMethod -Uri "${Prefix}latest.yml" | ConvertFrom-Yaml
+$Prefix2 = 'https://filecdn.minimax.chat/public/minimax-agent-prod/release/'
+$Object2 = Invoke-RestMethod -Uri "${Prefix2}latest.yml" | ConvertFrom-Yaml
+
+if ($Object1.version -ne $Object2.version) {
+  $this.Log("x64 version: $($Object1.version)")
+  $this.Log("arm64 version: $($Object2.version)")
+  throw 'Inconsistent versions detected'
+}
 
 # Version
 $this.CurrentState.Version = $Object1.version
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = Join-Uri $Prefix $Object1.files[0].url
+  InstallerUrl = Join-Uri $Prefix1 $Object1.files[0].url
+}
+$this.CurrentState.Installer += [ordered]@{
+  InstallerLocale = 'zh-CN'
+  InstallerUrl    = Join-Uri $Prefix2 $Object1.files[0].url
 }
 
 switch -Regex ($this.Check()) {
@@ -15,6 +27,40 @@ switch -Regex ($this.Check()) {
     try {
       # ReleaseTime
       $this.CurrentState.ReleaseTime = $Object1.releaseDate | Get-Date -AsUTC
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    try {
+      # ReleaseNotesUrl (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotesUrl'
+        Value  = $ReleaseNotesUrl = 'https://agent.minimax.io/docs/changelog'
+      }
+
+      $Object2 = Invoke-WebRequest -Uri $ReleaseNotesUrl | ConvertFrom-Html
+
+      $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//h2[contains(., '$($this.CurrentState.Version)')]")
+      if ($ReleaseNotesTitleNode) {
+        $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node -and $Node.Name -ne 'h2'; $Node = $Node.NextSibling) { $Node }
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
+        }
+
+        # ReleaseNotesUrl (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotesUrl'
+          Value  = $ReleaseNotesUrl + '#' + $ReleaseNotesTitleNode.Attributes['id'].Value
+        }
+      } else {
+        $this.Log("No ReleaseTime and ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
     } catch {
       $_ | Out-Host
       $this.Log($_, 'Warning')
