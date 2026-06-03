5f8f58bbe83fd6e5200d4c9aa4f95f6dd731d7e7
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-06-03 23:26:43 +0800

    MoonshotAI.Kimi: Update

--- Tasks/MoonshotAI.Kimi/Script.ps1
+++ Tasks/MoonshotAI.Kimi/Script.ps1
@@ -1,30 +1,20 @@
-$Object1 = Invoke-RestMethod -Uri 'https://appsupport.moonshot.cn/api/startup' -Method Post -Headers @{
-  'x-msh-platform' = 'windows'
-  'x-msh-version'  = $this.Status.Contains('New') ? '1.1.9' : $this.LastState.Version
-} -Body '{}' -ContentType 'application/json'
+$Prefix = 'https://kimi-img.moonshot.cn/app/upgrade/'
 
-if ($Object1.upgrade.is_latest_version) {
-  $this.Log("The version $($this.LastState.Version) from the last state is the latest, skip checking", 'Info')
-  return
-}
+$Object1 = Invoke-RestMethod -Uri "${Prefix}latest.yml" | ConvertFrom-Yaml
 
 # Version
-$this.CurrentState.Version = $Object1.upgrade.version
+$this.CurrentState.Version = $Object1.version
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = $Object1.upgrade.apk_url
+  InstallerUrl = Join-Uri $Prefix $Object1.files[0].url
 }
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
     try {
-      # ReleaseNotes (zh-CN)
-      $this.CurrentState.Locale += [ordered]@{
-        Locale = 'zh-CN'
-        Key    = 'ReleaseNotes'
-        Value  = $Object1.upgrade.upgrade_dialog.content | Format-Text
-      }
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.releaseDate | Get-Date -AsUTC
     } catch {
       $_ | Out-Host
       $this.Log($_, 'Warning')
