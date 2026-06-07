5c220c953500a5c74f01644f6657865302485492
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-06-07 09:55:46 +0800

    Cisco.Webex: Add arm64 installer

--- Tasks/Cisco.Webex/Script.ps1
+++ Tasks/Cisco.Webex/Script.ps1
@@ -1,5 +1,12 @@
 # x64
 $Object1 = Invoke-RestMethod -Uri 'https://client-upgrade-a.wbx2.com/client-upgrade/api/v1/webexteamsdesktop/upgrade/@me?channel=gold&model=win-64&r=AC805C72-1377-4FE6-B272-1FB4417B50B7'
+# arm64
+$Object2 = Invoke-RestMethod -Uri 'https://client-upgrade-a.wbx2.com/client-upgrade/api/v1/webexteamsdesktop/upgrade/@me?channel=gold&model=win-arm-64&r=AC805C72-1377-4FE6-B272-1FB4417B50B7'
+
+if ($Object1.manifest.version -ne $Object2.manifest.version) {
+  $this.Log("Inconsistent versions: x64: $($Object1.manifest.version), arm64: $($Object2.manifest.version)", 'Error')
+  return
+}
 
 # Version
 $this.CurrentState.Version = $Object1.manifest.version
@@ -8,6 +15,10 @@ $this.CurrentState.Version = $Object1.manifest.version
 $this.CurrentState.Installer += [ordered]@{
   Architecture = 'x64'
   InstallerUrl = $Object1.manifest.manualInstallPackageLocation
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'arm64'
+  InstallerUrl = $Object2.manifest.manualInstallPackageLocation
 }
 
 switch -Regex ($this.Check()) {
