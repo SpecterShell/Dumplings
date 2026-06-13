545a0e35a222e9df943df03fe363c513522dd10f
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-06-13 20:11:58 +0800

    uu201.CharacterArc: Fix the version prefix string to remove

--- Tasks/uu201.CharacterArc/Script.ps1
+++ Tasks/uu201.CharacterArc/Script.ps1
@@ -1,7 +1,7 @@
 $Object1 = Invoke-GitHubApi -Uri 'https://api.github.com/repos/uu201/character-arc/releases/latest'
 
 # Version
-$this.CurrentState.Version = $Object1.tag_name -replace '^v'
+$this.CurrentState.Version = $Object1.tag_name -replace '^v' -replace '^\.'
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
