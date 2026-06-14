90428f29ca7657a09af22456cd14e280f1a91b3e
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-06-15 00:10:18 +0800

    Add OrbForge.Orb.EXE

---
+++ Tasks/OrbForge.Orb/Script.ps1
@@ -0,0 +1,42 @@
+$Object1 = Invoke-RestMethod -Uri 'https://pkgs.orb.net/stable/windows/orb.appinstaller'
+
+# Version
+$this.CurrentState.Version = $Object1.AppInstaller.MainPackage.Version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerType = 'appx'
+  InstallerUrl  = $Object1.AppInstaller.MainPackage.Uri
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      $Object2 = Invoke-WebRequest -Uri 'https://orb.net/the-forge/changelog' | ConvertFrom-Html
+
+      $ReleaseNotesNode = $Object2.SelectSingleNode("//div[contains(@class, 'changelog-item') and contains(.//div[contains(@class, 'version-col')], '$($this.CurrentState.Version.Split('.')[0..2] -join '.')')]")
+      if ($ReleaseNotesNode) {
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesNode.SelectSingleNode('.//div[contains(@class, "release-notes")]') | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
