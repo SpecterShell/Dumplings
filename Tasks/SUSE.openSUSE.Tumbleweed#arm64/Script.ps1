553682ebb34a3052e04a3d2a327d71c15b47e0e9
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-06-18 14:12:04 +0800

    SUSE.openSUSE.Tumbleweed: Split into two tasks by architecture

---
+++ Tasks/SUSE.openSUSE.Tumbleweed#arm64/Script.ps1
@@ -0,0 +1,28 @@
+$Object1 = $Global:DumplingsStorage.WSLDistributions.distributions.Where({ $_.PackageFamilyName -eq '46932SUSE.openSUSETumbleweed_022rs5jcyhyac' }, 'First')[0]
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Query        = [ordered]@{}
+  Architecture = 'arm64'
+  InstallerUrl = $Object1.Arm64PackageUrl
+}
+
+# Version
+$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d{6})').Groups[1].Value
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.InstallerFiles[$this.CurrentState.Installer[0].InstallerUrl] = $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+    # RealVersion
+    $this.CurrentState.RealVersion = $InstallerFile | Read-ProductVersionFromMSIX
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
---
+++ Tasks/SUSE.openSUSE.Tumbleweed#x64/Script.ps1
@@ -0,0 +1,28 @@
+$Object1 = $Global:DumplingsStorage.WSLDistributions.distributions.Where({ $_.PackageFamilyName -eq '46932SUSE.openSUSETumbleweed_022rs5jcyhyac' }, 'First')[0]
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Query        = [ordered]@{}
+  Architecture = 'x64'
+  InstallerUrl = $Object1.Amd64PackageUrl
+}
+
+# Version
+$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d{6})').Groups[1].Value
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.InstallerFiles[$this.CurrentState.Installer[0].InstallerUrl] = $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+    # RealVersion
+    $this.CurrentState.RealVersion = $InstallerFile | Read-ProductVersionFromMSIX
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
