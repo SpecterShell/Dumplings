ed788fe80562fc8cebc3ddb5e7ee6a66cd3c42a4
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-06-18 22:25:06 +0800

    MicroDicom.MicroDicom: Update

--- Tasks/MicroDicom.DICOMViewer/Script.ps1
+++ Tasks/MicroDicom.DICOMViewer/Script.ps1
@@ -3,25 +3,11 @@ $Object1 = Invoke-WebRequest -Uri $Prefix
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  Architecture = 'x86'
-  InstallerUrl = $InstallerUrlX86 = Join-Uri $Prefix $Object1.Links.Where({ try { $_.href.EndsWith('.exe') -and -not $_.href.Contains('ShellExtension') -and $_.href.Contains('win32') } catch {} }, 'First')[0].href
-}
-$VersionX86 = [regex]::Match($InstallerUrlX86, '(\d+(?:\.\d+)+)').Groups[1].Value
-
-$this.CurrentState.Installer += [ordered]@{
-  Architecture = 'x64'
-  InstallerUrl = $InstallerUrlX64 = Join-Uri $Prefix $Object1.Links.Where({ try { $_.href.EndsWith('.exe') -and -not $_.href.Contains('ShellExtension') -and $_.href.Contains('x64') } catch {} }, 'First')[0].href
-}
-$VersionX64 = [regex]::Match($InstallerUrlX64, '(\d+(?:\.\d+)+)').Groups[1].Value
-
-if ($VersionX86 -ne $VersionX64) {
-  $this.Log("x86 version: ${VersionX86}")
-  $this.Log("x64 version: ${VersionX64}")
-  throw 'Inconsistent versions detected'
+  InstallerUrl = Join-Uri $Prefix $Object1.Links.Where({ try { $_.href.EndsWith('.exe') -and $_.href -notmatch 'ShellExtension' -and $_.href -match 'Setup' } catch {} }, 'First')[0].href
 }
 
 # Version
-$this.CurrentState.Version = $VersionX64
+$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
