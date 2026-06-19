207e422a5089d602fc3196476805fa4668d1da90
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-06-19 12:29:46 +0800

    SonicWall: Create mock array for obtaining data from webpage

--- Tasks/#SonicWall/Script.ps1
+++ Tasks/#SonicWall/Script.ps1
@@ -1,4 +1,6 @@
 $EdgeDriver = Get-EdgeDriver -Headless
 $EdgeDriver.Navigate().GoToUrl('https://www.sonicwall.com/products/remote-access/vpn-clients')
-
-$Global:DumplingsStorage.SonicWallApps = $EdgeDriver.ExecuteScript('return window.__next_f', $null)
+$Script = $EdgeDriver.FindElement([OpenQA.Selenium.By]::XPath('//script[contains(., "NetExtender for Windows")]')).GetAttribute('textContent')
+$null = $EdgeDriver.ExecuteScript('self.__next_f = [];', $null)
+$null = $EdgeDriver.ExecuteScript($Script, $null)
+$Global:DumplingsStorage.SonicWallApps = $EdgeDriver.ExecuteScript('return self.__next_f', $null)
