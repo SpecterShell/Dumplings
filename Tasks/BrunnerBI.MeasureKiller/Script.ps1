03e2bcebeadb39a47f8875dcc50ae61a5b93d598
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-06-24 17:32:20 +0800

    BrunnerBI.MeasureKiller: Update

--- Tasks/BrunnerBI.MeasureKiller/Script.ps1
+++ Tasks/BrunnerBI.MeasureKiller/Script.ps1
@@ -22,69 +22,25 @@ $this.CurrentState.Installer += [ordered]@{
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
     try {
-      $Object2 = [System.IO.StringReader]::new(((Invoke-WebRequest -Uri 'https://en.brunner.bi/measurekiller' | ConvertFrom-Html).SelectSingleNode('//main') | Get-TextContent))
+      $Object2 = Invoke-WebRequest -Uri 'https://measurekiller.com/download' | ConvertFrom-Html
 
-      while ($Object2.Peek() -ne -1) {
-        $String = $Object2.ReadLine()
-        if ($String -match 'Release notes') {
-          if ($String -match '(\d{1,2}\W+\d{1,2}\W+20\d{2})') {
-            # ReleaseTime
-            $this.CurrentState.ReleaseTime = [datetime]::ParseExact($Matches[1], 'MM/dd/yyyy', $null).ToString('yyyy-MM-dd')
-          } else {
-            $this.Log("No ReleaseTime for version $($this.CurrentState.Version)", 'Warning')
-          }
-          break
-        }
-      }
-      while ($Object2.Peek() -ne -1) {
-        $String = $Object2.ReadLine()
-        if ($String -match "Version $([regex]::Escape($this.CurrentState.Version))") {
-          break
-        }
-      }
-      if ($Object2.Peek() -ne -1) {
-        $ReleaseNotesObjects = [System.Collections.Generic.List[string]]::new()
-        while ($Object2.Peek() -ne -1) {
-          $String = $Object2.ReadLine()
-          $ReleaseNotesObjects.Add($String)
-        }
+      $ReleaseNotesNode = $Object2.SelectSingleNode("//article[contains(.//summary, '$($this.CurrentState.Version)')]")
+      if ($ReleaseNotesNode) {
+        # ReleaseTime
+        $this.CurrentState.ReleaseTime = [regex]::Match($ReleaseNotesNode.SelectSingleNode('.//summary').InnerText, '([a-zA-Z]+\W+\d{1,2}\W+20\d{2})').Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
+
+        # Remove list prefix
+        $ReleaseNotesNode.SelectNodes('.//*[text()="·"]').ForEach({ $_.Remove() })
+        # Remove "View on GitHub"
+        $ReleaseNotesNode.SelectNodes('.//a[contains(., "View on GitHub")]').ForEach({ $_.Remove() })
         # ReleaseNotes (en-US)
         $this.CurrentState.Locale += [ordered]@{
           Locale = 'en-US'
           Key    = 'ReleaseNotes'
-          Value  = $ReleaseNotesObjects | Format-Text
+          Value  = $ReleaseNotesNode.SelectNodes('.//summary/following-sibling::node()') | Get-TextContent | Format-Text
         }
       } else {
         $this.Log("No ReleaseTime and ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
-      }
-
-      $Object2.Close()
-    } catch {
-      $_ | Out-Host
-      $this.Log($_, 'Warning')
-    }
-
-    try {
-      $Object2 = Invoke-WebRequest -Uri 'https://en.brunner.bi/measurekiller' | ConvertFrom-Html
-      $ReleaseTimeNode = $Object2.SelectSingleNode("//p[contains(., 'Release notes') and ./following-sibling::p[contains(., 'Version $($this.CurrentState.Version)')]]")
-      if ($ReleaseTimeNode) {
-        # ReleaseTime
-        $this.CurrentState.ReleaseTime = [datetime]::ParseExact(
-          [regex]::Match($ReleaseTimeNode.InnerText, '(\d{1,2}\W+\d{1,2}\W+20\d{2})').Groups[1].Value,
-          'MM/dd/yyyy',
-          $null
-        ).ToString('yyyy-MM-dd')
-
-        $ReleaseNotesTitleNode = $ReleaseTimeNode.SelectSingleNode("./following-sibling::p[contains(., 'Version $($this.CurrentState.Version)')]")
-        $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node -and -not $Node.InnerText.StartsWith('Version '); $Node = $Node.NextSibling) { $Node }
-        # ReleaseNotes (en-US)
-        $this.CurrentState.Locale += [ordered]@{
-          Locale = 'en-US'
-          Key    = 'ReleaseNotes'
-          Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
-        }
-      } else {
-        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
       }
     } catch {
       $_ | Out-Host
