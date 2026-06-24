ea510520dadf881d74a179a113f539c3190d5405
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-06-24 17:48:00 +0800

    SplitmediaLabs.XSplitBroadcaster: Switch to MSI installer

--- Tasks/SplitmediaLabs.XSplitBroadcaster/Script.ps1
+++ Tasks/SplitmediaLabs.XSplitBroadcaster/Script.ps1
@@ -6,26 +6,12 @@ $this.CurrentState.Version = $Object1.Update.ProductVersion
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = $Object1.Update.URL
+  InstallerType = 'msi'
+  InstallerUrl  = $Object1.Update.URL -replace '\.exe$', '.msi'
 }
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
-    $this.InstallerFiles[$this.CurrentState.Installer[0].InstallerUrl] = $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl | Rename-Item -NewName { "${_}.exe" } -PassThru | Select-Object -ExpandProperty 'FullName'
-    $InstallerFileExtracted = New-TempFolder
-    Expand-AdvancedInstaller -Path $InstallerFile -DestinationPath $InstallerFileExtracted | Out-Null
-    $InstallerFile2 = Get-ChildItem -Path $InstallerFileExtracted -Include '*.msi' -Recurse | Select-Object -First 1
-    # ProductCode
-    $this.CurrentState.Installer[0]['ProductCode'] = $InstallerFile2 | Read-ProductCodeFromMsi
-    # AppsAndFeaturesEntries
-    $this.CurrentState.Installer[0]['AppsAndFeaturesEntries'] = @(
-      [ordered]@{
-        UpgradeCode   = $InstallerFile2 | Read-UpgradeCodeFromMsi
-        InstallerType = 'msi'
-      }
-    )
-    Remove-Item -Path $InstallerFileExtracted -Recurse -Force -ErrorAction 'Continue' -ProgressAction 'SilentlyContinue'
-
     $this.Print()
     $this.Write()
   }
