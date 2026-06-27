764b0bb307ec3203866d788145eae0cc1deead0c
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-06-28 00:35:45 +0800

    Meld.Meld: Switch to Gnome GitLab source

--- Tasks/Meld.Meld/Script.ps1
+++ Tasks/Meld.Meld/Script.ps1
@@ -1,23 +1,21 @@
-$Object1 = Invoke-WebRequest -Uri 'https://meldmerge.org/'
+$Object1 = curl -fsSL 'https://gitlab.gnome.org/api/v4/projects/301/releases/permalink/latest' | Join-String -Separator "`n" | ConvertFrom-Json
+
+# Version
+$this.CurrentState.Version = $Object1.tag_name -creplace '^v'
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = $Object1.Links.Where({ try { $_.href.EndsWith('.msi') } catch {} }, 'First')[0].href
+  InstallerUrl = $Object1.assets.links.Where({ $_.name -match 'Windows installer' }, 'First')[0].url | ConvertTo-UnescapedUri
 }
-
-# Version
-$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d+(?:\.\d+){2})').Groups[1].Value
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
     try {
-      $Object2 = Invoke-RestMethod -Uri "https://gitlab.gnome.org/api/v4/projects/301/releases/$($this.CurrentState.Version)"
-
       # ReleaseTime
-      $this.CurrentState.ReleaseTime = $Object2.released_at.ToUniversalTime()
+      $this.CurrentState.ReleaseTime = $Object1.released_at.ToUniversalTime()
 
-      if (-not [string]::IsNullOrWhiteSpace($Object2.description)) {
-        $ReleaseNotesObject = $Object2.description | Convert-MarkdownToHtml -Extensions 'advanced', 'emojis', 'hardlinebreak'
+      if (-not [string]::IsNullOrWhiteSpace($Object1.description)) {
+        $ReleaseNotesObject = $Object1.description | Convert-MarkdownToHtml -Extensions 'advanced', 'emojis', 'hardlinebreak'
         $ReleaseNotesTitleNode = $ReleaseNotesObject.SelectSingleNode("/h1[contains(., '$($this.CurrentState.Version)')]")
         if ($ReleaseNotesTitleNode) {
           # ReleaseNotes (en-US)
@@ -33,10 +31,11 @@ switch -Regex ($this.Check()) {
         $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
       }
 
-      # ReleaseNotesUrl
+      # ReleaseNotesUrl (en-US)
       $this.CurrentState.Locale += [ordered]@{
-        Key   = 'ReleaseNotesUrl'
-        Value = $Object2._links.self
+        Locale = 'en-US'
+        Key    = 'ReleaseNotesUrl'
+        Value  = $Object1._links.self
       }
     } catch {
       $_ | Out-Host
