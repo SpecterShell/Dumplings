ee37115839eaaa28f89b40e311530dab7145dcb1
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-06-28 16:57:53 +0800

    YouXiao.YXFile: Update

--- Tasks/YouXiao.YXFile/Script.ps1
+++ Tasks/YouXiao.YXFile/Script.ps1
@@ -1,71 +1,22 @@
-function Read-Installer {
-  $this.InstallerFiles[$this.CurrentState.Installer[0].InstallerUrl] = $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
-  # Version
-  $this.CurrentState.Version = $InstallerFile | Read-ProductVersionFromExe
-}
+$Object1 = Invoke-RestMethod -Uri 'https://www.yxfile.com.cn/data/downloads.json'
 
-$Object1 = Invoke-RestMethod -Uri 'https://www.yxfile.com.cn/js/bottom.js'
+# Version
+$this.CurrentState.Version = $Object1.platforms.win.version
+
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = [regex]::Match($Object1, 'downloadWindowsBakUrl\s*=\s*"([^"]+)"').Groups[1].Value
-}
-
-$Object2 = Invoke-WebRequest -Uri $this.CurrentState.Installer[0].InstallerUrl -Method Head
-# Hash
-$this.CurrentState.Hash = $Object2.Headers.'Content-MD5'[0]
-
-# Case 0: Force submit the manifest
-if ($Global:DumplingsPreference.Contains('Force')) {
-  $this.Log('Skip checking states', 'Info')
-
-  Read-Installer
-
-  $this.Print()
-  $this.Write()
-  $this.Message()
-  $this.Submit()
-  return
-}
-
-# Case 1: The task is new
-if ($this.Status.Contains('New')) {
-  $this.Log('New task', 'Info')
-
-  Read-Installer
-
-  $this.Print()
-  $this.Write()
-  return
-}
-
-# Case 2: The hash is unchanged
-if ($this.CurrentState.Hash -eq $this.LastState.Hash) {
-  $this.Log("The version $($this.LastState.Version) from the last state is the latest", 'Info')
-  return
-}
-
-Read-Installer
-
-# Case 3: The current state has an invalid version
-if ([string]::IsNullOrWhiteSpace($this.CurrentState.Version)) {
-  throw 'The current state has an invalid version'
+  InstallerUrl = $Object1.platforms.win.downloadUrl
 }
 
 switch -Regex ($this.Check()) {
-  # Case 5: The hash and the version have changed
-  'Updated|Rollbacked' {
+  'New|Changed|Updated' {
     $this.Print()
     $this.Write()
-    $this.Message()
-    $this.Submit()
   }
-  # Case 4: The hash has changed, but the version is not
-  Default {
-    $this.Log('The hash has changed, but the version is not', 'Info')
-    $this.Config.IgnorePRCheck = $true
-    $this.Print()
-    $this.Write()
+  'Changed|Updated' {
     $this.Message()
+  }
+  'Updated' {
     $this.Submit()
   }
 }
