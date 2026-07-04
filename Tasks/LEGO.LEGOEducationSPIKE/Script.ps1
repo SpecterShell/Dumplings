4659d6b993f84fc3e96620cc0e9bc7004c3c06a1
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-07-04 20:03:52 +0800

    LEGO.LEGOEducationSPIKE: Remove zh-CN installer

--- Tasks/LEGO.LEGOEducationSPIKE/Script.ps1
+++ Tasks/LEGO.LEGOEducationSPIKE/Script.ps1
@@ -4,15 +4,15 @@ $Object2 = $Object1.result.pageContext.page.spots.Where({ $_.__dataType -eq 'dow
 $Version1 = $Object2.version
 
 # China
-$Object3 = Invoke-RestMethod -Uri 'https://legoeducation.cn/page-data/zh-cn/downloads/spike-app/software/page-data.json'
-$Object4 = $Object3.result.pageContext.page.spots.Where({ $_.__dataType -eq 'downloadSection' }, 'First')[0].items.Where({ $_.software.device -eq 'Windows 10' }, 'First')[0].software.language.Where({ $_.__downloadType -eq 'file' }, 'First')[0]
-$Version2 = $Object4.version
+# $Object3 = Invoke-RestMethod -Uri 'https://legoeducation.cn/page-data/zh-cn/downloads/spike-app/software/page-data.json'
+# $Object4 = $Object3.result.pageContext.page.spots.Where({ $_.__dataType -eq 'downloadSection' }, 'First')[0].items.Where({ $_.software.device -eq 'Windows 10' }, 'First')[0].software.language.Where({ $_.__downloadType -eq 'file' }, 'First')[0]
+# $Version2 = $Object4.version
 
-if ($Version1 -ne $Version2) {
-  $this.Log("Global version: ${Version1}")
-  $this.Log("China version: ${Version2}")
-  throw 'Inconsistent versions detected'
-}
+# if ($Version1 -ne $Version2) {
+#   $this.Log("Global version: ${Version1}")
+#   $this.Log("China version: ${Version2}")
+#   throw 'Inconsistent versions detected'
+# }
 
 # Version
 $this.CurrentState.Version = $Version1
@@ -21,18 +21,19 @@ $this.CurrentState.Version = $Version1
 $this.CurrentState.Installer += [ordered]@{
   InstallerUrl = $Object2.url
 }
-$this.CurrentState.Installer += [ordered]@{
-  InstallerLocale = 'zh-CN'
-  InstallerUrl    = Join-Uri 'https://legoeducation.cn/' $Object4.url
-}
+# $this.CurrentState.Installer += [ordered]@{
+#   InstallerLocale = 'zh-CN'
+#   InstallerUrl    = Join-Uri 'https://legoeducation.cn/' $Object4.url
+# }
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
     try {
-      # ReleaseNotesUrl
+      # ReleaseNotesUrl (en-US)
       $this.CurrentState.Locale += [ordered]@{
-        Key   = 'ReleaseNotesUrl'
-        Value = ($Object1.result.pageContext.page.spots.Where({ $_.__dataType -eq 'downloadSection' }, 'First')[0].appReleaseNotes | ConvertFrom-Markdown).Html | Get-EmbeddedLinks | Where-Object -FilterScript { $_.outerHTML.Contains($this.CurrentState.Version) } | Select-Object -First 1 -ExpandProperty href
+        Locale = 'en-US'
+        Key    = 'ReleaseNotesUrl'
+        Value  = ($Object1.result.pageContext.page.spots.Where({ $_.__dataType -eq 'downloadSection' }, 'First')[0].appReleaseNotes | ConvertFrom-Markdown).Html | Get-EmbeddedLinks | Where-Object -FilterScript { $_.outerHTML.Contains($this.CurrentState.Version) } | Select-Object -First 1 -ExpandProperty href
       }
     } catch {
       $_ | Out-Host
