b5adc43141ed8fbf0a1b0addf1e796980e9317fc
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-07-04 19:56:30 +0800

    SonicWall: Add more SonicWall installer URL information to prefetch

--- Tasks/#SonicWall/Script.ps1
+++ Tasks/#SonicWall/Script.ps1
@@ -1,6 +1,7 @@
 $EdgeDriver = Get-EdgeDriver -Headless
 $EdgeDriver.Navigate().GoToUrl('https://www.sonicwall.com/products/remote-access/vpn-clients')
-$Script = $EdgeDriver.FindElement([OpenQA.Selenium.By]::XPath('//script[contains(., "NetExtender for Windows")]')).GetAttribute('textContent')
 $null = $EdgeDriver.ExecuteScript('self.__next_f = [];', $null)
-$null = $EdgeDriver.ExecuteScript($Script, $null)
+$null = $EdgeDriver.ExecuteScript($EdgeDriver.FindElement([OpenQA.Selenium.By]::XPath('//script[contains(., "ConnectTunnel")]')).GetAttribute('textContent'), $null)
+$null = $EdgeDriver.ExecuteScript($EdgeDriver.FindElement([OpenQA.Selenium.By]::XPath('//script[contains(., "GVCSetup")]')).GetAttribute('textContent'), $null)
+$null = $EdgeDriver.ExecuteScript($EdgeDriver.FindElement([OpenQA.Selenium.By]::XPath('//script[contains(., "NetExtender for Windows")]')).GetAttribute('textContent'), $null)
 $Global:DumplingsStorage.SonicWallApps = $EdgeDriver.ExecuteScript('return self.__next_f', $null)
