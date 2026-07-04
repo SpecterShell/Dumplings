46636e6ad764f2f412775790bf588886378a3836
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-07-04 19:09:54 +0800

    Citrix.Workspace: Add x64 installer

--- Tasks/Citrix.Workspace/Script.ps1
+++ Tasks/Citrix.Workspace/Script.ps1
@@ -1,13 +1,23 @@
 $Prefix = 'https://downloadplugins.citrix.com/ReceiverUpdates/Prod'
-
 $Object1 = (Invoke-RestMethod -Uri "${Prefix}/catalog_win3.xml").Catalog.CatalogVersion.Installers.Where({ $_.name -eq '1CDF566D-B2C7-47CA-802F-6283C862E1D6' -or $_.name -eq 'WorkspaceApp' }, 'First')[0].Installer.Where({ $_.Stream -eq 'Current' }) | Sort-Object -Property { $_.Version -replace '\d+', { $_.Value.PadLeft(20) } } -Bottom 1
+$Object2 = (Invoke-RestMethod -Uri "${Prefix}/catalog_win3_x64.xml").Catalog.CatalogVersion.Installers.Where({ $_.name -eq '1CDF566D-B2C7-47CA-802F-6283C862E1D6' -or $_.name -eq 'WorkspaceAppX64' }, 'First')[0].Installer.Where({ $_.Stream -eq 'Current' }) | Sort-Object -Property { $_.Version -replace '\d+', { $_.Value.PadLeft(20) } } -Bottom 1
+
+if ($Object1.Version -ne $Object2.Version) {
+  $this.Log("Inconsistent versions: x86: $($Object1.Version), x64: $($Object2.Version)", 'Error')
+  return
+}
 
 # Version
-$this.CurrentState.Version = $Object1.Version
+$this.CurrentState.Version = $Object2.Version
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x86'
   InstallerUrl = "${Prefix}$($Object1.DownloadURL)"
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = "${Prefix}$($Object2.DownloadURL)"
 }
 
 switch -Regex ($this.Check()) {
