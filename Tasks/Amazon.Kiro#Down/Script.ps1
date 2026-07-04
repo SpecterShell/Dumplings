377e27fb07f48329be4078ccc5d62043fa1e6f08
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-07-04 20:52:16 +0800

    Amazon.Kiro: Add download source

--- Tasks/Amazon.Kiro#Down/Script.ps1
+++ Tasks/Amazon.Kiro#Down/Script.ps1
@@ -1,27 +1,16 @@
-$Object1 = Invoke-RestMethod -Uri 'https://prod.download.desktop.kiro.dev/stable/metadata-win32-x64-user-stable.json'
-
-# Version
-$this.CurrentState.Version = $Object1.currentRelease
-
-$Object2 = $Object1.releases.Where({ $_.updateTo.version -eq $this.CurrentState.Version }, 'First')[0]
+$Object1 = Invoke-WebRequest -Uri 'https://kiro.dev/downloads/'
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
   Architecture = 'x64'
-  Scope        = 'user'
-  InstallerUrl = $Object2.updateTo.url
+  InstallerUrl = $Object1.Links.Where({ try { $_.href.EndsWith('.exe') -and $_.href.Contains('x64') } catch {} }, 'First')[0].href
 }
+
+# Version
+$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
-    try {
-      # ReleaseTime
-      $this.CurrentState.ReleaseTime = $Object2.updateTo.pub_date | Get-Date -Format 'yyyy-MM-dd'
-    } catch {
-      $_ | Out-Host
-      $this.Log($_, 'Warning')
-    }
-
     try {
       # ReleaseNotesUrl (en-US)
       $this.CurrentState.Locale += [ordered]@{
