f04c45a64e1df8ad22ca115f09bdb303bca2af5f
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-07-20 23:27:32 +0800

    DW.Spectrum: Update

--- Tasks/DW.Spectrum.Client/Script.ps1
+++ Tasks/DW.Spectrum.Client/Script.ps1
@@ -27,13 +27,13 @@ switch -Regex ($this.Check()) {
         Value  = $ReleaseNotesUrl = $Object1.releaseNotes
       }
 
-      $Object2 = Invoke-WebRequest -Uri $ReleaseNotesUrl | Read-ResponseContent -Encoding 'windows-1252' | ConvertFrom-Html
+      $Object2 = Invoke-WebRequest -Uri $ReleaseNotesUrl | ConvertFrom-Html
 
       # Replace all Wingdings characters with their Unicode equivalents
       $Object2.SelectNodes('//span[contains(@style, "font-family:Wingdings")]').ForEach({ if ($_.InnerText -match 'l|o') { $_.InnerHtml = '<span>-</span>' } })
       # Remove o:p nodes
       $Object2.SelectNodes('//*[local-name() = "o:p"]').ForEach({ $_.Remove() })
-      $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//div[@class='WordSection1']/*[contains(., 'DW Spectrum Release note for $($this.CurrentState.Version)')]")
+      $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//h1[contains(., 'DW Spectrum Release note for $($this.CurrentState.Version)')]")
       if ($ReleaseNotesTitleNode) {
         # ReleaseNotes (en-US)
         $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node -and $Node.InnerText -notmatch 'DW Spectrum Release note for '; $Node = $Node.NextSibling) {
