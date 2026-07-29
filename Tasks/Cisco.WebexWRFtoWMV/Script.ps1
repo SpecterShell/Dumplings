572ad756bf970b8adabc460a0296b54d2d9bfa45
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-07-29 23:04:09 +0800

    Cisco.WebexWRFtoWMV: Fix

--- Tasks/Cisco.WebexWRFtoWMV/Script.ps1
+++ Tasks/Cisco.WebexWRFtoWMV/Script.ps1
@@ -21,7 +21,7 @@ $this.CurrentState.Installer += [ordered]@{
   InstallerUrl = $Object1.Where({ try { $_.href.EndsWith('.zip') -and $_.href.Contains('atwrf2wmv') } catch {} }, 'First')[0].href
 }
 
-$Object2 = Get-WinGetWinINetResponseHeader -Uri $this.CurrentState.Installer[0].InstallerUrl
+$Object2 = Get-WinGetDeliveryOptimizationResponseHeader -Uri $this.CurrentState.Installer[0].InstallerUrl
 $ETag = $Object2.Headers.ETag[0]
 
 # Case 0: Force submit the manifest
