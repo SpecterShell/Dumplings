b3f903758067ad27a351ca9a150fb82f9d78df84
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-07-29 16:08:38 +0800

    Google.Antigravity: Remove ReleaseTime

--- Tasks/Google.Antigravity/Script.ps1
+++ Tasks/Google.Antigravity/Script.ps1
@@ -17,13 +17,13 @@ $this.CurrentState.Installer += [ordered]@{
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
-    try {
-      # ReleaseTime
-      $this.CurrentState.ReleaseTime = $Object1.releaseDate | Get-Date -AsUTC
-    } catch {
-      $_ | Out-Host
-      $this.Log($_, 'Warning')
-    }
+    # try {
+    #   # ReleaseTime
+    #   $this.CurrentState.ReleaseTime = $Object1.releaseDate | Get-Date -AsUTC
+    # } catch {
+    #   $_ | Out-Host
+    #   $this.Log($_, 'Warning')
+    # }
 
     $this.Print()
     $this.Write()
