4d47c17c98fe5eeb33863135551e5b66585eca9a
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-08-04 21:14:01 +0800

    Add SaturnPCBDesign.SaturnPCBToolkit

---
+++ Tasks/SaturnPCBDesign.SaturnPCBToolkit/Script.ps1
@@ -0,0 +1,43 @@
+$Object1 = Invoke-WebRequest -Uri 'https://saturnpcb.com/thank-you-pcb-toolkit/'
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerType = 'msi'
+  InstallerUrl  = $Object1.Links.Where({ try { $_.href.EndsWith('.msi') -and $_.href -match 'Setup' } catch {} }, 'First')[0].href
+}
+
+# Version
+$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      $Object2 = Invoke-WebRequest -Uri 'https://saturnpcb.com/saturn-pcb-toolkit/' | ConvertFrom-Html
+
+      $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//h3[starts-with(text(), 'Version $($this.CurrentState.Version) ')]")
+      if ($ReleaseNotesTitleNode) {
+        # ReleaseNotes (en-US)
+        $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node -and $Node.Name -ne 'h3'; $Node = $Node.NextSibling) { $Node }
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
