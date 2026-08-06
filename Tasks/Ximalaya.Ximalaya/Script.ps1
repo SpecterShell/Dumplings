f235df28a24ad30590a037e2d35c4b37b308015b
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-08-07 01:18:15 +0800

    Ximalaya.Ximalaya: Switch to the download source

--- Tasks/Ximalaya.Ximalaya/Script.ps1
+++ Tasks/Ximalaya.Ximalaya/Script.ps1
@@ -1,43 +1,15 @@
-$Prefix = 'https://pc.ximalaya.com/ximalaya-pc-updater/api/v1/update/full/'
-$Headers = @{
-  platform     = 'win32'
-  buildversion = '0'
-  version      = '0.0.0'
-  uid          = ''
-}
-
-$Object1 = Invoke-RestMethod -Uri "${Prefix}latest.yml" -Headers $Headers | ConvertFrom-Yaml
-
-if ($Object1.version -eq '0.0.0') {
-  $this.Log('The endpoint returned an invalid response', 'Error')
-  return
-}
-
-# Version
-$this.CurrentState.Version = $Object1.version
+$InstallerUrl = Get-RedirectedUrl1st -Uri 'https://www.ximalaya.com/down/lite/v2?client=win&channelId=99&subChannelId=100001'
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = Get-RedirectedUrl -Uri "${Prefix}$($Object1.files[0].url)" -Headers $Headers
+  InstallerUrl = $InstallerUrl | Split-Uri -LeftPart 'Path'
 }
+
+# Version
+$this.CurrentState.Version = [regex]::Match($InstallerUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
-    try {
-      # ReleaseTime
-      $this.CurrentState.ReleaseTime = $Object1.releaseDate | Get-Date -AsUTC
-
-      # ReleaseNotes (zh-CN)
-      $this.CurrentState.Locale += [ordered]@{
-        Locale = 'zh-CN'
-        Key    = 'ReleaseNotes'
-        Value  = $Object1.releaseNotes | Format-Text
-      }
-    } catch {
-      $_ | Out-Host
-      $this.Log($_, 'Warning')
-    }
-
     $this.Print()
     $this.Write()
   }
