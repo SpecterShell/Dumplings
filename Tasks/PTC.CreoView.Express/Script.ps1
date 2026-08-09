4a9c3d1a1b097e1296e211cc11d21289e361c1d5
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-08-09 23:32:15 +0800

    PTC.CreoView.Express: Update download page URL

--- Tasks/PTC.CreoView.Express/Script.ps1
+++ Tasks/PTC.CreoView.Express/Script.ps1
@@ -1,4 +1,4 @@
-$Object1 = Invoke-WebRequest -Uri 'https://www.ptc.com/en/products/creo/creo-view/extension-express-download' | ConvertFrom-Html
+$Object1 = Invoke-WebRequest -Uri 'https://www.ptc.com/en/products/creo/ptc-product-view/extension-express-download' | ConvertFrom-Html
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
