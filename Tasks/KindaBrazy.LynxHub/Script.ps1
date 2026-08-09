a198613d55aad212bd2c261310dfc9b7739becc9
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-08-10 01:07:44 +0800

    KindaBrazy.LynxHub: Update repo owner. Add en-US locale to ReleaseNotes

--- Tasks/KindaBrazy.LynxHub/Script.ps1
+++ Tasks/KindaBrazy.LynxHub/Script.ps1
@@ -1,7 +1,4 @@
-$RepoOwner = 'KindaBrazy'
-$RepoName = 'LynxHub'
-
-$Object1 = Invoke-GitHubApi -Uri "https://api.github.com/repos/${RepoOwner}/${RepoName}/releases/latest"
+$Object1 = Invoke-GitHubApi -Uri 'https://api.github.com/repos/TheLynxHub/LynxHub/releases/latest'
 
 # Version
 $this.CurrentState.Version = $Object1.tag_name -creplace '^V'
@@ -35,10 +32,11 @@ switch -Regex ($this.Check()) {
         $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
       }
 
-      # ReleaseNotesUrl
+      # ReleaseNotesUrl (en-US)
       $this.CurrentState.Locale += [ordered]@{
-        Key   = 'ReleaseNotesUrl'
-        Value = $Object1.html_url
+        Locale = 'en-US'
+        Key    = 'ReleaseNotesUrl'
+        Value  = $Object1.html_url
       }
     } catch {
       $_ | Out-Host
