fafdab00df0c38e53985b1c1238479891506ee3b
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-08-10 21:45:57 +0800

    hellodigua.ChatLab: Update repo owner

--- Tasks/hellodigua.ChatLab/Script.ps1
+++ Tasks/hellodigua.ChatLab/Script.ps1
@@ -1,4 +1,4 @@
-$Object1 = Invoke-GitHubApi -Uri 'https://api.github.com/repos/hellodigua/ChatLab/releases/latest'
+$Object1 = Invoke-GitHubApi -Uri 'https://api.github.com/repos/ChatLab/ChatLab/releases/latest'
 
 # Version
 $this.CurrentState.Version = $Object1.tag_name -replace '^v'
