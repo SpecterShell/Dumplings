6ff9f0f06b76930222a4842a9cccf47724b4891e
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-08-10 21:16:21 +0800

    Add Spokenly.Spokenly

---
+++ Tasks/Spokenly.Spokenly/Script.ps1
@@ -0,0 +1,39 @@
+$Object1 = Invoke-RestMethod -Uri 'https://spokenly.app/windows/latest.json'
+
+# Version
+$this.CurrentState.Version = $Object1.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture  = 'x64'
+  InstallerType = 'nullsoft'
+  InstallerUrl  = $Object1.platforms.'windows-x86_64'.url
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.pub_date.ToUniversalTime()
+
+      # ReleaseNotes (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotes'
+        Value  = $Object1.notes | Format-Text
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
