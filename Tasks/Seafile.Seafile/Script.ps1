16555f1f6a6232f7e5ff0324e365edf48e4fbffd
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-08-14 23:52:52 +0800

    Seafile.Seafile: Remove zh-CN installer

--- Tasks/Seafile.Seafile/Script.ps1
+++ Tasks/Seafile.Seafile/Script.ps1
@@ -1,26 +1,26 @@
 # Global
 $Object1 = Invoke-WebRequest -Uri 'https://www.seafile.com/en/download/'
 # China
-$Object2 = Invoke-WebRequest -Uri 'https://www.seafile.com/download/'
+# $Object2 = Invoke-WebRequest -Uri 'https://www.seafile.com/download/'
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerLocale = 'en-US'
+  # InstallerLocale = 'en-US'
   InstallerUrl    = $InstallerUrl = $Object1.Links.Where({ try { $_.href -match 'seafile-\d+(?:\.\d+)+-en+\.msi$' } catch {} }, 'First')[0].href
 }
 $Version = [regex]::Match($InstallerUrl, '(\d+(\.\d+)+)').Groups[1].Value
 
-$this.CurrentState.Installer += [ordered]@{
-  InstallerLocale = 'zh-CN'
-  InstallerUrl    = $InstallerUrlCN = $Object2.Links.Where({ try { $_.href -match 'seafile-\d+(?:\.\d+)+\.msi$' } catch {} }, 'First')[0].href
-}
-$VersionCN = [regex]::Match($InstallerUrlCN, '(\d+(\.\d+)+)').Groups[1].Value
+# $this.CurrentState.Installer += [ordered]@{
+#   InstallerLocale = 'zh-CN'
+#   InstallerUrl    = $InstallerUrlCN = $Object2.Links.Where({ try { $_.href -match 'seafile-\d+(?:\.\d+)+\.msi$' } catch {} }, 'First')[0].href
+# }
+# $VersionCN = [regex]::Match($InstallerUrlCN, '(\d+(\.\d+)+)').Groups[1].Value
 
-if ($Version -ne $VersionCN) {
-  $this.Log("Global version: ${Version}")
-  $this.Log("China version: ${VersionCN}")
-  throw 'Inconsistent versions detected'
-}
+# if ($Version -ne $VersionCN) {
+#   $this.Log("Global version: ${Version}")
+#   $this.Log("China version: ${VersionCN}")
+#   throw 'Inconsistent versions detected'
+# }
 
 # Version
 $this.CurrentState.Version = $Version
