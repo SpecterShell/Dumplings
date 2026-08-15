243e90e74ab15760430d0500950996eed0b67a39
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-08-15 23:14:42 +0800

    PWPW: Parse version from installer URLs

--- Tasks/PWPW.SigillumSign/Script.ps1
+++ Tasks/PWPW.SigillumSign/Script.ps1
@@ -1,95 +1,33 @@
-function Read-Installer {
-  $this.InstallerFiles[$this.CurrentState.Installer[0].InstallerUrl] = $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
-  # Version
-  $this.CurrentState.Version = $InstallerFile | Read-ProductVersionFromMsi
-  # InstallerSha256
-  $this.CurrentState.Installer[0]['InstallerSha256'] = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
-}
-
-$this.CurrentState.Installer += [ordered]@{
+# Installer
+$this.CurrentState.Installer += $InstallerX86 = [ordered]@{
   Architecture = 'x86'
   InstallerUrl = Join-Uri $Global:DumplingsStorage.PWPWDownloadPagePrefix $Global:DumplingsStorage.PWPWDownloadPage.Links.Where({ try { $_.href -match 'Sigillum_[\d.]+_x86\.msi$' } catch {} }, 'First')[0].href
 }
-$this.CurrentState.Installer += [ordered]@{
+$VersionX86 = [regex]::Match($InstallerX86.InstallerUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
+
+$this.CurrentState.Installer += $InstallerX64 = [ordered]@{
   Architecture = 'x64'
   InstallerUrl = Join-Uri $Global:DumplingsStorage.PWPWDownloadPagePrefix $Global:DumplingsStorage.PWPWDownloadPage.Links.Where({ try { $_.href -match 'Sigillum_[\d.]+_x64\.msi$' } catch {} }, 'First')[0].href
 }
+$VersionX64 = [regex]::Match($InstallerX64.InstallerUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
 
-$Object1 = Invoke-WebRequest -Uri $this.CurrentState.Installer[0].InstallerUrl -Method Head
-$ETag = $Object1.Headers.ETag[0]
-
-# Case 0: Force submit the manifest
-if ($Global:DumplingsPreference.Contains('Force')) {
-  $this.Log('Skip checking states', 'Info')
-
-  # ETag
-  $this.CurrentState.ETag = @($ETag)
-
-  Read-Installer
-
-  $this.Print()
-  $this.Write()
-  $this.Message()
-  $this.Submit()
+if ($VersionX86 -ne $VersionX64) {
+  $this.Log("Inconsistent versions: x64: ${VersionX86}, arm64: ${VersionX64}", 'Error')
   return
 }
 
-# Case 1: The task is new
-if ($this.Status.Contains('New')) {
-  $this.Log('New task', 'Info')
-
-  # ETag
-  $this.CurrentState.ETag = @($ETag)
-
-  Read-Installer
-
-  $this.Print()
-  $this.Write()
-  return
-}
-
-# Case 2: The ETag is unchanged
-if ($ETag -in $this.LastState.ETag) {
-  $this.Log("The version $($this.LastState.Version) from the last state is the latest (Global)", 'Info')
-  return
-}
-
-Read-Installer
-
-# Case 3: The current state has an invalid version
-if ([string]::IsNullOrWhiteSpace($this.CurrentState.Version)) {
-  throw 'The current state has an invalid version'
-}
-
-# Case 4: The ETag has changed, but the SHA256 is not
-if ($this.CurrentState.Installer[0].InstallerSha256 -eq $this.LastState.Installer[0].InstallerSha256) {
-  $this.Log('The ETag has changed, but the SHA256 is not', 'Info')
-
-  # ETag
-  $this.CurrentState.ETag = $this.LastState.ETag + $ETag
-
-  $this.Write()
-  return
-}
-
-# ETag
-$this.CurrentState.ETag = @($ETag)
+# Version
+$this.CurrentState.Version = $VersionX64
 
 switch -Regex ($this.Check()) {
-  # Case 6: The ETag, the SHA256 and the version have changed
-  'Updated|Rollbacked' {
+  'New|Changed|Updated' {
     $this.Print()
     $this.Write()
-    $this.Message()
-    $this.Submit()
   }
-  # Case 5: The ETag and the SHA256 have changed, but the version is not
-  default {
-    $this.Log('The ETag and the SHA256 have changed, but the version is not', 'Info')
-    $this.Config.IgnorePRCheck = $true
-    $this.Print()
-    $this.Write()
+  'Changed|Updated' {
     $this.Message()
+  }
+  'Updated' {
     $this.Submit()
   }
 }
