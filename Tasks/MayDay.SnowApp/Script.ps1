6216818452ecb068252210f7aac8d1962a525bbc
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-08-16 12:30:15 +0800

    Add MayDay.SnowApp

--- Tasks/MayDay.SnowApp/Script.ps1
+++ Tasks/MayDay.SnowApp/Script.ps1
@@ -1,4 +1,4 @@
-$Object1 = Invoke-GitHubApi -Uri 'https://api.github.com/repos/incldue/CineFlow/releases/latest'
+$Object1 = Invoke-GitHubApi -Uri 'https://api.github.com/repos/MayDay-wpf/snow-app/releases/latest'
 
 # Version
 $this.CurrentState.Version = $Object1.tag_name -replace '^v'
@@ -16,22 +16,23 @@ switch -Regex ($this.Check()) {
       $this.CurrentState.ReleaseTime = $Object1.published_at.ToUniversalTime()
 
       if (-not [string]::IsNullOrWhiteSpace($Object1.body)) {
-        # ReleaseNotes (zh-CN)
+        # ReleaseNotes (en-US)
         $this.CurrentState.Locale += [ordered]@{
-          Locale = 'zh-CN'
+          Locale = 'en-US'
           Key    = 'ReleaseNotes'
           Value  = $Object1.body | Convert-MarkdownToHtml -Extensions 'advanced', 'emojis', 'hardlinebreak' | Get-TextContent | Format-Text
         }
       } else {
-        $this.Log("No ReleaseNotes (zh-CN) for version $($this.CurrentState.Version)", 'Warning')
+        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
       }
 
-      # ReleaseNotesUrl (zh-CN)
+      # ReleaseNotesUrl (en-US)
       $this.CurrentState.Locale += [ordered]@{
-        Locale = 'zh-CN'
+        Locale = 'en-US'
         Key    = 'ReleaseNotesUrl'
         Value  = $Object1.html_url
       }
+
     } catch {
       $_ | Out-Host
       $this.Log($_, 'Warning')
