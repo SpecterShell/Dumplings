a32cae330d27862bd5a52a6e8112beb0111d2278
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-08-21 12:10:13 +0800

    BitSum.ProcessLasso: Update

--- Tasks/BitSum.ProcessLasso.Beta/Script.ps1
+++ Tasks/BitSum.ProcessLasso.Beta/Script.ps1
@@ -1,101 +1,38 @@
-function Read-Installer {
-  $this.InstallerFiles[$this.CurrentState.Installer[0].InstallerUrl] = $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
-  # Version
-  $this.CurrentState.Version = $InstallerFile | Read-FileVersionFromExe
-  # InstallerSha256
-  $this.CurrentState.Installer[0]['InstallerSha256'] = (Get-FileHash -Path $InstallerFile -Algorithm SHA256).Hash
-}
+$Prefix = 'https://dl.bitsum.com/files/'
+$Object1 = (Invoke-WebRequest -Uri "${Prefix}processlasso-releases.json").Content | ConvertFrom-Json -AsHashtable
+$Object2 = $Object1.releases.Where({ $_.channel -eq 'beta' }, 'First')[0]
 
-$Object1 = Invoke-WebRequest -Uri 'https://bitsum.com/download-process-lasso/'
+# Version
+$this.CurrentState.Version = $Object2.version
 
-# x86
-$this.CurrentState.Installer += $InstallerX86 = [ordered]@{
+# Installer
+$this.CurrentState.Installer += [ordered]@{
   Architecture = 'x86'
-  InstallerUrl = $Object1.Links.Where({ try { $_.href.EndsWith('.exe') -and $_.href.Contains('beta') -and $_.href.Contains('32') } catch {} }, 'First')[0].href
+  InstallerUrl = $Prefix + 'beta/' + $Object2.files.Keys.Where({ $_ -match '32' }, 'First')[0]
 }
-$Object2 = Invoke-WebRequest -Uri $InstallerX86.InstallerUrl -Method Head
-# Last Modified
-$this.CurrentState.LastModified = $Object2.Headers.'Last-Modified'[0]
-
 # x64
-$this.CurrentState.Installer += $InstallerX64 = [ordered]@{
+$this.CurrentState.Installer += [ordered]@{
   Architecture = 'x64'
-  InstallerUrl = $Object1.Links.Where({ try { $_.href.EndsWith('.exe') -and $_.href.Contains('beta') -and $_.href.Contains('64') } catch {} }, 'First')[0].href
-}
-$Object3 = Invoke-WebRequest -Uri $InstallerX64.InstallerUrl -Method Head
-# Last Modified
-$this.CurrentState.LastModifiedX64 = $Object3.Headers.'Last-Modified'[0]
-
-# Case 0: Force submit the manifest
-if ($Global:DumplingsPreference.Contains('Force')) {
-  $this.Log('Skip checking states', 'Info')
-
-  Read-Installer
-
-  $this.Print()
-  $this.Write()
-  $this.Message()
-  $this.Submit()
-  return
-}
-
-# Case 1: The task is new
-if ($this.Status.Contains('New')) {
-  $this.Log('New task', 'Info')
-
-  Read-Installer
-
-  $this.Print()
-  $this.Write()
-  return
-}
-
-# Case 2: The Last Modified is unchanged
-if ([datetime]$this.CurrentState.LastModified -eq [datetime]$this.LastState.LastModified) {
-  $this.Log("The version $($this.LastState.Version) from the last state is the latest (x86)", 'Info')
-  return
-} elseif ([datetime]$this.CurrentState.LastModified -lt [datetime]$this.LastState.LastModified) {
-  $this.Log("The last modified datetime from the current state `"$($this.CurrentState.LastModified)`" is older than the one from the last state `"$($this.LastState.LastModified)`" (x86)", 'Warning')
-  return
-}
-if ([datetime]$this.CurrentState.LastModifiedX64 -eq [datetime]$this.LastState.LastModifiedX64) {
-  $this.Log("The version $($this.LastState.Version) from the last state is the latest (x64)", 'Info')
-  return
-} elseif ([datetime]$this.CurrentState.LastModifiedX64 -lt [datetime]$this.LastState.LastModifiedX64) {
-  $this.Log("The last modified datetime from the current state `"$($this.CurrentState.LastModifiedX64)`" is older than the one from the last state `"$($this.LastState.LastModifiedX64)`" (x64)", 'Warning')
-  return
-}
-
-Read-Installer
-
-# Case 3: The current state has an invalid version
-if ([string]::IsNullOrWhiteSpace($this.CurrentState.Version)) {
-  throw 'The current state has an invalid version'
-}
-
-# Case 4: The Last Modified has changed, but the SHA256 is not
-if ($this.CurrentState.Installer[0].InstallerSha256 -eq $this.LastState.Installer[0].InstallerSha256) {
-  $this.Log('The Last Modified has changed, but the SHA256 is not', 'Info')
-
-  $this.Write()
-  return
+  InstallerUrl = $Prefix + 'beta/' + $Object2.files.Keys.Where({ $_ -match '64' }, 'First')[0]
 }
 
 switch -Regex ($this.Check()) {
-  # Case 6: The Last Modified, the SHA256 and the version have changed
-  'Updated|Rollbacked' {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object2.date | Get-Date -Format 'yyyy-MM-dd'
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
     $this.Print()
     $this.Write()
-    $this.Message()
-    $this.Submit()
   }
-  # Case 5: The Last Modified and the SHA256 have changed, but the version is not
-  default {
-    $this.Log('The Last Modified and the SHA256 have changed, but the version is not', 'Info')
-    $this.Config.IgnorePRCheck = $true
-    $this.Print()
-    $this.Write()
+  'Changed|Updated' {
     $this.Message()
+  }
+  'Updated' {
     $this.Submit()
   }
 }
