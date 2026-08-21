5370ce8d627b85648f3476c17601f24c2948b7cd
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-08-21 19:58:27 +0800

    Remove Linkly (on request)

--- Tasks/#Linkly/Script.ps1
+++
@@ -1,2 +0,0 @@
-$Global:DumplingsStorage.LinklySoftwarePage = Invoke-WebRequest -Uri 'https://linkly.com.au/resources-support/software/'
-$Global:DumplingsStorage.LinklySoftwarePageObject = $Global:DumplingsStorage.LinklySoftwarePage | ConvertFrom-Html
