f4e7ef62106550f09909863a7030c2438a4ad2e0
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-08-22 15:08:47 +0800

    DuongDieuPhap.ImageGlass: Update

--- Tasks/DuongDieuPhap.ImageGlass/Script.ps1
+++ Tasks/DuongDieuPhap.ImageGlass/Script.ps1
@@ -1,15 +1,18 @@
-$RepoOwner = 'd2phap'
-$RepoName = 'ImageGlass'
-
-$Object1 = Invoke-GitHubApi -Uri "https://api.github.com/repos/${RepoOwner}/${RepoName}/releases/latest"
+$Object1 = Invoke-GitHubApi -Uri 'https://api.github.com/repos/d2phap/ImageGlass/releases/latest'
 
 # Version
-$this.CurrentState.Version = $Object1.tag_name -creplace '^v'
+$this.CurrentState.Version = $Object1.tag_name -replace '^v'
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  Architecture = 'x64'
-  InstallerUrl = $Object1.assets.Where({ $_.name.EndsWith('.msi') -and $_.name.Contains('x64') }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
+  Architecture  = 'x64'
+  InstallerType = 'msix'
+  InstallerUrl  = $Object1.assets.Where({ $_.name.EndsWith('.msix') -and $_.name.Contains('x64') -and $_.name -notmatch 'pro|business' }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture  = 'arm64'
+  InstallerType = 'msix'
+  InstallerUrl  = $Object1.assets.Where({ $_.name.EndsWith('.msix') -and $_.name.Contains('arm64') -and $_.name -notmatch 'pro|business' }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
 }
 
 switch -Regex ($this.Check()) {
