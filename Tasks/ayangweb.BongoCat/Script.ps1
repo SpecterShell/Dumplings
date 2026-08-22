1075263a23f5d42e797c49a57d778f6d41183c74
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-08-22 15:25:49 +0800

    ayangweb.BongoCat: Update repo owner

--- Tasks/ayangweb.BongoCat/Script.ps1
+++ Tasks/ayangweb.BongoCat/Script.ps1
@@ -1,7 +1,4 @@
-$RepoOwner = 'ayangweb'
-$RepoName = 'BongoCat'
-
-$Object1 = Invoke-GitHubApi -Uri "https://api.github.com/repos/${RepoOwner}/${RepoName}/releases/latest"
+$Object1 = Invoke-GitHubApi -Uri "https://api.github.com/repos/BongoCatPet/BongoCat/releases/latest"
 
 # Version
 $this.CurrentState.Version = $Object1.tag_name -creplace '^v'
