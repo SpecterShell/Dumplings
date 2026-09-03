05ab14e8495dc8b38bde5d5e7840dcf5232c408d
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-09-03 14:23:01 +0800

    Perimeter81.HarmonySASE: Update

--- Tasks/Perimeter81.HarmonySASE/Script.ps1
+++ Tasks/Perimeter81.HarmonySASE/Script.ps1
@@ -1,12 +1,25 @@
-$Object1 = Invoke-WebRequest -Uri 'https://support.perimeter81.com/docs/downloading-the-agent'
+$Object1 = Invoke-WebRequest -Uri 'https://sc1.checkpoint.com/documents/Infinity_Portal/WebAdminGuides/EN/SASE-Admin-Guide/SASE_Security/Topics/introduction/supported_devices_and_operating_systems.html'
 
 # Installer
-$this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = $Object1.Links.Where({ try { $_.href.EndsWith('.msi') } catch {} }, 'First')[0].href
+$this.CurrentState.Installer += $InstallerX64 = [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = $Object1.Links.Where({ try { $_.href.EndsWith('.msi') -and -not $_.href.Contains('arm64') } catch {} }, 'First')[0].href
+}
+$VersionX64 = [regex]::Match($InstallerX64.InstallerUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
+
+$this.CurrentState.Installer += $InstallerARM64 = [ordered]@{
+  Architecture = 'arm64'
+  InstallerUrl = $Object1.Links.Where({ try { $_.href.EndsWith('.msi') -and $_.href.Contains('arm64') } catch {} }, 'First')[0].href
+}
+$VersionARM64 = [regex]::Match($InstallerARM64.InstallerUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
+
+if ($VersionX64 -ne $VersionARM64) {
+  $this.Log("Inconsistent versions: x64: ${VersionX64}, arm64: ${VersionARM64}", 'Error')
+  return
 }
 
 # Version
-$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
+$this.CurrentState.Version = $VersionX64
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
