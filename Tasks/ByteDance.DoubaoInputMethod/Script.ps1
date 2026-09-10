87ac69c46eec1f46c4a754c9a5f944983dc0c675
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-09-10 17:01:50 +0800

    Add ByteDance.DoubaoInputMethod

---
+++ Tasks/ByteDance.DoubaoInputMethod/Script.ps1
@@ -0,0 +1,32 @@
+$Object1 = Invoke-RestMethod -Uri 'https://shurufa.doubao.com/api/v1/app/download_url?platform=windows'
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture  = 'x64'
+  InstallerType = 'inno'
+  InstallerUrl  = $Object1.data.url | ConvertTo-UnescapedUri
+}
+
+# Version
+$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.data.release_date | Get-Date -Format 'yyyy-MM-dd'
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
