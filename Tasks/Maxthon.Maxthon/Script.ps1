0dd73b514083e315e6e21c8bee5719d084841337
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-09-10 12:59:33 +0800

    Maxthon.Maxthon: Remove x86 installers

--- Tasks/Maxthon.Maxthon/Script.ps1
+++ Tasks/Maxthon.Maxthon/Script.ps1
@@ -16,18 +16,19 @@ $Aes.Dispose()
 $AesDecryptor.Dispose()
 
 # Global x86
-$Object3 = $Object2.win10.en.maxthon86.Where({ $_.channels -contains 'stable' }, 'First')[0]
+# $Object3 = $Object2.win10.en.maxthon86.Where({ $_.channels -contains 'stable' }, 'First')[0]
 # Global x64
 $Object4 = $Object2.win10.en.maxthon64.Where({ $_.channels -contains 'stable' }, 'First')[0]
 # China x86
-$Object5 = $Object2.win10.cn.maxthon86.Where({ $_.channels -contains 'stable' }, 'First')[0]
+# $Object5 = $Object2.win10.cn.maxthon86.Where({ $_.channels -contains 'stable' }, 'First')[0]
 # China x64
 $Object6 = $Object2.win10.cn.maxthon64.Where({ $_.channels -contains 'stable' }, 'First')[0]
 
-if ((@($Object3, $Object4, $Object5, $Object6) | Sort-Object -Property 'version' -Unique).Count -gt 1) {
-  $this.Log("Global x86 version: $($Object3.version)")
+# if ((@($Object3, $Object4, $Object5, $Object6) | Sort-Object -Property 'version' -Unique).Count -gt 1) {
+if ((@($Object4, $Object6) | Sort-Object -Property 'version' -Unique).Count -gt 1) {
+  # $this.Log("Global x86 version: $($Object3.version)")
   $this.Log("Global x64 version: $($Object4.version)")
-  $this.Log("China x86 version: $($Object5.version)")
+  # $this.Log("China x86 version: $($Object5.version)")
   $this.Log("China x64 version: $($Object6.version)")
   throw 'Inconsistent versions detected'
 }
@@ -36,25 +37,55 @@ if ((@($Object3, $Object4, $Object5, $Object6) | Sort-Object -Property 'version'
 $this.CurrentState.Version = $Object4.version
 
 # Installer
+# $this.CurrentState.Installer += [ordered]@{
+#   Architecture  = 'x86'
+#   InstallerType = 'exe'
+#   InstallerUrl  = $Object3.url
+# }
+# $this.CurrentState.Installer += [ordered]@{
+#   Architecture        = 'x86'
+#   InstallerType       = 'zip'
+#   NestedInstallerType = 'portable'
+#   InstallerUrl        = $Object3.url -replace 'maxthon_', 'maxthon_portable_' -replace '\.exe$', '.zip'
+# }
 $this.CurrentState.Installer += [ordered]@{
-  Architecture = 'x86'
-  InstallerUrl = $Object3.url
+  Architecture  = 'x64'
+  InstallerType = 'exe'
+  InstallerUrl  = $Object4.url
 }
 $this.CurrentState.Installer += [ordered]@{
-  Architecture = 'x64'
-  InstallerUrl = $Object4.url
+  Architecture        = 'x64'
+  InstallerType       = 'zip'
+  NestedInstallerType = 'portable'
+  InstallerUrl        = $Object4.url -replace 'maxthon_', 'maxthon_portable_' -replace '\.exe$', '.zip'
 }
-$this.CurrentState.Installer += [ordered]@{
-  InstallerLocale = 'zh-CN'
-  Architecture    = 'x86'
-  # InstallerUrl    = "https://dl-cn.maxthon.com/mx7/maxthon_$($Object5.version)_x64.exe"
-  InstallerUrl    = $Object5.url
-}
+# $this.CurrentState.Installer += [ordered]@{
+#   InstallerLocale = 'zh-CN'
+#   Architecture    = 'x86'
+#   InstallerType   = 'exe'
+#   # InstallerUrl    = "https://dl-cn.maxthon.com/mx7/maxthon_$($Object5.version)_x64.exe"
+#   InstallerUrl    = $Object5.url
+# }
+# $this.CurrentState.Installer += [ordered]@{
+#   InstallerLocale     = 'zh-CN'
+#   Architecture        = 'x86'
+#   InstallerType       = 'zip'
+#   NestedInstallerType = 'portable'
+#   InstallerUrl        = $Object5.url -replace 'maxthon_', 'maxthon_portable_' -replace '\.exe$', '.zip'
+# }
 $this.CurrentState.Installer += [ordered]@{
   InstallerLocale = 'zh-CN'
   Architecture    = 'x64'
+  InstallerType   = 'exe'
   # InstallerUrl    = "https://dl-cn.maxthon.com/mx7/maxthon_$($Object6.version)_x64.exe"
   InstallerUrl    = $Object6.url
+}
+$this.CurrentState.Installer += [ordered]@{
+  InstallerLocale     = 'zh-CN'
+  Architecture        = 'x64'
+  InstallerType       = 'zip'
+  NestedInstallerType = 'portable'
+  InstallerUrl        = $Object6.url -replace 'maxthon_', 'maxthon_portable_' -replace '\.exe$', '.zip'
 }
 
 switch -Regex ($this.Check()) {
