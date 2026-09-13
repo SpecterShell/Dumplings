46f0b9ab565e95ef43d76d51c4dee38e154fcde5
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-09-13 22:52:06 +0800

    Add Absolute

---
+++ Tasks/Absolute.SecureAccessClient/Script.ps1
@@ -0,0 +1,29 @@
+$Prefix = 'https://www.absolute.com/customers/support/windows-and-macos-client-files'
+$Object1 = Invoke-WebRequest -Uri $Prefix
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture  = 'x64'
+  InstallerType = 'wix'
+  InstallerUrl  = Join-Uri $Prefix $Object1.Links.Where({ try { $_.href -match 'SecureAccess_client' -and $_.href.EndsWith('.msi') -and $_.href.Contains('x64') -and $_.href -notmatch 'RS' } catch {} }, 'First')[0].href
+}
+
+# Version
+$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.InstallerFiles[$this.CurrentState.Installer[0].InstallerUrl] = $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+    # RealVersion
+    $this.CurrentState.RealVersion = $InstallerFile | Read-ProductVersionFromMsi
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
