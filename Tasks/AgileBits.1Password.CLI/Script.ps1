7e7e92d85092720d602979b555802efa19ac705d
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-09-14 00:35:48 +0800

    Fix InstallerUrl pattern. Add arm64 installer

--- Tasks/AgileBits.1Password.CLI/Script.ps1
+++ Tasks/AgileBits.1Password.CLI/Script.ps1
@@ -1,13 +1,20 @@
-$Object1 = Invoke-RestMethod -Uri "https://app-updates.agilebits.com/check/1/0/CLI2/en/2.0.0/N"
+$Object1 = Invoke-RestMethod -Uri 'https://app-updates.agilebits.com/check/1/0/CLI2/en/2.0.0/N'
 
 # Version
 $this.CurrentState.Version = $Object1.available -eq '0' ? $this.LastState.Version: $Object1.version
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  Architecture  = 'x64'
-  InstallerType = 'zip'
-  InstallerUrl = "https://cache.agilebits.com/dist/1P/op2/pkg/$($this.CurrentState.Version)/op_windows_amd64_v$($this.CurrentState.Version).zip"
+  Architecture        = 'x86'
+  InstallerType       = 'zip'
+  NestedInstallerType = 'portable'
+  InstallerUrl        = "https://cache.agilebits.com/dist/1P/op2/pkg/v$($this.CurrentState.Version)/op_windows_386_v$($this.CurrentState.Version).zip"
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture        = 'x64'
+  InstallerType       = 'zip'
+  NestedInstallerType = 'portable'
+  InstallerUrl        = "https://cache.agilebits.com/dist/1P/op2/pkg/v$($this.CurrentState.Version)/op_windows_amd64_v$($this.CurrentState.Version).zip"
 }
 
 switch -Regex ($this.Check()) {
