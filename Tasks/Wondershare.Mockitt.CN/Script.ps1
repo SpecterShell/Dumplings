d3e632ad4a417d25df47a18fafbc06c1ea8709a2
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-09-14 21:32:21 +0800

    Wondershare.Mockitt.CN: Update

--- Tasks/Wondershare.Mockitt.CN/Script.ps1
+++ Tasks/Wondershare.Mockitt.CN/Script.ps1
@@ -1,20 +1,17 @@
-$Object1 = Invoke-WebRequest -Uri 'https://modao.cc/feature/downloads.html' | ConvertFrom-Html
-
-# Version
-$this.CurrentState.Version = [regex]::Match(
-  $Object1.SelectSingleNode('/html/body/main/section[2]/div/p').InnerText,
-  'v([\d\.]+)'
-).Groups[1].Value
+$Object1 = Invoke-WebRequest -Uri 'https://modao.cc/feature/downloads' | ConvertFrom-Html
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
   Architecture = 'x86'
-  InstallerUrl = Join-Uri 'https://cdn-release.modao.cc/' $Object1.SelectSingleNode('//*[contains(@class, "btn-download-pc") and contains(@data-href, ".exe") and contains(@data-href, "ia32")]').Attributes['data-href'].Value
+  InstallerUrl = Join-Uri 'https://cdn-release.modao.cc/' $Object1.SelectSingleNode('//*[contains(@class, "btn-download-pc") and contains(@data-href, ".exe") and contains(@data-href, "ia32") and not(contains(@data-href, "modao-ai"))]').Attributes['data-href'].Value
 }
 $this.CurrentState.Installer += [ordered]@{
   Architecture = 'x64'
-  InstallerUrl = Join-Uri 'https://cdn-release.modao.cc/' $Object1.SelectSingleNode('//*[contains(@class, "btn-download-pc") and contains(@data-href, ".exe") and contains(@data-href, "x64")]').Attributes['data-href'].Value
+  InstallerUrl = Join-Uri 'https://cdn-release.modao.cc/' $Object1.SelectSingleNode('//*[contains(@class, "btn-download-pc") and contains(@data-href, ".exe") and contains(@data-href, "x64") and not(contains(@data-href, "modao-ai"))]').Attributes['data-href'].Value
 }
+
+# Version
+$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d+(?:\.\d+)+)').Groups[1].Value
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
