3eba29c45e98300113bdd2c00ebe4967a82ca222
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-09-14 23:39:11 +0800

    Add Argente.PasswordManager

---
+++ Tasks/Argente.PasswordManager/Script.ps1
@@ -0,0 +1,44 @@
+# x86
+$Object1 = $Global:DumplingsStorage.ArgenteX86.data.Where({ $_.name -eq 'Argente Password Manager' -and $_.installer -eq 'passwordmanagerx86' }, 'First')[0]
+# x64
+$Object2 = $Global:DumplingsStorage.ArgenteX64.data.Where({ $_.name -eq 'Argente Password Manager' -and $_.installer -eq 'passwordmanagerx64' }, 'First')[0]
+# arm64
+$Object3 = $Global:DumplingsStorage.ArgenteARM64.data.Where({ $_.name -eq 'Argente Password Manager' -and $_.installer -eq 'passwordmanagerarm64' }, 'First')[0]
+
+if (@(@($Object1.version, $Object2.version, $Object3.version) | Sort-Object -Unique).Count -gt 1) {
+  $this.Log("Inconsistent versions: x86: $($Object1.version), x64: $($Object2.version), arm64: $($Object3.version)", 'Error')
+  return
+}
+
+# Version
+$this.CurrentState.Version = $Object2.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture  = 'x86'
+  InstallerType = 'inno'
+  InstallerUrl  = 'https://argenteutilities.com/en/download/passwordmanagerx86'
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture  = 'x64'
+  InstallerType = 'inno'
+  InstallerUrl  = 'https://argenteutilities.com/en/download/passwordmanagerx64'
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture  = 'arm64'
+  InstallerType = 'inno'
+  InstallerUrl  = 'https://argenteutilities.com/en/download/passwordmanagerarm64'
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
