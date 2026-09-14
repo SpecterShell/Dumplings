7624604f8bad5204445f82dfa43b0979fd97d0a2
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-09-15 00:34:38 +0800

    ZSeven-W.OpenPencil.CLI: Fix InstallerType

--- Tasks/ZSeven-W.OpenPencil.CLI/Script.ps1
+++ Tasks/ZSeven-W.OpenPencil.CLI/Script.ps1
@@ -5,14 +5,16 @@ $this.CurrentState.Version = $Object1.tag_name -replace '^v'
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  Architecture  = 'x64'
-  InstallerType = 'nullsoft'
-  InstallerUrl  = $Object1.assets.Where({ $_.name -match 'op-cli' -and $_.name.EndsWith('.zip') -and $_.name.Contains('x86_64') -and $_.name -match 'windows' }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
+  Architecture        = 'x64'
+  InstallerType       = 'zip'
+  NestedInstallerType = 'portable'
+  InstallerUrl        = $Object1.assets.Where({ $_.name -match 'op-cli' -and $_.name.EndsWith('.zip') -and $_.name.Contains('x86_64') -and $_.name -match 'windows' }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
 }
 $this.CurrentState.Installer += [ordered]@{
-  Architecture  = 'arm64'
-  InstallerType = 'nullsoft'
-  InstallerUrl  = $Object1.assets.Where({ $_.name -match 'op-cli' -and $_.name.EndsWith('.zip') -and $_.name.Contains('aarch64') -and $_.name -match 'windows' }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
+  Architecture        = 'arm64'
+  InstallerType       = 'zip'
+  NestedInstallerType = 'portable'
+  InstallerUrl        = $Object1.assets.Where({ $_.name -match 'op-cli' -and $_.name.EndsWith('.zip') -and $_.name.Contains('aarch64') -and $_.name -match 'windows' }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
 }
 
 switch -Regex ($this.Check()) {
