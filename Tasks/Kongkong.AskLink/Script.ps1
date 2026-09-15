2d1a964d261ebd82430d7b394d1fbbc6a6aab486
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-09-15 15:44:17 +0800

    Kongkong.AskLink: Switch to the version property of the feed as the version field value of the task

--- Tasks/Kongkong.AskLink/Script.ps1
+++ Tasks/Kongkong.AskLink/Script.ps1
@@ -1,7 +1,7 @@
 $Object1 = (Invoke-RestMethod -Uri 'https://api.asklink.com/download/list').data.Where({ $_.clientType -eq 'WIN' }, 'First')[0]
 
 # Version
-$this.CurrentState.Version = [regex]::Match($Object1.name, '(\d+(?:\.\d+)+)').Groups[1].Value
+$this.CurrentState.Version = $Object1.version
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
