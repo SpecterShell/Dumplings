156f77381fe5cc5cffa9a5ef3099360f82329040
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-09-15 15:22:13 +0800

    Add Amazon.KiroCrew

---
+++ Tasks/Amazon.KiroCrew/Script.ps1
@@ -0,0 +1,64 @@
+$Prefix = 'https://updates.crew.kiro.dev/feed/stable/'
+
+$Feed = Invoke-RestMethod -Uri "${Prefix}latest.yml" | ConvertFrom-ElectronBuilderUpdateFeed
+
+# Version
+$this.CurrentState.Version = $Feed.Version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = Join-Uri $Prefix $Feed.Files[0].Url
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Feed.ReleaseDate | Get-Date -AsUTC
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    try {
+      # ReleaseNotesUrl (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotesUrl'
+        Value  = "https://github.com/kirodotdev/KiroCrew/releases"
+      }
+
+      $Object2 = Invoke-GitHubApi -Uri "https://api.github.com/repos/kirodotdev/KiroCrew/releases/tags/v$($this.CurrentState.Version)"
+
+      if (-not [string]::IsNullOrWhiteSpace($Object2.body)) {
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $Object2.body | Convert-MarkdownToHtml -Extensions 'advanced', 'emojis', 'hardlinebreak' | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+
+      # ReleaseNotesUrl (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotesUrl'
+        Value  = "https://github.com/kirodotdev/KiroCrew/releases/tag/v$($this.CurrentState.Version)"
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
