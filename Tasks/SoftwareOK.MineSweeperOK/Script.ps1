885d7e51b0fba209fe5389386b06bd21febaddec
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-09-18 01:08:46 +0800

    Add SoftwareOK

---
+++ Tasks/SoftwareOK.MineSweeperOK/Script.ps1
@@ -0,0 +1,49 @@
+$Object1 = Invoke-WebRequest -Uri 'https://www.softwareok.com/?seite=Microsoft/MineSweeperOK/Autoupdate'
+
+# Version
+$this.CurrentState.Version = [regex]::Match($Object1.Content, 'Current version: (\d+(?:\.\d+)+)').Groups[1].Value
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x86'
+  InstallerUrl = 'https://www.softwareok.com/Download/MineSweeperOK_Installer.zip'
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      $Object2 = Invoke-WebRequest -Uri 'https://www.softwareok.com/?seite=Microsoft/MineSweeperOK/History' | ConvertFrom-Html
+
+      $ReleaseNotesTitleNode = $Object2.SelectSingleNode("//b[contains(text(), 'New in version $($this.CurrentState.Version)')]")
+      if ($ReleaseNotesTitleNode) {
+        # ReleaseTime
+        $this.CurrentState.ReleaseTime = [regex]::Match(
+          $ReleaseNotesTitleNode.InnerText,
+          '(\d{1,2}\W+[a-zA-Z]+\W+20\d{2})'
+        ).Groups[1].Value | Get-Date -Format 'yyyy-MM-dd'
+
+        $ReleaseNotesNodes = for ($Node = $ReleaseNotesTitleNode.NextSibling; $Node -and -not $Node.InnerText.Contains('Back to MineSweeperOK description') -and -not $Node.InnerText.Contains('New in version'); $Node = $Node.NextSibling) { $Node }
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesNodes | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseTime and ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
