50a17254f83862c0b24e13701ae46d105528beec
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-09-17 22:27:37 +0800

    Add soulvon.AnyBridge

---
+++ Tasks/soulvon.AnyBridge/Script.ps1
@@ -0,0 +1,55 @@
+$Object1 = Invoke-RestMethod -Uri 'https://github.com/soulvon/AnyBridge/releases/latest/download/latest.json'
+
+# Version
+$this.CurrentState.Version = $Object1.version -replace '^v'
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture  = 'x64'
+  InstallerType = 'nullsoft'
+  InstallerUrl  = $Object1.platforms.'windows-x86_64'.url | ConvertTo-UnescapedUri
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture  = 'x64'
+  InstallerType = 'wix'
+  InstallerUrl  = $Object1.platforms.'windows-x86_64-msi'.url | ConvertTo-UnescapedUri
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.pub_date | Get-Date -AsUTC
+
+      if (-not [string]::IsNullOrWhiteSpace($Object1.notes)) {
+        # ReleaseNotes (zh-CN)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'zh-CN'
+          Key    = 'ReleaseNotes'
+          Value  = $Object1.notes
+        }
+      } else {
+        $this.Log("No ReleaseNotes (zh-CN) for version $($this.CurrentState.Version)", 'Warning')
+      }
+
+      # ReleaseNotesUrl (zh-CN)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'zh-CN'
+        Key    = 'ReleaseNotesUrl'
+        Value  = "https://github.com/soulvon/AnyBridge/releases/tag/v$($this.CurrentState.Version)"
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
