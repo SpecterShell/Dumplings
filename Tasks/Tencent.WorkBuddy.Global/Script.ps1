a1d3911ad52bbb13eeb3924f12462c11111feb0a
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-09-18 22:45:05 +0800

    Add Tencent.WorkBuddy.Global

---
+++ Tasks/Tencent.WorkBuddy.Global/Script.ps1
@@ -0,0 +1,35 @@
+$Object1 = Invoke-RestMethod -Uri 'https://www.workbuddy.ai/v2/update?platform=workbuddy-win32-x64-user'
+
+# Version
+$this.CurrentState.Version = $Object1.productVersion
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = $Object1.url
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.timestamp | ConvertFrom-UnixTimeSeconds
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.InstallerFiles[$this.CurrentState.Installer[0].InstallerUrl] = $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+    # RealVersion
+    $this.CurrentState.RealVersion = $InstallerFile | Read-ProductVersionFromExe
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
