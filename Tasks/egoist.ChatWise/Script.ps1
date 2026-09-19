bc0c111a66e63e0fb1fe6b66fe78ef75d6b02b45
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-09-19 17:00:03 +0800

    Add egoist.ChatWise

---
+++ Tasks/egoist.ChatWise/Script.ps1
@@ -0,0 +1,46 @@
+$Prefix = 'https://releases.chatwise.app/'
+
+$Object1 = Invoke-RestMethod -Uri "${Prefix}latest.yml" | ConvertFrom-ElectronBuilderUpdateFeed
+
+# Version
+$this.CurrentState.Version = $Object1.Version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = Join-Uri $Prefix $Object1.Files[0].Url
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.ReleaseDate | Get-Date -AsUTC
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    try {
+      $Release = (Invoke-RestMethod -Uri "${Prefix}releases").Where({ $_.version -eq $this.CurrentState.Version }, 'First')[0]
+
+      # ReleaseNotes (en-US)
+      $this.CurrentState.Locale += [ordered]@{
+        Locale = 'en-US'
+        Key    = 'ReleaseNotes'
+        Value  = $Release.changelog | Format-Text
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
