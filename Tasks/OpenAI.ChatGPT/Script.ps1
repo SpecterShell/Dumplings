c89d5f55e3d1e9469514903b74857ce571dc0ec5
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-09-21 00:28:16 +0800

    OpenAI.ChatGPT: Add InstallerType

--- Tasks/OpenAI.ChatGPT/Script.ps1
+++ Tasks/OpenAI.ChatGPT/Script.ps1
@@ -5,12 +5,14 @@ $this.CurrentState.Version = $Object1.buildVersion
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  Architecture = 'x64'
-  InstallerUrl = "https://persistent.oaistatic.com/codex-app-prod/releases/$($this.CurrentState.Version)/ChatGPT-x64.msix"
+  Architecture  = 'x64'
+  InstallerType = 'msix'
+  InstallerUrl  = "https://persistent.oaistatic.com/codex-app-prod/releases/$($this.CurrentState.Version)/ChatGPT-x64.msix"
 }
 $this.CurrentState.Installer += [ordered]@{
-  Architecture = 'arm64'
-  InstallerUrl = "https://persistent.oaistatic.com/codex-app-prod/releases/$($this.CurrentState.Version)/ChatGPT-arm64.msix"
+  Architecture  = 'arm64'
+  InstallerType = 'msix'
+  InstallerUrl  = "https://persistent.oaistatic.com/codex-app-prod/releases/$($this.CurrentState.Version)/ChatGPT-arm64.msix"
 }
 
 switch -Regex ($this.Check()) {
