70f6921ea4d4e070f9d24eee8459ba53c202092f
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-09-20 11:07:41 +0800

    Add MacPawWay.ClearVPN

---
+++ Tasks/MacPawWay.ClearVPN/Script.ps1
@@ -0,0 +1,43 @@
+$Object1 = Invoke-RestMethod -Uri 'https://clearvpn.com/downloads/windows/com.macpaw.clearvpn.windows.xml'
+
+# Version
+$this.CurrentState.Version = $Object1.enclosure.version
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.enclosure.url
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    try {
+      # ReleaseTime
+      $this.CurrentState.ReleaseTime = $Object1.pubDate | Get-Date -AsUTC
+
+      $ReleaseNotesObject = $Object1.description | ConvertFrom-Html
+      $ReleaseNotesNode = $ReleaseNotesObject.SelectSingleNode('//div[@lang="en"]')
+      if ($ReleaseNotesNode) {
+        # ReleaseNotes (en-US)
+        $this.CurrentState.Locale += [ordered]@{
+          Locale = 'en-US'
+          Key    = 'ReleaseNotes'
+          Value  = $ReleaseNotesNode | Get-TextContent | Format-Text
+        }
+      } else {
+        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+      }
+    } catch {
+      $_ | Out-Host
+      $this.Log($_, 'Warning')
+    }
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
