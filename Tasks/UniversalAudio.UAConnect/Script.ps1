e0e6bb3ea532efb499d81d054139379576a83f4b
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-09-20 14:04:03 +0800

    Add UniversalAudio.UAConnect

---
+++ Tasks/UniversalAudio.UAConnect/Script.ps1
@@ -0,0 +1,24 @@
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = Get-RedirectedUrl -Uri 'https://www.uaudio.com/apps/uaconnect/win/installer'
+}
+
+# Version
+$this.CurrentState.Version = [regex]::Match($this.CurrentState.Installer[0].InstallerUrl, '(\d+(?:_\d+)+)').Groups[1].Value -replace '_', '.'
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.InstallerFiles[$this.CurrentState.Installer[0].InstallerUrl] = $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl
+    # RealVersion
+    $this.CurrentState.RealVersion = $InstallerFile | Read-ProductVersionFromExe
+
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
