15b4b0e47058f18de7eede607c6d7a07ff2795bb
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-09-23 15:30:56 +0800

    Bionus.Imgbrd-Grabber: Preserve version prefix

--- Tasks/Bionus.Imgbrd-Grabber/Script.ps1
+++ Tasks/Bionus.Imgbrd-Grabber/Script.ps1
@@ -1,7 +1,7 @@
 $Object1 = Invoke-GitHubApi -Uri 'https://api.github.com/repos/Bionus/imgbrd-grabber/releases/latest'
 
 # Version
-$this.CurrentState.Version = $Object1.tag_name -replace '^v'
+$this.CurrentState.Version = $Object1.tag_name
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
