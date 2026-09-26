ce76d4acd6a0a356c9b9d9b787ce223e1e15ed92
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-09-26 20:02:39 +0800

    GauzyTech.NeatReader: Remove global installer

--- Tasks/GauzyTech.NeatReader/Script.ps1
+++ Tasks/GauzyTech.NeatReader/Script.ps1
@@ -1,6 +1,6 @@
-# Global
-$Object1 = Invoke-WebRequest -Uri 'https://www.neat-reader.com/download/start-download?target=windows' | ConvertFrom-Html
-$Version1 = $Object1.SelectSingleNode('/html/body/input[2]').Attributes['value'].Value.Trim()
+# # Global
+# $Object1 = Invoke-WebRequest -Uri 'https://www.neat-reader.com/download/start-download?target=windows' | ConvertFrom-Html
+# $Version1 = $Object1.SelectSingleNode('/html/body/input[2]').Attributes['value'].Value.Trim()
 
 # China
 $Object2 = Invoke-WebRequest -Uri 'https://www.neat-reader.cn/downloads/windows' | ConvertFrom-Html
@@ -9,21 +9,21 @@ $Version2 = [regex]::Match(
   '([\d\.]+)'
 ).Groups[1].Value
 
-if ($Version1 -ne $Version2) {
-  $this.Log("Global version: ${Version1}")
-  $this.Log("China version: ${Version2}")
-  throw 'Inconsistent versions detected'
-}
+# if ($Version1 -ne $Version2) {
+#   $this.Log("Global version: ${Version1}")
+#   $this.Log("China version: ${Version2}")
+#   throw 'Inconsistent versions detected'
+# }
 
 # Version
-$this.CurrentState.Version = $Version1
+$this.CurrentState.Version = $Version2
 
 # Installer
+# $this.CurrentState.Installer += [ordered]@{
+#   InstallerUrl = "https://neat-reader-release.oss-cn-hongkong.aliyuncs.com/NeatReader Setup ${Version1}.exe"
+# }
 $this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = "https://neat-reader-release.oss-cn-hongkong.aliyuncs.com/NeatReader Setup ${Version1}.exe"
-}
-$this.CurrentState.Installer += [ordered]@{
-  InstallerLocale = 'zh-CN'
+  # InstallerLocale = 'zh-CN'
   InstallerUrl    = $Object2.SelectSingleNode('/html/body/div[3]/div/div/div[1]/a[1]').Attributes['href'].Value | ConvertTo-UnescapedUri
 }
 
