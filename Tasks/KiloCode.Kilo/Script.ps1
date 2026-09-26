014c24577e529418558727e25ad4d284e1d55f70
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-09-26 17:01:32 +0800

    KiloCode.Kilo: Filter releases to exclude IDE plugin releases

--- Tasks/KiloCode.Kilo/Script.ps1
+++ Tasks/KiloCode.Kilo/Script.ps1
@@ -1,4 +1,4 @@
-$Object1 = Invoke-GitHubApi -Uri 'https://api.github.com/repos/Kilo-Org/kilocode/releases/latest'
+$Object1 = (Invoke-GitHubApi -Uri 'https://api.github.com/repos/Kilo-Org/kilocode/releases').Where({ -not $_.prerelease -and -not $_.draft -and $_.tag_name -like 'v*' }, 'First')[0]
 
 # Version
 $this.CurrentState.Version = $Object1.tag_name -replace '^v'
