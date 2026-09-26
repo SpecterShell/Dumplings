016c7bf5ddd09b0a976ad919f9350320902b025a
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-09-26 16:32:01 +0800

    Winamp.Winamp: Switch to download source

--- Tasks/Winamp.Winamp/Script.ps1
+++ Tasks/Winamp.Winamp/Script.ps1
@@ -1,26 +1,9 @@
-$Object1 = (Invoke-RestMethod -Uri 'http://client.winamp.com/update/latest-version.php').Split()
-
-# Version
-$this.CurrentState.Version = $Object1[0].Trim()
-
-# Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = $Object1[1].Trim() | Split-Uri -LeftPart 'Path'
+  InstallerUrl = Get-RedirectedUrl -Uri 'https://download.winamp.com/winamp/winamp_latest_full.exe'
 }
 
-switch -Regex ($this.Check()) {
-  'New|Changed|Updated' {
-    $this.InstallerFiles[$this.CurrentState.Installer[0].InstallerUrl] = $InstallerFile = Get-TempFile -Uri $this.CurrentState.Installer[0].InstallerUrl | Rename-Item -NewName { "${_}.exe" } -PassThru | Select-Object -ExpandProperty 'FullName'
-    # RealVersion
-    $this.CurrentState.RealVersion = [regex]::Match(($InstallerFile | Read-ProductVersionFromExe), '\d+(?:\.\d+){2}').Value
-
-    $this.Print()
-    $this.Write()
-  }
-  'Changed|Updated' {
-    $this.Message()
-  }
-  'Updated' {
-    $this.Submit()
-  }
-}
+$Result = $this.CheckInstallerUpdates(@{
+    Validator   = 'LastModified'
+    ReadVersion = { param($Path) Read-ProductVersionFromNSIS -Path $Path }
+  })
+$this.CompleteInstallerUpdates($Result)
