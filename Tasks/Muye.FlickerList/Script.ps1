0a4319f7d2649654b117e031850956a3464bc948
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2026-09-26 20:27:47 +0800

    Muye.FlickerList: Switch to download page source

--- Tasks/Muye.FlickerList/Script.ps1
+++ Tasks/Muye.FlickerList/Script.ps1
@@ -1,18 +1,24 @@
-$Object1 = Invoke-RestMethod -Uri "https://static.flicker.cool/flicker/download/stable/$($this.Status.Contains('New') ? '5.4.3' : $this.LastState.Version)/win32/latest.yml" | ConvertFrom-Yaml
+$Object1 = Invoke-WebRequest -Uri 'https://nartick.com/'
+$AppScriptUrl = 'https:' + [regex]::Match($Object1.Content, '//static\.flicker\.cool/flicker-home/static/js/app\.[0-9a-f]+\.js').Value
+$Object2 = Invoke-WebRequest -Uri $AppScriptUrl
+$Match = [regex]::Match($Object2.Content, '="(\d+(?:\.\d+)+)",\w+="([^"]*)",\w+="nartick://"')
+
+$Prefix = "https://static.flicker.cool/flicker/download/stable/$($Match.Groups[1].Value)$($Match.Groups[2].Value)/win32/"
+$Object3 = Invoke-RestMethod -Uri "${Prefix}latest.yml" | ConvertFrom-Yaml
 
 # Version
-$this.CurrentState.Version = $Object1.version
+$this.CurrentState.Version = $Object3.version
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
-  InstallerUrl = "https://static.flicker.cool/flicker/download/stable/$($this.CurrentState.Version)/win32/$($Object1.files[0].url)"
+  InstallerUrl = "${Prefix}$($Object3.files[0].url)"
 }
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
     try {
       # ReleaseTime
-      $this.CurrentState.ReleaseTime = $Object1.releaseDate | Get-Date -AsUTC
+      $this.CurrentState.ReleaseTime = $Object3.releaseDate | Get-Date -AsUTC
     } catch {
       $_ | Out-Host
       $this.Log($_, 'Warning')
